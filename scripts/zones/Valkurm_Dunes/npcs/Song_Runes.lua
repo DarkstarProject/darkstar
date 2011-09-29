052f5ff4bd9abde9743538c3c4c06b514b4d1a3d
dacrybabysuck <dacrybabysuck@gmail.com>
2011-09-29 02:41:39 +0000

    QUEST ADD: Path of the Bard (Bard Flag #3)

--- scripts/zones/Valkurm_Dunes/npcs/Song_Runes.lua
+++ scripts/zones/Valkurm_Dunes/npcs/Song_Runes.lua
@@ -1,31 +1,31 @@
 -----------------------------------
--- Area: Lower Jeuno
--- NPC: Bki Tbujhja
--- Involved in Quest: The Old Monument
+--  Area: Valkurm Dunes
+--  NPC: Song Runes
+--  Finishes Quest: Path of the Bard
 -----------------------------------
 
+require("scripts/globals/settings");
+require("scripts/globals/titles");
 require("scripts/globals/quests");
-require("scripts/zones/Lower_Jeuno/TextIDs");
+require("scripts/zones/Valkurm_Dunes/TextIDs");
 
 -----------------------------------
--- onTrade Action
+-- onTrade
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end; 
+end;
 
 -----------------------------------
--- onTrigger Action
+-- onTrigger
 -----------------------------------
 
 function onTrigger(player,npc)
 
-	if (player:getVar("TheOldMonument_Event") == 1) then
-		player:startEvent(0x00b5);
-	elseif (player:getVar("PathOfTheBard") == 0 and player:getQuestStatus(JEUNO,A_MINSTREL_IN_DESPAIR) == 2 and player:getQuestStatus(JEUNO,PATH_OF_THE_BARD) == 0) then
-		player:startEvent(0x00b6);
+	if (player:getVar("PathOfTheBard_Event") == 1) then
+		player:startEvent(0x0002);
 	end
-end; 
+end;
 
 -----------------------------------
 -- onEventUpdate
@@ -43,10 +43,13 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-	
-	if (csid == 0x00b5) then
-	   	player:setVar("TheOldMonument_Event",2);
-	elseif (csid == 0x00b6 and option == 0) then
-		player:setVar("PathOfTheBard_Event",1)
-	end
+
+	if (csid == 0x0002) then
+		player:completeQuest(JEUNO,PATH_OF_THE_BARD);
+		player:addGil(GIL_RATE*3000);
+		player:addFame(BASTOK,BAS_FAME*10);
+		player:addFame(SAN_D_ORIA,SAN_FAME*10);
+		player:addFame(WINDURST,WIN_FAME*10);
+		player:setVar("PathOfTheBard_Event",0);
+		player:unlockJob(10); -- Bard
 end;
