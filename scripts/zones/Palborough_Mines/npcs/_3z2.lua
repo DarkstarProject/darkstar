a13704ca589cbee6fc29a8edaf3391de37284f79
onetimeffxi <onetimeffxi@gmail.com>
2011-10-08 22:28:43 +0000

    Boat at the dock in Palborough Mines to zone you into Zeruhn.

---
+++ scripts/zones/Palborough_Mines/npcs/_3z2.lua
@@ -0,0 +1,48 @@
+-----------------------------------
+-- Area: Palborough Mines
+-- NPC: Old Toolbox
+-- Continues Quest: The Eleventh's Hour (100%)
+-----------------------------------
+
+require("scripts/globals/quests");
+require("scripts/globals/settings");
+require("scripts/zones/Palborough_Mines/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+ 	player:startEvent(0x0e);
+
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID2: %u",csid);
+--printf("RESULT2: %u",option);
+
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+
+   if (csid==0x0e) then
+	player:setPos(-73, 0, 60, 1, 0xac);
+   end
+end;
