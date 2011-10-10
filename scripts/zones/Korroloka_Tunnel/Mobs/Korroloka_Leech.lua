3c176b7219d7fa3dc6bfe0aec8ca82391d764975
dacrybabysuck <dacrybabysuck@gmail.com>
2011-10-10 18:51:35 +0000

    QUEST UPDATE: Ayame and Kaede: fixed bug so that killed leeches are counted.

--- scripts/zones/Korroloka_Tunnel/Mobs/Korroloka_Leech.lua
+++ scripts/zones/Korroloka_Tunnel/Mobs/Korroloka_Leech.lua
@@ -1,5 +1,5 @@
 require("scripts/globals/settings");
-
+require("scripts/globals/keyItems");
 -----------------------------------
 -- onMobSpawn Action
 -----------------------------------
@@ -7,12 +7,12 @@ function OnMobSpawn(mob)
 end;
 
 function onMobDeath(mob, killer)
-	
-	if (killer:getVar("AyameAndKaede_Event") == 2) then
+
+	if (killer:getVar("AyameAndKaede_Event") == 1 and killer:hasKeyItem(STRANGELY_SHAPED_CORAL) == false) then
 		leeches = killer:getVar("KorrolokaLeeches");
-	
+
 		killer:setVar("KorrolokaLeeches",leeches+1);
 		killer:setVar("KorrolokaLeeches_Timer",os.time());
 	end
-	
+
 end;
