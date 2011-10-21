135f2e0874a2308704b905767338b147cfe72450
onetimeffxi <onetimeffxi@gmail.com>
2011-10-21 04:15:14 +0000

    Blood Weapon adjusted time.

--- scripts/globals/abilities/blood_weapon.lua
+++ scripts/globals/abilities/blood_weapon.lua
@@ -5,5 +5,5 @@
 require("scripts/globals/settings");
 require("scripts/globals/status");
 function OnUseAbility(player, target, ability)
-	player:addStatusEffect(EFFECT_BLOOD_WEAPON,1,0,90);
+	player:addStatusEffect(EFFECT_BLOOD_WEAPON,1,0,30);
 end;
