779b188a1134b78951ce668a7af231ae04c9c087
xtehgrantagex <xtehgrantagex@gmail.com>
2011-10-25 05:05:25 +0000

    Protect Spell and Effect

---
+++ scripts/globals/effects/protect.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+--
+-- 	EFFECT_HASTE
+-- 	
+-----------------------------------
+
+-----------------------------------
+-- onEffectGain Action
+-----------------------------------
+
+function onEffectGain(target,effect)
+   target:addMod(MOD_HASTE,effect:getPower());
+end;
+
+-----------------------------------
+-- onEffectTick Action
+-----------------------------------
+
+function onEffectTick(target,effect)
+end;
+
+-----------------------------------
+-- onEffectLose Action
+-----------------------------------
+
+function onEffectLose(target,effect)
+   target:delMod(MOD_HASTE,effect:getPower());
+end;
---
+++ scripts/globals/spells/protect.lua
@@ -0,0 +1,11 @@
+-----------------------------------------
+--	Spell: Protect
+-----------------------------------------
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+   target:addStatusEffect(EFFECT_PROTECT,10,0,1800);
+end;
