679660ce4655d2d0e2d08bfe7f8e21758fb935da
jraul48 <jraul48@gmail.com>
2011-10-25 23:16:27 +0000

    QUEST A TASTE FOR MEAT, this quest can work but 1 npc dont have all cs

--- scripts/zones/Port_San_dOria/npcs/Antreneau.lua
+++ scripts/zones/Port_San_dOria/npcs/Antreneau.lua
@@ -1,28 +1,21 @@
 -----------------------------------
 -- Area: Northern San d'Oria
--- NPC: Thierride
+-- NPC: Antreneau
 -- Quest NPC
 -----------------------------------
-require("scripts/globals/settings");
+require("scripts/globals/titles");
 package.loaded["scripts/globals/quests"] = nil;
 require("scripts/globals/quests");
-package.loaded["scripts/zones/Northern_San_dOria/TextIDs"] = nil;
-require("scripts/zones/Northern_San_dOria/TextIDs");
+package.loaded["scripts/zones/Port_San_dOria/TextIDs"] = nil;
+require("scripts/zones/Port_San_dOria/TextIDs");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
-if (player:getQuestStatus(0,100) == 1) then
-	count = trade:getItemCount();
-		carta = trade:hasItemQty(4358, 5);
-		gil = trade:getGil();
-		if (carta and count == 5 and gil == 0) then
-			player:startEvent(0x0319);
-			end
-			end
-			end;
+
+end;
 
 -----------------------------------
 -- onTrigger Action
@@ -30,10 +23,16 @@ if (player:getQuestStatus(0,100) == 1) then
 
 function onTrigger(player,npc)
 	conejo = player:getQuestStatus(0,100);
+	
 	if (conejo == 0) then
-	player:startEvent(0x0318);
+	player:startEvent(0x020f);
+
+	elseif (conejo == 2 and player:hasItem(4371) == false) then
+	player:startEvent(0x0212);
+elseif (player:hasItem(4371)) then
+player:startEvent(0x020d);
 	end
-end; 
+	end; 
 
 -----------------------------------
 -- onEventUpdate
@@ -51,14 +50,15 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
- if (csid == 0x0318) then
-     player:getQuestStatus(0,100);
-    player:addQuest(100);
- elseif (csid == 0x0319) then
-  player:completeQuest(0,100);
-  player:addFame(0,SAN_FAME*30);
-  player:tradeComplete();
-  player:addGil(GIL_RATE*150);
-  player:messageSpecial(6404,GIL_RATE*150);
-end
+if (csid == 0x0212) then
+if (player:getQuestStatus(0,100) == 2 and player:getFreeSlotsCount() >= 1) then
+                        player:completeQuest(0,100);
+						player:addItem(4371,1);
+                        player:messageSpecial(6403,4371);
+						player:setTitle(RABBITER);
+						else
+						  player:messageSpecial(6402, 4371);
+		
+	end
+	end
 end;
