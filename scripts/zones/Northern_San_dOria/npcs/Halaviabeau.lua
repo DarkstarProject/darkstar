511b38302229997b08136149350c58de201c3fe3
jraul48 <jraul48@gmail.com>
2011-10-25 18:22:00 +0000

    UPDATE NPC EVENT

---
+++ scripts/zones/Northern_San_dOria/npcs/Halaviabeau.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+-- Area: Northern San d'Oria
+-- NPC: Halaviabeau
+-- Quest NPC
+-----------------------------------
+
+
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:startEvent(0x0263);
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+
+end;
