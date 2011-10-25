017d1937c7261a9cd59d43ce3074ba7eb3951924
jraul48 <jraul48@gmail.com>
2011-10-25 17:36:45 +0000

    NPC EVENT this npc now have event, can set home point with this npc

---
+++ scripts/zones/Northern_San_dOria/npcs/Pulloie.lua
@@ -0,0 +1,50 @@
+-----------------------------------
+-- Area: Northern San d'Oria
+-- NPC: Pulloie
+-- Quest NPC
+-----------------------------------
+
+package.loaded["scripts/zones/Northern_San_dOria/TextIDs"] = nil;
+require("scripts/zones/Northern_San_dOria/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	if (player:getNation() == 0) then
+	player:startEvent(0x0253);
+else
+	player:startEvent(0x0256);
+	end
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+if (csid == 0x0253 or csid == 0x0256 and option == 0) then
+player:setHomePoint();
+player:messageSpecial(HOMEPOINT_SET);
+end
+end;
