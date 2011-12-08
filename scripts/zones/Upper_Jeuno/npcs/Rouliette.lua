8c1e2aa609ec15498a2c8ff4fb6917b21c605e23
thrydwolf <thrydwolf@gmail.com>
2011-12-08 05:37:08 +0000

    QUEST ADD: Candle-making (Jeuno)

--- scripts/zones/Upper_Jeuno/npcs/Rouliette.lua
+++ scripts/zones/Upper_Jeuno/npcs/Rouliette.lua
@@ -1,9 +1,15 @@
 -----------------------------------
 -- Area: Upper Jeuno
--- NPC: Rouliette
--- Standard Info NPC
+-- NPC: Rouliette 
+-- Starts and Finishes Quest: Candle-making
+-- @zone 244 
+-- @pos -24 -2 11
 -----------------------------------
 
+require("scripts/globals/settings");
+require("scripts/globals/titles");
+package.loaded["scripts/globals/quests"] = nil;
+require("scripts/globals/quests");
 package.loaded["scripts/zones/Upper_Jeuno/TextIDs"] = nil;
 require("scripts/zones/Upper_Jeuno/TextIDs");
 
@@ -12,14 +18,22 @@ require("scripts/zones/Upper_Jeuno/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
+	if (player:getQuestStatus(JEUNO,CANDLE_MAKING) == QUEST_ACCEPTED and trade:hasItemQty(531,1) == true and trade:getGil() == 0 and trade:getItemCount() == 1) then 
+		player:startEvent(0x0025);
+	end
 end; 
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
-function onTrigger(player,npc)
-player:startEvent(0x001E);
+function onTrigger(player,npc) 
+	CandleMaking = player:getQuestStatus(JEUNO,CANDLE_MAKING);
+	if(CandleMaking == QUEST_ACCEPTED or (CandleMaking == QUEST_AVAILABLE and(player:getFameLevel(WINDURST) + player:getFameLevel(SANDORIA) + player:getFameLevel(BASTOK))/3 >= 4)) then 	
+		player:startEvent(0x0024);  -- Start Quest Candle-making
+	else
+		player:startEvent(0x001e);  --Standard dialog
+	end
 end; 
 
 -----------------------------------
@@ -38,7 +52,17 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+	if(csid == 0x0024 and player:getQuestStatus(JEUNO,CANDLE_MAKING) == QUEST_AVAILABLE) then 
+		player:addQuest(JEUNO,CANDLE_MAKING);
+	elseif(csid == 0x0025) then 
+		player:completeQuest(JEUNO,CANDLE_MAKING);
+		player:setTitle(BELIEVER_OF_ALTANA);
+		player:addKeyItem(HOLY_CANDLE);
+		player:messageSpecial(KEYITEM_OBTAINED,HOLY_CANDLE);
+		player:needToZone(true);
+		player:addFame(WINDURST, WIN_FAME*10);
+		player:addFame(SANORIA, SAN_FAME*10);
+		player:addFame(BASTOK, BAS_FAME*10);
+	end
 end;
-
-
 
