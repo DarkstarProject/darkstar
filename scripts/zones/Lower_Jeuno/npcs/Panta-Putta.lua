994a005218070ebedf10c975c8bea17778b47c07
Ezekyel38 <Ezekyel38@gmail.com>
2011-12-12 10:58:53 +0000

    QUEST ADD: The Wonder Magic Set (Jeuno), Child's Play (Jeuno)

--- scripts/zones/Lower_Jeuno/npcs/Panta-Putta.lua
+++ scripts/zones/Lower_Jeuno/npcs/Panta-Putta.lua
@@ -1,7 +1,9 @@
 -----------------------------------
 -- Area: Lower Jeuno
 -- NPC: Panta-Putta
--- Standard Info NPC
+-- Starts and Finishes Quest: The Wonder Magic Set
+-- @zone 245
+-- @pos -61 0 -140
 -----------------------------------
 
 package.loaded["scripts/zones/Lower_Jeuno/TextIDs"] = nil;
@@ -19,8 +21,14 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x004E);
-end; 
+	if(player:getFameLevel(JEUNO) >= 4 and player:getQuestStatus(JEUNO,THE_WONDER_MAGIC_SET) == QUEST_AVAILABLE) then 
+		player:startEvent(0x004D); -- Start quest
+	elseif(player:hasKeyItem(WONDER_MAGIC_SET) == true) then
+		player:startEvent(0x0021); -- Finish quest
+	else
+		player:startEvent(0x004E); -- Standard dialog
+	end
+end;
 
 -----------------------------------
 -- onEventUpdate
@@ -38,6 +46,21 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+	if(csid == 0x004D and option == 1) then 
+		player:addQuest(JEUNO,THE_WONDER_MAGIC_SET);
+	elseif(csid == 0x0021) then 
+		if (player:getFreeSlotsCount() == 0) then
+			player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,13328);
+		else
+			player:completeQuest(JEUNO,THE_WONDER_MAGIC_SET);
+			player:setTitle(FOOLS_ERRAND_RUNNER);
+			player:delKeyItem(WONDER_MAGIC_SET);
+			player:addItem(13328);
+			player:messageSpecial(ITEM_OBTAINED,13328);
+			player:addFame(JEUNO,30);
+			player:needToZone(true);
+		end
+	end
 end;
 
 
--- scripts/zones/Port_Jeuno/npcs/Karl.lua
+++ scripts/zones/Port_Jeuno/npcs/Karl.lua
@@ -1,9 +1,15 @@
 -----------------------------------
 -- Area: Port Jeuno
 -- NPC: Karl
--- Standard Info NPC
+-- Starts and Finishes Quest: Child's Play
+-- @zone 246
+-- @pos -60 0 -8
 -----------------------------------
 
+require("scripts/globals/titles");
+require("scripts/globals/settings");
+package.loaded["scripts/globals/quests"] = nil;
+require("scripts/globals/quests");
 package.loaded["scripts/zones/Port_Jeuno/TextIDs"] = nil;
 require("scripts/zones/Port_Jeuno/TextIDs");
 
@@ -12,6 +18,9 @@ require("scripts/zones/Port_Jeuno/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
+	if (player:getQuestStatus(JEUNO,CHILD_S_PLAY) == QUEST_ACCEPTED and trade:hasItemQty(776,1) == true and trade:getGil() == 0 and trade:getItemCount() == 1) then 
+		player:startEvent(0x0001); -- Finish quest 
+	end
 end; 
 
 -----------------------------------
@@ -19,8 +28,18 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x3a);
-end; 
+	ChildsPlay = player:getQuestStatus(JEUNO,CHILD_S_PLAY);
+	
+	if(player:getQuestStatus(JEUNO,THE_WONDER_MAGIC_SET) == QUEST_ACCEPTED and ChildsPlay == QUEST_AVAILABLE) then 
+		player:startEvent(0x0000); -- Start quest 
+	elseif(ChildsPlay == QUEST_ACCEPTED) then
+		player:startEvent(0x003d); -- mid quest CS
+	else
+		player:startEvent(0x003a); -- Standard dialog
+	end
+
+end;
+-- Lure of the Wildcat (Jeuno) 0x013c
 
 -----------------------------------
 -- onEventUpdate
@@ -38,7 +57,14 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+	if(csid == 0x0000) then 
+		player:addQuest(JEUNO,CHILD_S_PLAY);
+	elseif(csid == 0x0001) then 
+		player:completeQuest(JEUNO,CHILD_S_PLAY);
+		player:setTitle(TRADER_OF_MYSTERIES);
+		player:addKeyItem(WONDER_MAGIC_SET);
+		player:messageSpecial(KEYITEM_OBTAINED,WONDER_MAGIC_SET);
+		player:addFame(JEUNO,30);
+		player:tradeComplete(trade);
+	end
 end;
-
-
-
