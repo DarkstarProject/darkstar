a9b91ba2206e13531c100f631b1dae597653d5a5
thrydwolf <thrydwolf@gmail.com>
2011-12-15 21:13:54 +0000

    BUG FIX: Jugner_Forest/Zone.lua had incorrect parameters. Changed "elseif (b == false) then" to "if (b == false) then" Changed "player:getQuestStatus(WINDURST, I_CAN_HEAR_A_RAINBOW) == 1" to "player:getQuestStatus(WINDURST, I_CAN_HEAR_A_RAINBOW) == QUEST_ACCEPTED"

--- scripts/zones/Jugner_Forest/Zone.lua
+++ scripts/zones/Jugner_Forest/Zone.lua
@@ -24,14 +24,14 @@ end;
 function onZoneIn(player,prevZone)
 cs = -1;
 
-	if (player:getQuestStatus(WINDURST, I_CAN_HEAR_A_RAINBOW) == 1 and player:hasItem(1125)) then
+	if (player:getQuestStatus(WINDURST, I_CAN_HEAR_A_RAINBOW) == QUEST_ACCEPTED and player:hasItem(1125)) then
 		colors = player:getVar("ICanHearARainbow");
 		b = (tonumber(colors) % 32 >= 16);
 		v = (tonumber(colors) % 128 >= 64);
 
 		cs = 0x000f;
 
-		elseif (b == false) then
+		if (b == false) then
 			player:setVar("ICanHearARainbow_Weather",6);
 			player:setVar("ICanHearARainbow",colors+16);
 		elseif (v == false) then
