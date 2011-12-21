70d20c0fb63a653f765b0f1039c45ca496dfad2f
diatanato <diatanato@gmail.com>
2011-12-21 04:43:04 +0000

    SCRIPTS: Zeruhn_Mines - new npcs scripts (thanks aeodan)

---
+++ scripts/zones/Zeruhn_Mines/npcs/Dodoi.lua
@@ -0,0 +1,41 @@
+-----------------------------------
+-- Area: Zeruhn Mines
+-- NPC: Dodoi
+-- Standard Info NPC
+-----------------------------------
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:startEvent(0x006B);
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+
+
---
+++ scripts/zones/Zeruhn_Mines/npcs/Rasmus.lua
@@ -0,0 +1,41 @@
+-----------------------------------
+-- Area: Zeruhn Mines
+-- NPC: Rasmus
+-- Standard Info NPC
+-----------------------------------
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:startEvent(0x0065);
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+
+
