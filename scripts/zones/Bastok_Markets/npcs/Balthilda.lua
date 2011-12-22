3fa4cdfdf239fc8ae639f58390f52320eab25193
diatanato <diatanato@gmail.com>
2011-12-22 12:21:28 +0000

    SCRIPTS: магазины Balthilda и Ciqala (Madrigal)

--- scripts/zones/Bastok_Markets/npcs/Balthilda.lua
+++ scripts/zones/Bastok_Markets/npcs/Balthilda.lua
@@ -10,6 +10,7 @@
 -- Auto-Script: Requires Verification. Verified standard dialog - thrydwolf 12/18/2011
 -----------------------------------
 
+require("scripts/globals/shop");
 package.loaded["scripts/zones/Bastok_Markets/TextIDs"] = nil;
 require("scripts/zones/Bastok_Markets/TextIDs");
 
@@ -26,6 +27,21 @@ end;
 
 function onTrigger(player,npc)
 	player:showText(npc, BALTHILDA_SHOP_DIALOG);
+
+    stock = 
+    {
+        0x30B9,1904,3,  --Poet's Circlet
+        0x3140,1288,3,  --Tunic
+        0x3139,2838,3,  --Linen Robe
+        0x31C0,602,3,   --Mitts
+        0x31B9,1605,3,  --Linen Cuffs
+        0x3240,860,3,   --Slacks
+        0x3239,2318,3,  --Linen Slops
+        0x32C0,556,3,   --Solea
+        0x32B9,1495,3,  --Holly Clogs
+        0x349D,1150,3   --Leather Ring
+    }
+	showNationShop(player, BASTOK, stock);
 end;
 
 -----------------------------------
--- scripts/zones/Bastok_Markets/npcs/Ciqala.lua
+++ scripts/zones/Bastok_Markets/npcs/Ciqala.lua
@@ -8,6 +8,7 @@
 -- Auto-Script: Requires Verification
 -----------------------------------
 
+require("scripts/globals/shop");
 package.loaded["scripts/zones/Bastok_Markets/TextIDs"] = nil;
 require("scripts/zones/Bastok_Markets/TextIDs");
 
@@ -24,6 +25,26 @@ end;
 
 function onTrigger(player,npc)
 	player:showText(npc, CIQALA_SHOP_DIALOG);
+
+    stock = 
+    {
+        0x4008,4818,1,  --Metal Knuckles
+        0x4103,11285,1, --Battleaxe
+        0x4141,4186,1,  --Greataxe
+        0x4294,6033,1,  --Warhammer
+
+        0x4007,828,2,   --Brass Knuckles
+        0x4101,1435,2,  --Brass Axe
+        0x4140,618,2,   --Butterfly Axe
+        0x4293,2129,2,  --Brass Hammer
+
+        0x4006,224,3,   --Bronze Knuckles
+        0x4100,290,3,   --Bronze Axe
+        0x4292,312,3,   --Bronze Hammer
+        0x4299,47,3,    --Maple Wand
+        0x42C0,58,3     --Ash Staff
+    }
+	showNationShop(player, BASTOK, stock);
 end;
 
 -----------------------------------
