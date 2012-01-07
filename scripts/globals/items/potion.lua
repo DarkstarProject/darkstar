d5d15c1e3e2d4828160f6f9a31733bde01ccd2bc
diatanato <diatanato@gmail.com>
2012-01-07 07:40:22 +0000

    SCRIPT: potion (CipherX)

---
+++ scripts/globals/items/potion.lua
@@ -0,0 +1,23 @@
+-----------------------------------------
+-- ID: 4112
+-- Item: Potion
+-- Item Effect: Restores 50 HP
+-----------------------------------------
+
+require("scripts/globals/settings");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+return 0;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	target:addHP(50*ITEM_POWER);
+end;
