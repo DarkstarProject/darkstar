f4eaeb0032db81b2ffef74dde2a99347136916c1
Ezekyel38 <Ezekyel38@gmail.com>
2012-01-14 11:13:59 +0000

    UPDATE NPC: Shiva_Prime

--- scripts/zones/Cloister_of_Frost/Mobs/Shiva_Prime.lua
+++ scripts/zones/Cloister_of_Frost/Mobs/Shiva_Prime.lua
@@ -28,13 +28,5 @@ function onMobDeath(mob, killer)
 	pZone = killer:getZone();
 	
 	killer:startEvent(0x7D01,0,record,0,(os.time() - killer:getVar("ShivaFight_Timer")),partyMembers,0,0);
-	--player:delKeyItem(TUNING_FORK_OF_ICE);
-	--player:addKeyItem(WHISPER_OF_FROST);
-	killer:messageSpecial(KEYITEM_OBTAINED,WHISPER_OF_FROST);
-	killer:setVar("ShivaFight_Timer",0);
-	killer:setVar("ShivaFight_Killed",0);
-	killer:setVar(tostring(pZone) .. "_Ready",0);
-	killer:setVar(tostring(pZone) .. "_Field",0);
-	killer:setVar(tostring(pZone) .. "_Fight",0);
-	killer:delStatusEffect(EFFECT_BATTLEFIELD);
+	
 end;
