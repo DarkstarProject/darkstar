8377ca3944a88bebfe780b46f9c44ca9b728a66e
diatanato <diatanato@gmail.com>
2012-01-20 10:20:21 +0000

    SCRIPTS FIX: Southern_San_dOria Helbort - забыли require("scripts/globals/keyitems")

--- scripts/zones/Southern_San_dOria/npcs/Helbort.lua
+++ scripts/zones/Southern_San_dOria/npcs/Helbort.lua
@@ -5,12 +5,13 @@
 --  @zone 230
 --  @pos  71 -1 65
 -----------------------------------
-
-require("scripts/globals/titles");
-require("scripts/globals/settings");
-package.loaded["scripts/globals/quests"] = nil;
-require("scripts/globals/quests");
 package.loaded["scripts/zones/Southern_San_dOria/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/globals/settings");
+require("scripts/globals/titles");
+require("scripts/globals/quests");
+require("scripts/globals/keyitems");
 require("scripts/zones/Southern_San_dOria/TextIDs");
 
 -----------------------------------
