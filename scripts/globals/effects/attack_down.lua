598aa8cad9440a266f64dea9e943bb7874e733b6
diatanato <diatanato@gmail.com>
2012-01-22 11:14:23 +0000

    SCRIPTS: заглушки для эффектов

---
+++ scripts/globals/effects/attack_down.lua
@@ -0,0 +1,26 @@
+-----------------------------------
+--
+--
+--
+-----------------------------------
+
+-----------------------------------
+-- onEffectGain Action
+-----------------------------------
+
+function onEffectGain(target,effect)
+end;
+
+-----------------------------------
+-- onEffectTick Action
+-----------------------------------
+
+function onEffectTick(target,effect)
+end;
+
+-----------------------------------
+-- onEffectLose Action
+-----------------------------------
+
+function onEffectLose(target,effect)
+end;
