f79438ec8d0955faf9be5d5be8c19fc34cb2d606
diatanato <diatanato@gmail.com>
2012-01-27 10:19:29 +0000

    SCRIPTS: flee

--- scripts/globals/abilities/flee.lua
+++ scripts/globals/abilities/flee.lua
@@ -1,19 +1,17 @@
 -----------------------------------
-
---	Flee		
-
+--	Flee
 -----------------------------------
- 
 
-
-require("scripts/globals/settings");
 require("scripts/globals/status");
 
+-----------------------------------
+-- 
+-----------------------------------
 
 function OnUseAbility(player, target, ability)
-buff = 30;
-if (player:getEquipID(8) == 14094) or (player:getEquipID(8) == 15357) then
-buff = (buff + 15);
-end
-player:addStatusEffect(EFFECT_FLEE,1,0,buff);
+	buff = 30;
+	if (player:getEquipID(8) == 14094) or (player:getEquipID(8) == 15357) then
+		buff = buff + 15;
+	end
+	player:addStatusEffect(EFFECT_FLEE,100,0,buff);
 end;
