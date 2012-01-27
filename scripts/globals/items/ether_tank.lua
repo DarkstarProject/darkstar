27e8d0746ddb416b52bec9ed0cdf8e0aff534ea8
7cipherx7 <7cipherx7@gmail.com>
2012-01-27 07:57:53 +0000

    Ether Tank

---
+++ scripts/globals/items/ether_tank.lua
@@ -0,0 +1,25 @@
+-----------------------------------------
+--	ID: 13682
+--	Ether Tank
+--	When used, you will obtain one Ether
+-----------------------------------------
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+result = 0;
+	if (target:getFreeSlotsCount() == 0) then
+		result = 308;
+	end
+return result;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	target:addItem(4128,1);
+end;
