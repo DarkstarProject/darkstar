af302181533bd28208980f99c85a868c6ba2895b
diatanato <diatanato@gmail.com>
2012-01-28 17:08:19 +0000

    SCRIPTS: potion drops + hi+poiton drops

---
+++ scripts/globals/items/bottle_of_hi-potion_drops.lua
@@ -0,0 +1,29 @@
+-----------------------------------------
+-- ID: 5328
+-- Item: Hi-Potion Drop
+-- Item Effect: Restores 110 HP
+-----------------------------------------
+
+require("scripts/globals/status");
+require("scripts/globals/settings");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+result = 0;
+	if (target:hasStatusEffect(EFFECT_MEDICINE)) then
+		result = 111;
+	end
+return result;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	target:addHP(110*ITEM_POWER);
+	target:addStatusEffect(EFFECT_MEDICINE,0,0,300);
+end;
