88281f1a5a61ba9e7d081c43a9a4d7a12a998629
diatanato <diatanato@gmail.com>
2012-01-29 19:17:04 +0000

    SCRIPTS: Cherry Muffin

---
+++ scripts/globals/items/cherry_muffin.lua
@@ -0,0 +1,54 @@
+-----------------------------------------
+-- ID: 5653
+-- Item: Cherry Muffin
+-- Food Effect: 30Min, All Races
+-----------------------------------------
+-- Intelligence 1
+-- Magic % 10
+-- Magic Cap 80
+-- Agility -1
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+result = 0;
+	if (target:hasStatusEffect(EFFECT_FOOD)) then
+		result = 246;
+	end
+return result;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+    target:addStatusEffect(EFFECT_FOOD,0,0,1800,0,5653);
+end;
+
+-----------------------------------------
+-- onEffectGain Action
+-----------------------------------------
+
+function onEffectGain(target,effect)
+    target:addMod(MOD_INT, 1);
+    target:addMod(MOD_FOOD_MPP, 10);
+	target:addMod(MOD_FOOD_MP_CAP, 80);
+    target:addMod(MOD_AGI, -1);
+end;
+
+-----------------------------------------
+-- onEffectLose Action
+-----------------------------------------
+
+function onEffectLose(target,effect)
+    target:delMod(MOD_INT, 1);
+    target:delMod(MOD_FOOD_MPP, 10);
+	target:delMod(MOD_FOOD_MP_CAP, 80);
+    target:delMod(MOD_AGI, -1);
+end;
