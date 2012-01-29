73dd86b2edef9e29d97a122c342f2b031e6939c0
diatanato <diatanato@gmail.com>
2012-01-29 12:47:42 +0000

    SCRIPTS: Fast Blade - плохая идея, плохой скрипт

---
+++ scripts/globals/weaponskills/fast_blade.lua
@@ -0,0 +1,13 @@
+-----------------------------------
+-- Fast Blade      
+-- Sword weapon skill
+-- Skill level: 5
+-- Delivers a two-hit attack. Damage varies with TP.
+-- Modifiers: STR:20% ; DEX:20% 
+-- 100%TP 	200%TP 	300%TP
+-- 1.00 	1.50 	2.00 
+-----------------------------------
+
+function OnUseWeaponSkill(attacker, target, wsID)
+	return 65;
+end
