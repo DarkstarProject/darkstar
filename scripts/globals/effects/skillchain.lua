936d60c06acc1cf58e3a20f51bf8f2fd4e960311
diatanato <diatanato@gmail.com>
2012-01-29 18:12:44 +0000

    effect skillchain

---
+++ scripts/globals/effects/skillchain.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+--
+--  EFFECT_SKILLCHAIN
+--	
+-----------------------------------
+ 
+require("scripts/globals/status");
+
+-----------------------------------
+-- onEffectGain Action
+-----------------------------------
+
+function onEffectGain(target,effect)
+end;
+
+-----------------------------------
+-- onEffectTick Action
+-----------------------------------
+
+function onEffectTick(target,effect)
+end;
+
+-----------------------------------
+-- onEffectLose Action
+-----------------------------------
+
+function onEffectLose(target,effect)
+end;
