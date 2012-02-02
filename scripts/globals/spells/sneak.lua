b3b8ef92e14cfdbaa741483379599fbd896ce667
diatanato <diatanato@gmail.com>
2012-02-02 02:39:12 +0000

    SCRIPTS: spell sneak

--- scripts/globals/spells/sneak.lua
+++ scripts/globals/spells/sneak.lua
@@ -7,5 +7,24 @@
 -----------------------------------------
 
 function onSpellCast(caster,target,spell)
-   target:addStatusEffect(EFFECT_SNEAK,0,10,300);
+	if (target:hasStatusEffect(EFFECT_SNEAK) == false) then
+		
+		duration = math.random(40, 300);
+		duration = math.random(40, 300);
+		duration = math.random(40, 300);
+		
+		if (target:getMainLvl() < 20) then
+			duration = duration * target:getMainLvl() / 20; -- level adjustment
+		end
+		if (target:getEquipID(15) == 13692) then -- skulker's cape
+			duration = duration * 1.5;
+		end
+		if (caster:hasStatusEffect(EFFECT_COMPOSURE)) and (caster == target) then
+			duration = duration * 3;
+		end
+		spell:setMsg(0);
+		target:addStatusEffect(EFFECT_SNEAK,0,10,math.floor(duration),FLAG_DISPELABLE);
+	else
+		spell:setMsg(75); -- no effect.
+	end
 end;
