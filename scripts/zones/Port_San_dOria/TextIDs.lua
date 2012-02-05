e0505d20e21d6960651eda3de6282ddaf56078cc
diatanato <diatanato@gmail.com>
2012-02-05 17:47:02 +0000

    SCRIPTS: Post San d'Oria - обновил номера сообщений

--- scripts/zones/Port_San_dOria/TextIDs.lua
+++ scripts/zones/Port_San_dOria/TextIDs.lua
@@ -11,54 +11,54 @@ FULL_INVENTORY_AFTER_TRADE = 6402; -- You cannot obtain the <item>. Try trading 
 			 HOMEPOINT_SET = 24; -- Home point set!
 
 -- Quest Items
-    BEETLE_SHELL = 889; -- 
+    BEETLE_SHELL = 889;  -- 
+  MagicmartFlyer = 532;  -- item: Magicmart_flyer
+  
+   FLYER_REFUSED = 7102; -- This person isn't interested.
+   FLYER_ALREADY = 7103; -- This person already has a flyer.
+  FLYER_ACCEPTED = 7104; -- Your flyer is accepted!
+   FLYERS_HANDED = 7105; -- You've handed out <number> flyer(s).
 
-  MagicmartFlyer = 532; -- item: Magicmart_flyer
-   FLYER_REFUSED = 7100; -- This person isn't interested.
-   FLYER_ALREADY = 7101; -- This person already has a flyer.
-  FLYER_ACCEPTED = 7102; -- Your flyer is accepted!
-   FLYERS_HANDED = 7103; -- You've handed out <number> flyer(s).
 
+ PORTAURE_DIALOG = 7367; -- What's this? A magic shop? Hmm...I could use a new line of work, and magic just might be the ticket!
+  ANSWALD_DIALOG = 7387; -- A magic shop? Oh, it's right near here. I'll go check it out sometime.
+  PRIETTA_DIALOG = 7411; -- This is the first I've heard of a magic shop here in San d'Oria. Such arts have never been popular in the Kingdom.
+   AUVARE_DIALOG = 7418; -- What have I got here? Look, I can't read, but I takes what I gets, and you ain't getting it back!
+    MIENE_DIALOG = 7471; -- Oh, a magic shop... Here in San d'Oria? I'd take a look if I got more allowance.
 
- PORTAURE_DIALOG = 7365; -- What's this? A magic shop? Hmm...I could use a new line of work, and magic just might be the ticket!
-  ANSWALD_DIALOG = 7385; -- A magic shop? Oh, it's right near here. I'll go check it out sometime.
-  PRIETTA_DIALOG = 7409; -- This is the first I've heard of a magic shop here in San d'Oria. Such arts have never been popular in the Kingdom.
-   AUVARE_DIALOG = 7416; -- What have I got here? Look, I can't read, but I takes what I gets, and you ain't getting it back!
-    MIENE_DIALOG = 7469; -- Oh, a magic shop... Here in San d'Oria? I'd take a look if I got more allowance.
-
- ANSWALD_MESSAGE = 7959; -- Answald looks over curiously for a moment.
- PRIETTA_MESSAGE = 7960; -- Prietta looks over curiously for a moment.
-   MIENE_MESSAGE = 7961; -- Miene looks over curiously for a moment.
-PORTAURE_MESSAGE = 7962; -- Portaure looks over curiously for a moment.
-  AUVARE_MESSAGE = 7963; -- Auvare looks over curiously for a moment.
+ ANSWALD_MESSAGE = 7961; -- Answald looks over curiously for a moment.
+ PRIETTA_MESSAGE = 7962; -- Prietta looks over curiously for a moment.
+   MIENE_MESSAGE = 7963; -- Miene looks over curiously for a moment.
+PORTAURE_MESSAGE = 7964; -- Portaure looks over curiously for a moment.
+  AUVARE_MESSAGE = 7965; -- Auvare looks over curiously for a moment.
 
 -- Shop Texts
-           ALBINIE_SHOP_DIALOG = 7429; -- Welcome to my simple shop.
-          COULLAVE_SHOP_DIALOG = 7475; -- Can I help you?
-        CROUMANGUE_SHOP_DIALOG = 7476; -- Can't fight on an empty stomach. How about some nourishment?
+           ALBINIE_SHOP_DIALOG = 7431; -- Welcome to my simple shop.
+          COULLAVE_SHOP_DIALOG = 7477; -- Can I help you?
+        CROUMANGUE_SHOP_DIALOG = 7478; -- Can't fight on an empty stomach. How about some nourishment?
 
-	      VENDAVOQ_OPEN_DIALOG = 7483; -- Vandoolin! Vendavoq vring voods vack vrom Vovalpolos! Vuy! Vuy!
-	    VENDAVOQ_CLOSED_DIALOG = 7484; -- Vandoolin... Vendavoq's vream vo vell voods vrom vometown vf Vovalpolos...
+	      VENDAVOQ_OPEN_DIALOG = 7485; -- Vandoolin! Vendavoq vring voods vack vrom Vovalpolos! Vuy! Vuy!
+	    VENDAVOQ_CLOSED_DIALOG = 7486; -- Vandoolin... Vendavoq's vream vo vell voods vrom vometown vf Vovalpolos...
 
-       BONMAURIEUT_OPEN_DIALOG = 7822; -- My shipment is in! Would you like to see what has just arrived from the Elshimo Uplands?
-     BONMAURIEUT_CLOSED_DIALOG = 7819; -- I would like to sell goods from the Elshimo Uplands, but I cannot, as it's under foreign control.
+       BONMAURIEUT_OPEN_DIALOG = 7824; -- My shipment is in! Would you like to see what has just arrived from the Elshimo Uplands?
+     BONMAURIEUT_CLOSED_DIALOG = 7821; -- I would like to sell goods from the Elshimo Uplands, but I cannot, as it's under foreign control.
 
-             MILVA_OPEN_DIALOG = 7478; -- How about some produce from Sarutabaruta?
-           MILVA_CLOSED_DIALOG = 7480; -- I want to import produce from Sarutabaruta... But I can't do anything until we control that region!
+             MILVA_OPEN_DIALOG = 7480; -- How about some produce from Sarutabaruta?
+           MILVA_CLOSED_DIALOG = 7482; -- I want to import produce from Sarutabaruta... But I can't do anything until we control that region!
 
-		      FIVA_OPEN_DIALOG = 7477; -- I've got imports from Kolshushu!
-		    FIVA_CLOSED_DIALOG = 7479; -- I'm trying to sell goods from Kolshushu. But I can't because we don't have enough influence there.
+		      FIVA_OPEN_DIALOG = 7479; -- I've got imports from Kolshushu!
+		    FIVA_CLOSED_DIALOG = 7481; -- I'm trying to sell goods from Kolshushu. But I can't because we don't have enough influence there.
 
-	  DEGUERENDARS_OPEN_DIALOG = 7485; -- Welcome! Have a look at these rare goods from Tavnazia!
-	DEGUERENDARS_CLOSED_DIALOG = 7486; -- With that other nation in control of the region, there is no way for me to import goods from Tavnazia...
-DEGUERENDARS_COP_NOT_COMPLETED = 7487; -- <Sigh> Why must I wait for the Kingdom to issue a permit allowing me to set up shop? How am I to feed my children in the meantime!?
+	  DEGUERENDARS_OPEN_DIALOG = 7487; -- Welcome! Have a look at these rare goods from Tavnazia!
+	DEGUERENDARS_CLOSED_DIALOG = 7488; -- With that other nation in control of the region, there is no way for me to import goods from Tavnazia...
+DEGUERENDARS_COP_NOT_COMPLETED = 7489; -- <Sigh> Why must I wait for the Kingdom to issue a permit allowing me to set up shop? How am I to feed my children in the meantime!?
 
 
-	       PATOLLE_OPEN_DIALOG = 7821; -- Hey, <Multiple Choice (Player Gender)>[mister/miss]! How about some specialty goods from Kuzotz?
-	     PATOLLE_CLOSED_DIALOG = 7482; -- I'm trying to find goods from Kuzotz. But how can I when it's under foreign control?
+	       PATOLLE_OPEN_DIALOG = 7823; -- Hey, <Multiple Choice (Player Gender)>[mister/miss]! How about some specialty goods from Kuzotz?
+	     PATOLLE_CLOSED_DIALOG = 7484; -- I'm trying to find goods from Kuzotz. But how can I when it's under foreign control?
 
-		     NIMIA_OPEN_DIALOG = 7820; -- Hello, friend! Can I interest you in specialty goods from the Elshimo Lowlands?
-	       NIMIA_CLOSED_DIALOG = 7481; -- I can't sell goods from the lowlands of Elshimo because it's under foreign control.
+		     NIMIA_OPEN_DIALOG = 7822; -- Hello, friend! Can I interest you in specialty goods from the Elshimo Lowlands?
+	       NIMIA_CLOSED_DIALOG = 7483; -- I can't sell goods from the lowlands of Elshimo because it's under foreign control.
 
 
 -- perdiouvilet - Lure of the Wildcat (San d'Oria)
--- scripts/zones/Port_San_dOria/npcs/Albinie.lua
+++ scripts/zones/Port_San_dOria/npcs/Albinie.lua
@@ -4,9 +4,10 @@
 -- Standard Merchant NPC
 -- Working 100%
 -----------------------------------
+package.loaded["scripts/zones/Port_San_dOria/TextIDs"] = nil;
+-----------------------------------
 
 require("scripts/globals/shop");
-package.loaded["scripts/zones/Port_San_dOria/TextIDs"] = nil;
 require("scripts/zones/Port_San_dOria/TextIDs");
 
 -----------------------------------
@@ -24,19 +25,20 @@ function onTrigger(player,npc)
 	
 player:showText(npc,ALBINIE_SHOP_DIALOG);
 
-stock = {0x02bb,5688,1, --Oak Log 
-		 0x0284,1800,1, --Mythril Ore 
-		 0x0343,225,1,  --Flax Flower
+stock = {
+	0x02bb,5688,1, --Oak Log 
+	0x0284,1800,1, --Mythril Ore 
+	0x0343,225,1,  --Flax Flower
 		 
-		 0x02b6,2543,2, --Chestnut Log  
-		 0x0280,10,2,   --Copper Ore 
-		 0x0283,810,2,  --Iron Ore 
-		 0x0341,18,2,   --Moko Grass  
-		 0x11da,50,2,   --Bird Egg 
+	0x02b6,2543,2, --Chestnut Log  
+	0x0280,10,2,   --Copper Ore 
+	0x0283,810,2,  --Iron Ore 
+	0x0341,18,2,   --Moko Grass  
+	0x11da,50,2,   --Bird Egg 
 		 
-		 0x02ba,86,3,   --Ash Log 
-		 0x0001,1800,3} --Chocobo Bedding 
- 
+	0x02ba,86,3,   --Ash Log 
+	0x0001,1800,3  --Chocobo Bedding 
+}
 showShop(player, SANDORIA, stock);
 end; 
 
