2e99b2a2fafa78c28c70975e4fa8230241a1d632
diatanato <diatanato@gmail.com>
2012-02-06 02:41:17 +0000

    SCRIPTS FIX: имена файлов должны быть полностью идентичны именам в базе данных, иначе это не будет работать под Linux

---
+++ scripts/zones/Qufim_Island/npcs/Nightflowers.lua
@@ -0,0 +1,60 @@
+-----------------------------------
+-- Area: Qufim Island
+-- NPC: Night Flowers
+-- Involved in Quest: Save My Son (Beastmaster Flag #1)
+-----------------------------------
+package.loaded["scripts/zones/Qufim_Island/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/globals/quests");
+require("scripts/zones/Qufim_Island/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	
+	currentTime = VanadielHour();
+	
+	if(currentTime >= 22 or currentTime < 6) then
+		if(player:getQuestStatus(JEUNO,SAVE_MY_SON) == QUEST_ACCEPTED and player:getVar("SaveMySon_Event") == 0) then
+			player:startEvent(0x0000);
+		else
+			player:messageSpecial(NOW_THAT_NIGHT_HAS_FALLEN);
+		end
+	else
+		player:messageSpecial(THESE_WITHERED_FLOWERS);
+	end
+	
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+
+	if(csid == 0x0000) then
+		player:setVar("SaveMySon_Event",1);
+	end
+	
+end;
