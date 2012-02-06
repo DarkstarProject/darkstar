814d2cc9ae6de6fe87f3367a9ed9177472ba07c3
diatanato <diatanato@gmail.com>
2012-02-06 06:44:51 +0000

    SCRIPTS: nin spells - tonko_ichi + tonko_ni

---
+++ scripts/globals/spells/tonko_ichi.lua
@@ -0,0 +1,21 @@
+-----------------------------------------
+--	Spell: Tonko: Ichi
+-- 	Lessens chance of being detected by sound
+--  
+--  Duration is 3 minutes (non-random duration)
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	if (target:hasStatusEffect(EFFECT_INVISIBLE) == false) then
+		spell:setMsg(0);
+		target:addStatusEffect(EFFECT_INVISIBLE,0,10,180,FLAG_DISPELABLE);
+	else
+		spell:setMsg(75); -- no effect.
+	end
+end;
---
+++ scripts/globals/spells/tonko_ni.lua
@@ -0,0 +1,21 @@
+-----------------------------------------
+--	Spell: Tonko: ni
+-- 	Lessens chance of being detected by sound
+--  
+--  Duration is 5 minutes (non-random duration)
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	if (target:hasStatusEffect(EFFECT_INVISIBLE) == false) then
+		spell:setMsg(0);
+		target:addStatusEffect(EFFECT_INVISIBLE,0,10,300,FLAG_DISPELABLE);
+	else
+		spell:setMsg(75); -- no effect.
+	end
+end;
