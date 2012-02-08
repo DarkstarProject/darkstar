d2b5faf3d70e6c64a3c1e109a9d46fe280ba7bf6
diatanato <diatanato@gmail.com>
2012-02-08 03:58:20 +0000

    SCRIPTS: Port_San_dOria/npcs/Thierride.lua - старый формат скрипта, ошибка в onEventFinish

--- scripts/zones/Port_San_dOria/npcs/Thierride.lua
+++ scripts/zones/Port_San_dOria/npcs/Thierride.lua
@@ -2,11 +2,14 @@
 -- Area: Northern San d'Oria
 -- NPC: Thierride
 -- Quest NPC
+-- @zone 232
+-- @pos -67 -5 -28
 -----------------------------------
-require("scripts/globals/settings");
-package.loaded["scripts/globals/quests"] = nil;
-require("scripts/globals/quests");
 package.loaded["scripts/zones/Northern_San_dOria/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/globals/settings");
+require("scripts/globals/quests");
 require("scripts/zones/Northern_San_dOria/TextIDs");
 
 -----------------------------------
@@ -14,24 +17,23 @@ require("scripts/zones/Northern_San_dOria/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-if (player:getQuestStatus(0,100) == 1) then
-	count = trade:getItemCount();
+	if (player:getQuestStatus(SANDORIA, A_TASTE_FOR_MEAT) == QUEST_ACCEPTED) then
+		count = trade:getItemCount();
 		carta = trade:hasItemQty(4358, 5);
-		gil = trade:getGil();
-		if (carta and count == 5 and gil == 0) then
+		if (carta and count == 5) then
 			player:startEvent(0x0319);
-			end
-			end
-			end;
+		end
+	end
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
-	conejo = player:getQuestStatus(0,100);
-	if (conejo == 0) then
-	player:startEvent(0x0318);
+	conejo = player:getQuestStatus(SANDORIA, A_TASTE_FOR_MEAT);
+	if (conejo == QUEST_AVAILABLE) then
+		player:startEvent(0x0318);
 	end
 end; 
 
@@ -51,14 +53,13 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
- if (csid == 0x0318) then
-     player:getQuestStatus(0,100);
-    player:addQuest(100);
- elseif (csid == 0x0319) then
-  player:completeQuest(0,100);
-  player:addFame(0,SAN_FAME*30);
-  player:tradeComplete();
-  player:addGil(GIL_RATE*150);
-  player:messageSpecial(6404,GIL_RATE*150);
-end
+	if (csid == 0x0318) then
+		player:addQuest(SANDORIA, A_TASTE_FOR_MEAT);
+	elseif (csid == 0x0319) then
+		player:tradeComplete();
+		player:addFame(SANDORIA, SAN_FAME*30);
+		player:addGil(GIL_RATE*150);
+		player:messageSpecial(6404, GIL_RATE*150);
+		player:completeQuest(SANDORIA, A_TASTE_FOR_MEAT);
+	end
 end;
