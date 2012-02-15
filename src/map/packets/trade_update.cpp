9af4974522bb415ea66b5e35384809863efd944c
diatanato <diatanato@gmail.com>
2012-02-15 06:31:02 +0000

    пакеты для реализации обмена (trade)

---
+++ src/map/packets/trade_update.cpp
@@ -0,0 +1,44 @@
+/*
+===========================================================================
+
+  Copyright (c) 2010-2012 Darkstar Dev Teams
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+
+  This file is part of DarkStar-server source code.
+
+===========================================================================
+*/
+
+#include "../../common/socket.h"
+
+#include "../itemutils.h"
+
+#include "trade_update.h"
+
+/************************************************************************
+*																		*
+*																		*
+*																		*
+************************************************************************/
+
+CTradeUpdatePacket::CTradeUpdatePacket(CItem* PItem, uint32 quantity, uint8 slot)
+{
+	this->type = 0x23;
+	this->size = 0x14;
+
+	WBUFL(data,(0x04)-4) = quantity;
+	WBUFB(data,(0x0A)-4) = PItem->getID();
+	WBUFB(data,(0x0D)-4) = slot;
+}
