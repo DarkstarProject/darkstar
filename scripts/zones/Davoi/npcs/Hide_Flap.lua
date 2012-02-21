53a087b1d3905b7e913c6fbdac1f112cac5c312b
Ezekyel38 <Ezekyel38@gmail.com>
2012-02-21 20:32:54 +0000

    UPDATE QUEST: The First Meeting, Change NM position

--- scripts/zones/Davoi/npcs/Hide_Flap.lua
+++ scripts/zones/Davoi/npcs/Hide_Flap.lua
@@ -3,7 +3,7 @@
 -- NPC:  Hide Flap
 -- Involved in Quest: The Doorman, The First Meeting
 -- @zone 149
--- @pos 293 3 -213
+-- @pos 293 3 -213 (WAR)(K-9) @pos -124 3 -43 (MNK)(F-7)
 -----------------------------------
 package.loaded["scripts/zones/Davoi/TextIDs"] = nil;
 -----------------------------------
@@ -26,13 +26,13 @@ end;
 
 function onTrigger(player,npc)
 	
-	theDoorman = player:getQuestStatus(BASTOK,THE_DOORMAN);
-	swordGripMaterial = player:hasKeyItem(SWORD_GRIP_MATERIAL);
+	X = npc:getXPos();
+	Z = npc:getZPos();
 	
-	theFirstMeeting = player:getQuestStatus(BASTOK,THE_FIRST_MEETING);
+	swordGripMaterial = player:hasKeyItem(SWORD_GRIP_MATERIAL);
 	martialArtsScroll = player:hasKeyItem(SANDORIAN_MARTIAL_ARTS_SCROLL);
-
-	if(theDoorman == QUEST_ACCEPTED) then
+	
+	if(player:getQuestStatus(BASTOK,THE_DOORMAN) == QUEST_ACCEPTED and X >= 289 and X <= 293 and Z <= -214 and Z >= -218) then
 		if(player:getVar("theDoormanKilledNM") >= 2 and swordGripMaterial == false and player:getVar("theDoormanMyMob") == 1) then
 			player:addKeyItem(SWORD_GRIP_MATERIAL);
 			player:messageSpecial(KEYITEM_OBTAINED, SWORD_GRIP_MATERIAL);
@@ -41,18 +41,18 @@ function onTrigger(player,npc)
 		elseif(swordGripMaterial == true or player:getVar("theDoormanCS") >= 2) then 
 			player:messageSpecial(YOU_SEE_NOTHING);
 		else
-			SpawnMob(17387965,180):updateEnmity(player); -- Gavotvut
-			SpawnMob(17387966,180):updateEnmity(player); -- Barakbok
+			SpawnMob(17387965,300):updateEnmity(player); -- Gavotvut
+			SpawnMob(17387966,300):updateEnmity(player); -- Barakbok
 			player:setVar("theDoormanMyMob",1);
 		end
-	elseif(theFirstMeeting == QUEST_ACCEPTED and martialArtsScroll == false) then
+	elseif(player:getQuestStatus(BASTOK,THE_FIRST_MEETING) == QUEST_ACCEPTED and martialArtsScroll == false and X <= -122 and X >= -126 and Z <= -41 and Z >= -45) then
 		if(player:getVar("theFirstMeetingKilledNM") >= 2) then
 			player:addKeyItem(SANDORIAN_MARTIAL_ARTS_SCROLL);
 			player:messageSpecial(KEYITEM_OBTAINED,SANDORIAN_MARTIAL_ARTS_SCROLL);
 			player:setVar("theFirstMeetingKilledNM",0);
 		else
-			SpawnMob(17387967,180):updateEnmity(player); -- Bilopdop
-			SpawnMob(17387968,180):updateEnmity(player); -- Deloknok
+			SpawnMob(17387967,300):updateEnmity(player); -- Bilopdop
+			SpawnMob(17387968,300):updateEnmity(player); -- Deloknok
 		end
 	else
 		player:messageSpecial(YOU_SEE_NOTHING);
