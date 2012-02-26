f3a268f685bdb59195c2f0c7b343866ac675b290
7cipherx7 <7cipherx7@gmail.com>
2012-02-26 05:30:55 +0000

    BLM quake as per (GM)

---
+++ scripts/globals/spells/quake.lua
@@ -0,0 +1,12 @@
+-----------------------------------------
+-- Spell: Quake
+-- Deals earth damage to an enemy.
+-----------------------------------------
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	return target:takeMagicDamage(caster);
+end;
