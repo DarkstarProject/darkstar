abb467912a8ce5ff22ab91b612236f05694eb069
diatanato <diatanato@gmail.com>
2012-03-09 05:28:17 +0000

    SQL: linkshells.sql

--- sql/linkshells.sql
+++ sql/linkshells.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Lun 20 Février 2012 à 21:20
+-- Généré le : Ven 09 Mars 2012 à 09:27
 -- Version du serveur: 6.0.0
 -- Version de PHP: 5.2.9-2
 
@@ -28,11 +28,13 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 DROP TABLE IF EXISTS `linkshells`;
 CREATE TABLE IF NOT EXISTS `linkshells` (
   `linkshellid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(20) NOT NULL DEFAULT '',
   `name` varchar(15) NOT NULL,
   `color` smallint(5) unsigned NOT NULL DEFAULT '61440',
-  `message` varchar(120) NOT NULL DEFAULT '',
-  `setOn` int(10) unsigned NOT NULL DEFAULT '0',
   `poster` varchar(15) NOT NULL DEFAULT '',
+  `message` varchar(115) NOT NULL DEFAULT '',
+  `messagetime` int(10) unsigned NOT NULL DEFAULT '0',
+  `postrights` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`linkshellid`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=1 ;
