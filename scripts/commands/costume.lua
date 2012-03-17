1fd9e808ce9467bfe98aee02d550df37d6df6f9b
diatanato <diatanato@gmail.com>
2012-03-17 15:26:24 +0000

    @costume - команда позволяет найти необходимую модель перебором

---
+++ scripts/commands/costume.lua
@@ -0,0 +1,13 @@
+-----------------------------------
+--	[Command name]: costume
+--	[Author      ]: 
+--	[Description ]:
+-----------------------------------
+
+-----------------------------------
+-- Action
+-----------------------------------
+
+function onTrigger(player,costume)
+	player:costume(costume);
+end;
