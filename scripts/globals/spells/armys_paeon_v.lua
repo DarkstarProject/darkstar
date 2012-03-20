1bc03ff7796672db8de64ccf2460f07e2f955062
diatanato <diatanato@gmail.com>
2012-03-20 15:29:28 +0000

    armys_paeon

---
+++ scripts/globals/spells/armys_paeon_v.lua
@@ -0,0 +1,32 @@
+-----------------------------------------
+-- Spell: Army's Paeon V
+-- Gradually restores target's HP.
+-----------------------------------------
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	
+	local sItem = caster:getEquipID(2);
+	-- If Job is Bard and Level is at or above 75 then get bonus HP.
+	if(caster:getMainJob() == 10 and caster:getMainLvl() >= 75) then 
+	    hp = hp + 1;
+	end
+	-- Ebony Harp
+	if(sItem == 17357) then 
+	    hp = hp + 1;
+	end	
+	-- Ebony Harp +1 and Ebony Harp + 2
+	if(sItem == 17833 or sItem == 17848) then 
+	    hp = hp + 2;
+	end	
+	-- Until someone finds a way to delete Effects by tier we should not allow bard spells to stack.
+	-- Since all the tiers use the same effect buff it is hard to delete a specific one.
+	target:delStatusEffect(EFFECT_PAEON);
+	target:addStatusEffect(EFFECT_PAEON,hp,3,60);
+end;
