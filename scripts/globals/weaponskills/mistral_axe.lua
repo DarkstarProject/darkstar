627de46b3b0b1448379f6c5d82e5221e544b98b8
kegsay <kegsay@gmail.com>
2012-03-22 22:32:00 +0000

    SCRIPTS: Axe weaponskills. slish-slash

--- scripts/globals/weaponskills/mistral_axe.lua
+++ scripts/globals/weaponskills/mistral_axe.lua
@@ -1,7 +1,28 @@
-------------------------
---    Hard Slash      -- 
-------------------------
-
+-----------------------------------
+-- Mistral Axe   
+-- Axe weapon skill
+-- Skill: 225
+-- Modifiers: STR:50% - 1hit, damage varies
+-- 100%TP 	200%TP 	300%TP
+-- 2.5		3		3.5
+-----------------------------------
+require("/scripts/globals/settings");
+require("/scripts/globals/weaponskills");
 function OnUseWeaponSkill(attacker, target, wsID)
-	return 65;
+	numHits = 1;
+	--ftp damage mods (for Damage Varies with TP; lines are calculated in the function
+	ftp100 = 2.5; ftp200 = 3; ftp300 = 3.5;
+	--wscs are in % so 0.2=20%
+	str_wsc = 0.5; dex_wsc = 0.0; vit_wsc = 0.0; agi_wsc = 0.0; int_wsc = 0.0; mnd_wsc = 0.0; chr_wsc = 0.0;
+	--critical mods, again in % (ONLY USE FOR CRITICAL HIT VARIES WITH TP)
+	crit100 = 0.0; crit200=0.0; crit300=0.0;
+	canCrit = false;
+	--accuracy mods (ONLY USE FOR ACCURACY VARIES WITH TP) , should be the acc at those %s NOT the penalty values. Leave 0 if acc doesnt vary with tp.
+	acc100 = 0.0; acc200=0.0; acc300=0.0;
+	--attack multiplier (only some WSes use this, this varies the actual ratio value, see Tachi: Kasha) 1 is default.
+	atkmulti = 1;
+	
+	damage = doPhysicalWeaponskill(attacker,target, numHits,  str_wsc,dex_wsc,vit_wsc,agi_wsc,int_wsc,mnd_wsc,chr_wsc,  canCrit,crit100,crit200,crit300,  acc100,acc200,acc300,   atkmulti);
+	
+	return damage;
 end
