f6f706b1eb3dbcabf665f01ef319041d80cb66df
metalfiiish <metalfiiish@gmail.com>
2012-03-29 13:45:15 +0000

    BRD SPELLS - Knight's Minne I, II and IV

---
+++ scripts/globals/spells/knights_minne.lua
@@ -0,0 +1,50 @@
+-----------------------------------------
+-- Spell: Knight's Minne I
+-- Grants Defense bonus to all allies.
+-----------------------------------------
+
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	duration = 120;
+	local sItem = caster:getEquipID(2);
+	local sLvl = caster:getSkillLevel(40); -- Gets skill level of Singing
+	
+	if (sLvl < 6) then -- If your skill level is below 6 your stuck at the minimum
+		power = 7;
+	end
+	
+	if (sLvl >= 6 and sLvl <= 61) then -- If your above 5 skill then you get the bonus of 1 more defense for every 8 skill 
+		sBoost = math.floor((sLvl - 5)/8); 
+		power = 7 + sBoost;
+	end
+	
+	if(sLvl >= 61) then -- The bonus caps at skill 61
+		power = 14;
+	end
+	
+	if(sItem == 17373 or sItem == 17354) then -- Maple Harp +1 or Harp will add 3 more
+		power = power + 3;
+	end
+	
+	if(sItem == 17374) then -- Harp +1 gives 5 more
+		power = power + 5;
+		
+	end
+		
+	
+-- Until someone finds a way to delete Effects by tier we should not allow bard spells to stack.
+-- Since all the tiers use the same effect buff it is hard to delete a specific one.
+	if(target:hasStatusEffect(EFFECT_MINNE) == true) then
+		target:delStatusEffect(EFFECT_MINNE);
+	end
+	
+	target:addStatusEffect(EFFECT_MINNE,power,0,duration,FLAG_DISPELABLE);	
+
+	return 0;
+end;
---
+++ scripts/globals/spells/knights_minne_iii.lua
@@ -0,0 +1,50 @@
+-----------------------------------------
+-- Spell: Knight's Minne III
+-- Grants Defense bonus to all allies.
+-----------------------------------------
+
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	duration = 120;
+	local sItem = caster:getEquipID(2);
+	local sLvl = caster:getSkillLevel(40); -- Gets skill level of Singing
+	
+	if (sLvl < 118) then -- If your skill level is below 118 your stuck at the minimum
+		power = 26;
+	end
+	
+	if (sLvl >= 118 and sLvl <= 186) then -- If your above 117 skill then you get the bonus of 1 more defense for every 5 skill 
+		sBoost = math.floor((sLvl - 117)/5); 
+		power = 26 + sBoost;
+	end
+	
+	if(sLvl >= 187) then -- The bonus caps at skill 187
+		power = 40;
+	end
+	
+	if(sItem == 17373 or sItem == 17354) then -- Maple Harp +1 or Harp will add 3 more
+		power = power + 3;
+	end
+	
+	if(sItem == 17374) then -- Harp +1 gives 5 more
+		power = power + 5;
+		
+	end
+		
+	
+-- Until someone finds a way to delete Effects by tier we should not allow bard spells to stack.
+-- Since all the tiers use the same effect buff it is hard to delete a specific one.
+	if(target:hasStatusEffect(EFFECT_MINNE) == true) then
+		target:delStatusEffect(EFFECT_MINNE);
+	end
+	
+	target:addStatusEffect(EFFECT_MINNE,power,0,duration,FLAG_DISPELABLE);	
+
+	return 0;
+end;
