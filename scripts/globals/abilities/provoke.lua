a9eb572665bbd2dae2f8e4fc1ac883a07c220a70
rafael3100 <rafael3100@aim.com>
2012-04-01 17:12:15 +0000

    - Forgot header and function header. - abilities LUA files start with lowercase letter.

--- scripts/globals/abilities/provoke.lua
+++ scripts/globals/abilities/provoke.lua
@@ -1,4 +1,13 @@
+-----------------------------------
+-- Ability: Provoke
+-----------------------------------
 
+
+
+
+-----------------------------------
+-- OnUseAbility
+-----------------------------------
 
 function OnUseAbility(user, target, ability)
 	--leave blank please! This file will be deleted when the core is updated.
