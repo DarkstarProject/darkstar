7e7965310e0d9a46dc804839dadc465b556c786c
whasf <whasf@dspt.info>
2012-04-30 23:04:21 +0000

    SCRIPTS: Fixed up sleep.lua but the monster won't sleep. Something in the core is wrong...?

--- scripts/globals/spells/sleep.lua
+++ scripts/globals/spells/sleep.lua
@@ -9,17 +9,25 @@ require("scripts/globals/magic");
 
 function onSpellCast(caster,target,spell)
 	duration = 60;
-	resm = applyResistance(caster,spell,target);
+	staff = StaffBonus(caster, spell);
+	pINT = caster:getStat(MOD_INT);
+	mINT = target:getStat(MOD_INT);
+	dINT = (pINT - mINT);
+	resm = applyResistance(caster,spell,target,dINT,37,staff);
 	if(resm < 0.5) then
+		spell:setMsg(75);
 		--TODO: Set resist msg
-		return 0;
+		--return 0;
 	end
 	
 	if(target:hasStatusEffect(EFFECT_SLEEP_II) or target:hasStatusEffect(EFFECT_SLEEP_I)) then
 		--TODO: No effect
+		spell:setMsg(85);
 	else
+		spell:setMsg(237);
 		target:addStatusEffect(EFFECT_SLEEP_I,1,0,duration);
+	--	print ("slept!");
 	end
-	
-	return 0;
+	target:updateEnmity(caster,320,1);
+	return EFFECT_SLEEP_I;
 end;
