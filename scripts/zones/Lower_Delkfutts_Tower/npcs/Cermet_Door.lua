09ba015628bcd8f47d76137801c902d2ce438744
bluekirby0 <bluekirby0@gmail.com>
2012-05-11 16:10:42 +0000

    Altered door in Lower Delkfutt's Tower to get rid of a console error.

--- scripts/zones/Lower_Delkfutts_Tower/npcs/Cermet_Door.lua
+++ scripts/zones/Lower_Delkfutts_Tower/npcs/Cermet_Door.lua
@@ -20,22 +20,28 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	
 	player:startEvent(0x0014); -- missing walk-through animation, but it's the best I could find.
 	return 1;
-	
 end;
   
+-----------------------------------
+-- onEventUpdate Action
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+	--print("CSID:",csid);
+	--print("RESULT:",option);
+end;
+
 -----------------------------------
 -- onEventFinish Action
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---print("CSID:",csid);
---print("RESULT:",option);
+	--print("CSID:",csid);
+	--print("RESULT:",option);
 
 	if(csid == 0x0014 and option == 1) then
 		player:setPos(313, 16, 20, 128, 0x9E);	-- to Upper Delkfutt's Tower
 	end
-	
 end;
