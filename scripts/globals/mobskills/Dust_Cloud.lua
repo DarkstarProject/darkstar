fe840ddd392811708a9a55fad273700af6749062
kegsay <kegsay@gmail.com>
2012-05-11 15:54:49 +0000

    Edited the rabbit skills too.

--- scripts/globals/mobskills/Dust_Cloud.lua
+++ scripts/globals/mobskills/Dust_Cloud.lua
@@ -24,7 +24,8 @@ function OnMobWeaponSkill(target, mob, skill)
 	
 	dmgmod = 1;
 	accmod = 1;
-	dmg = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*6,accmod,dmgmod,TP_MAB_BONUS,1);
+	info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*6,accmod,dmgmod,TP_MAB_BONUS,1);
+	dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
 	target:delHP(dmg);
 	return dmg;
 	
--- scripts/globals/mobskills/Foot_Kick.lua
+++ scripts/globals/mobskills/Foot_Kick.lua
@@ -15,7 +15,8 @@ function OnMobWeaponSkill(target, mob, skill)
 	numhits = 1;
 	accmod = 1;
 	dmgmod = 1;
-	dmg = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_CRIT_VARIES,1,2,3);
+	info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_CRIT_VARIES,1,2,3);
+	dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
 	target:delHP(dmg);
 	return dmg;
 	
