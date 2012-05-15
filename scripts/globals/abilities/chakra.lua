6a55d11ef5cfe1c97198b1d07c2f611948ffb167
bluekirby0 <bluekirby0@gmail.com>
2012-05-15 15:22:11 +0000

    ADD: Chakra job ability for Monk. Takes into account equipment known to enhance the effect.

---
+++ scripts/globals/abilities/chakra.lua
@@ -0,0 +1,41 @@
+-----------------------------------
+-- Ability: Chakra
+-----------------------------------
+ 
+require("scripts/globals/settings");
+require("scripts/globals/status");
+
+-----------------------------------
+-- OnUseAbility
+-----------------------------------
+
+function OnUseAbility(player, target, ability)
+	local hp = player:getHP();
+	local vit = player:getStat(MOD_VIT);
+	local multi = 2;
+	
+	local main = player:getEquipID(1);
+	local hand = player:getEquipID(7);
+	
+	if(player:hasStatusEffect(EFFECT_POISON)) then
+		player:delStatusEffect(EFFECT_POISON);
+	end
+	if(player:hasStatusEffect(EFFECT_BLINDNESS)) then
+		player:delStatusEffect(EFFECT_BLINDNESS);
+	end
+	if((main == 12639) or (main == 14474)) then -- Temple Cyclas (+1) equipped
+		if(player:hasStatusEffect(EFFECT_PARALYSIS)) then
+			player:delStatusEffect(EFFECT_PARALYSIS);
+		end
+		multi = multi + 1;
+	end
+	if((hand == 15103) or (hand == 14910)) then -- Melee Gloves (+1) equipped
+		if(player:hasStatusEffect(EFFECT_DISEASE)) then
+			player:delStatusEffect(EFFECT_DISEASE);
+		end
+		multi = multi + 0.6;
+	end
+	local recover = (multi * vit);
+	player:setHP((hp + recover));
+	player:messageBasic(24,0,recover); -- <Player> <recovers/recover> <#> HP.
+end;
