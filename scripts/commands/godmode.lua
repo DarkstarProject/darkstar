71ec78e03ef2551d046b5822ace737370a7743e9
bluekirby0 <bluekirby0@gmail.com>
2012-05-19 16:56:18 +0000

    COMMAND: Added in a "godmode" GM command that toggles several effects useful for certain types of testing.

---
+++ scripts/commands/godmode.lua
@@ -0,0 +1,29 @@
+-----------------------------------
+--	[Command name]: godmode
+--	[Author      ]: bluekirby0
+--	[Description ]: Grants the user several special abilities that may aid in testing, or just plain showing off.
+-----------------------------------
+
+-----------------------------------
+-- Action
+-----------------------------------
+
+function onTrigger(player,target)
+	if (player:getVar("GodMode") == 0) then
+		player:setVar("GodMode",1)
+		player:addStatusEffect(EFFECT_INVINCIBLE,1,0,0);
+		player:addStatusEffect(EFFECT_PERFECT_DODGE,1,0,0);
+		player:addStatusEffect(EFFECT_MIGHTY_STRIKES,1,0,0);
+		player:addStatusEffect(EFFECT_HUNDRED_FISTS,1,0,0);
+		player:addStatusEffect(EFFECT_SNEAK_ATTACK,1,0,0);
+		player:addStatusEffect(EFFECT_REGAIN,100,1,0);
+	else
+		player:setVar("GodMode",0)
+		player:delStatusEffect(EFFECT_INVINCIBLE);
+		player:delStatusEffect(EFFECT_PERFECT_DODGE);
+		player:delStatusEffect(EFFECT_MIGHTY_STRIKES);
+		player:delStatusEffect(EFFECT_HUNDRED_FISTS);
+		player:delStatusEffect(EFFECT_SNEAK_ATTACK);
+		player:delStatusEffect(EFFECT_REGAIN);
+	end
+end;
