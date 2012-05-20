98d9827844e35fc33e3887f63c7df6c48aac227c
metalfiiish <metalfiiish@gmail.com>
2012-05-20 13:03:05 +0000

    SCRIPTS: Fixed Boost so it will now check for certain equipment that boosts the bonus attack. Also changed the logic used to stack, to make it more clean.

--- scripts/globals/abilities/boost.lua
+++ scripts/globals/abilities/boost.lua
@@ -10,15 +10,18 @@ require("scripts/globals/status");
 -----------------------------------
 
 function OnUseAbility(player, target, ability)
+	sHands = player:getEquipID(5);
+	power = 12.5
+	if(sHands == 13962 or sHands == 14891) then
+		power = power + 5.5;
+	end
+	
 	if(player:hasStatusEffect(EFFECT_BOOST) == true) then
 		effect = player:getStatusEffect(EFFECT_BOOST);
-		duration = effect:getDuration();
-		lastTick = effect:getLastTick();
-		duration = duration/100 - lastTick;
-		Power = effect:getPower() + 12.5;
-		player:delStatusEffect(EFFECT_BOOST);
-		player:addStatusEffect(EFFECT_BOOST,Power,1,duration);
+		newPower = effect:getPower() + power;
+		effect:setPower(newPower);
+		player:addMod(MOD_ATTP,power);
 	else
-		player:addStatusEffect(EFFECT_BOOST,12.5,1,180);
+		player:addStatusEffect(EFFECT_BOOST,power,1,180);
 	end
 end;
--- scripts/globals/effects/boost.lua
+++ scripts/globals/effects/boost.lua
@@ -9,7 +9,7 @@
 -----------------------------------
 
 function onEffectGain(target,effect)
-	target:addMod(MOD_ATTP,effect);
+	target:addMod(MOD_ATTP,effect:getPower());
 end;
 
 -----------------------------------
@@ -24,5 +24,5 @@ end;
 -----------------------------------
 
 function onEffectLose(target,effect)
-	target:delMod(MOD_ATTP,effect);
+	target:delMod(MOD_ATTP,effect:getPower());
 end;
