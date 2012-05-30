866c2920abdcdd57c513e636590c519c30db294e
wonderingcatentropy <wonderingcatentropy@gmail.com>
2012-05-30 21:31:07 +0000

    SCRIPT: Updated dialogue for East Sarutabaruta NPC related to mission

--- scripts/zones/East_Sarutabaruta/TextIDs.lua
+++ scripts/zones/East_Sarutabaruta/TextIDs.lua
@@ -7,6 +7,7 @@ ITEM_CANNOT_BE_OBTAINED = 6375; -- You cannot obtain the item <item> come back a
        KEYITEM_OBTAINED = 6381; -- Obtained key item: <keyitem>
 
 -- Dialog Texts
-TABY_CANATAHEY_DIALOG = 7324; -- This is the entrrrance to Windurst. Please maintain orderrrly conduct while you'rrre in town.
- HEIH_PORHIAAP_DIALOG = 7325; -- These grrrasslands make up East Sarutabaruta. Lately the number of monsters has drrramatically increased, causing us all sorts of trrrouble.
-  SAMA_GOHJIMA_DIALOG = 7327; -- The ministerrr of the Orastery is in the laborrratory beneath here. To get there, you should check the walls verrry carrrefully.
+  TABY_CANATAHEY_DIALOG = 7324; -- This is the entrrrance to Windurst. Please maintain orderrrly conduct while you'rrre in town.
+   HEIH_PORHIAAP_DIALOG = 7325; -- These grrrasslands make up East Sarutabaruta. Lately the number of monsters has drrramatically increased, causing us all sorts of trrrouble.
+ SAMA_GOHJIMA_PREDIALOG = 7327; -- The ministerrr of the Orastery is in the laborrratory beneath here. To get there, you should check the walls verrry carrrefully.
+SAMA_GOHJIMA_POSTDIALOG = 7328; --"Were you able to find the laborrratory? There are many such hidden passages in the Horutoto Ruins.?Prompt?"
