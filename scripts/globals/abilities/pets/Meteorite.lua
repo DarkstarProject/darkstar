851e528b7c70c3f3a09ff6adc8586621c81c8268
kegsay <kegsay@gmail.com>
2012-06-02 18:00:58 +0000

    Minor tweak to carby aggressive bps

--- scripts/globals/abilities/pets/Meteorite.lua
+++ scripts/globals/abilities/pets/Meteorite.lua
@@ -11,5 +11,6 @@ require("/scripts/globals/monstertpmoves");
 function OnPetAbility(target, pet, skill)
 	dint = pet:getStat(MOD_INT) - target:getStat(MOD_INT);
 	dmg = 500 + dint*1.5 + skill:getTP()/2;
+	target:updateEnmityFromDamage(pet,dmg);
 	return dmg;
 end
--- scripts/globals/abilities/pets/Poison_Nails.lua
+++ scripts/globals/abilities/pets/Poison_Nails.lua
@@ -12,8 +12,8 @@ function OnPetAbility(target, pet, skill)
 	numhits = 1;
 	accmod = 1;
 	dmgmod = 1;
-	info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
-	dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
+	info = MobPhysicalMove(pet,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,2,3);
+	dmg = MobFinalAdjustments(info.dmg,pet,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
 	target:delHP(dmg);
 	target:updateEnmityFromDamage(pet,dmg);
 	if(target:getStatusEffect(EFFECT_POISON)==nil) then
