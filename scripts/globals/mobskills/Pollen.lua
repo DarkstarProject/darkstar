dc6d5e09c01bf79042aee71258768bf6a7721517
metalfiiish <metalfiiish@gmail.com>
2012-06-03 15:10:08 +0000

    SCRIPTS: Fixed mob TP move pollen. Previously it would heal itself for up to 800 hp. Currently it will only heal 4-7% of it's max hp.

--- scripts/globals/mobskills/Pollen.lua
+++ scripts/globals/mobskills/Pollen.lua
@@ -1,16 +1,21 @@
+---------------------------------------------------
+--  Pollen
+--
+--  Description: Restores HP.
+--  
+---------------------------------------------------
 require("/scripts/globals/settings");
 require("/scripts/globals/status");
 require("/scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function OnMobWeaponSkill(target, mob, skill)
-	base = mob:getMainLvl() + 2*mob:getMainLvl()*(skill:getTP()/100); --base is around 5~150 level depending
-	M = 5;
-	multiplier = 1+(1- (mob:getHP()/mob:getMaxHP())) * M;	--higher multiplier the lower your HP. at 15% HP, multiplier is 1+0.85*M
-	base = base * multiplier;
-	
-	if(mob:getHP()+base > mob:getMaxHP()) then
-		base = mob:getMaxHP() - mob:getHP(); --cap it
+
+	m = math.random(1,4)+3; -- Sets multiplier to 4-7
+	base = (mob:getMaxHP()/100)* m; -- Finds out how much hp 1% of the monsters total hp is and then multiplies it by our above multiplyer
+									--  the end result is that the monster will heal 4-7% of it's hp when using this move.
+	if(mob:getHP()+base > mob:getMaxHP()) then  -- If the heal would make the monsters hp go above it's Max hp then 
+		base = mob:getMaxHP() - mob:getHP(); -- this line caps it at the max possible heal
 	end
 	skill:setMsg(MSG_SELF_HEAL);
 	mob:addHP(base);
