9664e3cdfa6c35834b649252ce58e8bb96fb6752
7cipherx7 <7cipherx7@gmail.com>
2012-06-04 01:46:55 +0000

    update to exp chains

--- src/map/charutils.cpp
+++ src/map/charutils.cpp
@@ -2054,7 +2054,7 @@ void DistributeExperiencePoints(CCharEntity* PChar, CMobEntity* PMob)
 									else if (PMob->m_Family == 206 && PMob->getZone() == 5) monsterbonus = 1.10f;
 										else if (PMob->m_Family == 208 && PMob->getZone() == 24) monsterbonus = 1.23f;
 					if (monsterbonus > 1.00f) exp *= monsterbonus;
-					permonstercap = ((PMember->PParty != NULL && pcinzone > 1) ? 1.35f : 1.0f);
+					permonstercap = ((PMember->PParty != NULL && pcinzone > 1) ? 1.35f : 1.15f);
 					if (PMember->GetMLevel() <= 50) 
 					{
 						if (exp > (200*permonstercap)) exp = 200*permonstercap;
@@ -2209,7 +2209,7 @@ void DistributeExperiencePoints(CCharEntity* PChar, CMobEntity* PMob)
 	{
 		baseexp  = GetRealExp(maxlevel, PMob->GetMLevel());
 		exp = baseexp;
-		permonstercap = 1.0f;
+		permonstercap = 1.15f;
 		if (PMob->m_Family == 58 && PMob->getZone() == 65) monsterbonus = 1.05f;
 		else if (PMob->m_Family == 245 && PMob->getZone() == 51) monsterbonus = 1.05f;
 			else if (PMob->m_Family == 186 && PMob->getZone() == 153) monsterbonus = 1.23f;
