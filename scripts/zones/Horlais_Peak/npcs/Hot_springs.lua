33b52d45fbf66a7e2026857b1112a5f09c6f02a7
wonderingcatentropy <wonderingcatentropy@gmail.com>
2012-06-05 02:01:52 +0000

    QUEST: Addition of quest Secret of the Damp Scroll in Norg. BUG: Fixed incorrect csid used for Sand d'Oria quest The General's Secret and added TextID for no quest active onTrigger.

--- scripts/zones/Horlais_Peak/npcs/Hot_springs.lua
+++ scripts/zones/Horlais_Peak/npcs/Hot_springs.lua
@@ -4,9 +4,12 @@
 -- @Zone: 139
 -- @pos:  444 -37 -18
 -----------------------------------
+package.loaded["scripts/zones/Horlais_Peak/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/globals/titles");
 require("scripts/globals/keyitems");
 require("scripts/globals/settings");
-package.loaded["scripts/zones/Horlais_Peak/TextIDs"] = nil;
 require("scripts/zones/Horlais_Peak/TextIDs");
 
 -----------------------------------
@@ -14,7 +17,9 @@ require("scripts/zones/Horlais_Peak/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-
+	if(player:getQuestStatus(OUTLANDS,SECRET_OF_THE_DAMP_SCROLL) == QUEST_ACCEPTED and trade:hasItemQty(1210,1) and trade:getItemCount() == 1) then
+		player:startEvent(0x0002,1210);
+	end
 end;
 
 -----------------------------------
@@ -22,10 +27,14 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-if (player:getQuestStatus(0,60) == 1) and (player:hasKeyItem(164) == true) then
-player:startEvent(0x0002);
-end
-	end; 
+	if (player:getQuestStatus(0,60) == 1) and (player:hasKeyItem(164) == true) then
+		player:addKeyItem(165)
+		player:messageSpecial(KEYITEM_OBTAINED,165);
+		player:delKeyItem(164);
+	else
+		player:messageSpecial(ORDINARY);
+	end
+end; 
 
 -----------------------------------
 -- onEventUpdate
@@ -43,9 +52,12 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-if (csid == 0x0002) then
-player:addKeyItem(165)
-player:messageSpecial(KEYITEM_OBTAINED,165);
-player:delKeyItem(164);
-end
+	if (csid == 0x0002) then
+		player:tradeComplete();
+		player:addItem(4949); -- Scroll of Jubaku: Ichi
+		player:messageSpecial(ITEM_OBTAINED, 4949); 
+		player:addFame(OUTLANDS,NORG_FAME*75);
+		player:setTitle(270);
+		player:completeQuest(OUTLANDS,SECRET_OF_THE_DAMP_SCROLL);
+	end
 end;
--- scripts/zones/Norg/npcs/Shivivi.lua
+++ scripts/zones/Norg/npcs/Shivivi.lua
@@ -1,9 +1,17 @@
 -----------------------------------
 -- Area: Norg
 -- NPC: Shivivi
--- Standard Info NPC
+-- Starts Quest: Secret of the Damp Scroll
+-- @zone 252
+-- @pos 68.729 -6.281 -6.432
+-----------------------------------
+package.loaded["scripts/zones/Norg/TextIDs"] = nil;
 -----------------------------------
 
+require("scripts/globals/settings");
+require("scripts/globals/shop");
+require("scripts/globals/quests");
+require("scripts/zones/Norg/TextIDs");
 -----------------------------------
 -- onTrade Action
 -----------------------------------
@@ -16,7 +24,16 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x0055);
+	DampScroll = player:getQuestStatus(OUTLANDS,SECRET_OF_THE_DAMP_SCROLL);
+	mLvl = player:getMainLvl();
+	
+	if(DampScroll == QUEST_AVAILABLE and player:getFameLevel(NORG) >= 3 and mLvl >= 10 and player:hasItem(1210) == true) then
+		player:startEvent(0x001f,1210); -- Start the quest
+	elseif(DampScroll == QUEST_ACCEPTED) then
+		player:startEvent(0x0020); -- Reminder Dialogue
+	else
+		player:startEvent(0x0055);
+	end
 end; 
 
 -----------------------------------
@@ -35,6 +52,9 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+	if(csid == 0x001f) then
+		player:addQuest(OUTLANDS,SECRET_OF_THE_DAMP_SCROLL);
+	end
 end;
 
 
