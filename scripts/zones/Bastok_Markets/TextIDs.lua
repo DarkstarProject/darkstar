c6be637935ff8fceb629ff0cdc315c38ee59399e
wonderingcatentropy <wonderingcatentropy@gmail.com>
2012-06-13 00:49:34 +0000

    BUG FIX: Every vendor in both Bastok Mines and Markets were using dialog that was completely incorrect. This should repair them all. Thanks to Boris-The-Blade for the bug report.

--- scripts/zones/Bastok_Markets/TextIDs.lua
+++ scripts/zones/Bastok_Markets/TextIDs.lua
@@ -18,31 +18,31 @@ CONQUEST = 7535; -- You've earned conquest points!
 YOUVE_EARNED_CONQUEST_POINTS = 7535; -- You've earned conquest points!
 
 -- Shop Texts
-	  BALTHILDA_SHOP_DIALOG = 7439; -- Feeling defenseless of late? Brunhilde's Armory has got you covered!
-	  BRUNHILDE_SHOP_DIALOG = 7437; -- Welcome to my store! You want armor, you want shields? I've got them all!
-		 CIQALA_SHOP_DIALOG = 7435; -- A weapon is the most precious thing to an adventurer! Well, after his life, of course. Choose wisely.
-	  CARMELIDE_SHOP_DIALOG = 7445; -- Ah, welcome, welcome! What might I interest you in?
-CHARGINGCHOKOBO_SHOP_DIALOG = 7438; -- Hello. What piece of armor are you missing?
-	  HARMODIOS_SHOP_DIALOG = 7444; -- Add music to your adventuring life! Welcome to Harmodios's.
-	   HORTENSE_SHOP_DIALOG = 7447; -- Hello there! We have instruments and music sheets at Harmodios's!
-		  MJOLL_SHOP_DIALOG = 7440; -- Welcome. Have a look and compare! You'll never find better wares anywhere.
-		  OLWYN_SHOP_DIALOG = 7441; -- Welcome to Mjoll's Goods! What can I do for you?
-	  PERITRAGE_SHOP_DIALOG = 7436; -- Hey! I've got just the thing for you!
-		  RAGHD_SHOP_DIALOG = 7446; -- Give a smile to that special someone! Welcome to Carmelide's.
-		 SORORO_SHOP_DIALOG = 7443; -- Hello-mellow, welcome to Sororo's Scribe and Notary! Hmm? No, we sell magic spells! What did you think?
-		 TEERTH_SHOP_DIALOG = 7432; -- Welcome to the Goldsmiths' Guild shop. What can I do for you?
-		 VISALA_SHOP_DIALOG = 7433; -- Welcome to the Goldsmiths' Guild shop. How may I help you?
-		ZHIKKOM_SHOP_DIALOG = 7434; -- Hello! Welcome to the only weaponry store in Bastok, the Dragon's Claws!
-		  ZAIRA_SHOP_DIALOG = 7442; -- Greetings. What spell are you looking for?
+	  BALTHILDA_SHOP_DIALOG = 7455; -- Feeling defenseless of late? Brunhilde's Armory has got you covered!
+	  BRUNHILDE_SHOP_DIALOG = 7453; -- Welcome to my store! You want armor, you want shields? I've got them all!
+		 CIQALA_SHOP_DIALOG = 7451; -- A weapon is the most precious thing to an adventurer! Well, after his life, of course. Choose wisely.
+	  CARMELIDE_SHOP_DIALOG = 7461; -- Ah, welcome, welcome! What might I interest you in?
+CHARGINGCHOKOBO_SHOP_DIALOG = 7454; -- Hello. What piece of armor are you missing?
+	  HARMODIOS_SHOP_DIALOG = 7460; -- Add music to your adventuring life! Welcome to Harmodios's.
+	   HORTENSE_SHOP_DIALOG = 7463; -- Hello there! We have instruments and music sheets at Harmodios's!
+		  MJOLL_SHOP_DIALOG = 7456; -- Welcome. Have a look and compare! You'll never find better wares anywhere. 
+		  OLWYN_SHOP_DIALOG = 7457; -- Welcome to Mjoll's Goods! What can I do for you? 
+	  PERITRAGE_SHOP_DIALOG = 7452; -- Hey! I've got just the thing for you!
+		  RAGHD_SHOP_DIALOG = 7462; -- Give a smile to that special someone! Welcome to Carmelide's.
+		 SORORO_SHOP_DIALOG = 7459; -- Hello-mellow, welcome to Sororo's Scribe and Notary! Hmm? No, we sell magic spells! What did you think?
+		 TEERTH_SHOP_DIALOG = 7448; -- Welcome to the Goldsmiths' Guild shop. What can I do for you?
+		 VISALA_SHOP_DIALOG = 7449; -- Welcome to the Goldsmiths' Guild shop. How may I help you?
+		ZHIKKOM_SHOP_DIALOG = 7450; -- Hello! Welcome to the only weaponry store in Bastok, the Dragon's Claws!
+		  ZAIRA_SHOP_DIALOG = 7458; -- Greetings. What spell are you looking for?
 
-   OGGODETT_OPEN_DIALOG = 7448; -- Hello there! Might I interest you in some specialty goods from Aragoneu?
- OGGODETT_CLOSED_DIALOG = 7331; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Aragoneu, but it's not easy getting stuff out of areas that aren't under Bastokan control.
+   OGGODETT_OPEN_DIALOG = 7464; -- Hello there! Might I interest you in some specialty goods from Aragoneu?
+ OGGODETT_CLOSED_DIALOG = 7347; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Aragoneu, but it's not easy getting stuff out of areas that aren't under Bastokan control.
 
-	 YAFAFA_OPEN_DIALOG = 7449; -- Hello! I've got some goods from Kolshushu--interested?
-   YAFAFA_CLOSED_DIALOG = 7330; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Kolshushu, but it's not easy getting stuff out of areas that aren't under Bastokan control.
+	 YAFAFA_OPEN_DIALOG = 7465; -- Hello! I've got some goods from Kolshushu--interested?
+   YAFAFA_CLOSED_DIALOG = 7346; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Kolshushu, but it's not easy getting stuff out of areas that aren't under Bastokan control.
 
-  SOMNPAEMN_OPEN_DIALOG = 7450; -- Welcome! I have goods straight from Sarutabaruta! What say you?
-SOMNPAEMN_CLOSED_DIALOG = 7329; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Sarutabaruta, but it's not easy getting stuff out of areas that aren't under Bastokan control.
-		   
+  SOMNPAEMN_OPEN_DIALOG = 7466; -- Welcome! I have goods straight from Sarutabaruta! What say you?
+SOMNPAEMN_CLOSED_DIALOG = 7345; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Sarutabaruta, but it's not easy getting stuff out of areas that aren't under Bastokan control.
+
 -- Standard NPC Dialogs
-   ITEM_DELIVERY_DIALOG = 7418; -- Need something sent to a friend's house? Sending items to your own room? You've come to the right place!
+   ITEM_DELIVERY_DIALOG = 7434; -- Need something sent to a friend's house? Sending items to your own room? You've come to the right place!
--- scripts/zones/Bastok_Mines/TextIDs.lua
+++ scripts/zones/Bastok_Mines/TextIDs.lua
@@ -22,42 +22,42 @@ VIRNAGE_DIALOG_2 = 10768; -- Hand my letter to Eperdur in the San d'Oria Cathedr
 THE_GATE_IS_LOCKED = 11929; -- The gate is locked.
 
 -- Shop Texts
-          GRISELDA_SHOP_DIALOG = 10684; -- Good of you to drop by the Bat's Lair Inn! Why don't you try some of our specialty plates?
-             BOYTZ_SHOP_DIALOG = 10682; -- Welcome to Boytz's Knickknacks.
-            DEEGIS_SHOP_DIALOG = 10680; -- The only things an adventurer needs are courage and a good suit of armor! Welcome to Deegis's Armour!
-          GELZERIO_SHOP_DIALOG = 12172; -- ...Yes?
-        NEIGEPANCE_SHOP_DIALOG = 10685; -- Hello there. A well-fed chocobo is a happy chocobo!
-        PROUDBEARD_SHOP_DIALOG = 11416; -- Would you be interested in a nice suit of adventurer-issue armor? Be careful when you buy, though. We offer no refunds.
-          ZEMEDARS_SHOP_DIALOG = 10682; -- Everything in our store is top-grade and Galka-made! What're you lookin' for?
+          GRISELDA_SHOP_DIALOG = 10700; -- Good of you to drop by the Bat's Lair Inn! Why don't you try some of our specialty plates?
+             BOYTZ_SHOP_DIALOG = 10698; -- Welcome to Boytz's Knickknacks.
+            DEEGIS_SHOP_DIALOG = 10696; -- The only things an adventurer needs are courage and a good suit of armor! Welcome to Deegis's Armour!
+          GELZERIO_SHOP_DIALOG = 10699; -- ...Yes?
+        NEIGEPANCE_SHOP_DIALOG = 10701; -- Hello there. A well-fed chocobo is a happy chocobo!
+        PROUDBEARD_SHOP_DIALOG = 11432; -- Would you be interested in a nice suit of adventurer-issue armor? Be careful when you buy, though. We offer no refunds.
+          ZEMEDARS_SHOP_DIALOG = 10697; -- Everything in our store is top-grade and Galka-made! What're you lookin' for?
 
-		   FAUSTIN_OPEN_DIALOG = 10686; -- Hello there! Might I interest you specialty goods from Ronfaure?
-		 FAUSTIN_CLOSED_DIALOG = 10567; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Ronfaure, but it's not easy getting stuff from areas that aren't under Bastokan control.
+		   FAUSTIN_OPEN_DIALOG = 10702; -- Hello there! Might I interest you specialty goods from Ronfaure?
+		 FAUSTIN_CLOSED_DIALOG = 10583; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Ronfaure, but it's not easy getting stuff from areas that aren't under Bastokan control.
 
-		RODELLIEUX_OPEN_DIALOG = 10688; -- Hello there! Might I interest you specialty goods from Fauregandi?
-	  RODELLIEUX_CLOSED_DIALOG = 10568; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Fauregandi, but it's not easy getting stuff from areas that aren't under Bastokan control.
+		RODELLIEUX_OPEN_DIALOG = 10704; -- Hello there! Might I interest you specialty goods from Fauregandi?
+	  RODELLIEUX_CLOSED_DIALOG = 10584; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Fauregandi, but it's not easy getting stuff from areas that aren't under Bastokan control.
 
-		   TIBELDA_OPEN_DIALOG = 10689; -- Goods of all varieties, imported directly from the northern land of Valdeaunia!
-		 TIBELDA_CLOSED_DIALOG = 10570; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Valdeaunia, but it's not easy getting stuff from areas that aren't under Bastokan control.
+		   TIBELDA_OPEN_DIALOG = 10705; -- Goods of all varieties, imported directly from the northern land of Valdeaunia!
+		 TIBELDA_CLOSED_DIALOG = 10586; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Valdeaunia, but it's not easy getting stuff from areas that aren't under Bastokan control.
 
-			 MILLE_OPEN_DIALOG = 10687; -- Hello there! Might I interest you specialty goods from Norvallen?
-		   MILLE_CLOSED_DIALOG = 10569; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Norvallen, but it's not easy getting stuff from areas that aren't under Bastokan control.
+			 MILLE_OPEN_DIALOG = 10703; -- Hello there! Might I interest you specialty goods from Norvallen?
+		   MILLE_CLOSED_DIALOG = 10585; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Norvallen, but it's not easy getting stuff from areas that aren't under Bastokan control.
 
-			GALDEO_OPEN_DIALOG = 11245; -- Come! Take a look at all the wonderful goods from Li'Telor.
-		  GALDEO_CLOSED_DIALOG = 10571; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Li'Telor, but it's not easy getting stuff from areas that aren't under Bastokan control.
+			GALDEO_OPEN_DIALOG = 11261; -- Come! Take a look at all the wonderful goods from Li'Telor.
+		  GALDEO_CLOSED_DIALOG = 10587; -- Sorry, I don't have anything to sell you. I'm trying to start a business selling goods from Li'Telor, but it's not easy getting stuff from areas that aren't under Bastokan control.
 
-		   AULAVIA_OPEN_DIALOG = 11246; -- May I interest you in some specialty goods from Vollbow?
-		 AULAVIA_CLOSED_DIALOG = 11247; -- I'm trying to start a business selling goods from Vollbow, but it's not easy getting stuff from areas that aren't under Bastokan control.
+		   AULAVIA_OPEN_DIALOG = 11262; -- May I interest you in some specialty goods from Vollbow?
+		 AULAVIA_CLOSED_DIALOG = 11263; -- I'm trying to start a business selling goods from Vollbow, but it's not easy getting stuff from areas that aren't under Bastokan control.
 
-	  EMALIVEULAUX_OPEN_DIALOG = 12011; -- Rare Tavnazian imports! Get them before they're gone!
-	EMALIVEULAUX_CLOSED_DIALOG = 12012; -- I'd love to sell you goods imported from the island of Tavnazia, but with the area under foreign control, I can't secure my trade routes...
-EMALIVEULAUX_COP_NOT_COMPLETED = 12010; -- I'd like to start my own business someday, but I just haven't found anything that truly interests me.
+	  EMALIVEULAUX_OPEN_DIALOG = 12027; -- Rare Tavnazian imports! Get them before they're gone!
+	EMALIVEULAUX_CLOSED_DIALOG = 12028; -- I'd love to sell you goods imported from the island of Tavnazia, but with the area under foreign control, I can't secure my trade routes...
+EMALIVEULAUX_COP_NOT_COMPLETED = 12026; -- I'd like to start my own business someday, but I just haven't found anything that truly interests me.
 
-             ODOBA_SHOP_DIALOG = 10690; -- Welcome to the Alchemists' Guild! Looking for something specific?
-          MAYMUNAH_SHOP_DIALOG = 10691; -- Welcome to the Alchemists' Guild. How may I help you?
+             ODOBA_SHOP_DIALOG = 10706; -- Welcome to the Alchemists' Guild! Looking for something specific?
+          MAYMUNAH_SHOP_DIALOG = 10707; -- Welcome to the Alchemists' Guild. How may I help you?
 
 -- Weather Dialog
 MARIADOK_DIALOG = 6666; -- Your fate rides on the changing winds of Vana'diel. I can give you insight on the local weather.
 
 -- Standard NPC Dialog
-ITEM_DELIVERY_DIALOG = 10223; -- Need something sent to a friend's house? Sending items to your own house? You've come to the right place!
+ITEM_DELIVERY_DIALOG = 10239; -- Need something sent to a friend's house? Sending items to your own house? You've come to the right place!
      HEMEWMEW_DIALOG = 6866; -- Hello, <Multiple Choice (Player Gender)>[sir/ma'am]. I have been appointed by the Guildworkers' Union to manage the trading of manufactured crafts and the exchange of guild points.
