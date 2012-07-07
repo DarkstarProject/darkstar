205c19b669b13f9a88536db8a946f1e6fa6b1d39
diatanato <diatanato@gmail.com>
2012-07-07 12:28:26 +0000

    SCRIPTS: eerie cloak +1 - логика смены времени суток (18:00-06:00). события изменения уровня персонажа на данный момент не существует

--- scripts/globals/items/eerie_cloak_+1.lua
+++ scripts/globals/items/eerie_cloak_+1.lua
@@ -1,5 +1,5 @@
 -----------------------------------------
---	ID: 11300
+--	ID: 11301
 --	Equip: eerie cloak +1
 --  Adds "Refresh" Effect +1 every 3 sec
 -----------------------------------------
@@ -12,14 +12,15 @@ require("scripts/globals/status");
 -- need to add check for refresh effect
 -- Active while the level of your Main Job is a multiple of 13 and Nighttime.
 -- (I.E. 18:00-06:00 and Level 13, 26, 39, 52, 65, 78 and 91.) 
-function onItemCheck(target)
-	if (target:getEquipID(5) == 11289) then
-		target:addStatusEffectEx(EFFECT_AUTO_REFRESH,0,1,3,0,11289);
+
+function onItemCheck(target, totd)
+	if (target:getEquipID(5) == 11301 and totd ~= 4) then
+		if (totd == 0 and (VanadielTOTD() == 3 or VanadielTOTD() == 7 or VanadielTOTD() == 8)) or totd == 7) then
+			target:addStatusEffectEx(EFFECT_AUTO_REFRESH,0,1,3,0,11301);
+		end
 	else
-		target:delStatusEffect(EFFECT_AUTO_REFRESH,11289);
+		target:delStatusEffect(EFFECT_AUTO_REFRESH,11301);
 	end
-
-
 end;
 
 -----------------------------------
