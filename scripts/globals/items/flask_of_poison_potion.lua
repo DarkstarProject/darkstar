64b4001f0d5a7c71add78bd03941c66780f1a937
whasf <whasf@dspt.info>
2012-07-08 02:48:31 +0000

    SCRIPTS from PrBlahBlahtson

---
+++ scripts/globals/items/flask_of_poison_potion.lua
@@ -0,0 +1,28 @@
+-----------------------------------------
+-- ID: 4157
+-- Item: Poison Potion
+-- Item Effect: Removes 60 HP over 180 seconds
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+    return 0;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	if(target:hasStatusEffect(EFFECT_POISON) == false) then
+		target:addStatusEffect(EFFECT_POISON,1,3,180);
+	else
+		target:messageBasic(423);
+	end
+end;
+
