ca932fad71c437017d534ab824ca6368a46f6eee
bluekirby0 <bluekirby0@gmail.com>
2012-07-10 03:39:29 +0000

    Fixed the one-way door in Ranguemont Pass to open via NPC only.

---
+++ scripts/zones/Ranguemont_Pass/npcs/_4m0.lua
@@ -0,0 +1,18 @@
+-----------------------------------
+-- Area: Ranguemont Pass
+-- NPC: _4m0
+-- Type: Door
+-- @zone: 166
+-- @pos: -180 1.375 -159
+-- 
+-----------------------------------
+package.loaded["scripts/zones/Ranguemont_Pass/TextIDs"] = nil;
+-----------------------------------
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	return 1;
+end;
