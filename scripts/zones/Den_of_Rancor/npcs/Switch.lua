fc31c0fcabdab653139fdd08d03806b24763d47a
Ezekyel38 <Ezekyel38@gmail.com>
2012-07-10 10:44:19 +0000

    SCRIPTS: Add switch for the door at Den of Rancor

---
+++ scripts/zones/Den_of_Rancor/npcs/Switch.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+-- Area: Den of Rancor
+-- NPC:  Switch
+-- @pos -56 45 40 160
+-----------------------------------
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	GetNPCByID(17433049):openDoor(); -- drop gate to Sacrificial Chamber   
+end;
