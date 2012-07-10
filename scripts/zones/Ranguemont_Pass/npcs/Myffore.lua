e73843f435de5181c66d0670ab0e68a2162e9428
bluekirby0 <bluekirby0@gmail.com>
2012-07-10 03:42:18 +0000

    Silly oversight on my part.

--- scripts/zones/Ranguemont_Pass/npcs/Myffore.lua
+++ scripts/zones/Ranguemont_Pass/npcs/Myffore.lua
@@ -39,8 +39,8 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-	printf("CSID: %u",csid);
-	printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 	if(csid == 0x0001 and option == 0) then
 		GetNPCByID(17457333):openDoor()
 	end
