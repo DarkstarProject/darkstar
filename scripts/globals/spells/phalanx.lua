3fa3f121768b4ab51dcdea8df3ed6d343d9d164a
metalfiiish <metalfiiish@gmail.com>
2012-07-11 17:43:57 +0000

    FIX: Fixed Phalanx to properly lengthen duration when Composure is active.

--- scripts/globals/spells/phalanx.lua
+++ scripts/globals/spells/phalanx.lua
@@ -13,7 +13,7 @@ function onSpellCast(caster,target,spell)
 	enhskill = caster:getSkillLevel(ENHANCING_MAGIC_SKILL);
 	final = 0;
 	duration = 180;
-	if (caster:hasStatusEffect(EFFECT_COMPOSURE) == true and caster == target) then
+	if (caster:hasStatusEffect(EFFECT_COMPOSURE) == true and caster:getID() == target:getID()) then
 		duration = duration * 3;
 	end
 	
