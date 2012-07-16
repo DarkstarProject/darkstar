03b9724b8c27f47b37e290377a7da4f1e169f848
bluekirby0 <bluekirby0@gmail.com>
2012-07-16 08:21:19 +0000

    BCNM: Misison 5-1 fight fixed to no longer use the Archlich's script to complete the fight. It now relies on the BCNM-specific script properly.
    Made a few fixes to the BCNM script that were not caught because it was being bypassed before.

--- scripts/zones/QuBia_Arena/bcnms/rank_5_mission.lua
+++ scripts/zones/QuBia_Arena/bcnms/rank_5_mission.lua
@@ -2,6 +2,9 @@
 -- Area: Qu'Bia Arena
 -----------------------------------
 
+require("scripts/globals/keyitems");
+require("scripts/globals/titles");
+
 -- After registering the BCNM via bcnmRegister(bcnmid)
 function OnBcnmRegister(player,instance)
 end;
@@ -20,31 +23,34 @@ end;
 
 function OnBcnmLeave(player,instance,leavecode)
 -- print("leave code "..leavecode);
-        
-        if(leavecode == 2) then -- play end CS. Need time and battle id for record keeping + storage
-                player:startEvent(0x7d02,1,1,1,instance:getTimeInside(),1,1,0);
-        elseif(leavecode == 4) then
-                player:startEvent(0x7d01);
-        end
-        
+	if(leavecode == 2) then -- play end CS. Need time and battle id for record keeping + storage
+		if(player:hasKeyItem(NEW_FEIYIN_SEAL) == true) then
+			player:startEvent(0x7d01,1,1,1,instance:getTimeInside(),1,0,0);
+		else -- Gives skip dialogue if previously completed
+			player:startEvent(0x7d01,1,1,1,instance:getTimeInside(),1,0,1);
+		end
+	elseif(leavecode == 4) then
+		player:startEvent(0x7d02);
+	end
 end;
 
 function onEventUpdate(player,csid,option)
 -- print("bc update csid "..csid.." and option "..option);
-	if (csid == 0x7d02) then
+	if (csid == 0x7d01) then
 		player:delStatusEffect(EFFECT_BATTLEFIELD);
+	end;
 end;
         
 function onEventFinish(player,csid,option)
 -- print("bc finish csid "..csid.." and option "..option);
         
-	if(csid == 0x7d02) then
+	if(csid == 0x7d01) then
 		if(player:hasKeyItem(NEW_FEIYIN_SEAL) == true) then
 			player:addKeyItem(BURNT_SEAL);
 			player:messageSpecial(KEYITEM_OBTAINED,BURNT_SEAL);
 			player:setVar("MissionStatus",12);
 			player:delKeyItem(NEW_FEIYIN_SEAL);
-			SetServerVariable("[BF]Mission_5-1_Enter",0);
-		end
-        
+		end;
+		player:setTitle(ARCHMAGE_ASSASSIN);
+	end;
 end;
--- scripts/zones/QuBia_Arena/mobs/Archlich_Taber_quoan.lua
+++ scripts/zones/QuBia_Arena/mobs/Archlich_Taber_quoan.lua
@@ -3,14 +3,6 @@
 -- NPC:  Archlich Taber'quoan
 -- Mission 5-1 BCNM Fight
 -----------------------------------
-package.loaded["scripts/zones/Balgas_Dais/TextIDs"] = nil;
------------------------------------
-
-require("scripts/globals/settings");
-require("scripts/globals/titles");
-require("scripts/globals/keyitems");
-require("scripts/zones/Balgas_Dais/TextIDs");
-require("scripts/globals/effects/battlefield");
 
 -----------------------------------
 -- onMobSpawn Action
@@ -30,25 +22,6 @@ function onMobDeath(mob,killer)
 	for i = self+1, self+6, 1 do
 		DespawnMob(i);
 	end
-
-	
-	-- Record management
-	record = GetServerVariable("[BF]Mission_5-1_QuBia_Arena_record");
-	partyMembers = 1;
-	killer:setTitle(ARCHMAGE_ASSASSIN);
-	
-	newtimer = os.time() - killer:getVar("BCNM_Timer");
-		
-	if(newtimer < record) then
-		SetServerVariable("[BF]Mission_5-1_QuBia_Arena_record",newtimer);
-	end
-
-	if(killer:hasKeyItem(NEW_FEIYIN_SEAL) == true) then
-		killer:startEvent(0x7d01,0,record,0,newtimer,partyMembers,0,0);
-	else
-		killer:delStatusEffect(EFFECT_BATTLEFIELD);
-	end
-
 end;
 
 -----------------------------------
@@ -78,9 +51,6 @@ end;
 function onEventUpdate(player,csid,option)
 --printf("onUpdate CSID: %u",csid);
 --printf("onUpdate RESULT: %u",option);
-	if (csid == 0x7d01) then
-		player:delStatusEffect(EFFECT_BATTLEFIELD);
-	end
 end;
 
 -----------------------------------
@@ -90,17 +60,4 @@ end;
 function onEventFinish(player,csid,option)
 --printf("onFinish CSID: %u",csid);
 --printf("onFinish RESULT: %u",option);
-
-	pZone = player:getZone();
-	if(csid == 0x7d01) then
-		if(player:hasKeyItem(NEW_FEIYIN_SEAL) == true) then
-			player:addKeyItem(BURNT_SEAL);
-			player:messageSpecial(KEYITEM_OBTAINED,BURNT_SEAL);
-			player:setVar("MissionStatus",12);
-			player:delKeyItem(NEW_FEIYIN_SEAL);
-			SetServerVariable("[BF]Mission_5-1_Enter",0);
-		end
-		player:levelRestriction(0);
-	end
-	
 end;
