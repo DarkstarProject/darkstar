c0e0b0724c04a9f5608bbce76ae201aa6afb8b36
teschnei <teschnei@gmail.com>
2012-07-26 01:41:36 +0000

    WEAPON SKILLS: -fixed chunks of random weaponskills that got destroyed by SVN's patch tool

--- scripts/globals/weaponskills/blade_hi.lua
+++ scripts/globals/weaponskills/blade_hi.lua
@@ -24,14 +24,15 @@ require("scripts/globals/weaponskills");
         
 function OnUseWeaponSkill(player, target, wsID) 
         
-        numHits = 4;
+        numHits = 1;
         ftp100 = 4; ftp200 = 4; ftp300 = 4;
         str_wsc = 0.0; dex_wsc = 0.0; vit_wsc = 0.0; agi_wsc = 0.6; int_wsc = 0.0; mnd_wsc = 0.0; chr_wsc = 0.0;
         crit100 = 0.15; crit200 = 0.2; crit300 = 0.25;
         canCrit = true;
         acc100 = 0.0; acc200= 0.0; acc300= 0.0;
         atkmulti = 1;
-        damage = doPhysicalWeaponskill(player,target,numHits,str_wsc,dex_wsc,vit_wsc,agi_wsc,int_wsc,mnd_wsc,chr_wsc,canCrit,crit100,crit200,crit300,acc100,acc200,acc300,atkmulti);        
-        return damage;
+        damage, tpHits, extraHits = doPhysicalWeaponskill(player,target,numHits,str_wsc,dex_wsc,vit_wsc,agi_wsc,int_wsc,mnd_wsc,chr_wsc,canCrit,crit100,crit200,crit300,acc100,acc200,acc300,atkmulti);        
+
+        return tpHits, extraHits, damage;
         
 end     
