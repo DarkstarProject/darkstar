690bf5e6474d79c0a96e76f0c7ec8f503b938611
samuel.l.hyman <samuel.l.hyman@gmail.com>
2012-07-29 02:09:32 +0000

    SCRIPTS: More ninjutsu debuffs.
    Submitted by Dunharrow.

---
+++ scripts/globals/spells/hojo_ni.lua
@@ -0,0 +1,57 @@
+-----------------------------------------
+-- Spell: Hojo:Ni
+-- Description: Inflicts Slow on target. 
+-- Edited from slow.lua
+-----------------------------------------
+
+require("scripts/globals/status");
+require("scripts/globals/magic");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function onSpellCast(caster,target,spell)
+	
+	dINT = (caster:getStat(MOD_INT) - target:getStat(MOD_INT));
+	staff = StaffBonus(caster,spell);
+	--Power for Hojo is a flat 19.5% reduction
+	power = 19.5;
+	--Duration and Resistance calculation
+	duration = 300 * applyResistance(caster,spell,target,dINT,NINJUTSU_SKILL,staff);
+	--Calculates the resist chance from Resist Blind trait
+	if(math.random(0,100) >= target:getMod(MOD_SLOWRES)) then
+		-- Spell succeeds if a 1 or 1/2 resist check is achieved
+		if(duration >= 150) then
+			--Looks for another Slow or Haste effect and replaces it if stronger
+			slow = target:getStatusEffect(EFFECT_SLOW);
+			haste = target:getStatusEffect(EFFECT_HASTE);
+			if(slow ~= nil) then
+				if(slow:getPower() > power) then
+					target:delStatusEffect(EFFECT_SLOW);	
+					target:addStatusEffect(EFFECT_SLOW,power,0,duration);
+					spell:setMsg(237);
+				else
+					spell:setMsg(75);
+				end
+			elseif(haste ~= nil) then
+				if(haste:getPower() < (-1 * power)) then
+					target:delStatusEffect(EFFECT_HASTE);	
+					target:addStatusEffect(EFFECT_SLOW,power,0,duration);
+					spell:setMsg(237);
+				else
+					spell:setMsg(75);
+				end
+			else
+				target:addStatusEffect(EFFECT_SLOW,power,0,duration);
+				spell:setMsg(237);
+			end
+		else
+			spell:setMsg(85);
+		end
+	else
+		spell:setMsg(284);
+	end
+	target:updateEnmity(caster,320,1);
+	return EFFECT_SLOW;
+end;
