4bb938a3822aab62dd668599f93c0f4e943c2317
bluekirby0 <bluekirby0@gmail.com>
2012-08-04 20:47:36 +0000

    QUEST: Added "Exit the Gambler" quest (thanks DeVeous)

--- scripts/zones/Northern_San_dOria/npcs/Aurege.lua
+++ scripts/zones/Northern_San_dOria/npcs/Aurege.lua
@@ -5,9 +5,12 @@
 --  Starts Quest: Exit the Gambler
 --  @zone: 231
 --  @pos: -156.253 11.999 253.691
---
 -----------------------------------
-
+require("scripts/globals/quests");
+require("scripts/globals/keyitems");
+require("scripts/globals/titles");
+package.loaded["scripts/zones/Northern_San_dOria/TextIDs"] = nil;
+require("scripts/zones/Northern_San_dOria/TextIDs");
 -----------------------------------
 -- onTrade Action
 -----------------------------------
@@ -18,9 +21,16 @@ end;
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
-
 function onTrigger(player,npc)
-	player:startEvent(0x0209);	
+        exitTheGambler = player:getQuestStatus(SANDORIA,EXIT_THE_GAMBLER);
+        
+        if (player:hasKeyItem(MAP_OF_KING_RANPERRES_TOMB)) then
+           player:startEvent(0x0202);
+        elseif (exitTheGambler == QUEST_COMPLETED) then
+           player:startEvent(0x0204);
+        else
+           player:startEvent(0x0209);
+        end
 end;
 
 -----------------------------------
@@ -39,5 +49,15 @@ end;
 function onEventFinish(player,csid,option)
 	-- printf("CSID: %u",csid);
 	-- printf("RESULT: %u",option);
+	exitTheGambler = player:getQuestStatus(SANDORIA,EXIT_THE_GAMBLER);
+	
+	if (exitTheGambler == QUEST_AVAILABLE) then
+	   player:addQuest(SANDORIA,EXIT_THE_GAMBLER);
+	elseif (exitTheGambler == QUEST_COMPLETED and player:hasKeyItem(MAP_OF_KING_RANPERRES_TOMB) == false) then
+           player:messageSpecial(KEYITEM_OBTAINED,MAP_OF_KING_RANPERRES_TOMB);
+           player:addKeyItem(MAP_OF_KING_RANPERRES_TOMB);
+	   player:setTitle(DAYBREAK_GAMBLER);
+           player:addFame(SANDORIA,SAN_FAME*30);
+	end
 end;
 
--- scripts/zones/Southern_San_dOria/npcs/Varchet.lua
+++ scripts/zones/Southern_San_dOria/npcs/Varchet.lua
@@ -4,17 +4,41 @@
 --  Type: NPC
 -- @zone: 230
 --  @pos: 116.484 -1 91.554
--- 
--- Auto-Script: Requires Verification (Verified by Brawndo)
 -----------------------------------
+require("scripts/globals/quests");
 package.loaded["scripts/zones/Southern_San_dOria/TextIDs"] = nil;
------------------------------------
+require("scripts/zones/Southern_San_dOria/TextIDs");
 
+GAME_WON = 0;
+GAME_LOST = 2;
+GAME_TIE = 3;
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
+
+	if (trade:getGil() == 5) then
+	   player:delGil(5);
+	   vdie1 = math.random(1,6);
+  	   vdie2 = math.random(1,6);
+  	   vtotal = vdie1 + vdie2;
+  	   pdie1 = math.random(1,6);
+  	   pdie2 = math.random(1,6);
+  	   ptotal = pdie1 + pdie2;
+
+           if (ptotal > vtotal) then
+              player:startEvent(0x0207,vdie1,vdie2,vtotal,pdie1,pdie2,ptotal,GAME_WON);
+           elseif (vtotal > ptotal) then
+              player:startEvent(0x0207,vdie1,vdie2,vtotal,pdie1,pdie2,ptotal,GAME_LOST);
+           elseif (ptotal == vtotal) then
+              player:startEvent(0x0207,vdie1,vdie2,vtotal,pdie1,pdie2,ptotal,GAME_TIE);
+           else
+              printf("Results Varchet's Roll : %u, Player's Roll : %u", vtotal, ptotal);
+           end
+	else
+	    player:startEvent(0x0260);
+	end
 end;
 
 -----------------------------------
@@ -22,7 +46,14 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:startEvent(0x020d);
+
+        exitTheGambler = player:getQuestStatus(SANDORIA,EXIT_THE_GAMBLER);
+        
+        if (exitTheGambler == QUEST_ACCEPTED) then
+           player:startEvent(0x027e);
+        else
+           player:startEvent(0x020d);
+        end
 end;
 
 -----------------------------------
@@ -30,8 +61,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-	-- printf("CSID: %u",csid);
-	-- printf("RESULT: %u",option);
+	printf("U CSID: %u",csid);
+	printf("U RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -39,7 +70,27 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-	-- printf("CSID: %u",csid);
-	-- printf("RESULT: %u",option);
+	--printf("F CSID: %u",csid);
+	--printf("F RESULT: %u",option);
+	
+	exitTheGambler = player:getQuestStatus(SANDORIA,EXIT_THE_GAMBLER);
+	npc = player:getEventTarget();
+	
+	if (csid == 519) then
+          if (option == GAME_WON) then
+  	    player:addGil(10);
+  	    player:messageSpecial(GIL_OBTAINED,10);
+  	    if (exitTheGambler == QUEST_ACCEPTED) then
+  	      player:completeQuest(SANDORIA,EXIT_THE_GAMBLER);
+  	      player:showText(npc,7629);
+  	    end
+          elseif (option == GAME_TIE) then
+            player:addGil(5);
+  	    player:messageSpecial(GIL_OBTAINED,5);
+  	    player:showText(npc,7622);
+          else
+            player:showText(npc,7620);
+          end
+        end
 end;
 
