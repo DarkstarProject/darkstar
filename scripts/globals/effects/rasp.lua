19fc8df17996a395bf3965ae685a87d520b1bcaa
xworndownx <no_email@no_email>
2012-08-22 20:29:49 +0000

    Stoneskin checks added to the OnTick: Submitted by PrBlahBlahtson

--- scripts/globals/effects/rasp.lua
+++ scripts/globals/effects/rasp.lua
@@ -20,8 +20,23 @@ end;
 -----------------------------------
 
 function onEffectTick(target,effect)
-	target:delHP(effect:getPower());
-	target:wakeUp();
+	skin = target:getMod(MOD_STONESKIN);
+    if(skin>0) then
+        if(skin >= effect:getPower()) then --absorb all damage
+            target:delMod(MOD_STONESKIN,effect:getPower());
+            if(target:getMod(MOD_STONESKIN)==0) then
+                target:delStatusEffect(EFFECT_STONESKIN);
+            end
+        else --absorbs some damage then wear
+            target:delMod(MOD_STONESKIN,skin);
+            target:delStatusEffect(EFFECT_STONESKIN);
+			target:wakeUp();
+			target:delHP(effect:getPower() - skin);
+        end
+	else
+		target:delHP(effect:getPower());
+		target:wakeUp();
+	end
 end;
 
 -----------------------------------
