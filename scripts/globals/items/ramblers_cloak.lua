96733c7e1b713ac113ed190c16c8a8b746a4e43b
zharthemad <zharthemad@gmail.com>
2012-08-24 03:34:28 +0000

    Made it so tidal talisman will port party members. Added ramblers_cloak.lua for latent effect takes effect

---
+++ scripts/globals/items/ramblers_cloak.lua
@@ -0,0 +1,29 @@
+-----------------------------------------
+--	ID: 11312
+--	Equip: Ramblers Cloak
+--  Latent effect: STR +5
+--	Active when TP >= 100%, should not active during weapon skills
+-- Note: Script is not working 100%, there is no check on equipment after it has been equiped or unequiped.
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+
+	TP = target:getTP();
+	body = target:getEquipID(5);
+
+	if(body == 11312) then
+		if (TP >= 100) then
+			target:addMod(MOD_STR, 5);
+		elseif (TP < 100) then
+			target:delMod(MOD_STR);
+		end
+	elseif (body ~= 11312 and TP >= 100) then
+		target:delMod(MOD_STR, 5);
+	end
+end;
