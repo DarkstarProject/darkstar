ca70509e2c12ea85beb643ae3d23ace1189ad9ba
whasf <whasf@dspt.info>
2012-08-28 02:03:51 +0000

    SCRIPTS: This will make all of the monoliths in sky open and close the way they should.
    Thank you Zedingo

--- scripts/zones/The_Shrine_of_RuAvitau/npcs/Monolith.lua
+++ scripts/zones/The_Shrine_of_RuAvitau/npcs/Monolith.lua
@@ -26,12 +26,12 @@ function onTrigger(player,npc)
 	-- Activateur Jaune: 17506738,17506740,17506742,17506744,17506762,17506764,17506770,17506772,17506774
 	-- Porte Jaune: 17506718,17506717,17506716,17506715,17506727,17506728,17506729,17506730,17506734,17506736
 	
-	nID = npc:getID();
-	printf("id: %u", nID);
+	local nID = npc:getID();
+--	printf("id: %u", nID);
 	GetNPCByID(17506774):setAnimation(1);
 	if(nID == 17506746 or nID == 17506748 or nID == 17506750 or nID == 17506752 or nID == 17506758 or 
 	   nID == 17506760 or nID == 17506766 or nID == 17506768 or nID == 17506776) then
-		GetNPCByID(17506719):setAnimation(8);
+		GetNPCByID(17506719):setAnimation(8);--blue door
 		GetNPCByID(17506720):setAnimation(8);
 		GetNPCByID(17506721):setAnimation(8);
 		GetNPCByID(17506722):setAnimation(8);
@@ -41,7 +41,7 @@ function onTrigger(player,npc)
 		GetNPCByID(17506732):setAnimation(8);
 		GetNPCByID(17506733):setAnimation(8);
 		GetNPCByID(17506735):setAnimation(8);
-		GetNPCByID(17506718):setAnimation(9);
+		GetNPCByID(17506718):setAnimation(9);--yellow door
 		GetNPCByID(17506717):setAnimation(9);
 		GetNPCByID(17506716):setAnimation(9);
 		GetNPCByID(17506715):setAnimation(9);
@@ -51,8 +51,26 @@ function onTrigger(player,npc)
 		GetNPCByID(17506730):setAnimation(9);
 		GetNPCByID(17506734):setAnimation(9);
 		GetNPCByID(17506736):setAnimation(9);
+		GetNPCByID(17506745):setAnimation(8);--blue monolith
+		GetNPCByID(17506747):setAnimation(8);
+		GetNPCByID(17506749):setAnimation(8);
+		GetNPCByID(17506751):setAnimation(8);
+		GetNPCByID(17506757):setAnimation(8);
+		GetNPCByID(17506759):setAnimation(8);
+		GetNPCByID(17506765):setAnimation(8);
+		GetNPCByID(17506767):setAnimation(8);
+		GetNPCByID(17506775):setAnimation(8);
+		GetNPCByID(17506737):setAnimation(9);--yellow monolith
+		GetNPCByID(17506739):setAnimation(9);
+		GetNPCByID(17506741):setAnimation(9);
+		GetNPCByID(17506743):setAnimation(9);
+		GetNPCByID(17506761):setAnimation(9);
+		GetNPCByID(17506763):setAnimation(9);
+		GetNPCByID(17506769):setAnimation(9);
+		GetNPCByID(17506771):setAnimation(9);
+		GetNPCByID(17506773):setAnimation(9);
 	else
-		GetNPCByID(17506719):setAnimation(9);
+		GetNPCByID(17506719):setAnimation(9);-- Activateur Jaune: 17506738,17506740,17506742,17506744,17506762,17506764,17506770,17506772,17506774
 		GetNPCByID(17506720):setAnimation(9);
 		GetNPCByID(17506721):setAnimation(9);
 		GetNPCByID(17506722):setAnimation(9);
@@ -72,6 +90,24 @@ function onTrigger(player,npc)
 		GetNPCByID(17506730):setAnimation(8);
 		GetNPCByID(17506734):setAnimation(8);
 		GetNPCByID(17506736):setAnimation(8);
+		GetNPCByID(17506745):setAnimation(9);--blue monolith
+		GetNPCByID(17506747):setAnimation(9);
+		GetNPCByID(17506749):setAnimation(9);
+		GetNPCByID(17506751):setAnimation(9);
+		GetNPCByID(17506757):setAnimation(9);
+		GetNPCByID(17506759):setAnimation(9);
+		GetNPCByID(17506765):setAnimation(9);
+		GetNPCByID(17506767):setAnimation(9);
+		GetNPCByID(17506775):setAnimation(9);
+		GetNPCByID(17506737):setAnimation(8);--yellow monolith
+		GetNPCByID(17506739):setAnimation(8);
+		GetNPCByID(17506741):setAnimation(8);
+		GetNPCByID(17506743):setAnimation(8);
+		GetNPCByID(17506761):setAnimation(8);
+		GetNPCByID(17506763):setAnimation(8);
+		GetNPCByID(17506769):setAnimation(8);
+		GetNPCByID(17506771):setAnimation(8);
+		GetNPCByID(17506773):setAnimation(8);
 	end
 end;
 
--- scripts/zones/VeLugannon_Palace/npcs/Monolith.lua
+++ scripts/zones/VeLugannon_Palace/npcs/Monolith.lua
@@ -26,17 +26,17 @@ function onTrigger(player,npc)
 	-- Activateur Jaune: 17502629, 17502627, 17502625, 17502623, 17502621
 	-- Porte Jaune: 17502605, 17502606, 17502607, 17502608, 17502609, 17502610, 17502611, 17502612
 	
-	nID = npc:getID();
+	local nID = npc:getID();
 	
 	if(nID == 17502639 or nID == 17502637 or nID == 17502635 or nID == 17502633 or nID == 17502631) then
-		GetNPCByID(17502619):setAnimation(8);
+		GetNPCByID(17502619):setAnimation(8);--blue door
 		GetNPCByID(17502618):setAnimation(8);
 		GetNPCByID(17502617):setAnimation(8);
 		GetNPCByID(17502616):setAnimation(8);
 		GetNPCByID(17502615):setAnimation(8);
 		GetNPCByID(17502614):setAnimation(8);
 		GetNPCByID(17502613):setAnimation(8);
-		GetNPCByID(17502605):setAnimation(9);
+		GetNPCByID(17502605):setAnimation(9);--yellow door
 		GetNPCByID(17502606):setAnimation(9);
 		GetNPCByID(17502607):setAnimation(9);
 		GetNPCByID(17502608):setAnimation(9);
@@ -44,6 +44,16 @@ function onTrigger(player,npc)
 		GetNPCByID(17502610):setAnimation(9);
 		GetNPCByID(17502611):setAnimation(9);
 		GetNPCByID(17502612):setAnimation(9);
+		GetNPCByID(17502638):setAnimation(8);--blue monolith
+		GetNPCByID(17502636):setAnimation(8);
+		GetNPCByID(17502634):setAnimation(8);
+		GetNPCByID(17502632):setAnimation(8);
+		GetNPCByID(17502630):setAnimation(8);
+		GetNPCByID(17502628):setAnimation(9);--yellow monolith
+		GetNPCByID(17502626):setAnimation(9);
+		GetNPCByID(17502624):setAnimation(9);
+		GetNPCByID(17502622):setAnimation(9);
+		GetNPCByID(17502620):setAnimation(9);
 	else
 		GetNPCByID(17502619):setAnimation(9);
 		GetNPCByID(17502618):setAnimation(9);
@@ -60,6 +70,16 @@ function onTrigger(player,npc)
 		GetNPCByID(17502610):setAnimation(8);
 		GetNPCByID(17502611):setAnimation(8);
 		GetNPCByID(17502612):setAnimation(8);
+		GetNPCByID(17502638):setAnimation(9);
+		GetNPCByID(17502636):setAnimation(9);
+		GetNPCByID(17502634):setAnimation(9);
+		GetNPCByID(17502632):setAnimation(9);
+		GetNPCByID(17502630):setAnimation(9);
+		GetNPCByID(17502628):setAnimation(8);
+		GetNPCByID(17502626):setAnimation(8);
+		GetNPCByID(17502624):setAnimation(8);
+		GetNPCByID(17502622):setAnimation(8);
+		GetNPCByID(17502620):setAnimation(8);
 	end
 end;
 
