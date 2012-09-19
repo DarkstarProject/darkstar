130040cef47970f921388622f51320128d5f73dd
diatanato <diatanato@gmail.com>
2012-09-19 03:50:02 +0000

    FIX: Northern_San_dOria/npcs/Vamorcote.lua

--- scripts/zones/Northern_San_dOria/npcs/Vamorcote.lua
+++ scripts/zones/Northern_San_dOria/npcs/Vamorcote.lua
@@ -8,8 +8,8 @@
 -- Auto-Script: Requires Verification (Verified by Brawndo)
 -----------------------------------
 package.loaded["scripts/zones/Northern_San_dOria/TextIDs"] = nil;
-
 -----------------------------------
+
 require("scripts/globals/quests");
 require("scripts/globals/settings");
 require("scripts/zones/Northern_San_dOria/TextIDs");
@@ -19,13 +19,9 @@ require("scripts/zones/Northern_San_dOria/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
--- "The Setting Sun" conditional script
-theSettingSun = player:getQuestStatus(SANDORIA,THE_SETTING_SUN);
-	
-	if (theSettingSun == QUEST_ACCEPTED) then
-		count = trade:getItemCount();
-		engravedKey = trade:hasItemQty(535,1);
-		if (engravedKey == true and count == 1) then
+	-- "The Setting Sun" conditional script
+	if (player:getQuestStatus(SANDORIA,THE_SETTING_SUN) == QUEST_ACCEPTED) then
+		if (trade:hasItemQty(535,1) and trade:getItemCount() == 1) then
 			player:startEvent (0x0292)
 		end;
 	end;
@@ -36,23 +32,19 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:delQuest(SANDORIA, BLACKMAIL);
 	-- Look at the "The Setting Sun" quest status and San d'Oria player's fame
 	theSettingSun = player:getQuestStatus(SANDORIA,THE_SETTING_SUN);
-	sanFame = player:getFameLevel(SANDORIA);
-	blackMail = player:getQuestStatus(SANDORIA, BLACKMAIL);
-		
 	
-	if (theSettingSun == QUEST_AVAILABLE  and sanFame >= 5 and blackMail ~= QUEST_COMPLETED) then	
+	if (theSettingSun == QUEST_AVAILABLE  and 
+		player:getFameLevel(SANDORIA) >= 5 and 
+		player:getQuestStatus(SANDORIA, BLACKMAIL) ~= QUEST_COMPLETED) 
+	then	
 		player:startEvent(0x028e,0,535,535); --The quest is offered to the player.
-
 	elseif (theSettingSun == QUEST_ACCEPTED) then
 		player:startEvent(0x028f,0,0,535); --The NPC asks if the player got the key.'
-		
-	elseif (theSettingSun == QUEST_COMPLETED and player:needToZone() == true) then
+	elseif (theSettingSun == QUEST_COMPLETED and player:needToZone()) then
 		player:startEvent(0x0293); --The quest is already done by the player and the NPC does small talks.
 	else
-		 
 		player:startEvent(0x028b);
 	end;
 end;
@@ -73,18 +65,15 @@ end;
 function onEventFinish(player,csid,option)
 	-- printf("CSID: %u",csid);
 	-- printf("RESULT: %u",option);
-	TheSettingSun = player:getQuestStatus(SANDORIA,THE_SETTING_SUN);
+	
 	if(csid == 0x028e and option == 1) then --Player accepts the quest
 		player:addQuest(SANDORIA,THE_SETTING_SUN);
-	end;
-			
-	
-	if (csid == 0x0292)then --The player trades the Engraved Key to the NPC. Here come the rewards!
+	elseif (csid == 0x0292) then --The player trades the Engraved Key to the NPC. Here come the rewards!
 		player:tradeComplete();
 		player:addGil(GIL_RATE*10000);
 		player:messageSpecial(GIL_OBTAINED,GIL_RATE*10000);
-		player:completeQuest(SANDORIA,THE_SETTING_SUN);
 		player:addFame(SANDORIA,SAN_FAME*30);
+		player:completeQuest(SANDORIA,THE_SETTING_SUN);
 	end;
 end;
 
