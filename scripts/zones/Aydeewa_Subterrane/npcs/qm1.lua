a015eb42234a7ff8b8ad76ee84ef2e441b8e5fef
diatanato <diatanato@gmail.com>
2012-10-19 07:13:58 +0000

    SCRISCRIPTS: Aydeewa_Subterrane (Ezekyel)

---
+++ scripts/zones/Aydeewa_Subterrane/npcs/qm1.lua
@@ -0,0 +1,48 @@
+-----------------------------------
+-- Area: Aydeewa Subterrane
+-- NPC:  ??? (Spawn Nosferatu(ZNM T3))
+-- @pos -199 8 -62 68 
+-----------------------------------
+package.loaded["scripts/zones/Aydeewa_Subterrane/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/zones/Aydeewa_Subterrane/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+	
+	if(trade:hasItemQty(2584,1) and trade:getItemCount() == 1) then -- Trade Pure Blood
+		player:tradeComplete();
+		SpawnMob(17056158,180):updateEnmity(player);
+	end
+	
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:messageSpecial(NOTHING_HAPPENS);
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+-- printf("CSID: %u",csid);
+-- printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+-- printf("CSID: %u",csid);
+-- printf("RESULT: %u",option);
+end;
---
+++ scripts/zones/Aydeewa_Subterrane/npcs/qm3.lua
@@ -0,0 +1,48 @@
+-----------------------------------
+-- Area: Aydeewa Subterrane
+-- NPC:  ??? (Spawn Chigre(ZNM T1))
+-- @pos -217 35 12 68
+-----------------------------------
+package.loaded["scripts/zones/Aydeewa_Subterrane/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/zones/Aydeewa_Subterrane/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+	
+	if(trade:hasItemQty(2602,1) and trade:getItemCount() == 1) then -- Trade Spoilt Blood
+		player:tradeComplete();
+		SpawnMob(17056187,180):updateEnmity(player);
+	end
+	
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:messageSpecial(NOTHING_HAPPENS);
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+-- printf("CSID: %u",csid);
+-- printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+-- printf("CSID: %u",csid);
+-- printf("RESULT: %u",option);
+end;
