ec32cbfa5e22b4af8d9688a8a774ecde8851f18f
diatanato <diatanato@gmail.com>
2012-10-22 08:55:05 +0000

    CORE: небольшой эксперимент с merits

---
+++ src/map/merit.cpp
@@ -0,0 +1,58 @@
+﻿/*
+===========================================================================
+
+  Copyright (c) 2010-2012 Darkstar Dev Teams
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+
+  This file is part of DarkStar-server source code.
+
+===========================================================================
+*/
+
+#include "merit.h"
+
+/************************************************************************
+*                                                                       *
+*  Две версии значений - до abyssea и после                             *
+*                                                                       *
+************************************************************************/
+
+// массив больше на одно значение, заполняемое нулем
+
+#if define ABYSSEA_EXPANSION
+uint8 upgrade[8][16] =
+{
+    {1,2,3,4,5,5,5,5,5,7,7,7,9,9,9},
+    {3,6,9,9,9,12,12,12,12,15,15,15},
+    {1,2,3,3,3,3,3,3},
+    {1,2,3,3},
+    {1,2,3,3,3,3,3,3},
+    {1,2,3,4,5},
+    {1,2,3,4,5},
+    {3,4,5,5,5},
+};
+#else
+uint8 upgrade[8][9] =
+{
+    {1,2,3,4,5,5,5,5},
+    {3,6,9,9,9},
+    {1,2,3,3,3,3,3,3},
+    {1,2,3,3},
+    {1,2,3,3,3,3,3,3},
+    {1,2,3,4},
+    {1,2,3,4,5},
+    {3,4,5,5,5},
+};
+#endif
---
+++ src/map/merit.h
@@ -0,0 +1,65 @@
+﻿/*
+===========================================================================
+
+  Copyright //(c) 2010-2012 Darkstar Dev Teams
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  //(at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+
+  This file is part of DarkStar-server source code.
+
+===========================================================================
+*/
+
+#ifndef _CMERIT_H
+#define _CMERIT_H
+
+#include "../common/cbasetypes.h"
+
+/************************************************************************
+*                                                                       *
+*                                                                       *
+*                                                                       *
+************************************************************************/
+
+enum MERIT_CATEGORY
+{
+    MCATEGORY_HP_MP = 0x0040,
+};
+
+/************************************************************************
+*                                                                       *
+*                                                                       *
+*                                                                       *
+************************************************************************/
+
+enum MERIT_TYPE
+{
+    MERIT_MAX_HP = MCATEGORY_HP_MP + 0x00,
+	MERIT_MAX_MP = MCATEGORY_HP_MP + 0x02,
+};
+
+/************************************************************************
+*                                                                       *
+*                                                                       *
+*                                                                       *
+************************************************************************/
+
+struct Merit_t
+{
+    uint16 id;      // id мерита
+    uint8  next;    // необходимое количество меритов для следующего усиления
+    uint8  count;   // текущее количество усилений
+};
+
+#endif
