8244935d597fc7bdf0fc661b45e46eb000b3995d
metalfiiish <metalfiiish@gmail.com>
2012-10-26 08:42:57 +0000

    SCRIPTS: Added persikos tank. Requested in bug 312.

---
+++ scripts/globals/items/persikos_au_lait_tank.lua
@@ -0,0 +1,25 @@
+-----------------------------------------
+--	ID: 16227
+--	Persikos Tank
+--	When used, you will obtain one Persikos au lait
+-----------------------------------------
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+	result = 0;
+	if (target:getFreeSlotsCount() == 0) then
+		result = 308;
+	end
+	return result;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	target:addItem(4303,1);
+end;
