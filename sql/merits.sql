ba607114b5824d2660dba9ae433f5713cb267351
diatanato <diatanato@gmail.com>
2012-11-04 07:45:18 +0000

    SQL: merits.sql example

--- sql/merits.sql
+++ sql/merits.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Sam 25 Juin 2011 à 10:09
+-- GÃ©nÃ©rÃ© le : Dim 04 Novembre 2012 Ã  11:43
 -- Version du serveur: 6.0.0
 -- Version de PHP: 5.2.9-2
 
@@ -16,13 +16,13 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `dspdb`
+-- Base de donnÃ©es: `dspdb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table 'merits'
+-- Structure de la table `merits`
 --
 
 DROP TABLE IF EXISTS `merits`;
@@ -33,8 +33,12 @@ CREATE TABLE IF NOT EXISTS `merits` (
   `value` smallint(5) NOT NULL DEFAULT '0',
   `jobs` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`meritid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `merit`
+-- Contenu de la table `merits`
 --
+
+INSERT INTO `merits` VALUES(64, 'max_hp', 8, 10, 1048575);
+INSERT INTO `merits` VALUES(66, 'max_mp', 8, 10, 1048575);
+INSERT INTO `merits` VALUES(222, 'guarding_skill', 4, 2, 131074);
