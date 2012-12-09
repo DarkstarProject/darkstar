614b8352ab4f41af59768926c982db92eb0d3dcb
whasf <whasf@dspt.info>
2012-12-09 17:46:57 +0000

    SCRIPTS: MMM NPCs, just basic scripts. Thank you Radical Dreamer

---
+++ scripts/zones/Lower_Jeuno/npcs/Duplidoc.lua
@@ -0,0 +1,44 @@
+-----------------------------------
+-- Area: Lower Jeuno
+-- NPC: Duplidoc
+-- Standard Info NPC
+-----------------------------------
+
+package.loaded["scripts/zones/Lower_Jeuno/TextIDs"] = nil;
+require("scripts/zones/Lower_Jeuno/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+player:startEvent(0x4e28);
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+
+
