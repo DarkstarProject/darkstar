a180c25431f1ad5157b6c1bc08e9fea1f5b77031
linkandentity <linkandentity@gmail.com>
2012-12-18 13:37:12 +0000

    Bhaflau thickets, Can trade coin to Hamta-Iramta to pass

--- scripts/zones/Bhaflau_Thickets/npcs/Hamta-Iramta.lua
+++ scripts/zones/Bhaflau_Thickets/npcs/Hamta-Iramta.lua
@@ -15,6 +15,14 @@ package.loaded["scripts/zones/Bhaflau_Thickets/TextIDs"] = nil;
 -----------------------------------
 
 function onTrade(player,npc,trade)
+
+	if(trade:getItemCount() == 1 and trade:hasItemQty(2185,1)) then -- Silver
+		player:tradeComplete();
+		player:setPos(-458,-16,0,189); -- using the pos method until the problem below is fixed
+		-- player:startEvent(0x0087); -- << this CS goes black at the end, never fades in
+		return 1;
+	end
+
 end;
 
 -----------------------------------
@@ -22,7 +30,22 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:startEvent(0x0086);
+
+	-- NPC is on a slant which makes this really difficult
+	
+	if(player:getXPos() < -456 and player:getXPos() > -459 and player:getYPos() < -16.079) then 
+		player:startEvent(0x0086);
+			
+	elseif(player:getXPos() < -459 and player:getXPos() > -462 and player:getYPos() < -16.070) then 
+		player:startEvent(0x0086);
+
+	elseif(player:getXPos() < -462 and player:getXPos() > -464 and player:getYPos() < -16.071) then 
+		player:startEvent(0x0086);
+		
+	else
+		player:startEvent(0x0088);
+	end
+
 end;
 
 -----------------------------------
@@ -40,6 +63,6 @@ end;
 
 function onEventFinish(player,csid,option)
 	-- printf("CSID: %u",csid);
-	-- printf("RESULT: %u",option);
+	-- printf("RESULT: %u",option);	
 end;
 
