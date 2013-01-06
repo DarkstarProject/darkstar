eb79ac59ad819735b1f5631e936f88a124739ec0
bluekirby0 <bluekirby0@gmail.com>
2013-01-06 12:59:26 +0000

    Fixed transport table so it aligns with the updated npc table properly

--- sql/transport.sql
+++ sql/transport.sql
@@ -56,8 +56,8 @@ INSERT INTO `transport` VALUES(2, 'Jeuno-Kazham_Airship', 17784932, 17784931, -4
 INSERT INTO `transport` VALUES(3, 'Jeuno-Sandoria_Airship', 17784932, 17784928, -68.000, 10.450, 117.000, 64, 474, 18, 19, 0, 360, 12, 60, 12, 223);
 INSERT INTO `transport` VALUES(4, 'Jeuno-Windurst_Airship', 17784932, 17784930, -6.000, 10.450, -117.000, 192, 476, 22, 23, 90, 360, 12, 60, 12, 225);
 INSERT INTO `transport` VALUES(5, 'Jeuno-Bastok_Airship', 17784932, 17784929, -70.000, 10.450, -117.000, 192, 475, 20, 21, 180, 360, 12, 60, 16, 224);
-INSERT INTO `transport` VALUES(6, 'Sandoria-Jeuno_Airship', 17727593, 17727590, 20.000, -2.000, 44.000, 64, 369, 18, 19, 180, 360, 12, 60, 16, 223);
-INSERT INTO `transport` VALUES(7, 'Windurst-Jeuno_Airship', 17760415, 17760414, 242.281, -3.522, 61.994, 96, 416, 18, 19, 270, 360, 18, 60, 14, 225);
+INSERT INTO `transport` VALUES(6, 'Sandoria-Jeuno_Airship', 17727594, 17727591, 20.000, -2.000, 44.000, 64, 369, 18, 19, 180, 360, 12, 60, 16, 223);
+INSERT INTO `transport` VALUES(7, 'Windurst-Jeuno_Airship', 17760416, 17760415, 242.281, -3.522, 61.994, 96, 416, 18, 19, 270, 360, 18, 60, 14, 225);
 INSERT INTO `transport` VALUES(8, 'Bastok-Jeuno_Airship', 17743967, 17743961, -36.458, 6.365, -77.322, 128, 315, 18, 19, 0, 360, 14, 60, 16, 224);
 INSERT INTO `transport` VALUES(9, 'Selbina-Mhaura_Boat', 17793084, 17793083, 9.294, 0.000, -69.775, 0, 485, 18, 19, 382, 480, 18, 80, 16, 220);
 INSERT INTO `transport` VALUES(10, 'Mhaura-Selbina_Boat', 17797178, 17797177, -0.516, 0.003, -8.409, 0, 493, 18, 19, 382, 480, 18, 80, 16, 221);
