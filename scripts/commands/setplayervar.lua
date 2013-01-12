2d6fc8cc85708a6b473d099acc441e56c3ef4656
linkandentity <linkandentity@gmail.com>
2013-01-12 18:36:36 +0000

    Added GM command "@setplayervar" (Not to be used lightly!)
    For dev's to fix a players broken quest/mission status.

---
+++ scripts/commands/setplayervar.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+--	[Command name]: sets a players variable
+--	[Author      ]: link
+--	
+-- DO NOT USE UNLESS YOU UNDERSTAND WHAT IT DOES
+-- Miss use can destroy a players mission, quest satus
+--
+-- example:
+--
+-- @setplayervar Link zilartstatus 1
+-- will set links zilartstatus to 1 
+-----------------------------------
+
+-----------------------------------
+-- Action
+-----------------------------------
+
+function onTrigger(player,target,variable,value)
+	pc = GetPlayerByName(target);
+	pc:setVar(variable,value);
+end;
