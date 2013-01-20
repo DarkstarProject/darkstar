abc33c269441f5a4282344ec1998319f252f774a
whasf <whasf@dspt.info>
2013-01-20 18:13:55 +0000

    SCRIPTS: QUEST - Distant Loyalties (Thank you The_Kuti)

--- scripts/zones/Bastok_Markets/npcs/Michea.lua
+++ scripts/zones/Bastok_Markets/npcs/Michea.lua
@@ -21,6 +21,7 @@ function onTrade(player,npc,trade)
 count = trade:getItemCount();
 CopperIngot = trade:hasItemQty(648,1);
 SilverIngot = trade:hasItemQty(744,1);
+MythrilIngot = trade:hasItemQty(653,1);
 
 	if ((CopperIngot == true) and count == 1) then
 		TheElvaanGoldsmith = player:getQuestStatus(BASTOK,THE_ELVAAN_GOLDSMITH);
@@ -39,8 +40,15 @@ SilverIngot = trade:hasItemQty(744,1);
 			player:completeQuest(BASTOK,FATHER_FIGURE);
 			player:addFame(BASTOK,BAS_FAME*120);
 			player:startEvent(0x00f1);
-		end
-	end
+		end;
+	elseif ((MythrilIngot == true) and count == 1) then
+		DistantLoyalties = player:getQuestStatus(SANDORIA,DISTANT_LOYALTIES);
+		DistantLoyaltiesProgress = player:getVar("DistantLoyaltiesProgress");
+		if (DistantLoyalties == 1 and DistantLoyaltiesProgress == 2)		then
+			player:tradeComplete();
+			player:startEvent(0x013d);
+		end;
+	end;
 	
 end; 
 
@@ -53,15 +61,28 @@ function onTrigger(player,npc)
 pFame = player:getFameLevel(BASTOK);
 FatherFigure = player:getQuestStatus(BASTOK,FATHER_FIGURE);
 TheElvaanGoldsmith = player:getQuestStatus(BASTOK,THE_ELVAAN_GOLDSMITH);
+DistantLoyalties = player:getQuestStatus(SANDORIA,DISTANT_LOYALTIES);
+DistantLoyaltiesProgress = player:getVar("DistantLoyaltiesProgress");
 
 	if (TheElvaanGoldsmith == 0) then
 		player:startEvent(0x00d7);
-	elseif (FatherFigure == 0 and TheElvaanGoldsmith == 2 and pFame >= 2) then
-		player:startEvent(0x00f0);
+	elseif (DistantLoyalties ~= 1) then
+			if (FatherFigure == 0 and TheElvaanGoldsmith == 2 and pFame >= 2) then
+				player:startEvent(0x00f0);
+			else
+				player:startEvent(0x007d);
+			end;
 	else
-		player:startEvent(0x007d);
-	end
-		
+		if (DistantLoyaltiesProgress == 1 and player:hasKeyItem(GOLDSMITHING_ORDER)) then
+			player:startEvent(0x013b);
+		elseif (DistantLoyaltiesProgress == 2) then
+			player:startEvent(0x013c);
+		elseif (DistantLoyaltiesProgress == 3 and player:needToZone() == true) then
+			player:startEvent(0x013d);
+		elseif (DistantLoyaltiesProgress == 3 and player:needToZone() == false) then
+			player:startEvent(0x013e);
+		end;
+	end;	
 end; 
 
 -----------------------------------
@@ -89,10 +110,16 @@ function onEventFinish(player,csid,option)
 		player:messageSpecial(GIL_OBTAINED,GIL_RATE*180);
 	elseif (csid == 0x00f1) then
 		player:messageSpecial(GIL_OBTAINED,GIL_RATE*2200);
+	elseif (csid == 0x013b) then
+		player:delKeyItem(GOLDSMITHING_ORDER);
+		player:setVar("DistantLoyaltiesProgress",2);
+	elseif (csid == 0x013d) then
+		player:setVar("DistantLoyaltiesProgress",3);
+		player:needToZone(true);
+	elseif (csid == 0x013e) then
+		player:setVar("DistantLoyaltiesProgress",4);
+		player:addKeyItem(MYTHRIL_HEARTS);
+		player:messageSpecial(KEYITEM_OBTAINED,MYTHRIL_HEARTS);
 	end
 	
 end;
-
-
-
-
--- scripts/zones/Southern_San_dOria/npcs/Femitte.lua
+++ scripts/zones/Southern_San_dOria/npcs/Femitte.lua
@@ -2,12 +2,14 @@
 --	Area: Southern San d'Oria
 --	NPC: Femitte
 --  General Info NPC
+--  Involved in Quest: Distant Loyalties
 -------------------------------------
 package.loaded["scripts/zones/Southern_San_dOria/TextIDs"] = nil;
 -----------------------------------
 
 require("scripts/globals/settings");
 require("scripts/globals/quests");
+require("scripts/globals/keyitems");
 require("scripts/zones/Southern_San_dOria/TextIDs");
 
 ----------------------------------- 
@@ -32,7 +34,20 @@ end;
 -----------------------------------
  
 function onTrigger(player,npc) 
+
+DistantLoyaltiesProgress = player:getVar("DistantLoyaltiesProgress");
+Fame = player:getFameLevel(SANDORIA);
+DistantLoyalties = player:getQuestStatus(SANDORIA,DISTANT_LOYALTIES);
+
+	if (Fame >= 4 and DistantLoyalties == 0) then
+		player:startEvent(0x0297);
+	elseif (DistantLoyalties == 1 and DistantLoyaltiesProgress == 1) then
+		player:startEvent(0x0298);
+	elseif (DistantLoyalties == 1 and DistantLoyaltiesProgress == 4 and player:hasKeyItem(MYTHRIL_HEARTS)) then
+		player:startEvent(0x0299);
+	else
 	player:startEvent(0x295);
+	end;
 end; 
 
 -----------------------------------
@@ -51,6 +66,24 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+
+	if (csid == 0x0297 and option == 0) then
+		player:addKeyItem(GOLDSMITHING_ORDER);
+		player:messageSpecial(KEYITEM_OBTAINED,GOLDSMITHING_ORDER);
+		player:addQuest(DISTANT_LOYALTIES);
+		player:getVar("DistantLoyaltiesProgress");
+		player:setVar("DistantLoyaltiesProgress",1);
+	elseif (csid == 0x0299) then
+		if (player:getFreeSlotsCount() == 0) then
+			player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,13585);
+		else
+			player:delKeyItem(MYTHRIL_HEARTS);
+			player:addItem(13585,1);
+			player:messageSpecial(ITEM_OBTAINED,13585);
+			player:setVar("DistantLoyaltiesProgress",0);
+			player:completeQuest(SANDORIA,DISTANT_LOYALTIES);
+		end;
+	end;
 end;
 
 
