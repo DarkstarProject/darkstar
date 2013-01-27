ea335739667430dcb90b562aae8b00965b5c7a43
kegsay <kegsay@gmail.com>
2013-01-27 14:43:38 +0000

    EXAMPLE: How to add specific magic to monsters. (Suzaku done as an example)
    Edit the mob_pool row to hasSpellScript=1. Open up that monster's file and write your function. 
    
    NOTE: Currently BLM/RDM/WHM will use their spells IN ADDITION to the special script. This means your script will only execute occasionally. There will eventually be another row in mob_pools which will indicate which spell types to cast (or 0 to only use the spell script) rather than it being hard-coded as it is now.

--- scripts/zones/RuAun_Gardens/mobs/Suzaku.lua
+++ scripts/zones/RuAun_Gardens/mobs/Suzaku.lua
@@ -21,3 +21,22 @@ function onMobDeath(mob, killer)
 	killer:showText(mob,SKY_GOD_OFFSET + 8);
 	GetNPCByID(17310048):hideNPC(900);
 end;
+
+-- Return the selected spell ID.
+function onMonsterMagicPrepare(mob, target)
+    -- Suzaku uses     Burn, Fire IV, Firaga III, Flare 
+    -- Let's give -ga3 a higher distribution than the others.
+    rnd = math.random();
+    
+    if (rnd < 0.5) then
+        return 176; -- firaga 3
+    elseif (rnd < 0.7) then
+         return 147; -- fire 4
+    elseif (rnd < 0.9) then
+        return 204; -- flare
+    else
+        return 147; -- fire 4
+        -- TODO: when enfeebs work: return 235; -- burn
+    end
+    
+end;
