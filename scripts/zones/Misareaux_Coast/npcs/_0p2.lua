eec1a6513006647156f2b8fcc195d1e97b88028b
lemaire_michel <lemaire_michel@rocketmail.com>
2013-01-27 03:11:43 +0000

    Fix :COP 2.5 step 1 and 2

--- scripts/zones/Misareaux_Coast/npcs/_0p2.lua
+++ scripts/zones/Misareaux_Coast/npcs/_0p2.lua
@@ -21,7 +21,16 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
+	if(player:getCurrentMission(COP) == THE_SAVAGE and player:getVar("PromathiaStatus") == 0)then
+	       player:startEvent(0x0008);
+	elseif(player:getCurrentMission(COP) == ANCIENT_VOWS and player:getVar("PromathiaStatus") == 0)then
+		player:startEvent(0x0006);
+	elseif(player:hasCompletedMission(COP,AN_ETERNAL_MELODY))then
 	player:startEvent(0x0228);
+	else
+	player:messageSpecial(DOOR_CLOSED);
+	end
+	
 	return 1;
 end;
 
@@ -41,4 +50,10 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+	if(csid == 0x0006)then
+		player:setVar("PromathiaStatus",1);
+	elseif(csid == 0x0008 and option == 1)then
+		player:setVar("PromathiaStatus",1);
+		player:setPos(729,-20,410,88,0x1D); -- Go to Riverne #B01
+	end
 end;
--- scripts/zones/Misareaux_Coast/npcs/_0p3.lua
+++ scripts/zones/Misareaux_Coast/npcs/_0p3.lua
@@ -21,24 +21,8 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	
-	local z = player:getZPos();
-	if(player:getCurrentMission(COP) == THE_SAVAGE and player:getVar("PromathiaStatus") == 0)then
-	        player:startEvent(0x0008);
-	elseif(player:getCurrentMission(COP) == ANCIENT_VOWS and player:getVar("PromathiaStatus") == 0)then
-		player:startEvent(0x0006);
-	elseif(player:hasCompletedMission(COP,AN_ETERNAL_MELODY))then
-		if(z > 330)then
-			player:startEvent(0x0229);
-		else
-			player:startEvent(0x0228);
-		end	     
-	else
-		player:messageSpecial(DOOR_CLOSED);
-	end
-	
-	return 1;
-	
+	player:startEvent(0x0229);
+	return 1;	
 end;
 
 -----------------------------------
@@ -58,10 +42,5 @@ function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 	
-	if(csid == 0x0006)then
-		player:setVar("PromathiaStatus",1);
-	elseif(csid == 0x0008 and option == 1)then
-		player:setVar("PromathiaStatus",1);
-		player:setPos(729,-20,410,88,0x1D); -- Go to Riverne #B01
-	end
+
 end;
