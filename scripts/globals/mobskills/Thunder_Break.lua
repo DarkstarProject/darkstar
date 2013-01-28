ab6fc00693a14c1d3557dce590fc637e7f7f4e1a
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-01-28 23:50:00 +0000

    fixed golem thunder_break bug

--- scripts/globals/mobskills/Thunder_Break.lua
+++ scripts/globals/mobskills/Thunder_Break.lua
@@ -18,7 +18,7 @@ function OnMobWeaponSkill(target, mob, skill)
 	local typeEffect = EFFECT_STUN;
 	if(target:hasStatusEffect(typeEffect) == false) then
 		local statmod = MOD_INT;
-		local resist = applyPlayerResistance(mob,skill,target,mob:getMod(statmod)-target:getMod(statmod),0,ELE_THUNDEr);
+		local resist = applyPlayerResistance(mob,skill,target,mob:getMod(statmod)-target:getMod(statmod),0,ELE_THUNDER);
 		if(resist > 0.2) then
 			target:addStatusEffect(typeEffect,1,0,math.random(4,8));--power=1;tic=0;duration=5;
 		end
