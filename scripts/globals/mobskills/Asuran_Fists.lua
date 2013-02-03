70e9682c924448528d0dede2370bf539bff1dada
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-02-03 15:02:48 +0000

    final tweaks to maat tp moves

--- scripts/globals/mobskills/Asuran_Fists.lua
+++ scripts/globals/mobskills/Asuran_Fists.lua
@@ -21,7 +21,7 @@ end;
 function OnMobWeaponSkill(target, mob, skill)
 	local numhits = 8;
 	local accmod = 1;
-	local dmgmod = 1.2;
+	local dmgmod = 1;
 	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,1,1);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_NONE,info.hitslanded);
 	target:delHP(dmg);
--- scripts/globals/mobskills/Dragon_Kick.lua
+++ scripts/globals/mobskills/Dragon_Kick.lua
@@ -18,7 +18,7 @@ function OnMobWeaponSkill(target, mob, skill)
 
 	local numhits = 1;
 	local accmod = 1;
-	local dmgmod = 3.2;
+	local dmgmod = 3;
 	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,1.75,2.50);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_NONE,info.hitslanded);
 	target:delHP(dmg);
