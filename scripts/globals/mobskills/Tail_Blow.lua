63e3303e09653b076689822e6b718967cf6572b6
whasf <whasf@dspt.info>
2013-02-03 01:05:56 +0000

    SCRIPTS: Fix spacing error

--- scripts/globals/mobskills/Tail_Blow.lua
+++ scripts/globals/mobskills/Tail_Blow.lua
@@ -26,7 +26,8 @@ function OnMobWeaponSkill(target, mob, skill)
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_NONE,info.hitslanded);
 
 	local typeEffect = EFFECT_STUN;
-	if(target:hasStatusEffect(typeEffect) == false and MobPhysicalHit(skill, dmg, target, info.hitslanded)) thenlocal statmod = MOD_INT;
+	if(target:hasStatusEffect(typeEffect) == false and MobPhysicalHit(skill, dmg, target, info.hitslanded)) then 
+		local statmod = MOD_INT;
 		local resist = applyPlayerResistance(mob,skill,target,mob:getMod(statmod)-target:getMod(statmod),0,ELE_EARTH);
 		if(resist > 0.2) then
 			target:addStatusEffect(typeEffect,1,0,math.random(2,6));--power=1;tic=0;duration=4;
