efbd07bed7d2460c2cfffdddc0b196323caee964
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-02-07 05:04:43 +0000

    fixed status stomp move, was broken

--- scripts/globals/mobskills/Seismostomp.lua
+++ scripts/globals/mobskills/Seismostomp.lua
@@ -18,26 +18,29 @@ function OnMobWeaponSkill(target, mob, skill)
 
     local numhits = 1;
     local accmod = 1;
-    local dmgmod = 2.5;
-
-    local shadows = info.hitslanded;
+    local dmgmod = 2.3;
 
     if(mob:isMobType(MOBTYPE_NOTORIOUS)) then
-        -- wipe shadows
-        shadows = MOBPARAM_WIPE_SHADOWS;
         dmgmod = dmgmod + math.random();
     end
 
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
 
-    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_NONE,info.hitslanded);
+    local shadows = info.hitslanded;
+
+    -- wipe shadows
+    if(mob:isMobType(MOBTYPE_NOTORIOUS)) then
+        shadows = MOBPARAM_WIPE_SHADOWS;
+    end
+
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_NONE,shadows);
 
     local typeEffect = EFFECT_STUN;
     if(target:hasStatusEffect(typeEffect) == false and MobPhysicalHit(skill, dmg, target, info.hitslanded)) then
         local statmod = MOD_INT;
         local resist = applyPlayerResistance(mob,typeEffect,target,mob:getMod(statmod)-target:getMod(statmod),0,ELE_EARTH);
         if(resist > 0.2) then
-            target:addStatusEffect(typeEffect,1,0,8*resist);--power=1;tic=0;duration=5;
+            target:addStatusEffect(typeEffect,1,0,10*resist);--power=1;tic=0;duration=5;
         end
     end
 
