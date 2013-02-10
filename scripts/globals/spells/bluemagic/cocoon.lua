ef79e7f72930032dca669629fad83d7fee9671b4
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-02-10 20:33:45 +0000

    SCRIPTS: Fixed a few bugs in Cocoon (Bug 476)

--- scripts/globals/spells/bluemagic/cocoon.lua
+++ scripts/globals/spells/bluemagic/cocoon.lua
@@ -11,22 +11,24 @@ require("scripts/globals/bluemagic");
 -----------------------------------
 
 function onSpellCast(caster, target, spell)
-	local power = target:getStat(MOD_DEF) * 0.5;
 	local defUp = target:getStatusEffect(EFFECT_DEFENSE_BOOST);
 	local defDown = target:getStatusEffect(EFFECT_DEFENSE_DOWN);
+	local power = 50; -- Percentage, not amount.
 	if (defUp ~= nil) then
-		if (defUp:getPower() <= power) then
+		if (defUp:getPower() < power) then
 			target:delStatusEffect(EFFECT_DEFENSE_BOOST);
 			target:addStatusEffect(EFFECT_DEFENSE_BOOST,power,0,90);
 		else
 			spell:setMsg(75);
 		end
-	elseif (defDown ~= nil then
+	elseif (defDown ~= nil) then
 		if (defDown:getPower() > (-1*power)) then
 			target:delStatusEffect(EFFECT_DEFENSE_DOWN);
 			target:addStatusEffect(EFFECT_DEFENSE_BOOST,power,0,90);
 		else
 			spell:setMsg(75);
 		end
+	else
+		target:addStatusEffect(EFFECT_DEFENSE_BOOST,power,0,90);
 	end
 end;
