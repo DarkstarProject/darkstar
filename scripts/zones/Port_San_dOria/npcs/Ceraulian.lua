887e949d752f73689c6c34a69ad69252f1e70da2
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-02-11 00:03:51 +0000

    SCRIPTS: Let's try this again.  Ceraulian and the qm in Temple of Ugly are now scripted for DRG AF.  Find the bugs and win a shiny whasfbux, my pretties. - Renamed crit_hit_evasion_down.lua when I noticed a WS was looking for critical_hit_evasion_down.lua.  Should impact nobody?

--- scripts/zones/Port_San_dOria/npcs/Ceraulian.lua
+++ scripts/zones/Port_San_dOria/npcs/Ceraulian.lua
@@ -2,13 +2,14 @@
 -- Area: Port San d'Oria
 -- NPC: Ceraulian
 -- Involved in Quest: The Holy Crest
--- @zone 
--- @pos 
+-- @pos 0 -8 -122 232
 -----------------------------------
 package.loaded["scripts/zones/Port_San_dOria/TextIDs"] = nil;
 -----------------------------------
 
 require("scripts/globals/quests");
+require("scripts/globals/settings");
+require("scripts/globals/keyitems");
 require("scripts/zones/Port_San_dOria/TextIDs");
 
 -----------------------------------
@@ -16,6 +17,11 @@ require("scripts/zones/Port_San_dOria/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
+	if (player:getQuestStatus(SANDORIA,CHASING_QUOTAS) == QUEST_ACCEPTED and player:getVar("ChasingQuotas_Progress") == 0 and
+		trade:getItemCount() == 1 and trade:hasItemQty(12494,1) and trade:getGil() == 0) then -- Trading gold hairpin only
+			player:tradeComplete();
+			player:startEvent(17);
+	end
 end;
 
 -----------------------------------
@@ -23,9 +29,54 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
+	local Quotas_Status = player:getQuestStatus(SANDORIA,CHASING_QUOTAS);
+	local Quotas_Progress = player:getVar("ChasingQuotas_Progress");
+	local Quotas_No = player:getVar("ChasingQuotas_No");
+	local Stalker_Status = player:getQuestStatus(SANDORUA,KNIGHT_STALKER);
+	local Stalker_Progress = player:getVar("KnightStalker_Progress");
 
 	if(player:getMainLvl() >= 30 and player:getQuestStatus(SANDORIA,THE_HOLY_CREST) == QUEST_AVAILABLE) then
 		player:startEvent(0x0018);
+
+	-- Chasing Quotas (DRG AF2)
+	elseif (Quotas_Status == QUEST_AVAILABLE and player:getMainJob() == 14 and player:getMainLvl() >= AF1_QUEST_LEVEL and Quotas_No == 0) then
+		player:startEvent(18); -- Long version of quest start
+	elseif (Quotas_No == 1) then
+		player:startEvent(14); -- Short version for those that said no.
+	elseif (Quotas_Status == QUEST_ACCEPTED and Quotas_Progress == 0) then
+		players:startEvent(13); -- Reminder to bring Gold Hairpin
+	elseif (Quotas_Progress == 1) then
+		if (tonumber(os.date("%j")) == player:getVar("ChasingQuotas_date")) then
+			player:startEvent(3); -- Fluff cutscene because you haven't waited a day
+		else
+			player:startEvent(7); -- Boss got mugged
+		end
+	elseif (Quotas_Progress == 2) then
+		player:startEvent(8); -- Go investigate
+	elseif (Quotas_Progress == 3) then
+		player:startEvent(6); -- Earring is a clue, non-required CS
+	elseif (Quotas_Progress == 4 or Quotas_Progress == 5) then
+		player:startEvent(9); -- Fluff text until Ceraulian is necessary again
+	elseif (Quotas_Progress == 6) then
+		player:startEvent(15); -- End of AF2
+
+	elseif (Quotas_Status == QUEST_COMPLETED and Stalker_Status == QUEST_AVAILABLE) then
+		player:startEvent(16); -- Fluff text until DRG AF3
+
+	-- Knight Stalker (DRG AF3)
+	elseif (Stalker_Status == QUEST_ACCEPTED and Stalker_Progress == 0) then
+		player:startEvent(19); -- Fetch the last Dragoon's helmet
+	elseif (Stalker_Progress == 1) then
+		if (player:hasKeyItem(CHALLENGE_TO_THE_ROYAL_KNIGHTS) == false) then
+			player:startEvent(23); -- Reminder to get helmet
+		else
+			player:startEvent(20); -- Response if you try to turn in the challenge to Ceraulian
+		end
+	elseif (player:getVar("KnightStalker_Option1") == 1) then
+		player:startEvent(22);
+	elseif (Stalker_Status == QUEST_COMPLETED) then
+		player:startEvent(21);
+
 	else
 		player:startEvent(0x024b);
 	end
@@ -51,6 +102,33 @@ function onEventFinish(player,csid,option)
 
 	if(csid == 0x0018) then
 		player:setVar("TheHolyCrest_Event",1);
+
+	-- Chasing Quotas (DRG AF2)
+	elseif (csid == 18) then
+		if option == 0 then
+			player:setVar("ChasingQuotas_No",1);
+		else
+			player:addQuest(SANDORIA,CHASING_QUOTAS);
+		end
+	elseif (csid == 14 and option == 1) then
+		player:setVar("ChasingQuotas_No",0);
+		player:addQuest(SANDORIA,CHASING_QUOTAS);
+	elseif (csid == 17) then
+		player:setVar("ChasingQuotas_Progress",1);
+		player:setVar("ChasingQuotas_date", os.date("%j"));
+	elseif (csid == 7) then
+		player:setVar("ChasingQuotas_Progress",2);
+		player:setVar("ChasingQuotas_date",0);
+	elseif (csid == 15) then
+		player:delKeyItem(RANCHURIOMES_LEGACY);
+		player:addItem(14227);
+		player:messageSpecial(ITEM_OBTAINED,14227); -- Drachen Brais
+		player:addFame(SANDORIA,AF2_FAME*SAN_FAME);
+
+		-- Knight Stalker (DRG AF3)
+	elseif (csid == 19) then
+		player:setVar("KnightStalker_Progress",1);
+	elseif (csid == 22) then
+		player:setVar("KnightStalker_Option1",0);
 	end
-	
 end;
--- scripts/zones/Temple_of_Uggalepih/npcs/qm15.lua
+++ scripts/zones/Temple_of_Uggalepih/npcs/qm15.lua
@@ -1,28 +1,21 @@
 -----------------------------------
 -- Area: Temple of Uggalepih
--- NPC:  ??? (Death From Above NM)
--- Involved In Mission: Death From Above
+-- NPC:  ??? (Knight Stalker Quest)
 -- @pos 58 1 -70 159
 -----------------------------------
 package.loaded["scripts/zones/Temple_of_Uggalepih/TextIDs"] = nil;
 -----------------------------------
 
 require("scripts/zones/Temple_of_Uggalepih/TextIDs");
+require("scripts/globals/keyitems");
+require("scripts/globals/quests");
+require("scripts/globals/titles");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
-
-	if(trade:hasItemQty(1267,1) and trade:getItemCount() == 1) then -- Trade Bee Larvae
-		player:tradeComplete();
-		-- Note: ??? needs to Dissapear for 15 min
-		SpawnMob(17428810,300):updateEnmity(player);
-	else
-		player:messageSpecial(NOTHING_HAPPENS);
-	end
-
 end;
 
 -----------------------------------
@@ -30,5 +23,43 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:messageSpecial(NM_OFFSET + 4);
+	local NM_Kill = player:getVar("KnightStalker_Kill");
+	if (player:getVar("KnightStalker_Progress") == 4 and player:getMainJob() == 14 and player:getPetID() == 48 and NM_Kill == 0) then
+		-- These mobs specifically will not engage unless aggro'd.
+		player:messageSpecial(SOME_SORT_OF_CEREMONY + 1); -- Your wyvern reacts violently to this spot!
+		SpawnMob(17428807,180);
+		SpawnMob(17428808,180);
+	elseif (NM_Kill == 1) then
+		player:startEvent(67);
+	else
+		player:messageSpecial(SOME_SORT_OF_CEREMONY);
+	end
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+	if(csid == 67) then
+		player:addItem(12519);
+		player:messageSpecial(ITEM_OBTAINED,12519); -- Drachen Armet
+		player:addFame(SANDORIA,AF3_FAME*SAN_FAME);
+		player:addTitle(PARAGON_OF_DRAGOON_EXCELLENCE);
+		player:delKeyItem(CHALLENGE_TO_THE_ROYAL_KNIGHTS);
+		player:setVar("KnightStalker_Option1",1); -- Optional post-quest cutscenes.
+		player:setVar("KnightStalker_Option2",1);
+		player:completeQuest(SANDORIA,KNIGHT_STALKER);
+	end
 end;
