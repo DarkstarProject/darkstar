5a6254de3f1f344383e7196f110f0d11b8f95925
whasf <whasf@dspt.info>
2013-02-13 00:10:05 +0000

    SCRIPTS: Unforgiven Quest (Thanks Xx_DeMoLiSH)

--- scripts/zones/Tavnazian_Safehold/npcs/Pradiulot.lua
+++ scripts/zones/Tavnazian_Safehold/npcs/Pradiulot.lua
@@ -1,14 +1,33 @@
 -----------------------------------
 -- Area: Tavnazian Safehold
 -- NPC: Pradiulot
--- Standard Info NPC
+-- Involved in Quest: Unforgiven
+-- @zone 26
+-- @pos -20.814 -22 8.399
 -----------------------------------
+package.loaded["scripts\zones\Tavnazian_Safehold\TextIDs"] = nil;
+-----------------------------------
+require("scripts/zones/Tavnazian_Safehold/TextIDs");
+require("scripts/globals/quests");
+-----------------------------------
+-- For those who don't know
+-- at the end of if(player:getQuestStatus(REGION,QUEST_NAME)
+-- == 0 means QUEST_AVAILABLE
+-- == 1 means QUEST_ACCEPTED
+-- == 2 means QUEST_COMPLETED 
+-- e.g. if(player:getQuestStatus(OTHER_AREAS,UNFORGIVEN) == 0 
+-- means if(player:getQuestStatus(OTHER_AREAS,UNFORGIVEN) == QUEST AVAILABLE
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
+
+if(player:getQuestStatus(OTHER_AREAS,UNFORGIVEN) == 2 and trade:getGil() == 1 == true) then
+		player:startEvent(0x00CE); -- Dialogue after completing quest (optional)
+		end
+	
 end; 
 
 -----------------------------------
@@ -16,7 +35,18 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x0173);
+
+local Unforgiven = player:getQuestStatus(OTHER_AREAS,UNFORGIVEN);
+
+if (Unforgiven == 1 and player:getVar("UnforgivenVar") == 1) then
+	player:startEvent(0x00CC); -- Dialogue for final stage of Unforgiven Quest
+
+elseif(player:getQuestStatus(OTHER_AREAS,UNFORGIVEN) == 2 and player:getVar("UnforgivenVar") == 2) then
+	player:startEvent(0x00CE); -- Dialogue after completing quest (optional)
+
+else	
+	player:startEvent(0x0173); -- Default Dialogue
+end
 end; 
 
 -----------------------------------
@@ -35,6 +65,17 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+if (csid == 0x00CC) then
+	player:setVar("UnforgivenVar",2);
+	player:addKeyItem(440)
+	player:messageSpecial(KEYITEM_OBTAINED,440); -- Map of Tavnazia
+	player:completeQuest(OTHER_AREAS,UNFORGIVEN);
+	player:addFame(OTHER_AREAS,30);
+	
+elseif (csid == 0x00CE) then
+	player:setVar("UnforgivenVar",0);
+	
+	end
 end;
 
 
--- scripts/zones/Tavnazian_Safehold/npcs/qm2.lua
+++ scripts/zones/Tavnazian_Safehold/npcs/qm2.lua
@@ -1,19 +1,15 @@
 -----------------------------------
 -- Area: Tavnazian Safehold
--- NPC: Elysia
--- Starts Quest: Unforgiven
+-- NPC: ???
+-- Involved in Quest: Unforgiven
 -- @zone 26
--- @pos -50.410 -22.204 -41.640
+-- @pos 110.714 -40.856 -53.154
 -----------------------------------
 package.loaded["scipts\zones\Tavnazian_Safehold\TextIDs"] = nil;
 require("scripts/zones/Tavnazian_Safehold/TextIDs")
 require("scripts/globals/quests");
 require("scripts/globals/keyitems");
-require("scripts/globals/settings");
-require("scripts/globals/player");
-require("scripts/globals/titles");
 -----------------------------------
-
 -- For those who don't know
 -- at the end of if(player:getQuestStatus(REGION,QUEST_NAME)
 -- == 0 means QUEST_AVAILABLE
@@ -36,23 +32,13 @@ end;
 function onTrigger(player,npc)
 
 local Unforgiven = player:getQuestStatus(OTHER_AREAS,UNFORGIVEN);
-	if (Unforgiven == 0) then
-		player:startEvent(0x00C8); -- Quest Start Dialogue
-	
-	elseif (Unforgiven == 1 and player:getVar("UnforgivenVar") == 1) then
-		player:startEvent(0x00CB); -- Dialogue if player hasn't talked to Pradiulot (2nd stage of Quest)
-	
-	elseif (Unforgiven == 1 and player:hasKeyItem(609) == false) then
-		player:startEvent(0x00C9); -- Dialogue if player doesn't have keyitem
-	
-	elseif (Unforgiven == 1 and player:hasKeyItem(609) == true) then
-		player:startEvent(0x00CA); -- Dialogue if player has keyitem (1st stage of Quest)
-	
-	else
-		player:startEvent(0x00BE);
-		
-		end
-end;
+
+	if (Unforgiven == 1 and player:hasKeyItem(609) == false) then
+		player:addKeyItem(609);
+		player:messageSpecial(KEYITEM_OBTAINED,609) -- ALABASTER HAIRPIN for Unforgiven Quest
+
+	end
+end
 -----------------------------------
 -- onEventUpdate
 -----------------------------------
@@ -69,11 +55,5 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-if (csid == 0x00C8) then
-	player:addQuest(OTHER_AREAS,UNFORGIVEN);
 
-elseif (csid == 0x00CA) then
-	player:setVar("UnforgivenVar",1); -- SET THE VAR
 end
-
-end;
