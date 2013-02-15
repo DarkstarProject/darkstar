639cfe14ff60e58654955644725adfdc5ec9a703
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-02-15 06:45:43 +0000

    SCRIPTS: A few more files I missed... (DRG AF)

--- scripts/zones/Temple_of_Uggalepih/mobs/Cleuvarion_M_Resoaix.lua
+++ scripts/zones/Temple_of_Uggalepih/mobs/Cleuvarion_M_Resoaix.lua
@@ -18,7 +18,7 @@ end;
 function onMobDeath(mob, killer)
 	local Rompaulion = GetMobAction(17428808);
 	-- Get credit if other NM is dead/despawned or in the process of dieing/fading out
-	if (player:getVar("KnightStalker_Progress") == 4 and (Rompaulion == 0 or (Rompaulion >= 20 and Rompaulion <= 23))) then
+	if (killer:getVar("KnightStalker_Progress") == 4 and (Rompaulion == 0 or (Rompaulion >= 20 and Rompaulion <= 23))) then
 		killer:setVar("KnightStalker_Kill",1);
 	end
 end;
--- scripts/zones/Temple_of_Uggalepih/mobs/Rompaulion_S_Citalle.lua
+++ scripts/zones/Temple_of_Uggalepih/mobs/Rompaulion_S_Citalle.lua
@@ -18,7 +18,7 @@ end;
 function onMobDeath(mob, killer)
 	local Cleuvarion = GetMobAction(17428807);
 	-- Get credit if other NM is dead/despawned or in the process of dieing/fading out
-	if (player:getVar("KnightStalker_Progress") == 4 and (Cleuvarion == 0 or (Cleuvarion >= 20 and Cleuvarion <= 23))) then
+	if (killer:getVar("KnightStalker_Progress") == 4 and (Cleuvarion == 0 or (Cleuvarion >= 20 and Cleuvarion <= 23))) then
 		killer:setVar("KnightStalker_Kill",1);
 	end
 end;
