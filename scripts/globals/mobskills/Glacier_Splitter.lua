f694e9a7b8a1496fe03a4d8ba1a7f32788cd9bb7
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-02-16 01:44:21 +0000

    SCRIPTS: Fixes Glacier Splitter and Disseverment not displaying damage, and not inflicting their respective status effects.

--- scripts/globals/mobskills/Glacier_Splitter.lua
+++ scripts/globals/mobskills/Glacier_Splitter.lua
@@ -27,7 +27,7 @@ function OnMobWeaponSkill(target, mob, skill)
 	local typeEffect = EFFECT_PARALYSIS;
 	if(target:hasStatusEffect(typeEffect) == false and MobPhysicalHit(skill, dmg, target, info.hitslanded)) then
 		local statmod = MOD_INT;
-		local resist = applyPlayerResistance(mob,typeEffect,target,mob:getStat(statmod)-target:getStat(statmod),0,ELE_IE);
+		local resist = applyPlayerResistance(mob,typeEffect,target,mob:getStat(statmod)-target:getStat(statmod),0,ELE_ICE);
 		if(resist > 0.2) then
 			target:addStatusEffect(typeEffect,15,3,60*resist);
 		end
