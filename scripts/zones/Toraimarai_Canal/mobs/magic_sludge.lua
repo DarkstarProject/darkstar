333bf2c1d78cdbe023fe046d0f7a84515daedf7c
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-02-26 05:03:00 +0000

    commiting this first

---
+++ scripts/zones/Toraimarai_Canal/mobs/magic_sludge.lua
@@ -0,0 +1,29 @@
+-----------------------------------
+-- Area: Toraimarai Canal
+--  NM: Magic Sludge
+-----------------------------------
+
+require("scripts/globals/titles");
+require("scripts/globals/status");
+
+-----------------------------------
+-- OnMobInitialise Action
+-----------------------------------
+
+function onMobInitialize(mob)
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob,target)
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+	killer:setVar("rootProblem",3);
+end;
