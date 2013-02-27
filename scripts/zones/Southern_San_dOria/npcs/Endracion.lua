d5d31f40c5d80786ced520a294f6f226e3e7697d
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-02-27 07:52:37 +0000

    SCRIPTS: Fixes the Rank 4 -> Rank 7 in San d'Oria bug.
    For admins:
    You can just replace the LUA without restarting, if necessary.  This seems to be spreading surprisingly fast, so you may want to.
    
    Fixing those affected:
    1. Have them log out if they're logged in, as the fix to rank won't stick otherwise.
    2. Change their rank in char_profile from 7 to 5 in San d'Oria
    3. Have them log in again
    4. @delmission 0 17 Playername
    5. @setplayervar Playername MissionStatus 9
    
    Capitalization matters.  When I fixed my test character, no other steps were necessary to get back on track, but future progression may have unforeseen snags.
    
    They can either talk to Endracion or Halver at that point, and they'll advance as intended.  Sorry for so much trouble today.

--- scripts/zones/Southern_San_dOria/npcs/Endracion.lua
+++ scripts/zones/Southern_San_dOria/npcs/Endracion.lua
@@ -70,11 +70,11 @@ function onTrigger(player,npc)
 			player:startEvent(0x03e8); -- Start First Mission "Smash the Orcish scouts"
 		elseif(player:hasKeyItem(ANCIENT_SANDORIAN_BOOK)) then
 	        player:startEvent(0x040b);
-	    elseif(player:getCurrentMission(0,17) and player:getVar("MissionStatus",4) and tonumber(os.date("%j")) == player:getVar("Wait1DayForRanperre_date")) then
+	    elseif(player:getCurrentMission(SANDORIA) == RANPERRE_S_FINAL_REST and player:getVar("MissionStatus",4) and tonumber(os.date("%j")) == player:getVar("Wait1DayForRanperre_date")) then
 	        player:startEvent(0x040d);
-		elseif(player:getCurrentMission(0,17) and player:getVar("MissionStatus") == 6) then
+		elseif(player:getCurrentMission(SANDORIA) == RANPERRE_S_FINAL_REST and player:getVar("MissionStatus") == 6) then
 		    player:startEvent(0x040f);
-		elseif(player:getCurrentMission(0,17) and player:getVar("MissionStatus") == 9) then
+		elseif(player:getCurrentMission(SANDORIA) == RANPERRE_S_FINAL_REST and player:getVar("MissionStatus") == 9) then
 		    player:startEvent(0x0409);
 		elseif((CurrentMission ~= 255) and not (player:getVar("MissionStatus") == 8)) then
 			player:startEvent(0x03e9); -- Have mission already activated
