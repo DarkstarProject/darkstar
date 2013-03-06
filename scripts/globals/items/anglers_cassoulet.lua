1273c9f6f2a47ed636d7566cdbfd526f04322648
metalfiiish <metalfiiish@gmail.com>
2013-03-06 12:32:23 +0000

    SCRIPTS: Quest added Beans Ahoy. Added item anglers cassoulet.

---
+++ scripts/globals/items/anglers_cassoulet.lua
@@ -0,0 +1,46 @@
+-----------------------------------------
+-- ID: 5704
+-- Item: Anglers Cassoulet
+-- Item Effect: Restores 60 HP over 1 hour,  / 3600 seconds.
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+    return 0;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	if(target:hasStatusEffect(EFFECT_FOOD) == false and target:hasStatusEffect(EFFECT_REGEN) == false) then
+		target:addStatusEffect(EFFECT_FOOD,0,0,3600,5704);
+		
+	else
+		target:messageBasic(423);
+	end
+end;
+----------------------------------------
+-- onEffectGain
+----------------------------------------
+function onEffectGain(target,effect)
+	target:addStatusEffect(EFFECT_REGEN,1,3,3600);
+	target:addMod(MOD_VIT, -1)
+	target:addMod(MOD_AGI, 5)
+	
+end
+-----------------------------------------
+-- onEffectLose Action
+-----------------------------------------
+
+function onEffectLose(target,effect)
+	target:delMod(MOD_VIT, -1);
+	target:delMod(MOD_AGI, 5);
+
+end;
--- scripts/zones/Southern_San_dOria_[S]/npcs/Thierride.lua
+++ scripts/zones/Southern_San_dOria_[S]/npcs/Thierride.lua
@@ -2,17 +2,41 @@
 -- Area: Southern SandOria [S]
 -- NPC: Thierride
 -- @zone 80
--- @pos -124 2 14
+-- @pos -124 -2 14
 -----------------------------------
-package.loaded["scripts/zones/Southern_San_dOria_[S]/TextIDs"] = nil;
-require("scripts/zones/Southern_San_dOria_[S]/TextIDs");
 
+require("scripts/zones/Southern_San_dOria_[S]/TextIDs");
+require("scripts/globals/quests");
+
+-----------------------------------
+
+-- Item 1019 = Lufet Salt
+-- Had to use setVar because you have to trade Salts one at a time according to the wiki.
+-- Lufet Salt can be obtained by killing Crabs in normal West Ronfaure.
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
+	local lufetSalt = trade:hasItemQty(1019,1);
+	local cnt = trade:getItemCount();
+	local beansAhoy = player:getQuestStatus(CRYSTAL_WAR,BEANS_AHOY);
+	if(lufetSalt and cnt == 1 and beansAhoy == QUEST_ACCEPTED) then
+		if (player:getVar("BeansAhoy") == 0 == true) then
+			
+			player:startEvent(0x0151); -- Traded the Correct Item Dialogue (NOTE: You have to trade the Salts one at according to wiki)
+	
+		elseif(player:needsToZone() == false) then
+			player:startEvent(0x0154); -- Quest Complete Dialogue
+			
+		end
+	
+	else
+		player:startEvent(0x0153); -- Wrong Item Traded
+	
+	end
+	
 end;
 
 -----------------------------------
@@ -20,7 +44,21 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x014D);
+	local beansAhoy = player:getQuestStatus(CRYSTAL_WAR,BEANS_AHOY);
+	if(beansAhoy == QUEST_AVAILABLE) then
+		player:startEvent(0x014E); -- Quest Start
+	
+	elseif(beansAhoy == QUEST_ACCEPTED) then
+		player:startEvent(0x014F); -- Quest Active, NPC Repeats what he says but as normal 'text' instead of cutscene.
+	
+	elseif(beansAhoy == QUEST_COMPLETED and getConquestTally() ~= player:getVar("BeansAhoy_ConquestWeek")) then
+		player:startEvent(0x0156);
+	elseif(beansAhoy == QUEST_COMPLETED) then
+		player:startEvent(0x0155);
+	else
+		player:startEvent(0x014D); -- Default Dialogue
+		
+	end
 end;
 
 -----------------------------------
@@ -39,4 +77,31 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
+
+	if (csid == 0x014E) then
+		player:addQuest(CRYSTAL_WAR,BEANS_AHOY);
+	
+	elseif (csid == 0x0151) then
+		player:tradeComplete();
+		player:setVar("BeansAhoy",1);
+		player:needsToZone(true);
+	
+	elseif (csid == 0x0154 or csid == 0x0156) then
+		if(player:hasItem(5704,1) or player:getFreeSlotsCount() < 1) then
+			player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,5704)
+		
+		else
+			player:addItem(5704,1);
+			player:messageSpecial(ITEM_OBTAINED,5704);
+			player:setVar("BeansAhoy_ConquestWeek",getConquestTally());
+			if(csid == 0x0154) then
+				player:completeQuest(CRYSTAL_WAR,BEANS_AHOY);
+				player:setVar("BeansAhoy",0);
+				player:tradeComplete();
+			end
+			
+		end
+	end
+	
+
 end;
