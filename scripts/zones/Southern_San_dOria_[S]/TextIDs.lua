b3bdf57d8afba94b86b43edc74d66b57bb816908
diatanato <diatanato@gmail.com>
2013-03-06 07:21:24 +0000

    SCRIPTS: fix quest Seeing Spot r2888

--- scripts/zones/Southern_San_dOria_[S]/TextIDs.lua
+++ scripts/zones/Southern_San_dOria_[S]/TextIDs.lua
@@ -5,3 +5,6 @@ ITEM_CANNOT_BE_OBTAINED = 12639; -- You cannot obtain the item <item> come back 
           ITEM_OBTAINED = 6378; -- Obtained: <item>
            GIL_OBTAINED = 6379; -- Obtained <number> gil
        KEYITEM_OBTAINED = 6381; -- Obtained key item: <keyitem>
+       
+-- NPC Dialogs
+WYATT_DIALOG = 10993; -- Ahhh, sorry, sorry. The name's Wyatt, an' I be an armor merchant from Jeuno. Ended up 'ere in San d'Oria some way or another, though.
--- scripts/zones/Southern_San_dOria_[S]/npcs/Wyatt.lua
+++ scripts/zones/Southern_San_dOria_[S]/npcs/Wyatt.lua
@@ -5,6 +5,8 @@
 -- @pos 124 0 84
 -----------------------------------
 package.loaded["scripts/zones/Southern_San_dOria_[S]/TextIDs"] = nil;
+-----------------------------------
+
 require("scripts/zones/Southern_San_dOria_[S]/TextIDs");
 require("scripts/globals/titles");
 require("scripts/globals/quests");
@@ -14,10 +16,8 @@ require("scripts/globals/quests");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-
-
-	if (trade:hasItemQty(2506,4) and trade:getItemCount() == 4 and trade:getGil() == 0) then
-		player:startEvent(0x004);
+	if (trade:getItemCount() == 4 and trade:hasItemQty(2506,4)) then
+		player:startEvent(0x0004);
 	end
 end;
 
@@ -28,16 +28,12 @@ end;
 function onTrigger(player,npc)
 	local seeingSpots = player:getQuestStatus(CRYSTAL_WAR,SEEING_SPOTS);
 	if (seeingSpots == QUEST_AVAILABLE) then
-		player:startEvent(0x002);
-			
+		player:startEvent(0x0002);
 	elseif (seeingSpots == QUEST_ACCEPTED) then
-		player:startEvent(0x003);
-	
+		player:startEvent(0x0003);
 	else
-		player:showText(npc, 10993); --Ahhh, sorry, sorry. The name's Wyatt, an' I be an armor merchant from Jeuno. Ended up 'ere in San d'Oria some way or another, though.
+		player:showText(npc, WYATT_DIALOG);
 	end
-
-	
 end;
 
 -----------------------------------
@@ -56,23 +52,19 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-	if (csid == 0x002) then
+	if (csid == 0x0002) then
 		player:addQuest(CRYSTAL_WAR,SEEING_SPOTS);
-
-	elseif (csid == 0x004) then
+	elseif (csid == 0x0004) then
 		player:tradeComplete();
 		if(player:getQuestStatus(CRYSTAL_WAR,SEEING_SPOTS) == QUEST_ACCEPTED) then
 			player:addTitle(LADY_KILLER);
 			player:addGil(GIL_RATE*3000);
-			player:messageSpecial(GIL_OBTAINED,3000);
+			player:messageSpecial(GIL_OBTAINED,GIL_RATE*3000);
 			player:completeQuest(CRYSTAL_WAR,SEEING_SPOTS);
-			
 		else
 			player:addTitle(LADY_KILLER);
 			player:addGil(GIL_RATE*1500);
-			player:messageSpecial(GIL_OBTAINED,1500);
+			player:messageSpecial(GIL_OBTAINED,GIL_RATE*1500);
 		end
-
 	end
-
 end;
