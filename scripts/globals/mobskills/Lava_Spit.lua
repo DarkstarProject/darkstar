bc21121d0398732e51a8f9999e8b63895a7d1282
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-03-08 03:43:11 +0000

    MOBS: lavaspit will no longer wipe a whole alliance

--- scripts/globals/mobskills/Lava_Spit.lua
+++ scripts/globals/mobskills/Lava_Spit.lua
@@ -15,8 +15,8 @@ function OnMobSkillCheck(target,mob,skill)
 end;
 
 function OnMobWeaponSkill(target, mob, skill)
-	local dmgmod = 1.5;
-	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg() * 10,ELE_FIRE,dmgmod,TP_MAB_BONUS,1);
+	local dmgmod = 1;
+	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg() * 5,ELE_FIRE,dmgmod,TP_MAB_BONUS,1);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_FIRE,MOBPARAM_IGNORE_SHADOWS);
 	target:delHP(dmg);
 	return dmg;
