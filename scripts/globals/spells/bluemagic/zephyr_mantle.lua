8043708fac2ecc6306858dca1b9d03680e5b9c38
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-03-23 01:13:04 +0000

    SCRIPTS: "...wait, Zephyr Mantle is just Blink with 4 shadows."  And that's why Zephyr Mantle does something now.

---
+++ scripts/globals/spells/bluemagic/zephyr_mantle.lua
@@ -0,0 +1,25 @@
+-----------------------------------------
+-- Spell: Zephyr Mantle
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function OnMagicCastingCheck(caster,target,spell)
+	return 0;
+end;
+
+function onSpellCast(caster,target,spell)
+	duration = 300; -- 5 Minutes
+
+	if(target:addStatusEffect(EFFECT_BLINK, 4, 0, duration)) then
+		spell:setMsg(230);
+	else
+		spell:setMsg(75);
+	end
+
+	return EFFECT_BLINK;
+end;
