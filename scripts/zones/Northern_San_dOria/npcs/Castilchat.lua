7e919c4053d5b455ccc603ba884d893c9c8576b4
metalfiiish <metalfiiish@gmail.com>
2013-03-25 01:40:49 +0000

    SCRIPTS: Fix for bug #247. Improper text for Cutscene in Trial Size by Ice. Also did some clean up.

--- scripts/zones/Northern_San_dOria/npcs/Castilchat.lua
+++ scripts/zones/Northern_San_dOria/npcs/Castilchat.lua
@@ -7,6 +7,7 @@
 package.loaded["scripts/zones/Northern_San_dOria/TextIDs"] = nil;
 -----------------------------------
 
+require("scripts/globals/status");
 require("scripts/globals/quests");
 require("scripts/zones/Northern_San_dOria/TextIDs");
 
@@ -17,13 +18,11 @@ require("scripts/zones/Northern_San_dOria/TextIDs");
 function onTrade(player,npc,trade)
 	
 	-- "Flyers for Regine" conditional script
-	FlyerForRegine = player:getQuestStatus(SANDORIA,FLYERS_FOR_REGINE);
-	TrialSizeByIce = player:getQuestStatus(SANDORIA,TRIAL_SIZE_TRIAL_BY_ICE);
-
-	if(FlyerForRegine == QUEST_ACCEPTED and trade:hasItemQty(532,1) and trade:getItemCount() == 1) then
+	local count = trade:getItemCount();
+	if(player:getQuestStatus(SANDORIA,FLYERS_FOR_REGINE) == QUEST_ACCEPTED and trade:hasItemQty(532,1) and count == 1) then
 		player:messageSpecial(FLYER_REFUSED);
-	elseif(trade:hasItemQty(1545,1) and TrialSizeByIce == QUEST_ACCEPTED and player:getMainJob() == 15) then -- Trade mini fork of ice
-		player:startEvent(0x02de,0,1545,4,player:getMainLvl());
+	elseif(trade:hasItemQty(1545,1) and player:getQuestStatus(SANDORIA,TRIAL_SIZE_TRIAL_BY_ICE) == QUEST_ACCEPTED and player:getMainJob() == JOB_SMN and count == 1) then -- Trade mini fork of ice
+		player:startEvent(0x02de,0,1545,4,20);
 	end
 	
 end;
@@ -31,22 +30,18 @@ end;
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
-
 function onTrigger(player,npc)
-	
-	TrialSizeByIce = player:getQuestStatus(SANDORIA,TRIAL_SIZE_TRIAL_BY_ICE);
-	mLvl = player:getMainLvl();
-	mJob = player:getMainJob();
-	realday = tonumber(os.date("%j"));
 
-	if(mLvl >= 20 and mJob == 15 and TrialSizeByIce == QUEST_AVAILABLE and player:getFameLevel(SANDORIA) >= 2) then -- Requires player to be Summoner at least lvl 20
+	local TrialSizeByIce = player:getQuestStatus(SANDORIA,TRIAL_SIZE_TRIAL_BY_ICE);
+
+	if(player:getMainLvl() >= 20 and player:getMainJob() == JOB_SMN and TrialSizeByIce == QUEST_AVAILABLE and player:getFameLevel(SANDORIA) >= 2) then -- Requires player to be Summoner at least lvl 20
 		player:startEvent(0x02dd,0,1545,4,20); 	--mini tuning fork of ice, zone, level
 	elseif(TrialSizeByIce == QUEST_ACCEPTED) then
 		IceFork = player:hasItem(1545);
 		
 		if(IceFork) then 
 			player:startEvent(0x02c4); --Dialogue given to remind player to be prepared
-		elseif(IceFork == false and realday ~= player:getVar("TrialSizeIce_date")) then
+		elseif(IceFork == false and tonumber(os.date("%j")) ~= player:getVar("TrialSizeIce_date")) then
 			player:startEvent(0x02e1,0,1545,4,20); -- Need another mini tuning fork
 		else
 			player:startEvent(0x02f6); -- Standard dialog when you loose, and you don't wait 1 real day
