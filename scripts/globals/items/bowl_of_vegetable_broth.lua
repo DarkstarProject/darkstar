62d84c86f4f7627c3e6be0035230393424922416
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-03-25 19:07:06 +0000

    SCRIPTS: Missing comma in Vegetable Broth crashes server (Bug #673)

--- scripts/globals/items/bowl_of_vegetable_broth.lua
+++ scripts/globals/items/bowl_of_vegetable_broth.lua
@@ -36,7 +36,7 @@ end;
 -----------------------------------------
 
 function onEffectGain(target,effect)
-	target:addMod(MOD_VIT -1);
+	target:addMod(MOD_VIT, -1);
 	target:addMod(MOD_AGI, 5);
 	target:addMod(MOD_RACC, 6);
 	target:addMod(MOD_HPHEAL, 3);
@@ -47,7 +47,7 @@ end;
 -----------------------------------------
 
 function onEffectLose(target,effect)
-	target:delMod(MOD_VIT -1);
+	target:delMod(MOD_VIT, -1);
 	target:delMod(MOD_AGI, 5);
 	target:delMod(MOD_RACC, 6);
 	target:delMod(MOD_HPHEAL, 3);
