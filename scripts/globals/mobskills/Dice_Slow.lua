6ec20c3c83b6a0cd363c3afdd2ccf8f83f80a350
metalfiiish <metalfiiish@gmail.com>
2013-03-26 15:07:27 +0000

    SCRIPTS: Fix a typo in Dyanmis - Goblin's TP move "Dice Slow".

--- scripts/globals/mobskills/Dice_Slow.lua
+++ scripts/globals/mobskills/Dice_Slow.lua
@@ -18,8 +18,8 @@ function OnMobWeaponSkill(target, mob, skill)
     local slowed = false;
     local sleeped = false;
 
-    slowed = MobStatusEffectMove(mob, target, EFFECT_SLOW, 128, 0, 120));
-    sleeped = MobStatusEffectMove(mob, target, EFFECT_SLEEP_I, 1, 0, 30));
+    slowed = MobStatusEffectMove(mob, target, EFFECT_SLOW, 128, 0, 120);
+    sleeped = MobStatusEffectMove(mob, target, EFFECT_SLEEP_I, 1, 0, 30);
 
     skill:setMsg(MSG_ENFEEB_IS);
     if(sleeped) then
