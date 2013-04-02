e56422e05a42bb2b09d6e94848035a8879d5b348
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-04-02 03:36:45 +0000

    SCRIPTS: Abihaal in Nashmau will now allow players to enter the dock in Nashmau, if they have the gil. (Thanks Ryekel!)

---
+++ scripts/zones/Nashmau/npcs/Abihaal.lua
@@ -0,0 +1,43 @@
+-----------------------------------
+-- Area: Nashmau
+-- NPC: Abihaal
+-- Standard Info NPC
+-----------------------------------
+
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+   player:startEvent(0x0DD,player:getGil(),100);
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+
+   if(csid == 0x0DD and option == 333) then
+      player:delGil(100);
+   end
+end;
