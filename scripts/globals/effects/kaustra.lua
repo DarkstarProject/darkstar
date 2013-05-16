a695eb8cda5e01b0bd6ae3ea1579c855bf0a4a8b
teschnei <teschnei@gmail.com>
2013-05-16 22:21:28 +0000

    SCRIPTS: fixed some errors in embrava I forgot to delete after copy/pasting added kaustra

---
+++ scripts/globals/effects/kaustra.lua
@@ -0,0 +1,45 @@
+-----------------------------------
+--
+-- 	EFFECT_KAUSTRA
+-- 	
+-----------------------------------
+
+require("scripts/globals/status");
+require("scripts/globals/magic");
+
+-----------------------------------
+-- onEffectGain Action
+-----------------------------------
+
+function onEffectGain(target,effect)
+
+end;
+
+-----------------------------------
+-- onEffectTick Action
+-----------------------------------
+
+function onEffectTick(target,effect)
+	if(target:hasStatusEffect(EFFECT_STONESKIN)) then
+		local skin = target:getMod(MOD_STONESKIN);
+		local dmg = effect:getPower();
+		if(skin >= dmg) then --absorb all damage
+			target:delMod(MOD_STONESKIN,effect:getPower());
+		else
+			target:delStatusEffect(EFFECT_STONESKIN);
+			target:delHP(dmg - skin);
+			target:wakeUp();
+		end
+	else
+		target:delHP(effect:getPower());
+		target:wakeUp();
+	end
+end;
+
+-----------------------------------
+-- onEffectLose Action
+-----------------------------------
+
+function onEffectLose(target,effect)
+
+end;
--- scripts/globals/spells/embrava.lua
+++ scripts/globals/spells/embrava.lua
@@ -17,19 +17,16 @@ function OnMagicCastingCheck(caster,target,spell)
 end;
 
 function onSpellCast(caster,target,spell)
-	
-	if(target:hasStatusEffect(EFFECT_VIT_DOWN) or caster:hasStatusEffect(EFFECT_VIT_BOOST)) then
-		spell:setMsg(75); -- no effect
-	else
-		local skill = caster:getSkillLevel(ENHANCING_MAGIC_SKILL);
-		if (skill > 500) then
-			skill = 500;
-		end
-		local regen = math.floor(skill / 7) + 1;
-		local refresh = math.floor(skill / 100) + 1;
-		local haste = math.floor(skill/(500/256));
-		
-		target:addStatusEffect(EFFECT_EMBRAVA,regen,0,180,0,refresh,haste);
+
+	local skill = caster:getSkillLevel(ENHANCING_MAGIC_SKILL);
+	if (skill > 500) then
+		skill = 500;
 	end
+	local regen = math.floor(skill / 7) + 1;
+	local refresh = math.floor(skill / 100) + 1;
+	local haste = math.floor(skill/(500/256));
+	
+	target:addStatusEffect(EFFECT_EMBRAVA,regen,0,180,0,refresh,haste);
+		
 	return EFFECT_EMBRAVA;
 end;
