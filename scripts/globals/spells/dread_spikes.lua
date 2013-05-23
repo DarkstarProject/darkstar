3f6047dc792cba0edc7e609fd436562df39d2b13
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-05-23 00:44:55 +0000

    SCRIPTS: DSP_DEBUG_BREAK_IF(somebody didn't define their variable before using it)

--- scripts/globals/spells/dread_spikes.lua
+++ scripts/globals/spells/dread_spikes.lua
@@ -13,15 +13,17 @@ function OnMagicCastingCheck(caster,target,spell)
 end;
 
 function onSpellCast(caster,target,spell)
-    duration = 60;
-  local typeEffect = EFFECT_DREAD_SPIKES;
-    if (caster:hasStatusEffect(EFFECT_COMPOSURE) == true and caster:getID() == target:getID()) then
-        duration = duration * 3;
-    end
+   local duration = 60;
+   local power = 0;
+   local typeEffect = EFFECT_DREAD_SPIKES;
+
+   if (caster:hasStatusEffect(EFFECT_COMPOSURE) == true and caster:getID() == target:getID()) then
+      duration = duration * 3;
+   end
    if(target:addStatusEffect(typeEffect,power,0,duration)) then
-     spell:setMsg(230);
+      spell:setMsg(230);
    else
-     spell:setMsg(75);
+      spell:setMsg(75);
    end
 
    return typeEffect;
