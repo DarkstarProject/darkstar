7abef3d906f4fb1fd25b0b1a54132e0a1a1ba4e8
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-05-24 04:06:54 +0000

    Ferry Upgrade Part 1.5 SCRIPTS: Nashmau NPC gives time estimates - Riding the ferry from Nashmau will no longer crash the client.

--- scripts/zones/Nashmau/Zone.lua
+++ scripts/zones/Nashmau/Zone.lua
@@ -16,12 +16,12 @@ function onInitialize(zone)
 zone:registerRegion(1,10.816,-20,-105,13.221,20,-103.1);
 end;
 
------------------------------------		
--- onZoneIn		
------------------------------------		
+-----------------------------------
+-- onZoneIn
+-----------------------------------
 
-function onZoneIn(player,prevZone)		
-	cs = -1;	
+function onZoneIn(player,prevZone)
+	local cs = -1;
   if ((player:getXPos() == 0) and (player:getYPos() == 0) and (player:getZPos() == 0))then 
    if(prevZone == 58) then 
       cs = 0x00C9;
@@ -31,11 +31,11 @@ function onZoneIn(player,prevZone)
    end
   end
  return cs;
-end;		
+end;
 
------------------------------------		
--- onRegionEnter		
------------------------------------		
+-----------------------------------
+-- onRegionEnter
+-----------------------------------
 function onRegionEnter(player,region)
   if(GetNPCByID(16994321):getAnimation() == 8) then
     player:setPos(11.384,1.289,-108.847,63);
@@ -43,38 +43,38 @@ function onRegionEnter(player,region)
 end;
 
 
------------------------------------	
--- onRegionLeave	
------------------------------------	
+-----------------------------------
+-- onRegionLeave
+-----------------------------------
 
 function onRegionLeave(player,region)
 end;
 
 
------------------------------------		
--- onTransportEvent		
------------------------------------		
+-----------------------------------
+-- onTransportEvent
+-----------------------------------
 function onTransportEvent(player,transport)
    if(transport == 59)then
-     player:startEvent(0x00c8,0,0,0,0,0,0,0,0,59);
+     player:startEvent(0x00c8);
    end
 end;
 
------------------------------------	
--- onEventUpdate	
------------------------------------	
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
 
-function onEventUpdate(player,csid,option)	
+function onEventUpdate(player,csid,option)
 	--printf("CSID: %u",csid);
 	--printf("RESULT: %u",option);
-end;	
+end;
 
------------------------------------	
--- onEventFinish	
------------------------------------	
+-----------------------------------
+-- onEventFinish
+-----------------------------------
 
-function onEventFinish(player,csid,option)	
-   if(csid == 0x00c8) then	
+function onEventFinish(player,csid,option)
+   if(csid == 0x00c8) then
      player:setPos(0,-2,0,0,59);
    end
-end;	
+end;
--- scripts/zones/Nashmau/npcs/Yohj_Dukonlhy.lua
+++ scripts/zones/Nashmau/npcs/Yohj_Dukonlhy.lua
@@ -17,7 +17,18 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x00E7);
+   -- Based on /scripts/zones/Mhaura/Dieh_Yamilsiah.lua
+   local timer = 1152 - ((os.time() - 1009810800)%1152);
+   local direction = 0; -- Arrive, 1 for depart
+   local waiting = 431; -- Offset for Nashmau
+
+   if (timer <= waiting) then
+      direction = 1; -- Ship arrived, switch dialog from "arrive" to "depart"
+   else
+      timer = timer - waiting; -- Ship hasn't arrived, subtract waiting time to get time to arrival
+   end
+
+   player:startEvent(231,timer,direction);
 end; 
 
 -----------------------------------
@@ -37,6 +48,3 @@ function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 end;
-
-
-
