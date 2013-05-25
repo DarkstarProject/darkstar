fedecfc8bad0bb3939e597a9bbbf8371648fe763
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-05-25 01:14:28 +0000

    SCRIPTS: Fixed some nulls in Qu'Hau Spring, for your server stability needs. - Qu'Hau Spring now checks the time and weather when you trade. - Qu'Hau Spring now uses a cutscene for a successful trade. - Some other stuff having to do with the spring that'll hopefully be relevant in a day or two :)

--- scripts/zones/RoMaeve/npcs/Qu_Hau_Spring.lua
+++ scripts/zones/RoMaeve/npcs/Qu_Hau_Spring.lua
@@ -1,55 +1,62 @@
------------------------------------	
+-----------------------------------
 -- Qu_Hau_Spring
 -- Area: Ro'Maeve
------------------------------------	
-	
-require("/scripts/globals/settings");	
+-----------------------------------
+package.loaded["scripts/zones/RoMaeve/TextIDs"] == nil;
+require["scripts/zones/RoMaeve/TextIDs"];
+require("scripts/globals/quests");
+require("scripts/globals/keyitems");
 
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
 
+function onTrigger(player,npc)
+end;
 
--- TODO:
--- Can only get Ark Pentasphere if
--- 1. The moon is full (Done)
--- 2. game time must be between 0:00 - 3:00
--- 3. weather must be clear
-	
-	
------------------------------------	
--- onTrigger Action	
------------------------------------	
-	
-function onTrigger(player,npc)	
-end;	
-	
------------------------------------	
--- onTrade Action	
------------------------------------	
-	
-function onTrade(player,npc,trade)	
+-----------------------------------
+-- onTrade Action
+-----------------------------------
 
-	if(trade:hasItemQty(1408,1) and trade:hasItemQty(917,1) and trade:getItemCount() == 2 and IsMoonFull() == true) then
-		
-		if (player:getFreeSlotsCount() == 0) then 
-			player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,1550);
-		else 
-			player:addItem(1550);
-			player:messageSpecial(ITEM_OBTAINED,1550);
-			player:tradeComplete();
-		end
-	end
+function onTrade(player,npc,trade)
+   local DMRepeat = player:getQuestStatus(OUTLANDS,DIVINE_MIGHT_REPEAT);
+   local Hour = VanadielHour();
 
-end;	
-	
------------------------------------	
--- onEventSelection	
------------------------------------	
-	
-function onEventUpdate(player,csid,menuchoice)	
-end;	
-	
------------------------------------	
--- onEventFinish Action	
------------------------------------	
-	
-function onEventFinish(player,csid,option)	
-end;	
+   if (player:getWeather() == 0 and IsMoonFull() == true and Hour >= 0 and Hour <= 2) then
+      -- Quests not yet implemented.
+      -- if ((player:getQuestStatus(OUTLANDS,DIVINE_MIGHT) == QUEST_ACCEPTED or DMRepeat == QUEST_ACCEPTED) and
+         if (trade:hasItemQty(1408,1) and trade:hasItemQty(917,1) and trade:getItemCount() == 2) then
+            player:startEvent(7,917,1408); -- Ark Pentasphere Trade
+      elseif (DMRepeat == QUEST_ACCEPTED and trade:hasItemQty(1261,1) and trade:getItemCount() == 1 and
+         player:hasKeyItem(MOONLIGHT_ORE) == false) then
+            player:startEvent(8); -- Moonlight Ore trade
+      end
+   end
+end;
+
+-----------------------------------
+-- onEventSelection
+-----------------------------------
+
+function onEventUpdate(player,csid,menuchoice)
+end;
+
+-----------------------------------
+-- onEventFinish Action
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+   if (csid == 7) then
+      if (player:getFreeSlotsCount() == 0) then 
+         player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,1550);
+      else 
+         player:addItem(1550);
+         player:messageSpecial(ITEM_OBTAINED,1550);
+         player:tradeComplete();
+      end
+   elseif (csid == 8) then
+      player:tradeComplete();
+      player:addKeyItem(MOONLIGHT_ORE);
+      player:messageSpecial(KEYITEM_OBTAINED,MOONLIGHT_ORE);
+   end
+end;
