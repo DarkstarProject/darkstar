e070ffb5849bdb2d61ff5cb408762ce03ff9f0b0
PrBlahBlahtson <PrBlahBlahtson@gmail.com>
2013-06-11 03:14:23 +0000

    SCRIPTS: Breaks two doors in Lower Delkfutt's so players can no longer walk into the void and get stuck.

---
+++ scripts/zones/Lower_Delkfutts_Tower/npcs/_543.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+-- Area: Lower Delkfutt's Tower
+-- NPC:  Cermet Door
+-- Never opens.
+-----------------------------------
+package.loaded["scripts/zones/Lower_Delkfutts_Tower/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/zones/Lower_Delkfutts_Tower/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	return 1;
+end; 
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--print("CSID:",csid);
+--print("RESULT:",option);
+end;
