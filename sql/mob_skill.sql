71fa6b4e6e9018c261b26390b1728cc63d84804d
bluekirby0 <bluekirby0@gmail.com>
2013-07-07 05:20:04 +0000

    DB: Fixing skill ID of 10000 Needles so people will stop harassing me about it while I'm working on other stuff

--- sql/mob_skill.sql
+++ sql/mob_skill.sql
@@ -165,7 +165,7 @@ INSERT INTO `mob_skill` VALUES (68,212,68,'Photosynthesis', 0, 7.0, 2000, 1500, 
 INSERT INTO `mob_skill` VALUES (68,362,68,'Photosynthesis', 0, 7.0, 2000, 1500, 1, 0);
 INSERT INTO `mob_skill` VALUES (66,362,66,'1000_Needles', 1, 10.0, 2000, 1500, 4, 0);
 INSERT INTO `mob_skill` VALUES (65,362,65,'Needleshot', 0, 7.0, 2000, 1500, 4, 0);
-INSERT INTO `mob_skill` VALUES (66,362,66,'10000_Needles', 1, 10.0, 2000, 1500, 4, 0); -- Only used by certain Notorious Monsters
+INSERT INTO `mob_skill` VALUES (67,362,67,'10000_Needles', 1, 10.0, 2000, 1500, 4, 0); -- Only used by certain Notorious Monsters
 
 -- Funguar
 INSERT INTO `mob_skill` VALUES (52,116,52,'Frogkick', 0, 7.0, 2000, 1500, 4, 0);
