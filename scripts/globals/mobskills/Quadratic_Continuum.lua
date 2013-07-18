920bfc2cdb943db0c2284408d4ba87bbc2b79a8b
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-07-18 21:55:24 +0000

    DATABASE: corrected droplists for yugados, rarabs and crabs in west / east sarutabaruta. Weakened quadratic continuum.

--- scripts/globals/mobskills/Quadratic_Continuum.lua
+++ scripts/globals/mobskills/Quadratic_Continuum.lua
@@ -10,7 +10,7 @@ end;
 function OnMobWeaponSkill(target, mob, skill)
     local numhits = 4;
     local accmod = 1;
-    local dmgmod = math.random(1,2)+math.random();
+    local dmgmod = 1.1;
     local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,1,1.5,2);
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
     target:delHP(dmg);
