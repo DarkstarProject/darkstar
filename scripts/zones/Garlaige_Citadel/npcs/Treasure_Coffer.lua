616f9da3a83c8a12718166511afbc4f9cfe7fc45
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-07-25 03:28:13 +0000

    SCRIPTS: typo fix for garage citadel. Forgot to mention in last commit. Fixed bug where shields would absorb the wrong amount of dmg.

--- scripts/zones/Garlaige_Citadel/npcs/Treasure_Coffer.lua
+++ scripts/zones/Garlaige_Citadel/npcs/Treasure_Coffer.lua
@@ -28,16 +28,16 @@ function onTrade(player,npc,trade)
 	-- trade:hasItemQty(1023,1);			-- Living Key
 	-- trade:hasItemQty(1022,1);			-- Thief's Tools
 	local questItemNeeded = 0;
-	
+
 	-- Player traded a key.
-	if((trade:hasItemQty(1047,1) or trade:hasItemQty(1115,1) or trade:hasItemQty(1023,1) or trade:hasItemQty(1022,1)) and trade:getItemCount() == 1) then 
-		
+	if((trade:hasItemQty(1047,1) or trade:hasItemQty(1115,1) or trade:hasItemQty(1023,1) or trade:hasItemQty(1022,1)) and trade:getItemCount() == 1) then
+
 		-- IMPORTANT ITEM: AF Keyitems, AF Items, & Map -----------
 		local mJob = player:getMainJob();
 		local zone = player:getZone();
 		local AFHandsActivated = player:getVar("BorghertzAlreadyActiveWithJob");
 		local listAF = getAFbyZone(zone);
-		if((AFHandsActivated == 11 or AFHandsActivated == 4) and player:hasKeyItem(OLD_GAUNTLETS) == false) then 
+		if((AFHandsActivated == 11 or AFHandsActivated == 4) and player:hasKeyItem(OLD_GAUNTLETS) == false) then
 			questItemNeeded = 1;
 		else
 			for nb = 1,table.getn(listAF),3 do
@@ -48,7 +48,7 @@ function onTrade(player,npc,trade)
 			end
 		end
 		--------------------------------------
-		
+
 		local pack = openChance(player,npc,trade,TreasureType,TreasureLvL,TreasureMinLvL,questItemNeeded);
 		local success = 0;
 		if(pack[2] ~= nil) then
@@ -57,14 +57,14 @@ function onTrade(player,npc,trade)
 		else
 			success = pack[1];
 		end
-		
+
 		if(success ~= -2) then
 			player:tradeComplete();
-			
+
 			if(math.random() <= success) then
 				-- Succeded to open the coffer
 				player:messageSpecial(CHEST_UNLOCKED);
-				
+
 				if(questItemNeeded == 1) then
 					player:addKeyItem(OLD_GAUNTLETS);
 					player:messageSpecial(KEYITEM_OBTAINED,OLD_GAUNTLETS); -- Old Gauntlets (KI)
@@ -77,13 +77,13 @@ function onTrade(player,npc,trade)
 						end
 					end
 				else
-					player:setVar("["..zone.."]".."Treasure_"..TreasureType,os.time() + math.random(CHEST_MIN_ILLUSION_TIME,CHEST_MAX_ILLUSION_TIME)); 
-					
+					player:setVar("["..zone.."]".."Treasure_"..TreasureType,os.time() + math.random(CHEST_MIN_ILLUSION_TIME,CHEST_MAX_ILLUSION_TIME));
+
 					local loot = cofferLoot(zone,npc);
 					-- print("loot array: "); -- debug
 					-- print("[1]", loot[1]); -- debug
 					-- print("[2]", loot[2]); -- debug
-					
+
 					if(loot[1]=="gil") then
 						player:addGil(loot[2]*GIL_RATE);
 						player:messageSpecial(GIL_OBTAINED,loot[2]*GIL_RATE);
@@ -92,12 +92,12 @@ function onTrade(player,npc,trade)
 						player:addItem(loot[2]);
 						player:messageSpecial(ITEM_OBTAINED,loot[2]);
 					end
-					UpdateTreasureSpawnPoint(npc:getID());
-				else
-					player:messageSpecial(CHEST_MIMIC);
-					spawnMimic(zone,npc,player);
-					UpdateTreasureSpawnPoint(npc:getID(), true);
 				end
+				UpdateTreasureSpawnPoint(npc:getID());
+			else
+				player:messageSpecial(CHEST_MIMIC);
+				spawnMimic(zone,npc,player);
+				UpdateTreasureSpawnPoint(npc:getID(), true);
 			end
 		end
 	end
@@ -110,7 +110,7 @@ end;
 
 function onTrigger(player,npc)
 	player:messageSpecial(CHEST_LOCKED,1047);
-end; 
+end;
 
 -----------------------------------
 -- onEventUpdate
