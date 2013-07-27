9cc07d35f0069c92a87585c7b65194846d68e7f2
ben.dangelo2 <ben.dangelo2@gmail.com>
2013-07-27 23:53:08 +0000

    CORE: fixed Mazween's shop list

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Mazween.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Mazween.lua
@@ -13,14 +13,14 @@ require("scripts/zones/Aht_Urhgan_Whitegate/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end; 
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
-	
+
 player:showText(npc,MAZWEEN_SHOP_DIALOG);
 
 stock = {0x1313,11200,		-- Scroll of Sleepga
@@ -29,7 +29,7 @@ stock = {0x1313,11200,		-- Scroll of Sleepga
 	 0x12E7,14000,		-- Bio II
 	 0x1296,5160,		-- Poisonga
 	 0x1316,19932,		-- Stone III
-	 0x1315,22682,		-- Water III
+	 4779,22682,		-- Water III
 	 0x12DD,27744,		-- Aero III
 	 0x129C,33306,		-- Fire III
 	 0x12E1,39368,		-- Blizzard III
@@ -38,10 +38,10 @@ stock = {0x1313,11200,		-- Scroll of Sleepga
 	 0x1311,44000,		-- Absorb-ACC
 	 0x12A1,30780,		-- Drain II
 	 0x1315,70560,		-- Dread Spikes
-	 0x4856,79800}		-- Aspir II
- 
+	 4856,79800}		-- Aspir II
+
 showShop(player, STATIC, stock);
-end; 
+end;
 
 -----------------------------------
 -- onEventUpdate
