613186723117866657405f35caa875e29f4ed387
whasf <whasf@dspt.info>
2013-08-11 19:43:02 +0000

    SCRIPTS: Valley of Sorrows Field Manual (Thank you Apocalypse612)

---
+++ scripts/zones/Valley_of_Sorrows/npcs/Field_Manual.lua
@@ -0,0 +1,38 @@
+-----------------------------------	
+-- Field Manual	
+-- Area: Valley of Sorrows
+-----------------------------------	
+	
+require("/scripts/globals/settings");	
+require("/scripts/globals/fieldsofvalor");	
+	
+-----------------------------------	
+-- onTrigger Action	
+-----------------------------------	
+	
+function onTrigger(player,npc)	
+	startFov(FOV_EVENT_SORROWS,player);
+end;	
+	
+-----------------------------------	
+-- onTrade Action	
+-----------------------------------	
+	
+function onTrade(player,npc,trade)	
+end;	
+	
+-----------------------------------	
+-- onEventSelection	
+-----------------------------------	
+	
+function onEventUpdate(player,csid,menuchoice)	
+	updateFov(player,csid,menuchoice,139,140,141,0,0);
+end;	
+	
+-----------------------------------	
+-- onEventFinish Action	
+-----------------------------------	
+	
+function onEventFinish(player,csid,option)	
+	finishFov(player,csid,option,139,140,141,0,0,FOV_EVENT_SORROWS);
+end;	
