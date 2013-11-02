dad10325504a7a7f4f8e5254e29f1b9fddd37921
teschnei <teschnei@gmail.com>
2013-11-02 18:18:14 +0000

    SQL: corrected npcids in East Ronfaure, La Theine Plateau, South Gustaberg, Eastern Altepa Desert, The Sanctuary of Zi'Tah, Western Altepa Desert and Behemoth's Dominion for Oct. 8th Version Update (thanks killeon) SCRIPTS: updated logging points in East Ronfaure for Oct 8th Version Update -updated La Theine Plateau zone script for modified npcids (thanks killeon)

--- scripts/zones/La_Theine_Plateau/Zone.lua
+++ scripts/zones/La_Theine_Plateau/Zone.lua
@@ -17,11 +17,11 @@ require("scripts/globals/weather");
 -----------------------------------
 
 function onInitialize( zone)
-end;		
+end;
 
------------------------------------		
--- onZoneIn		
------------------------------------		
+-----------------------------------
+-- onZoneIn
+-----------------------------------
 
 function onZoneIn( player, prevZone)
 
@@ -30,79 +30,77 @@ function onZoneIn( player, prevZone)
 	if( player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
 		player:setPos( -272.118, 21.715, 98.859, 243);
 	end
-	
+
 	if( triggerLightCutscene( player)) then -- Quest: I Can Hear A Rainbow
 		cs = 0x007b;
-	elseif( prevZone == 193 and player:getVar( "darkPuppetCS") == 5 and player:getFreeSlotsCount() >= 1) then		
-		cs = 0x007a;	
-	end		
-	return cs;		
-end;			
+	elseif( prevZone == 193 and player:getVar( "darkPuppetCS") == 5 and player:getFreeSlotsCount() >= 1) then
+		cs = 0x007a;
+	end
+	return cs;
+end;
 
------------------------------------			
--- onRegionEnter		
------------------------------------		
+-----------------------------------
+-- onRegionEnter
+-----------------------------------
 
-function onRegionEnter( player, region)		
-end;		
+function onRegionEnter( player, region)
+end;
 
------------------------------------		
--- onEventUpdate		
------------------------------------		
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
 
-function onEventUpdate( player, csid, option)		
---printf("CSID: %u",csid);	
+function onEventUpdate( player, csid, option)
+--printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 
-	if( csid == 0x007b) then	
+	if( csid == 0x007b) then
 		lightCutsceneUpdate( player);  -- Quest: I Can Hear A Rainbow
-	end		
-end;			
+	end
+end;
 
------------------------------------			
--- onEventFinish			
------------------------------------			
+-----------------------------------
+-- onEventFinish
+-----------------------------------
 
-function onEventFinish( player, csid, option)			
---printf("CSID: %u",csid);		
+function onEventFinish( player, csid, option)
+--printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 
-	if( csid == 0x007b) then		
+	if( csid == 0x007b) then
 		lightCutsceneFinish( player);  -- Quest: I Can Hear A Rainbow
-	elseif( csid == 0x007a) then	
+	elseif( csid == 0x007a) then
 		player:addItem( 14096);
 		player:messageSpecial( ITEM_OBTAINED, 14096); -- Chaos Sollerets
 		player:setVar( "darkPuppetCS", 0);
 		player:addFame( BASTOK, AF2_FAME);
 		player:completeQuest( BASTOK,DARK_PUPPET);
-	end	
+	end
 end;
 
 function OnZoneWeatherChange(weather)
-    
-    local _2u0 = GetNPCByID(17195599);
+
+    local _2u0 = GetNPCByID(17195600);
     local VanadielTOTD = VanadielTOTD();
     local I_Can_Hear_a_Rainbow = GetServerVariable("I_Can_Hear_a_Rainbow");
-    
+
     if (I_Can_Hear_a_Rainbow == 1 and weather ~= WEATHER_RAIN and VanadielTOTD >= TIME_DAWN and VanadielTOTD <= TIME_EVENING and _2u0:getAnimation() == 9) then
         _2u0:setAnimation(8);
     elseif (I_Can_Hear_a_Rainbow == 1 and weather == WEATHER_RAIN and _2u0:getAnimation() == 8) then
         _2u0:setAnimation(9);
         SetServerVariable("I_Can_Hear_a_Rainbow", 0);
     end
-    
 end;
 
 function OnTOTDChange(TOTD)
 
-    local _2u0 = GetNPCByID(17195599);
+    local _2u0 = GetNPCByID(17195600);
     local I_Can_Hear_a_Rainbow = GetServerVariable("I_Can_Hear_a_Rainbow");
-    
+
     if (I_Can_Hear_a_Rainbow == 1 and TOTD >= TIME_DAWN and TOTD <= TIME_EVENING and _2u0:getAnimation() == 9) then
         _2u0:setAnimation(8);
     elseif (I_Can_Hear_a_Rainbow == 1 and TOTD < TIME_DAWN or TOTD > TIME_EVENING and _2u0:getAnimation() == 8) then
         _2u0:setAnimation(9);
         SetServerVariable("I_Can_Hear_a_Rainbow", 0);
     end
-
 end;
