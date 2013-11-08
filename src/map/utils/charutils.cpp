bc2912b81b4c8a29bd2d7b459f0b76929cf87c83
teschnei <teschnei@gmail.com>
2013-11-08 00:15:38 +0000

    CORE: updated BuildingCharPetAbilityTable to use CheckAbilityAddType (to function properly with merits and astral flow)

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -1927,7 +1927,7 @@ void BuildingCharPetAbilityTable(CCharEntity* PChar, CPetEntity* PPet, uint32 Pe
 		{
 			CAbility* PAbility = AbilitiesList.at(i);
 
-			if (PPet->GetMLevel() >= PAbility->getLevel() && PetID>=8 && PetID<=15 && PAbility->getAddType() == ADDTYPE_NORMAL) //carby/fen/ele avatars NOT diabolos
+			if (PPet->GetMLevel() >= PAbility->getLevel() && PetID>=8 && PetID<=15 && CheckAbilityAddtype(PChar, PAbility)) //carby/fen/ele avatars NOT diabolos
 			{
 				//16 IDs per avatar starting from 496
 				if(PAbility->getID()>= (496+((PetID-8)*16)) && PAbility->getID() < (496+((PetID-7)*16))){ //pet ability
