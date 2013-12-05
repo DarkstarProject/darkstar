1bc36c99732459513e24d96020a6be3e8cbcaca1
teschnei <teschnei@gmail.com>
2013-12-05 22:13:30 +0000

    SCRIPTS: Desperate Blows parity update

--- scripts/globals/effects/last_resort.lua
+++ scripts/globals/effects/last_resort.lua
@@ -12,9 +12,8 @@ require("scripts/globals/status");
 
 function onEffectGain(target,effect)
 	target:addMod(MOD_ATTP,15 + target:getMerit(MERIT_LAST_RESORT_EFFECT));
-	-- print(target:getMerit(MERIT_LAST_RESORT_EFFECT));
 	target:addMod(MOD_DEFP,-15 - target:getMerit(MERIT_LAST_RESORT_EFFECT));
-	target:addMod(MOD_HASTE_ABILITY, effect:getPower())
+	target:addMod(MOD_HASTE_ABILITY, 156+effect:getPower())
 end;
 
 -----------------------------------
@@ -31,5 +30,5 @@ end;
 function onEffectLose(target,effect)
 	target:delMod(MOD_ATTP,15 + target:getMerit(MERIT_LAST_RESORT_EFFECT));
 	target:delMod(MOD_DEFP,-15 - target:getMerit(MERIT_LAST_RESORT_EFFECT));
-	target:delMod(MOD_HASTE_ABILITY, effect:getPower())
+	target:delMod(MOD_HASTE_ABILITY, 156+effect:getPower())
 end;
