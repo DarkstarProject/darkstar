fd99f28bfa06a22efc53fbf96f577e81c3ebace1
linkandentity <linkandentity@gmail.com>
2013-12-10 16:12:22 +0000

    SCRIPTS: And the double_shot.lua ability for good measure.

---
+++ scripts/globals/abilities/double_shot.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+-- Ability: Double Shot
+-----------------------------------
+
+require("scripts/globals/settings");
+require("scripts/globals/status");
+
+-----------------------------------
+-- OnUseAbility
+-----------------------------------
+
+function OnAbilityCheck(player,target,ability)
+	return 0,0;
+end;
+
+function OnUseAbility(player, target, ability)
+
+	local chance = 40;
+	
+	player:addStatusEffect(EFFECT_DOUBLE_SHOT,chance,0,90);
+end;
