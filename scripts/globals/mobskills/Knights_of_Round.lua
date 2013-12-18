f838c350378fc024205680446a20646fe806b8c2
linkandentity <linkandentity@gmail.com>
2013-12-18 13:03:59 +0000

    SCRIPTS: Crash fixes in Knights_of_Round.lua and Geirskogul.lua mobskill scripts.

--- scripts/globals/mobskills/Knights_of_Round.lua
+++ scripts/globals/mobskills/Knights_of_Round.lua
@@ -21,6 +21,7 @@ function OnMobWeaponSkill(target, mob, skill)
 	local numhits = 1;
 	local accmod = 1;
 	local dmgmod = 2.5;
+	local duration = 20;
 	
 	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_DMG_VARIES,3,3,3);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
