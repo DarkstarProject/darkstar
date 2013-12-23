4a1c7f2cf10e430ae0a8f76cd0a6352673b243ab
teschnei <teschnei@gmail.com>
2013-12-23 00:31:32 +0000

    SCRIPTS: Shikikoyo script fix

--- scripts/globals/abilities/shikikoyo.lua
+++ scripts/globals/abilities/shikikoyo.lua
@@ -11,18 +11,18 @@ require("scripts/globals/status");
 -- OnUseAbility
 -----------------------------------
 function OnAbilityCheck(player,target,ability)
-	if (player:getTP() <= 100) then
+	if (player:getTP() < 100) then
 		return MSGBASIC_NOT_ENOUGH_TP, 0;
 	end
 	return 0,0;
 end;
 
 function OnUseAbility(player, target, ability)
-	local pTP = -(100 - player:getTP());
+	local pTP = player:getTP() - 100;
 
 	if (pTP > 0) then
 		player:setTP(100);
-		target:setTP(target:getTP() + pTP + (12 - player:getMerit(MERIT_SHIKIKOYO)));
+		target:setTP(target:getTP() + pTP + (player:getMerit(MERIT_SHIKIKOYO) - 12));
 	end
 	
 end;
