5ba7ce46cbe8ffd24b0097bac93ea5d4a5af846e
atom0s <atom0s@live.com>
2013-12-30 23:07:11 +0000

    SCRIPTS: Added a command to test messagespecial as a GM.

---
+++ scripts/commands/messagespecial.lua
@@ -0,0 +1,27 @@
+---------------------------------------------------------------------------------------------------
+-- func: messagespecial
+-- auth: atom0s
+-- desc: Injects a message special packet.
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "iiiii"
+};
+
+function onTrigger(player, msgid, param1, param2, param3, param4, param5)
+    if (param5 ~= nil) then
+        player:messageSpecial(msgid, param1, param2, param3, param4, param5);
+    elseif (param4 ~= nil) then
+        player:messageSpecial(msgid, param1, param2, param3, param4);
+    elseif (param3 ~= nil) then
+        player:messageSpecial(msgid, param1, param2, param3);
+    elseif (param2 ~= nil) then
+        player:messageSpecial(msgid, param1, param2);
+    elseif (param1 ~= nil) then
+        player:messageSpecial(msgid, param1);
+    else
+        player:messageSpecial(msgid);
+    end
+end
