8944e776e72eb43ae91805f33825c0108cadd513
teschnei <teschnei@gmail.com>
2013-12-30 07:28:46 +0000

    SCRIPTS: r4332 update

--- scripts/zones/West_Ronfaure/npcs/Palcomondau.lua
+++ scripts/zones/West_Ronfaure/npcs/Palcomondau.lua
@@ -313,7 +313,7 @@ function onPath(npc)
 
 	if(npc:atPoint(pathfind.get(path, 45))) then
 
-		local gac = GetNPCByID(17187491); -- Gachemage
+		local gac = GetNPCByID(npc:getID() + 3); -- Gachemage
 
 		gac:showText(npc, PALCOMONDAU_REPORT);
 
