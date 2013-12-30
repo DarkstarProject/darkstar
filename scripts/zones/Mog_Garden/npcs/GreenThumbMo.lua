d18f5b7cddf664c490e0060aaf3ab0932755357c
atom0s <atom0s@live.com>
2013-12-30 20:58:23 +0000

    SCRIPTS: Added basic Mog Garden support. (Green Thumb Moogle will just instantly homepoint whomever talks to him.)

---
+++ scripts/zones/Mog_Garden/npcs/GreenThumbMo.lua
@@ -0,0 +1,33 @@
+
+-----------------------------------
+-- onTrade
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end;
+
+-----------------------------------
+-- onTrigger
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:warp();
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
