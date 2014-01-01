432a335cbc961c3f7a97512ba5a3f40c18432e3d
teschnei <teschnei@gmail.com>
2014-01-01 09:16:27 +0000

    SCRIPTS: add Knights Minne V and Valor Minuet V

---
+++ scripts/globals/spells/valor_minuet_v.lua
@@ -0,0 +1,57 @@
+-----------------------------------------
+-- Spell: Valor Minuet V
+-- Grants Attack bonus to all allies.
+-----------------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
+function OnMagicCastingCheck(caster,target,spell)
+	return 0;
+end;
+
+function onSpellCast(caster,target,spell)
+
+	local sLvl = caster:getSkillLevel(SKILL_SNG); -- Gets skill level of Singing
+    local iLvl = caster:getWeaponSkillLevel(SLOT_RANGED);
+
+	local power = 32;
+
+    if (sLvl+iLvl > 500) then
+        power = power + math.floor((sLvl+iLvl-500) / 6);
+    end
+    
+	if(power >= 62) then
+		power = 62;
+	end
+    
+	local iBoost = caster:getMod(MOD_MINUET_EFFECT) + caster:getMod(MOD_ALL_SONGS_EFFECT);
+    if (iBoost > 0) then
+        power = power + 1 + (iBoost-1)*4;
+    end
+
+    power =  power + caster:getMerit(MERIT_MINUET_EFFECT);
+    
+    if (caster:hasStatusEffect(EFFECT_SOUL_VOICE)) then
+        power = power * 2;
+    elseif (caster:hasStatusEffect(EFFECT_MARCATO)) then
+        power = power * 1.5;
+    end
+    caster:delStatusEffect(EFFECT_MARCATO);
+    
+    local duration = 120;
+    duration = duration * ((iBoost * 0.1) + (caster:getMod(MOD_SONG_DURATION_BONUS)/100) + 1);
+    
+    if (caster:hasStatusEffect(EFFECT_TROUBADOUR)) then
+        duration = duration * 2;
+    end
+    
+	if not (target:addBardSong(caster,EFFECT_MINUET,power,0,duration,caster:getID(), 0, 5)) then
+        spell:setMsg(75);
+    end
+
+	return EFFECT_MINUET;
+end;
