55825262555512a8842e6d195dd6ec6642017ed2
atom0s <atom0s@live.com>
2014-01-04 11:06:55 +0000

    SCRIPT: Added Tanners Belt item script. (Thanks saidin)

---
+++ scripts/globals/items/tanners_belt.lua
@@ -0,0 +1,48 @@
+-----------------------------------------
+-- ID: 15448
+-- Item: Tanners belt
+-- Enchantment: Synthesis image support
+-- 2Min, All Races
+-----------------------------------------
+-- Enchantment: Synthesis image support
+-- Duration: 2Min
+-- Leathercraft Skill +1
+-----------------------------------------
+ 
+require("scripts/globals/status");
+ 
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+ 
+function onItemCheck(target)
+local result = 0;
+        if (target:hasStatusEffect(EFFECT_LEATHERCRAFT_IMAGERY) == true) then
+                result = 240;
+        end
+return result;
+end;
+ 
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+ 
+function onItemUse(target)
+        target:addStatusEffect(EFFECT_LEATHERCRAFT_IMAGERY,53,0,120);
+end;
+ 
+-----------------------------------------
+-- onEffectGain Action
+-----------------------------------------
+ 
+function onEffectGain(target,effect)
+        target:addMod(MOD_SKILL_LTH, 1);
+end;
+ 
+-----------------------------------------
+-- onEffectLose Action
+-----------------------------------------
+ 
+function onEffectLose(target,effect)
+    target:delMod(MOD_SKILL_LTH, 1);
+end;
