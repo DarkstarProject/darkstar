96101ac80a7d35de9a7207343fc4d7c875ee3453
xdemolish <xdemolish@hotmail.com>
2014-01-05 13:03:44 +0000

    SCRIPTS: San d'Oria Mission 7-1 typo fix. (Thank you Mishima)

--- scripts/zones/West_Ronfaure/mobs/Marauder_Dvogzog.lua
+++ scripts/zones/West_Ronfaure/mobs/Marauder_Dvogzog.lua
@@ -3,17 +3,16 @@
 -- MOB:  Marauder Dvogzog	
 -----------------------------------	
 	
-require("/scripts/globals/fieldsofvalor");	
-require("/scripts/globals/missions");	
+require("scripts/globals/fieldsofvalor");	
+require("scripts/globals/missions");	
 
 -----------------------------------	
 -- onMobDeath	
 -----------------------------------	
 	
 function onMobDeath(mob,killer)	
-	if(killer:getCurrentMission(0,18) and killer:getVar("MissionStatus") == 1) then
+	if(killer:getCurrentMission(SANDORIA) == PRESTIGE_OF_THE_PAPSQUE and killer:getVar("MissionStatus") == 1) then
 		killer:setVar("Mission7-1MobKilled",1);
-	else
-	    checkRegime(killer,mob,4,1);
 	end
+	    checkRegime(killer,mob,4,1);
 end;	
