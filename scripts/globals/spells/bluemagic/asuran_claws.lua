508f8b48efdb15e4eac6a9142e628899357bfbb9
teschnei <teschnei@gmail.com>
2014-01-19 19:40:01 -0700

    SCRIPTS: lowered multiplier on Asuran Claws

--- scripts/globals/spells/bluemagic/asuran_claws.lua
+++ scripts/globals/spells/bluemagic/asuran_claws.lua
@@ -15,8 +15,8 @@ function onSpellCast(caster,target,spell)
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
     params.tpmod = TPMOD_ACC; params.dmgtype = DMGTYPE_H2H; params.scattr = SC_IMPACTION;
 	params.numhits = 6;
-	params.multiplier = 20.0; params.tp150 = 1.05; params.tp300 = 1.1; params.azuretp = 1.2; params.duppercap = 21; 
-	params.str_wsc = 0.0; params.dex_wsc = 0.0; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.3;
+	params.multiplier = 1.0; params.tp150 = 1.05; params.tp300 = 1.1; params.azuretp = 1.2; params.duppercap = 21; 
+	params.str_wsc = 0.3; params.dex_wsc = 0.0; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
     damage = BluePhysicalSpell(caster, target, spell, params);
     damage = BlueFinalAdjustments(caster, target, spell, damage, params);
     
