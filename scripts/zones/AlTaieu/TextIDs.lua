32cf059f9430a0c874a0c48bff91ed0d4ed5895b
Demolish <xdemolish@hotmail.com>
2014-01-21 17:33:52 +0000

    SCRIPTS: Updated TextIDs in Abdhaljs Isle-Purgonorgo, Abyssea Altepa, Abyssea Attohwa, Abyssea Empyreal Paradox, Abyssea Grauberg, Abyssea Konschtat, Abyssea La Theine, Abyssea Misareaux, Abyssea Tahrongi, Abyssea Uleguerand, Abyssea Vunkerl, Aht Urhgan Whitegate, AlTaieu, Al Zahbi, Altar Room, Alzadaal Undersea Ruins, Apollyon, Arrapago Reef, Arrapago Remnants, Attohwa Chasm, Aydeewa Subterrane.

--- scripts/zones/AlTaieu/TextIDs.lua
+++ scripts/zones/AlTaieu/TextIDs.lua
@@ -1,10 +1,10 @@
 -- Variable TextID   Description text
 
 -- General Texts
-ITEM_CANNOT_BE_OBTAINED = 6375; -- You cannot obtain the item <item> come back again after sorting your inventory
-          ITEM_OBTAINED = 6378; -- Obtained: <item>
-           GIL_OBTAINED = 6379; -- Obtained <number> gil
-       KEYITEM_OBTAINED = 6381; -- Obtained key item: <keyitem>
+ITEM_CANNOT_BE_OBTAINED = 6376; -- You cannot obtain the item <item> come back again after sorting your inventory
+          ITEM_OBTAINED = 6379; -- Obtained: <item>
+           GIL_OBTAINED = 6380; -- Obtained <number> gil
+       KEYITEM_OBTAINED = 6382; -- Obtained key item: <keyitem>
      
 -- Quasilumin Texts
-QUASILUMIN_01 = 7322; -- This is Al'Taieu. The celestial capital overflowing with the blessings of Altana.
+QUASILUMIN_01 = 7323; -- This is Al'Taieu. The celestial capital overflowing with the blessings of Altana.
