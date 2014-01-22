42fda95a3cd47aa926c83dca82469f575beb1d9f
atom0s <atom0s@live.com>
2014-01-22 03:10:28 -0800

    SCRIPT: Added new command to set a players nation.

---
+++ scripts/commands/setplayernation.lua
@@ -0,0 +1,29 @@
+---------------------------------------------------------------------------------------------------
+-- func: setplayernation
+-- auth: atom0s
+-- desc: Sets the target players nation.
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "si"
+};
+
+function onTrigger(player, target, nation)
+    if (target == nil) then
+        target = player:getName();
+    end
+    
+    if (nation > 2 or nation < 0) then
+        player:PrintToPlayer( "Invalid nation id given! Must be 0, 1, or 2!" );
+        return;
+    end
+        
+    local targ = GetPlayerByName( target );
+    if (targ ~= nil) then
+        targ:setNation( nation );
+    else
+        player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
+    end
+end
