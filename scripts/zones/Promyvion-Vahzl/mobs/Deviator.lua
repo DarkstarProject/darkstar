ad01fcf361914b4954e7ce0fcee1b601fe56ebeb
Apocalypse612 <Apocalypse612@yahoo.com>
2014-01-27 12:23:55 -0500

    SCRIPTS: Deviator, Provoker, & Wailer will now spawn. SQL: Gave the NMs proper spawn positions, immunity, size, drop table, etc etc.

---
+++ scripts/zones/Promyvion-Vahzl/mobs/Deviator.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+-- Area: Promyvion vahzl
+-- NPC:  Deviator
+-----------------------------------
+
+
+-----------------------------------
+-- onMobSpawn
+-----------------------------------
+
+function OnMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+	GetNPCByID(16867687):hideNPC(900); -- 15m Hide Time?, qm1
+end;
---
+++ scripts/zones/Promyvion-Vahzl/mobs/Provoker.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+-- Area: Promyvion vahzl
+-- NPC:  Provoker
+-----------------------------------
+
+
+-----------------------------------
+-- onMobSpawn
+-----------------------------------
+
+function OnMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+	GetNPCByID(16867689):hideNPC(900); -- 15m Hide Time?, qm3
+end;
