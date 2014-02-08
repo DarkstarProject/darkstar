fbe5b696ef913ffd89fc185a75ab43b192caeb58
EDGECOM <dwh_48048@hotmail.com>
2014-02-08 16:15:51 -0500

    no message

--- sql/fishing_rods.sql
+++ sql/fishing_rods.sql
@@ -1,23 +1,67 @@
-﻿# Host: 192.168.1.17  (Version: 5.6.15)
-# Date: 2014-02-06 17:14:54
-# Generator: MySQL-Front 5.3  (Build 4.102)
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: dspdb
+-- ------------------------------------------------------
+-- Server version	5.6.15
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-#
-# Structure for table "fishing_rods"
-#
+--
+-- Table structure for table `fishing_rods`
+--
 
 DROP TABLE IF EXISTS `fishing_rods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fishing_rods` (
   `rodid` int(11) DEFAULT NULL,
   `name` text,
   `type` int(11) DEFAULT NULL,
   `brokenid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-#
-# Data for table "fishing_rods"
-#
+--
+-- Dumping data for table `fishing_rods`
+--
 
-INSERT INTO `fishing_rods` VALUES (17389,'Bamboo Fishing Rod',0,487),(17384,'Carbon Fishing Rod',1,490),(17383,'Clothespole',0,482),(17381,'Composite Fishing Rod',1,473),(17011,'Ebisu Fishing Rod',0,0),(17388,'Fastwater Fishing Rod',0,488),(17385,'Glass Fiber Fishing Rod',1,491),(17015,'Halcyon Fishing Rod',1,1833),(17014,'Hume Fishing Rod',0,1832),(17386,'Lu Shang\'s Fishing Rod',0,489),(17380,'Mithran Fishing Rod',0,483),(17382,'Single Hook Fishing Rod',1,472),(17387,'Tarutaru Fishing Rod',0,484),(17391,'Willow Fishing Rod',0,485),(17390,'Yew Fishing Rod',0,486),(17012,'Judges Fishing Rod',1,0);
+LOCK TABLES `fishing_rods` WRITE;
+/*!40000 ALTER TABLE `fishing_rods` DISABLE KEYS */;
+INSERT INTO `fishing_rods` VALUES (17389,'Bamboo Fishing Rod',0,487);
+INSERT INTO `fishing_rods` VALUES (17384,'Carbon Fishing Rod',1,490);
+INSERT INTO `fishing_rods` VALUES (17383,'Clothespole',0,482);
+INSERT INTO `fishing_rods` VALUES (17381,'Composite Fishing Rod',1,473);
+INSERT INTO `fishing_rods` VALUES (17011,'Ebisu Fishing Rod',0,0);
+INSERT INTO `fishing_rods` VALUES (17388,'Fastwater Fishing Rod',0,488);
+INSERT INTO `fishing_rods` VALUES (17385,'Glass Fiber Fishing Rod',1,491);
+INSERT INTO `fishing_rods` VALUES (17015,'Halcyon Fishing Rod',1,1833);
+INSERT INTO `fishing_rods` VALUES (17014,'Hume Fishing Rod',0,1832);
+INSERT INTO `fishing_rods` VALUES (17386,'Lu Shang\'s Fishing Rod',0,489);
+INSERT INTO `fishing_rods` VALUES (17380,'Mithran Fishing Rod',0,483);
+INSERT INTO `fishing_rods` VALUES (17382,'Single Hook Fishing Rod',1,472);
+INSERT INTO `fishing_rods` VALUES (17387,'Tarutaru Fishing Rod',0,484);
+INSERT INTO `fishing_rods` VALUES (17391,'Willow Fishing Rod',0,485);
+INSERT INTO `fishing_rods` VALUES (17390,'Yew Fishing Rod',0,486);
+INSERT INTO `fishing_rods` VALUES (17012,'Judges Fishing Rod',1,0);
+/*!40000 ALTER TABLE `fishing_rods` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-02-08 15:58:00
