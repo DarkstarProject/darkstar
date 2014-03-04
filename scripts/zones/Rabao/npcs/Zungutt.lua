1aa145b0f4d913ba16413770e4fc81d621edb396
Tom_Neverwinter <Tom_Neverwinter@yahoo.com>
2014-03-04 01:28:49 -0600

    Remaining Melody Minstrels

---
+++ scripts/zones/Rabao/npcs/Zungutt.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+--	Area: Rabao
+--	NPC: Zungutt
+--	Standard Info NPC
+-----------------------------------
+package.loaded["scripts/zones/Port_Windurst/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/zones/Port_Windurst/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+	player:startEvent(0x0065);
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
+end;
