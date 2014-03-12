09770373da9272384c09e1ebf3714d0357bb9134
Apocalypse612 <Apocalypse612@yahoo.com>
2014-03-12 17:56:19 -0400

    SCRIPTS: Bug 836- Add missing NPC Drozga. Fixed OP Warp NPCs displaying wrong nation text if you can't use their services.

--- scripts/zones/Port_Windurst/npcs/Drozga.lua
+++ scripts/zones/Port_Windurst/npcs/Drozga.lua
@@ -1,37 +1,40 @@
 -----------------------------------
 -- Area: Port Windurst
--- NPC:  Rottata
--- Outpost Teleporter NPC
--- @pos 193.111 -12.999 215.638 240
+-- NPC:  Drozga
+-- Standard Merchant NPC
 -----------------------------------
 package.loaded["scripts/zones/Port_Windurst/TextIDs"] = nil;
 -----------------------------------
 
-require("scripts/globals/conquest");
+require("scripts/globals/shop");
 require("scripts/zones/Port_Windurst/TextIDs");
-
-guardnation = WINDURST;
-csid 		= 0x0228;
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end;
+end; 
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
-	
-	if(guardnation == player:getNation()) then
-		player:startEvent(csid,0,0,0,0,0,0,player:getMainLvl(),1073741823 - player:getNationTeleport(guardnation));
-	else
-		player:startEvent(csid,0,0,0,0,0,512,0,0);
-	end
-	
+    player:showText(npc,DROZGA_SHOP_DIALOG);
+
+    stock = {
+        12432,   1493,       -- Faceguard
+        12560,   2296,       -- Scale Mail
+        12688,   1225,       -- Scale Fng. Gnt.
+        12816,   1843,       -- Scale Cuisses
+        12944,   1117,       -- Scale Greaves
+        13192,    437,       -- Leather Belt
+        13327,   1287,       -- Silver Earring
+        13469,   1287,       -- Leather Ring
+    }
+    showShop(player, WINDURST, stock);
+
 end;
 
 -----------------------------------
@@ -41,10 +44,6 @@ end;
 function onEventUpdate(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-	
-	loca = option - 1073741829;
-	player:updateEvent(player:getGil(),OP_TeleFee(player,loca),player:getCP(),OP_TeleFee(player,loca));
-	
 end;
 
 -----------------------------------
@@ -54,11 +53,4 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-	
-	if(option >= 5 and option <= 23) then
-		if (player:delGil(OP_TeleFee(player,option-5))) then
-            toOutpost(player,option);
-        end
-	end
-	
 end;
