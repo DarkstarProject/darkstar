858e1bbb3019ad1712d6344b211980fa39030a1a
Apocalypse612 <Apocalypse612@yahoo.com>
2014-03-20 01:44:12 -0400

    SCRIPTS: Ugrihd Coin Exchange added (Thanks for help Maxtherabbit)

--- scripts/zones/Aht_Urhgan_Whitegate/TextIDs.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/TextIDs.lua
@@ -4,7 +4,7 @@
 ITEM_CANNOT_BE_OBTAINED = 213; -- You cannot obtain the item <item> come back again after sorting your inventory
 ITEM_CANNOT_BE_OBTAINEDX = 215; -- You cannot obtain the ≺Possible Special Code: 01 ... Try trading again after sorting your inventory.≺Prompt≻
           ITEM_OBTAINED = 216; -- Obtained: <item>
-		 ITEM_OBTAINEDX = 225; -- You obtain ≺Numeric Parameter 1≻ ≺Possible Special Code: 01≻
+         ITEM_OBTAINEDX = 225; -- You obtain ≺Numeric Parameter 1≻ ≺Possible Special Code: 01≻
            GIL_OBTAINED = 217; -- Obtained <number> gil
        KEYITEM_OBTAINED = 219; -- Obtained key item: <keyitem>
     NOT_HAVE_ENOUGH_GIL = 221; -- You do not have enough gil
@@ -22,26 +22,28 @@ MUSHAYRA_DIALOG = 4892; -- Sorry for all the trouble. Please ignore Hadahda the 
 
  -- Other Dialogs
 ITEM_DELIVERY_DIALOG = 9251; -- You have something you want delivered?
-		RUNIC_PORTAL = 4512; -- You cannot use the runic portal without the Empire's authorization.
+        RUNIC_PORTAL = 4512; -- You cannot use the runic portal without the Empire's authorization.
 IMAGE_SUPPORT_ACTIVE = 1335; -- You have to wait a bit longer before asking for synthesis image support again.
 
 -- Shop Texts
-	 GAVRIE_SHOP_DIALOG = 9165; -- Remember to take your medicine in small doses... Sometimes you can get a little too much of a good thing!
-	 MALFUD_SHOP_DIALOG = 9166; -- Welcome, welcome! Flavor your meals with Malfud's ingredients!
-	RUBAHAH_SHOP_DIALOG = 9167; -- Flour! Flooour! Corn! Rice and beans! Get your rice and beans here! If you're looking for grain, you've come to the right place!
-	MULNITH_SHOP_DIALOG = 9168; -- Drawn in by my shop's irresistible aroma, were you? How would you like some of the Near East's famous skewers to enjoy during your journeys?
-	SALUHWA_SHOP_DIALOG = 9169; -- Looking for undentable shields? This shop's got the best of 'em! These are absolute must-haves for a mercenary's dangerous work!
-	  DWAGO_SHOP_DIALOG = 9170; -- Buy your goods here...or you'll regret it!
-KULHAMARIYO_SHOP_DIALOG = 9171; -- Some fish to savorrr while you enjoy the sights of Aht Urhgan?
-KHAFJHIFANM_SHOP_DIALOG = 9172; -- How about a souvenir for back home? There's nothing like dried dates to remind you of good times in Al Zahbi!
-   HAGAKOFF_SHOP_DIALOG = 9173; -- Welcome! Fill all your destructive needs with my superb weaponry! No good mercenary goes without a good weapon!
-	 BAJAHB_SHOP_DIALOG = 9174; -- Good day! If you want to live long, you'll buy your armor here.
-	MAZWEEN_SHOP_DIALOG = 9175; -- Magic scrolls! Get your magic scrolls here!
-   FAYEEWAH_SHOP_DIALOG = 9176; -- Why not sit back a spell and enjoy the rich aroma and taste of a cup of chai?
-	 YAFAAF_SHOP_DIALOG = 9177; -- There's nothing like the mature taste and luxurious aroma of coffee... Would you like a cup?≺Prompt≻
+UGRIHD_PURCHASE_DIALOGUE = 4575; -- Salaheem's Sentinels values your contribution to the success of the company. | Please come again!
 
-	WAHRAGA_SHOP_DIALOG = 9179; -- Welcome to the Alchemists' Guild.
-	 WAHNID_SHOP_DIALOG = 9178; -- All the fishing gear you'll ever need, here in one place!
+      GAVRIE_SHOP_DIALOG = 9165; -- Remember to take your medicine in small doses... Sometimes you can get a little too much of a good thing!
+      MALFUD_SHOP_DIALOG = 9166; -- Welcome, welcome! Flavor your meals with Malfud's ingredients!
+     RUBAHAH_SHOP_DIALOG = 9167; -- Flour! Flooour! Corn! Rice and beans! Get your rice and beans here! If you're looking for grain, you've come to the right place!
+     MULNITH_SHOP_DIALOG = 9168; -- Drawn in by my shop's irresistible aroma, were you? How would you like some of the Near East's famous skewers to enjoy during your journeys?
+     SALUHWA_SHOP_DIALOG = 9169; -- Looking for undentable shields? This shop's got the best of 'em! These are absolute must-haves for a mercenary's dangerous work!
+       DWAGO_SHOP_DIALOG = 9170; -- Buy your goods here...or you'll regret it!
+ KULHAMARIYO_SHOP_DIALOG = 9171; -- Some fish to savorrr while you enjoy the sights of Aht Urhgan?
+ KHAFJHIFANM_SHOP_DIALOG = 9172; -- How about a souvenir for back home? There's nothing like dried dates to remind you of good times in Al Zahbi!
+    HAGAKOFF_SHOP_DIALOG = 9173; -- Welcome! Fill all your destructive needs with my superb weaponry! No good mercenary goes without a good weapon!
+      BAJAHB_SHOP_DIALOG = 9174; -- Good day! If you want to live long, you'll buy your armor here.
+     MAZWEEN_SHOP_DIALOG = 9175; -- Magic scrolls! Get your magic scrolls here!
+    FAYEEWAH_SHOP_DIALOG = 9176; -- Why not sit back a spell and enjoy the rich aroma and taste of a cup of chai?
+      YAFAAF_SHOP_DIALOG = 9177; -- There's nothing like the mature taste and luxurious aroma of coffee... Would you like a cup?≺Prompt≻
+ 
+     WAHRAGA_SHOP_DIALOG = 9179; -- Welcome to the Alchemists' Guild.
+      WAHNID_SHOP_DIALOG = 9178; -- All the fishing gear you'll ever need, here in one place!
 
 -- Automaton
       AUTOMATON_RENAME = 5749; -- Your automaton has a new name.≺Prompt≻
