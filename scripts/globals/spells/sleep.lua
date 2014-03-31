6e6521bf317cd830d1f5b4e6df44c4f5ebb4ba9c
maxtherabbit <alexgoolsby@gmail.com>
2014-03-30 22:13:14 -0400

    SCRIPTS: applyResistanceEffect to sleep and poison

--- scripts/globals/spells/sleep.lua
+++ scripts/globals/spells/sleep.lua
@@ -13,11 +13,11 @@ end;
 
 function onSpellCast(caster,target,spell)
 	local duration = 60;
-	local bonus = AffinityBonus(caster, spell:getElement());
+	--local bonus = AffinityBonus(caster, spell:getElement()); Removed: affinity bonus is added in applyResistance
 	local pINT = caster:getStat(MOD_INT);
 	local mINT = target:getStat(MOD_INT);
 	local dINT = (pINT - mINT);
-	local resm = applyResistance(caster,spell,target,dINT,ENFEEBLING_MAGIC_SKILL,bonus);
+	local resm = applyResistanceEffect(caster,spell,target,dINT,ENFEEBLING_MAGIC_SKILL,0,EFFECT_SLEEP_I);
 	if(resm < 0.5) then
 		spell:setMsg(85);--resist message
 		return EFFECT_SLEEP_I;
