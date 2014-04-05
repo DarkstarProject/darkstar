e31823a5c98f89b10d0ff00e86d976adfaca4fbc
maxtherabbit <alexgoolsby@gmail.com>
2014-04-05 00:51:05 -0400

    autohp, part deux

--- src/map/packets/zone_in.cpp
+++ src/map/packets/zone_in.cpp
@@ -180,7 +180,7 @@ CZoneInPacket::CZoneInPacket(CCharEntity * PChar, int16 csid)
 
 	WBUFL(data,(0xA0)-4) = PChar->GetPlayTime();				// время, проведенное персонажем в игре с момента создания
 
-	uint32 pktTime = CVanaTime::getInstance()->getVanaTime();
+	uint32 pktTime = CVanaTime::getInstance()->getVanaTime() + 2; //buffering this by 2 prevents autohp
 
     WBUFL(data,(0x38)-4) = pktTime + VTIME_BASEDATE;
     WBUFL(data,(0x3C)-4) = pktTime;
