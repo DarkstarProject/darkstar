f9d4b8ade216f50e7b8e88669d1dea6fce7e45c1
TeoTwawki <arawn.annwn@yandex.com>
2014-04-09 16:40:48 -0400

    Unborked warcry.

--- scripts/globals/abilities/warcry.lua
+++ scripts/globals/abilities/warcry.lua
@@ -13,7 +13,7 @@ function OnAbilityCheck(player,target,ability)
 end;
 
 function OnUseAbility(player, target, ability)
-    local merit = target:getMerit(MERIT_SAVAGERY);
+    local merit = player:getMerit(MERIT_SAVAGERY);
     local power = 0;
     local duration = 30;
 
@@ -27,5 +27,5 @@ function OnUseAbility(player, target, ability)
     duration = duration + player:getMod(MOD_WARCRY_DURATION);
 
 
-    player:addStatusEffect(EFFECT_WARCRY,power,0,duration,0,merit);
+    target:addStatusEffect(EFFECT_WARCRY,power,0,duration,0,merit);
 end;
