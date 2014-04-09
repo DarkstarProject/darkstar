fae039793eb8a55edddbf5941c2eea4f31d0671b
starlightknight <starlightknight7@gmail.com>
2014-04-08 23:31:24 -0400

    April Update: Text ID Updates for Most Major Cities
    Aligns with new client 30140402_1
    
    Updates
    All San'dOria Zones
    All Jeuno Zones
    All Bastok Zones
    All Windurst Zones
    Selbina
    Mhaura
    Norg
    Whitegate / Al Zhabi

--- scripts/zones/Selbina/TextIDs.lua
+++ scripts/zones/Selbina/TextIDs.lua
@@ -6,23 +6,23 @@ ITEM_CANNOT_BE_OBTAINED = 6376; -- You cannot obtain the item <item>. Come back 
            GIL_OBTAINED = 6380; -- Obtained <number> gil.
        KEYITEM_OBTAINED = 6382; -- Obtained key item: <keyitem>.
     NOT_HAVE_ENOUGH_GIL = 6384; -- You do not have enough gil.
-          HOMEPOINT_SET = 6445; -- Home point set!
- FISHING_MESSAGE_OFFSET = 6504; -- You can't fish here.
-        SUBJOB_UNLOCKED = 6798; -- You can now designate a support job.
+          HOMEPOINT_SET = 6450; -- Home point set!
+ FISHING_MESSAGE_OFFSET = 6509; -- You can't fish here.
+        SUBJOB_UNLOCKED = 6805; -- You can now designate a support job.
 
 -- NPC texts
-NOMAD_MOOGLE_DIALOG = 6596; -- I'm a traveling moogle, kupo. I help adventurers in the Outlands access items they have stored in a Mog House elsewhere, kupo.
+NOMAD_MOOGLE_DIALOG = 6603; -- I'm a traveling moogle, kupo. I help adventurers in the Outlands access items they have stored in a Mog House elsewhere, kupo.
 
 -- Shop Texts
-  HERMINIA_SHOP_DIALOG = 6972; -- Hello there. What can I do for you?
- TORAPIONT_SHOP_DIALOG = 6973; -- Arm yourself before you step outside.
-  DOHDJUMA_SHOP_DIALOG = 6974; -- I'm Dohdjuma, and I sell all kinds of things.
-CLOTHCRAFT_SHOP_DIALOG = 6975; -- Welcome to the Weavers' Guild salesroom.
-   FISHING_SHOP_DIALOG = 6976; -- Welcome to the Fishermen's Guild salesroom.
-   QUELPIA_SHOP_DIALOG = 6977; -- In need of otherworldly protection?
-CHUTARMIRE_SHOP_DIALOG = 6978; -- I have items for those who delve in the black arts!
-   FALGIMA_SHOP_DIALOG = 6979; -- In the market for spells Hexes and inantations?
+  HERMINIA_SHOP_DIALOG = 6979; -- Hello there. What can I do for you?
+ TORAPIONT_SHOP_DIALOG = 6980; -- Arm yourself before you step outside.
+  DOHDJUMA_SHOP_DIALOG = 6981; -- I'm Dohdjuma, and I sell all kinds of things.
+CLOTHCRAFT_SHOP_DIALOG = 6982; -- Welcome to the Weavers' Guild salesroom.
+   FISHING_SHOP_DIALOG = 6983; -- Welcome to the Fishermen's Guild salesroom.
+   QUELPIA_SHOP_DIALOG = 6984; -- In need of otherworldly protection?
+CHUTARMIRE_SHOP_DIALOG = 6985; -- I have items for those who delve in the black arts!
+   FALGIMA_SHOP_DIALOG = 6986; -- In the market for spells Hexes and inantations?
 
 -- Item Delivery
-WENZEL_DELIVERY_DIALOG = 7526; -- My independent survey confirms the town entrance as the preferred location from which adventurers send parcels.
- BORIS_DELIVERY_DIALOG = 7527; -- My independent survey confirms the inn as the preferred location from which adventurers send parcels.
+WENZEL_DELIVERY_DIALOG = 7533; -- My independent survey confirms the town entrance as the preferred location from which adventurers send parcels.
+ BORIS_DELIVERY_DIALOG = 7534; -- My independent survey confirms the inn as the preferred location from which adventurers send parcels.
