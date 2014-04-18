148b41bdf2e6f068d991cc5ce64a3f3e4763a4ee
linkandentity <linkandentity@gmail.com>
2014-04-18 19:00:18 +0100

    CORE: Corrected issues with the setMobPos function in lua/luautils.cpp

--- src/map/lua/luautils.cpp
+++ src/map/lua/luautils.cpp
@@ -726,31 +726,36 @@ int32 DespawnMob(lua_State* L)
 
 int32 setMobPos(lua_State *L)
 {
-	if( !lua_isnil(L,1) && lua_isnumber(L,1) )
+	if (!lua_isnil(L, 1) && lua_isnumber(L, 1))
 	{
-		uint32 mobid = (uint32)lua_tointeger(L,1);
+		uint32 mobid = (uint32)lua_tointeger(L, 1);
 
-        CMobEntity* PMob = (CMobEntity*)zoneutils::GetEntity(mobid, TYPE_MOB);
-        if (PMob != NULL)
-        {
+		CMobEntity* PMob = (CMobEntity*)zoneutils::GetEntity(mobid, TYPE_MOB);
+		if (PMob != NULL)
+		{
 			//if mob is in battle, do not warp it
-			if (PMob->m_OwnerID.id == 0 && PMob->PBattleAI->GetCurrentAction() != ACTION_ATTACK)
+			if (PMob->PBattleAI->GetCurrentAction() == ACTION_NONE ||
+				PMob->PBattleAI->GetCurrentAction() == ACTION_SPAWN ||
+				PMob->PBattleAI->GetCurrentAction() == ACTION_ROAMING ||
+				PMob->PBattleAI->GetCurrentAction() == ACTION_DEATH ||
+				PMob->PBattleAI->GetCurrentAction() == ACTION_FADE_OUT ||
+				PMob->PBattleAI->GetCurrentAction() == ACTION_DESPAWN)
 			{
-				if( !lua_isnil(L,2) && lua_isnumber(L,2) )
-					PMob->loc.p.x = (float) lua_tonumber(L,2);
+				if (!lua_isnil(L, 2) && lua_isnumber(L, 2))
+					PMob->loc.p.x = (float)lua_tonumber(L, 2);
 
-				if( !lua_isnil(L,3) && lua_isnumber(L,3) )
-					PMob->loc.p.y = (float) lua_tonumber(L,3);
+				if (!lua_isnil(L, 3) && lua_isnumber(L, 3))
+					PMob->loc.p.y = (float)lua_tonumber(L, 3);
 
-				if( !lua_isnil(L,4) && lua_isnumber(L,4) )
-					PMob->loc.p.z = (float) lua_tonumber(L,4);
+				if (!lua_isnil(L, 4) && lua_isnumber(L, 4))
+					PMob->loc.p.z = (float)lua_tonumber(L, 4);
 
-				if( !lua_isnil(L,5) && lua_isnumber(L,5) )
-					PMob->loc.p.rotation = (uint8) lua_tointeger(L,5);
-            }
+				if (!lua_isnil(L, 5) && lua_isnumber(L, 5))
+					PMob->loc.p.rotation = (uint8)lua_tointeger(L, 5);
+			}
 			else
 			{
-                ShowDebug(CL_CYAN"setMobPos: <%s> is currently in battle, will not warp it!\n" CL_RESET, PMob->GetName());
+				ShowDebug(CL_CYAN"setMobPos: <%s> is currently in battle, will not warp it!\n" CL_RESET, PMob->GetName());
 				return 1;
 			}
 		}
