6af189268567c691dcc5d6cc30632db987e6f531
Vanion- <andrew.connal.brown@gmail.com>
2014-04-20 17:41:00 -0700

    Update Heliodromos.lua
    fix for 10 minute depop on heliodromos

--- scripts/zones/Riverne-Site_A01/mobs/Heliodromos.lua
+++ scripts/zones/Riverne-Site_A01/mobs/Heliodromos.lua
@@ -21,13 +21,8 @@ function OnMobRoam(mob)
 		16900109
 	};
 	local Heliodromos_Despawn = GetServerVariable("Heliodromos_Despawn");
-	local Despawn_Time = 0;
 	
-	if ( Heliodromos_Despawn > 0) then
-		Despawn_Time = os.time() + 600;
-	end
-	
-	if (Despawn_Time <= os.time() and Despawn_Time > 0) then
+	if (Heliodromos_Despawn > 0 and Heliodromos_Despawn <= os.time()) then
 		for i=1, table.getn(Heliodromos_Table), 1 do
 			if (Heliodromos_PH_Table[i] ~= nil) then
 				if (GetMobAction(Heliodromos_PH_Table[i]) == 0) then
@@ -38,7 +33,6 @@ function OnMobRoam(mob)
 				end
 			end
 		end
-		Despawn_Time = 0;
 	end
 	
 	
@@ -66,17 +60,18 @@ function onMobDeath(mob, killer)
 		16900108,
 		16900109
 	};
-	local Heliodromos_Despawn = GetServerVariable("Heliodromos_Despawn");
-	
 	
 	for i = 1, table.getn(Heliodromos_Table), 1 do
 		if (Heliodromos_Table[i] ~= nil) then
 			if (Heliodromos == Heliodromos_Table[i]) then
 				SetServerVariable("Heliodromos_ToD", (os.time() + math.random((43200), (54000))));
-				SetServerVariable("Heliodromos_Despawn", Heliodromos_Despawn + 1);
 				DeterMob(Heliodromos_PH_Table[i], false);
 				SpawnMob(Heliodromos_PH_Table[i], "", GetMobRespawnTime(Heliodromos_PH_Table[i]));
 			end
 		end
+	end
+	
+	if (GetServerVariable("Heliodromos_Despawn") == 0) then
+		SetServerVariable("Heliodromos_Despawn", os.time() + 600);
 	end
 end;
