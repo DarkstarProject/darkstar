df7c3369184606df48650a933abef9c16467342e
teschnei <teschnei@gmail.com>
2014-05-30 03:06:03 -0600

    CORE: temp items buff to 50

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -463,7 +463,7 @@ void LoadChar(CCharEntity* PChar)
 	{
 		PChar->getStorage(LOC_INVENTORY)->AddBuff((uint8)Sql_GetIntData(SqlHandle,0));
 		PChar->getStorage(LOC_MOGSAFE)->AddBuff((uint8)Sql_GetIntData(SqlHandle,1));
-		PChar->getStorage(LOC_TEMPITEMS)->AddBuff(18);
+		PChar->getStorage(LOC_TEMPITEMS)->AddBuff(50);
 		PChar->getStorage(LOC_MOGLOCKER)->AddBuff((uint8)Sql_GetIntData(SqlHandle,2));
 		PChar->getStorage(LOC_MOGSATCHEL)->AddBuff((uint8)Sql_GetIntData(SqlHandle,3));
 		PChar->getStorage(LOC_MOGSACK)->AddBuff((uint8)Sql_GetIntData(SqlHandle,4));
