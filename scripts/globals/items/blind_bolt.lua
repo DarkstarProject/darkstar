256bd664c06dbd8f712a7df69b75fa942a177d73
MarkWaldron <markewaldron@gmail.com>
2014-06-09 12:29:06 -0400

    Migrate blind bolt effect from battleutils to lua
    Removed case for blind bolt in battleutils
    Added it to scripts\global\items

---
+++ scripts/globals/items/blind_bolt.lua
@@ -0,0 +1,27 @@
+-----------------------------------------
+-- ID: 18150
+-- Item: Blind Bolt
+-- Additional Effect: Blindness
+-----------------------------------------
+require("scripts/globals/status");
+require("scripts/globals/magic");
+
+-----------------------------------
+-- onAdditionalEffect Action
+-----------------------------------
+function onAdditionalEffect(player,target,damage)
+    local chance = 95;
+    if (target:getMainLvl() > player:getMainLvl()) then
+        chance = chance - 5 * (target:getMainLvl() - player:getMainLvl())
+        chance = utils.clamp(chance, 5, 95);
+    end
+    if (math.random(0,99) >= chance or applyResistanceAddEffect(player,target,ELE_DARK,0) <= 0.5) then
+        return 0,0,0;
+    else
+        target:delStatusEffect(EFFECT_BLINDNESS)
+        if (not target:hasStatusEffect(EFFECT_BLINDNESS)) then
+            target:addStatusEffect(EFFECT_BLINDNESS, 10, 0, 30);
+        end
+        return SUBEFFECT_BLIND, 160, EFFECT_BLINDNESS;
+    end
+end;
