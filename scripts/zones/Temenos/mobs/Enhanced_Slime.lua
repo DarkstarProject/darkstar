68139a7d1c27de7e7bc981dea800ff8e9c00461f
cantsee <michaelj29@gmail.com>
2014-07-02 16:14:46 -0600

    Update GateID

--- scripts/zones/Temenos/mobs/Enhanced_Slime.lua
+++ scripts/zones/Temenos/mobs/Enhanced_Slime.lua
@@ -32,7 +32,7 @@ local cofferID=Randomcoffer(5,GetInstanceRegion(1298));
     local mobX = mob:getXPos();
 	local mobY = mob:getYPos();
 	local mobZ = mob:getZPos();
-	GetNPCByID(16928768+468):setStatus(STATUS_NORMAL);
+	GetNPCByID(16928770+468):setStatus(STATUS_NORMAL);
   if(cofferID~=0)then
    	GetNPCByID(16928768+cofferID):setPos(mobX,mobY,mobZ);
 	GetNPCByID(16928768+cofferID):setStatus(STATUS_NORMAL);
