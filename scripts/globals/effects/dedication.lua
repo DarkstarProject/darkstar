96eee522c45939376a49cb0b1164447754bedc9e
MarkWaldron <markewaldron@gmail.com>
2014-07-09 21:27:14 -0400

    Revert "Core: Fix dedication loss on log off"
    This reverts commit b9b83c704f2d0b0875cf09a9290433ffe5ef66f1.

--- scripts/globals/effects/dedication.lua
+++ scripts/globals/effects/dedication.lua
@@ -11,6 +11,7 @@ require("scripts/globals/status");
 -----------------------------------
 
 function onEffectGain(target,effect)
+	target:addMod(MOD_DEDICATION, effect:getPower(), effect:getSubPower());
 end;
 
 -----------------------------------
@@ -25,4 +26,5 @@ end;
 -----------------------------------
 
 function onEffectLose(target,effect)
+	target:delMod(MOD_DEDICATION, effect:getPower(), effect:getSubPower());
 end;
--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -4186,19 +4186,21 @@ void SaveCharPoints(CCharEntity* PChar)
 uint32  AddExpBonus(CCharEntity* PChar, uint32 exp)
 {
     int32 bonus = 0;
-    if (PChar->StatusEffectContainer->GetStatusEffect(EFFECT_DEDICATION))
+    if (PChar->getMod(MOD_DEDICATION))
     {
-    	CStatusEffect* dedication = PChar->StatusEffectContainer->GetStatusEffect(EFFECT_DEDICATION);
-        int16 percentage = dedication->GetPower();
-        int16 cap = dedication->GetSubPower();        
-        bonus = exp * percentage / 100;
-        dedication->SetSubPower(cap -= bonus);
+        int16 percentage = PChar->getMod(MOD_DEDICATION);
+        int16 cap = PChar->getSubPower(MOD_DEDICATION);
 
-        if (cap <= 0)
+        int16 dedication = dsp_cap(exp * PChar->getMod(MOD_DEDICATION) / 100, 0, PChar->getSubPower(MOD_DEDICATION));
+
+        PChar->setSubPower(MOD_DEDICATION, PChar->getSubPower(MOD_DEDICATION) - dedication);
+
+        if (PChar->getSubPower(MOD_DEDICATION) == 0)
         {
             PChar->StatusEffectContainer->DelStatusEffect(EFFECT_DEDICATION);
         }
 
+        bonus = dedication;
     }
 
     bonus += exp * (PChar->getMod(MOD_EXP_BONUS) / 100.0f);
