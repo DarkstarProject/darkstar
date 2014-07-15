d4b39c55bb9e82def04e325d557d186b55481a34
TeoTwawki <arawn.annwn@yandex.com>
2014-07-15 04:02:01 -0400

    Improved retail-ness of recent NM additions.

--- scripts/zones/Cape_Teriggan/mobs/Tegmine.lua
+++ scripts/zones/Cape_Teriggan/mobs/Tegmine.lua
@@ -20,29 +20,42 @@ end;
 -----------------------------------
 
 function onAdditionalEffect(mob,target,damage)
-    local diff = mob:getStat(MOD_INT) - target:getStat(MOD_INT);
+    -- Wiki says nothing about proc rate, going with 80% for now.
+    -- I remember it going off every hit when I fought him.
+    local chance = 80;
+    local LV_diff = target:getMainLvl() - mob:getMainLvl();
 
-    if (diff > 20) then
-        diff = 20 + (diff - 20) / 2;
+    if (target:getMainLvl() > mob:getMainLvl()) then
+        chance = chance - 5 * LV_diff;
+        chance = utils.clamp(chance, 5, 95);
     end
 
-    local dmg_base = diff + (mob:getMainLvl() - target:getMainLvl()) + damage/2;
-    local dmg_rand = math.random(69,96);
-    local dmg = (dmg_base + dmg_rand)/2;
-    local params = {};
-    params.bonusmab = 0;
-    params.includemab = false;
-    dmg = addBonusesAbility(mob, ELE_WATER, target, dmg, params);
-    dmg = dmg * applyResistanceAddEffect(mob,target,ELE_WATER,0);
-    dmg = adjustForTarget(target,dmg,ELE_WATER);
-	dmg = finalMagicNonSpellAdjustments(mob,target,ELE_WATER,dmg);
-    
-    local message = 163;
-    if (dmg < 0) then
-        message = 167;
+    if (math.random(0,99) >= chance) then
+        return 0,0,0;
+    else
+        local INT_diff = mob:getStat(MOD_INT) - target:getStat(MOD_INT);
+
+        if (INT_diff > 20) then
+            INT_diff = 20 + (INT_diff - 20) / 2;
+        end
+
+        local dmg = INT_diff+LV_diff+damage/2;
+        local params = {};
+        params.bonusmab = 0;
+        params.includemab = false;
+        dmg = addBonusesAbility(mob, ELE_WATER, target, dmg, params);
+        dmg = dmg * applyResistanceAddEffect(mob,target,ELE_WATER,0);
+        dmg = adjustForTarget(target,dmg,ELE_WATER);
+
+        if (dmg < 0) then
+            dmg = 10
+        end
+        
+        dmg = finalMagicNonSpellAdjustments(mob,target,ELE_WATER,dmg);
+
+        return SUBEFFECT_WATER_DAMAGE,163,dmg;
     end
-    
-    return SUBEFFECT_WATER_DAMAGE,message,dmg;
+
 end;
 
 -----------------------------------
