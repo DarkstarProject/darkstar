7034c62ad65bbf73e4cd69e8a366ff8ad9b11fa5
atom0s <atom0s@live.com>
2014-08-13 01:15:44 -0700

    CORE: Adjusted party_member_update packet to properly align with the new update.

--- src/map/packets/party_member_update.cpp
+++ src/map/packets/party_member_update.cpp
@@ -1,22 +1,22 @@
 /*
 ===========================================================================
 
-  Copyright (c) 2010-2014 Darkstar Dev Teams
+Copyright (c) 2010-2014 Darkstar Dev Teams
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see http://www.gnu.org/licenses/
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
 
-  This file is part of DarkStar-server source code.
+This file is part of DarkStar-server source code.
 
 ===========================================================================
 */
@@ -31,37 +31,34 @@
 #include "../alliance.h"
 #include "../party.h"
 
-CPartyMemberUpdatePacket::CPartyMemberUpdatePacket(CCharEntity* PChar, uint8 MemberNumber, uint16 ZoneID) 
-{	
+CPartyMemberUpdatePacket::CPartyMemberUpdatePacket(CCharEntity* PChar, uint8 MemberNumber, uint16 ZoneID)
+{
+    this->type = 0xDD;
+    this->size = 0x20;
 
-		this->type = 0xDD;
-		this->size = 0x20;	
+    DSP_DEBUG_BREAK_IF(PChar == NULL);
 
+    WBUFL(data, (0x04) - 4) = PChar->id;
 
-		DSP_DEBUG_BREAK_IF(PChar == NULL);
+    if (PChar->PParty != NULL)
+    {
+        WBUFW(data, (0x14) - 4) = PChar->PParty->GetMemberFlags(PChar);
+    }
 
-		WBUFL(data,(0x04)-4) = PChar->id;
+    if (PChar->getZone() != ZoneID)
+    {
+        WBUFW(data, (0x20) - 4) = PChar->getZone();
+    }
+    else
+    {
+        WBUFL(data, (0x08) - 4) = PChar->health.hp;
+        WBUFL(data, (0x0C) - 4) = PChar->health.mp;
+        WBUFW(data, (0x10) - 4) = PChar->health.tp;
+        WBUFW(data, (0x18) - 4) = PChar->targid;
+        WBUFB(data, (0x1A) - 4) = MemberNumber;
+        WBUFB(data, (0x1D) - 4) = PChar->GetHPP();
+        WBUFB(data, (0x1E) - 4) = PChar->GetMPP();
+    }
 
-		if (PChar->PParty != NULL)
-		{
-			WBUFW(data,(0x14)-4) = PChar->PParty->GetMemberFlags(PChar);
-		}
-		if (PChar->getZone() != ZoneID) 
-		{
-			WBUFW(data,(0x1F)-4) = PChar->getZone();
-		} 
-		else
-		{
-			WBUFL(data,(0x08)-4) = PChar->health.hp;
-			WBUFL(data,(0x0C)-4) = PChar->health.mp; 
-			WBUFW(data,(0x10)-4) = PChar->health.tp;
-			WBUFW(data,(0x18)-4) = PChar->targid;
-			WBUFB(data,(0x1D)-4) = PChar->GetHPP(); 
-			WBUFB(data,(0x1E)-4) = PChar->GetMPP();
-			WBUFB(data,(0x1A)-4) = MemberNumber;
-		}
-		memcpy(data+(0x22)-4, PChar->GetName(), PChar->name.size()); 
-			
+    memcpy(data + (0x26) - 4, PChar->GetName(), PChar->name.size());
 }
-
-
