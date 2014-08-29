4e82d71dd5884cb2da5f7ce7cbeac3ccc02ffa50
Jari Vetoniemi <mailroxas@gmail.com>
2014-08-30 01:26:59 +0300

    Implement Warp Ring, Homing Ring and Return Ring

---
+++ scripts/globals/items/return_ring.lua
@@ -0,0 +1,30 @@
+-----------------------------------------
+--	ID: 15542
+--	Teleport Return Ring
+--  Enchantment: "Outpost Warp"
+-----------------------------------------
+
+require("scripts/globals/status");
+require("scripts/globals/conquest");
+require("scripts/globals/teleports");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+   local result = 0;
+   local region = target:getCurrentRegion()
+   if (hasOutpost(target, target:getCurrentRegion()) == 0 or GetRegionOwner(region) ~= target:getNation()) then
+	result = MSGBASIC_CANT_BE_USED_IN_AREA;
+   end
+   return result;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+   target:addStatusEffectEx(EFFECT_TELEPORT,0,TELEPORT_HOMING,0,1);
+end;
