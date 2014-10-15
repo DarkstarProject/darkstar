829a499df6e3a6843a60d32a1389e186e63bd626
Bope <moohoo42@gmail.com>
2014-10-14 23:12:59 -0400

    HP crystal typo and readd registration message

--- scripts/zones/Bastok_Markets/npcs/HomePoint#3.lua
+++ scripts/zones/Bastok_Markets/npcs/HomePoint#3.lua
@@ -27,7 +27,7 @@ function onTrigger(player,npc)
 	end
 	if(HOMEPOINT_TELEPORT == 1)then
 		--                       ?/1-Ru'lude5 /Lude-Ru'Aun/Tav-end/     ?/Gil /Expantion level/Registered   
-		player:startEvent(0x21fe,addtohps(player,1,13),player:getVar("hpmask1"),player:getVar("hpmask2"),player:getVar("hpmask3"),player:getVar("hpmask4"),player:getGil(),4095,13);
+		player:startEvent(0x21fe,0,player:getVar("hpmask1"),player:getVar("hpmask2"),player:getVar("hpmask3"),player:getVar("hpmask4"),player:getGil(),4095,13 + addtohps(player,1,13));
 	else
 		player:startEvent(0x21fe)
 	end
