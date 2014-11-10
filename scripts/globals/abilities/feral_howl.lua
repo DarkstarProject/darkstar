ea735de4a7453d2996d76c732311db23711f8080
maadgamer <maad.gamer@verizon.net>
2014-11-10 01:54:17 -0500

    Update feral_howl.lua
    changed to reflect new MOD submission in status.lua and modifier.h
    added 5% increase in accuracy per merit 
    correctly calls for feral howl modifier from augmented Monster Jackcoat +2
    still needs tweaking to make it match retail accuracy.

--- scripts/globals/abilities/feral_howl.lua
+++ scripts/globals/abilities/feral_howl.lua
@@ -17,18 +17,18 @@ end;
 function OnUseAbility(player, target, ability)
 	merits = player:getMerit(MERIT_FERAL_HOWL);
 	--printf("merits : %u",merits);
-	body = player:getEquipID(SLOT_BODY);
-	--printf("body : %u",body);
+	feralHowlMod = player:getMod(MOD_FERAL_HOWL_DURATION);
+	--printf("feralHowlMod : %u",feralHowlMod);
     if target:hasStatusEffect(EFFECT_TERROR) == true or target:hasStatusEffect(EFFECT_STUN) == true then -- effect already on, or target stunned, do nothing
---        spell:setMsg(75);
+    -- reserved for miss based on target already having stun or terror effect active
     else
         -- Calculate duration.
         duration = 10;
 		--printf("Duration : %u",duration);
-		if body == 10678 then
+		if feralHowlMod >= 1 then
 			-- http://wiki.ffxiclopedia.org/wiki/Monster_Jackcoat_(Augmented)_%2B2
 			-- add 20% duration per merit level if wearing Augmented Monster Jackcoat +2
-			duration = (duration + (duration * merit * 0.2));
+			duration = (duration + (duration * merits * 0.2));
 			--printf("Duration post merit : %u",duration);
 		end
     end
@@ -48,25 +48,26 @@ function OnUseAbility(player, target, ability)
 	--printf("level difference : %u",dLvl);
 
 	-- Determining what level of resistance the target will have to the ability
+	dLvl = (2 * dLvl) - merits; -- merits increase accuracy by 5% per level
 	if dLvl <= 0 then -- default level difference to 1 if mob is equal to the Beastmaster's level or less.
-		resist = 1 
+		resist = 1;
 	--printf("resist : %u",resist);
 	else
-	resist = math.random(1,(dLvl + 10)); -- calculate chance of missing based on number of levels mob is higher than you. Target gets 10% resist per level over BST
+	resist = math.random(1,(dLvl + 20)); -- calculate chance of missing based on number of levels mob is higher than you. Target gets 10% resist per level over BST
 	--printf("resist : %u",resist);
 	end
 
 	-- Adjusting duration based on resistance. Only fair way I could see to do it...
 	if resist >= 2 then
 		if resist >= (duration) then
-			resist = (duration - 2)
+			resist = (duration - 2);
 		end
 		duration = (duration - math.random(1,resist));
 		--printf("Duration post resist : %u",duration);
 	end
 	
 	-- execute ability based off of resistance value; space reserved for resist message
-    if resist <= 9 then -- still experimental. not exactly sure how to calculate hit %
+    if resist <= 18 then -- still experimental. not exactly sure how to calculate hit %
         target:addStatusEffect(EFFECT_TERROR,potency,0,duration);
     else
         -- reserved for text related to resist
