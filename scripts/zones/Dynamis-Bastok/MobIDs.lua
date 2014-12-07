eced170444f4494bc4482ad738523fd51000a40d
Tyler Schneider <teschnei@gmail.com>
2014-12-07 00:36:04 -0700

    Revert "Fixed dynamis-bastok's megaboss spawn"

--- scripts/zones/Dynamis-Bastok/MobIDs.lua
+++
@@ -1,7 +0,0 @@
------------------------------------
---  Area: Dynamis-Bastok (186)
--- (Taken from 'mob_spawn_points' table)
------------------------------------
-
--- GU_DHA_EFFIGY
-GU_DHA_EFFIGY=17539073;
