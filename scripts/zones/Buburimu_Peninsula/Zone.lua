5eda1878273419db32cb3e289a7a3d0b3494004b
Novah <novah1@gmail.com>
2014-12-09 16:24:40 -0500

    fixed duplicate manual

--- scripts/zones/Buburimu_Peninsula/Zone.lua
+++ scripts/zones/Buburimu_Peninsula/Zone.lua
@@ -16,7 +16,7 @@ require("scripts/globals/conquest");
 -----------------------------------
 
 function onInitialize(zone)
-    local manuals = {17261198,17261198};
+    local manuals = {17261198,17261199};
     
     SetFieldManual(manuals);
     
