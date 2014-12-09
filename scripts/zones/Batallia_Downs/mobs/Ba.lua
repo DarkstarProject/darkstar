8aaa086e175093ada32fae4e832a5b4876fa5485
TeoTwawki <arawn.annwn@yandex.com>
2014-12-09 02:29:33 -0500

    Fixed 2 busted FoV pages in Batallia Downs

--- scripts/zones/Batallia_Downs/mobs/Ba.lua
+++ scripts/zones/Batallia_Downs/mobs/Ba.lua
@@ -1,15 +1,15 @@
------------------------------------	
--- Area: Batallia Downs	
--- MOB:  Ba	
------------------------------------	
-	
-require("/scripts/globals/fieldsofvalor");	
-	
------------------------------------	
--- onMobDeath	
------------------------------------	
-	
-function onMobDeath(mob,killer)	
+-----------------------------------
+-- Area: Batallia Downs
+-- MOB:  Ba
+-----------------------------------
+
+require("/scripts/globals/fieldsofvalor");
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob,killer)
 	checkRegime(killer,mob,15,2);
 	checkRegime(killer,mob,73,2);
-end;	
+end;
