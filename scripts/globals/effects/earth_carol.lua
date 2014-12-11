05401e3850b7597b02e565029e176e128444b895
korietsu <spht.lhar@gmail.com>
2014-12-11 17:41:13 -0600

    Add Bard Carol Effects
    Added Bard Carol Effects, used barspells as template.

---
+++ scripts/globals/effects/earth_carol.lua
@@ -0,0 +1,30 @@
+-----------------------------------
+-- Earth Carol
+-- Increased Earth Resistance
+--    
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onEffectGain Action
+-----------------------------------
+
+function onEffectGain(target,effect)
+	target:addMod(MOD_EARTHRES,effect:getPower());
+end;
+
+-----------------------------------
+-- onEffectTick Action
+-----------------------------------
+
+function onEffectTick(target,effect)
+end;
+
+-----------------------------------
+-- onEffectLose Action
+-----------------------------------
+
+function onEffectLose(target,effect)
+	target:delMod(MOD_EARTHRES,effect:getPower());
+end;
---
+++ scripts/globals/effects/wind_carol.lua
@@ -0,0 +1,30 @@
+-----------------------------------
+-- Wind Carol
+-- Increased Wind Resistance
+--    
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onEffectGain Action
+-----------------------------------
+
+function onEffectGain(target,effect)
+	target:addMod(MOD_WINDRES,effect:getPower());
+end;
+
+-----------------------------------
+-- onEffectTick Action
+-----------------------------------
+
+function onEffectTick(target,effect)
+end;
+
+-----------------------------------
+-- onEffectLose Action
+-----------------------------------
+
+function onEffectLose(target,effect)
+	target:delMod(MOD_WINDRES,effect:getPower());
+end;
