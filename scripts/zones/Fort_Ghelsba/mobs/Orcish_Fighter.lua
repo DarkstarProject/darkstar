e899de62e53af558aa0940c7fb6da1220440e666
TeoTwawki <arawn.annwn@yandex.com>
2014-12-12 05:23:08 -0500

    Implemented Chariotbuster Byakzak
    Corrected his level and gave him his 2hr ability.
    
    Had to create an extra group of an Orcish Fighter because it needed a diff spawntype than the others in same zone to be retail.
    
    I am uncertain of wiki's accuracy on this NM saying its 21-24hr, seems more likely it'd be a % chance of NM per panzer kill, but going with "what wiki says" till we have reason not to.

---
+++ scripts/zones/Fort_Ghelsba/mobs/Orcish_Fighter.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+-- Area: Fort Ghelsba
+-- MOB:  Orcish Fighter
+-----------------------------------
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob,killer)
+    local OrcPanzer = 17354894;
+    local OrcFighter = 17354895;
+    -- Making sure we've got the correct Orcish Fighter here, as others exist in zone.
+    if (mob:getId() == OrcFighter) then
+        -- Set Orcish Panzer's respawn time to be 60-70 min from now.
+        GetMobByID(OrcPanzer):setRespawnTime(math.random((3600),(4200)));
+    end
+end;
+
+-----------------------------------
+-- onMobDespawn
+-----------------------------------
+
+function OnMobDespawn(mob)
+    local OrcPanzer = 17354894;
+    -- Set Orcish Panzer's respawn time to be 60-70 min from now.
+    GetMobByID(OrcPanzer):setRespawnTime(math.random((3600),(4200)));
+end;
