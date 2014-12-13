c2146a8fcaa66bd8f10cdba942e3777ddc0654d4
korietsu <spht.lhar@gmail.com>
2014-12-13 02:47:09 -0600

    Add Ability for Plasma Charge and Asuran Claws

---
+++ scripts/zones/Abyssea-La_Theine/mobs/Lusion.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: Abyssea - La Theine
+-- NPC:  Luison
+-----------------------------------
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobEngaged
+-- Change forms every 60 seconds
+-----------------------------------
+
+function onMobEngaged(mob,target)	
+	local changeTime = mob:getLocalVar("changeTime");
+	
+	if(mob:AnimationSub() == 0 and mob:getBattleTime() - changeTime > 60) then
+		mob:AnimationSub(1);
+		mob:setLocalVar("changeTime", mob:getBattleTime());
+	elseif(mob:AnimationSub() == 1 and mob:getBattleTime() - changeTime > 60) then
+		mob:AnimationSub(0);
+		mob:setLocalVar("changeTime", mob:getBattleTime());
+	end
+end;
---
+++ scripts/zones/La_Vaule_[S]/mobs/Lobison.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: La Vaule (S)
+-- NPC:  Lobison
+-----------------------------------
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobEngaged
+-- Change forms every 60 seconds
+-----------------------------------
+
+function onMobEngaged(mob,target)	
+	local changeTime = mob:getLocalVar("changeTime");
+	
+	if(mob:AnimationSub() == 0 and mob:getBattleTime() - changeTime > 60) then
+		mob:AnimationSub(1);
+		mob:setLocalVar("changeTime", mob:getBattleTime());
+	elseif(mob:AnimationSub() == 1 and mob:getBattleTime() - changeTime > 60) then
+		mob:AnimationSub(0);
+		mob:setLocalVar("changeTime", mob:getBattleTime());
+	end
+end;
