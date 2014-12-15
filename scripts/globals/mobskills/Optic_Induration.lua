7f51fcb098269c0a871603f5ef689e056778158d
korietsu <spht.lhar@gmail.com>
2014-12-14 21:25:10 -0600

    Zdei Patch 1
    Added all except core meltdown, added aw_zdei v 1

--- scripts/globals/mobskills/Optic_Induration.lua
+++ scripts/globals/mobskills/Optic_Induration.lua
@@ -10,22 +10,29 @@
 require("/scripts/globals/settings");
 require("/scripts/globals/status");
 require("/scripts/globals/monstertpmoves");
-
 ---------------------------------------------
+
 function OnMobSkillCheck(target,mob,skill)
+	if(mob:AnimationSub() == 2 or mob:AnimationSub() == 3) then
+		return 1;
+	end
 	return 0;
 end;
 
+
+
 function OnMobWeaponSkill(target, mob, skill)
 	local typeEffect = EFFECT_PETRIFICATION;
-
+	
 	MobStatusEffectMove(mob, target, typeEffect, 1, 0, 60);
-
+	
 	local dmgmod = 1;
 	local accmod = 1;
 	local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*3,ELE_DARK,dmgmod,TP_NO_EFFECT);
 	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_IGNORE_SHADOWS);
     target:delHP(dmg);
+	
 	mob:resetEnmity(target);
 	return dmg;
+
 end;
