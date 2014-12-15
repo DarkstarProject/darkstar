2220410a429d13fe11b25e73a3bd5f86e8114cd3
korietsu <spht.lhar@gmail.com>
2014-12-14 21:47:59 -0600

    Whitespace Because Whitespace

--- scripts/zones/The_Garden_of_RuHmet/mobs/Aw_zdei.lua
+++ scripts/zones/The_Garden_of_RuHmet/mobs/Aw_zdei.lua
@@ -11,22 +11,20 @@ require("scripts/globals/status");
 
 -----------------------------------
 -- OnMobSpawn Action
--- Set AnimationSub to 0, put it in pot form 
+-- Set AnimationSub to 0, put it in pot form
 -----------------------------------
 function onMobSpawn(mob)
 	mob:AnimationSub(0);
 end;
-
 
 -----------------------------------
 -- onMobFight Action
 -- Randomly change forms
 -----------------------------------
 function onMobFight(mob)
-
-	local randomTime = math.random(15,45);	
+	local randomTime = math.random(15,45);
 	local changeTime = mob:getLocalVar("changeTime");
-		
+
 	if(mob:AnimationSub() == 0 and mob:getBattleTime() - changeTime > randomTime) then
 		mob:AnimationSub(math.random(2,3));
 		mob:setLocalVar("changeTime", mob:getBattleTime());
@@ -35,10 +33,10 @@ function onMobFight(mob)
 		mob:setLocalVar("changeTime", mob:getBattleTime());
 	elseif(mob:AnimationSub() == 2 and mob:getBattleTime() - changeTime > randomTime) then
 		local aniChance = math.random(0,1);
-		
+
 		if(aniChance == 0) then
 			mob:AnimationSub(0);
-			mob:setLocalVar("changeTime", mob:getBattleTime());		
+			mob:setLocalVar("changeTime", mob:getBattleTime());
 		else
 			mob:AnimationSub(3)
 			mob:setLocalVar("changeTime", mob:getBattleTime());
@@ -47,4 +45,4 @@ function onMobFight(mob)
 		mob:AnimationSub(math.random(0,2));
 		mob:setLocalVar("changeTime", mob:getBattleTime());
 	end
-end
+end;
