78ff47986484febd1321435ecac1ef09b5fdd3e4
Turttle <brocollycookies@gmail.com>
2014-12-15 02:15:21 -0400

    Updated Ix'Aern DRK
    Added text to show when clicking the ???. Added spawn mobs on trade to
    the qm2. Added Aw'aern check to see if the mob killed is inside the ???
    room.

--- scripts/zones/The_Garden_of_RuHmet/TextIDs.lua
+++ scripts/zones/The_Garden_of_RuHmet/TextIDs.lua
@@ -8,6 +8,9 @@ ITEM_CANNOT_BE_OBTAINED = 6379; -- You cannot obtain the item <item> come back a
 
 -- Other
 NO_NEED_INVESTIGATE = 7595; -- There is no need to investigate further.
+   UNKNOWN_PRESENCE = 7732; -- You sense some unknown presence...
+       NONE_HOSTILE = 7733; -- You sense some unknown presence, but it does not seem hostile.
+	SHEER_ANIMOSITY = 7735; -- Player Name is enveloped in sheer animosity!
 
 -- conquest Base
 CONQUEST_BASE = 7416; -- Tallying conquest results...
