9e0b3d9d140ac2f8d4cb91abc7a59ebd0d2dcb23
Turttle <brocollycookies@gmail.com>
2014-12-15 12:54:07 -0400

    Updated typo
    Updated the way I commented out the file in Aw'aern and qm2. From single
    line to block format.

--- scripts/zones/The_Garden_of_RuHmet/mobs/Aw_aern.lua
+++ scripts/zones/The_Garden_of_RuHmet/mobs/Aw_aern.lua
@@ -21,61 +21,63 @@ end;
 -----------------------------------
 
 function onMobDeath(mob, killer)
-	--local VanadielHour = VanadielHour();
-	--local posi = GetServerVariable("[POSI]Ix_aern_drk")
+	--[[
+	local VanadielHour = VanadielHour();
+	local posi = GetServerVariable("[POSI]Ix_aern_drk")
 		--print(posi);
-	--i = GetServerVariable("[PH]Ix_aern_drk")
+	i = GetServerVariable("[PH]Ix_aern_drk")
 		--print(i);
-	 -- Check if Aw_aern are in the room with the ???
-		--mob = mob:getID();
-		--if (posi == 1) then 
-			--if (mob ~= Ix_aern_drk_PH_pos1[mob]) then
-				--if (i == 3) then 
-					--killer:messageSpecial(SHEER_ANIMOSITY);
-					--if(VanadielHour % 4 == 0) then 
-						--SetServerVariable("[PH]Ix_aern_drk", 0);
-					--end
-				--else
-					--i = i + 1; -- adds 1 to the kill count.
-					--SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
-				--end
-			--end
-		--elseif (posi == 2) then 
-			--if (mob ~= Ix_aern_drk_PH_pos2[mob]) then
-				--if (i == 3) then 
-					--killer:messageSpecial(SHEER_ANIMOSITY);
-					--if(VanadielHour % 4 == 0) then 
-						--SetServerVariable("[PH]Ix_aern_drk", 0);
-					--end
-				--else
-					--i = i + 1; -- adds 1 to the kill count.
-					--SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
-				--end
-			--end
-		--elseif (posi == 3) then 
-			--if (mob ~= Ix_aern_drk_PH_pos3[mob]) then
-				--if (i == 3) then 
-					--killer:messageSpecial(SHEER_ANIMOSITY);
-					--if(VanadielHour % 4 == 0) then 
-						--SetServerVariable("[PH]Ix_aern_drk", 0);
-					--end
-				--else
-					--i = i + 1; -- adds 1 to the kill count.
-					--SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
-				--end
-			--end
-		--elseif (posi == 4) then 
-			--if (mob ~= Ix_aern_drk_PH_pos4[mob]) then
-				--if (i == 3) then 
-					--killer:messageSpecial(SHEER_ANIMOSITY);
-					--if(VanadielHour % 4 == 0) then 
-						--SetServerVariable("[PH]Ix_aern_drk", 0);
-					--end
-				--else
-					--i = i + 1; -- adds 1 to the kill count.
-					--SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
-				--end
-			--end
+		--Check if Aw_aern are in the room with the ???
+		mob = mob:getID();
+		if (posi == 1) then 
+			if (mob ~= Ix_aern_drk_PH_pos1[mob]) then
+				if (i == 3) then 
+					killer:messageSpecial(SHEER_ANIMOSITY);
+					if(VanadielHour % 4 == 0) then 
+						SetServerVariable("[PH]Ix_aern_drk", 0);
+					end
+				else
+					i = i + 1; -- adds 1 to the kill count.
+					SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
+				end
+			end
+		elseif (posi == 2) then 
+			if (mob ~= Ix_aern_drk_PH_pos2[mob]) then
+				if (i == 3) then 
+					killer:messageSpecial(SHEER_ANIMOSITY);
+					if(VanadielHour % 4 == 0) then 
+						SetServerVariable("[PH]Ix_aern_drk", 0);
+					end
+				else
+					i = i + 1; -- adds 1 to the kill count.
+					SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
+				end
+			end
+		elseif (posi == 3) then 
+			if (mob ~= Ix_aern_drk_PH_pos3[mob]) then
+				if (i == 3) then 
+					killer:messageSpecial(SHEER_ANIMOSITY);
+					if(VanadielHour % 4 == 0) then 
+						SetServerVariable("[PH]Ix_aern_drk", 0);
+					end
+				else
+					i = i + 1; -- adds 1 to the kill count.
+					SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
+				end
+			end
+		elseif (posi == 4) then 
+			if (mob ~= Ix_aern_drk_PH_pos4[mob]) then
+				if (i == 3) then 
+					killer:messageSpecial(SHEER_ANIMOSITY);
+					if(VanadielHour % 4 == 0) then 
+						SetServerVariable("[PH]Ix_aern_drk", 0);
+					end
+				else
+					i = i + 1; -- adds 1 to the kill count.
+					SetServerVariable("[PH]Ix_aern_drk", i); -- set server variable to what i value is.
+				end
+			end
 		
-		--end
+		end
+		]]--
 end;
