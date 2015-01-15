0735fde452ee675ba2b1fb73df6e8edf9841f287
Joe <tisch.joe@gmail.com>
2015-01-15 16:57:11 -0500

    Update Durogg.lua

--- scripts/zones/Northern_San_dOria/npcs/Durogg.lua
+++ scripts/zones/Northern_San_dOria/npcs/Durogg.lua
@@ -15,55 +15,55 @@ require("scripts/globals/quests");
 -- onTrade Action
 -----------------------------------
 
-function onTrade( player, npc, trade)
+function onTrade(player,npc,trade)
 end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
-function onTrigger( player, npc)
+function onTrigger(player,npc)
 
 	-- Add-on Scenarios
-	AddonScenarios = 0xFFFFFFFE;
-	if( player:hasCompletedMission( AMK, DRENCHED_IT_BEGAN_WITH_A_RAINDROP)) then
+	local AddonScenarios = 0xFFFFFFFE;
+	if (player:hasCompletedMission(AMK,DRENCHED_IT_BEGAN_WITH_A_RAINDROP)) then
 		AddonScenarios = AddonScenarios - 2; -- Drenched! It Began with a Raindrop.
 	end
 -- *Need the correct csid
---	if( player:hasCompletedMission( AMK, HASTEN_IN_A_JAM_IN_JEUNO)) then
+--	if (player:hasCompletedMission(AMK,HASTEN_IN_A_JAM_IN_JEUNO)) then
 --		AddonScenarios = AddonScenarios - 4; -- Hasten! In a Jam in Jeuno?
 --	end
 
 	-- Seekers of Adoulin
-	SeekersOfAdoulin = 0xFFFFFFFE;
+	local SeekersOfAdoulin = 0xFFFFFFFE;
 -- *Need the correct csid
---	if( player:hasCompletedMission( SOA, RUMORS_FROM_THE_WEST)) then
+--	if (player:hasCompletedMission(SOA,RUMORS_FROM_THE_WEST)) then
 --		SeekersOfAdoulin = SeekersOfAdoulin - 2; -- Rumors from the West
 --	end
 
 	-- Determine if any cutscenes are available for the player.
-	gil = player:getGil();
-	if( AddonScenarios    == 0xFFFFFFFE and 
+	local gil = player:getGil();
+	if (AddonScenarios    == 0xFFFFFFFE and 
 	    SeekersOfAdoulin  == 0xFFFFFFFE)
 	then -- Player has no cutscenes available to be viewed.
 		gil = 0; -- Setting gil to a value less than 10(cost) will trigger the appropriate response from this npc.
 	end
 
-	player:startEvent( 0x0361, AddonScenarios, SeekersOfAdoulin, 0xFFFFFFFE, 0xFFFFFFFE, 0xFFFFFFFE, 0xFFFFFFFE, 10, gil); -- CSID, Missions, Fame, ?, ?, ?, ?, Cost, TotalGilPlayerHas
+	player:startEvent(0x0361,AddonScenarios,SeekersOfAdoulin,0xFFFFFFFE,0xFFFFFFFE,0xFFFFFFFE,0xFFFFFFFE,10,gil); -- CSID,Missions,Fame,?,?,?,?,Cost,TotalGilPlayerHas
 end;
 
 -----------------------------------
 -- onEventUpdate
 -----------------------------------
 
-function onEventUpdate( player, csid, option)
-	-- printf( "CSID: %u", csid);
-	-- printf( "RESULT: %u", option);
+function onEventUpdate(player,csid,option)
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 	
 	playCutscene = true;
-	if( player:delGil( 10) == false) then
+	if (player:delGil(10) == false) then
 		playCutscene = false; -- Cancel the cutscene.
-		player:updateEvent( 0);
+		player:updateEvent(0);
 	end
 end;
 
@@ -71,17 +71,17 @@ end;
 -- onEventFinish
 -----------------------------------
 
-function onEventFinish( player, csid, option)
-	-- printf( "CSID: %u", csid);
-	-- printf( "RESULT: %u", option);
+function onEventFinish(player,csid,option)
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 
-	if( playCutscene) then
-		if(     option ==   1) then		-- Drenched! It Began with a Raindrop
-			player:startEvent( 0x7549, 0, 0, 0, 0, 0, 0, 231);
---		elseif( option ==   2) then		-- Hasten! In a Jam in Jeuno?
---			player:startEvent( CSID, 0, 0, 0, 0, 0, 0, 231);
---		elseif( option ==  33) then		-- Rumors from the West
---			player:startEvent( CSID);
+	if (playCutscene) then
+		if (   option ==   1) then		-- Drenched! It Began with a Raindrop
+			player:startEvent(0x7549,0,0,0,0,0,0,231);
+--		elseif (option ==   2) then		-- Hasten! In a Jam in Jeuno?
+--			player:startEvent(CSID,0,0,0,0,0,0,231);
+--		elseif (option ==  33) then		-- Rumors from the West
+--			player:startEvent(CSID);
 		end
 	end
 end;
