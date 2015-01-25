681ee8767df1755090c0f5a72068ab7597355fc4
Joe <tisch.joe@gmail.com>
2015-01-25 00:19:32 -0500

    Update Bruno.lua

--- scripts/zones/Port_Bastok/npcs/Bruno.lua
+++ scripts/zones/Port_Bastok/npcs/Bruno.lua
@@ -4,8 +4,8 @@
 -- Standard Info NPC
 -----------------------------------
 
-
 package.loaded["scripts/zones/Port_Bastok/TextIDs"] = nil;
+
 require("scripts/zones/Port_Bastok/TextIDs");
 
 -----------------------------------
@@ -20,7 +20,25 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x016C);
+
+	local vHour = VanadielHour();
+	local vMin  = VanadielMinute();
+
+	while vHour >= 0 do
+		vHour = vHour - 6;
+	end
+
+	if(     vHour == -6) then vHour = 0;
+	elseif( vHour == -5) then vHour = 1;
+	elseif( vHour == -4) then vHour = 2;
+	elseif( vHour == -3) then vHour = 3;
+	elseif( vHour == -2) then vHour = 4;
+	elseif( vHour == -1) then vHour = 5;
+	end
+
+	local seconds = math.floor(2.4 * ((vHour * 60) + vMin));
+
+	player:startEvent( 0x016C, seconds, 0, 0, 0, 0, 0, 0, 0);
 end; 
 
 -----------------------------------
@@ -28,8 +46,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -37,9 +55,6 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
-
-
-
