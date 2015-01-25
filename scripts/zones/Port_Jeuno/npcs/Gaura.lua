4a065a68bdc27fd279d05324eef9f1fa4f9b651d
Joe <tisch.joe@gmail.com>
2015-01-25 00:27:10 -0500

    Update Gaura.lua

--- scripts/zones/Port_Jeuno/npcs/Gaura.lua
+++ scripts/zones/Port_Jeuno/npcs/Gaura.lua
@@ -5,6 +5,7 @@
 -----------------------------------
 
 package.loaded["scripts/zones/Port_Jeuno/TextIDs"] = nil;
+
 require("scripts/zones/Port_Jeuno/TextIDs");
 
 -----------------------------------
@@ -19,7 +20,22 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x2729);
+
+	local vHour = VanadielHour();
+	local vMin  = VanadielMinute();
+
+	while vHour >= 3 do
+		vHour = vHour - 6;
+	end
+
+	if(     vHour == -3) then vHour = 3;
+	elseif( vHour == -2) then vHour = 4;
+	elseif( vHour == -1) then vHour = 5;
+	end
+
+	local seconds = math.floor(2.4 * ((vHour * 60) + vMin));
+
+	player:startEvent( 0x2729, seconds, 0, 0, 0, 0, 0, 0, 0);
 end; 
 
 -----------------------------------
@@ -27,8 +43,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -36,9 +52,6 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
-
-
-
