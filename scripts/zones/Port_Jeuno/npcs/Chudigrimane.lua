26d6683a61ec44795936b5291ee4c2c1019ae542
Joe <tisch.joe@gmail.com>
2015-01-25 00:26:17 -0500

    Update Chudigrimane.lua

--- scripts/zones/Port_Jeuno/npcs/Chudigrimane.lua
+++ scripts/zones/Port_Jeuno/npcs/Chudigrimane.lua
@@ -5,6 +5,7 @@
 -----------------------------------
 
 package.loaded["scripts/zones/Port_Jeuno/TextIDs"] = nil;
+
 require("scripts/zones/Port_Jeuno/TextIDs");
 
 -----------------------------------
@@ -19,7 +20,17 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x6);
+
+	local vHour = VanadielHour();
+	local vMin  = VanadielMinute();
+
+	while vHour >= 6 do
+		vHour = vHour - 6;
+	end
+
+	local seconds = math.floor(2.4 * ((vHour * 60) + vMin));
+
+	player:startEvent( 0x0006, seconds, 0, 0, 0, 0, 0, 0, 0);
 end; 
 
 -----------------------------------
@@ -27,8 +38,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -36,9 +47,6 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
-
-
-
