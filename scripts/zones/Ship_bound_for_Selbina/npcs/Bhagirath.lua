6484cc8754e93678d84e6c48fed969c5dee98e98
Joe <tisch.joe@gmail.com>
2015-01-27 20:37:58 -0500

    Update Bhagirath.lua

--- scripts/zones/Ship_bound_for_Selbina/npcs/Bhagirath.lua
+++ scripts/zones/Ship_bound_for_Selbina/npcs/Bhagirath.lua
@@ -4,8 +4,8 @@
 -- Notes: Tells ship ETA time
 -- @pos 0.278 -14.707 -1.411 220
 -----------------------------------
+
 package.loaded["scripts/zones/Ship_bound_for_Selbina/TextIDs"] = nil;
------------------------------------
 
 require("scripts/zones/Ship_bound_for_Selbina/TextIDs");
 
@@ -21,7 +21,35 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:messageSpecial(ON_WAY_TO_SELBINA,0,0); -- Earth Time, Vana Hours. Needs a get-time function for boat?
+
+	local vHour = VanadielHour();
+	local vMin  = VanadielMinute();
+
+	while vHour >= 6 do
+		vHour = vHour - 8;
+	end
+
+	if(     vHour == -2) then vHour = 8;
+	elseif( vHour == -1) then vHour = 7;
+	elseif( vHour ==  0) then vHour = 6;
+	elseif( vHour ==  1) then vHour = 5;
+	elseif( vHour ==  2) then vHour = 4;
+	elseif( vHour ==  3) then vHour = 3;
+	elseif( vHour ==  4) then vHour = 2;
+	elseif( vHour ==  5) then vHour = 1;
+	elseif( vHour ==  6) then vHour = 0;
+	end
+
+	if( vHour == 0 and vMin > 40) then
+		vHour = 8;
+		vMin  = 40 - vMin;
+	end
+
+	local minutes = math.floor((2.4 * ((vHour * 60) + vMin)) / 60);
+
+	if( vHour > 7) then vHour = 7; end -- Normal players can't be on the boat longer than 7 Vanadiel hours.
+
+	player:messageSpecial( ON_WAY_TO_SELBINA, minutes, vHour);
 end; 
 
 -----------------------------------
@@ -29,8 +57,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -38,9 +66,6 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+	-- printf("CSID: %u",csid);
+	-- printf("RESULT: %u",option);
 end;
-
-
-
