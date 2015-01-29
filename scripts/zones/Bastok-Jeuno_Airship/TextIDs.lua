e61eb511e41820f29a4aa8aa2fdefffd0bfc6701
Joe <tisch.joe@gmail.com>
2015-01-28 21:16:58 -0500

    Update TextIDs.lua

--- scripts/zones/Bastok-Jeuno_Airship/TextIDs.lua
+++ scripts/zones/Bastok-Jeuno_Airship/TextIDs.lua
@@ -5,3 +5,10 @@ ITEM_CANNOT_BE_OBTAINED = 6379; -- You cannot obtain the item <item> come back a
           ITEM_OBTAINED = 6382; -- Obtained: <item>
            GIL_OBTAINED = 6383; -- Obtained <number> gil
        KEYITEM_OBTAINED = 6385; -- Obtained key item: <keyitem>
+
+-- Other
+
+       WILL_REACH_JEUNO = 7189; -- The airship will reach Jeuno in...
+      WILL_REACH_BASTOK = 7190; -- The airship will reach Bastok in...
+   IN_JEUNO_MOMENTARILY = 7191; -- We will be arriving in Jeuno momentarily.
+  IN_BASTOK_MOMENTARILY = 7192; -- We will be arriving in Bastok momentarily.
