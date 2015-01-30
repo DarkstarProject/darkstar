f043c923431390406e82da93625fe9a98849ab15
Joe <tisch.joe@gmail.com>
2015-01-29 19:41:04 -0500

    Update TextIDs.lua

--- scripts/zones/San_dOria-Jeuno_Airship/TextIDs.lua
+++ scripts/zones/San_dOria-Jeuno_Airship/TextIDs.lua
@@ -5,3 +5,10 @@ ITEM_CANNOT_BE_OBTAINED = 6379; -- You cannot obtain the item <item> come back a
           ITEM_OBTAINED = 6382; -- Obtained: <item>
            GIL_OBTAINED = 6383; -- Obtained <number> gil
        KEYITEM_OBTAINED = 6385; -- Obtained key item: <keyitem>
+
+-- Other
+
+       WILL_REACH_JEUNO = 7189; -- The airship will reach Jeuno in...
+    WILL_REACH_SANDORIA = 7190; -- The airship will reach San d'Oria in...
+   IN_JEUNO_MOMENTARILY = 7192; -- We will be arriving in Jeuno momentarily.
+IN_SANDORIA_MOMENTARILY = 7193; -- We will be arriving in San d'Oria momentarily.
