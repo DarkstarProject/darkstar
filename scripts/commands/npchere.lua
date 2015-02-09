70113b69b8b80604a8662fc9e0661956da89a0e8
TeoTwawki <arawn.annwn@yandex.com>
2015-02-09 02:20:38 -0500

    Aha! Have to set pos first then pop'em, can't use STATUS_UPDATE on NPC's.

--- scripts/commands/npchere.lua
+++ scripts/commands/npchere.lua
@@ -25,10 +25,9 @@ function onTrigger(player, npcId)
         return;
     end
 
-    npc:setStatus(STATUS_NORMAL);
     if (player:getZoneID() == npc:getZoneID()) then
         npc:setPos( player:getXPos(), player:getYPos(), player:getZPos(), player:getRotPos(), player:getZoneID() );
-        npc:setStatus(STATUS_UPDATE);
+        npc:setStatus(STATUS_NORMAL);
     else
         if (noDepop == nil or noDepop == 0) then
             npc:setStatus(STATUS_DISAPPEAR);
