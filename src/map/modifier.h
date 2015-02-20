702f6d4e012efd5f8b7bb54228ddc322b852b571
TeoTwawki <arawn.annwn@yandex.com>
2015-02-19 20:50:47 -0500

    Implement death resistance used by Shadow Ring, Eihwaz Ring, Atma of Cloak and Dagger, Atma of the Winged Gloom, and Atma of the Einherjar. Atma not included (I'm working on it in another branch).
    Wiki said proc rate between 5-10%, we know how unreliable un-parsed estimates are so I've gone with 5%.
    Elegant Ribbon and Flawless Ribbon are rumored to also use this, but I have not included them at this time.

--- src/map/modifier.h
+++ src/map/modifier.h
@@ -281,9 +281,8 @@ enum MODIFIER
 	MOD_SLOWRES                   = 0xFA, // Enhances "Resist Slow" effect (modId = 250)
 	MOD_STUNRES                   = 0xFB, // Enhances "Resist Stun" effect (modId = 251)
 	MOD_CHARMRES                  = 0xFC, // Enhances "Resist Charm" effect (modId = 252)
-
-	MOD_UNKNOWN                   = 0xFF, // (modId = 255)
-
+	MOD_DEATHRES                  = 0xFF, // Used by gear and ATMA that give resistance to instance KO (modId = 255)
+ 
 	MOD_PARALYZE                  = 0x101, // Paralyze -- percent chance to proc (modId = 257)
 	MOD_MIJIN_GAKURE              = 0x102, // Tracks whether or not you used this ability to die. (modId = 258)
 	MOD_DUAL_WIELD                = 0x103, // Percent reduction in dual wield delay. (modId = 259)
