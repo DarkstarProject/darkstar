ee81587db81359e7c14f97206efb8e89ea9e8360
nesstea <uber_ness@hotmail.com>
2015-03-03 16:54:47 -0800

    Changed offset to make the doors work.

--- scripts/zones/PsoXja/mobs/Gargoyle.lua
+++ scripts/zones/PsoXja/mobs/Gargoyle.lua
@@ -21,7 +21,7 @@ function onMobDeath(mob, killer)
 	for i = 0, 16, 1 do
 
 	local GargoyleOffset = 16814082 + (i-1);
-	local DoorOffset = 16814445 + (i-1);
+	local DoorOffset = 16814445 + (i);
 
 		if(mob:getID() == GargoyleOffset) then
 			GetNPCByID(DoorOffset):openDoor(30);
