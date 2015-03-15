dd2119874e595e310a3800bfacd4086931861e9f
nesstea <uber_ness@hotmail.com>
2015-03-14 17:16:00 -0700

    Moved buffs to onMobInitialize

--- scripts/zones/Attohwa_Chasm/mobs/Feeler_Antlion.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Feeler_Antlion.lua
@@ -9,13 +9,20 @@ require("scripts/globals/status");
 require("scripts/globals/magic");
 
 -----------------------------------
+-- onMobInitialize Action
+-----------------------------------
+
+function onMobInitialize(mob)
+	mob:addMod(MOD_REGAIN, 40);	-- Don't know exact value
+	mob:addMod(MOD_REGEN, 30);
+	mob:setLocalVar("SAND_BLAST",1);
+end;
+
+-----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
 function onMobSpawn(mob)
-	mob:addMod(MOD_REGAIN, 40);	-- Don't know exact value
-	mob:addMod(MOD_REGEN, 30);
-	mob:setLocalVar("SAND_BLAST",1);
 end;
 
 -----------------------------------
