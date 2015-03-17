907cd0610fc13eb5108a247235693fca20efbb2f
nesstea <uber_ness@hotmail.com>
2015-03-17 16:26:56 -0700

    Final adjustments

--- scripts/globals/items/holy_bolt.lua
+++ scripts/globals/items/holy_bolt.lua
@@ -2,6 +2,7 @@
 -- ID: 18153
 -- Item: Holy Bolt
 -- Additional Effect: Light Damage
+-- Bolt dmg is affected by light/dark staves and Chatoyant
 -----------------------------------------
 require("scripts/globals/status");
 require("scripts/globals/magic");
@@ -18,32 +19,15 @@ function onAdditionalEffect(player,target,damage)
     if (math.random(0,99) >= chance) then
         return 0,0,0;
     else
-        local diff = player:getStat(MOD_MND) - target:getStat(MOD_MND);
-		if (diff < 20) then
-            diff = diff+(diff-20)/2;
+        local dmg = player:getStat(MOD_MND) - target:getStat(MOD_MND);
+		if (dmg < 30) then
+            dmg = dmg+(dmg-30)/2;
         end
-        local dmg = diff + (player:getMainLvl() - target:getMainLvl());
         local params = {};
         params.bonusmab = 0;
-        params.includemab = true;
+        params.includemab = false;
         dmg = addBonusesAbility(player, ELE_LIGHT, target, dmg, params);
-        dmg = adjustForTarget(target,dmg,ELE_LIGHT);
-     		
-		-- Gear Adjustments
-		local main = player:getEquipID(SLOT_MAIN);
-		if (main == 18633 or main == 17558) then -- Apollo's staff / Chatoyant staff
-			dmg=dmg*1.2;
-		end
-		if (main == 17557) then
-			dmg=dmg*1.1;
-		end
-		if (main == 17559) then
-			dmg=dmg*0.9;
-		end
-		if (main == 17560) then
-			dmg=dmg*0.8;
-		end
-		
+        dmg = adjustForTarget(target,dmg,ELE_LIGHT);		
 		dmg = finalMagicNonSpellAdjustments(player,target,ELE_LIGHT,dmg);
         return SUBEFFECT_LIGHT_DAMAGE, 163, dmg;
     end
