5fff7b6537e984f6afac9728d75f38b5d3f88240
nesstea <uber_ness@hotmail.com>
2015-03-18 15:47:45 -0700

    roaming v1

--- scripts/zones/Boneyard_Gully/mobs/Race_Runner.lua
+++ scripts/zones/Boneyard_Gully/mobs/Race_Runner.lua
@@ -6,11 +6,50 @@
 require("scripts/globals/titles");
 require("scripts/globals/status");
 
+local path = {
+	-539, 0, -481,
+	-556, 0, -478,
+	-581, 0, -475,
+	-579, -3, -460,
+	-572, 2, -433,
+	-545, 1, -440,
+	-532, 0, -466
+};
+
 -----------------------------------
 -- onMobInitialize Action
 -----------------------------------
 
 function onMobInitialize(mob)
+end;
+
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+	onMobRoam(mob);
+end;
+
+-----------------------------------
+-- onMobRoamAction Action
+-----------------------------------
+
+function onMobRoamAction(mob)
+
+	pathfind.patrol(mob, path, PATHFLAG_REVERSE);
+
+end;
+
+-----------------------------------
+-- onMobRoam Action
+-----------------------------------
+
+function onMobRoam(mob)
+	-- move to start position if not moving
+	if(mob:isFollowingPath() == false) then
+		mob:pathThrough(pathfind.first(path));
+	end
 end;
 
 -----------------------------------
