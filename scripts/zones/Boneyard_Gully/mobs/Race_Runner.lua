f4936afedb155c5ee439ad6faf254ec88705c4ed
nesstea <uber_ness@hotmail.com>
2015-03-18 05:40:54 -0700

    For future AI coding

---
+++ scripts/zones/Boneyard_Gully/mobs/Race_Runner.lua
@@ -0,0 +1,35 @@
+-----------------------------------
+-- Area: Boneyard_Gully
+-- Name: Race Runner
+-----------------------------------
+
+require("scripts/globals/titles");
+require("scripts/globals/status");
+
+-----------------------------------
+-- onMobInitialize Action
+-----------------------------------
+
+function onMobInitialize(mob)
+end;
+
+-----------------------------------
+-- onMobEngaged Action
+-----------------------------------
+
+function onMobEngaged(mob,target)
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob,target)
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+end;
