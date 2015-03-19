afe0b3abf9f216a6a8edd3440a3c4db5dcdf2afe
Santssoft <santssoft@hotmail.com>
2015-03-19 16:26:48 +0100

    SQL: Set SPAWNTYPE_FOG to mob_groups
    CORE: New weather functions:
    	* Load weather from weather.dat in zone folder to std::vector
    	* Initialize weather on server start
    	* Update weather as task in a range of 3-30 minutes (each zone update individually)
    
    CORE: Remove TIME_FOG and logic to spawn fog mobs
    CORE: Add logic to spawn fog mobs when weather change to fog

--- src/map/vana_time.cpp
+++ src/map/vana_time.cpp
@@ -32,95 +32,95 @@ CVanaTime* CVanaTime::_instance = nullptr;
 
 CVanaTime::CVanaTime()
 {
-	setCustomOffset(0);
+    setCustomOffset(0);
     lastConquestUpdate = 0;
-	lastWeatherUpdate = 0;
-	lastVHourlyUpdate = 0;
-	lastVDailyUpdate = 0;
+    lastVHourlyUpdate = 0;
+    lastVDailyUpdate = 0;
     lastConquestTally = 0;
 }
 
 CVanaTime* CVanaTime::getInstance()
 {
-	if(_instance == nullptr) {
-		_instance = new CVanaTime();
-	}
-	return _instance;
+    if (_instance == nullptr)
+    {
+        _instance = new CVanaTime();
+    }
+    return _instance;
 }
 
 uint32 CVanaTime::getDate()
 {
-	return m_vanaDate;
+    return m_vanaDate;
 }
 
 uint32 CVanaTime::getYear()
 {
-	return m_vYear;
+    return m_vYear;
 }
 
 uint32 CVanaTime::getMonth()
 {
-	return m_vMon;
+    return m_vMon;
 }
 
 uint32 CVanaTime::getDayOfTheMonth()
 {
-	return m_vDate;
+    return m_vDate;
 }
 
 uint32 CVanaTime::getHour()
 {
-	return m_vHour;
+    return m_vHour;
 }
 
 uint32 CVanaTime::getMinute()
 {
-	return m_vMin;
+    return m_vMin;
 }
 
 uint32 CVanaTime::getWeekday()
 {
-	return m_vDay;
+    return m_vDay;
 }
 
 uint32 CVanaTime::getSysHour()
 {
-	time_t now = time(0);
-	tm *ltm = localtime(&now);
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
 
-	return ltm->tm_hour;
+    return ltm->tm_hour;
 }
 
 uint32 CVanaTime::getSysMinute()
 {
-	time_t now = time(0);
-	tm *ltm = localtime(&now);
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
 
-	return ltm->tm_min;
+    return ltm->tm_min;
 }
 
 uint32 CVanaTime::getSysSecond()
 {
-	time_t now = time(0);
-	tm *ltm = localtime(&now);
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
 
-	return ltm->tm_sec;
+    return ltm->tm_sec;
 }
 
 uint32 CVanaTime::getSysWeekDay()
 {
-	time_t now = time(0);
-	tm *ltm = localtime(&now);
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
 
-	return ltm->tm_wday;
+    return ltm->tm_wday;
 }
 
 uint32 CVanaTime::getSysYearDay()
 {
-	time_t now = time(0);
-	tm *ltm = localtime(&now);
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
 
-	return ltm->tm_yday;
+    return ltm->tm_yday;
 }
 
 uint32 CVanaTime::getVanaTime()
@@ -132,91 +132,88 @@ uint32 CVanaTime::getVanaTime()
 
 int32 CVanaTime::getCustomOffset()
 {
-	return m_customOffset;
+    return m_customOffset;
 }
 
 void CVanaTime::setCustomOffset(int32 offset)
 {
-	m_customOffset = offset;
-	m_TimeType = SyncTime();
+    m_customOffset = offset;
+    m_TimeType = SyncTime();
 }
 
 TIMETYPE CVanaTime::GetCurrentTOTD()
 {
-	return m_TimeType;
+    return m_TimeType;
 }
 
 uint32 CVanaTime::getMoonPhase()
 {
-	int32 phase = 0;
-	double daysmod = (int32)(((m_vanaDate / VTIME_DAY) + 26) % 84);
+    int32 phase = 0;
+    double daysmod = (int32)(((m_vanaDate / VTIME_DAY) + 26) % 84);
 
-	if (daysmod >= 42)
-	{
-		phase = (int32)(100 * ((daysmod - 42) / 42) + 0.5);
-	}
-	else
-	{
-		phase = (int32)(100 * (1 - (daysmod / 42)) + 0.5);
-	}
+    if (daysmod >= 42)
+    {
+        phase = (int32)(100 * ((daysmod - 42) / 42) + 0.5);
+    }
+    else
+    {
+        phase = (int32)(100 * (1 - (daysmod / 42)) + 0.5);
+    }
 
-	return phase;
+    return phase;
 }
 
 uint8 CVanaTime::getMoonDirection()
 {
-	double daysmod = (int32)(((m_vanaDate / VTIME_DAY) + 26) % 84);
+    double daysmod = (int32)(((m_vanaDate / VTIME_DAY) + 26) % 84);
 
-	if (daysmod == 42 || daysmod == 0)
-	{
-		return 0; //neither waxing nor waning
-	}
-	else if (daysmod < 42)
-	{
-		return 1;	//waning
-	}
-	else
-	{
-		return 2;   //waxing
-	}
+    if (daysmod == 42 || daysmod == 0)
+    {
+        return 0; //neither waxing nor waning
+    }
+    else if (daysmod < 42)
+    {
+        return 1;   //waning
+    }
+    else
+    {
+        return 2;   //waxing
+    }
 }
 
 uint8 CVanaTime::getRSERace()
 {
-	return (uint8)(((m_vanaDate / VTIME_WEEK) - 22) % 8) + 1;
+    return (uint8)(((m_vanaDate / VTIME_WEEK) - 22) % 8) + 1;
 }
 
 uint8 CVanaTime::getRSELocation()
 {
-	return (uint8)(((m_vanaDate / VTIME_WEEK) - 21) % 3);
+    return (uint8)(((m_vanaDate / VTIME_WEEK) - 21) % 3);
 }
 
 TIMETYPE CVanaTime::SyncTime()
 {
-	m_vanaDate  = (uint32)(this->getVanaTime() / 60.0 * 25) + 886 * VTIME_YEAR; //convert vana time (from SE epoch in earth seconds) to vanadiel minutes and add 886 vana years
+    m_vanaDate  = (uint32)(this->getVanaTime() / 60.0 * 25) + 886 * VTIME_YEAR; //convert vana time (from SE epoch in earth seconds) to vanadiel minutes and add 886 vana years
 
-	m_vYear = (uint32)( m_vanaDate / VTIME_YEAR);
-	m_vMon  = (uint32)((m_vanaDate / VTIME_MONTH) % 12) + 1;
-	m_vDate = (uint32)((m_vanaDate / VTIME_DAY) % 30 ) + 1;
-	m_vDay  = (uint32)((m_vanaDate % VTIME_WEEK)  / VTIME_DAY);
-	m_vHour = (uint32)((m_vanaDate % VTIME_DAY)   / VTIME_HOUR);
-	m_vMin  = (uint32)( m_vanaDate % VTIME_HOUR);
+    m_vYear = (uint32)( m_vanaDate / VTIME_YEAR);
+    m_vMon  = (uint32)((m_vanaDate / VTIME_MONTH) % 12) + 1;
+    m_vDate = (uint32)((m_vanaDate / VTIME_DAY) % 30 ) + 1;
+    m_vDay  = (uint32)((m_vanaDate % VTIME_WEEK)  / VTIME_DAY);
+    m_vHour = (uint32)((m_vanaDate % VTIME_DAY)   / VTIME_HOUR);
+    m_vMin  = (uint32)( m_vanaDate % VTIME_HOUR);
 
-	if (m_vMin == 0)
-	{
-		switch (m_vHour)
-		{
-            // FOG и MIDNIGHT являются TIME_NIGHT (это не ошибка)
-
+    if (m_vMin == 0)
+    {
+        switch (m_vHour)
+        {
             case  0: m_TimeType = TIME_NIGHT;   return TIME_MIDNIGHT;
-            case  2: m_TimeType = TIME_NIGHT;   return TIME_FOG;
-			case  4: m_TimeType = TIME_NEWDAY;	return TIME_NEWDAY;
-			case  6: m_TimeType = TIME_DAWN;	return TIME_DAWN;
-			case  7: m_TimeType = TIME_DAY;		return TIME_DAY;
-			case 17: m_TimeType = TIME_DUSK;	return TIME_DUSK;
-			case 18: m_TimeType = TIME_EVENING; return TIME_EVENING;
-			case 20: m_TimeType = TIME_NIGHT;	return TIME_NIGHT;
-		}
-	}
-	return TIME_NONE;
+            case  4: m_TimeType = TIME_NEWDAY;  return TIME_NEWDAY;
+            case  6: m_TimeType = TIME_DAWN;    return TIME_DAWN;
+            case  7: m_TimeType = TIME_DAY;     return TIME_DAY;
+            case 17: m_TimeType = TIME_DUSK;    return TIME_DUSK;
+            case 18: m_TimeType = TIME_EVENING; return TIME_EVENING;
+            case 20: m_TimeType = TIME_NIGHT;   return TIME_NIGHT;
+        }
+    }
+    return TIME_NONE;
 }
