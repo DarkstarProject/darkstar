de12f9ce8ddfff304da740812e599d8a9a3f706c
nesstea <uber_ness@hotmail.com>
2015-03-22 20:30:05 -0700

    Added clothcraft guild NPC and fixed bonecraft

--- scripts/zones/Windurst_Woods/npcs/Hauh_Colphioh.lua
+++ scripts/zones/Windurst_Woods/npcs/Hauh_Colphioh.lua
@@ -1,9 +1,9 @@
 -----------------------------------
 --  Area: Windurst Woods
---  NPC: Samigo-Pormigo
+--  NPC: Hauh Colphioh
 --  Type: Guildworker's Union Representative
 --  @zone: 241
---  @pos -9.782 -5.249 -134.432
+--  @pos -38.173 -1.25 -113.679
 -----------------------------------
 
 package.loaded["scripts/zones/Bastok_Markets/TextIDs"] = nil;
@@ -13,22 +13,27 @@ require("scripts/zones/Bastok_Markets/TextIDs");
 
 local keyitems = {
      [0] = {
-        id = BONE_PURIFICATION,
+        id = CLOTH_PURIFICATION,
         rank = 3,
         cost = 40000 
     },
     [1] = {
-        id = BONE_ENSORCELLMENT,
+        id = CLOTH_ENSORCELLMENT,
         rank = 3,
         cost = 40000 
     },
 	[2] = {
-        id = FILING,
+        id = SPINNING,
         rank = 3,
         cost = 10000 
     },
-    [3] = {
-        id = WAY_OF_THE_BONEWORKER,
+	[3] = {
+        id = FLETCHING,
+        rank = 3,
+        cost = 10000 
+    },
+    [4] = {
+        id = WAY_OF_THE_WEAVER,
         rank = 9,
         cost = 20000 
     }
@@ -36,42 +41,42 @@ local keyitems = {
 
 local items = {
     [2] = {
-        id = 15449,
-        rank = 3,
+        id = 15447, -- Weaver's Belt
+        rank = 4,
         cost = 10000 
     },
     [3] = {
-        id = 13947,
+        id = 13946, -- Magnifying Spectacles
         rank = 6,
         cost = 70000 
     },
     [4] = {
-        id = 14397,
+        id = 14395, -- Weaver's Apron
         rank = 7,
         cost = 100000 
     },
     [5] = {
-        id = 142, -- Drogaroga's Fang
+        id = 198, -- Gilt Tapestry
         rank = 9,
         cost = 150000 
     },
     [6] = {
-        id = 336, -- Boneworker's Signboard
+        id = 337, -- Weaver's Signboard
         rank = 9,
         cost = 200000
     },
 	[7] = {
-        id = 15824, -- Bonecrafter's Ring
+        id = 15822, -- Tailor's Ring
         rank = 6,
         cost = 80000
     },
 	[8] = {
-        id = 3663, -- Bonecraft Tools
+        id = 3665, -- Spinning Wheel
         rank = 7,
         cost = 50000
     },
 	[9] = {
-        id = 3326, -- Boneworker's Emblem
+        id = 3327, -- Weavers' Emblem
         rank = 9,
         cost = 15000
     }
@@ -82,7 +87,7 @@ local items = {
 -----------------------------------
 
 function onTrade(player,npc,trade)
-    unionRepresentativeTrade(player, npc, trade, 0x2727, 6);
+    unionRepresentativeTrade(player, npc, trade, 0x2727, 4);
 end;
 
 -----------------------------------
@@ -90,7 +95,7 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	unionRepresentativeTrigger(player, 6, 0x2726, "guild_bonecrafting", keyitems)
+	unionRepresentativeTrigger(player, 4, 0x2726, "guild_weaving", keyitems)
 end;
 
 -----------------------------------
@@ -111,7 +116,7 @@ function onEventFinish(player,csid,option,target)
 	 printf("RESULT: %u",option);
      
     if (csid == 0x2726) then
-        unionRepresentativeTriggerFinish(player, option, target, 6, "guild_bonecrafting", keyitems, items);
+        unionRepresentativeTriggerFinish(player, option, target, 4, "guild_weaving", keyitems, items);
     elseif(csid == 0x2727) then
         player:messageSpecial(GP_OBTAINED, option);
     end
