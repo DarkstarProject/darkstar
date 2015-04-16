195ad89d7843d51538d26a795def4a49df713922
Sylvain <sylvain.santucci@gmail.com>
2015-04-16 20:51:55 +0200

    Correct bug rank 9.1

--- scripts/zones/Valley_of_Sorrows/npcs/qm2.lua
+++ scripts/zones/Valley_of_Sorrows/npcs/qm2.lua
@@ -1,43 +1,39 @@
 -----------------------------------
 -- Area: Valley of Sorrows
 -- NPC:  qm1 (???)
--- Note: Used to spawn Adamantoise and Aspidochelone
--- @pos 0 0 -37 59
+-- Note: Used to rank 9.1 San d'oria
+-- @pos 91 -3 -16 128
 -----------------------------------
 package.loaded["scripts/zones/Valley_of_Sorrows/TextIDs"] = nil;
 -----------------------------------
 
 require("scripts/zones/Valley_of_Sorrows/TextIDs");
 require("scripts/globals/status");
+require("scripts/globals/keyitems");
+require("scripts/globals/missions");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
-	local Adamantoise = GetMobAction(17301537);
-	local Aspidochelone = GetMobAction(17301538);
-	-- Trade Clump of Red Pondweed
-	if((Aspidochelone == ACTION_NONE or Aspidochelone == ACTION_SPAWN) and trade:hasItemQty(3344,1) and trade:getItemCount() == 1) then -- Check trade, and if mob is ACTION_NONE (0) or waiting to spawn (24)
-		if (LandKingSystem_HQ == 1 or LandKingSystem_HQ == 2) then
-			player:tradeComplete();
-			SpawnMob(17301538,180):updateEnmity(player);
-		end
-		-- Trade Clump of Blue Pondweed
-	elseif((Adamantoise == ACTION_NONE or Adamantoise == ACTION_SPAWN) and trade:hasItemQty(3343,1) and trade:getItemCount() == 1) then
-		if (LandKingSystem_NQ == 1 or LandKingSystem_NQ == 2) then
-			player:tradeComplete();
-			SpawnMob(17301537,180):updateEnmity(player);
-		end
-	end
+--printf("CSID: %u",csid);
+--printf("RESULT: %u",option);
 end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
+local KEYITEM_OBTAINED = 6382; -- Put this as a local until the TextIDs have all been fixed.
 
 function onTrigger(player,npc)
-	player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
+	if(player:getCurrentMission(SANDORIA) == BREAKING_BARRIERS and player:getVar("MissionStatus") == 1) then
+		player:addKeyItem(FIGURE_OF_TITAN);
+		player:messageSpecial(KEYITEM_OBTAINED,FIGURE_OF_TITAN);
+		player:setVar("MissionStatus",2);
+	else
+		player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
+	end
 end;
 
 -----------------------------------
