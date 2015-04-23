e34f14d66d80d3c40be42bbf6a74443d460cb044
Turttle <brocollycookies@gmail.com>
2015-04-22 20:20:24 -0700

    Fixed typo
    Typo

--- scripts/commands/send.lua
+++ scripts/commands/send.lua
@@ -290,7 +290,7 @@ function onTrigger(player, p1, zoneId)
 	
 	
 	-- Not enough info.. 
-	if(p1 == nil and (p2 == nil or zone == nil)) then
+	if(p1 == nil and zone == nil) then
 		player:PrintToPlayer( string.format("Must specify 2 players or zone: @zoneplayer <player to be sent> (<player to arrive at> or zone) ") );
 		return;
 	end
