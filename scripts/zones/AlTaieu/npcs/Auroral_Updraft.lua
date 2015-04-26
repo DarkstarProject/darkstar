e4f42e60c5bfc1712d5b44824254b23d857f2f78
nesstea <uber_ness@hotmail.com>
2015-04-26 06:36:21 -0700

    Death to tabs and formatting fixes.

--- scripts/zones/AlTaieu/npcs/Auroral_Updraft.lua
+++ scripts/zones/AlTaieu/npcs/Auroral_Updraft.lua
@@ -21,22 +21,22 @@ end;
 
 function onTrigger(player,npc)
 
-	local Auroral_Offset = 16912902;
-	local npcID = npc:getID();
+    local Auroral_Offset = 16912902;
+    local npcID = npc:getID();
 
-	if(npcID == Auroral_Offset) then
-		player:startEvent(0x0096);
-	elseif(npcID == Auroral_Offset+1) then
-		player:startEvent(0x009B);
-	elseif(npcID == Auroral_Offset+2) then
-		player:startEvent(0x009B);
-	elseif(npcID == Auroral_Offset+3) then
-		player:startEvent(0x009B);
-	elseif(npcID == Auroral_Offset+4) then
-		player:startEvent(0x009B);
-	elseif(npcID == Auroral_Offset+5) then
-		player:startEvent(0x009B);
-	end
+    if(npcID == Auroral_Offset) then
+        player:startEvent(0x0096);
+    elseif(npcID == Auroral_Offset+1) then
+        player:startEvent(0x009B);
+    elseif(npcID == Auroral_Offset+2) then
+        player:startEvent(0x009B);
+    elseif(npcID == Auroral_Offset+3) then
+        player:startEvent(0x009B);
+    elseif(npcID == Auroral_Offset+4) then
+        player:startEvent(0x009B);
+    elseif(npcID == Auroral_Offset+5) then
+        player:startEvent(0x009B);
+    end
 end;
 
 -----------------------------------
@@ -55,10 +55,10 @@ end;
 function onEventFinish(player,csid,option)
 -- printf("CSID: %u",csid);
 -- printf("RESULT: %u",option);
-	if(csid == 0x009B) and option == 1 then
-		player:setPos(-25,-1 ,-620 ,208 ,33);
-	elseif(csid == 0x0096) and option == 1 then
-		player:setPos(611.931, 132.787, 773.427, 192, 32); -- To Sealion's Den {R}
-	end 
+    if(csid == 0x009B) and option == 1 then
+        player:setPos(-25,-1 ,-620 ,208 ,33);
+    elseif(csid == 0x0096) and option == 1 then
+        player:setPos(611.931, 132.787, 773.427, 192, 32); -- To Sealion's Den {R}
+    end 
 
 end;
--- scripts/zones/AlTaieu/npcs/HomePoint#1.lua
+++ scripts/zones/AlTaieu/npcs/HomePoint#1.lua
@@ -22,8 +22,7 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-
-	homepointMenu( player, 0x21fc, 85);
+    homepointMenu( player, 0x21fc, 85);
 end; 
 
 -----------------------------------
@@ -31,8 +30,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-	--printf("CSID: %u",csid);
-	--printf("RESULT: %u",option);
+    --printf("CSID: %u",csid);
+    --printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -40,16 +39,14 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-	--printf("CSID: %u",csid);
-	--printf("RESULT: %u",option);
-
-	if(csid == 0x21fc) then
-
-		if (option == 1) then	
-			player:setHomePoint();
-			player:messageSpecial(HOMEPOINT_SET);
-		else
-			hpTeleport( player, option);
-		end
-	end
+    --printf("CSID: %u",csid);
+    --printf("RESULT: %u",option);
+    if(csid == 0x21fc) then
+        if (option == 1) then    
+            player:setHomePoint();
+            player:messageSpecial(HOMEPOINT_SET);
+        else
+            hpTeleport( player, option);
+        end
+    end
 end;
