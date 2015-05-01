f6b7c71f2f04f419878b56e4958e8ff6ce7b73e8
TeoTwawki <arawn.annwn@yandex.com>
2015-04-30 21:05:33 -0400

    Fixed magic_hammer.lua

--- scripts/globals/spells/bluemagic/magic_hammer.lua
+++ scripts/globals/spells/bluemagic/magic_hammer.lua
@@ -30,33 +30,37 @@ end;
 -----------------------------------------
 
 function onSpellCast(caster,target,spell)
-
+    local damage = 0;
     local multi = 1.5;
-    local params = {};
-    -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
-        params.multiplier = multi;
-        params.tMultiplier = 1.0;
-        params.duppercap = 35;
-        params.str_wsc = 0.0;
-        params.dex_wsc = 0.0;
-        params.vit_wsc = 0.0;
-        params.agi_wsc = 0.0;
-        params.int_wsc = 0.0;
-        params.mnd_wsc = 0.39;
-        params.chr_wsc = 0.0;
-    damage = BlueMagicalSpell(caster, target, spell, params, MND_BASED);
-    damage = BlueFinalAdjustments(caster, target, spell, damage, params);
-    
-    if(caster:hasStatusEffect(EFFECT_AZURE_LORE)) then
+
+    if (caster:hasStatusEffect(EFFECT_AZURE_LORE)) then
         multi = multi + 0.50;
     end
-    
-        caster:addMP(dmg);
-    
-    if(target:isUndead()) then
-        spell:setMsg(75); -- No effect        
-        return damage;
+
+    local params = {};
+    -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
+    params.multiplier = multi;
+    params.tMultiplier = 1.0;
+    params.duppercap = 35;
+    params.str_wsc = 0.0;
+    params.dex_wsc = 0.0;
+    params.vit_wsc = 0.0;
+    params.agi_wsc = 0.0;
+    params.int_wsc = 0.0;
+    params.mnd_wsc = 0.39;
+    params.chr_wsc = 0.0;
+
+    if (target:isUndead()) then
+        spell:setMsg(75); -- No effect
+    else
+        dmg = BlueMagicalSpell(caster, target, spell, params, MND_BASED);
+        dmg = BlueFinalAdjustments(caster, target, spell, dmg, params);
+        if (target:getMP() > 0) then
+            caster:addMP(dmg);
+        else
+            return 0;
+        end
     end
-    
+
     return dmg;
 end;
