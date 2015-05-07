203a30332312803e4b9fb8f7504a88b81bf3a50d
demolish <xdemolish@hotmail.com>
2015-05-07 17:15:27 +0100

    CORE: fixed effects being removed on logout/disconnecting that should persist until the player has changed zones
    - added spell:castTime() to get/set a spell's cast/animation time in milliseconds (returns time if no parameter is passed)

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -746,7 +746,7 @@ int32 map_close_session(uint32 tick, map_session_data_t* map_session_data)
         uint64 ipp = map_session_data->client_addr;
         ipp |= port64 << 32;
 
-        map_session_data->PChar->StatusEffectContainer->SaveStatusEffects();
+        map_session_data->PChar->StatusEffectContainer->SaveStatusEffects(map_session_data->shuttingDown == 1);
 
         aFree(map_session_data->server_packet_data);
         delete map_session_data->PChar;
@@ -820,7 +820,7 @@ int32 map_cleanup(uint32 tick, CTaskMgr::CTask* PTask)
                             petutils::DespawnPet(PChar);
                         }
 
-
+                        PChar->StatusEffectContainer->SaveStatusEffects(true);
                         ShowDebug(CL_CYAN"map_cleanup: %s timed out, closing session\n" CL_RESET, PChar->GetName());
 
                         PChar->status = STATUS_SHUTDOWN;
@@ -828,7 +828,7 @@ int32 map_cleanup(uint32 tick, CTaskMgr::CTask* PTask)
                     }
                     else
                     {
-                        map_session_data->PChar->StatusEffectContainer->SaveStatusEffects();
+                        map_session_data->PChar->StatusEffectContainer->SaveStatusEffects(true);
                         Sql_Query(SqlHandle, "DELETE FROM accounts_sessions WHERE charid = %u;", map_session_data->PChar->id);
 
                         aFree(map_session_data->server_packet_data);
