e324b934045c1bbb2352ed699b6749da8a3b381a
madwraith <madwraith@live.com>
2015-05-25 17:29:39 -0400

    SCRIPTS: Updated stonega/waterga IV values

--- scripts/globals/spells/waterga_iv.lua
+++ scripts/globals/spells/waterga_iv.lua
@@ -14,18 +14,18 @@ function onMagicCastingCheck(caster,target,spell)
 	return 0;
 end;
 
-function onSpellCast(caster,target,spell)
+function onSpellCast(caster, target, spell)
     local spellParams = {};
     spellParams.hasMultipleTargetReduction = true;
     spellParams.resistBonus = 1.0;
-    spellParams.V0 = 540;
-    spellParams.V50 = 775;
-    spellParams.V100 = 970;
-    spellParams.V200 = 970;
-    spellParams.M0 = 4.7;
-    spellParams.M50 = 3.9;
-    spellParams.M100 = 2.95;
-    spellParams.M200 = 2.95;
+    spellParams.V0 = 850;
+    spellParams.V50 = 1130;
+    spellParams.V100 = 1367;
+    spellParams.V200 = 1762;
+    spellParams.M0 = 5.6;
+    spellParams.M50 = 4.74;
+    spellParams.M100 = 3.95;
+    spellParams.M200 = 3;
 
     return doElementalNuke(caster, spell, target, spellParams);
 end;
