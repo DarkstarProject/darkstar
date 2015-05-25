88ea84ae2e8a47b8168296cecb9dd90cf870344b
soapytoast <eraffxi@hushmail.com>
2015-05-24 22:28:26 -0700

    SCRIPTS windurst 8-1 to 9-1 additions/edits

--- scripts/zones/RoMaeve/npcs/Qu_Hau_Spring.lua
+++ scripts/zones/RoMaeve/npcs/Qu_Hau_Spring.lua
@@ -19,11 +19,11 @@ function onTrade(player,npc,trade)
 
    if (player:getWeather() == 0 and IsMoonFull() == true and Hour >= 0 and Hour <= 2) then
       if ((player:getQuestStatus(OUTLANDS,DIVINE_MIGHT) == QUEST_ACCEPTED or DMRepeat == QUEST_ACCEPTED) and
-         trade:hasItemQty(1408,1) and trade:hasItemQty(917,1) and trade:getItemCount() == 2) then
-            player:startEvent(7,917,1408); -- Ark Pentasphere Trade
+        trade:hasItemQty(1408,1) and trade:hasItemQty(917,1) and trade:getItemCount() == 2) then
+			player:startEvent(7,917,1408); -- Ark Pentasphere Trade
       elseif (DMRepeat == QUEST_ACCEPTED and trade:hasItemQty(1261,1) and trade:getItemCount() == 1 and
-         player:hasKeyItem(MOONLIGHT_ORE) == false) then
-            player:startEvent(8); -- Moonlight Ore trade
+        player:hasKeyItem(MOONLIGHT_ORE) == false) then
+			player:startEvent(8); -- Moonlight Ore trade
       end
    end
 end;
@@ -33,6 +33,14 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
+    local CurrentMission = player:getCurrentMission(WINDURST);
+	local MissionStatus = player:getVar("MissionStatus");
+
+	if(CurrentMission == VAIN and MissionStatus >= 1) then
+		player:startEvent(4);
+	else
+		player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
+	end
 end;
 
 -----------------------------------
@@ -47,17 +55,20 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-   if (csid == 7) then
+    if (csid == 7) then
       if (player:getFreeSlotsCount() == 0) then 
-         player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,1550);
+        player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,1550);
       else 
-         player:addItem(1550);
-         player:messageSpecial(ITEM_OBTAINED,1550);
-         player:tradeComplete();
+        player:addItem(1550);
+        player:messageSpecial(ITEM_OBTAINED,1550);
+        player:tradeComplete();
       end
-   elseif (csid == 8) then
-      player:tradeComplete();
-      player:addKeyItem(MOONLIGHT_ORE);
-      player:messageSpecial(KEYITEM_OBTAINED,MOONLIGHT_ORE);
-   end
+    elseif (csid == 8) then
+		player:tradeComplete();
+		player:addKeyItem(MOONLIGHT_ORE);
+		player:messageSpecial(KEYITEM_OBTAINED,MOONLIGHT_ORE);
+	elseif (csid == 4) then
+		player:setVar("MissionStatus",2);
+
+    end
 end;
