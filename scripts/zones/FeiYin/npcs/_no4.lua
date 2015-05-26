1d72b495d84c2b198e8d1db0ace9569dd2c0e35a
OzmaEra <f723a179@opayq.com>
2015-05-26 22:16:02 +0200

    moved everything in Rukususu.lua to this one

--- scripts/zones/FeiYin/npcs/_no4.lua
+++ scripts/zones/FeiYin/npcs/_no4.lua
@@ -1,9 +1,9 @@
 -----------------------------------
 --  Area: Fei'Yin
---  NPC:  Cermet Door (triggers Rukususu dialog)
+--  NPC: Rukususu (talk to Cermet Door _no5 to trigger)
 --  Type: Quest NPC
---  @pos -183 0 190 204
--- 	Involved in Quests: Curses, Foiled A-Golem!?,SMN AF2: Class Reunion, SMN AF3: Carbuncle Debacle
+--  @pos -194.133 -0.986 191.077 204
+-- 	Involved in quests: Curses, Foiled A-Golem!?,SMN AF2: Class Reunion, SMN AF3: Carbuncle Debacle
 --	Involved in Missions: Windurst 5-1/7-2/8-2
 -----------------------------------
 package.loaded["scripts/zones/FeiYin/TextIDs"] = nil;
@@ -13,6 +13,8 @@ require("scripts/globals/quests");
 require("scripts/globals/settings");
 require("scripts/zones/FeiYin/TextIDs");
 require("scripts/globals/keyitems");
+
+-----------------------------------
 
 -----------------------------------
 -- onTrade Action
@@ -26,10 +28,11 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-
-	
+	-- Windurst 8-2
+	if(player:getCurrentMission(WINDURST) == THE_JESTER_WHO_D_BE_KING and player:getVar("MissionStatus") == 1) then
+		player:startEvent(0x0016);
 	-- Curses, Foiled A_Golem!?
-	if(player:hasKeyItem(SHANTOTTOS_NEW_SPELL)) then
+	elseif(player:hasKeyItem(SHANTOTTOS_NEW_SPELL)) then
 		player:startEvent(0x000E); -- deliver spell
 	elseif(player:hasKeyItem(SHANTOTTOS_EXSPELL)) then
 		player:startEvent(0x000D); -- spell erased, try again!
@@ -38,7 +41,6 @@ function onTrigger(player,npc)
 	else
 		player:startEvent(0x000f); 
 	end
-	return 1;
 end;
 
 -----------------------------------
@@ -62,5 +64,12 @@ function onEventFinish(player,csid,option)
 	if(csid == 0x000E) then
 		player:setVar("foiledagolemdeliverycomplete",1);
 		player:delKeyItem(SHANTOTTOS_NEW_SPELL); -- remove key item
+	elseif(csid == 0x0016) then
+		player:addKeyItem(RHINOSTERY_RING);
+		player:messageSpecial(KEYITEM_OBTAINED,RHINOSTERY_RING);
+		if(player:hasKeyItem(AURASTERY_RING) and player:hasKeyItem(OPTISTERY_RING)) then
+			player:setVar("MissionStatus",2)
+		end
 	end
 end;
+
