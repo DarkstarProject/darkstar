42c303cdc3ebbdbbc367298a699938abcb8effc9
Loki79 <keegancarr79@gmail.com>
2015-05-31 08:34:26 -0500

    LUA: Added Skill Script with Model Checks For P. Warden / Lamps

---
+++ scripts/globals/mobskills/PW_Dreadstorm.lua
@@ -0,0 +1,33 @@
+---------------------------------------------
+--  Dreadstorm
+--
+--  Description: Deals magical damage in an area of effect. Additional effect: Terror
+--  Type: Magical
+--  Wipes Shadows
+--  Range: 13' radial
+---------------------------------------------
+require("/scripts/globals/settings");
+require("/scripts/globals/status");
+require("/scripts/globals/monstertpmoves");
+
+---------------------------------------------
+function onMobSkillCheck(target,mob,skill)
+    local mobSkin = mob:getModelId();
+
+    if(mobSkin == 1805) then
+        return 0;
+    else
+        return 1;
+    end
+end;
+
+function onMobWeaponSkill(target, mob, skill)
+
+-- TODO: Add EFFECT_TERROR once it actually does something.
+
+    local dmgmod = 2.5;
+    local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg() * 4,ELE_DARK,dmgmod,TP_MAB_BONUS,1);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_WIPE_SHADOWS);
+    target:delHP(dmg);
+    return dmg;
+end;
