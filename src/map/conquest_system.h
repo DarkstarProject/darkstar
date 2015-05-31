27f1de9112624950ed91afdb2e7cd45bc4ba6a4a
spynix <spynix@users.noreply.github.com>
2015-05-30 22:16:35 -0500

    CORE: Conquest and Influence
    1) Updated functions relating to gaining/losing influence to fix out of range values.
    2) Added appropriate helper function to conquest packet.

--- src/map/conquest_system.h
+++ src/map/conquest_system.h
@@ -19,7 +19,7 @@
   This file is part of DarkStar-server source code.
 
 ===========================================================================
-*/
+ */
 
 #ifndef _CONQUESTSYSTEM_H
 #define _CONQUESTSYSTEM_H
@@ -30,51 +30,49 @@
 
 #define SANDORIA            0x00
 #define BASTOK              0x01
-#define WINDURST            0x02  
+#define WINDURST            0x02
 #define BEASTMEN            0x03
 #define NEUTRAL             0xFF
 
-enum ConquestUpdate : uint8
-{
-    Conquest_Tally_Start = 0,
-    Conquest_Tally_End = 1,
-    Conquest_Update = 2
-};
+#define MAX_INFLUENCE_POINTS 2000000000
 
-/************************************************************************
-*                                                                       *
-*                                                                       *
-*                                                                       *
-************************************************************************/
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define BETWEEN(v, a, b) (MIN(MAX(v, a), b))
+
+enum ConquestUpdate : uint8 {
+  Conquest_Tally_Start = 0,
+  Conquest_Tally_End = 1,
+  Conquest_Update = 2
+};
 
 class CCharEntity;
 
-namespace conquest
-{
-	void	UpdateConquestSystem();										// Update conquest information in the DB
+namespace conquest {
+  void UpdateConquestSystem(); // Update conquest information in the DB
 
-	void	GainInfluencePoints(CCharEntity* PChar, uint32 points);		// Gain influence for player's nation (+1)
-	void	LoseInfluencePoints(CCharEntity* PChar);					// Lose influence for player's nation and gain for beastmen influence
-	
-    uint8	GetInfluenceGraphics(int32 san_inf, int32 bas_inf, int32 win_inf, int32 bst_inf);	// Get number for graphics in conquest menu (arrows)
-    uint8	GetInfluenceGraphics(REGIONTYPE RegionID);	                // Get number for graphics in conquest menu (arrows)
-    uint8   GetInfluenceRanking(int32 san_inf, int32 bas_inf, int32 win_inf, int32 bst_inf);
-    uint8   GetInfluenceRanking(int32 san_inf, int32 bas_inf, int32 win_inf);
+  void influence(CCharEntity *ch, int gain, bool died);
+  void LoseInfluencePoints(CCharEntity* PChar); // Lose influence for player's nation and gain for beastmen influence
 
-    void	UpdateConquestGM(ConquestUpdate type);						// Update conquest system by GM (modify in the DB and use @updateconquest)
-	void	UpdateWeekConquest();										// Update conquest system every sunday
+  uint8 GetInfluenceGraphics(int32 san_inf, int32 bas_inf, int32 win_inf, int32 bst_inf); // Get number for graphics in conquest menu (arrows)
+  uint8 GetInfluenceGraphics(REGIONTYPE RegionID); // Get number for graphics in conquest menu (arrows)
+  uint8 GetInfluenceRanking(int32 san_inf, int32 bas_inf, int32 win_inf, int32 bst_inf);
+  uint8 GetInfluenceRanking(int32 san_inf, int32 bas_inf, int32 win_inf);
 
-    uint8	GetBalance(uint8 sandoria, uint8 bastok, uint8 windurst,	// Ranking for 3 nations
-                       uint8 sandoria_prev, uint8 bastok_prev, uint8 windurst_prev);
-    uint8   GetBalance();
-    uint8   GetAlliance(uint8 sandoria, uint8 bastok, uint8 windurst);  // Determine if losing nations are allied
-    uint8	GetAlliance(uint8 sandoria, uint8 bastok, uint8 windurst,	// Determine if losing nations are allied
-        uint8 sandoria_prev, uint8 bastok_prev, uint8 windurst_prev);
-    bool    IsAlliance();                                               // Determine if losing nations are allied
-	uint8	GetNexTally();												// Next tally (weekly or every hour ?)
-    uint8	GetRegionOwner(REGIONTYPE RegionID);						// Get owner of the region
+  void UpdateConquestGM(ConquestUpdate type); // Update conquest system by GM (modify in the DB and use @updateconquest)
+  void UpdateWeekConquest(); // Update conquest system every sunday
 
-    uint32	AddConquestPoints(CCharEntity* PChar, uint32 exp);			// Add conquest points
+  uint8 GetBalance(uint8 sandoria, uint8 bastok, uint8 windurst, // Ranking for 3 nations
+          uint8 sandoria_prev, uint8 bastok_prev, uint8 windurst_prev);
+  uint8 GetBalance();
+  uint8 GetAlliance(uint8 sandoria, uint8 bastok, uint8 windurst); // Determine if losing nations are allied
+  uint8 GetAlliance(uint8 sandoria, uint8 bastok, uint8 windurst, // Determine if losing nations are allied
+          uint8 sandoria_prev, uint8 bastok_prev, uint8 windurst_prev);
+  bool IsAlliance(); // Determine if losing nations are allied
+  uint8 GetNexTally(); // Next tally (weekly or every hour ?)
+  uint8 GetRegionOwner(REGIONTYPE RegionID); // Get owner of the region
+
+  uint32 AddConquestPoints(CCharEntity* PChar, uint32 exp); // Add conquest points
 };
 
 #endif
--- src/map/packets/conquest_map.cpp
+++ src/map/packets/conquest_map.cpp
@@ -19,7 +19,7 @@
   This file is part of DarkStar-server source code.
 
 ===========================================================================
-*/
+ */
 
 #include "../../common/socket.h"
 
@@ -32,103 +32,115 @@
 
 #include "conquest_map.h"
 
+CConquestPacket::CConquestPacket(CCharEntity * PChar) {
+  this->type = 0x5E;
+  this->size = 0x5A;
 
-CConquestPacket::CConquestPacket(CCharEntity * PChar) 
-{
-	this->type = 0x5E; 
-	this->size = 0x5A;
-
-    const int8* Query = "SELECT region_id, region_control, region_control_prev, \
+  const int8* Query = "SELECT region_id, region_control, region_control_prev, \
                          sandoria_influence, bastok_influence, windurst_influence, \
                          beastmen_influence FROM conquest_system;";
 
-    int32 ret = Sql_Query(SqlHandle, Query);
+  int32 ret = Sql_Query(SqlHandle, Query);
 
-    uint8 sandoria_regions = 0;
-    uint8 bastok_regions = 0;
-    uint8 windurst_regions = 0;
-    uint8 sandoria_prev = 0;
-    uint8 bastok_prev = 0;
-    uint8 windurst_prev = 0;
+  uint8 sandoria_regions = 0;
+  uint8 bastok_regions = 0;
+  uint8 windurst_regions = 0;
+  uint8 sandoria_prev = 0;
+  uint8 bastok_prev = 0;
+  uint8 windurst_prev = 0;
 
-    if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0)
-    {
-        while (Sql_NextRow(SqlHandle) == SQL_SUCCESS)
-        {
-            int regionid = Sql_GetIntData(SqlHandle, 0);
-            int region_control = Sql_GetIntData(SqlHandle, 1);
-            int region_control_prev = Sql_GetIntData(SqlHandle, 2);
+  if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0) {
+    while (Sql_NextRow(SqlHandle) == SQL_SUCCESS) {
+      int regionid = Sql_GetIntData(SqlHandle, 0);
+      int region_control = Sql_GetIntData(SqlHandle, 1);
+      int region_control_prev = Sql_GetIntData(SqlHandle, 2);
 
-            if (region_control == 0)
-                sandoria_regions++;
-            else if (region_control == 1)
-                bastok_regions++;
-            else if (region_control == 2)
-                windurst_regions++;
+      if (region_control == 0)
+        sandoria_regions++;
+      else if (region_control == 1)
+        bastok_regions++;
+      else if (region_control == 2)
+        windurst_regions++;
 
-            if (region_control_prev == 0)
-                sandoria_prev++;
-            else if (region_control_prev == 1)
-                bastok_prev++;
-            else if (region_control_prev == 2)
-                windurst_prev++;
+      if (region_control_prev == 0)
+        sandoria_prev++;
+      else if (region_control_prev == 1)
+        bastok_prev++;
+      else if (region_control_prev == 2)
+        windurst_prev++;
 
-            int32 san_inf = Sql_GetIntData(SqlHandle, 3);
-            int32 bas_inf = Sql_GetIntData(SqlHandle, 4);
-            int32 win_inf = Sql_GetIntData(SqlHandle, 5);
-            int32 bst_inf = Sql_GetIntData(SqlHandle, 6);
-            WBUFB(data,0x1A+(regionid*4)) = conquest::GetInfluenceRanking(san_inf, bas_inf, win_inf, bst_inf);
-            WBUFB(data,0x1B+(regionid*4)) = conquest::GetInfluenceRanking(san_inf, bas_inf, win_inf);
-            WBUFB(data,0x1C+(regionid*4)) = conquest::GetInfluenceGraphics(san_inf, bas_inf, win_inf, bst_inf);
-            WBUFB(data,0x1D+(regionid*4)) = region_control+1;
+      int32 san_inf = Sql_GetIntData(SqlHandle, 3);
+      int32 bas_inf = Sql_GetIntData(SqlHandle, 4);
+      int32 win_inf = Sql_GetIntData(SqlHandle, 5);
+      int32 bst_inf = Sql_GetIntData(SqlHandle, 6);
+      
+      scale_influence_levels(&san_inf, &bas_inf, &win_inf, &bst_inf);
+      
+      WBUFB(data, 0x1A + (regionid * 4)) = conquest::GetInfluenceRanking(san_inf, bas_inf, win_inf, bst_inf);
+      WBUFB(data, 0x1B + (regionid * 4)) = conquest::GetInfluenceRanking(san_inf, bas_inf, win_inf);
+      WBUFB(data, 0x1C + (regionid * 4)) = conquest::GetInfluenceGraphics(san_inf, bas_inf, win_inf, bst_inf);
+      WBUFB(data, 0x1D + (regionid * 4)) = region_control + 1;
 
-            int64 total = san_inf + bas_inf + win_inf;
-            int64 totalBeastmen = total + bst_inf;
+      int64 total = san_inf + bas_inf + win_inf;
+      int64 totalBeastmen = total + bst_inf;
 
-            if (PChar->loc.zone->GetRegionID() == regionid)
-            {
-                WBUFB(data, (0x86)) = (san_inf*100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
-                WBUFB(data, (0x87)) = (bas_inf*100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
-                WBUFB(data, (0x88)) = (win_inf*100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
-                WBUFB(data, (0x89)) = (san_inf*100) / (total == 0 ? 1 : total);
-                WBUFB(data, (0x8A)) = (bas_inf*100) / (total == 0 ? 1 : total);
-                WBUFB(data, (0x8B)) = (win_inf*100) / (total == 0 ? 1 : total);
-                WBUFB(data, (0x94)) = (bst_inf*100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
-            }
-        }
+      if (PChar->loc.zone->GetRegionID() == regionid) {
+        WBUFB(data, (0x86)) = (san_inf * 100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
+        WBUFB(data, (0x87)) = (bas_inf * 100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
+        WBUFB(data, (0x88)) = (win_inf * 100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
+        WBUFB(data, (0x89)) = (san_inf * 100) / (total == 0 ? 1 : total);
+        WBUFB(data, (0x8A)) = (bas_inf * 100) / (total == 0 ? 1 : total);
+        WBUFB(data, (0x8B)) = (win_inf * 100) / (total == 0 ? 1 : total);
+        WBUFB(data, (0x94)) = (bst_inf * 100) / (totalBeastmen == 0 ? 1 : totalBeastmen);
+      }
     }
+  }
 
-	WBUFB(data,(0x04)) = conquest::GetBalance(sandoria_regions, bastok_regions, windurst_regions, sandoria_prev, bastok_prev, windurst_prev);
-    WBUFB(data,(0x05)) = conquest::GetAlliance(sandoria_regions, bastok_regions, windurst_regions, sandoria_prev, bastok_prev, windurst_prev);
+  WBUFB(data, (0x04)) = conquest::GetBalance(sandoria_regions, bastok_regions, windurst_regions, sandoria_prev, bastok_prev, windurst_prev);
+  WBUFB(data, (0x05)) = conquest::GetAlliance(sandoria_regions, bastok_regions, windurst_regions, sandoria_prev, bastok_prev, windurst_prev);
 
-	WBUFB(data,(0x8C)) = conquest::GetNexTally();
-    WBUFL(data,(0x90)) = charutils::GetPoints(PChar, charutils::GetConquestPointsName(PChar).c_str());
-	WBUFB(data,(0x9C)) = 0x01;
+  WBUFB(data, (0x8C)) = conquest::GetNexTally();
+  WBUFL(data, (0x90)) = charutils::GetPoints(PChar, charutils::GetConquestPointsName(PChar).c_str());
+  WBUFB(data, (0x9C)) = 0x01;
 
-	//uint8 packet[] = 
-    //{
-	//    0x80, 0x78, 0x52, 0x03, 0x1a, 0x46, 0x04, 0x00, 0x42, 0x46, 0x04, 0x00, 0x65, 0x3d, 0x04, 0x00
-    //};
-	//memcpy(data+(0xA0), &packet, 16);
+  //uint8 packet[] = 
+  //{
+  //    0x80, 0x78, 0x52, 0x03, 0x1a, 0x46, 0x04, 0x00, 0x42, 0x46, 0x04, 0x00, 0x65, 0x3d, 0x04, 0x00
+  //};
+  //memcpy(data+(0xA0), &packet, 16);
 
-	WBUFB(data,(0xA0)) = 16; // Situation: mamool ja niveau -> (1) 16 (2) 32 (3) 48 (4) 64 (5) 80 (6) 96 (7) 112 (8) 128
-	WBUFB(data,(0xA1)) = 17; // Situation: mercenaire trolls niveau -> 1~12 la suite avec un autre 
-	WBUFB(data,(0xA2)) = 0; // Situation: mamool ja status du siege -> (0) entrainement > (1) en marche > (2) attaque > (3) retraite | (4) defense (5) preparation
-	WBUFB(data,(0xA3)) = 4; // Situation: undead status du siege ? (3) defense (4) entrainement (5) defense
+  WBUFB(data, (0xA0)) = 16; // Situation: mamool ja niveau -> (1) 16 (2) 32 (3) 48 (4) 64 (5) 80 (6) 96 (7) 112 (8) 128
+  WBUFB(data, (0xA1)) = 17; // Situation: mercenaire trolls niveau -> 1~12 la suite avec un autre 
+  WBUFB(data, (0xA2)) = 0; // Situation: mamool ja status du siege -> (0) entrainement > (1) en marche > (2) attaque > (3) retraite | (4) defense (5) preparation
+  WBUFB(data, (0xA3)) = 4; // Situation: undead status du siege ? (3) defense (4) entrainement (5) defense
 
-	WBUFB(data,(0xA4)) = 0; // mamool ja: (13) preparation (26) attaque (32) entrainement
-	WBUFB(data,(0xA5)) = 0; // mamool ja: forces ennemies (1=32)
-	WBUFB(data,(0xA6)) = 0; // mamool ja: miroir archaique (1=2)
-	WBUFB(data,(0xA7)) = 0;
+  WBUFB(data, (0xA4)) = 0; // mamool ja: (13) preparation (26) attaque (32) entrainement
+  WBUFB(data, (0xA5)) = 0; // mamool ja: forces ennemies (1=32)
+  WBUFB(data, (0xA6)) = 0; // mamool ja: miroir archaique (1=2)
+  WBUFB(data, (0xA7)) = 0;
 
-	WBUFB(data,(0xA8)) = 0; // trolls: forces ennemies (66=8)
-	WBUFB(data,(0xA9)) = 0; // trolls: (70) attaque
-	WBUFB(data,(0xAA)) = 0; // trolls: miroir archaique (4=8)
-	WBUFB(data,(0xAB)) = 0;
-	WBUFB(data,(0xAC)) = 0; // undead: forces ennemies (101=12)
-	WBUFB(data,(0xAD)) = 0; // undead: (61) preparation
-	WBUFB(data,(0xAE)) = 0; // undead: miroir archaique (4=8)
-	WBUFB(data,(0xAF)) = 0;
+  WBUFB(data, (0xA8)) = 0; // trolls: forces ennemies (66=8)
+  WBUFB(data, (0xA9)) = 0; // trolls: (70) attaque
+  WBUFB(data, (0xAA)) = 0; // trolls: miroir archaique (4=8)
+  WBUFB(data, (0xAB)) = 0;
+  WBUFB(data, (0xAC)) = 0; // undead: forces ennemies (101=12)
+  WBUFB(data, (0xAD)) = 0; // undead: (61) preparation
+  WBUFB(data, (0xAE)) = 0; // undead: miroir archaique (4=8)
+  WBUFB(data, (0xAF)) = 0;
 
-	WBUFL(data,(0xB0)) = charutils::GetPoints(PChar, "imperial_standing");
+  WBUFL(data, (0xB0)) = charutils::GetPoints(PChar, "imperial_standing");
+}
+
+
+void CConquestPacket::scale_influence_levels(int *dorkia, int *bastok, int *windurst, int *beastmen) {
+  long double total = (long double) (((long double) *dorkia) + ((long double) *bastok) + ((long double) *windurst) + ((long double) *beastmen));
+  long double dorkia_scaled = (long double) ((long double) *dorkia * 5000) / total;
+  long double bastok_scaled = (long double) ((long double) *bastok * 5000) / total;
+  long double windurst_scaled = (long double) ((long double) *windurst * 5000) / total;
+  long double beastmen_scaled = (long double) ((long double) *beastmen * 5000) / total;
+
+  *dorkia = (int) dorkia_scaled;
+  *bastok = (int) bastok_scaled;
+  *windurst = (int) windurst_scaled;
+  *beastmen = (int) beastmen_scaled;
 }
