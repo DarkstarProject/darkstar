e35436252fa3fc3ddca1b9abec411f37a595aae5
OzmaEra <f723a179@opayq.com>
2015-06-02 04:25:53 +0200

    textid shift

--- scripts/zones/Crawlers_Nest/TextIDs.lua
+++ scripts/zones/Crawlers_Nest/TextIDs.lua
@@ -1,4 +1,4 @@
--- Variable TextID   Description text
+--- Variable TextID   Description text
 
 -- General Texts
 ITEM_CANNOT_BE_OBTAINED = 6569; -- You cannot obtain the item <item>. Come back after sorting your inventory.
