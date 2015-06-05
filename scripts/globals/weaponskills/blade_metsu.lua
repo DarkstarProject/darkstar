31fe00925cb81c182f2baee3b4c36319fd9788a3
Kthulu <Jcarnahan85@gmail.com>
2015-06-05 04:45:50 -0500

    Update blade_metsu.lua
    Incorrect modifier value, should be set to 80% instead of 60. 
    
    http://ffxiclopedia.wikia.com/wiki/Blade:_Metsu
    
    I'll be working on reworking paralyze on the whole this week so kikoku should be good across the board for 75 cap

--- scripts/globals/weaponskills/blade_metsu.lua
+++ scripts/globals/weaponskills/blade_metsu.lua
@@ -10,7 +10,7 @@
 -- Aligned with the Shadow Gorget, Breeze Gorget & Thunder Gorget.
 -- Aligned with the Shadow Belt, Breeze Belt & Thunder Belt.
 -- Element: None
--- Modifiers: DEX:60%
+-- Modifiers: DEX:80%
 -- 100%TP    200%TP    300%TP
 -- 3.00      3.00      3.00
 -----------------------------------
@@ -22,7 +22,7 @@ function onUseWeaponSkill(player, target, wsID)
 	local params = {};
 	params.numHits = 1;
 	params.ftp100 = 3; params.ftp200 = 3; params.ftp300 = 3;
-	params.str_wsc = 0.0; params.dex_wsc = 0.6; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
+	params.str_wsc = 0.0; params.dex_wsc = 0.8; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 0.0; params.chr_wsc = 0.0;
 	params.crit100 = 0.0; params.crit200 = 0.0; params.crit300 = 0.0;
 	params.canCrit = false;
 	params.acc100 = 0.0; params.acc200= 0.0; params.acc300= 0.0;
