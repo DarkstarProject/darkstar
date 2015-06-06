ea0864c7c2de9d4e6417285bf3c22f9566b744ee
Jupiter065 <Jupiter065@yahoo.ca>
2015-06-06 16:17:38 -0600

    SCRIPT: Cold wave fix
    Cold wave was using elemental magic

--- scripts/globals/spells/bluemagic/cold_wave.lua
+++ scripts/globals/spells/bluemagic/cold_wave.lua
@@ -35,7 +35,7 @@ function onSpellCast(caster,target,spell)
         spell:setMsg(75); -- no effect
     else        
         local dINT = caster:getStat(MOD_INT)-target:getStat(MOD_INT);
-        local resist = applyResistance(caster,spell,target,dINT,36,0);
+        local resist = applyResistance(caster,spell,target,dINT,BLUE_SKILL,0);
         if(resist <= 0.0) then
             spell:setMsg(85);
         else
