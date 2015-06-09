b5aaeb1c8c948e54f6b34955334d453e0a7da8b3
kitala1 <severanceday@hotmail.com>
2015-06-09 14:30:58 -0400

    Pandemonium Warden fixes SQL: Added missing mobid's for Pandemonium Lamps SCRIPTS: with the aid of TeoTwawki, PW is now mostly functional

--- scripts/zones/Aydeewa_Subterrane/npcs/qm2.lua
+++ scripts/zones/Aydeewa_Subterrane/npcs/qm2.lua
@@ -1,7 +1,7 @@
 -----------------------------------
 -- Area: Aydeewa Subterrane
 --  NPC: ??? (Spawn Pandemonium Warden)
--- @pos -217 35 12 68
+-- @pos 200 33 -140 68
 -----------------------------------
 package.loaded["scripts/zones/Aydeewa_Subterrane/TextIDs"] = nil;
 -----------------------------------
@@ -16,10 +16,6 @@ function onTrade(player,npc,trade)
     -- Pandemonium key
     if (trade:hasItemQty(2572,1) and trade:getItemCount() == 1 and GetMobAction(17056168) == 0) then
         SpawnMob(17056168,180):updateClaim(player); -- Pandemonium Warden
-        -- Pets
-        for i = 17056170, 17056177, 1 do 
-            SpawnMob(i,180):updateClaim(player);
-        end
         player:tradeComplete();
     end
 end;
