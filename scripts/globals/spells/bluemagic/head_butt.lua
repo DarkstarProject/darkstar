21ea8eec3f4c8e4ada18552f9fd0c8e10eb87dbf
TeoTwawki <arawn.annwn@yandex.com>
2015-06-09 03:46:50 -0400

    Scripts: blue magic head_butt is no longer 100% success rate stun, now has an actual resistance check.

--- scripts/globals/spells/bluemagic/head_butt.lua
+++ scripts/globals/spells/bluemagic/head_butt.lua
@@ -30,7 +30,8 @@ end;
 -----------------------------------------
 
 function onSpellCast(caster,target,spell)
-
+    local dINT = caster:getStat(MOD_INT) - target:getStat(MOD_INT);
+    local resist = applyResistanceEffect(caster,spell,target,dINT,SKILL_BLU,0,EFFECT_STUN)
     local params = {};
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
         params.tpmod = TPMOD_DAMAGE;
@@ -49,13 +50,17 @@ function onSpellCast(caster,target,spell)
         params.int_wsc = 0.2;
         params.mnd_wsc = 0.0;
         params.chr_wsc = 0.0;
-    damage = BluePhysicalSpell(caster, target, spell, params);
+    local damage = BluePhysicalSpell(caster, target, spell, params);
     damage = BlueFinalAdjustments(caster, target, spell, damage, params);
-    
-    if(target:hasStatusEffect(EFFECT_STUN)) then
+
+    if (target:hasStatusEffect(EFFECT_STUN)) then
+        -- caster:PrintToPlayer(string.format("was already stun. resLv: %f", resist));
         spell:setMsg(75); -- no effect
-    else    
+    elseif (resist > 0.25) then -- This line may need adjusting for retail accuracy.
+        -- caster:PrintToPlayer(string.format("landed stun. resLv: %f", resist));
         target:addStatusEffect(EFFECT_STUN,0,0,math.random(1,5));
+    -- else
+        -- caster:PrintToPlayer(string.format("did not land. resLv: %f", resist));
     end
 
     return damage;
