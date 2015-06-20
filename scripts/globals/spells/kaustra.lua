281e7d1969ed263785734e166ad180e17ad3a721
TeoTwawki <arawn.annwn@yandex.com>
2015-06-20 04:33:52 -0400

    Scripts: Fixed crashbug in Kaustra after @xdemolish showed me what was wrong. Also tidied up formatting.
    [ci skip]

--- scripts/globals/spells/kaustra.lua
+++ scripts/globals/spells/kaustra.lua
@@ -1,43 +1,42 @@
 --------------------------------------
--- 	Spell: Kaustra
--- 	Consumes 20% of your maximum MP. Relentless 
---  dark damage slowly devours an enemy.
+-- Spell: Kaustra
+-- Consumes 20% of your maximum MP. Relentless
+-- dark damage slowly devours an enemy.
 --------------------------------------
- 
+
 require("scripts/globals/settings");
 require("scripts/globals/status");
 require("scripts/globals/magic");
 
 -----------------------------------------
--- OnSpellCast
+-- onMagicCastingCheck
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-	return 0;
+    return 0;
 end;
 
+-----------------------------------------
+-- OnSpellCast
+-----------------------------------------
+
 function onSpellCast(caster,target,spell)
-	
-	local skill = caster:getSkillLevel(DARK_MAGIC_SKILL);
-	local dINT = caster:getStat(MOD_INT) - target:getStat(MOD_INT);
-	
-	if (skill > 500) then
-		skill = 500;
-	end
-	
-	local duration = 3 * (1 + (skill / 11));
-	local base = math.floor((math.floor(0.67 * caster:getMainLvl())/10)*(37 + math.floor(0.67*dINT)))
-	
-	local resist = applyResistance(caster,spell,target,dINT,skill,0)
-	
-	duration = duration * resist;
-	local dmg = base * resist;
-	
-	dmg = adjustForTarget(target,dmg,spell:getElement());
-	
-	dmg = finalMagicAdjustments(caster,target,spell,dmg);
-	
-	target:addStatusEffect(EFFECT_KAUSTRA,math.floor(dmg/3),3,duration);
-	
-	return dmg;
+    local skill = caster:getSkillLevel(DARK_MAGIC_SKILL);
+    local dINT = caster:getStat(MOD_INT) - target:getStat(MOD_INT);
+
+    if (skill > 500) then
+        skill = 500;
+    end
+
+    local duration = 3 * (1 + (skill / 11));
+    local base = math.floor((math.floor(0.67 * caster:getMainLvl())/10)*(37 + math.floor(0.67*dINT)))
+    local resist = applyResistance(caster,spell,target,dINT,DARK_MAGIC_SKILL,0);
+    local dmg = base * resist;
+    duration = duration * resist;
+    dmg = adjustForTarget(target,dmg,spell:getElement());
+    dmg = finalMagicAdjustments(caster,target,spell,dmg);
+
+    target:addStatusEffect(EFFECT_KAUSTRA,math.floor(dmg/3),3,duration);
+
+    return dmg;
 end;
