05a0e46fa3a19ac75e154b104f2b1f07632c8762
Vivitarut <stbigras@gmail.com>
2015-06-27 05:30:08 -0400

    Scripts/ quest: Gifts of the griffons - Incorrect csid for Loillie after trading plume.

--- scripts/zones/Southern_San_dOria_[S]/npcs/Loillie.lua
+++ scripts/zones/Southern_San_dOria_[S]/npcs/Loillie.lua
@@ -15,7 +15,7 @@ require("scripts/globals/quests");
 function onTrade(player,npc,trade)
 	if(player:getQuestStatus(CRYSTAL_WAR,GIFTS_OF_THE_GRIFFON) == QUEST_ACCEPTED and player:getVar("GiftsOfGriffonProg") == 2) then
 		if(trade:hasItemQty(2528,1) and trade:getItemCount() == 1 and player:getVar("GiftsOfGriffon_Loil") == 0) then
-			player:startEvent(0x01F) -- Gifts of Griffon Trade
+			player:startEvent(0x01D) -- Gifts of Griffon Trade
 		end
 	end
 end;
@@ -44,7 +44,7 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-	if(csid == 0x01F) then -- Gifts Of Griffon Trade
+	if(csid == 0x01D) then -- Gifts Of Griffon Trade
 		player:tradeComplete();
 		player:setVar("GiftsOfGriffon_Loil",1);
 	end
