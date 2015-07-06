9d8fbad598555e6906898f94b4e4852f0f9d4cee
demolish <xdemolish@hotmail.com>
2015-07-06 20:47:33 +0100

    CORE: status when zoning now gets set in CServerIPPacket - prevent CServerIPPacket being sent while char is already in the middle of zoning/logging (should get rid of zoning bug forever, if not then potential causes are narrowed down!)

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -831,7 +831,7 @@ int32 map_cleanup(uint32 tick, CTaskMgr::CTask* PTask)
 
                         ShowDebug(CL_CYAN"map_cleanup: %s timed out, closing session\n" CL_RESET, PChar->GetName());
 
-                        PChar->status = STATUS_SHUTDOWN;
+                        
                         PacketParser[0x00D](map_session_data, PChar, 0);
                     }
                     else
