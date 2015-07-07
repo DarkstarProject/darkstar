a6d0f6fe22afc251e983376d396283dde4110bd0
Halcyon <vyandra@gmail.com>
2015-07-07 07:52:22 -0700

    SCRIPTS: Gullible's Travels code clarity adjustments
    Changed '1' to 'Quest Accepted' in line 35
    Made the comments spacing more uniform

--- scripts/zones/Kazham/npcs/Magriffon.lua
+++ scripts/zones/Kazham/npcs/Magriffon.lua
@@ -32,15 +32,16 @@ end;
 function onTrigger(player,npc)
     
     local gulliblesTravelsStatus = player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS);
-    local magriffonGilRequest = gulliblesTravelsStatus == 1 and  --Checks if Gullible's Travels is started and gets previously requested gil #
-        player:getVar("MAGRIFFON_GIL_REQUEST");   
-    if(gulliblesTravelsStatus == QUEST_AVAILABLE and player:getFameLevel(KAZHAM) >= 6) then -- Gullible's Travels: Fame check and quest available check  
-        local gil = math.random(10, 30) * 1000; -- # 10,000 - 30,000 in ticks of 1k
+    local magriffonGilRequest = gulliblesTravelsStatus == QUEST_ACCEPTED and    --Checks if Gullible's Travels is started and gets previously requested gil #
+        player:getVar("MAGRIFFON_GIL_REQUEST");  
+        
+    if(gulliblesTravelsStatus == QUEST_AVAILABLE and player:getFameLevel(KAZHAM) >= 6) then   -- Gullible's Travels: Fame check and quest available check  
+        local gil = math.random(10, 30) * 1000;    -- # 10,000 - 30,000 in ticks of 1k
         player:setVar("MAGRIFFON_GIL_REQUEST", gil); 
         player:startEvent(0x0090, 0, gil);
-    elseif(gulliblesTravelsStatus == QUEST_ACCEPTED) then -- Gullible's Travels: 'In progress' check
+    elseif(gulliblesTravelsStatus == QUEST_ACCEPTED) then    -- Gullible's Travels: 'In progress' check
         player:startEvent(0x0091, 0, magriffonGilRequest);
-    elseif(gulliblesTravelsStatus == QUEST_COMPLETED) then -- Gullible's Travels: 'Complete' check
+    elseif(gulliblesTravelsStatus == QUEST_COMPLETED) then    -- Gullible's Travels: 'Complete' check
         player:startEvent(0x0093);
     else
         player:startEvent(0x008F);
@@ -60,15 +61,15 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    if(csid == 0x0090 and option == 1)  then        -- Gullible's Travels: First CS
+    if(csid == 0x0090 and option == 1)  then    -- Gullible's Travels: First CS
         player:addQuest(OUTLANDS, GULLIBLES_TRAVELS);
-    elseif(csid == 0x0092) then                     -- Gullible's Travels: Final CS
+    elseif(csid == 0x0092) then    -- Gullible's Travels: Final CS
         player:tradeComplete();
-        player:addGil(tradedGil - player:getVar("MAGRIFFON_GIL_REQUEST")); -- Returns extra gil paid
+        player:addGil(tradedGil - player:getVar("MAGRIFFON_GIL_REQUEST"));    -- Returns extra gil paid
         tradedGil = 0;
         player:setVar("MAGRIFFON_GIL_REQUEST", 0);
         player:addFame(KAZHAM, WIN_FAME*30);
-        player:setTitle(285);                       -- Global Variable not working for this quest
+        player:setTitle(285);    -- Global Variable not working for this quest
         player:completeQuest(OUTLANDS, GULLIBLES_TRAVELS);
     end
 end;
