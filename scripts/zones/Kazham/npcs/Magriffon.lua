34b6f272295258c90b9e6acdd30fa02a3fff3ed2
Halcyon <vyandra@gmail.com>
2015-07-07 05:12:28 -0700

    SCRIPTS:  Updated Gullible's Travel's variables
    -Added gulliblesTravelsStatus local variable
    -Added magriffonGilRequest local variable
    -Changed gil amounts generated to ticks of 1,000 within his range
    -Cleaned up tradedGil global variable
    -Touched up some of the comments

--- scripts/zones/Kazham/npcs/Magriffon.lua
+++ scripts/zones/Kazham/npcs/Magriffon.lua
@@ -7,18 +7,18 @@
 
 package.loaded["scripts/zones/Kazham/TextIDs"] = nil;
 require("scripts/globals/settings");
-require("scripts/globals/npc_util")
 require("scripts/globals/quests");
 require("scripts/globals/titles");
 require("scripts/zones/Kazham/TextIDs");
+
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
-    if(player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS) == 1) then
-        tradedGil = trade:getGil();
+    if(player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS) == QUEST_ACCEPTED) then
+        tradedGil = trade:getGil();        
         if(tradedGil >= player:getVar("MAGRIFFON_GIL_REQUEST")) then
             player:startEvent(0x0092);
         end
@@ -30,12 +30,17 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-    if(player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS) == 0 and player:getFameLevel(KAZHAM) >= 6) then --Checks if Gullible's Travels hasn't been started or completed and that player has at least 6 Kazham fame
-        player:setVar("MAGRIFFON_GIL_REQUEST", math.random(10000,30000));  --Generates an amount between 20,000 and 30,000 to request from the player
-        player:startEvent(0x0090, 0, player:getVar("MAGRIFFON_GIL_REQUEST"));
-    elseif(player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS) == 1) then --Checks if player is currently on Gullible's Travels
-        player:startEvent(0x0091, 0, player:getVar("MAGRIFFON_GIL_REQUEST"));
-    elseif(player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS) == 2) then --Checks if player has completed Gullible's Travels
+    
+    local gulliblesTravelsStatus = player:getQuestStatus(OUTLANDS, GULLIBLES_TRAVELS);
+    local magriffonGilRequest = gulliblesTravelsStatus == 1 and  --Checks if Gullible's Travels is started and gets previously requested gil #
+        player:getVar("MAGRIFFON_GIL_REQUEST");   
+    if(gulliblesTravelsStatus == QUEST_AVAILABLE and player:getFameLevel(KAZHAM) >= 6) then -- Gullible's Travels: Fame check and quest available check  
+        local gil = math.random(10, 30) * 1000; -- # 10,000 - 30,000 in ticks of 1k
+        player:setVar("MAGRIFFON_GIL_REQUEST", gil); 
+        player:startEvent(0x0090, 0, gil);
+    elseif(gulliblesTravelsStatus == QUEST_ACCEPTED) then -- Gullible's Travels: 'In progress' check
+        player:startEvent(0x0091, 0, magriffonGilRequest);
+    elseif(gulliblesTravelsStatus == QUEST_COMPLETED) then -- Gullible's Travels: 'Complete' check
         player:startEvent(0x0093);
     else
         player:startEvent(0x008F);
@@ -55,13 +60,15 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    if(csid == 0x0090 and option == 1)  then
+    if(csid == 0x0090 and option == 1)  then        -- Gullible's Travels: First CS
         player:addQuest(OUTLANDS, GULLIBLES_TRAVELS);
-    elseif(csid == 0x0092) then
+    elseif(csid == 0x0092) then                     -- Gullible's Travels: Final CS
         player:tradeComplete();
-        player:addGil(tradedGil - player:getVar("MAGRIFFON_GIL_REQUEST")); --Returns the player any extra gill they paid
+        player:addGil(tradedGil - player:getVar("MAGRIFFON_GIL_REQUEST")); -- Returns extra gil paid
+        tradedGil = 0;
+        player:setVar("MAGRIFFON_GIL_REQUEST", 0);
         player:addFame(KAZHAM, WIN_FAME*30);
-        player:setTitle(285); --Global Variable not working for this quest
+        player:setTitle(285);                       -- Global Variable not working for this quest
         player:completeQuest(OUTLANDS, GULLIBLES_TRAVELS);
     end
 end;
