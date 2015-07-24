b8c729358255f10782a90186e65d06c57e9b5513
Turttle <brocollycookies@gmail.com>
2015-07-24 11:08:21 -0400

    Spirit Within
    Adjusted the ws to match new TP values. Corrected calculations using
    correct old values from BGwiki
    https://www.bg-wiki.com/index.php?title=Spirits_Within&oldid=104829 and
    FFXIclopedia
    http://ffxiclopedia.wikia.com/wiki/Spirits_Within?oldid=306648

--- scripts/globals/weaponskills/spirits_within.lua
+++ scripts/globals/weaponskills/spirits_within.lua
@@ -7,9 +7,11 @@
 -- Delivers an unavoidable attack. Damage varies with HP and TP.
 -- Not aligned with any "elemental gorgets" or "elemental belts" due to it's absence of Skillchain properties.
 -- Element: None
+-- Modifiers: HP (Damage is calculated by TP Multiplier times current HP) 
 -- Modifiers: HP:
--- 100%TP    200%TP    300%TP
--- 12.5%       50%      100%
+-- 1000%TP    2000%TP    3000%TP
+-- 4/32       6/32      15/32   (Old Values - 2008 )
+-- 12.5% HP   50% HP    100% HP (New Values - ADOULIN)
 -----------------------------------
 require("scripts/globals/status");
 require("scripts/globals/settings");
@@ -21,22 +23,22 @@ function onUseWeaponSkill(player, target, wsID)
 	local HP = player:getHP();
 	local TP = player:getTP();
 	local WSC = 0;
-	if (TP == 300) then
-		WSC = HP * 0.46875;
-	elseif (TP >= 200) then
-		WSC = HP * ((TP - 200) / (100 / (0.46875 - 0.1875)));
-	elseif (TP >= 100) then
-		WSC = HP * ((TP - 100) / (100 / (0.1875 - 0.125)));
+	if (TP == 3000) then
+		WSC = HP * (15/32);
+	elseif (TP >= 2000) then
+		WSC = HP * (6/32);
+	elseif (TP >= 1000) then
+		WSC = HP * (4/32);
 	end
 
 	if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
 		-- Damage calculations changed based on: http://www.bg-wiki.com/bg/Spirits_Within http://www.bluegartr.com/threads/121610-Rehauled-Weapon-Skills-tier-lists?p=6142188&viewfull=1#post6142188
-		if (TP == 300) then
+		if (TP == 3000) then
 		WSC = HP;
-		elseif (TP >= 200) then
-		WSC = HP * .5
-		elseif (TP >= 100) then
-		WSC = HP * .125;
+		elseif (TP >= 2000) then
+		WSC = floor(HP * ( floor(0.72*TP) - 96 ) ÷ 256 )
+		elseif (TP >= 1000) then
+		WSC = floor(HP * ( floor(0.16*TP) + 16 ) ÷ 256 )
 		end
 	end
 
