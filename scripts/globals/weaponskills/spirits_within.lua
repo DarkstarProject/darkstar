74434b172db9268ac28a36a410cb2bfc08e9768f
Turttle <brocollycookies@gmail.com>
2015-07-24 22:49:09 -0400

    Spirit within patch
    Fixed the formulas.

--- scripts/globals/weaponskills/spirits_within.lua
+++ scripts/globals/weaponskills/spirits_within.lua
@@ -23,21 +23,21 @@ function onUseWeaponSkill(player, target, wsID)
 	local TP = player:getTP();
 	local WSC = 0;
 	if (TP == 300) then
-		WSC = HP * (15/32);
+		WSC = floor(HP * ( floor(0.72*TP) - 96 ) / 256 ); --0.46875 @300 TP
 	elseif (TP >= 200) then
-		WSC = HP * (6/32);
+		WSC = floor(HP * ( floor(0.47*TP) - 46) / 256 ); --0.1875 @200 TP 
 	elseif (TP >= 100) then
-		WSC = HP * (4/32);
+		WSC = floor(HP * ( floor(0.40*TP) - 8 ) / 256 );--0.125 @100 TP
 	end
 
 	if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
 		-- Damage calculations changed based on: http://www.bg-wiki.com/bg/Spirits_Within http://www.bluegartr.com/threads/121610-Rehauled-Weapon-Skills-tier-lists?p=6142188&viewfull=1#post6142188
 		if (TP == 300) then
-		WSC = HP;
+		WSC =  (HP * ( floor(1.00*TP) - 44 ) / 256 );-- 1.00 @300 TP
 		elseif (TP >= 200) then
-		WSC = floor(HP * ( floor(0.72*TP) - 96 ) / 256 )
+		WSC = (HP * ( floor(0.72*TP) - 16 ) / 256 );-- 0.50 @200 TP
 		elseif (TP >= 100) then
-		WSC = floor(HP * ( floor(0.16*TP) + 16 ) / 256 )
+		WSC = (HP * ( floor(0.40*TP) - 8 ) / 256 );--0.125 @100 TP
 		end
 	end
 
