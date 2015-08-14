79da69aff96997bd2b8cbf47d18eaec354373406
Hozu <hozu30825@gmail.com>
2015-08-14 16:54:38 -0400

    Scripts: Fixed the logic for BLU spells that apply multiple buffs.
    Actually grants both buffs now.

--- scripts/globals/spells/bluemagic/reactor_cool.lua
+++ scripts/globals/spells/bluemagic/reactor_cool.lua
@@ -49,12 +49,15 @@ function onSpellCast(caster,target,spell)
         caster:delStatusEffect(EFFECT_DIFFUSION);
     end;
 
-    if (target:addStatusEffect(typeEffectOne,powerOne,0,duration) == false and target:addStatusEffect(typeEffectTwo,powerTwo,0,duration) == false) then
+    if (target:addStatusEffect(typeEffectOne,powerOne,0,duration) == false and target:addStatusEffect(typeEffectTwo,powerTwo,0,duration) == false) then -- both statuses fail to apply
         spell:setMsg(75);
-    elseif (target:addStatusEffect(typeEffectOne,powerOne,0,duration) == false) then
+    elseif (target:addStatusEffect(typeEffectOne,powerOne,0,duration) == false) then -- the first status fails to apply
+        target:addStatusEffect(typeEffectTwo,powerTwo,0,duration)
         spell:setMsg(230);
         returnEffect = typeEffectTwo;
     else
+        target:addStatusEffect(typeEffectOne,powerOne,0,duration)
+        target:addStatusEffect(typeEffectTwo,powerTwo,0,duration)
         spell:setMsg(230);
     end;
 
