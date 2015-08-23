fbbb1d8693600373723e3b42cbe93577205c930c
Zenny89 <abattigaglia@gmail.com>
2015-08-23 16:58:58 -0400

    SCRIPTS: Fixed Claws of the Griffon Quest

---
+++ scripts/zones/Jugner_Forest_[S]/mobs/FingerfilcherD.lua
@@ -0,0 +1,34 @@
+-----------------------------------
+-- Area: Jugner Forest [S]
+-- NM: Fingerfilcher Dradzad
+-----------------------------------
+
+require("scripts/globals/titles");
+require("scripts/globals/status");
+
+-----------------------------------
+-- onMobEngaged Action
+-----------------------------------
+
+function onMobEngaged(mob,target)
+
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob,target)
+
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+    if (killer:getQuestStatus(CRYSTAL_WAR, CLAWS_OF_THE_GRIFFON) == QUEST_ACCEPTED) then
+        killer:needToZone(true);
+        killer:setVar("FingerfilcherKilled",1);
+    end
+end;
