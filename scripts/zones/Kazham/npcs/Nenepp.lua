d3b8e0e1f55988b9398142c78e3c2f74fad2541d
waytim <waytim@yahoo.com>
2015-08-24 18:11:00 -0400

    Update Nenepp.lua

--- scripts/zones/Kazham/npcs/Nenepp.lua
+++ scripts/zones/Kazham/npcs/Nenepp.lua
@@ -95,7 +95,7 @@ end;
 -- onEventFinish
 -----------------------------------
 
-function onEventFinish(player,csid,option)
+function onEventFinish(player,csid,option,npc)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 
