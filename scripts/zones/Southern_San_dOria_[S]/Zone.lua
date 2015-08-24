26e7e6c65f08ae3b21a205004720939b7e464749
Zenny89 <abattigaglia@gmail.com>
2015-08-24 18:51:27 -0400

    SCRIPTS/SQL: Added WOTG Missions

--- scripts/zones/Southern_San_dOria_[S]/Zone.lua
+++ scripts/zones/Southern_San_dOria_[S]/Zone.lua
@@ -9,6 +9,7 @@ package.loaded["scripts/zones/Southern_San_dOria_[S]/TextIDs"] = nil;
 require("scripts/globals/settings");
 require("scripts/zones/Southern_San_dOria_[S]/TextIDs");
 require("scripts/globals/quests");
+require("scripts/globals/missions");
 
 -----------------------------------
 -- onInitialize
@@ -26,10 +27,17 @@ function onZoneIn(player,prevZone)
     if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
         player:setPos(103.978,-1,-47.951,158);
     end
-    if (prevZone == 81 and player:getQuestStatus(CRYSTAL_WAR, KNOT_QUITE_THERE) == QUEST_ACCEPTED and player:getVar("KnotQuiteThere") == 2) then
-        cs = 0x003E;
-    elseif (prevZone == 81 and player:getQuestStatus(CRYSTAL_WAR, DOWNWARD_HELIX) == QUEST_ACCEPTED and player:getVar("DownwardHelix") == 0) then
-        cs = 0x0041;
+    if (prevZone == 81) then
+        if (player:getQuestStatus(CRYSTAL_WAR, KNOT_QUITE_THERE) == QUEST_ACCEPTED and player:getVar("KnotQuiteThere") == 2) then
+            cs = 0x003E;
+        elseif (player:getQuestStatus(CRYSTAL_WAR, DOWNWARD_HELIX) == QUEST_ACCEPTED and player:getVar("DownwardHelix") == 0) then
+            cs = 0x0041;
+        elseif (player:getCurrentMission(WOTG) == CAIT_SITH and
+               (player:getQuestStatus(CRYSTAL_WAR, WRATH_OF_THE_GRIFFON) == QUEST_COMPLETED or
+                player:getQuestStatus(CRYSTAL_WAR, A_MANIFEST_PROBLEM) == QUEST_COMPLETED or
+                player:getQuestStatus(CRYSTAL_WAR, BURDEN_OF_SUSPICION) == QUEST_COMPLETED)) then
+            cs = 0x0043;
+        end
     end    
     return cs;
 end;
@@ -61,5 +69,8 @@ function onEventFinish(player,csid,option)
         player:setVar("KnotQuiteThere",3);
     elseif (csid == 0x0041) then
         player:setVar("DownwardHelix",1);
+    elseif (csid == 0x0043) then
+        player:completeMission(WOTG, CAIT_SITH);
+        player:addMission(WOTG, THE_QUEEN_OF_THE_DANCE);
     end
 end;
