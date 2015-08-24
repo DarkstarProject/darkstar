3ab0e2a8e174310af5ee0fa75bfb2c67b03453c6
waytim <waytim@yahoo.com>
2015-08-24 18:11:36 -0400

    Update Roropp.lua

--- scripts/zones/Kazham/npcs/Roropp.lua
+++ scripts/zones/Kazham/npcs/Roropp.lua
@@ -230,7 +230,7 @@ end;
 -- onEventFinish
 -----------------------------------
 
-function onEventFinish(player,csid,option)
+function onEventFinish(player,csid,option,npc)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 
