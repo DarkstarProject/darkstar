11e7a9cc7f17eb92f5f63afa426d11cf83efa54b
waytim <waytim@yahoo.com>
2015-08-24 17:55:43 -0400

    Update Mumupp.lua

--- scripts/zones/Kazham/npcs/Mumupp.lua
+++ scripts/zones/Kazham/npcs/Mumupp.lua
@@ -111,7 +111,7 @@ end;
 -- onEventFinish
 -----------------------------------
 
-function onEventFinish(player,csid,option,npc)
+function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 
