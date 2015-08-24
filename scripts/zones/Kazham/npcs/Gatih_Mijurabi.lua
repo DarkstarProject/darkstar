16d3499a87e44b83109e5c85175ae898c3520db0
waytim <waytim@yahoo.com>
2015-08-24 17:47:13 -0400

    Update Gatih_Mijurabi.lua

--- scripts/zones/Kazham/npcs/Gatih_Mijurabi.lua
+++ scripts/zones/Kazham/npcs/Gatih_Mijurabi.lua
@@ -50,7 +50,7 @@ end;
 -- onEventFinish
 -----------------------------------
 
-function onEventFinish(player,csid,option)    
+function onEventFinish(player,csid,option)
 	-- printf("CSID: %u",csid);
 	-- printf("RESULT: %u",option);
     if (csid == 0x00BF) then
