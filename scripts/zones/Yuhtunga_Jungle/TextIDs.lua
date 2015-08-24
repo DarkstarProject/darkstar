831b3aba002f7aaf6fcee755ff979ec7ddf2d262
waytim <waytim@yahoo.com>
2015-08-24 18:41:13 -0400

    Update TextIDs.lua
    id shift

--- scripts/zones/Yuhtunga_Jungle/TextIDs.lua
+++ scripts/zones/Yuhtunga_Jungle/TextIDs.lua
@@ -5,34 +5,34 @@ ITEM_CANNOT_BE_OBTAINED = 6379; -- You cannot obtain the item <item>. Come back 
           ITEM_OBTAINED = 6383; -- Obtained: <item>.
            GIL_OBTAINED = 6384; -- Obtained <number> gil.
        KEYITEM_OBTAINED = 6386; -- Obtained key item: <keyitem>.
-        BEASTMEN_BANNER = 7119; -- There is a beastmen's banner.
- FISHING_MESSAGE_OFFSET = 7539; -- You can't fish here.
+        BEASTMEN_BANNER = 7121; -- There is a beastmen's banner.
+ FISHING_MESSAGE_OFFSET = 7541; -- You can't fish here.
 
 -- Conquest
-CONQUEST = 7206; -- You've earned conquest points!
+CONQUEST = 7208; -- You've earned conquest points!
 
 -- Logging
-   LOGGING_IS_POSSIBLE_HERE = 7681; -- Logging is possible here if you have
-HARVESTING_IS_POSSIBLE_HERE = 7688; -- Harvesting is possible here if you have
+   LOGGING_IS_POSSIBLE_HERE = 7683; -- Logging is possible here if you have
+HARVESTING_IS_POSSIBLE_HERE = 7690; -- Harvesting is possible here if you have
 
 -- ZM4 Dialog
-    CANNOT_REMOVE_FRAG = 7660; -- It is an oddly shaped stone monument. A shining stone is embedded in it, but cannot be removed...
- ALREADY_OBTAINED_FRAG = 7661; -- You have already obtained this monument's
-ALREADY_HAVE_ALL_FRAGS = 7662; -- You have obtained all of the fragments. You must hurry to the ruins of the ancient shrine!
-       FOUND_ALL_FRAGS = 7663; -- You now have all 8 fragments of light!
-       ZILART_MONUMENT = 7664; -- It is an ancient Zilart monument.
+    CANNOT_REMOVE_FRAG = 7662; -- It is an oddly shaped stone monument. A shining stone is embedded in it, but cannot be removed...
+ ALREADY_OBTAINED_FRAG = 7663; -- You have already obtained this monument's
+ALREADY_HAVE_ALL_FRAGS = 7664; -- You have obtained all of the fragments. You must hurry to the ruins of the ancient shrine!
+       FOUND_ALL_FRAGS = 7665; -- You now have all 8 fragments of light!
+       ZILART_MONUMENT = 7666; -- It is an ancient Zilart monument.
 
 -- Rafflesia Scent
-FLOWER_BLOOMING = 7640; -- A large flower is blooming.
-FOUND_NOTHING_IN_FLOWER = 7643; -- You find nothing inside the flower.
-FEEL_DIZZY = 7644; -- You feel slightly dizzy. You must have breathed in too much of the pollen.
+FLOWER_BLOOMING = 7642; -- A large flower is blooming.
+FOUND_NOTHING_IN_FLOWER = 7645; -- You find nothing inside the flower.
+FEEL_DIZZY = 7646; -- You feel slightly dizzy. You must have breathed in too much of the pollen.
 -- Other
 NOTHING_HAPPENS = 119; -- Nothing happens...
 
 -- conquest Base
-CONQUEST_BASE = 7038; -- Tallying conquest results...
+CONQUEST_BASE = 7040; -- Tallying conquest results...
 
 -- chocobo digging
-DIG_THROW_AWAY = 7552; -- You dig up ?Possible Special Code: 01??Possible Special Code: 01??Possible Special Code: 01? ?Possible Special Code: 01??Possible Special Code: 05?$?BAD CHAR: 8280??BAD CHAR: 80??BAD CHAR: 80?, but your inventory is full.
-FIND_NOTHING = 7554; -- You dig and you dig, but find nothing.
+DIG_THROW_AWAY = 7554; -- You dig up ?Possible Special Code: 01??Possible Special Code: 01??Possible Special Code: 01? ?Possible Special Code: 01??Possible Special Code: 05?$?BAD CHAR: 8280??BAD CHAR: 80??BAD CHAR: 80?, but your inventory is full.
+FIND_NOTHING = 7556; -- You dig and you dig, but find nothing.
 
