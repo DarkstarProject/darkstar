b77e256bc7e1a7eb74e44f0b2880df51d0d51579
wayne <waytim@yahoo.com>
2015-08-29 22:19:24 -0400

    removed respawntime for certain force spawned nm crawlers, now reflects wiki correctly

--- scripts/zones/Crawlers_Nest/npcs/qm7.lua
+++ scripts/zones/Crawlers_Nest/npcs/qm7.lua
@@ -13,15 +13,16 @@ require("scripts/zones/Crawlers_Nest/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-	-- Trade Rolanberry 864
-	if (GetMobAction(17584135) == 0 and trade:hasItemQty(4531,1) and trade:getItemCount() == 1) then 
-		player:tradeComplete();
-		if (math.random(1,100)<=71) then
-			SpawnMob(17584135,120):updateClaim(player); -- Awd Goggie
-		else 
-			player:messageSpecial(NOTHING_SEEMS_TO_HAPPEN);				
-		end
-	end
+    -- Trade Rolanberry 864
+    if (GetMobAction(17584135) == 0 and trade:hasItemQty(4531,1) and trade:getItemCount() == 1) then 
+        player:tradeComplete();
+        if (math.random(1,100)<=73) then
+            SpawnMob(17584135,120):updateClaim(player); -- Awd Goggie
+            npc:setStatus(STATUS_DISAPPEAR) -- hide ???
+        else 
+            player:messageSpecial(NOTHING_SEEMS_TO_HAPPEN);				
+        end
+    end
 end; 
 
 -----------------------------------
