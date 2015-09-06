9ea74177bfe05713b59414e3683169c9ee5be7eb
Vavehl <lorin@wmalumni.com>
2015-09-06 16:14:07 -0400

    Added Breath Mantle and High Breath Mantle scripts.

---
+++ scripts/globals/items/breath_mantle.lua
@@ -0,0 +1,44 @@
+-----------------------------------------
+-- ID: 15486
+-- Item: Breath Mantle
+-- Item Effect: HP+18 / Enmity+3
+-----------------------------------------
+
+require("scripts/globals/settings");
+require("scripts/globals/status");
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+	return 0;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+	if (target:hasStatusEffect(EFFECT_ENCHANTMENT) == false) then
+		target:addStatusEffect(EFFECT_ENCHANTMENT,0,0,1800,15486);
+    end;
+end;
+
+-----------------------------------------
+-- onEffectGain Action
+-----------------------------------------
+
+function onEffectGain(target,effect)
+	target:addMod(MOD_HP, 18);
+	target:addMod(MOD_ENMITY, 3);
+end;
+
+-----------------------------------------
+-- onEffectLose Action
+-----------------------------------------
+
+function onEffectLose(target,effect)
+	target:delMod(MOD_HP, 18);
+	target:delMod(MOD_ENMITY, 3);
+end;
