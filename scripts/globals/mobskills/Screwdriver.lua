7222078f35f01de9b0d94dd2b732f59a2afb342d
Hozu <Hozu@users.noreply.github.com>
2015-09-28 22:58:44 -0400

    Scripts: Re-enable Screwdriver

--- scripts/globals/mobskills/Screwdriver.lua
+++ scripts/globals/mobskills/Screwdriver.lua
@@ -11,7 +11,7 @@ require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-    return 1;
+    return 0;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
