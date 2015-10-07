3cdb9586325564a8df24d33e857653fbd46fa288
nesstea <nesstea@live.com>
2015-10-07 01:04:48 -0700

    Missed non-versioned Imp .lua files.

---
+++ scripts/zones/Abyssea-Uleguerand/mobs/Ermit_Imp.lua
@@ -0,0 +1,45 @@
+-----------------------------------
+-- Area: Abyssea - Uleguerand
+-- NPC: Ermit Imp
+-----------------------------------
+
+require("scripts/globals/status");
+    
+-----------------------------------
+-- onMobInitialize Action
+-----------------------------------
+
+function onMobInitialize(mob)
+end;
+
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob, target)
+end;
+
+-----------------------------------
+-- onCriticalHit
+-----------------------------------
+
+function onCriticalHit(mob)   
+ 
+    if (math.random(100) < 20 and mob:AnimationSub() == 0) then  -- 20% change to break that horn on crit   
+        mob:AnimationSub(1);
+    end
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+end;
