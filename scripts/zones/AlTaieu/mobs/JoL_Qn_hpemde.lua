0a02ef22db73a2b9dca0154c64ad5a72dfe03d1f
nesstea <nesstea@live.com>
2015-10-08 11:15:00 -0700

    [SCRIPT][SEA] Goldfish AI They now function like they should! I am not sure if the numbers are correct, but they are no where near game breaking.

--- scripts/zones/AlTaieu/mobs/JoL_Qn_hpemde.lua
+++ scripts/zones/AlTaieu/mobs/JoL_Qn_hpemde.lua
@@ -4,12 +4,33 @@
 -- Jailor of Love Pet version
 -----------------------------------
 
-
 -----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
 function onMobSpawn(mob)
+    mob:AnimationSub(6); -- Mouth Closed
+end;
+
+-----------------------------------
+-- onMobFight
+-----------------------------------
+
+function onMobFight(mob, target)     
+    local changeTime = mob:getLocalVar("changeTime");
+    
+    if (mob:AnimationSub() == 6 and mob:getBattleTime() - changeTime > 30) then
+        mob:AnimationSub(3); -- Mouth Open
+        mob:addMod(MOD_ATT, 250);
+        mob:delMod(MOD_DEF, 100);
+        mob:setLocalVar("changeTime", mob:getBattleTime());
+    
+    elseif (mob:AnimationSub() == 3 and mob:getBattleTime() - changeTime > 30) then
+        mob:AnimationSub(6); -- Mouth Closed
+        mob:delMod(MOD_ATT, 250);
+        mob:addMod(MOD_DEF, 100);
+        mob:setLocalVar("changeTime", mob:getBattleTime());
+    end
 end;
 
 -----------------------------------
