7e61cf7880c096a61600a8c0a6d5f9a648a22202
Hozu <hozu30825@gmail.com>
2015-10-17 03:01:16 -0400

    Scripts: Setting for Dynamis entry to count by midnights
    If the setting is enabled, the entry time is simply set to the previous server midnight instead of current time. This will allow for the next entry time to be the number of midnights equal to the setting for days for next entry (ie. if it's set to 3 days for next entry, it will go by 3 server midnights instead of a full 72 hours).

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -86,6 +86,7 @@ LandKingSystem_HQ = 2;
 
 -- DYNAMIS SETTINGS
     BETWEEN_2DYNA_WAIT_TIME = 1;        -- wait time between 2 Dynamis (in real day) min: 1 day
+        DYNA_MIDNIGHT_RESET = true;     -- if true, makes the wait time count by number of server midnights instead of full 24 hour intervals
              DYNA_LEVEL_MIN = 65;       -- level min for entering in Dynamis
     TIMELESS_HOURGLASS_COST = 500000;   -- cost of the timeless hourglass for Dynamis.
      CURRENCY_EXCHANGE_RATE = 100;      -- X Tier 1 ancient currency -> 1 Tier 2, and so on.  Certain values may conflict with shop items.  Not designed to exceed 198.
