ade7a6a70b544a43499c5cbbe3fc21f4dadb6dfb
some1here <email.steven.smith@gmail.com>
2015-11-04 14:20:16 -0700

    Update trait.h

--- src/map/trait.h
+++ src/map/trait.h
@@ -140,6 +140,9 @@ enum TRAITTYPE
     TRAIT_INSPIRATION         = 121,
     TRAIT_SLEIGHT_OF_SWORD    = 122,
     TRAIT_DAKEN               = 123
+    TRAIT_SUPERIOR            = 124 
+    TRAIT_MAGIC_ACC_BONUS     = 125
+    TRAIT_MAGIC_EVA_BONUS     = 126
 };
 
 #define MAX_TRAIT_ID    124
