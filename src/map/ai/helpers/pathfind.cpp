48b7dc36943d3e9f8f7593ad49ea57a2630e7949
Davenge <korisd@gmail.com>
2015-11-24 15:55:21 -0500

    CORE: Mobs no longer path through walls...

--- src/map/ai/helpers/pathfind.cpp
+++ src/map/ai/helpers/pathfind.cpp
@@ -284,6 +284,9 @@ void CPathFind::StepTo(position_t* pos, bool run)
     float stepDistance = ((float)speed / 10) / 2;
     float distanceTo = distance(m_PTarget->loc.p, *pos);
 
+    // face point mob is moving towards
+    LookAt(*pos);
+
     if (distanceTo <= stepDistance ||
         (m_pathFlags & PATHFLAG_SLIDE) && distance(m_originalPoint, m_PTarget->loc.p) < m_distanceFromPoint)
     {
@@ -308,8 +311,6 @@ void CPathFind::StepTo(position_t* pos, bool run)
 
     }
 
-    // face point mob is moving towards
-    LookAt(*pos);
 
     m_PTarget->loc.p.moving += ((0x36 * ((float)m_PTarget->speed / 0x28)) - (0x14 * (mode - 1)));
 
