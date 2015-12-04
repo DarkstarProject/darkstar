5c008c1b79a5d5b786cf008a186e98de05168dd3
Hozu <hozu30825@gmail.com>
2015-12-04 01:41:44 -0500

    Scripts/SQL: Add/update mobskills for (proto) Omega/Ultima.lua
    -Corrected the animations for Omega's moves
    -Did some scripting for all four mobs, mostly Proto Omega and some Proto Ultima.
    -Added Particle Shield, Ion Efflux, and Discharger.
    -Updated the checks on the other mobskills due to the scripting done for Proto Omega/Ultima.
    -Separated the mob skill lists for the four mobs, as they all have different moves.
    -Removed Equalizer due to #2450
    
    Citadel Buster was not added because I couldn't think of a good way to have it triggered. I would think that SE uses a dummy mobskill to trigger the countdown start, but I have no idea what they do for sure. Nuclear Waste doesn't have a good way to be added either, so the six elemental moves will remain usable at will during the time they're supposed to be usable.

--- scripts/globals/mobskills/Colossal_Blow.lua
+++ scripts/globals/mobskills/Colossal_Blow.lua
@@ -11,21 +11,30 @@ require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
- local mobID = mob:getID();   --(16908294 ,16908301 ,16908308 =omega ,16933124=proto-omega)
- local mobhp = mob:getHPP();
-   if (mobID == 16933124 and mobhp < 30 ) then -- omega second quadripedform
-	  return 0;
-   end
-   return 1;
+    local currentForm = mob:getLocalVar("form") -- this var is only set for proto-omega
+
+    if (currentForm == 2) then
+        return 0;
+    end
+    return 1;
 end;
 
 function onMobWeaponSkill(target, mob, skill)
 
-	local numhits = 1;
-	local accmod = 0.8;
-	local dmgmod = 5;
-	local info = MobPhysicalMove(mob,target,skill,numhits,accmod,dmgmod,TP_NO_EFFECT);
-	local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_SLASH,info.hitslanded);
-	target:delHP(dmg);
-	return dmg;
+    local currentHP = target:getHP();
+    -- remove all by 5%
+    local damage = 0;
+
+    -- if have more hp then 30%, then reduce to 5%
+    if (target:getHPP() > 30) then
+        damage = currentHP * .95;
+    else
+        -- else you die
+        damage = currentHP;
+    end
+    local dmg = MobFinalAdjustments(damage,mob,skill,target,MOBSKILL_PHYSICAL,MOBPARAM_PIERCE,MOBPARAM_IGNORE_SHADOWS);
+
+    target:delHP(dmg);
+    mob:resetEnmity(target);
+    return dmg;
 end;
--- scripts/zones/Temenos/mobs/Proto-Ultima.lua
+++ scripts/zones/Temenos/mobs/Proto-Ultima.lua
@@ -11,6 +11,32 @@ require("scripts/globals/limbus");
 -----------------------------------
 
 function onMobSpawn(mob)
+    mob:SetMagicCastingEnabled(false);
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob,target)
+
+	local phase = mob:getLocalVar("battlePhase");
+	if (mob:actionQueueEmpty() == true) then
+		if (mob:getHPP() < (80 - (phase * 20))) then
+			mob:useMobAbility(1268); -- use Dissipation on phase change
+			phase = phase + 1;
+			if (phase == 2) then -- enable Holy II
+				mob:SetMagicCastingEnabled(true);
+			end
+			if (phase == 4) then -- add Regain in final phase
+				if (mob:hasStatusEffect(EFFECT_REGAIN) == false) then
+					mob:addStatusEffect(EFFECT_REGAIN,7,3,0);
+					mob:getStatusEffect(EFFECT_REGAIN):setFlag(32);
+				end
+			end
+			mob:setLocalVar("battlePhase", phase); -- incrementing the phase here instead of in the Dissipation skill because stunning it prevents use.
+		end
+	end
 end;
 
 -----------------------------------
@@ -18,8 +44,8 @@ end;
 -----------------------------------
 
 function onMobDeath(mob, killer)
-	killer:addTitle(TEMENOS_LIBERATOR);
-	GetNPCByID(16928768+79):setPos(-559,5,-357);
-	GetNPCByID(16928768+79):setStatus(STATUS_NORMAL);
-	GetNPCByID(16928768+474):setStatus(STATUS_NORMAL);
+    killer:addTitle(TEMENOS_LIBERATOR);
+    GetNPCByID(16928768+79):setPos(-559,5,-357);
+    GetNPCByID(16928768+79):setStatus(STATUS_NORMAL);
+    GetNPCByID(16928768+474):setStatus(STATUS_NORMAL);
 end;
