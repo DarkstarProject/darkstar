de17e62ca9cbdedd4062e82d3abe7dffdc37270a
Musashi- <aceredi@gmail.com>
2015-12-11 11:46:49 -0800

    Sandworm East Ron S

--- scripts/zones/East_Ronfaure_[S]/mobs/Sandworm.lua
+++ scripts/zones/East_Ronfaure_[S]/mobs/Sandworm.lua
@@ -1,22 +1,67 @@
 -----------------------------------
 -- Area: East Ronfaure [S]
---  MOB: Sandworm
+-- NPC:  Sandworm
 -- Note:  Title Given if Sandworm does not Doomvoid
 -----------------------------------
 
 require("scripts/globals/titles");
+-----------------------------------
+-- onMobInitialize
+-----------------------------------
 
+function onMobInitialize(mob)	
+	
+	local mobids = {17109357,
+					17138041,
+					17166720,
+					17178901,
+					17121693,
+					17150317,
+					17174888}
+	local mob = mobids[math.random(1, #mobids)];
+	SpawnMob(mob);
+end;
 -----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
-function onMobSpawn(mob)
+function onMobSpawn(mob,player)
+		
+		
+		GetMobByID(17138041):setHP(0);
+		GetMobByID(17166720):setHP(0);
+		GetMobByID(17178901):setHP(0);
+		GetMobByID(17121693):setHP(0);
+		GetMobByID(17150317):setHP(0);
+		GetMobByID(17174888):setHP(0);		
+		
+		
+			
 end;
+
+-----------------------------------
+-- onMobWeaponSkill
+-----------------------------------
+function onMobWeaponSkill(target, mob, skill)
+     if (mob:getHP() < ((mob:getMaxHP() / 10) * 2.5)) then
+	mob:useMobAbility(1936);
+	end
+end
 
 -----------------------------------
 -- onMobDeath
 -----------------------------------
 
-function onMobDeath(mob, killer, ally)
-    ally:addTitle(SANDWORM_WRANGLER);
+function onMobDeath(mob,killer)
+	killer:addTitle(SANDWORM_WRANGLER);
+	
+	
+end;
+-----------------------------------
+-- onMobDespawn
+-----------------------------------
+
+function onMobDespawn(mob)
+	mob:setRespawnTime(math.random(64,128));
+	
 end;
