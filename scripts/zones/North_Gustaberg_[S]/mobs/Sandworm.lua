f9893ebf58bd53d2382081c43b6e863706f6ed8f
Musashi- <aceredi@gmail.com>
2015-12-11 11:53:06 -0800

    Sandworm North Gustaberg S

--- scripts/zones/North_Gustaberg_[S]/mobs/Sandworm.lua
+++ scripts/zones/North_Gustaberg_[S]/mobs/Sandworm.lua
@@ -1,22 +1,55 @@
 -----------------------------------
--- Area: Crystal War Areas
---  MOB: Sandworm
+-- Area: North_Gustaberg_S
+-- NPC:  Sandworm
 -- Note:  Title Given if Sandworm does not Doomvoid
 -----------------------------------
 
 require("scripts/globals/titles");
+-----------------------------------
+-- onMobInitialize
+-----------------------------------
+function onMobInitialize(mob)	
 
+end;
 -----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
 function onMobSpawn(mob)
+		
+		
+		
+		GetMobByID(17150317):setHP(0);
+		GetMobByID(17109357):setHP(0);
+		GetMobByID(17166720):setHP(0);
+		GetMobByID(17178901):setHP(0);
+		GetMobByID(17121693):setHP(0);
+		GetMobByID(17174888):setHP(0);	
+	
+	
+	
 end;
+-----------------------------------
+-- onMobWeaponSkill
+-----------------------------------
+function onMobWeaponSkill(target, mob, skill)
+     if (mob:getHP() < ((mob:getMaxHP() / 10) * 2.5)) then
+	mob:useMobAbility(1936);
+	end
+end
 
 -----------------------------------
 -- onMobDeath
 -----------------------------------
 
-function onMobDeath(mob, killer, ally)
-	ally:addTitle(SANDWORM_WRANGLER);
+function onMobDeath(mob, killer)
+	killer:addTitle(SANDWORM_WRANGLER);
+
+end;
+-----------------------------------
+-- onMobDespawn
+-----------------------------------
+
+function onMobDespawn(mob)
+	mob:setRespawnTime(math.random(75600,86400));
 end;
