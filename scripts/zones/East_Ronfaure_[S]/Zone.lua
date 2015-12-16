64424c3916e17fb5980a023a1755ab0c9f4ebde6
Musashi- <aceredi@gmail.com>
2015-12-15 23:32:20 -0800

    Sandworm East Ron S

--- scripts/zones/East_Ronfaure_[S]/Zone.lua
+++ scripts/zones/East_Ronfaure_[S]/Zone.lua
@@ -16,15 +16,15 @@ require("scripts/globals/missions");
 
 function onInitialize(zone)
 
-	local mobids = {17109357,
-			17138041,
-			17166720,
-			17178901,
-			17121693,
-			17150317,
-			17174888}
-	local mob = mobids[math.random(2, #mobids)];
-	SpawnMob(mob);
+local mobids = {17109357,
+	17138041,
+	17166720,
+	17178901,
+	17121693,
+	17150317,
+	17174888}
+local mob = mobids[math.random(2, #mobids)];
+SpawnMob(mob);
 
 end;
 
