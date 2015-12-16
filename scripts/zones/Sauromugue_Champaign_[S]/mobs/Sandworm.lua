852a75dcb93f50146e36d171d19bb5b33a1d63ed
Musashi- <aceredi@gmail.com>
2015-12-15 20:31:10 -0800

    Sandworm Saurom S

--- scripts/zones/Sauromugue_Champaign_[S]/mobs/Sandworm.lua
+++ scripts/zones/Sauromugue_Champaign_[S]/mobs/Sandworm.lua
@@ -1,5 +1,5 @@
 -----------------------------------
--- Area: Crystal War Areas
+-- Area: Sauromuge_Champaign S
 -- NPC:  Sandworm
 -- Note:  Title Given if Sandworm does not Doomvoid
 -----------------------------------
@@ -7,11 +7,40 @@
 require("scripts/globals/titles");
 
 -----------------------------------
+-- onMobInitialize
+-----------------------------------
+
+-----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
 function onMobSpawn(mob)
+	
+		DespawnMob(17166720); 
+		DespawnMob(17121693); 
+		DespawnMob(17138041); 
+		DespawnMob(17150317); 
+		DespawnMob(17109357); 
+		DespawnMob(17174888); 
+		local random = 0;
+		random = math.random(10);
+		
+		if (random<=7) then
+		mob:setLocalVar("SerketFight",1);
+		end
+		if (random >=8) then
+		mob:setLocalVar("LambtonWormFight",2);
+		end
 end;
+-----------------------------------
+-- onMobWeaponSkill
+-----------------------------------
+function onMobWeaponSkill(target, mob, skill)
+     if (mob:getHP() < ((mob:getMaxHP() / 10) * 2.5)) then
+	mob:useMobAbility(1936);
+	end
+end
+
 
 -----------------------------------
 -- onMobDeath
@@ -19,4 +48,13 @@ end;
 
 function onMobDeath(mob, killer)
 	killer:addTitle(SANDWORM_WRANGLER);
+-- mob:setRespawnTime(math.random(75600,86400));
+	mob:setRespawnTime(math.random(75600,86400));
+end;
+-----------------------------------
+-- onMobDespawn
+-----------------------------------
+
+function onMobDespawn(mob)
+
 end;
