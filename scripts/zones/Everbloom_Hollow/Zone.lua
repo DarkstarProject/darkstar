0d7e344d94199320321b5a7afbc8038cdc286e78
Musashi- <aceredi@gmail.com>
2015-12-15 20:37:57 -0800

    Everbloom King Arthro

--- scripts/zones/Everbloom_Hollow/Zone.lua
+++ scripts/zones/Everbloom_Hollow/Zone.lua
@@ -12,18 +12,38 @@ require("scripts/zones/Everbloom_Hollow/TextIDs");
 --  onInitialize
 -----------------------------------
 
-function onInitialize(zone)
+function onInitialize(zone,mob)
+	local mobids = {17129520, -- King Arthro's  Knights
+					17129521,
+					17129522,
+					17129523,
+					17129524,
+					17129525,
+					17129526,
+					17129527,
+					17129528,
+					17129529,
+					17129530,
+					17129531}
+	local mob = mobids[math.random(12, #mobids)];
+	DespawnMob(mob);
+	
 end;
 
 -----------------------------------
 -- onZoneIn
 -----------------------------------
 
-function onZoneIn(player,prevZone)
-cs = -1;
-
-return cs;
+function onZoneIn(player,prevZone,zone)
+	
+	cs = -1;
+	if (prevZone == 81 or prevZone == 84)then
+	GetMobByID(17109357):setHP(0);
+	GetMobByID(17121693):setHP(0);
+	end
+	return cs;
 end;
+
 
 -----------------------------------
 -- onRegionEnter          
@@ -49,6 +69,7 @@ function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 end;
+
 
 
 
