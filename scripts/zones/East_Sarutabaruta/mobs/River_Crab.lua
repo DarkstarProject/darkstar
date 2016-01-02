47ddc2a028950ca615e04e3dc26951b30437632d
Dan Dumont <ddumont@gmail.com>
2016-01-02 17:22:22 -0500

    Model Duke Decapod after Spiny Spipi

--- scripts/zones/East_Sarutabaruta/mobs/River_Crab.lua
+++ scripts/zones/East_Sarutabaruta/mobs/River_Crab.lua
@@ -1,9 +1,11 @@
 -----------------------------------
 -- Area: East Sarutabaruta
 --  MOB: River Crab
+-- Note: PH for Duke Decapod
 -----------------------------------
 
 require("scripts/globals/fieldsofvalor");
+require("scripts/zones/East_Sarutabaruta/MobIDs");
 
 -----------------------------------
 -- onMobDeath
@@ -11,4 +13,17 @@ require("scripts/globals/fieldsofvalor");
 
 function onMobDeath(mob,killer,ally)
     checkRegime(ally,mob,92,1);
+
+    mob = mob:getID();
+    if (Duke_Decapod_PH[mob] ~= nil) then
+        ToD = GetServerVariable("[POP]Duke_Decapod");
+        if (ToD <= os.time(t) and GetMobAction(Duke_Decapod) == 0) then
+            if (math.random((1),(15)) == 5) then
+                UpdateNMSpawnPoint(Duke_Decapod);
+                GetMobByID(Duke_Decapod):setRespawnTime(GetMobRespawnTime(mob));
+                SetServerVariable("[PH]Duke_Decapod", mob);
+                DeterMob(mob, true);
+            end
+        end
+    end
 end;
