4863f9d3070fbb5c76821c6dea3dd3f46ab48b78
Michael Lezon <mister.catsby@gmail.com>
2016-02-18 20:00:55 -0500

    changed the old sabertooth script based on feedback. mob is intended to only progress quest if it dies without claim to its dot effect.  script now checks to see if the killer is null before setting vars on nearby players.

--- scripts/zones/Sauromugue_Champaign/mobs/Old_Sabertooth.lua
+++ scripts/zones/Sauromugue_Champaign/mobs/Old_Sabertooth.lua
@@ -22,25 +22,18 @@ end;
 
 function onMobDeath(mob, killer)
 
-    local players = mob:getZone():getPlayers();
+    if (killer == nil) then
     
-    for i, player in pairs(players) do
+        local players = mob:getZone():getPlayers();
+        for i, player in pairs(players) do
 
-        --check to see if the player has accepted this quest.
-        if (player:getQuestStatus(WINDURST,THE_FANGED_ONE) == QUEST_ACCEPTED) then
+            if (player:getQuestStatus(WINDURST,THE_FANGED_ONE) == QUEST_ACCEPTED) then
 
-        --check to see if the player's x,y,z coordinates are near enough to the cave where the RNG quest mob dies.
-        --point 1: 690, -15, -338
-        --point 2: 654, -9, -383
-        --these are outside the playable space but used to form a box around the cave where this quest takes place
-            local playerX = player:getXPos();
-            --local playerY = player:getYPos(); we shouldn't really care about the y axis for this
-            local playerZ = player:getZPos();
+            local playerDistance = player:checkDistance(mob);
+                if (playerDistance < 32) then
 
-            if (playerX < 690 and playerX > 654 and
-                playerZ < -338 and playerZ > -383) then
-
-                player:setVar("TheFangedOneCS", 2);
+                    player:setVar("TheFangedOneCS", 2);
+                end;
             end;
         end;
     end;
