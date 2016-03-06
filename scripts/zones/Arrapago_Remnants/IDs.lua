90698cacb11d737ac31e5952ec1af56dde4ce846
Tyler Schneider <teschnei@gmail.com>
2016-03-06 01:09:43 -0700

    Added base Arrapago Remnants scripts

---
+++ scripts/zones/Arrapago_Remnants/IDs.lua
@@ -0,0 +1,29 @@
+Arrapago = {
+
+    text = {
+        -- General Texts
+        ITEM_CANNOT_BE_OBTAINED = 6379, -- You cannot obtain the item <item> come back again after sorting your inventory
+        ITEM_OBTAINED = 6384, -- Obtained: <item>
+        GIL_OBTAINED = 6385, -- Obtained <number> gil
+        KEYITEM_OBTAINED = 6387, -- Obtained key item: <keyitem>
+
+        -- Assault Texts
+        SALVAGE_START = 7231,
+        TIME_TO_COMPLETE = 7438,
+        MISSION_FAILED = 7439,
+        TIME_REMAINING_MINUTES = 7443,
+        TIME_REMAINING_SECONDS = 7444,
+        PARTY_FALLEN = 7446
+    },
+    
+    mobs = {
+        -- Arrapago Remnants
+        [65] = {
+        }
+    },
+    
+    npcs = {
+    }
+}
+
+return Arrapago;
--- scripts/zones/Arrapago_Remnants/Zone.lua
+++ scripts/zones/Arrapago_Remnants/Zone.lua
@@ -1,12 +1,11 @@
 -----------------------------------
--- 
--- Zone: Arrapago_Remnants
--- 
+--
+-- Zone: Arrapago Remnants
+--
 -----------------------------------
 
 require("scripts/globals/settings");
-package.loaded["scripts/zones/Arrapago_Remnants/TextIDs"] = nil;
-require("scripts/zones/Arrapago_Remnants/TextIDs");
+require("scripts/zones/Arrapago_Remnants/IDs");
 
 -----------------------------------
 --  onInitialize
@@ -16,17 +15,23 @@ function onInitialize(zone)
 end;
 
 -----------------------------------
--- onZoneIn
+-- onInstanceZoneIn
 -----------------------------------
 
-function onZoneIn(player,prevZone)
-cs = -1;
+function onInstanceZoneIn(player,instance)
+    local cs = -1;
 
-return cs;
+    local pos = player:getPos();
+    if (pos.x == 0 and pos.y == 0 and pos.z == 0) then
+        local entrypos = instance:getEntryPos();
+        player:setPos(entrypos.x, entrypos.y, entrypos.z, entrypos.rot);
+    end
+
+    player:addTempItem(5399);
 end;
 
 -----------------------------------
--- onRegionEnter          
+-- onRegionEnter
 -----------------------------------
 
 function onRegionEnter(player,region)
@@ -37,8 +42,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+    -- printf("CSID: %u",csid);
+    -- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -46,9 +51,17 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+    -- printf("CSID: %u",csid);
+    -- printf("RESULT: %u",option);
+    if (csid == 0x66) then
+        player:setPos(0,0,0,0,79);
+    end
 end;
 
+-----------------------------------
+-- onInstanceFailure
+-----------------------------------
 
-
+function onInstanceLoadFailed()
+    return 72;
+end;
