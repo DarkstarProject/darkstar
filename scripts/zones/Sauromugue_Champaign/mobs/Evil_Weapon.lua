0c2d5c88a0307ca71b888dbffe31348ab64f673e
Fiocitrine <justynwhitney@gmail.com>
2016-03-16 13:14:58 -0400

    Cherry picked my last commit. Hopefully this is what it needs.

--- scripts/zones/Sauromugue_Champaign/mobs/Evil_Weapon.lua
+++ scripts/zones/Sauromugue_Champaign/mobs/Evil_Weapon.lua
@@ -10,5 +10,19 @@ require("scripts/globals/fieldsofvalor");
 -----------------------------------
 
 function onMobDeath(mob,killer,ally)
+
     checkRegime(ally,mob,100,2);
+
+    local mobID = mob:getID();
+    if (Blighting_Brand_PH[mobID] ~= nil) then
+        local ToD = GetServerVariable("[POP]Blighting_Brand");
+        if (ToD <= os.time(t) and GetMobAction(Blighting_Brand) == 0) then
+            if (math.random(1,5) == 5) then
+                UpdateNMSpawnPoint(Blighting_Brand);
+                GetMobByID(Blighting_Brand):setRespawnTime(GetMobRespawnTime(mobID));
+                SetServerVariable("[PH]Blighting_Brand", mobID);
+                DeterMob(mobID, true);
+            end
+        end
+     end
 end;
