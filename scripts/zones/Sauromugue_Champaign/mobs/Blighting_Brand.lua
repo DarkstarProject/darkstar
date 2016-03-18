ae984db8542f16672c946b27b14976686fdda550
Fiocitrine <justynwhitney@gmail.com>
2016-03-18 01:09:42 -0400

    Added Blighting Brand, Eldritch Edge and an Evil_Weapon lua to the database. Also added a droplist for the two NMs along with a spell list for Blighting Brand as it's a RDM mob. I gave each Evil Weapon mob in both Sauromugue_Champaign and Rolanberry_Fields functions to spawn the NMs and respectively gave the NMs ToD. In addition to all this, I fixed the Lamina and Hyakinthos NM's drop rates for their rings. They were .5% I changed it to 5% drop rate.
    did all that shit before adding Eldritch Edge and Blinging Brand and their dropIds and fixed the Evil Weapon mobs to spawn them. This is a fix for the os.time(t) thing because the NMs were spawning after killing the PHs.

--- scripts/zones/Sauromugue_Champaign/mobs/Blighting_Brand.lua
+++ scripts/zones/Sauromugue_Champaign/mobs/Blighting_Brand.lua
@@ -11,4 +11,15 @@ require("scripts/globals/fieldsofvalor");
 
 function onMobDeath(mob,killer,ally)
     checkRegime(ally,mob,100,2);
+
+     -- Set Eldritch Edge's Window Open Time
+    local wait = math.random(5400,7200);
+    SetServerVariable("[POP]Blighting_Brand", os.time(t) + wait); -- 90 to 120 minutes
+    DeterMob(mob:getID(), true);
+
+    -- Set PH back to normal, then set to respawn spawn
+    local PH = GetServerVariable("[PH]Blighting_Brand");
+    SetServerVariable("[PH]Blighting_Brand", 0);
+    DeterMob(PH, false);
+    GetMobByID(PH):setRespawnTime(GetMobRespawnTime(PH));
 end;
