e783b38353502c746ed551e2bdf96d7a7cbcdfd9
ymkc <sixth_brother@gmx.net>
2016-03-29 10:25:44 +0000

    Cleanups

--- scripts/zones/FeiYin/mobs/Specter.lua
+++ scripts/zones/FeiYin/mobs/Specter.lua
@@ -18,17 +18,14 @@ function onMobDeath(mob,killer,ally)
 
     -- Check if Specter is within the Northern_Shadow_PH table
     if (Northern_Shadow_PH[mobID] ~= nil) then
-        -- printf("%u is a PH",mob);
         -- Get NM's previous ToD
         local Northern_Shadow_ToD = GetServerVariable("[POP]Northern_Shadow");
 
         -- Check if NM's window is open, and there is not an Northern Shadow popped already(ACTION_NONE = 0)
         if (Northern_Shadow_ToD <= os.time(t) and GetMobAction(Northern_Shadow) == 0) then
 
-            -- printf("NM's window open");
             -- Give Specter 5 percent chance to pop a NM
             if (math.random(1,20) == 5) then
-                -- printf("NM will pop");
                 UpdateNMSpawnPoint(Northern_Shadow);
                 GetMobByID(Northern_Shadow):setRespawnTime(GetMobRespawnTime(mobID));
                 SetServerVariable("[PH]Northern_Shadow", mobID);
@@ -39,17 +36,14 @@ function onMobDeath(mob,killer,ally)
 
     -- Check if Specter is within the Eastern_Shadow_PH table
     if (Eastern_Shadow_PH[mobID] ~= nil) then
-        -- printf("%u is a PH",mob);
         -- Get NM's previous ToD
         local Eastern_Shadow_ToD = GetServerVariable("[POP]Eastern_Shadow");
 
         -- Check if NM's window is open, and there is not an Eastern Shadow popped already(ACTION_NONE = 0)
         if (Eastern_Shadow_ToD <= os.time(t) and GetMobAction(Eastern_Shadow) == 0) then
 
-            -- printf("NM window open");
             -- Give Specter 5 percent chance to pop a NM
             if (math.random(1,20) == 5) then
-                -- printf("NM will pop");
                 UpdateNMSpawnPoint(Eastern_Shadow);
                 GetMobByID(Eastern_Shadow):setRespawnTime(GetMobRespawnTime(mobID));
                 SetServerVariable("[PH]Eastern_Shadow", mobID);
@@ -60,17 +54,14 @@ function onMobDeath(mob,killer,ally)
 
     -- Check if Specter is within the Southern_Shadow_PH table
     if (Southern_Shadow_PH[mobID] ~= nil) then
-        -- printf("%u is a PH",mob);
         -- Get NM's previous ToD
         local Southern_Shadow_ToD = GetServerVariable("[POP]Southern_Shadow");
 
         -- Check if NM's window is open, and there is not an Southern Shadow popped already(ACTION_NONE = 0)
         if (Southern_Shadow_ToD <= os.time(t) and GetMobAction(Southern_Shadow) == 0) then
 
-            -- printf("NM window open");
             -- Give Specter 5 percent chance to pop a NM
             if (math.random(1,20) == 5) then
-                -- printf("NM will pop");
                 UpdateNMSpawnPoint(Southern_Shadow);
                 GetMobByID(Southern_Shadow):setRespawnTime(GetMobRespawnTime(mobID));
                 SetServerVariable("[PH]Southern_Shadow", mobID);
@@ -81,17 +72,14 @@ function onMobDeath(mob,killer,ally)
 
     -- Check if Specter is within the Western_Shadow_PH table
     if (Western_Shadow_PH[mobID] ~= nil) then
-        -- printf("%u is a PH",mob);
         -- Get NM's previous ToD
         local Western_Shadow_ToD = GetServerVariable("[POP]Western_Shadow");
 
-        -- Check if NM's window is open, and there is not an Eastern Shadow popped already(ACTION_NONE = 0)
+        -- Check if NM's window is open, and there is not an Western Shadow popped already(ACTION_NONE = 0)
         if (Western_Shadow_ToD <= os.time(t) and GetMobAction(Western_Shadow) == 0) then
 
-            -- printf("NM window open");
             -- Give Specter 5 percent chance to pop a NM
             if (math.random(1,20) == 5) then
-                -- printf("NM will pop");
                 UpdateNMSpawnPoint(Western_Shadow);
                 GetMobByID(Western_Shadow):setRespawnTime(GetMobRespawnTime(mobID));
                 SetServerVariable("[PH]Western_Shadow", mobID);
