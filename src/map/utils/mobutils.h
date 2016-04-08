35e8e7f0ea599650d6f4488b066e87165ef23b9f
ymkc <sixth_brother@gmx.net>
2016-04-08 17:44:17 +0000

    Fixes for Charm
    Refactored to use zoneutils & instance_loader
    Removed unneccessary stuff
    Quick fix

--- src/map/utils/mobutils.h
+++ src/map/utils/mobutils.h
@@ -45,7 +45,7 @@ typedef std::unordered_map<uint32,ModsList_t*> ModsMap_t;
 
 namespace mobutils
 {
-	void	CalculateStats(CMobEntity* PMob);
+	void  CalculateStats(CMobEntity* PMob);
         void  SetupJob(CMobEntity* PMob);
         void  SetupRoaming(CMobEntity* PMob);
         void  SetupDynamisMob(CMobEntity* PMob);
