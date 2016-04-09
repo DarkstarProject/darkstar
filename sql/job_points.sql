a734b0b746b41efe2ef65a02d65ebd878b110ebb
some1here <some1@adelphia.net>
2016-04-09 13:40:58 -0600

    sql/job_points.sql

--- sql/job_points.sql
+++ sql/job_points.sql
@@ -241,4 +241,3 @@ INSERT INTO `job_points` VALUES ('1422', 'vivacious_pulse', '1', '22');
 INSERT INTO `job_points` VALUES ('1424', 'one_for_all_effect_duration', '1', '22');
 INSERT INTO `job_points` VALUES ('1426', 'gambit_effect_duration', '1', '22');
 
-
