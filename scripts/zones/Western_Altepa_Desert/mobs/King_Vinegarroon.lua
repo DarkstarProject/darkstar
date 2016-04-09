15ddee32c70ab7cba0a7023771a94b360b7c91a9
Hozu <hozu30825@gmail.com>
2016-04-09 16:57:11 -0400

    Scripts: Add enpetrify to King Vinegarroon
    100% proc rate barring resist.

--- scripts/zones/Western_Altepa_Desert/mobs/King_Vinegarroon.lua
+++ scripts/zones/Western_Altepa_Desert/mobs/King_Vinegarroon.lua
@@ -3,8 +3,17 @@
 --   NM: King Vinegarroon
 -----------------------------------
 
+require("scripts/globals/status");
 require("scripts/globals/titles");
 require("scripts/globals/weather");
+
+-----------------------------------
+-- onMobInitialize Action
+-----------------------------------
+
+function onMobInitialize(mob)
+    mob:setMobMod(MOBMOD_ADD_EFFECT,mob:getShortID());
+end;
 
 -----------------------------------
 -- onMobSpawn Action
@@ -47,4 +56,26 @@ function onMobDeath(mob, killer, ally)
     UpdateNMSpawnPoint(mob:getID());
     mob:setRespawnTime(math.random(75600,86400));
 
+end;
+
+-----------------------------------
+-- onAdditionalEffect
+-----------------------------------
+
+function onAdditionalEffect(mob, player)
+    local resist = applyResistanceAddEffect(mob,player,ELE_EARTH,EFFECT_PETRIFICATION);
+    if (resist <= 0.5) then -- "Has an innate Additional Effect of Petrification on all of its physical attacks. "
+        return 0,0,0;
+    else
+        local duration = 30;
+        if (mob:getMainLvl() > player:getMainLvl()) then
+            duration = duration + (mob:getMainLvl() - player:getMainLvl())
+        end
+        duration = utils.clamp(duration,1,45);
+        duration = duration * resist;
+        if (not player:hasStatusEffect(EFFECT_PETRIFICATION)) then
+            player:addStatusEffect(EFFECT_PETRIFICATION, 1, 0, duration);
+        end
+        return SUBEFFECT_PETRIFY, MSGBASIC_ADD_EFFECT_STATUS, EFFECT_PETRIFICATION;
+    end
 end;
