ec3bc00bd057d352d4994e0b92e959acfbb0f636
Tyler Schneider <teschnei@gmail.com>
2016-04-09 22:23:22 -0600

    Implemented the simple automaton attachments

--- scripts/globals/abilities/pets/attachments/mana_tank.lua
+++ scripts/globals/abilities/pets/attachments/mana_tank.lua
@@ -1,5 +1,5 @@
 -----------------------------------
--- Attachment: Inhibitor
+-- Attachment: Mana Tank
 -----------------------------------
 
 require("scripts/globals/status");
@@ -9,29 +9,35 @@ require("scripts/globals/status");
 -----------------------------------
 
 function onEquip(pet)
-    pet:addMod(MOD_STORE_TP, 5)
+    pet:addMod(MOD_MPP, 4);
 end
 
 function onUnequip(pet)
-    pet:delMod(MOD_STORE_TP, 5)
+    pet:delMod(MOD_MPP, 4);
 end
 
 function onManeuverGain(pet,maneuvers)
+    local bonus = 0
+    local frame = pet:getFrame()
+    if frame == 0x23 then bonus = 1 end
     if (maneuvers == 1) then
-        pet:addMod(MOD_STORE_TP, 10);
+        pet:addMod(MOD_REFRESH, 1 + bonus);
     elseif (maneuvers == 2) then
-        pet:addMod(MOD_STORE_TP, 10); 
+        pet:addMod(MOD_REFRESH, 1 + bonus); 
     elseif (maneuvers == 3) then
-        pet:addMod(MOD_STORE_TP, 15);
+        pet:addMod(MOD_REFRESH, 2 + bonus);
     end
 end
 
 function onManeuverLose(pet,maneuvers)
+    local bonus = 0
+    local frame = pet:getFrame()
+    if frame == 0x23 then bonus = 1 end
     if (maneuvers == 1) then
-        pet:delMod(MOD_STORE_TP, 10);
+        pet:delMod(MOD_REFRESH, 1 + bonus);
     elseif (maneuvers == 2) then
-        pet:delMod(MOD_STORE_TP, 10); 
+        pet:delMod(MOD_REFRESH, 1 + bonus); 
     elseif (maneuvers == 3) then
-        pet:delMod(MOD_STORE_TP, 15);
+        pet:delMod(MOD_REFRESH, 2 + bonus);
     end
 end
