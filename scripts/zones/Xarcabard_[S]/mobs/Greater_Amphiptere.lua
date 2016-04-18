2fc0de876c72376acc00e9778b14655b5f25604e
Hozu <hozu30825@gmail.com>
2016-04-18 17:59:08 -0400

    Scripts: Rudimentary Amphiptere support
    Enough to make it come down upon aggro so people can target/fight it. Goes back up on deaggro.

---
+++ scripts/zones/Xarcabard_[S]/mobs/Greater_Amphiptere.lua
@@ -0,0 +1,35 @@
+-----------------------------------
+-- Area: 
+--  MOB: Greater Amphiptere
+-----------------------------------
+
+require("scripts/globals/status");
+mixins = { require("scripts/mixins/families/amphiptere") }
+
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobEngaged
+-----------------------------------
+
+function onMobEngaged(mob, killer)
+end;
+
+-----------------------------------
+-- onMobDisengage
+-----------------------------------
+
+function onMobDisengage(mob)
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer, ally)
+end;
