94dfe538ff00c2d13157a3261b4e26f3b8a5b0b0
ymkc <sixth_brother@gmx.net>
2016-04-23 18:42:25 +0000

    Some more charm fixes

--- scripts/zones/Sea_Serpent_Grotto/mobs/Ironshell.lua
+++ scripts/zones/Sea_Serpent_Grotto/mobs/Ironshell.lua
@@ -9,6 +9,14 @@ require("scripts/globals/groundsofvalor");
 -- onMobDeath
 -----------------------------------
 
+function onMobSpawn(mob)
+    mob:setMobMod(MOBMOD_CHARMABLE, 1);
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
 function onMobDeath(mob,killer,ally)
     checkGoVregime(ally,mob,805,1);
 end;
