ac846c6c01fed5666a61918b298cf8073079c540
TeoTwawki <arawn.annwn@yandex.com>
2016-05-15 19:08:50 -0400

    Stop Jailers of Prudence, Hope, Justice, and Love from instantly despawning the second they go unclaimed.
    The second argument of spawnMOB is *NOT* the depop timer from claim loss, it is a depop timer from spawn! To have depop from unclaimed, use MOBMOD_IDLE_DESPAWN
    
    (If you ask me, this should be the other way around..But it is what it is.)

--- scripts/zones/AlTaieu/npcs/qm1.lua
+++ scripts/zones/AlTaieu/npcs/qm1.lua
@@ -1,12 +1,11 @@
 -----------------------------------
 -- Area: Al'Taieu
--- NPC:  ??? (Jailer of Hope Spawn)
+--  NPC: ??? (Jailer of Hope Spawn)
 -- Allows players to spawn the Jailer of Hope by trading the First Virtue, Deed of Placidity and HQ Phuabo Organ to a ???.
 -- @pos -693 -1 -62 33
 -----------------------------------
 package.loaded["scripts/zones/AlTaieu/TextIDs"] = nil;
 -----------------------------------
-
 require("scripts/zones/AlTaieu/TextIDs");
 
 -----------------------------------
@@ -14,11 +13,10 @@ require("scripts/zones/AlTaieu/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
-
     -- Trade the First Virtue, Deed of Placidity and HQ Phuabo Organ
     if (GetMobAction(16912838) == 0 and trade:hasItemQty(1850,1) and trade:hasItemQty(1851,1) and trade:hasItemQty(1852,1) and trade:getItemCount() == 3) then
         player:tradeComplete();
-        SpawnMob(16912838,900):updateClaim(player); -- Spawn Jailer of Hope
+        SpawnMob(16912838):updateClaim(player); -- Spawn Jailer of Hope
     end
 end; 
 
@@ -34,15 +32,15 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("onUpdate CSID: %u",csid);
---printf("onUpdate RESULT: %u",option);
+    -- printf("onUpdate CSID: %u",csid);
+    -- printf("onUpdate RESULT: %u",option);
 end;
 
 -----------------------------------
--- onEventFinish Action 
+-- onEventFinish Action
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("onFinish CSID: %u",csid);
---printf("onFinish RESULT: %u",option);
+    -- printf("onFinish CSID: %u",csid);
+    -- printf("onFinish RESULT: %u",option);
 end;
--- scripts/zones/AlTaieu/npcs/qm2.lua
+++ scripts/zones/AlTaieu/npcs/qm2.lua
@@ -1,25 +1,26 @@
 -----------------------------------
 -- Area: Al'Taieu
--- NPC:  ??? (Jailer of Justice Spawn)
+--  NPC: ??? (Jailer of Justice Spawn)
 -- Allows players to spawn the Jailer of Justice by trading the Second Virtue, Deed of Moderation, and HQ Xzomit Organ to a ???.
 -- @pos , -278 0 -463
 -----------------------------------
 package.loaded["scripts/zones/AlTaieu/TextIDs"] = nil;
 -----------------------------------
-
 require("scripts/zones/AlTaieu/TextIDs");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
-function onTrade(player,npc,trade)  
+function onTrade(player,npc,trade)
+    --[[
     -- Trade the Second Virtue, Deed of Moderation, and HQ Xzomit Organ
-    --[[if (GetMobAction(16912839) == 0 and trade:hasItemQty(1853,1) and trade:hasItemQty(1854,1) and trade:hasItemQty(1785,1) and
+    if (GetMobAction(16912839) == 0 and trade:hasItemQty(1853,1) and trade:hasItemQty(1854,1) and trade:hasItemQty(1785,1) and
     trade:getItemCount() == 3) then
         player:tradeComplete();
-        SpawnMob(16912839,900):updateClaim(player); -- Spawn Jailer of Justice
-    end]]
+        SpawnMob(16912839):updateClaim(player); -- Spawn Jailer of Justice
+    end
+    ]]
 end; 
 
 -----------------------------------
@@ -34,15 +35,15 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
---printf("onUpdate CSID: %u",csid);
---printf("onUpdate RESULT: %u",option);
+    -- printf("onUpdate CSID: %u",csid);
+    -- printf("onUpdate RESULT: %u",option);
 end;
 
 -----------------------------------
--- onEventFinish Action 
+-- onEventFinish Action
 -----------------------------------
 
 function onEventFinish(player,csid,option)
---printf("onFinish CSID: %u",csid);
---printf("onFinish RESULT: %u",option);
+    -- printf("onFinish CSID: %u",csid);
+    -- printf("onFinish RESULT: %u",option);
 end;
