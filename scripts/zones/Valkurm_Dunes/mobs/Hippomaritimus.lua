794caad4acb54a0c52b9597ce996669dee6da7ae
ymkc <sixth_brother@gmx.net>
2016-05-31 16:39:24 +0000

    Hippomaritimus

---
+++ scripts/zones/Valkurm_Dunes/mobs/Hippomaritimus.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+-- Area: Valkurm Dunes
+--  MOB: Hippomaritimus
+-----------------------------------
+
+-----------------------------------
+-- onMobSpawn
+-----------------------------------
+
+function onMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+    UpdateNMSpawnPoint(mob:getID());
+    mob:setRespawnTime(math.random(3600,5400)); -- 60-90min repop
+end;
