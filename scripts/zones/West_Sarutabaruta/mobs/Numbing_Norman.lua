919580b5d60907025a36dae623f55df7da88f7f2
ymkc <sixth_brother@gmx.net>
2016-06-01 16:38:26 +0000

    Numbing Norman

--- scripts/zones/West_Sarutabaruta/mobs/Numbing_Norman.lua
+++ scripts/zones/West_Sarutabaruta/mobs/Numbing_Norman.lua
@@ -6,6 +6,28 @@
 require("scripts/globals/fieldsofvalor");
 
 -----------------------------------
+-- onMobInitialize
+-----------------------------------
+
+function onMobInitialize(mob)
+    mob:setMobMod(MOBMOD_ADD_EFFECT,mob:getShortID());
+end;
+
+-----------------------------------
+-- onAdditionalEffect Action
+-----------------------------------
+function onAdditionalEffect(mob,target,damage)
+    -- Guesstimating 1 in 4 chance to poison on melee.
+    if ((math.random(1,100) >= 25) or (target:hasStatusEffect(EFFECT_PARALYSIS) == true)) then
+        return 0,0,0;
+    else
+        local duration = math.random(5,15);
+        target:addStatusEffect(EFFECT_PARALYSIS,5,3,duration);
+        return SUBEFFECT_PARALYSIS,0,EFFECT_PARALYSIS;
+    end
+end;
+
+-----------------------------------
 -- onMobDeath
 -----------------------------------
 
