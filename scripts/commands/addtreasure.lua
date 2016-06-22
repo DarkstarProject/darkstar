c36def092cc5ea6952daf42ac1ea1b4e3d9bcb46
TeoTwawki <arawn.annwn@yandex.com>
2016-06-22 14:51:23 -0400

    Add script binding for adding items directly to treasure pool.
    If droprate is set exactly zero, don't roll for the item.

---
+++ scripts/commands/addtreasure.lua
@@ -0,0 +1,32 @@
+---------------------------------------------------------------------------------------------------
+-- func: @addtreasure <itemId> <target player/party/alliance>
+-- desc: Adds an item directly to the treasure pool.
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "is"
+};
+
+function onTrigger(player, itemId, target)
+    local targ;
+    if (target == nil) then
+        targ = player
+    else
+        targ = GetPlayerByName(target);
+    end
+
+    if (targ == nil) then
+        player:PrintToPlayer(string.format("Player named '%s' not found!", target));
+        return
+    end
+
+    if (itemId == nil or tonumber(itemId) == nil or tonumber(itemId) == 0) then
+        player:PrintToPlayer("You must enter a valid item id.");
+        return;
+    end
+
+    targ:addTreasure(itemId);
+    player:PrintToPlayer(string.format("Item of ID %d was added to the treasure pool of %s or their party/alliance.", itemId, targ:getName()));
+end
