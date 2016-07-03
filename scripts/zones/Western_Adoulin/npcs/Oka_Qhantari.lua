65b39602dc8882697b329920988dffc168189962
ibm2431 <keruble@dekumi.com>
2016-07-03 07:39:06 -0700

    First batch of Western Adoulin NPCs

---
+++ scripts/zones/Western_Adoulin/npcs/Oka_Qhantari.lua
@@ -0,0 +1,51 @@
+-----------------------------------
+--  Area: Western Adoulin
+--  NPC: Oka Qhantari
+--  Type: Standard NPC and Quest NPC
+--  Involved With Quest: 'Order Up'
+--  @zone 256
+--  @pos -30 3 -6
+-----------------------------------
+require("scripts/globals/quests");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+    local Order_Up = player:getQuestStatus(ADOULIN, ORDER_UP);
+    local Order_Oka_Qhantari = bit.band(bit.rshift(player:getVar("Order_Up_NPCs"), 9), 1)
+
+    if ((Order_Up == QUEST_ACCEPTED) and (Order_Oka_Qhantari < 1)) then
+        -- Progresses Quest: 'Order Up'
+        player:startEvent(0x0047);
+    else
+        -- Standard Dialogue
+        player:startEvent(0x01FF);
+    end
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)    
+    if (csid == 0x0047) then
+        -- Progresses Quest: 'Order Up'
+        player:addVar("Order_Up_NPCs", bit.lshift(1, 9));
+    end
+end;
---
+++ scripts/zones/Western_Adoulin/npcs/Terwok.lua
@@ -0,0 +1,51 @@
+-----------------------------------
+--  Area: Western Adoulin
+--  NPC: Terwok
+--  Type: Standard NPC and Quest NPC
+--  Involved With Quest: 'Order Up'
+--  @zone 256
+--  @pos 127 4 -81
+-----------------------------------
+require("scripts/globals/quests");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end; 
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+    local Order_Up = player:getQuestStatus(ADOULIN, ORDER_UP);
+    local Order_Terwok = bit.band(bit.rshift(player:getVar("Order_Up_NPCs"), 7), 1);
+
+    if ((Order_Up == QUEST_ACCEPTED) and (Order_Terwok < 1)) then
+        -- Progresses Quest: 'Order Up'
+        player:startEvent(0x0043);
+    else
+        -- Standard Dialogue
+        player:startEvent(0x0214);
+    end
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)    
+    if (csid == 0x0043) then
+        -- Progresses Quest: 'Order Up'
+        player:addVar("Order_Up_NPCs", bit.lshift(1, 7));
+    end
+end;
