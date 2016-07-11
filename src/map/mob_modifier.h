323e4881ef3d153f5ac623bc9eec86d5ad4270fb
HeavensSword <heavenssword88@aol.com>
2016-07-11 10:44:32 -0600

    Created new mob mod 'MOBMOD_MULTI_HIT' for specifying if a mob has the capability of attacking more than once per round. e.g. Charybdis.

--- src/map/mob_modifier.h
+++ src/map/mob_modifier.h
@@ -94,7 +94,8 @@ enum MOBMODIFIER : int
     MOBMOD_NO_STANDBACK       = 62, // Mob will never standback
     MOBMOD_ATTACK_SKILL_LIST  = 63, // skill list to use in place of regular attacks
     MOBMOD_CHARMABLE          = 64, // mob is charmable
-    MOBMOD_NO_MOVE            = 65  // Mob will not be able to move
+    MOBMOD_NO_MOVE            = 65, // Mob will not be able to move
+    MOBMOD_MULTI_HIT          = 66  // Mob will not be able to move
 };
 
 #endif
