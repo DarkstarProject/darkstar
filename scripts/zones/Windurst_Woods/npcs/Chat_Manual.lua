655a5bd7d5216f7095cccc8afb8696a01fbd65db
thedax <daxtsu@gmail.com>
2016-08-11 19:22:53 -0400

    Implement Chat Manual NPCs in Bastok Markets, Aht Urgan Whitegate, Eastern/Western Adoulin, Southern San d'Oria, Ru'Lude Gardens, and Windurst Woods.

---
+++ scripts/zones/Windurst_Woods/npcs/Chat_Manual.lua
@@ -0,0 +1,43 @@
+-----------------------------------
+--  Area: Windurst Woods
+--   NPC: Chat Manual
+--  Type: Tutorial NPC
+-- @zone: 241
+--  @pos 10.928 1.915 -40.094
+-----------------------------------
+package.loaded["scripts/zones/Windurst_Woods/TextIDs"] = nil;
+-----------------------------------
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+    player:startEvent(0x17da);
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+    -- printf("CSID: %u",csid);
+    -- printf("RESULT: %u",option);
+end;
+
+-----------------------------------
+-- onEventFinish
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+    -- printf("CSID: %u",csid);
+    -- printf("RESULT: %u",option);
+end;
+
