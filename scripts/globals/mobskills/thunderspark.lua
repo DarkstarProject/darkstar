b9faa7a8d8356c889f125ecc40d3f444a3abeede
Tyler Schneider <teschnei@gmail.com>
2016-08-24 20:26:48 -0600

    Fixed mob Ramuh's Thunderspark triggering a debug break

--- scripts/globals/mobskills/thunderspark.lua
+++ scripts/globals/mobskills/thunderspark.lua
@@ -12,12 +12,12 @@ end;
 
 function onMobWeaponSkill(target, mob, skill)
     local typeEffect = EFFECT_PARALYSIS;
-    MobStatusEffectMove(mob, target, EFFECT_PARALYZE, 30, 0, 60);
+    MobStatusEffectMove(mob, target, typeEffect, 30, 0, 60);
 
     local dmgmod = 1;
     local accmod = 1;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*3,ELE_LIGHTNING,dmgmod,TP_NO_EFFECT);
-    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_WATER,MOBPARAM_IGNORE_SHADOWS);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_THUNDER,MOBPARAM_IGNORE_SHADOWS);
 
     target:delHP(dmg);
     return dmg;
