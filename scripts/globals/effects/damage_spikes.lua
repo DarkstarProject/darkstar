1c9e14ab8ac9fd4a057eac803b1a459762f85757
TeoTwawki <arawn.annwn@yandex.com>
2016-09-01 20:26:44 -0400

    Correct subeffect animation for "additional effect: dispel"

--- scripts/globals/effects/damage_spikes.lua
+++ scripts/globals/effects/damage_spikes.lua
@@ -12,7 +12,7 @@ require("scripts/globals/status");
 
 function onEffectGain(target,effect)
     -- TODO: Is this non elemental damage? Physical? Fire?
-    -- Why would SE use a separate status effects form blaze spikes if its fire though..
+    -- Why would SE use a separate status effects from blaze spikes if its fire though..
     target:addMod(MOD_SPIKES,1);
     target:addMod(MOD_SPIKES_DMG, effect:getPower());
 end;
