d92aa59f9e57d90d6cfe3c44f37f0318327065ee
TeoTwawki <arawn.annwn@yandex.com>
2016-09-04 18:54:26 -0400

    Lets check the GM tier, instead of the GM flag..

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -3693,7 +3693,7 @@ void SmallPacket0x0B5(map_session_data_t* session, CCharEntity* PChar, CBasicPac
     {
         //this makes sure a command isn't sent to chat
     }
-    else if (RBUFB(data, (0x06)) == '#' && PChar->nameflags.flags & FLAG_GM)
+    else if (RBUFB(data, (0x06)) == '#' && PChar->m_GMlevel > 0)
     {
         message::send(MSG_CHAT_SERVMES, 0, 0, new CChatMessagePacket(PChar, MESSAGE_SYSTEM_1, data[7]));
     }
