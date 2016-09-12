c2ab911a8433dca482e7bffa93a2517e16cd0b38
TeoTwawki <arawn.annwn@yandex.com>
2016-09-12 08:32:37 -0400

    Fix undefined procedure call emanating from scripts/zones/Attohwa_Chasm/mobs/Sargas.lua

--- scripts/zones/Attohwa_Chasm/mobs/Sargas.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Sargas.lua
@@ -59,6 +59,13 @@ function onSpikesDamage(mob,target,damage)
 end;
 
 -----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end;
+
+-----------------------------------
 -- onMobDespawn
 -----------------------------------
 
