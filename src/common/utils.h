6e0321a4a7e0e707e119410cc278778b49fc0344
Tyler Schneider <teschnei@gmail.com>
2016-09-24 12:13:33 -0600

    Readded mob magic range check
    Fixes #3321

--- src/common/utils.h
+++ src/common/utils.h
@@ -34,6 +34,11 @@ int32 checksum(uint8* buf,uint32 buflen,char checkhash[16]);
 
 float distance(const position_t& A, const position_t& B);		// distance between positions
 float distanceSquared(const position_t& A, const position_t& B);// squared distance between positions (use squared unless otherwise needed)
+constexpr float square(float distance)                          // constexpr square (used with distanceSquared)
+{
+    return distance * distance;
+}
+
 int32 intpow32(int32 base, int32 exponent);						// Exponential power of integers
 void getMSB(uint32* result,uint32 value);						// fast Most Significant Byte search under GCC or MSVC. Fallback included.
 float rotationToRadian(uint8 rotation);
