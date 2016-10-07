d962be3a8bb990cf4ea6dfddab7c45cd2f6de6ca
Tahir Akhlaq <xdemolish@hotmail.com>
2016-10-07 15:04:35 +0100

    added modifier for rampart duration

--- scripts/globals/abilities/rampart.lua
+++ scripts/globals/abilities/rampart.lua
@@ -22,11 +22,6 @@ end;
 -----------------------------------
 
 function onUseAbility(player,target,ability)
-    local buff = 30;
-    -- add 15 seconds if player wears valor coronet or its upgrade
-    local head = player:getEquipID(SLOT_HEAD);
-    if (head == 15078) or (head == 15251) then
-        buff = buff + 15;
-    end
-    target:addStatusEffect(EFFECT_MAGIC_SHIELD,1,0,buff);
+    local duration = 30 + player:getMod(MOD_RAMPART_DURATION);
+    target:addStatusEffect(EFFECT_MAGIC_SHIELD, 1, 0, duration);
 end;
