79e2f0cc32981cd3fec3b921e8175916989c7ef2
TeoTwawki <arawn.annwn@yandex.com>
2016-10-08 05:59:21 -0400

    Fix some issues with JoL fight (untargetable Ru'Phuabo, mobs depopping when they should not)

--- scripts/zones/AlTaieu/mobs/Jailer_of_Love.lua
+++ scripts/zones/AlTaieu/mobs/Jailer_of_Love.lua
@@ -48,68 +48,68 @@ function onMobFight(mob, target)
         local phuabo3 = GetMobAction(16912855);
 
         if (SPAWNS == 0) then -- Spawns first 3 xzomit
-            SpawnMob(16912858, 300):updateEnmity(target);
-            SpawnMob(16912859, 300):updateEnmity(target);
-            SpawnMob(16912860, 300):updateEnmity(target);
+            SpawnMob(16912858):updateEnmity(target);
+            SpawnMob(16912859):updateEnmity(target);
+            SpawnMob(16912860):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 1);
         elseif (SPAWNS == 1) then -- spawns first 3 hpemde
-            SpawnMob(16912867, 300):updateEnmity(target);
-            SpawnMob(16912868, 300):updateEnmity(target);
-            SpawnMob(16912869, 300):updateEnmity(target);
+            SpawnMob(16912867):updateEnmity(target);
+            SpawnMob(16912868):updateEnmity(target);
+            SpawnMob(16912869):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 2);
         elseif (SPAWNS == 2) then -- spawns first 3 phuabo
-            SpawnMob(16912849, 300):updateEnmity(target);
-            SpawnMob(16912850, 300):updateEnmity(target);
-            SpawnMob(16912851, 300):updateEnmity(target);
+            SpawnMob(16912849):updateEnmity(target);
+            SpawnMob(16912850):updateEnmity(target);
+            SpawnMob(16912851):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 3);
         elseif (SPAWNS == 3) then -- Spawns second 3 xzomit
-            SpawnMob(16912861, 300):updateEnmity(target);
-            SpawnMob(16912862, 300):updateEnmity(target);
-            SpawnMob(16912863, 300):updateEnmity(target);
+            SpawnMob(16912861):updateEnmity(target);
+            SpawnMob(16912862):updateEnmity(target);
+            SpawnMob(16912863):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 4);
         elseif (SPAWNS == 4) then -- spawns second 3 hpemde
-            SpawnMob(16912870, 300):updateEnmity(target);
-            SpawnMob(16912871, 300):updateEnmity(target);
-            SpawnMob(16912872, 300):updateEnmity(target);
+            SpawnMob(16912870):updateEnmity(target);
+            SpawnMob(16912871):updateEnmity(target);
+            SpawnMob(16912872):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 5);
         elseif (SPAWNS == 5) then -- spawns second 3 phuabo
-            SpawnMob(16912852, 300):updateEnmity(target);
-            SpawnMob(16912853, 300):updateEnmity(target);
-            SpawnMob(16912854, 300):updateEnmity(target);
+            SpawnMob(16912852):updateEnmity(target);
+            SpawnMob(16912853):updateEnmity(target);
+            SpawnMob(16912854):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 6);
         elseif (SPAWNS == 6) then -- Spawns last 3 xzomit
-            SpawnMob(16912864, 300):updateEnmity(target);
-            SpawnMob(16912865, 300):updateEnmity(target);
-            SpawnMob(16912866, 300):updateEnmity(target);
+            SpawnMob(16912864):updateEnmity(target);
+            SpawnMob(16912865):updateEnmity(target);
+            SpawnMob(16912866):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 7);
         elseif (SPAWNS == 7) then -- spawns last 3 hpemde
-            SpawnMob(16912873, 300):updateEnmity(target);
-            SpawnMob(16912874, 300):updateEnmity(target);
-            SpawnMob(16912875, 300):updateEnmity(target);
+            SpawnMob(16912873):updateEnmity(target);
+            SpawnMob(16912874):updateEnmity(target);
+            SpawnMob(16912875):updateEnmity(target);
             mob:setLocalVar("pop_pets", os.time());
             mob:setLocalVar("SPAWNS", 8);
         elseif (SPAWNS >= 8) then -- switch to ONLY popping phuabo (still up to 3 at a time)
             if (phuabo1 == ACTION_NONE or phuabo1 == ACTION_SPAWN) then
-                SpawnMob(16912849, 300):updateEnmity(target);
-                SpawnMob(16912850, 300):updateEnmity(target);
-                SpawnMob(16912851, 300):updateEnmity(target);
+                SpawnMob(16912849):updateEnmity(target);
+                SpawnMob(16912850):updateEnmity(target);
+                SpawnMob(16912851):updateEnmity(target);
                 mob:setLocalVar("pop_pets", os.time());
             elseif (phuabo2 == ACTION_NONE or phuabo2 == ACTION_SPAWN) then
-                SpawnMob(16912852, 300):updateEnmity(target);
-                SpawnMob(16912853, 300):updateEnmity(target);
-                SpawnMob(16912854, 300):updateEnmity(target);
+                SpawnMob(16912852):updateEnmity(target);
+                SpawnMob(16912853):updateEnmity(target);
+                SpawnMob(16912854):updateEnmity(target);
                 mob:setLocalVar("pop_pets", os.time());
             elseif (phuabo3 == ACTION_NONE or phuabo3 == ACTION_SPAWN) then
-                SpawnMob(16912855, 300):updateEnmity(target);
-                SpawnMob(16912856, 300):updateEnmity(target);
-                SpawnMob(16912857, 300):updateEnmity(target);
+                SpawnMob(16912855):updateEnmity(target);
+                SpawnMob(16912856):updateEnmity(target);
+                SpawnMob(16912857):updateEnmity(target);
                 mob:setLocalVar("pop_pets", os.time());
             end
         end
@@ -123,7 +123,7 @@ end;
 function onMobDespawn(mob)
     local AV_CHANCE = 25;
     if (AV_CHANCE > math.random(0,99)) then
-        SpawnMob(16912876, 180);
+        SpawnMob(16912876);
     end
 end;
 
