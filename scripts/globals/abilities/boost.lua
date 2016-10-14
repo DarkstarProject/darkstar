c8627752d1372762099756c182bbfcc4a92f8142
wrenffxi <oliviapekoe@gmail.com>
2016-10-14 09:23:55 -0400

    MOD_BOOST_EFFECT

--- scripts/globals/abilities/boost.lua
+++ scripts/globals/abilities/boost.lua
@@ -21,11 +21,7 @@ end;
 -----------------------------------
 
 function onUseAbility(player,target,ability)
-    local sHands = player:getEquipID(SLOT_HANDS);
-    local power = 12.5
-    if (sHands == 13962 or sHands == 14891) then
-        power = power + 5.5;
-    end
+    local power = 12.5 + (0.10 * player:getMod(MOD_BOOST_EFFECT));
 
     if (player:hasStatusEffect(EFFECT_BOOST) == true) then
         local effect = player:getStatusEffect(EFFECT_BOOST);
