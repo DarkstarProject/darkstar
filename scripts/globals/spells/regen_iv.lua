790ee7b4119883b974bacb0bfef9f0e0e02e7f99
wrenffxi <oliviapekoe@gmail.com>
2016-10-18 15:20:06 -0400

    Renamed MOD_REGEN_ADD_HP to MOD_LIGHT_ARTS_REGEN

--- scripts/globals/spells/regen_iv.lua
+++ scripts/globals/spells/regen_iv.lua
@@ -21,7 +21,7 @@ function onSpellCast(caster,target,spell)
 
     local hp = math.ceil(30 * (1 + 0.01 * caster:getMod(MOD_REGEN_MULTIPLIER))); -- spell base times gear multipliers
     hp = hp + caster:getMerit(MERIT_REGEN_EFFECT); -- bonus hp from merits
-    hp = hp + caster:getMod(MOD_REGEN_ADD_HP); -- bonus hp from light arts
+    hp = hp + caster:getMod(MOD_LIGHT_ARTS_REGEN); -- bonus hp from light arts
 
     local duration = 60;
 
