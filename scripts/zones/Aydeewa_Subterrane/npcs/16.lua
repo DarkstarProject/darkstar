841e2207cc78b326ddaf309e3b1bde4bb98b7a52
starlightknight <starlightknight7@gmail.com>
2016-10-20 09:39:01 -0400

    SCRIPTS: TOAU-27 Misplaced Nobility
    Cutscene in Aydeewa Subterrane at the blank at F-8/G-8.
    This is the same blank as in the earlier TOAU mission
    
    Sources:
    
    http://ffxiclopedia.wikia.com/wiki/Misplaced_Nobility
    https://www.bg-wiki.com/bg/Aht_Urhgan_Mission_27
    http://pastebin.com/w0TqRPMy

--- scripts/zones/Aydeewa_Subterrane/npcs/16.lua
+++ scripts/zones/Aydeewa_Subterrane/npcs/16.lua
@@ -1,6 +1,6 @@
 -----------------------------------
 -- Area: Aydeewa Subterrane
--- NPC: Blank (TOAU-20 Cutscene)
+-- NPC: Blank (TOAU-20 Cutscene, TOAU-27 Cutscene)
 -- @pos -298 36 -38 68
 -----------------------------------
 package.loaded["scripts/zones/Aydeewa_Subterrane/TextIDs"] = nil;
@@ -23,6 +23,8 @@ end;
 function onTrigger(player,npc)
     if (player:getCurrentMission(TOAU) == TEAHOUSE_TUMULT and player:getVar("AhtUrganStatus") == 1) then
         player:startEvent(11);
+    elseif (player:getCurrentMission(TOAU) == MISPLACED_NOBILITY) then
+        player:startEvent(12);
     end
 end; 
 
@@ -43,10 +45,13 @@ function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
 
-    if(csid == 11) then
+    if (csid == 11) then
         player:completeMission(TOAU,TEAHOUSE_TUMULT);
         player:setVar("AhtUrganStatus",0);
         player:addMission(TOAU,FINDERS_KEEPERS);
+    elseif (csid == 12) then
+        player:completeMission(TOAU,MISPLACED_NOBILITY);
+        player:addMission(TOAU,BASTION_OF_KNOWLEDGE);
     end
 end;
 
