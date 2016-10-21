2c5158e5cc6bc8896229a6ff3de831685a08a861
starlightknight <starlightknight7@gmail.com>
2016-10-21 12:29:50 -0400

    SCRIPTS: TOAU-41 Light of Judgment
    Cutscene with Rodin-Comidin in Aht Urgan Whitegate
    
    Sources:
    
    http://ffxiclopedia.wikia.com/wiki/Light_of_Judgment
    https://www.bg-wiki.com/bg/Aht_Urhgan_Mission_41
    gedads data: http://pastebin.com/w0TqRPMy

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Rodin-Comidin.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Rodin-Comidin.lua
@@ -2,10 +2,14 @@
 -- Area: Aht Urhgan Whitegate
 -- NPC: Rodin-Comidin
 -- Standard Info NPC
+-- Involved in Missions: TOAU-41
+-- @pos 17.205 -5.999 51.161 50
 -----------------------------------
 package.loaded["scripts/zones/Aht_Urhgan_Whitegate/TextIDs"] = nil;
 -----------------------------------
 
+require("scripts/globals/keyitems");
+require("scripts/globals/missions");
 require("scripts/zones/Aht_Urhgan_Whitegate/TextIDs");
 
 -----------------------------------
@@ -20,7 +24,11 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x0299);
+    if (player:getCurrentMission(TOAU) == LIGHT_OF_JUDGMENT) then
+        player:startEvent(3137,0,0,0,0,0,0,0,0,0);
+    else
+        player:startEvent(0x0299);
+    end
 end; 
 
 -----------------------------------
@@ -28,8 +36,8 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
+    printf("CSID: %u",csid);
+    printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -37,7 +45,15 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
+    printf("CSID: %u",csid);
+    printf("RESULT: %u",option);
+
+    if (csid == 3137) then
+        player:completeMission(TOAU,LIGHT_OF_JUDGMENT);
+        player:addKeyItem(NYZUL_ISLE_ROUTE);
+        player:messageSpecial(KEYITEM_OBTAINED,NYZUL_ISLE_ROUTE);
+        player:addMission(TOAU,PATH_OF_DARKNESS);
+    end
+
 end;
 
