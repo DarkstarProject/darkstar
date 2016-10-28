1a15ee3cfcfa562f15324df113419b95fa66d374
Fiocitrine <justynwhitney@gmail.com>
2016-10-28 17:38:15 -0400

    Added Brothers ENM to database drop table mob spell lists so on and so forth

---
+++ scripts/zones/Bearclaw_Pinnacle/npcs/Armoury_Crate.lua
@@ -0,0 +1,41 @@
+-----------------------------------
+-- Area: Bearclaw Pinnacle
+-- NPC:  Armoury Crate
+-----------------------------------
+package.loaded["scripts/zones/Balgas_Dais/TextIDs"] = nil;
+-------------------------------------
+
+require("scripts/globals/titles");
+require("scripts/globals/quests");
+require("scripts/zones/Balgas_Dais/TextIDs");
+
+-----------------------------------
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+    player:getBCNMloot();
+end;
+
+-----------------------------------
+-- onEventUpdate
+-----------------------------------
+
+function onEventUpdate(player,csid,option)
+end;
+
+-----------------------------------
+-- onEventFinish Action
+-----------------------------------
+
+function onEventFinish(player,csid,option)
+end;
---
+++ scripts/zones/Uleguerand_Range/npcs/Chamnaet_Spring.lua
@@ -0,0 +1,38 @@
+-----------------------------------
+--  Area: Uleguerand Range
+--  NPC:  Chamnaet Spring
+--  Receive Chamnaet Ice upon trading Cotton Pouch
+-- @pos -305.240 3.605 17.977
+
+-----------------------------------
+package.loaded["scripts/zones/Uleguerand_Range/TextIDs"] = nil;
+-----------------------------------
+
+require("scripts/globals/keyitems");
+require("scripts/zones/Uleguerand_Range/TextIDs");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+    -- Trade Cotton Pouch
+    if (trade:hasItemQty(1779,1) and trade:getItemCount() == 1) then
+        if (player:getFreeSlotsCount() == 0) then
+            player:messageSpecial(ITEM_CANNOT_BE_OBTAINED, 1780); -- Chamnaet Ice
+        else
+            player:tradeComplete();
+            player:addItem(1780);
+            player:messageSpecial(ITEM_OBTAINED, 1780); -- Chamnaet Ice
+        end
+    end
+end;
+
+-----------------------------------
+-- onTrigger Action
+-----------------------------------
+
+function onTrigger(player,npc)
+
+    
+end;
