6edefb9084136cb398758bf6f7a9521645f5d089
wrenffxi <oliviapekoe@gmail.com>
2016-12-14 10:47:18 -0500

    Fixed the inaccurate pos in comments for BLM AF3 QMs.

--- scripts/zones/Toraimarai_Canal/npcs/qm9.lua
+++ scripts/zones/Toraimarai_Canal/npcs/qm9.lua
@@ -2,8 +2,7 @@
 -- Area: Toraimarai Canal
 -- NPC:  ???
 -- Involved In Quest: Wild Card
--- @zone 169 // not accurate
--- @pos 220 16 -50 // not accurate
+-- @pos -137 16 151 169
 -----------------------------------
 package.loaded["scripts/zones/Toraimarai_Canal/TextIDs"] = nil;
 -----------------------------------
