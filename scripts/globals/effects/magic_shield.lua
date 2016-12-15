499abfcb3a374305de62da89b49f5763334a2340
FFera <eraffxi@users.noreply.github.com>
2016-12-14 21:21:29 -0500

    Update magic_shield.lua

--- scripts/globals/effects/magic_shield.lua
+++ scripts/globals/effects/magic_shield.lua
@@ -1,65 +1,31 @@
 -----------------------------------
---
--- Magic Shield BLOCKS all magic attacks
--- Magic Shield from Rampart provides magical stoneskin at vit*2
---
+-- Ability: Rampart
+-- Grants a Magic Shield effect and enhances defense for party members within area of effect.
+-- Obtained: Paladin Level 62
+-- Recast Time: 5:00
+-- Duration: 0:30
 -----------------------------------
 
+require("scripts/globals/settings");
 require("scripts/globals/status");
 
 -----------------------------------
--- onEffectGain Action
+-- onAbilityCheck
 -----------------------------------
 
-function onEffectGain(target,effect)
-    if (target:getObjType() == TYPE_PC) then
-		target:setMod(MOD_MAGIC_SHIELD, effect:getPower());
-		target:setVar("Rampart", 1);
-	elseif (effect:getPower() == 3) then -- arcane stomp
-        target:addMod(MOD_FIRE_ABSORB, 100);
-        target:addMod(MOD_EARTH_ABSORB, 100);
-        target:addMod(MOD_WATER_ABSORB, 100);
-        target:addMod(MOD_WIND_ABSORB, 100);
-        target:addMod(MOD_ICE_ABSORB, 100);
-        target:addMod(MOD_LTNG_ABSORB, 100);
-        target:addMod(MOD_LIGHT_ABSORB, 100);
-        target:addMod(MOD_DARK_ABSORB, 100);
-    elseif (effect:getPower() == 1) then
-        target:addMod(MOD_UDMGMAGIC, -101);
-    elseif (effect:getPower() == 0) then
-        target:addMod(MOD_MAGIC_ABSORB, 100);
-    end;
+function onAbilityCheck(player,target,ability)
+    return 0,0;
 end;
 
 -----------------------------------
--- onEffectTick Action
+-- onUseAbility
 -----------------------------------
 
-function onEffectTick(target,effect)
-end;
+function onUseAbility(player,target,ability)
+    local duration = 30 + player:getMod(MOD_RAMPART_DURATION);
+    local vit = player:getStat(MOD_VIT);
+    local subpower = vit*2;
 
------------------------------------
--- onEffectLose Action
------------------------------------
+    target:addStatusEffect(EFFECT_MAGIC_SHIELD, 4, 0, duration, subpower);
 
-function onEffectLose(target,effect)
-    if (target:getObjType() == TYPE_PC) then
-		target:delMod(MOD_MAGIC_SHIELD, 0);
-		target:setVar("Rampart", 0);
-    elseif (target:getVar("Rampart") == 0) then
-	    if (effect:getPower() == 3) then -- arcane stomp
-            target:delMod(MOD_FIRE_ABSORB, 100);
-            target:delMod(MOD_EARTH_ABSORB, 100);
-            target:delMod(MOD_WATER_ABSORB, 100);
-            target:delMod(MOD_WIND_ABSORB, 100);
-            target:delMod(MOD_ICE_ABSORB, 100);
-            target:delMod(MOD_LTNG_ABSORB, 100);
-            target:delMod(MOD_LIGHT_ABSORB, 100);
-            target:delMod(MOD_DARK_ABSORB, 100);
-        elseif (effect:getPower() == 1) then
-           target:delMod(MOD_UDMGMAGIC, -101);
-        elseif (effect:getpower() == 0) then
-           target:delMod(MOD_MAGIC_ABSORB, 100);
-        end;
-    end;
 end;
