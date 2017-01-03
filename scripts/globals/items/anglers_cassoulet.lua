3c96ca438e2dadd9081d9b4fd734423e524d891d
wrenffxi <oliviapekoe@gmail.com>
2017-01-03 15:51:21 -0500

    reverting change to anglers_cassoulet. should give regen effect rather than auto-regen mod.

--- scripts/globals/items/anglers_cassoulet.lua
+++ scripts/globals/items/anglers_cassoulet.lua
@@ -15,28 +15,28 @@ require("scripts/globals/status");
 -- OnItemCheck
 -----------------------------------------
 function onItemCheck(target)
-    local result = 0;
-    if (target:hasStatusEffect(EFFECT_FOOD) == true or target:hasStatusEffect(EFFECT_FIELD_SUPPORT_FOOD) == true) then
-        result = 246;
-    end
-    return result;
+    return 0;
 end;
 
 -----------------------------------------
 -- OnItemUse
 -----------------------------------------
 function onItemUse(target)
-    target:addStatusEffect(EFFECT_FOOD,0,0,3600,5704);
+    if (target:hasStatusEffect(EFFECT_FOOD) == false and target:hasStatusEffect(EFFECT_REGEN) == false) then
+        target:addStatusEffect(EFFECT_FOOD,0,0,3600,5704);
+    else
+        target:messageBasic(423);
+    end
 end;
 
 ----------------------------------------
 -- onEffectGain
 ----------------------------------------
 function onEffectGain(target,effect)
+    target:addStatusEffect(EFFECT_REGEN,1,3,3600);
     target:addMod(MOD_VIT, -1);
     target:addMod(MOD_AGI, 5);
     target:addMod(MOD_RACC, 5);
-    target:addMod(MOD_REGEN, 1);
 end
 
 -----------------------------------------
@@ -46,5 +46,4 @@ function onEffectLose(target,effect)
     target:delMod(MOD_VIT, -1);
     target:delMod(MOD_AGI, 5);
     target:delMod(MOD_RACC, 5);
-    target:delMod(MOD_REGEN, 1);
 end;
