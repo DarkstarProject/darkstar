3bf439a834624cf195ac31605693ac5db8480396
wrenffxi <oliviapekoe@gmail.com>
2017-02-15 17:40:08 -0500

    Fe'e (Up in Arms kraken NM) is supposed to attack really fast at the start of the fight, and slow down as it takes damage. I eyeballed these delay values from watching YouTube vids of the fight.

--- scripts/zones/Waughroon_Shrine/mobs/Fe_e.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Fe_e.lua
@@ -3,12 +3,37 @@
 --  MOB: fe_e
 -----------------------------------
 
-
 -----------------------------------
 -- onMobSpawn Action
 -----------------------------------
 
 function onMobSpawn(mob)
+end;
+
+-----------------------------------
+-- onMobFight Action
+-----------------------------------
+
+function onMobFight(mob,target)
+
+    local hpp = mob:getHPP();
+
+    if hpp < 14 then
+        mob:setDelay(2700);
+    elseif hpp < 28 then
+        mob:setDelay(2350);
+    elseif hpp < 42 then
+        mob:setDelay(2000);
+    elseif hpp < 56 then
+        mob:setDelay(1650);
+    elseif hpp < 70 then
+        mob:setDelay(1300);
+    elseif hpp < 84 then
+        mob:setDelay(950);
+    else
+        mob:setDelay(600);
+    end
+
 end;
 
 -----------------------------------
