cc179436f840db4fa953fd85b2009aa050056516
TeoTwawki <arawn.annwn@yandex.com>
2017-03-09 14:22:29 -0500

    local vars not global. Indents indents indents.

--- scripts/zones/Upper_Jeuno/npcs/Antonia.lua
+++ scripts/zones/Upper_Jeuno/npcs/Antonia.lua
@@ -1,47 +1,47 @@
 -----------------------------------
 -- Area: Upper Jeuno
--- NPC: Antonia
+--  NPC: Antonia
 -- Standard Merchant NPC
 -----------------------------------
-
-require("scripts/globals/shop");
 package.loaded["scripts/zones/Upper_Jeuno/TextIDs"] = nil;
 require("scripts/zones/Upper_Jeuno/TextIDs");
+-----------------------------------
+require("scripts/globals/shop");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end; 
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
-    
-player:showText(npc,ANTONIA_SHOP_DIALOG);
-
-stock = {0x5400,100100, -- Arasy Sainti
-     0x5432,100100, -- Arasy Knife
-     0x5464,100100, -- Arasy Sword
-     0x5496,100100, -- Arasy Claymore
-     0x54C8,100100, -- Arasy Tabar
-     0x5502,100100, -- Arasy Axe
-     0x5534,100100, -- Arasy Scythe
-     0x5569,100100, -- Arasy Lance
-     0x5595,100100, -- Yoshikiri
-     0x55C8,100100, -- Ashijiro no Tachi
-     0x55FF,100100, -- Arasy Rod
-     0x563A,100100, -- Arasy Staff
-     0x566A,100100, -- Arasy Bow
-     0x5677,100100, -- Arasy Gun
-     0x5390,100100, -- Animator Z
-     0x5391,100100} -- Arasy Sachet
- 
-showShop(player, STATIC, stock);
-end; 
+    local stock =
+    {
+        21504, 100100, -- Arasy Sainti
+        21554, 100100, -- Arasy Knife
+        21604, 100100, -- Arasy Sword
+        21654, 100100, -- Arasy Claymore
+        21704, 100100, -- Arasy Tabar
+        21762, 100100, -- Arasy Axe
+        21812, 100100, -- Arasy Scythe
+        21865, 100100, -- Arasy Lance
+        21909, 100100, -- Yoshikiri
+        21960, 100100, -- Ashijiro no Tachi
+        22015, 100100, -- Arasy Rod
+        22074, 100100, -- Arasy Staff
+        22122, 100100, -- Arasy Bow
+        22135, 100100, -- Arasy Gun
+        21392, 100100, -- Animator Z
+        21393, 100100  -- Arasy Sachet
+    }
+    player:showText(npc,ANTONIA_SHOP_DIALOG);
+    showShop(player, STATIC, stock);
+end;
 
 -----------------------------------
 -- onEventUpdate
@@ -60,4 +60,3 @@ function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
 end;
-
