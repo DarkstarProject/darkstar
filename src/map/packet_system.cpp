04f58a5d30da58f8d3e20fe515379ac2d61f5648
UnknownX <jaguar51896@gmail.com>
2017-03-12 15:33:52 -0500

    Players now return to their home point when the death timer runs out.

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -695,23 +695,7 @@ void SmallPacket0x01A(map_session_data_t* session, CCharEntity* PChar, CBasicPac
     {
         if (!PChar->isDead())
             return;
-        // remove weakness on homepoint
-        PChar->StatusEffectContainer->DelStatusEffectSilent(EFFECT_WEAKNESS);
-        PChar->StatusEffectContainer->DelStatusEffectSilent(EFFECT_LEVEL_SYNC);
-
-        PChar->health.hp = PChar->GetMaxHP();
-        PChar->health.mp = PChar->GetMaxMP();
-
-        PChar->loc.boundary = 0;
-        PChar->loc.p = PChar->profile.home_point.p;
-        PChar->loc.destination = PChar->profile.home_point.destination;
-
-        PChar->status = STATUS_DISAPPEAR;
-        PChar->animation = ANIMATION_NONE;
-        PChar->updatemask |= UPDATE_HP;
-
-        PChar->clearPacketList();
-        charutils::SendToZone(PChar, 2, zoneutils::GetZoneIPP(PChar->loc.destination));
+        PChar->OnDeathTimer();
     }
     break;
     case 0x0C: // assist
