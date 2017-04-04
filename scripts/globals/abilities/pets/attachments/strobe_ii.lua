2a573876b238fdf99550e4fbf9ef26425801a224
UnknownX <jaguar51896@gmail.com>
2017-04-04 13:31:31 -0500

    Added Strobe and Strobe II

---
+++ scripts/globals/abilities/pets/attachments/strobe_ii.lua
@@ -0,0 +1,39 @@
+-----------------------------------
+-- Attachment: Strobe II
+-- http://forum.square-enix.com/ffxi/threads/49065?p=565264#post565264
+-- Values are currently PRIOR TO NOVEMBER 2015 UPDATE!
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onEquip(pet)
+    pet:addMod(MOD_ENMITY, 10) -- 20 currently on retail
+end
+
+function onUnequip(pet)
+    pet:delMod(MOD_ENMITY, 10) -- 20 currently on retail
+end
+
+function onManeuverGain(pet,maneuvers)
+    if (maneuvers == 1) then
+        pet:addMod(MOD_ENMITY, 15); -- 20 currently on retail
+    elseif (maneuvers == 2) then
+        pet:addMod(MOD_ENMITY, 15); -- 25 currently on retail
+    elseif (maneuvers == 3) then
+        pet:addMod(MOD_ENMITY, 20); -- 35 currently on retail
+    end
+end
+
+function onManeuverLose(pet,maneuvers)
+    if (maneuvers == 1) then
+        pet:delMod(MOD_ENMITY, 15); -- 20 currently on retail
+    elseif (maneuvers == 2) then
+        pet:delMod(MOD_ENMITY, 15); -- 25 currently on retail
+    elseif (maneuvers == 3) then
+        pet:delMod(MOD_ENMITY, 20); -- 35 currently on retail
+    end
+end
