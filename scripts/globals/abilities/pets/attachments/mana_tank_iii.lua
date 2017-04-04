7b41fa6b729063498ab84b15865d959bfa6f885c
UnknownX <jaguar51896@gmail.com>
2017-04-04 13:02:44 -0500

    Added Mana Tank III

--- scripts/globals/abilities/pets/attachments/mana_tank_iii.lua
+++ scripts/globals/abilities/pets/attachments/mana_tank_iii.lua
@@ -1,5 +1,5 @@
 -----------------------------------
--- Attachment: Mana Tank II
+-- Attachment: Mana Tank III
 -----------------------------------
 
 require("scripts/globals/status");
@@ -9,25 +9,25 @@ require("scripts/globals/status");
 -----------------------------------
 
 function onEquip(pet)
-    pet:addMod(MOD_MPP, 8);
+    pet:addMod(MOD_MPP, 13);
 end
 
 function onUnequip(pet)
-    pet:delMod(MOD_MPP, 8);
+    pet:delMod(MOD_MPP, 13);
 end
 
 function onManeuverGain(pet,maneuvers)
     local bonus = 0
     local frame = pet:getAutomatonFrame()
     if (maneuvers == 1) then
+        if frame == 0x23 then bonus = 3 end
+        pet:addMod(MOD_REFRESH, 7 + bonus);
+    elseif (maneuvers == 2) then
+        if frame == 0x23 then bonus = 2 end
+        pet:addMod(MOD_REFRESH, 1 + bonus);
+    elseif (maneuvers == 3) then
         if frame == 0x23 then bonus = 2 end
         pet:addMod(MOD_REFRESH, 2 + bonus);
-    elseif (maneuvers == 2) then
-        if frame == 0x23 then bonus = 1 end
-        pet:addMod(MOD_REFRESH, 2 + bonus);
-    elseif (maneuvers == 3) then
-        if frame == 0x23 then bonus = 1 end
-        pet:addMod(MOD_REFRESH, 1 + bonus);
     end
 end
 
@@ -35,13 +35,13 @@ function onManeuverLose(pet,maneuvers)
     local bonus = 0
     local frame = pet:getAutomatonFrame()
     if (maneuvers == 1) then
+        if frame == 0x23 then bonus = 3 end
+        pet:delMod(MOD_REFRESH, 7 + bonus);
+    elseif (maneuvers == 2) then
+        if frame == 0x23 then bonus = 2 end
+        pet:delMod(MOD_REFRESH, 1 + bonus);
+    elseif (maneuvers == 3) then
         if frame == 0x23 then bonus = 2 end
         pet:delMod(MOD_REFRESH, 2 + bonus);
-    elseif (maneuvers == 2) then
-        if frame == 0x23 then bonus = 1 end
-        pet:delMod(MOD_REFRESH, 2 + bonus);
-    elseif (maneuvers == 3) then
-        if frame == 0x23 then bonus = 1 end
-        pet:delMod(MOD_REFRESH, 1 + bonus);
     end
 end
