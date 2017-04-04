bb4cca434732c90b8f958ad45a8456beecae56eb
UnknownX <jaguar51896@gmail.com>
2017-04-04 17:11:09 -0500

    Added Percolator

---
+++ scripts/globals/abilities/pets/attachments/percolator.lua
@@ -0,0 +1,37 @@
+-----------------------------------
+-- Attachment: Percolator
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onEquip(pet)
+    pet:addMod(MOD_COMBAT_SKILLUP_RATE, 20);
+end
+
+function onUnequip(pet)
+    pet:delMod(MOD_COMBAT_SKILLUP_RATE, 20);
+end
+
+function onManeuverGain(pet,maneuvers)
+    if (maneuvers == 1) then
+        pet:addMod(MOD_COMBAT_SKILLUP_RATE, 20);
+    elseif (maneuvers == 2) then
+        pet:addMod(MOD_COMBAT_SKILLUP_RATE, 20);
+    elseif (maneuvers == 3) then
+        pet:addMod(MOD_COMBAT_SKILLUP_RATE, 20);
+    end
+end
+
+function onManeuverLose(pet,maneuvers)
+    if (maneuvers == 1) then
+        pet:delMod(MOD_COMBAT_SKILLUP_RATE, 20);
+    elseif (maneuvers == 2) then
+        pet:delMod(MOD_COMBAT_SKILLUP_RATE, 20);
+    elseif (maneuvers == 3) then
+        pet:delMod(MOD_COMBAT_SKILLUP_RATE, 20);
+    end
+end
