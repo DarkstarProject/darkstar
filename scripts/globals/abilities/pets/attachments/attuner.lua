53c278c4d6ed83de69281cc2dedc029f5ef3e9c4
UnknownX <jaguar51896@gmail.com>
2017-04-17 06:34:22 -0500

    Added Attuner attachment

---
+++ scripts/globals/abilities/pets/attachments/attuner.lua
@@ -0,0 +1,100 @@
+-----------------------------------
+-- Attachment: Attuner
+-----------------------------------
+
+require("scripts/globals/status")
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onEquip(pet)
+    pet:addListener("AUTOMATON_ON_DEPLOY", "AUTO_ATTUNER_ENGAGE", function(pet, target)
+        local master = pet:getMaster()
+        if pet:getLocalVar("attuner") > 0 then
+            for maneuvers = master:countEffect(EFFECT_FIRE_MANEUVER), 1, -1  do
+                if maneuvers == 1 then
+                    pet:delMod(MOD_ATTP, 18) -- Ignore 15% def
+                    pet:delMod(MOD_RATTP, 18)
+                elseif maneuvers == 2 then
+                    pet:delMod(MOD_ATTP, 25) -- Ignore 30% def
+                    pet:delMod(MOD_RATTP, 25)
+                elseif maneuvers == 3 then
+                    pet:delMod(MOD_ATTP, 39) -- Ignore 45% def
+                    pet:delMod(MOD_RATTP, 39)
+                end
+            end
+            pet:setLocalVar("attuner", 0)
+        end
+
+        if pet:getMainLvl() < target:getMainLvl() then
+            local maneuvers = 
+            pet:setLocalVar("attuner", 1)
+            for maneuvers = 1, master:countEffect(EFFECT_FIRE_MANEUVER) do
+                if maneuvers == 1 then
+                    pet:addMod(MOD_ATTP, 18) -- Ignore 15% def
+                    pet:addMod(MOD_RATTP, 18)
+                elseif maneuvers == 2 then
+                    pet:addMod(MOD_ATTP, 25) -- Ignore 30% def
+                    pet:addMod(MOD_RATTP, 25)
+                elseif maneuvers == 3 then
+                    pet:addMod(MOD_ATTP, 39) -- Ignore 45% def
+                    pet:addMod(MOD_RATTP, 39)
+                end
+            end
+        end
+    end)
+    pet:addListener("DISENGAGE", "AUTO_ATTUNER_DISENGAGE", function(pet)
+        if pet:getLocalVar("attuner") > 0 then
+            local master = pet:getMaster()
+            for maneuvers = master:countEffect(EFFECT_FIRE_MANEUVER), 1, -1  do
+                if maneuvers == 1 then
+                    pet:delMod(MOD_ATTP, 18) -- Ignore 15% def
+                    pet:delMod(MOD_RATTP, 18)
+                elseif maneuvers == 2 then
+                    pet:delMod(MOD_ATTP, 25) -- Ignore 30% def
+                    pet:delMod(MOD_RATTP, 25)
+                elseif maneuvers == 3 then
+                    pet:delMod(MOD_ATTP, 39) -- Ignore 45% def
+                    pet:delMod(MOD_RATTP, 39)
+                end
+            end
+            pet:setLocalVar("attuner", 0)
+        end
+    end)
+end
+
+function onUnequip(pet)
+    pet:removeListener("AUTO_ATTUNER_ENGAGE")
+    pet:removeListener("AUTO_ATTUNER_DISENGAGE")
+end
+
+function onManeuverGain(pet,maneuvers)
+    if pet:getLocalVar("attuner") > 0 then
+        if maneuvers == 1 then
+            pet:addMod(MOD_ATTP, 18) -- Ignore 15% def
+            pet:addMod(MOD_RATTP, 18)
+        elseif maneuvers == 2 then
+            pet:addMod(MOD_ATTP, 25) -- Ignore 30% def
+            pet:addMod(MOD_RATTP, 25)
+        elseif maneuvers == 3 then
+            pet:addMod(MOD_ATTP, 39) -- Ignore 45% def
+            pet:addMod(MOD_RATTP, 39)
+        end
+    end
+end
+
+function onManeuverLose(pet,maneuvers)
+    if pet:getLocalVar("attuner") > 0 then
+        if maneuvers == 1 then
+            pet:delMod(MOD_ATTP, 18) -- Ignore 15% def
+            pet:delMod(MOD_RATTP, 18)
+        elseif maneuvers == 2 then
+            pet:delMod(MOD_ATTP, 25) -- Ignore 30% def
+            pet:delMod(MOD_RATTP, 25)
+        elseif maneuvers == 3 then
+            pet:delMod(MOD_ATTP, 39) -- Ignore 45% def
+            pet:delMod(MOD_RATTP, 39)
+        end
+    end
+end
