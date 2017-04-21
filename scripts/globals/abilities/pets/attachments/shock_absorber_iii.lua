ccdfeae7f4ec875bbbec7744b9aac50a1735193c
UnknownX <jaguar51896@gmail.com>
2017-04-21 01:36:30 -0500

    Added Shock Absorber III just to get it out of the way

---
+++ scripts/globals/abilities/pets/attachments/shock_absorber_iii.lua
@@ -0,0 +1,34 @@
+-----------------------------------
+-- Attachment: Shock Absorber III
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onEquip(pet)
+	pet:setLocalVar("stoneskin", VanadielTime() + 180)
+	pet:setLocalVar("shockabsorber", pet:getLocalVar("shockabsorber") + 4)
+end
+
+function onUnequip(pet)
+	pet:setLocalVar("shockabsorber", pet:getLocalVar("shockabsorber") - 4)
+end
+
+function onManeuverGain(pet,maneuvers)
+end
+
+function onManeuverLose(pet,maneuvers)
+end
+
+function onAttachmentCheck(pet,target)
+	local master = pet:getMaster()
+    if master and master:countEffect(EFFECT_EARTH_MANEUVER) > 0 and pet:getLocalVar("stoneskin") < VanadielTime() then
+        pet:setLocalVar("stoneskin", VanadielTime() + 180)
+        return 1946, pet
+    else
+        return 0
+    end
+end
