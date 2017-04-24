c4c7ea6f1272e5c2bdfcecf25e8f8039de0cca15
UnknownX <jaguar51896@gmail.com>
2017-04-24 01:30:39 -0500

    Fixed Economizer again

--- scripts/globals/abilities/pets/attachments/economizer.lua
+++ scripts/globals/abilities/pets/attachments/economizer.lua
@@ -22,9 +22,9 @@ end
 
 function onAttachmentCheck(pet,target)
 	local master = pet:getMaster()
-	local maneuvers = master and master:countEffect(EFFECT_DARK_MANEUVER) or 7
+	local maneuvers = (master and master:countEffect(EFFECT_DARK_MANEUVER) > 0) and master:countEffect(EFFECT_DARK_MANEUVER) or 7
 	local mpthreshold = 60 - maneuvers * 10
-	local mpp = (pet:getMaxMP() > 0) and math.ceil(pet:getMP()/pet:getMaxMP()) or 100
+	local mpp = (pet:getMaxMP() > 0) and math.ceil(pet:getMP()/pet:getMaxMP() * 100) or 100
     if mpp < mpthreshold and pet:getLocalVar("mprestore") < VanadielTime() then
         pet:setLocalVar("mprestore", VanadielTime() + 180)
         return 2068, pet
