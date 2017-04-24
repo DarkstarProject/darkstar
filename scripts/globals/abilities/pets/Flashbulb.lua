7517107ee9a12829998216482d8259805817aa73
UnknownX <jaguar51896@gmail.com>
2017-04-23 23:22:02 -0500

    Added Flashbulb

---
+++ scripts/globals/abilities/pets/Flashbulb.lua
@@ -0,0 +1,40 @@
+---------------------------------------------
+--  Flashbulb
+---------------------------------------------
+
+require("scripts/globals/status")
+require("scripts/globals/settings")
+require("scripts/globals/automatonweaponskills")
+
+---------------------------------------------------
+
+function onMobSkillCheck(target, automaton, skill)
+    return 0
+end
+
+function onAutomatonAbility(automaton, target, skill, tp, master, action)
+	local highest = automaton:getSkillLevel(22)
+	local highestskill = 22
+	if automaton:getSkillLevel(23) > highest then
+		highestskill = 23
+		highest = automaton:getSkillLevel(23)
+	end
+	if automaton:getSkillLevel(24) > highest then
+		highestskill = 24
+	end
+
+    local resist = applyResistanceAbility(automaton, target, 7, highestskill, 150)
+    local duration = 12 * resist
+
+    if resist > 0.0625 then
+        if target:addStatusEffect(EFFECT_FLASH, 200, 0, duration) then
+            skill:setMsg(243) -- MSG_ENFEEB
+        else
+            skill:setMsg(189) -- MSG_NO_EFFECT
+        end
+    else
+        spell:setMsg(85) -- MSG_RESIST
+    end
+
+    return EFFECT_FLASH
+end
---
+++ scripts/globals/abilities/pets/attachments/flashbulb.lua
@@ -0,0 +1,31 @@
+-----------------------------------
+-- Attachment: Flashbulb
+-----------------------------------
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onEquip(pet)
+end
+
+function onUnequip(pet)
+end
+
+function onManeuverGain(pet,maneuvers)
+end
+
+function onManeuverLose(pet,maneuvers)
+end
+
+function onAttachmentCheck(pet,target)
+	local master = pet:getMaster()
+    if master and master:countEffect(EFFECT_LIGHT_MANEUVER) > 0 and pet:getLocalVar("flash") < VanadielTime() then
+        pet:setLocalVar("flash", VanadielTime() + 45)
+        return 1947
+    else
+        return 0
+    end
+end
