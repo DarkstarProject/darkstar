8f4ec6cc1259d8be763d6d2a3f6f924905fafada
UnknownX <jaguar51896@gmail.com>
2017-04-24 01:16:33 -0500

    Fixed Economizer MP check

--- scripts/globals/abilities/pets/attachments/economizer.lua
+++ scripts/globals/abilities/pets/attachments/economizer.lua
@@ -24,7 +24,8 @@ function onAttachmentCheck(pet,target)
 	local master = pet:getMaster()
 	local maneuvers = master and master:countEffect(EFFECT_DARK_MANEUVER) or 7
 	local mpthreshold = 60 - maneuvers * 10
-    if pet:getMaxMP() > 0 and pet:getMPP() < mpthreshold and pet:getLocalVar("mprestore") < VanadielTime() then
+	local mpp = (pet:getMaxMP() > 0) and math.ceil(pet:getMP()/pet:getMaxMP()) or 100
+    if mpp < mpthreshold and pet:getLocalVar("mprestore") < VanadielTime() then
         pet:setLocalVar("mprestore", VanadielTime() + 180)
         return 2068, pet
     else
