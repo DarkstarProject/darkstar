37232ed7f2875bf74c7934958abd3e043e89459a
dwn134 <dwn134@gmail.com>
2017-05-24 21:23:19 -0500

    Update Leonhardt.lua
    added quest too many chefs

--- scripts/zones/Metalworks/npcs/Leonhardt.lua
+++ scripts/zones/Metalworks/npcs/Leonhardt.lua
@@ -1,6 +1,7 @@
 -----------------------------------
 -- Area: Metalworks
 -- NPC:  Leonhardt
+-- Involved in Quest: Too Many Chefs
 -- Standard Info NPC
 -----------------------------------
 package.loaded["scripts/zones/Metalworks/TextIDs"] = nil;
@@ -13,14 +14,32 @@ require("scripts/zones/Metalworks/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
+
+	if (player:getVar("TOO_MANY_CHEFS") == 3) then
+		if trade:hasItemQty(2527,1) then		-- Trade Red Oven Mitt
+			player:tradeComplete();
+			player:startEvent(0x03b6); 
+		
+	end
+end	
 end; 
+
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x03B1);
+
+	if (player:getVar("TOO_MANY_CHEFS") == 1) then
+		player:startEvent(0x03b4); -- part 2 Too Many Chefs
+	
+	
+	else
+		player:startEvent(0x03b1); -- standard
+	end
+	
+	
 end; 
 
 -----------------------------------
@@ -39,5 +58,9 @@ end;
 function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
+	if (csid == 0x03b4) then
+		player:setVar("TOO_MANY_CHEFS",2);
+	elseif (csid == 0x03b6) then
+		player:setVar("TOO_MANY_CHEFS",4);
+	end	
 end;
-
