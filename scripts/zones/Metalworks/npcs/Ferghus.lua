b96b58aca8c065f5a9db91d86b58f4e580520fcf
dwn134 <dwn134@gmail.com>
2017-05-24 21:21:49 -0500

    Update Ferghus.lua
    Quest Too many chefs added

--- scripts/zones/Metalworks/npcs/Ferghus.lua
+++ scripts/zones/Metalworks/npcs/Ferghus.lua
@@ -1,12 +1,16 @@
 -----------------------------------
 -- Area: Metalworks
 -- NPC:  Ferghus
+-- Starts Quest: Too Many Chefs (1,86)
 -- Type: Standard Info NPC
 -----------------------------------
 package.loaded["scripts/zones/Metalworks/TextIDs"] = nil;
 -----------------------------------
 
 require("scripts/zones/Metalworks/TextIDs");
+require("scripts/globals/quests");
+require("scripts/globals/status");
+require("scripts/globals/titles");
 
 -----------------------------------
 -- onTrade Action
@@ -20,7 +24,17 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-    player:startEvent(0x01A4);
+		
+	local TooManyChefs = player:getQuestStatus(BASTOK,TOO_MANY_CHEFS);
+	pFame = player:getFameLevel(BASTOK);
+		
+	if (TooManyChefs == QUEST_AVAILABLE and pFame >= 5) then
+		player:startEvent(0x03b2); -- Start Quest "Too Many Chefs"
+	elseif 	(player:getVar("TOO_MANY_CHEFS") == 4) then -- after trade to Leonhardt
+		player:startEvent(0x03b3);
+	else
+		player:startEvent(0x01A4); -- Standard
+	end
 end; 
 
 -----------------------------------
@@ -39,5 +53,13 @@ end;
 function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
+	
+	if (csid == 0x03b2 and option == 0) then
+		player:addQuest(BASTOK,TOO_MANY_CHEFS);
+		player:setVar("TOO_MANY_CHEFS",1);
+	
+	elseif (csid == 0x03b3) then
+		player:setVar("TOO_MANY_CHEFS",5);
+	end
+		
 end;
-
