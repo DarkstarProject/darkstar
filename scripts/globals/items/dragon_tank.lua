f97fb0276140bbb86d69a97ebddf20e2b5985cd4
TeoTwawki <arawn.annwn@yandex.com>
2017-07-13 22:45:07 -0400

    Dear crafters, here take this dragon tank.
    No, you can't use it to smite your enemies. But it does dispense beverages.

---
+++ scripts/globals/items/dragon_tank.lua
@@ -0,0 +1,25 @@
+-----------------------------------------
+-- ID: 11002
+-- Dragon Tank
+-- Dispense: Dragon Fruit au Lait
+-----------------------------------------
+
+-----------------------------------------
+-- OnItemCheck
+-----------------------------------------
+
+function onItemCheck(target)
+    local result = 0;
+    if (target:getFreeSlotsCount() == 0) then
+        result = 308;
+    end
+    return result;
+end;
+
+-----------------------------------------
+-- OnItemUse
+-----------------------------------------
+
+function onItemUse(target)
+    target:addItem(5933,1);
+end;
