af7be368cd67207228a841a12d70150f5cd70401
wrenffxi <oliviapekoe@gmail.com>
2017-08-03 12:31:51 -0400

    command input validation and standardization [b-c]

--- scripts/commands/addmission.lua
+++ scripts/commands/addmission.lua
@@ -17,11 +17,18 @@ function error(player, msg)
 end;
 
 function onTrigger(player, logId, missionId, target)
-    local logName;
 
     -- validate logId
+    local logName;
+    if (logId == nil) then
+        error(player, "You must provide a logID.");
+        return;
+    elseif (tonumber(logId) ~= nil) then
+        logId = tonumber(logId);
+        logId = MISSION_LOGS[logId];
+    end
     if (logId ~= nil) then
-        logId = tonumber(logId) or _G[string.upper(logId)];
+        logId = _G[string.upper(logId)];
     end
     if ((type(logId) == "table") and logId.mission_log ~= nil) then
         logName = logId.full_name;
@@ -31,6 +38,15 @@ function onTrigger(player, logId, missionId, target)
         return;
     end
     
+    -- validate missionId
+    if (missionId ~= nil) then
+        missionId = tonumber(missionId) or _G[string.upper(missionId)];
+    end
+    if (missionId == nil or missionId < 0) then
+        error(player, "Invalid missionID.");
+        return;
+    end
+
     -- validate target
     local targ;
     if (target == nil) then
@@ -41,14 +57,6 @@ function onTrigger(player, logId, missionId, target)
             error(player, string.format("Player named '%s' not found!", target));
             return;
         end
-    end
-
-    -- validate missionId
-    if (missionId == nil) then
-        error(player, "Invalid missionID.");
-        return;
-    else
-        missionId = tonumber(missionId) or _G[string.upper(missionId)];
     end
 
     -- add mission
--- scripts/commands/capskill.lua
+++ scripts/commands/capskill.lua
@@ -11,31 +11,24 @@ cmdprops =
     parameters = "s"
 };
 
-function onTrigger(player, skill)
-    if (skill == nil) then
-        player:PrintToPlayer( "You must enter a valid skill enum." );
+function error(player, msg)
+    player:PrintToPlayer(msg);
+    player:PrintToPlayer("@capskill <skillID>");
+end;
+
+function onTrigger(player, skillId)
+    -- validate skillId
+    if (skillId == nil) then
+        error(player, "You must provide a skillID.");
+        return;
+    end
+    skillId = tonumber(skillId) or _G[string.upper(skillId)];
+    if (skillId == nil or skillId == 0) then
+        error(player, "Invalid skillID.");
         return;
     end
 
-    skill = tonumber(skill) or _G(skill);
-
-    if (tonumber(skill) ~= 0 and tonumber(skill) ~= nil) then
-        local skillId = tonumber(skill);
-        player:capSkill( skillId );
-
-        for k, v in pairs(skillList) do
-            if (v == skillId) then
-                player:PrintToPlayer( string.format( "Capped skill '%s'.", k ) );
-                return;
-            end
-        end
-    end
-
-    local skillId = skillList[ string.lower( skill ) ];
-    if (skillId == nil) then
-        player:PrintToPlayer( string.format( "Invalid skill '%s' given.", skill ) );
-    else
-        player:capSkill( skillId );
-        player:PrintToPlayer( string.format( "Capped skill '%s'.", skill ) );
-    end
+    -- cap skill
+    player:capSkill( skillId );
+    player:PrintToPlayer( string.format( "Capped skillID %i.", skillId ) );
 end
