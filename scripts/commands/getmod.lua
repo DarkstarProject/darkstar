8fb52ed88993b31cbb05f6a466f43be4cda19543
ShelbyZ <user@shagman.codes>
2017-08-12 09:40:14 -0700

    Updating getmod to reflect recent input validation
    - validate modID
    - validate target (player or cursor)
    - updated string to provide name of target for the mod id output

--- scripts/commands/getmod.lua
+++ scripts/commands/getmod.lua
@@ -1,24 +1,31 @@
 ---------------------------------------------------------------------------------------------------
--- func: getmod
--- desc: gets a mod on the target/player
+-- func: getmod <modID>
+-- desc: gets a mod by ID on the player or cursor target
 ---------------------------------------------------------------------------------------------------
 
 cmdprops =
 {
     permission = 3,
-    parameters = "is"
+    parameters = "i"
 };
 
-function onTrigger(player, id, target)
-    local effectTarget = player;
-    if (target ~= nil) then
-        effectTarget = target;
-    end
+function error(player, msg)
+    player:PrintToPlayer(msg);
+    player:PrintToPlayer("@getmod <modID>");
+end;
 
+function onTrigger(player, id)
+    -- validate modID
     if (id == nil) then
-        player:PrintToPlayer( "Mod id cannot be nil." );
+        error(player, "Invalid modID.");
         return;
     end
 
-    player:PrintToPlayer(string.format("Mod %i: %i", id, effectTarget:getMod(id)));
-end
+    -- validate target
+    local effectTarget = player:getCursorTarget();
+    if (effectTarget == nil) then
+        effectTarget = player;
+    end
+
+    player:PrintToPlayer(string.format("%s's Mod ID %i: %i", effectTarget:getName(), id, effectTarget:getMod(id)));
+end;
