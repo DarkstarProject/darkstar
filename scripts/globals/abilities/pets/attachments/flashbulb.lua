0f0257adb6c881b8262147efe21d44fbd12caa0f
UnknownX <jaguar51896@gmail.com>
2017-08-13 15:18:32 -0500

    Added a distance check to Flashbulb

--- scripts/globals/abilities/pets/attachments/flashbulb.lua
+++ scripts/globals/abilities/pets/attachments/flashbulb.lua
@@ -22,7 +22,7 @@ end
 
 function onAttachmentCheck(pet,target)
     local master = pet:getMaster()
-    if master and master:countEffect(EFFECT_LIGHT_MANEUVER) > 0 and pet:getLocalVar("flash") < VanadielTime() then
+    if master and master:countEffect(EFFECT_LIGHT_MANEUVER) > 0 and pet:getLocalVar("flash") < VanadielTime() and (pet:checkDistance(target) - target:getModelSize()) < 7 then
         pet:setLocalVar("flash", VanadielTime() + 45)
         return 1947
     else
