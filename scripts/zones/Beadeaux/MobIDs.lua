aed93719a61d4a5dfe8f2a75d22c2256d1906e2d
Jason Stahl <jastahl@my.canyons.edu>
2017-08-22 10:26:20 -0700

    Changed ge_dha_Evileye to have ph Made PH lua Ge_dha_Evileye uses 2hr properly

---
+++ scripts/zones/Beadeaux/MobIDs.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: Beadeaux (254)
+-- Comments: -- posX, posY, posZ
+-- (Taken from 'mob_spawn_points' table)
+-----------------------------------
+
+Ge_Dha_Evileye = 17379450; -- -238 1 -202, 254
+Ge_Dha_Evileye_PH = 
+{[17379447] = '1',};  -- Old Quadav
