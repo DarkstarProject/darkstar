6acf39a0514a3c9cbb28c3a48a81569477935018
bn7 <email.steven.smith@gmail.com>
2017-08-28 21:06:49 -0600

    Create water_vi.lua

---
+++ scripts/globals/spells/water_vi.lua
@@ -0,0 +1,31 @@
+++-----------------------------------------
++-- Spell: Water VI
++-- Deals water damage to an enemy.
++-----------------------------------------
++
++require("scripts/globals/magic");
++require("scripts/globals/status");
++
++-----------------------------------------
++-- OnSpellCast
++-----------------------------------------
++
++function onMagicCastingCheck(caster, target, spell)
++    return 0;
++end;
++
++function onSpellCast(caster, target, spell)
++    local spellParams = {};
++    spellParams.hasMultipleTargetReduction = false;
++    spellParams.resistBonus = 1.0;
+     spellParams.V0 = 1350;
++    spellParams.V50 = 1100;
++    spellParams.V100 = 1590;
++    spellParams.V200 = 2100;
++    spellParams.M0 = 4.35;
++    spellParams.M50 = 4.74;
++    spellParams.M100 = 4.75;
++    spellParams.M200 = 4;
+
++    return doElementalNuke(caster, spell, target, spellParams);
++end;
