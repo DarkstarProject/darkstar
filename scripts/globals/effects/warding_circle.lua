4316a97243f4d5591b2e3ce2d19cb3ea753f188b
Whitechaser <bjosiahlance@gmail.com>
2017-09-11 12:33:16 +0800

    Corrected bonus Killer effect chance granted by 'Circle' JAs to +15%

--- scripts/globals/effects/warding_circle.lua
+++ scripts/globals/effects/warding_circle.lua
@@ -11,7 +11,7 @@ require("scripts/globals/status");
 -----------------------------------
 
 function onEffectGain(target,effect)
-   target:addMod(MOD_DEMON_KILLER,8);
+   target:addMod(MOD_DEMON_KILLER, effect:getPower());
 end;
 
 -----------------------------------
@@ -26,5 +26,5 @@ end;
 -----------------------------------
 
 function onEffectLose(target,effect)
-   target:delMod(MOD_DEMON_KILLER,8);
+   target:delMod(MOD_DEMON_KILLER, effect:getPower());
 end;
