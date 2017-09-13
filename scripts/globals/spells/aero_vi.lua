661a60cd3199658e7b8285d9d02cf6492caddd07
Ninji <ninji418@gmail.com>
2017-09-12 20:33:21 -0400

    Update aero_vi.lua

--- scripts/globals/spells/aero_vi.lua
+++ scripts/globals/spells/aero_vi.lua
@@ -18,13 +18,13 @@
 +    local spellParams = {};
 +    spellParams.hasMultipleTargetReduction = false;
 +    spellParams.resistBonus = 1.0;
-     spellParams.V0 = 1350;
-+    spellParams.V50 = 1100;
-+    spellParams.V100 = 1590;
-+    spellParams.V200 = 2100;
-+    spellParams.M0 = 4.35;
-+    spellParams.M50 = 4.74;
-+    spellParams.M100 = 4.75;
+     spellParams.V0 = 862;
++    spellParams.V50 = 1169;
++    spellParams.V100 = 1443;
++    spellParams.V200 = 1870;
++    spellParams.M0 = 6.1;
++    spellParams.M50 = 5.5;
++    spellParams.M100 = 4.85;
 +    spellParams.M200 = 4;
 
 +    return doElementalNuke(caster, spell, target, spellParams);
