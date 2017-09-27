62ba3b36f0355ce9d70d46574839f6dffb28d5ec
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2017-09-27 12:34:26 -0500

    Various Fix to allow Placeholder Respawn

--- scripts/zones/Giddeus/mobs/Yagudo_Mendicant.lua
+++ scripts/zones/Giddeus/mobs/Yagudo_Mendicant.lua
@@ -22,12 +22,15 @@ function onMobDespawn(mob)
     if (Hoo_Mjuu_the_Torrent_PH[mobID] ~= nil) then
 
         local ToD = GetServerVariable("[POP]Hoo_Mjuu_the_Torrent");
+        local checkPH = GetServerVariable("[PH]Hoo_Mjuu_the_Torrent");
         if (ToD <= os.time() and GetMobAction(Hoo_Mjuu_the_Torrent) == 0) then
             if (math.random(1,20) == 5) then
                 UpdateNMSpawnPoint(Hoo_Mjuu_the_Torrent);
                 GetMobByID(Hoo_Mjuu_the_Torrent):setRespawnTime(GetMobRespawnTime(mobID));
-                SetServerVariable("[PH]Hoo_Mjuu_the_Torrent", mobID);
-                DisallowRespawn(mobID, true);
+                if (checkPH == 0) then
+                    SetServerVariable("[PH]Hoo_Mjuu_the_Torrent", mobID);
+                    DisallowRespawn(mobID, true);
+                end
             end
         end
     end
