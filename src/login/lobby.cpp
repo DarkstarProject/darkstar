e2f5872ab106e8411b619c4056247fa94582c726
Zynjec <zynjec@gmail.com>
2017-09-28 20:20:49 -0400

    Lobby now loads number of content id's from database.

--- src/login/lobby.cpp
+++ src/login/lobby.cpp
@@ -131,7 +131,17 @@ int32 lobbydata_parse(int32 fd)
             CharList[0] = 0xE0; CharList[1] = 0x08;
             CharList[4] = 0x49; CharList[5] = 0x58; CharList[6] = 0x46; CharList[7] = 0x46; CharList[8] = 0x20;
 
-            CharList[28] = 16; // количество ячеек, доступных для создания персонажей (0-16)
+            const int8 *contentIDQuery = "SELECT content_ids FROM accounts WHERE id = %u;";
+            int32 contentIDRet = Sql_Query(SqlHandle, contentIDQuery, sd->accid);
+            if (contentIDRet == SQL_SUCCESS && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+            {
+                CharList[28] = Sql_GetUIntData(SqlHandle, 0);
+            }
+            else
+            {
+                do_close_lobbydata(sd, fd);
+                return -1;
+            }
 
             const char *pfmtQuery = "SELECT charid, charname, pos_zone, pos_prevzone, mjob,\
 												 race, face, head, body, hands, legs, feet, main, sub,\
@@ -142,9 +152,9 @@ int32 lobbydata_parse(int32 fd)
 											INNER JOIN char_look  USING(charid) \
 											INNER JOIN char_jobs  USING(charid) \
 										  WHERE accid = %i \
-										  LIMIT 16;";
+										  LIMIT %u;";
 
-            int32 ret = Sql_Query(SqlHandle, pfmtQuery, sd->accid);
+            int32 ret = Sql_Query(SqlHandle, pfmtQuery, sd->accid, CharList[28]);
             if (ret == SQL_ERROR)
             {
                 do_close_lobbydata(sd, fd);
