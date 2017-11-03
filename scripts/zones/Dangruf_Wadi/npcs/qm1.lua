a7697bef57296c2f2d03aeaf2f34d7d936bce547
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2017-11-03 05:33:46 -0500

    Implement Chocoboleech NM (#4074)
    * Implement Chocoboleech
    
    * Implement Chocoboleech NM
    
    ??? spawn points are not accurate to the exact pixel but they're pretty close from what I can do. We can at least Implement it until someone can get the exact capture(s) from retail.
    
    * Implement Chocoboleech NM

--- scripts/zones/Dangruf_Wadi/npcs/qm1.lua
+++ scripts/zones/Dangruf_Wadi/npcs/qm1.lua
@@ -14,6 +14,19 @@ require("scripts/zones/Dangruf_Wadi/TextIDs");
 -----------------------------------
 
 function onTrade(player,npc,trade)
+
+    local x = npc:getXPos();
+    local y = npc:getYPos();
+    local z = npc:getZPos();
+    local mob = GetMobByID(17559869);
+
+    -- Trade Fresh Blood
+    if (GetMobAction(17559869) == 0 and trade:hasItemQty(1898,1) and trade:getItemCount() == 1) then
+        player:tradeComplete();
+        SpawnMob(17559869):updateClaim(player); -- Chocoboleech
+        mob:setPos(x+1,y,z);
+        npc:setStatus(STATUS_DISAPPEAR);
+    end
 end;
 
 -----------------------------------
