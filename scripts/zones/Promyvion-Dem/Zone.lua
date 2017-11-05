7d31603bb7656c74d5358c273d16b4991bc522aa
wrenffxi <oliviapekoe@gmail.com>
2017-11-04 22:27:54 -0400

    Tabled Promyvion receptacles and memory streams Receptacles now spawn strays correctly, even when not engaged Implemented random portal logic

--- scripts/zones/Promyvion-Dem/Zone.lua
+++ scripts/zones/Promyvion-Dem/Zone.lua
@@ -4,52 +4,26 @@
 --
 -----------------------------------
 package.loaded["scripts/zones/Promyvion-Dem/TextIDs"] = nil;
+package.loaded["scripts/zones/Promyvion-Dem/MobIDs"] = nil;
 -----------------------------------
 require("scripts/zones/Promyvion-Dem/TextIDs");
-require("scripts/globals/settings");
-require("scripts/globals/missions");
+require("scripts/zones/Promyvion-Dem/MobIDs");
 require("scripts/globals/keyitems");
+require("scripts/globals/missions");
+require("scripts/globals/settings");
 require("scripts/globals/status");
 
------------------------------------
--- onInitialize
------------------------------------
-
 function onInitialize(zone)
-
-    --
-    zone:registerRegion(11,157,-4,-82,161,4,-77);       -- Level 1  Return
-    zone:registerRegion(12,117,-4,-283,122,4,-277);     -- Level 1   MR
-    --
-    zone:registerRegion(21,-383,-4,-2,-278,4,2);        -- Level Two Return
-    zone:registerRegion(22,-82,-4,76,-77,4,80);         -- Level Two MR1
-    zone:registerRegion(23,-361,-4,36,-356,4,42);       -- Level Two MR2
-    zone:registerRegion(24,-83,-4,-83,-77,4,-76);       -- Level Two MR3
-    zone:registerRegion(25,-282,-4,-202,-277,4,-196);   -- Level Two MR4
-    --
-    zone:registerRegion(31,-160,-4,437,-157,4,441);     -- Level Three West Return
-    zone:registerRegion(32,-42,-4,317,-37,4,322);       -- Level Three West MR1
-    zone:registerRegion(33,-322,-4,156,-316,4,162);     -- Level Three West MR2
-    zone:registerRegion(34,-122,-4,157,-118,4,163);     -- Level Three West MR3
-    --
-    zone:registerRegion(35,-2,-4,-322,2,4,-317);        -- Level Three East Return
-    zone:registerRegion(36,37,-4,-203,43,4,-198);       -- Level Three East MR1
-    zone:registerRegion(37,-122,-4,-242,-116,4,-237);   -- Level Three East MR2
-    zone:registerRegion(38,-122,-4,-402,-116,4,-396);   -- Level Three East MR3
-    --
-    zone:registerRegion(41,357,-4,237,361,4,242);       -- Level Four Return
-
+    for k, v in pairs(MEMORY_STREAMS) do
+        zone:registerRegion(k,v[1],v[2],v[3],v[4],v[5],v[6]);
+    end
 end;
-
------------------------------------
--- onZoneIn
------------------------------------
 
 function onZoneIn(player,prevZone)
     local cs = -1;
 
     if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
-        player:setPos(185.891, 0, -52.331, 128, 18); -- {R}
+        player:setPos(185.891, 0, -52.331, 128);
     end
 
     if (player:getCurrentMission(COP) == BELOW_THE_ARKS and player:getVar("PromathiaStatus") == 2) then
@@ -60,26 +34,22 @@ function onZoneIn(player,prevZone)
         if (player:hasKeyItem(LIGHT_OF_HOLLA) and player:hasKeyItem(LIGHT_OF_MEA)) then
             if (player:getVar("cslastpromy") == 1) then
                 player:setVar("cslastpromy",0)
-                cs = 0x0034;
+                cs = 52;
             end
         elseif (player:hasKeyItem(LIGHT_OF_HOLLA) or player:hasKeyItem(LIGHT_OF_MEA)) then
             if (player:getVar("cs2ndpromy") == 1) then
                 player:setVar("cs2ndpromy",0)
-                cs = 0x0033;
+                cs = 51;
             end
         end
     end
 
     if (player:getVar("FirstPromyvionDem") == 1) then
-        cs = 0x0032;
+        cs = 50;
     end
 
     return cs;
 end;
-
------------------------------------
--- afterZoneIn
------------------------------------
 
 function afterZoneIn(player)
     if (ENABLE_COP_ZONE_CAP == 1) then -- ZONE WIDE LEVEL RESTRICTION
@@ -87,141 +57,27 @@ function afterZoneIn(player)
     end
 end;
 
------------------------------------
--- onRegionEnter
------------------------------------
-
 function onRegionEnter(player,region)
-    -- regionID =region:GetRegionID();
-    -- printf("regionID: %u",regionID);
-    if (player:getAnimation()==0) then
-        switch (region:GetRegionID()): caseof
-        {
-            [11] = function (x)
-                player:startEvent(0x002E);
-            end,
-            [12] = function (x)
-                if (GetNPCByID(16851268):getAnimation() == 8) then
-                    player:startEvent(30);
-                end
-            end,
-            [21] = function (x)
-                player:startEvent(41);
-            end,
-            [22] = function (x)
-                if (GetNPCByID(16851273):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(35);
-                    else
-                        player:startEvent(34);
-                    end
-                end
-            end,
-            [23] = function (x)
-                if (GetNPCByID(16851275):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(35);
-                    else
-                        player:startEvent(34);
-                    end
-                end
-            end,
-            [24] = function (x)
-                if (GetNPCByID(16851272):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(35);
-                    else
-                        player:startEvent(34);
-                    end
-                end
-            end,
-            [25] = function (x)
-                if (GetNPCByID(16851274):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(35);
-                    else
-                        player:startEvent(34);
-                    end
-                end
-            end,
-            [31] = function (x)
-                player:startEvent(30);
-            end,
-            [32] = function (x)
-                if (GetNPCByID(16851277):getAnimation() == 8) then
-                    player:startEvent(32);
-                end
-            end,
-            [33] = function (x)
-                if (GetNPCByID(16851276):getAnimation() == 8) then
-                    player:startEvent(32);
-                end
-            end,
-            [34] = function (x)
-                if (GetNPCByID(16851278):getAnimation() == 8) then
-                    player:startEvent(32);
-                end
-            end,
-            [35] = function (x)
-                player:startEvent(30);
-            end,
-            [36] = function (x)
-                if (GetNPCByID(16851269):getAnimation() == 8) then
-                    player:startEvent(32);
-                end
-            end,
-            [37] = function (x)
-                if (GetNPCByID(16851270):getAnimation() == 8) then
-                    player:startEvent(32);
-                end
-            end,
-            [38] = function (x)
-                if (GetNPCByID(16851271):getAnimation() == 8) then
-                    player:startEvent(32);
-                end
-            end,
-            [41] = function (x)
-                if (player:getVar("MemoryReceptacle") == 2) then
-                    player:startEvent(31);
-                else
-                    player:startEvent(34);
-                end
-            end,
-        }
+    if (player:getAnimation() == 0) then
+        local regionId = region:GetRegionID();
+        local events = MEMORY_STREAMS[regionId][7];
+        local event = events[math.random(#events)];
+        if (regionId < 100 or GetNPCByID(regionId):getAnimation() == ANIMATION_OPEN_DOOR) then
+            player:startEvent(event);
+        end
     end
 end;
-
------------------------------------
--- onRegionLeave
------------------------------------
 
 function onRegionLeave(player,region)
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
-
-    if (csid == 0x002e and option == 1) then
+    if (csid == 46 and option == 1) then
         player:setPos(-226.193, -46.459, -280.046, 127, 14); -- To Hall of Transference {R}
-    elseif (csid == 0x0032) then
+    elseif (csid == 50) then
         player:setVar("FirstPromyvionDem",0);
-    end
-
-    if (option==1) then
-        player:setVar("MemoryReceptacle",0);
     end
 end;
--- scripts/zones/Promyvion-Mea/Zone.lua
+++ scripts/zones/Promyvion-Mea/Zone.lua
@@ -3,51 +3,21 @@
 -- Zone: Promyvion-Mea (20)
 --
 -----------------------------------
- package.loaded["scripts/zones/Promyvion-Mea/TextIDs"] = nil;
+package.loaded["scripts/zones/Promyvion-Mea/TextIDs"] = nil;
+package.loaded["scripts/zones/Promyvion-Mea/MobIDs"] = nil;
 ----------------------------------
 require("scripts/zones/Promyvion-Mea/TextIDs");
-require("scripts/globals/settings");
-require("scripts/globals/missions");
+require("scripts/zones/Promyvion-Mea/MobIDs");
 require("scripts/globals/keyitems");
+require("scripts/globals/missions");
+require("scripts/globals/settings");
 require("scripts/globals/status");
 
------------------------------------
--- onInitialize
------------------------------------
-
 function onInitialize(zone)
-
-    --                      -96,0,174,155              -- Level One Start
-    zone:registerRegion(11,-122,-4,197,-117,4,202);    -- Level One Return
-    zone:registerRegion(12,-283,-4,237,-276,4,242);    -- Level One MR
-
-    --                      -70,0,-233,105             -- Level Two Start
-    zone:registerRegion(21,-1,-4,-121,2,4,-118);       -- Level Two Return
-    zone:registerRegion(22,-82,-4,-42,-78,4,-38);      -- Level Two MR1
-    zone:registerRegion(23,-322,-4,-361,-318,4,-357);  -- Level Two MR2
-    zone:registerRegion(24,-42,-4,-321,-37,4,-317);    -- Level Two MR3
-    zone:registerRegion(25,77,-4,-241,81,4,-238);      -- Level Two MR4
-
-    --                      -167,0,172,38              -- Level Three West Start
-    zone:registerRegion(31,-161,-4,158,-157,4,161);    -- Level Three West Return
-    zone:registerRegion(32,-42,-4,-2,-38,4,2);         -- Level Three West MR1  no receptacle
-    zone:registerRegion(33,-241,-4,-42,-238,4,-37);    -- Level Three West MR2
-    zone:registerRegion(34,-321,-4,-42,-318,4,-38);    -- Level Three West MR3
-
-    --                      68,0,-76,254               -- Level THREE East Start
-    zone:registerRegion(35,158,-4,-281,161,4,-278);    -- Level Three East Return
-    zone:registerRegion(36,240,-4,-322,244,4,-317);    -- Level Three East MR1 OKK
-    zone:registerRegion(37,198,-4,-2,201,4,2);         -- Level Three East MR2 no receptacle
-    zone:registerRegion(38,358,-4,-41,362,4,-38);      -- Level Three East MR3
-
-    --                      -106,0,384,38              -- Level FOUR Start
-    zone:registerRegion(41,-82,-4,358,-78,4,361);      -- Level Four Return
-
+    for k, v in pairs(MEMORY_STREAMS) do
+        zone:registerRegion(k,v[1],v[2],v[3],v[4],v[5],v[6]);
+    end
 end;
-
- -----------------------------------
- -- onZoneIn
- -----------------------------------
 
 function onZoneIn(player,prevZone)
     local cs = -1;
@@ -64,26 +34,22 @@ function onZoneIn(player,prevZone)
         if (player:hasKeyItem(LIGHT_OF_HOLLA) and player:hasKeyItem(LIGHT_OF_DEM)) then
             if (player:getVar("cslastpromy") == 1) then
                 player:setVar("cslastpromy",0)
-                cs = 0x0034;
+                cs = 52;
             end
         elseif (player:hasKeyItem(LIGHT_OF_HOLLA) or player:hasKeyItem(LIGHT_OF_DEM)) then
             if (player:getVar("cs2ndpromy") == 1) then
                 player:setVar("cs2ndpromy",0)
-                cs = 0x0033;
+                cs = 51;
             end
         end
     end
 
     if (player:getVar("FirstPromyvionMea") == 1) then
-        cs = 0x0032;
+        cs = 50;
     end
 
      return cs;
 end;
-
------------------------------------
--- afterZoneIn
------------------------------------
 
 function afterZoneIn(player)
     if (ENABLE_COP_ZONE_CAP == 1) then -- ZONE WIDE LEVEL RESTRICTION
@@ -91,138 +57,27 @@ function afterZoneIn(player)
     end
 end;
 
------------------------------------
--- onRegionEnter
------------------------------------
-
 function onRegionEnter(player,region)
     if (player:getAnimation() == 0) then
-        switch (region:GetRegionID()): caseof
-        {
-            [11] = function (x)
-                player:startEvent(0x002e);
-            end,
-            [12] = function (x)
-                if (GetNPCByID(16859453):getAnimation() == 8) then
-                    player:startEvent(30);
-                end
-            end, -- player:setPos(-70,0,-233,105);
-            [21] = function (x)
-                player:startEvent(41); -- player:setPos(-96,0,174,155);
-            end,
-            [22] = function (x)
-                if (GetNPCByID(16859456):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(37); -- player:setPos(-167,0,172,38);
-                    else
-                        player:startEvent(33); -- player:setPos(68,0,-76,254);
-                    end
-                end
-            end,
-            [23] = function (x)
-                if (GetNPCByID(16859460):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(37); -- player:setPos(-167,0,172,38);
-                    else
-                        player:startEvent(33); -- player:setPos(68,0,-76,254);
-                    end
-                end
-            end,
-            [24] = function (x)
-                if (GetNPCByID(16859461):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(37); -- player:setPos(-167,0,172,38);
-                    else
-                        player:startEvent(33); -- player:setPos(68,0,-76,254);
-                    end
-                end
-            end,
-            [25] = function (x)
-                if (GetNPCByID(16859462):getAnimation() == 8) then
-                    if (player:getVar("MemoryReceptacle") == 2) then
-                        player:startEvent(37); -- player:setPos(-167,0,172,38);
-                    else
-                        player:startEvent(33); -- player:setPos(68,0,-76,254);
-                    end
-                end
-            end,
-            [31] = function (x)
-                player:startEvent(30); -- player:setPos(-70,0,-233,105);
-            end,
-            [32] = function (x)
-                player:startEvent(31); -- player:setPos(-106,0,384,38);
-            end,
-            [33] = function (x)
-                if (GetNPCByID(16859455):getAnimation() == 8) then
-                    player:startEvent(31); -- player:setPos(-106,0,384,38);
-                end
-            end,
-            [34] = function (x)
-                if (GetNPCByID(16859454):getAnimation() == 8) then
-                    player:startEvent(31); -- player:setPos(-106,0,384,38);
-                end
-            end,
-            [35] = function (x)
-                player:startEvent(30); -- player:setPos(-70,0,-233,105);
-            end,
-            [36] = function (x)
-                if (GetNPCByID(16859463):getAnimation() == 8) then
-                    player:startEvent(31); -- player:setPos(-106,0,384,38);
-                end
-            end,
-            [37] = function (x)
-                if (GetNPCByID(16859458):getAnimation() == 8) then
-                    player:startEvent(31); -- player:setPos(-106,0,384,38);
-                end
-            end,
-            [38] = function (x)
-                if (GetNPCByID(16859459):getAnimation() == 8) then
-                    player:startEvent(31); -- player:setPos(-106,0,384,38);
-                end
-            end,
-            [41] = function (x)
-                if (player:getVar("MemoryReceptacle") == 2) then
-                    player:startEvent(37); -- player:setPos(-167,0,172,38);
-                else
-                    player:startEvent(33); -- player:setPos(68,0,-76,254);
-                end
-            end,
-        }
+        local regionId = region:GetRegionID();
+        local events = MEMORY_STREAMS[regionId][7];
+        local event = events[math.random(#events)];
+        if (regionId < 100 or GetNPCByID(regionId):getAnimation() == ANIMATION_OPEN_DOOR) then
+            player:startEvent(event);
+        end
     end
 end;
-
------------------------------------
--- onRegionLeave
------------------------------------
 
 function onRegionLeave(player,region)
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,region)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
-
-    if (csid == 0x002e and option == 1) then
+    if (csid == 46 and option == 1) then
         player:setPos(279.988, -86.459, -25.994, 63, 14); -- To Hall of Transferance {R}
-    elseif (csid == 0x0032) then
+    elseif (csid == 50) then
         player:setVar("FirstPromyvionMea",0);
     end
-
-    if (option == 1) then
-        player:setVar("MemoryReceptacle",0);
-    end
-
- end;
+end;
