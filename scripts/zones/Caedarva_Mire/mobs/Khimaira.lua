e124c3920876ceea41294cf2150a07a6cb438566
wrenffxi <oliviapekoe@gmail.com>
2017-11-21 14:41:36 -0500

    Khimaira SPAWNTYPE scripted, with hourly intervals set on maintenance and despawn. http://ffxiclopedia.wikia.com/wiki/Khimaira Fixed case of Caedarva Toad to match QM9

--- scripts/zones/Caedarva_Mire/mobs/Khimaira.lua
+++ scripts/zones/Caedarva_Mire/mobs/Khimaira.lua
@@ -2,20 +2,12 @@
 -- Area: Caedarva Mire
 --  MOB: Khimaira
 -----------------------------------
-
 require("scripts/globals/titles");
-
------------------------------------
--- onMobSpawn Action
------------------------------------
-
-function onMobSpawn(mob)
-end;
-
------------------------------------
--- onMobDeath
------------------------------------
 
 function onMobDeath(mob, player, isKiller)
     player:addTitle(KHIMAIRA_CARVER);
+end;
+
+function onMobDespawn(mob)
+    mob:setRespawnTime(math.random(48,72) * 3600); -- 48 to 72 hours, in 1-hour increments
 end;
