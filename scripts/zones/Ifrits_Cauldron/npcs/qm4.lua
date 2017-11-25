208c9469650a04e04545c74ef7755c3c8ccbfd96
ymkc <sixth_brother@gmx.net>
2017-11-25 23:32:56 +0000

    Adjust Bastok Mission 6-2 fight

--- scripts/zones/Ifrits_Cauldron/npcs/qm4.lua
+++ scripts/zones/Ifrits_Cauldron/npcs/qm4.lua
@@ -10,42 +10,25 @@ package.loaded["scripts/zones/Ifrits_Cauldron/TextIDs"] = nil;
 require("scripts/globals/missions");
 require("scripts/zones/Ifrits_Cauldron/TextIDs");
 
------------------------------------
--- onTrade Action
------------------------------------
-
 function onTrade(player,npc,trade)
-
     if (player:getCurrentMission(BASTOK) == THE_PIRATE_S_COVE and player:getVar("MissionStatus") == 2) then
-        if (GetMobAction(17616897) == 0 and GetMobAction(17616898) == 0 and trade:hasItemQty(646,1) and trade:getItemCount() == 1) then
+        if (not GetMobByID(17616897):isSpawned() and not GetMobByID(17616898):isSpawned() and trade:hasItemQty(646,1) and trade:getItemCount() == 1) then
             player:tradeComplete();
-            SpawnMob(17616897):updateClaim(player);
+            SpawnMob(17616897):lookAt(player:getPos());
             SpawnMob(17616898):updateClaim(player);
             npc:setStatus(STATUS_DISAPPEAR);
         end
     end
 end;
 
------------------------------------
--- onTrigger Action
------------------------------------
-
 function onTrigger(player,npc)
     player:messageSpecial(NOTHING_OUT_OF_ORDINARY);
 end;
-
------------------------------------
--- onEventUpdate
------------------------------------
 
 function onEventUpdate(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
 end;
-
------------------------------------
--- onEventFinish
------------------------------------
 
 function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
