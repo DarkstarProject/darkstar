cc7ff2927cfe597a4004e93f11eb6a4c11b7aa08
wrenffxi <oliviapekoe@gmail.com>
2017-11-27 10:50:19 -0500

    Huwasi, Lii_Jixa_the_Somnolist, Mucoid_Mass, Patripatan, Saa_Doyi_the_Fervid, Teporingo, Thoon

--- scripts/zones/Meriphataud_Mountains/mobs/Coeurl.lua
+++ scripts/zones/Meriphataud_Mountains/mobs/Coeurl.lua
@@ -1,14 +1,16 @@
 -----------------------------------
 -- Area: Meriphataud Mountains
 --  MOB: Coeurl
+-- Note: PH for Patripatan
 -----------------------------------
-
+require("scripts/zones/Meriphataud_Mountains/MobIDs");
 require("scripts/globals/fieldsofvalor");
-
------------------------------------
--- onMobDeath
------------------------------------
+require("scripts/globals/mobs");
 
 function onMobDeath(mob, player, isKiller)
     checkRegime(player,mob,63,1);
+end;
+
+function onMobDespawn(mob)
+    phOnDespawn(mob,PATRIPATAN_PH,5,math.random(3600,10800)); -- 1 to 3 hours
 end;
