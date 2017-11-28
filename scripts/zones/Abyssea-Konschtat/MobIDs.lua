b350837557f73991789c7b8fa66456b6138ecaa4
Scott <ksgilvin100@roadrunner.com>
2017-11-28 10:06:01 -0500

    Implement Abyssea-Konschtat/MobIDs.lua

---
+++ scripts/zones/Abyssea-Konschtat/MobIDs.lua
@@ -0,0 +1,21 @@
+require("scripts/globals/keyitems");
+
+-- [QM] = {'name', {list of required trades}, {list of required KIs}, nmID}
+NM_POPS = 
+{
+    [16839078] = { 'qm1', {2914},                                                                                                        {}, 16838718}, -- Ashtaerth The Gallvexed
+    [16839079] = { 'qm2', {2911},                                                                                                        {}, 16838767}, -- Sarcophilus
+    [16839080] = { 'qm3', {2909},                                                                                                        {}, 16838782}, -- Bombadeel
+    [16839081] = { 'qm4', {2908},                                                                                                        {}, 16838837}, -- Hexenpilz
+    [16839082] = { 'qm5', {2910},                                                                                                        {}, 16838871}, -- Keratyrannos
+    [16839083] = { 'qm6', {2904},                                                                                                        {}, 16838885}, -- Lentor
+    [16839084] = { 'qm7', {2903},                                                                                                        {}, 16838899}, -- Bloodguzzler
+    [16839085] = { 'qm8', {2907},                                                                                                        {}, 16838946}, -- Clingy Clare
+    [16839086] = { 'qm9', {2906},                                                                                                        {}, 16838962}, -- Siranpa-Kamuy
+    [16839087] = {'qm10', {2912},                                                                                                        {}, 16838979}, -- Alkonost
+	[16839088] = {'qm11', {2913},                                                                                                        {}, 16838993}, -- Arimaspi
+    [16839089] = {'qm12', {2905},                                                                                                        {}, 16839033}, -- Fear Gorta
+    [16839090] = {'qm13',     {}, {FRAGRANT_TREANT_PETAL,FETID_RAFFLESIA_STALK,DECAYING_MORBOL_TOOTH,TURBID_SLIME_OIL,VENOMOUS_PEISTE_CLAW}, 16839007}, -- Eccentric Eve
+    [16839091] = {'qm14',     {},                                       {TATTERED_HIPPOGRAPH_WING,CRACKED_WIVRE_HORN,MUCID_AHRIMAN_EYEBALL}, 16838872}, -- Kukulkan
+    [16839095] = {'qm18',     {},                                                                                  {TWISTED_TONBERRY_CROWN}, 16839068}, -- Bloodeye Vileberry
+}
