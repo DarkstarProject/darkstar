a1ea27344f0d56b3973a07fdc2e974edaccdbf2a
Scott <ksgilvin100@roadrunner.com>
2017-11-30 21:19:27 -0500

    Implement Abyssea-Grauberg/MobIDs.lua

--- scripts/zones/Abyssea-Grauberg/npcs/qm12.lua
+++ scripts/zones/Abyssea-Grauberg/npcs/qm12.lua
@@ -2,25 +2,24 @@
 -- Zone: Abyssea-Grauberg
 --  NPC: qm12 (???)
 -- Spawns Alfard
--- !pos ? ? ? 254
+-- !pos 309 -32 173 254
 -----------------------------------
-require("scripts/globals/keyitems");
-require("scripts/globals/status");
+require("scripts/globals/abyssea");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+    abysseaOnTrade(player,npc,trade);
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
---[[
-    if (GetMobAction(17818060) == ACTION_NONE) then -- NM not already spawned from this
-        if (player:hasKeyItem(VENOMOUS_HYDRA_FANG)) then
-            player:startEvent(1020, VENOMOUS_HYDRA_FANG); -- Ask if player wants to use KIs
-        else
-            player:startEvent(1021, VENOMOUS_HYDRA_FANG); -- Do not ask, because player is missing at least 1.
-        end
-    end
-]]
+    abysseaOnTrigger(player,npc);
 end;
 
 -----------------------------------
@@ -28,8 +27,7 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID2: %u",csid);
-    -- printf("RESULT2: %u",option);
+    abysseaOnEventUpdate(player,csid,option);
 end;
 
 -----------------------------------
@@ -37,10 +35,5 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
-    if (csid == 1020 and option == 1) then
-        SpawnMob(17818060):updateClaim(player); -- Spawn NM, Despawn after inactive for 5 minutes (pt has to reclaim within 5 of a wipe)
-        player:delKeyItem(VENOMOUS_HYDRA_FANG);
-    end
+    abysseaOnEventFinish(player,csid,option);
 end;
