440d1fe58622e24c5cd2074a933d589784890cf9
dragonarmor <ksgilvin100@roadrunner.com>
2017-11-30 22:24:06 -0500

    Key Item Typo

--- scripts/zones/Abyssea-Grauberg/MobIDs.lua
+++ scripts/zones/Abyssea-Grauberg/MobIDs.lua
@@ -16,14 +16,14 @@ NM_POPS =
 --  [17818090] = {'qm10',           {3273},                                                {}, 17818050}, -- Burstrox Powderpate
 --  [17818091] = {'qm11',               {}, {WARPED_CHARIOT_PLATE,SHATTERED_IRON_GIANT_CHAIN}, 17818051}, -- Raja
 --  [17818092] = {'qm12',               {},                             {VENOMOUS_HYDRA_FANG}, 17818052}, -- Alfard
---  [17818093] = {'qm13',               {},                                   {VACANT_BUGARD}, 17818053}, -- Azdaja
+--  [17818093] = {'qm13',               {},                               {VACANT_BUGARD_EYE}, 17818053}, -- Azdaja
 --  [17818094] = {'qm14',               {},                       {VARIEGATED_URAGNITE_SHELL}, 17818054}, -- Amphitrite
 --  [17818095] = {'qm15',               {}, {WARPED_CHARIOT_PLATE,SHATTERED_IRON_GIANT_CHAIN}, 17818055}, -- Raja
 --  [17818096] = {'qm16',               {},                             {VENOMOUS_HYDRA_FANG}, 17818056}, -- Alfard
---  [17818097] = {'qm17',               {},                                   {VACANT_BUGARD}, 17818057}, -- Azdaja
+--  [17818097] = {'qm17',               {},                               {VACANT_BUGARD_EYE}, 17818057}, -- Azdaja
 --  [17818098] = {'qm18',               {},                       {VARIEGATED_URAGNITE_SHELL}, 17818058}, -- Amphitrite
 --  [17818099] = {'qm19',               {}, {WARPED_CHARIOT_PLATE,SHATTERED_IRON_GIANT_CHAIN}, 17818059}, -- Raja
 --  [17818100] = {'qm20',               {},                             {VENOMOUS_HYDRA_FANG}, 17818060}, -- Alfard
---  [17818101] = {'qm21',               {},                                   {VACANT_BUGARD}, 17818061}, -- Azdaja
+--  [17818101] = {'qm21',               {},                               {VACANT_BUGARD_EYE}, 17818061}, -- Azdaja
 --  [17818102] = {'qm22',               {},                       {VARIEGATED_URAGNITE_SHELL}, 17818062}, -- Amphitrite
 }
