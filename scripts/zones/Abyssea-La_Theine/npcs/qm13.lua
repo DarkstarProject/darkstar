06e8961b8e17d6f93f0cf0ea0230c6840391324b
Scott <ksgilvin100@roadrunner.com>
2017-12-02 00:14:55 -0500

    Implemenet Abyssea-La_Theine/MobIDs.lua

--- scripts/zones/Abyssea-La_Theine/npcs/qm13.lua
+++ scripts/zones/Abyssea-La_Theine/npcs/qm13.lua
@@ -2,26 +2,24 @@
 -- Zone: Abyssea-LaTheine
 --  NPC: qm13 (???)
 -- Spawns Briareus
--- !pos ? ? ? 132
+-- !pos -179 7 259 132
 -----------------------------------
-require("scripts/globals/keyitems");
-require("scripts/globals/status");
+require("scripts/globals/abyssea");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+    abysseaOnTrade(player,npc,trade);
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
---[[
-    if (GetMobAction(17318456) == ACTION_NONE) then -- NM not already spawned from this
-        if (player:hasKeyItem(DENTED_GIGAS_SHIELD) and player:hasKeyItem(WARPED_GIGAS_ARMBAND)
-        and player:hasKeyItem(SEVERED_GIGAS_COLLAR)) then -- I broke it into 3 lines at the 'and' because it was so long.
-            player:startEvent(1020, DENTED_GIGAS_SHIELD, WARPED_GIGAS_ARMBAND, SEVERED_GIGAS_COLLAR); -- Ask if player wants to use KIs
-        else
-            player:startEvent(1021, DENTED_GIGAS_SHIELD, WARPED_GIGAS_ARMBAND, SEVERED_GIGAS_COLLAR); -- Do not ask, because player is missing at least 1.
-        end
-    end
-]]
+    abysseaOnTrigger(player,npc);
 end;
 
 -----------------------------------
@@ -29,8 +27,7 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
+    abysseaOnEventUpdate(player,csid,option);
 end;
 
 -----------------------------------
@@ -38,12 +35,5 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
-    if (csid == 1020 and option == 1) then
-        SpawnMob(17318456):updateClaim(player); -- Spawn NM, Despawn after inactive for 5 minutes (pt has to reclaim within 5 of a wipe)
-        player:delKeyItem(DENTED_GIGAS_SHIELD);
-        player:delKeyItem(WARPED_GIGAS_ARMBAND);
-        player:delKeyItem(SEVERED_GIGAS_COLLAR);
-    end
+    abysseaOnEventFinish(player,csid,option);
 end;
