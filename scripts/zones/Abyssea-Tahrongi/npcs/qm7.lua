24b4f50b15112b468f3b44105e63c7539fe518fd
dragonarmor <ksgilvin100@roadrunner.com>
2017-12-05 15:04:57 -0500

    Implement Abyssea-Tahrongi/MobIDs.lua (#4333)
    * Implement Abyssea-Tahrongi/MobIDs.lua

--- scripts/zones/Abyssea-Tahrongi/npcs/qm7.lua
+++ scripts/zones/Abyssea-Tahrongi/npcs/qm7.lua
@@ -2,23 +2,16 @@
 -- Zone: Abyssea-Tahrongi
 --  NPC: qm7 (???)
 -- Spawns Hedetet
--- !pos ? ? ? 45
+-- !pos -279 7 126 45
 -----------------------------------
-require("scripts/globals/status");
+require("scripts/globals/abyssea");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
---[[
-    if (trade:hasItemQty(2921,1) and trade:hasItemQty(2948,1) and trade:getItemCount() == 2) then -- Player has all the required items.
-        if (GetMobAction(16961923) == ACTION_NONE) then -- Mob not already spawned from this
-            SpawnMob(16961923):updateClaim(player); -- Spawn NM, Despawn after inactive for 5 minutes (pt has to reclaim within 5 of a wipe)
-            player:tradeComplete();
-        end
-    end
-]]
+    abysseaOnTrade(player,npc,trade);
 end;
 
 -----------------------------------
@@ -26,7 +19,7 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-    player:startEvent(1010, 2921, 2948); -- Inform payer what items they need.
+    abysseaOnTrigger(player,npc);
 end;
 
 -----------------------------------
@@ -34,8 +27,6 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -43,6 +34,4 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
