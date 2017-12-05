e8965ee89965e2818299ebcee2f183b844b327a1
dragonarmor <ksgilvin100@roadrunner.com>
2017-12-05 13:54:34 -0500

    Update qm13 Pos Data

--- scripts/zones/Abyssea-Misareaux/npcs/qm13.lua
+++ scripts/zones/Abyssea-Misareaux/npcs/qm13.lua
@@ -2,7 +2,7 @@
 -- Zone: Abyssea-Misareaux
 --  NPC: qm13 (???)
 -- Spawns Cirein-Croin
--- !pos 346 15 -437 216
+-- !pos 39.146 -15.500 519.988 216
 -----------------------------------
 require("scripts/globals/abyssea");
 
