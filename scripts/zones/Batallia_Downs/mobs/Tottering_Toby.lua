3b9edbad30ea76eaa9e0a90ad01fe2388dddf9b9
wrenffxi <oliviapekoe@gmail.com>
2017-12-06 01:17:46 -0500

    Lottery NMs Tottering Toby and Blubbery Bulge

--- scripts/zones/Batallia_Downs/mobs/Tottering_Toby.lua
+++ scripts/zones/Batallia_Downs/mobs/Tottering_Toby.lua
@@ -7,13 +7,4 @@ function onMobDeath(mob, player, isKiller)
 end;
 
 function onMobDespawn(mob)
-    -- Set Tottering_Toby's Window Open Time
-    SetServerVariable("[POP]Tottering_Toby", os.time() + math.random(3600,10800)); -- 1-6 hours
-    DisallowRespawn(mob:getID(), true);
-
-    -- Set PH back to normal, then set to respawn spawn
-    local PH = GetServerVariable("[PH]Tottering_Toby");
-    SetServerVariable("[PH]Tottering_Toby", 0);
-    DisallowRespawn(PH, false);
-    GetMobByID(PH):setRespawnTime(GetMobRespawnTime(PH));
 end;
