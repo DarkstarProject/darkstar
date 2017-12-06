3c845b609a2ffaa1c478942a7d81867c740e649d
Scott <ksgilvin100@roadrunner.com>
2017-12-05 23:24:54 -0500

    Implement Abyssea-Uleguerand/MobIDs.lua

--- scripts/zones/Abyssea-Uleguerand/npcs/qm15.lua
+++ scripts/zones/Abyssea-Uleguerand/npcs/qm15.lua
@@ -2,25 +2,24 @@
 -- Zone: Abyssea-Uleguerand
 --  NPC: qm15 (???)
 -- Spawns Pantokrator
--- !pos ? ? ? 253
+-- !pos -198 -175 140 253
 -----------------------------------
-require("scripts/globals/keyitems");
-require("scripts/globals/status");
+require("scripts/globals/abyssea");
+
+-----------------------------------
+-- onTrade Action
+-----------------------------------
+
+function onTrade(player,npc,trade)
+    abysseaOnTrade(player,npc,trade);
+end;
 
 -----------------------------------
 -- onTrigger Action
 -----------------------------------
 
 function onTrigger(player,npc)
---[[
-    if (GetMobAction(17813918) == ACTION_NONE) then -- NM not already spawned from this
-        if (player:hasKeyItem(WARPED_IRON_GIANT_NAIL) and player:hasKeyItem(DENTED_CHARIOT_SHIELD)) then
-            player:startEvent(1020, WARPED_IRON_GIANT_NAIL, DENTED_CHARIOT_SHIELD); -- Ask if player wants to use KIs
-        else
-            player:startEvent(1025, WARPED_IRON_GIANT_NAIL, DENTED_CHARIOT_SHIELD); -- Do not ask, because player is missing at least 1.
-        end
-    end
-]]
+    abysseaOnTrigger(player,npc);
 end;
 
 -----------------------------------
@@ -28,8 +27,7 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID2: %u",csid);
-    -- printf("RESULT2: %u",option);
+    abysseaOnEventUpdate(player,csid,option);
 end;
 
 -----------------------------------
@@ -37,11 +35,5 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
-    if (csid == 1020 and option == 1) then
-        SpawnMob(17813918):updateClaim(player); -- Spawn NM, Despawn after inactive for 5 minutes (pt has to reclaim within 5 of a wipe)
-        player:delKeyItem(WARPED_IRON_GIANT_NAIL);
-        player:delKeyItem(DENTED_CHARIOT_SHIELD);
-    end
+    abysseaOnEventFinish(player,csid,option);
 end;
--- scripts/zones/Abyssea-Uleguerand/npcs/qm9.lua
+++ scripts/zones/Abyssea-Uleguerand/npcs/qm9.lua
@@ -2,23 +2,16 @@
 -- Zone: Abyssea-Uleguerand
 --  NPC: qm9 (???)
 -- Spawns Chillwing Hwitti
--- !pos ? ? ? 253
+-- !pos -484 -20 -85 253
 -----------------------------------
-require("scripts/globals/status");
+require("scripts/globals/abyssea");
 
 -----------------------------------
 -- onTrade Action
 -----------------------------------
 
 function onTrade(player,npc,trade)
---[[
-    if (trade:hasItemQty(3256,1) and trade:getItemCount() == 1) then -- Player has all the required items.
-        if (GetMobAction(17813939) == ACTION_NONE) then -- Mob not already spawned from this
-            SpawnMob(17813939):updateClaim(player); -- Spawn NM, Despawn after inactive for 5 minutes (pt has to reclaim within 5 of a wipe)
-            player:tradeComplete();
-        end
-    end
-]]
+    abysseaOnTrade(player,npc,trade);
 end;
 
 -----------------------------------
@@ -26,7 +19,7 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-    player:startEvent(1010, 3256); -- Inform player what items they need.
+    abysseaOnTrigger(player,npc);
 end;
 
 -----------------------------------
@@ -34,8 +27,6 @@ end;
 -----------------------------------
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
 -----------------------------------
@@ -43,6 +34,4 @@ end;
 -----------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
