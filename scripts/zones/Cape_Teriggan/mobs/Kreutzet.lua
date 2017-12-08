766c23a317637c6d08aa33db4fbecdfb3d6573b1
ymkc <sixth_brother@gmx.net>
2017-12-08 23:34:19 +0000

    Fix Stormwind

--- scripts/zones/Cape_Teriggan/mobs/Kreutzet.lua
+++ scripts/zones/Cape_Teriggan/mobs/Kreutzet.lua
@@ -1,37 +1,37 @@
 ----------------------------------
 -- Area: Cape Teriggan
---  NM:  Kruetzet
+--  NM:  Kreutzet
 -----------------------------------
 
 require("scripts/globals/weather");
 
------------------------------------
--- onMobDeath
------------------------------------
+function onMobWeaponSkill(target, mob, skill)
+    if (skill:getID() == 926) then
+        local stormwindCounter = mob:getLocalVar("stormwindCounter");
+
+        stormwindCounter = stormwindCounter +1;
+        mob:setLocalVar("stormwindCounter", stormwindCounter);
+        mob:setLocalVar("stormwindDamage", stormwindCounter); -- extra var for dmg calculation (in stormwind.lua)
+
+        if (stormwindCounter > 2) then
+            mob:setLocalVar("stormwindCounter", 0);
+        else
+            mob:useMobAbility(926);
+        end
+    end;
+end;
 
 function onMobDeath(mob, player, isKiller)
 end;
 
------------------------------------
--- onMobDespawn
------------------------------------
-
 function onMobDespawn(mob)
-
     -- Set Kruetzet's spawnpoint and respawn time (9-12 hours)
     UpdateNMSpawnPoint(mob:getID());
-    mob:setRespawnTime(math.random((32400),(43200)));
-
+    mob:setRespawnTime(math.random(32400,43200));
 end;
 
------------------------------------
--- onMobDisengage
------------------------------------
-
 function onMobDisengage(mob, weather)
-
     if (weather ~= WEATHER_WIND and weather ~= WEATHER_GALE) then
         DespawnMob(mob:getID());
     end
-
 end;
