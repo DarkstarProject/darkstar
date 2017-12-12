e10b0db61b67224304665947dce617a6e8187046
ymkc <sixth_brother@gmx.net>
2017-12-12 19:36:19 +0000

    Adjust San o'Dorian Mission 9-1

--- scripts/zones/Batallia_Downs/TextIDs.lua
+++ scripts/zones/Batallia_Downs/TextIDs.lua
@@ -5,6 +5,7 @@ ITEM_CANNOT_BE_OBTAINED = 6403; -- You cannot obtain the item <item>. Come back 
           ITEM_OBTAINED = 6409; -- Obtained: <item>.
            GIL_OBTAINED = 6410; -- Obtained <number> gil.
        KEYITEM_OBTAINED = 6412; -- Obtained key item: <keyitem>.
+NOTHING_OUT_OF_ORDINARY = 6423; -- There is nothing out of the ordinary here.
  FISHING_MESSAGE_OFFSET = 7229; -- You can't fish here.
 
 -- Other Dialog
