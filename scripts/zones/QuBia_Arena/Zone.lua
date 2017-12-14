e5728276e6f0ff613f920983d8e9419ea85360b0
ymkc <sixth_brother@gmx.net>
2017-12-14 20:51:16 +0000

    table Sandy 9-2 onEventFinish

--- scripts/zones/QuBia_Arena/Zone.lua
+++ scripts/zones/QuBia_Arena/Zone.lua
@@ -9,16 +9,8 @@ package.loaded["scripts/zones/QuBia_Arena/TextIDs"] = nil;
 require("scripts/globals/settings");
 require("scripts/zones/QuBia_Arena/TextIDs");
 
------------------------------------
--- onInitialize
------------------------------------
-
 function onInitialize(zone)
 end;
-
------------------------------------
--- onZoneIn
------------------------------------
 
 function onZoneIn(player,prevZone)
     local cs = -1;
@@ -28,10 +20,6 @@ function onZoneIn(player,prevZone)
     return cs;
 end;
 
------------------------------------
--- onConquestUpdate
------------------------------------
-
 function onConquestUpdate(zone, updatetype)
     local players = zone:getPlayers();
     for name, player in pairs(players) do
@@ -39,54 +27,35 @@ function onConquestUpdate(zone, updatetype)
     end
 end;
 
------------------------------------
--- onRegionEnter
------------------------------------
-
 function onRegionEnter(player,region)
 end;
-
------------------------------------
--- onEventUpdate
------------------------------------
 
 function onEventUpdate(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
     if (csid == 32004) then
-        if (player:getBattlefield():getBattlefieldNumber() == 1) then
-            SpawnMob(17621014);
-            SpawnMob(17621015);
-            SpawnMob(17621016);
-            local trion = player:getBattlefield():insertAlly(14183)
-            trion:setSpawn(-403,-201,413,58);
+        local battlefield = player:getBattlefield();
+        if battlefield then
+            local area = battlefield:getBattlefieldNumber();
+            local tabre =
+            {
+                [1] = { mobids = {17621014, 17621015, 17621016}, trionPos = {-403, -201, 413, 58}, playerPos = {-400, -201, 419, 61} },
+                [2] = { mobids = {17621028, 17621029, 17621030}, trionPos = {-3, -1, 4, 61}, playerPos = {0, -1, 10, 61} },
+                [3] = { mobids = {17621042, 17621043, 17621044}, trionPos = {397, 198, -395, 64}, playerPos = {399, 198, -381, 57} }
+            }
+            for _, mobid in pairs(tabre[area].mobids) do
+                SpawnMob(mobid)
+            end
+
+            local trion = battlefield:insertAlly(14183);
+            trion:setSpawn(tabre[area].trionPos);
             trion:spawn();
-            player:setPos(-400,-201,419,61);
-        elseif (player:getBattlefield():getBattlefieldNumber() == 2) then
-            SpawnMob(17621028);
-            SpawnMob(17621029);
-            SpawnMob(17621030);
-            local trion = player:getBattlefield():insertAlly(14183)
-            trion:setSpawn(-3,-1,4,61);
-            trion:spawn();
-            player:setPos(0,-1,10,61);
-        elseif (player:getBattlefield():getBattlefieldNumber() == 3) then
-            SpawnMob(17621042);
-            SpawnMob(17621043);
-            SpawnMob(17621044);
-            local trion = player:getBattlefield():insertAlly(14183)
-            trion:setSpawn(397,198,-395,64);
-            trion:spawn();
-            player:setPos(399,198,-381,57);
+            player:setPos(tabre[area].playerPos);
         end
     end
 end;
