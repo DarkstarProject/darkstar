3392b193ed2f9767afb33839110ba0e97ebeb086
David Norris <dwn134@gmail.com>
2017-12-19 22:15:30 -0600

    Chariot moves unlocked for other mobs and skill lists

--- scripts/globals/mobskills/homing_missle.lua
+++ scripts/globals/mobskills/homing_missle.lua
@@ -8,9 +8,10 @@ require("scripts/globals/monstertpmoves");
 ---------------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-    local mobSkin = mob:getModelId();
+    -- See PW's script
+    local phase = mob:getLocalVar("phase");
 
-    if (mobSkin == 1825) then
+    if (phase == 8) then
         return 0;
     else
         return 1;
