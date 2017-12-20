a6b4ab2430a630d8105ef2861b3d58d4e0ad4988
Scott <ksgilvin100@roadrunner.com>
2017-12-20 18:00:50 -0500

    Last of ID changes from what i can tell.

--- scripts/zones/Beaucedine_Glacier_[S]/TextIDs.lua
+++ scripts/zones/Beaucedine_Glacier_[S]/TextIDs.lua
@@ -4,4 +4,4 @@
 ITEM_CANNOT_BE_OBTAINED = 6380; -- You cannot obtain the item <item> come back again after sorting your inventory
           ITEM_OBTAINED = 6386; -- Obtained: <item>
            GIL_OBTAINED = 6387; -- Obtained <number> gil
-       KEYITEM_OBTAINED = 8660; -- Obtained key item: <keyitem>
+       KEYITEM_OBTAINED = 6389; -- Obtained key item: <keyitem>
