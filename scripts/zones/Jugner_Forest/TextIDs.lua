8bd43ea8c3f20800e0933108565f9d0b57e7a8bf
Scott <ksgilvin100@roadrunner.com>
2017-12-20 18:41:55 -0500

    More Fixes. Will have to do more double checking next time before pr. my bad.

--- scripts/zones/Jugner_Forest/TextIDs.lua
+++ scripts/zones/Jugner_Forest/TextIDs.lua
@@ -1,9 +1,9 @@
 -- Variable TextID   Description text
 
 -- General Texts
- ITEM_CANNOT_BE_OBTAINED = 6403; -- You cannot obtain the item <item>. Come back after sorting your inventory.
-           ITEM_OBTAINED = 6409; -- Obtained: <item>.
-            GIL_OBTAINED = 6410; -- Obtained <number> gil.
+ ITEM_CANNOT_BE_OBTAINED = 6402; -- You cannot obtain the item <item>. Come back after sorting your inventory.
+           ITEM_OBTAINED = 6408; -- Obtained: <item>.
+            GIL_OBTAINED = 6409; -- Obtained <number> gil.
         KEYITEM_OBTAINED = 6411; -- Obtained key item: <keyitem>.
          BEASTMEN_BANNER = 7150; -- There is a beastmen's banner.
   FISHING_MESSAGE_OFFSET = 7702; -- You can't fish here.
--- scripts/zones/Sauromugue_Champaign/TextIDs.lua
+++ scripts/zones/Sauromugue_Champaign/TextIDs.lua
@@ -1,9 +1,9 @@
 -- Variable TextID   Description text
 
 -- General Texts
- ITEM_CANNOT_BE_OBTAINED = 6403; -- You cannot obtain the item <item>. Come back after sorting your inventory.
-           ITEM_OBTAINED = 6409; -- Obtained: <item>.
-            GIL_OBTAINED = 6410; -- Obtained <number> gil.
+ ITEM_CANNOT_BE_OBTAINED = 6402; -- You cannot obtain the item <item>. Come back after sorting your inventory.
+           ITEM_OBTAINED = 6408; -- Obtained: <item>.
+            GIL_OBTAINED = 6409; -- Obtained <number> gil.
         KEYITEM_OBTAINED = 6411; -- Obtained key item: <keyitem>.
   FISHING_MESSAGE_OFFSET = 7236; -- You can't fish here.
 
