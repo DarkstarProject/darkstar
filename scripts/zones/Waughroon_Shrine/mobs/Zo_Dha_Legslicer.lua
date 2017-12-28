ad5fa33f1130145471aa1e5e6baa5269fb30bff8
wrenffxi <oliviapekoe@gmail.com>
2017-12-28 11:47:43 -0500

    missing horlais peak bcnm luas missing waughroon shrine bcnm luas added some waughroon shrine bcnm mob luas added special self-destruct move for 3, 2, 1, ....

---
+++ scripts/zones/Waughroon_Shrine/mobs/Zo_Dha_Legslicer.lua
@@ -0,0 +1,12 @@
+-----------------------------------
+-- Area: Waughroon Shrine
+-- MOB: Zo'Dha Legslicer
+-- BCNM Fight: Grimshell Shocktroopers
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")};
+
+function onMobSpawn(mob)
+end;
+
+function onMobDeath(mob, player, isKiller)
+end;
