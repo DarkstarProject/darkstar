436566781145f90a02b6a122518cede5b05e91a5
wrenffxi <oliviapekoe@gmail.com>
2017-12-31 15:20:59 -0500

    moved a bunch of entity IDs out of lua scripts.  a few of them were wrong, so I updated them to current polutils extract.

--- scripts/zones/Batallia_Downs/npcs/qm2.lua
+++ scripts/zones/Batallia_Downs/npcs/qm2.lua
@@ -5,20 +5,16 @@
 -----------------------------------
 package.loaded["scripts/zones/Batallia_Downs/TextIDs"] = nil;
 -----------------------------------
-
 require("scripts/zones/Batallia_Downs/TextIDs");
+require("scripts/zones/Batallia_Downs/MobIDs");
 require("scripts/globals/keyitems");
 
------------------------------------
--- onTrigger
------------------------------------
-
 function onTrigger(player,npc)
-    local Sturmtiger = player:getVar("SturmtigerKilled");
+    local sturmtigerKilled = player:getVar("SturmtigerKilled");
     
-    if (player:getVar("ChasingQuotas_Progress") == 5 and Sturmtiger == 0) then
-        SpawnMob(17207696,300):updateClaim(player);
-    elseif (Sturmtiger == 1) then
+    if (player:getVar("ChasingQuotas_Progress") == 5 and sturmtigerKilled == 0) then
+        SpawnMob(STURMTIGER,300):updateClaim(player);
+    elseif (sturmtigerKilled == 1) then
         player:addKeyItem(RANCHURIOMES_LEGACY);
         player:messageSpecial(KEYITEM_OBTAINED,RANCHURIOMES_LEGACY);
         player:setVar("ChasingQuotas_Progress",6);
@@ -28,27 +24,11 @@ function onTrigger(player,npc)
     end
 end;
 
------------------------------------
--- onTrade
------------------------------------
-
 function onTrade(player,npc,trade)
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
--- scripts/zones/Beaucedine_Glacier/npcs/Mirror_Pond.lua
+++ scripts/zones/Beaucedine_Glacier/npcs/Mirror_Pond.lua
@@ -7,47 +7,25 @@
 package.loaded["scripts/zones/Beaucedine_Glacier/TextIDs"] = nil;
 -----------------------------------
 require("scripts/zones/Beaucedine_Glacier/TextIDs");
+require("scripts/zones/Beaucedine_Glacier/MobIDs");
 require("scripts/globals/keyitems");
 require("scripts/globals/quests");
-
------------------------------------
--- onTrade Action
------------------------------------
 
 function onTrade(player,npc,trade)
 end;
 
------------------------------------
--- onTrigger Action
------------------------------------
-
 function onTrigger(player,npc)
-    local LoveAndIce = player:getQuestStatus(BASTOK,LOVE_AND_ICE);
-
-    if (npc:getID() == 17232193) then -- Mirror Pond at J-8
-        if (LoveAndIce == QUEST_ACCEPTED and player:hasKeyItem(CARMELOS_SONG_SHEET) == true) then
+    if (npc:getID() == MIRROR_POND_J8) then
+        if (player:getQuestStatus(BASTOK,LOVE_AND_ICE) == QUEST_ACCEPTED and player:hasKeyItem(CARMELOS_SONG_SHEET)) then
             player:startEvent(100);
         end
     end
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("updateRESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("finishRESULT: %u",option);
-
     if (csid == 100) then
         player:setVar("LoveAndIceProgress",1);
         player:delKeyItem(CARMELOS_SONG_SHEET);
