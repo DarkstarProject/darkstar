b3b5f98ffbfa5f4b9f792f491d053e188548c3fc
wrenffxi <oliviapekoe@gmail.com>
2017-12-31 22:12:51 -0500

    more lua id cleaning

--- scripts/zones/Toraimarai_Canal/npcs/qm11.lua
+++ scripts/zones/Toraimarai_Canal/npcs/qm11.lua
@@ -1,32 +1,17 @@
 -----------------------------------
 -- Area: Toraimarai Canal
 -- NPC:  ???
--- Involved In Quest: Wild Card
+-- Involved In Quest: The Root of the Problem
 -- !pos 19 17 59 169
 -----------------------------------
-package.loaded["scripts/zones/Toraimarai_Canal/TextIDs"] = nil;
------------------------------------
-
-require("scripts/globals/settings");
-require("scripts/globals/keyitems");
-require("scripts/globals/treasure");
-require("scripts/globals/quests");
-require("scripts/zones/Toraimarai_Canal/TextIDs");
------------------------------------
--- onTrade Action
------------------------------------
+require("scripts/zones/Toraimarai_Canal/MobIDs");
 
 function onTrade(player,npc,trade)
-
 end;
-
------------------------------------
--- onTrigger Action
------------------------------------
 
 function onTrigger(player,npc)
     if (player:getVar("rootProblem") == 2) then
-        if (player:getVar("rootProblemQ1") == 2 and player:getVar("rootProblemQ2") == 2) then
+        if (not GetMobByID(MAGIC_SLUDGE):isSpawned() and player:getVar("rootProblemQ1") == 2 and player:getVar("rootProblemQ2") == 2) then
             player:startEvent(48);
         end
     elseif (player:getVar("rootProblem") == 3) then
@@ -34,28 +19,11 @@ function onTrigger(player,npc)
     end
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,csid,option)
-    -- printf("CSID2: %u",csid);
-    -- printf("RESULT2: %u",option);
-
 end;
-
------------------------------------
--- onEventFinish
------------------------------------
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
-
     if (csid == 48 and option ~= 0) then
-        SpawnMob(17469516):updateClaim(player);
+        SpawnMob(MAGIC_SLUDGE):updateClaim(player);
     end
-
 end;
-
-
--- scripts/zones/Uleguerand_Range/MobIDs.lua
+++ scripts/zones/Uleguerand_Range/MobIDs.lua
@@ -1,3 +1,5 @@
 JORMUNGAND              = 16797969;
+GEUSH_URVAN             = 16798078;
 
+GEUSH_URVAN_QM          = 16798097;
 WATERFALL               = 16798112;
