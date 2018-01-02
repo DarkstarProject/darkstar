6cf25b810756c1c701964c63a7a597da58aa86c3
Scott <ksgilvin100@roadrunner.com>
2018-01-02 14:30:41 -0500

    Dec Update TextIDs Corrections

--- scripts/zones/Dangruf_Wadi/TextIDs.lua
+++ scripts/zones/Dangruf_Wadi/TextIDs.lua
@@ -1,9 +1,9 @@
 -- Variable TextID   Description text
 
 -- General Texts
-ITEM_CANNOT_BE_OBTAINED = 6540; -- You cannot obtain the item <item>. Come back after sorting your inventory.
-          ITEM_OBTAINED = 6546; -- Obtained: <item>.
-           GIL_OBTAINED = 6547; -- Obtained <number> gil.
+ITEM_CANNOT_BE_OBTAINED = 6539; -- You cannot obtain the item <item>. Come back after sorting your inventory.
+          ITEM_OBTAINED = 6545; -- Obtained: <item>.
+           GIL_OBTAINED = 6546; -- Obtained <number> gil.
        KEYITEM_OBTAINED = 6548; -- Obtained key item: <keyitem>.
  FISHING_MESSAGE_OFFSET = 7206; -- You can't fish here.
 
