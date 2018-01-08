38691cdaab24bbb965fcb36db5edd59480c192bf
wrenffxi <oliviapekoe@gmail.com>
2018-01-07 21:25:18 -0500

    more ids out of entity luas tabled RuAun portals fixed pincerstone animation fixed RuAun treasure coffer ID

--- scripts/zones/RuAun_Gardens/npcs/qm1.lua
+++ scripts/zones/RuAun_Gardens/npcs/qm1.lua
@@ -6,46 +6,26 @@
 -----------------------------------
 package.loaded["scripts/zones/RuAun_Gardens/TextIDs"] = nil;
 -----------------------------------
-
 require("scripts/zones/RuAun_Gardens/TextIDs");
-
------------------------------------
--- onTrade Action
------------------------------------
+require("scripts/zones/RuAun_Gardens/MobIDs");
+require("scripts/globals/status");
 
 function onTrade(player,npc,trade)
-
     -- Trade Gem of the North and Winterstone
-    if (GetMobAction(17309980) == 0 and trade:hasItemQty(1424,1) and trade:hasItemQty(1425,1) and trade:getItemCount() == 2) then
+    if (not GetMobByID(GENBU):isSpawned() and trade:hasItemQty(1424,1) and trade:hasItemQty(1425,1) and trade:getItemCount() == 2) then
         player:tradeComplete();
-        SpawnMob(17309980):updateClaim(player); -- Spawn Genbu
+        SpawnMob(GENBU):updateClaim(player);
         player:showText(npc,SKY_GOD_OFFSET + 5);
         npc:setStatus(STATUS_DISAPPEAR);
     end
 end;
 
------------------------------------
--- onTrigger Action
------------------------------------
-
 function onTrigger(player,npc)
     player:messageSpecial(SKY_GOD_OFFSET);
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
--- scripts/zones/RuAun_Gardens/npcs/qm3.lua
+++ scripts/zones/RuAun_Gardens/npcs/qm3.lua
@@ -6,46 +6,26 @@
 -----------------------------------
 package.loaded["scripts/zones/RuAun_Gardens/TextIDs"] = nil;
 -----------------------------------
-
 require("scripts/zones/RuAun_Gardens/TextIDs");
-
------------------------------------
--- onTrade Action
------------------------------------
+require("scripts/zones/RuAun_Gardens/MobIDs");
+require("scripts/globals/status");
 
 function onTrade(player,npc,trade)
-
     -- Trade Gem of the West and Autumnstone
-    if (GetMobAction(17309982) == 0 and trade:hasItemQty(1422,1) and trade:hasItemQty(1423,1) and trade:getItemCount() == 2) then
+    if (not GetMobByID(BYAKKO):isSpawned() and trade:hasItemQty(1422,1) and trade:hasItemQty(1423,1) and trade:getItemCount() == 2) then
         player:tradeComplete();
-        SpawnMob(17309982):updateClaim(player); -- Spawn Byakko
+        SpawnMob(BYAKKO):updateClaim(player);
         player:showText(npc,SKY_GOD_OFFSET + 11);
         npc:setStatus(STATUS_DISAPPEAR);
     end
 end;
 
------------------------------------
--- onTrigger Action
------------------------------------
-
 function onTrigger(player,npc)
     player:messageSpecial(SKY_GOD_OFFSET + 2);
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
-
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
------------------------------------
--- onEventFinish
------------------------------------
-
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
