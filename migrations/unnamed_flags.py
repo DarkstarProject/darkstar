1979c04495d531911e2de61fe55f9a099a6dad12
Tyler Schneider <teschnei@gmail.com>
2018-01-08 20:11:32 -0700

    Fixed some issues with displayhead
    Fixes #4487

--- migrations/unnamed_flags.py
+++ migrations/unnamed_flags.py
@@ -14,7 +14,7 @@ def needs_to_run(cur):
     return cur.fetchone()
 
 def migrate(cur, db):
-    cur.execute("ALTER TABLE `chars` ADD COLUMN `nnameflags` tinyint(1) unsigned NOT NULL DEFAULT '0';")
+    cur.execute("ALTER TABLE `chars` ADD COLUMN `nnameflags` int(10) unsigned NOT NULL DEFAULT '0';")
     cur.execute("UPDATE `chars` o, `chars` n SET o.`nnameflags` = (CASE WHEN n.isnewplayer THEN 0x0 ELSE 0x04 END) WHERE o.charid = n.charid;")
     print("Committing changes")
     db.commit()
