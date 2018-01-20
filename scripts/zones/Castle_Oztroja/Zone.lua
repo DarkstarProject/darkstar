f1a8803d5a9f3116ee671699a4dda559eead8151
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-01-20 13:39:00 -0600

    Implement Handle Pattern for Oztroja Door to Upper Floors

--- scripts/zones/Castle_Oztroja/Zone.lua
+++ scripts/zones/Castle_Oztroja/Zone.lua
@@ -10,10 +10,6 @@ require("scripts/zones/Castle_Oztroja/MobIDs");
 require("scripts/globals/conquest");
 require("scripts/globals/zone");
 
------------------------------------
--- onInitialize
------------------------------------
-
 function onInitialize(zone)
     UpdateNMSpawnPoint(YAGUDO_AVATAR);
     GetMobByID(YAGUDO_AVATAR):setRespawnTime(math.random(900, 10800));
@@ -21,10 +17,6 @@ function onInitialize(zone)
     UpdateTreasureSpawnPoint(17396210);
     UpdateTreasureSpawnPoint(17396211);
 end;
-
------------------------------------
--- onZoneIn
------------------------------------
 
 function onZoneIn(player,prevZone)
     local cs = -1;
@@ -34,10 +26,6 @@ function onZoneIn(player,prevZone)
     return cs;
 end;
 
------------------------------------
--- onConquestUpdate
------------------------------------
-
 function onConquestUpdate(zone, updatetype)
     local players = zone:getPlayers();
 
@@ -46,25 +34,36 @@ function onConquestUpdate(zone, updatetype)
     end
 end;
 
------------------------------------
--- onRegionEnter
------------------------------------
-
 function onRegionEnter(player,region)
 end;
 
------------------------------------
--- onEventUpdate
------------------------------------
+Oz_Handle_Table =
+{
+    [0] = {[17396196] = 8, [17396197] = 8, [17396198] = 8, [17396199] = 8},
+    [1] = {[17396196] = 8, [17396197] = 9, [17396198] = 9, [17396199] = 9},
+    [2] = {[17396196] = 9, [17396197] = 8, [17396198] = 9, [17396199] = 9},
+    [3] = {[17396196] = 9, [17396197] = 8, [17396198] = 8, [17396199] = 9},
+    [4] = {[17396196] = 9, [17396197] = 8, [17396198] = 9, [17396199] = 8},
+    [5] = {[17396196] = 8, [17396197] = 8, [17396198] = 9, [17396199] = 8},
+    [6] = {[17396196] = 9, [17396197] = 9, [17396198] = 8, [17396199] = 9},
+    [7] = {[17396196] = 9, [17396197] = 9, [17396198] = 9, [17396199] = 8},
+    [8] = {[17396196] = 8, [17396197] = 8, [17396198] = 9, [17396199] = 9},
+};
+
+function onGameHour(zone)
+    local VanadielHour = VanadielHour();
+    if (VanadielHour % 24 == 0) then -- Change handles every game day
+        Oz_handleSet = math.random(0,8);
+        for i,v in pairs(Oz_Handle_Table[Oz_handleSet]) do
+            GetNPCByID(i):setAnimation(v);
+        end
+    end
+end;
 
 function onEventUpdate(player,csid,option)
     -- printf("CSID: %u",csid);
     -- printf("RESULT: %u",option);
 end;
-
------------------------------------
--- onEventFinish
------------------------------------
 
 function onEventFinish(player,csid,option)
     -- printf("CSID: %u",csid);
