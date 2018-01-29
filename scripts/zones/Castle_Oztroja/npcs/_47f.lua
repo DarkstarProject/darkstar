4df6550ff1ebae70ef6af096cfc06842ad3e2ede
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-01-29 15:58:07 -0600

    Forgot to do 'f' -- Add ANIMATION_ status'

--- scripts/zones/Castle_Oztroja/npcs/_47f.lua
+++ scripts/zones/Castle_Oztroja/npcs/_47f.lua
@@ -4,28 +4,27 @@
 -- Notes: Opens door _471
 -- !pos -182 -15 -19 151
 -----------------------------------
-
+require("scripts/zones/Castle_Oztroja/MobIDs");
 require("scripts/globals/settings");
+require("scripts/globals/status");
 
 function onTrigger(player,npc)
     local DoorID = npc:getID()-2;
     local DoorA = GetNPCByID(DoorID):getAnimation();
 
-    if (DoorA == 9 and npc:getAnimation() == 9) then
-        npc:setAnimation(8);
-    elseif (DoorA == 9 and npc:getAnimation() == 8) then
-        npc:setAnimation(9);
+    if (DoorA == ANIMATION_CLOSE_DOOR and npc:getAnimation() == ANIMATION_CLOSE_DOOR) then
+        npc:setAnimation(ANIMATION_OPEN_DOOR);
+    elseif (DoorA == ANIMATION_CLOSE_DOOR and npc:getAnimation() == ANIMATION_OPEN_DOOR) then
+        npc:setAnimation(ANIMATION_CLOSE_DOOR);
     end
 
---    print (Oz_handleSet);
-
     npc:timer(1500, function(npc)
-        if (npc:getAnimation() == Oz_Handle_Table[Oz_handleSet][npc:getID()+34]
-            and GetNPCByID(npc:getID()+1):getAnimation() == Oz_Handle_Table[Oz_handleSet][npc:getID()+35]
-            and GetNPCByID(npc:getID()+2):getAnimation() == Oz_Handle_Table[Oz_handleSet][npc:getID()+36]
-            and GetNPCByID(npc:getID()+3):getAnimation() == Oz_Handle_Table[Oz_handleSet][npc:getID()+37]) then
+        if (npc:getAnimation() == OZ_HANDLE_TABLE[Oz_handleSet][npc:getID()+34]
+            and GetNPCByID(npc:getID()+1):getAnimation() == OZ_HANDLE_TABLE[Oz_handleSet][npc:getID()+35]
+            and GetNPCByID(npc:getID()+2):getAnimation() == OZ_HANDLE_TABLE[Oz_handleSet][npc:getID()+36]
+            and GetNPCByID(npc:getID()+3):getAnimation() == OZ_HANDLE_TABLE[Oz_handleSet][npc:getID()+37]) then
                 GetNPCByID(DoorID):openDoor(6);
-                for i = npc:getID(), npc:getID()+3, 1 do GetNPCByID(i):setAnimation(9); end
+                for i = npc:getID(), npc:getID()+3, 1 do GetNPCByID(i):setAnimation(ANIMATION_CLOSE_DOOR); end
         end
     end);
 end;
