ac955db21618d94a3558e18d114827832b0bf9be
wrenffxi <oliviapekoe@gmail.com>
2018-02-07 02:34:43 -0500

    slimmed Stale Draft script and added job special to Swift Belt NMs moved IDs out of RoMaeve zone lua Pevv the Riverleaper only has one wyvern per instance

--- scripts/zones/Sacrarium/MobIDs.lua
+++ scripts/zones/Sacrarium/MobIDs.lua
@@ -1,4 +1,15 @@
 OLD_PROFESSOR_MARISELLE     = 16891970;
 
+-- {nmId, {keys are allowable player races}}
+SWIFT_BELT_NMS =
+{
+    {16892068, {[1] = true, [2] = true}},   -- Balor (hume)
+    {16892069, {[3] = true, [4] = true}},   -- Luaith (elvaan)
+    {16892070, {[5] = true, [6] = true}},   -- Lobais (tarutaru)
+    {16892073, {[7] = true}},               -- Caithleann (mithra)
+    {16892074, {[8] = true}},               -- Indich (galka)
+};
+
+STALE_DRAFT_OFFSET          = 16892097;
 SACRARIUM_LABYRINTH_OFFSET  = 16892110;
 SACRARIUM_TREASURE_CHEST    = 16892183;
