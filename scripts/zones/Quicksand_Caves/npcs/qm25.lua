a761aebb3422fcde8d3cc878a1c2befc8e4c7728
wrenffxi <oliviapekoe@gmail.com>
2018-02-10 04:52:34 -0500

    id cleanup in entity luas, quicksand caves and rangeumont pass updated ornate door pressure plate records in npc_list (one was missing) pressure plate weight check no longer uses server variables

--- scripts/zones/Quicksand_Caves/npcs/qm25.lua
+++ scripts/zones/Quicksand_Caves/npcs/qm25.lua
@@ -6,19 +6,14 @@
 package.loaded["scripts/zones/Quicksand_Caves/TextIDs"] = nil;
 -----------------------------------
 require("scripts/zones/Quicksand_Caves/TextIDs");
+require("scripts/globals/npc_util");
 -----------------------------------
 
 function onTrigger(player,npc)
-    local newPosition = npcUtil.pickNewPosition(npc:getID(), anticanTagPositions, true);
-
-    if (player:getFreeSlotsCount() == 0) then
-        player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,1190);
-    else
-        if (player:addItem(1190)) then
-            npc:hideNPC(7200);
-            npc:setPos(newPosition.x, newPosition.y, newPosition.z);
-            player:messageSpecial(ITEM_OBTAINED, 1190);
-            SetServerVariable("[POP]Antican_Tag", os.time() + 7200); -- "pause" UpdateNPCSpawnPoint
-        end
+    if (npcUtil.giveItem(player, 1190)) then
+        local newPosition = npcUtil.pickNewPosition(npc:getID(), ANTICAN_TAG_POSITIONS, true);
+        npc:hideNPC(7200);
+        npc:setPos(newPosition.x, newPosition.y, newPosition.z);
+        SetServerVariable("[POP]Antican_Tag", os.time() + 7200); -- "pause" UpdateNPCSpawnPoint
     end
 end;
