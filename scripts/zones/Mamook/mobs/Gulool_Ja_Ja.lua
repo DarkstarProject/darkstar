d029e97b945157981cfb9ac2916d9127fe2c11b2
David Norris <dwn134@gmail.com>
2018-02-11 18:19:48 -0600

    made changes to Ja Ja

--- scripts/zones/Mamook/mobs/Gulool_Ja_Ja.lua
+++ scripts/zones/Mamook/mobs/Gulool_Ja_Ja.lua
@@ -5,63 +5,56 @@
 require("scripts/globals/titles");
 require("scripts/globals/status");
 require("scripts/zones/Mamook/MobIDs");
+mixins = {require("scripts/mixins/job_special")};
 
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:setLocalVar("1Hour", math.random(3,5)); 
     mob:setMod(MOD_DOUBLE_ATTACK, 20);
+    mob:setMobMod(MOBMOD_DRAW_IN, 2);
 end;
 
 function onMobEngaged(mob,target)
-    for i = 17043876, 17043879, 1 do
+    for i = GULOOL_JA_JA + 1, GULOOL_JA_JA + 4 do
         SpawnMob(i):updateEnmity(target);
     end
 end;
 
 function onMobFight(mob, target)
-    local petIDs = {17043876,17043877,17043878,17043879};
-    local petStatus = {GetMobAction(petIDs[1]),GetMobAction(petIDs[2]),GetMobAction(petIDs[3]),GetMobAction(petIDs[4])};
-    local HPP = mob:getHPP();
-
-    if (mob:getLocalVar("use1Hour") == 0) then
-        if (HPP <= mob:getLocalVar("1Hour")) then
-            mob:useMobAbility(731); -- Mijin Gakure
-            mob:setLocalVar("use1Hour", 1);
-        end
-    end
+    local petIDs = {GULOOL_JA_JA + 1, GULOOL_JA_JA + 2, GULOOL_JA_JA + 3, GULOOL_JA_JA + 4};
+    local petStatus = {GetCurrentAction(petIDs[1]),GetCurrentAction(petIDs[2]),GetCurrentAction(petIDs[3]),GetCurrentAction(petIDs[4])};
 
     if (mob:getBattleTime() % 15 < 2 and mob:getBattleTime() > 10) then
-        if (GetMobAction(Gulool_Ja_Ja+1) == 0) then
-            GetMobByID(Gulool_Ja_Ja+1):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
-            SpawnMob(Gulool_Ja_Ja+1):updateEnmity(target);
-        elseif (GetMobAction(Gulool_Ja_Ja+2) == 0) then
-            GetMobByID(Gulool_Ja_Ja+2):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
-            SpawnMob(Gulool_Ja_Ja+2):updateEnmity(target);
-        elseif (GetMobAction(Gulool_Ja_Ja+3) == 0) then
-            GetMobByID(Gulool_Ja_Ja+3):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
-            SpawnMob(Gulool_Ja_Ja+3):updateEnmity(target);
-        elseif (GetMobAction(Gulool_Ja_Ja+4) == 0) then
-            GetMobByID(Gulool_Ja_Ja+4):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
-            SpawnMob(Gulool_Ja_Ja+4):updateEnmity(target);
+        if (not GetMobByID(GULOOL_JA_JA + 1):isSpawned()) then
+            GetMobByID(GULOOL_JA_JA + 1):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
+            SpawnMob(GULOOL_JA_JA + 1):updateEnmity(target);
+        elseif (not GetMobByID(GULOOL_JA_JA + 2):isSpawned()) then
+            GetMobByID(GULOOL_JA_JA + 2):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
+            SpawnMob(GULOOL_JA_JA + 2):updateEnmity(target);
+        elseif (not GetMobByID(GULOOL_JA_JA + 3):isSpawned()) then
+            GetMobByID(GULOOL_JA_JA + 3):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
+            SpawnMob(GULOOL_JA_JA + 3):updateEnmity(target);
+        elseif (not GetMobByID(GULOOL_JA_JA + 4):isSpawned()) then
+            GetMobByID(GULOOL_JA_JA + 4):setSpawn(mob:getXPos()+math.random(1,5), mob:getYPos(), mob:getZPos()+math.random(1,5));
+            SpawnMob(GULOOL_JA_JA + 4):updateEnmity(target);
         end
     end
     for i = 1, 4 do
-        if petStatus[i] == 16 then
+        if petStatus[i] == ACTION_ROAMING then
             GetMobByID(petIDs[i]):updateEnmity(target);
         end
     end
 end;
 
 function onMobDisengage(mob)
-    for i = 1,4 do DespawnMob(Gulool_Ja_Ja + i) end
+    for i = 1,4 do DespawnMob(GULOOL_JA_JA + i) end
 end;
 
 function onMobDeath(mob, player, isKiller)
     player:addTitle(SHINING_SCALE_RIFLER);
-    for i = 1,4 do DespawnMob(Gulool_Ja_Ja + i) end
+    for i = 1,4 do DespawnMob(GULOOL_JA_JA + i) end
 end;
 
 function onMobDespawn(mob)
-    for i = 1,4 do DespawnMob(Gulool_Ja_Ja + i) end
+    for i = 1,4 do DespawnMob(GULOOL_JA_JA + i) end
 end;
