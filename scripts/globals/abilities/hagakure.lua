8f140d276d3f4175cd4317021a7d7a0e660ca8dc
NeuromancerXI <3996176+neuromancerxi@users.noreply.github.com>
2018-03-05 05:56:26 -0800

    Remove Function Headers from Ability Script
    Removed headers from scripts/globals/abilities/hagakure.lua

--- scripts/globals/abilities/hagakure.lua
+++ scripts/globals/abilities/hagakure.lua
@@ -8,17 +8,11 @@
 
 require("scripts/globals/status");
 
------------------------------------
--- onAbilityCheck
------------------------------------
 
 function onAbilityCheck(player,target,ability)
     return 0,0;
 end;
 
------------------------------------
--- onUseAbility
------------------------------------
 
 function onUseAbility(player,target,ability)
         target:delStatusEffect(EFFECT_HAGAKURE);
