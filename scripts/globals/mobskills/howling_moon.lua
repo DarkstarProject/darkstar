69c5e1ed1b1df380b7f8b21de2a242c40d1fd0a3
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-03-11 23:56:55 -0500

    Revert changes to howling_moon.lua

--- scripts/globals/mobskills/howling_moon.lua
+++ scripts/globals/mobskills/howling_moon.lua
@@ -17,8 +17,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     local dmgmod = 3;
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg() * 9,ELE_DARK,dmgmod,TP_MAB_BONUS,1);
-    local dmg = mobAddBonuses(mob,nil,target,info.dmg,ELE_DARK);
-    dmg = MobFinalAdjustments(dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_WIPE_SHADOWS);
+    local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,MOBSKILL_MAGICAL,MOBPARAM_DARK,MOBPARAM_WIPE_SHADOWS);
     target:delHP(dmg);
     return dmg;
 
