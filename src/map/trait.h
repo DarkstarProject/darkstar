8f22fb46cd1696128f68d55fef99bc3b0fb88bae
nfaletra <autkast33@gmail.com>
2018-03-14 13:29:00 -0600

    No equals!

--- src/map/trait.h
+++ src/map/trait.h
@@ -177,13 +177,13 @@ public:
 
 private:
 
-    uint8   m_id = { 0 };
-    uint8   m_job = { 0 };
-    uint8   m_level = { 0 };
-    Mod     m_mod = { Mod::NONE };
-    int16   m_value = { 0 };
-    uint8   m_rank = { 0 };
-    uint32  m_meritId = { 0 };
+    uint8   m_id {0};
+    uint8   m_job {0};
+    uint8   m_level {0};
+    Mod     m_mod {Mod::NONE};
+    int16   m_value {0};
+    uint8   m_rank {0};
+    uint32  m_meritId {0};
 };
 
 /************************************************************************
