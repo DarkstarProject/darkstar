d345c22f5fb97891196717e4a458f3f268b7cb4c
TeoTwawki <TeoTwawki@users.noreply.github.com>
2018-03-23 18:51:45 -0400

    remove default timestamp

--- sql/audit_gm.sql
+++ sql/audit_gm.sql
@@ -10,7 +10,7 @@
 
 DROP TABLE IF EXISTS `audit_gm`;
 CREATE TABLE `audit_gm` (
-  `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_time` datetime NOT NULL,
   `gm_name` varchar(16) NOT NULL,
   `command` varchar(40) NOT NULL,
   `full_string` varchar(200) NOT NULL,
