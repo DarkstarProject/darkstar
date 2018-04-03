93cd677390a63b4091580e6234985558fa3cdbe2
TeoTwawki <arawn.annwn@yandex.com>
2018-04-02 21:32:44 -0400

    Fix line ends which looks like were wrong in the initial revision of the file years ago.

--- scripts/globals/items/ducal_guards_ring.lua
+++ scripts/globals/items/ducal_guards_ring.lua
@@ -1,1 +1,19 @@
-------------------------------------------- ID: 14657-- Ducal Guard Ring-- Enchantment: "Teleport-RuLude Gardens"-----------------------------------------require("scripts/globals/teleports");-----------------------------------------function onItemCheck(target)    local result = 0    if not target:isZoneVisited(243) then        result = 56    end    return resultend;function onItemUse(target)    ducalGuardRing(target)end;
+-----------------------------------------
+-- ID: 14657
+-- Ducal Guard Ring
+-- Enchantment: "Teleport-RuLude Gardens"
+-----------------------------------------
+require("scripts/globals/teleports");
+-----------------------------------------
+
+function onItemCheck(target)
+    local result = 0
+    if not target:isZoneVisited(243) then
+        result = 56
+    end
+    return result
+end
+
+function onItemUse(target)
+    ducalGuardRing(target)
+end
