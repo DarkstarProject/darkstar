3d410e331625b3a510266516a0f4e6fe60fe1384
wrenffxi <oliviapekoe@gmail.com>
2018-04-07 01:41:15 -0400

    entity IDs in Lower Delkfutt's and La Theine rewrote ram pop logic - no more server vars fixed desynced IDs (la theine rainbow, fallen egg) moved egg randomization code into la theine globals

--- scripts/zones/La_Theine_Plateau/mobs/Nihniknoovi.lua
+++ scripts/zones/La_Theine_Plateau/mobs/Nihniknoovi.lua
@@ -2,6 +2,8 @@
 -- Area: La Theine Plateau
 --  MOB: Nihniknoovi
 -----------------------------------
+require("scripts/globals/status");
+-----------------------------------
 
 function onMobInitialize(mob)
     mob:setMobMod(MOBMOD_IDLE_DESPAWN, 600);
