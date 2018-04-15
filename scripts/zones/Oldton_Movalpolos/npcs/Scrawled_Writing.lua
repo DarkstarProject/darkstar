0f04639fa43679515bd597f17f616058b0be329f
Moon <infernalforest@gmail.com>
2018-04-15 19:54:44 -0400

    forgot to re add mob:setpos

--- scripts/zones/Oldton_Movalpolos/npcs/Scrawled_Writing.lua
+++ scripts/zones/Oldton_Movalpolos/npcs/Scrawled_Writing.lua
@@ -23,6 +23,7 @@ function onTrade(player,npc,trade)
     if (GetMobAction(GOBLIN_WOLFMAN) == 0 and trade:hasItemQty(4541,1) and trade:getItemCount() == 1) then
         player:tradeComplete();
         SpawnMob(GOBLIN_WOLFMAN):updateClaim(player); -- Goblin Wolfman
+		mob:setPos(x-1,y,z);
         npc:setStatus(STATUS_DISAPPEAR);
     end
 end;
