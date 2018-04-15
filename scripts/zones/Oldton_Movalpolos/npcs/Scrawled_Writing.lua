fbbc4d0886e3db6230846c46806b5ba63ac09461
Moon <infernalforest@gmail.com>
2018-04-15 13:17:18 -0400

    update

--- scripts/zones/Oldton_Movalpolos/npcs/Scrawled_Writing.lua
+++ scripts/zones/Oldton_Movalpolos/npcs/Scrawled_Writing.lua
@@ -3,29 +3,27 @@
 --  NPC: Scrawled_Writing
 -- Allows players to spawn NM Goblin Wolfman
 -----------------------------------
-package.loaded["scripts/zones/Oldton_Movalpolos/TextIDs"] = nil;
+require("scripts/zones/Oldton_Movalpolos/MobIDs");
+require("scripts/globals/npc_util");
 -----------------------------------
-require("scripts/zones/Oldton_Movalpolos/TextIDs");
------------------------------------
-
-function onSpawn(npc)
-end;
 
 function onTrade(player,npc,trade)
-
-    local x = npc:getXPos();
-    local y = npc:getYPos();
-    local z = npc:getZPos();
-    local mob = GetMobByID(16822459);
-
-    -- Trade Goblin Drink
-    if (GetMobAction(16822459) == 0 and trade:hasItemQty(4541,1) and trade:getItemCount() == 1) then
-        player:tradeComplete();
-        SpawnMob(16822459):updateClaim(player); -- Goblin Wolfman
+    local mob = GetMobByID(GOBLIN_WOLFMAN);
+    if ( npcUtil.tradeHas(trade, 4541) and not mob:isSpawned() ) then
+        player:confirmTrade();
+        local x = npc:getXPos();
+        local y = npc:getYPos();
+        local z = npc:getZPos();
         mob:setPos(x-1,y,z);
-        npc:setStatus(STATUS_DISAPPEAR);
+        SpawnMob(GOBLIN_WOLFMAN):updateClaim(player);
     end
 end;
 
 function onTrigger(player,npc)
+end;
+
+function onEventUpdate(player,csid,option)
+end;
+
+function onEventFinish(player,csid,option)
 end;
