6015bcd5f1803c0e585b38a0321a6a84304a73ac
David Norris <dwn134@gmail.com>
2018-04-15 08:55:41 -0500

    para 2 wont overwrite para 1

--- scripts/globals/spells/paralyze_ii.lua
+++ scripts/globals/spells/paralyze_ii.lua
@@ -14,41 +14,31 @@ function onMagicCastingCheck(caster,target,spell)
 end;
 
 function onSpellCast(caster,target,spell)
+    local merits = caster:getMerit(MERIT_PARALYZE_II);
 
-    if (target:hasStatusEffect(dsp.effects.PARALYSIS)) then -- Effect already on, do nothing
-        spell:setMsg(msgBasic.MAGIC_NO_EFFECT);
-    else
-        -- Calculate duration.
+    -- Pull base stats
+    local dMND = caster:getStat(MOD_MND) - target:getStat(MOD_MND);
+
+    -- Base potency
+    local potency = math.floor(dMND / 4) + 20;
+
+    if (potency > 30) then
+        potency = 30;
+    end
+
+    if (potency < 10) then
+        potency = 10;
+    end
+
+    if (merits > 1) then
+        potency = potency + merits - 1;
+    end
+
+    if (caster:hasStatusEffect(dsp.effects.SABOTEUR)) then
+        potency = potency * 2;
+    end
+
         local duration = 120;
-
-        if (caster:hasStatusEffect(dsp.effects.SABOTEUR)) then
-            duration = duration * 2;
-        end
-
-        local dMND = caster:getStat(MOD_MND) - target:getStat(MOD_MND);
-
-        -- Calculate potency.
-        local merits = caster:getMerit(MERIT_PARALYZE_II);
-        local potency = math.floor(dMND / 4) + 20;
-        if (potency > 30) then
-            potency = 30;
-        end
-
-        if (potency < 10) then
-            potency = 10;
-        end
-
-        if (merits > 1) then
-            potency = potency + merits - 1;
-        end
-
-        if (caster:hasStatusEffect(dsp.effects.SABOTEUR)) then
-            potency = potency * 2;
-            caster:delStatusEffect(dsp.effects.SABOTEUR);
-        end
-
-        -- printf("Duration : %u",duration);
-        -- printf("Potency : %u",potency);
         local params = {};
         params.diff = nil;
         params.attribute = MOD_MND;
@@ -57,18 +47,21 @@ function onSpellCast(caster,target,spell)
         params.effect = dsp.effects.PARALYSIS;
         local resist = applyResistanceEffect(caster, target, spell, params);
 
+    if (duration >= 90) then
+        if (caster:hasStatusEffect(dsp.effects.SABOTEUR)) then
+            duration = duration * 2;
+        end
+        caster:delStatusEffect(dsp.effects.SABOTEUR);
+
         if (resist >= 0.5) then -- There are no quarter or less hits, if target resists more than .5 spell is resisted completely
             if (target:addStatusEffect(dsp.effects.PARALYSIS,potency,0,duration*resist)) then
                 spell:setMsg(msgBasic.MAGIC_ENFEEB_IS);
             else
-                -- no effect
                 spell:setMsg(msgBasic.MAGIC_NO_EFFECT);
             end
         else
-            -- resist
             spell:setMsg(msgBasic.MAGIC_RESIST);
         end
     end
-
     return dsp.effects.PARALYSIS;
 end;
