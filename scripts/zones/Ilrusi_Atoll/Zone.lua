3ef8425ddeda8e5fe367661c69f2d31a1d59a89d
wrenffxi <oliviapekoe@gmail.com>
2018-04-15 10:28:41 -0400

    ids from entity luas I-J

--- scripts/zones/Ilrusi_Atoll/Zone.lua
+++ scripts/zones/Ilrusi_Atoll/Zone.lua
@@ -1,13 +1,8 @@
 -----------------------------------
---
 -- Zone: Ilrusi_Atoll
 --  zone 55
 -----------------------------------
-require("scripts/globals/settings");
-package.loaded["scripts/zones/Ilrusi_Atoll/TextIDs"] = nil;
------------------------------------
-require("scripts/zones/Ilrusi_Atoll/TextIDs");
-require("scripts/globals/settings");
+require("scripts/zones/Ilrusi_Atoll/globals");
 -----------------------------------
 
 function onInitialize(zone)
@@ -16,12 +11,7 @@ end;
 function onZoneIn(player,prevZone)
     local cs = -1;
 
-   --------------RANDOMIZE COFFER------------------------
-  local correctcoffer = math.random(17002505,17002516);
-  SetServerVariable("correctcoffer",correctcoffer);
-  printf("corect_golden_salvage_coffer: %u",correctcoffer);
-  ---------------------------------------------------
-
+    ILRUSI_ATOLL.randomizeFigurehead();
 
     return cs;
 end;
@@ -30,12 +20,7 @@ function onRegionEnter(player,region)
 end;
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid);
-    -- printf("RESULT: %u",option);
 end;
-
