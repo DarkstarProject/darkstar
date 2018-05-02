83bdad14f6b2e6989f42fffd422448df1994e83f
David Norris <dwn134@gmail.com>
2018-05-02 15:43:54 -0500

    tabled mods

--- scripts/zones/Alzadaal_Undersea_Ruins/mobs/Armed_Gears.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/mobs/Armed_Gears.lua
@@ -7,30 +7,40 @@
 -- add add random elemental magic absorb to elements its casting
 
 mixins = {require("scripts/mixins/job_special")};
+require("scripts/globals/status");
 
-function onMobSpawn(mob)
-    mob:addMod(MOD_MDEF, 60);
-    mob:addMod(MOD_DEF, 60);
+function onMobInitialize(mob)
+    mob:addMod(dsp.mod.MDEF, 60);
+    mob:addMod(dsp.mod.DEF, 60);
     mob:AnimationSub(0);
 end;
 
 function onMobFight(mob,target)
     local mobHPP = mob:getHPP();
     if (mobHPP >= 26 and mobHPP <= 49) then
-        mob:AnimationSub(1); -- double gear
+        if mob:AnimationSub() ~= 1 then
+            mob:AnimationSub(1); -- double gear
+        end
         if mob:getLocalVar("Def1") == 0 then
-            mob:delMod(MOD_MDEF, 10);
-            mob:delMod(MOD_MDEF, 10);
+            mob:delMod(dsp.mod.MDEF, 10);
+            mob:delMod(dsp.mod.DEF, 20);
             mob:setLocalVar("Def1",1);
         end
     elseif mobHPP <= 25 then
-        mob:AnimationSub(2);
+        if mob:AnimationSub() ~= 2 then
+            mob:AnimationSub(2); -- single gear
+        end
         if mob:getLocalVar("Def2") == 0 then
-            mob:delMod(MOD_MDEF, 10);
-            mob:delMod(MOD_MDEF, 10);
+            mob:delMod(dsp.mod.MDEF, 10);
+            mob:delMod(dsp.mod.DEF, 20);
             mob:setLocalVar("Def2",1);
         end
     elseif mobHPP > 50 then
-        mob:AnimationSub(0); -- tripple gear
+        if mob:AnimationSub() ~= 0 then
+            mob:AnimationSub(0); -- tripple gear
+        end
     end
+end;
+
+function onMobDeath(mob, player, isKiller)
 end;
