2a56461275e56844cdbfc5307d0d2679aa14187b
Matthew James Davis <davismj@foursails.co>
2018-05-17 12:09:16 +0900

    style(bostaunieux_oubliette): Remove trailing spaces after commas

--- scripts/zones/Bostaunieux_Oubliette/mobs/Bodach.lua
+++ scripts/zones/Bostaunieux_Oubliette/mobs/Bodach.lua
@@ -9,9 +9,9 @@
 require("scripts/globals/wsquest")
 
 function onMobInitialize(mob)
-    mob:setMobMod(dsp.mobMod.EXP_BONUS, -100)
+    mob:setMobMod(dsp.mobMod.EXP_BONUS,-100)
 end
 
-function onMobDeath(mob, player, isKiller)
-    dsp.wsquest.handleWsnmDeath(dsp.wsquest.asuran_fists, player)
+function onMobDeath(mob,player,isKiller)
+    dsp.wsquest.handleWsnmDeath(dsp.wsquest.asuran_fists,player)
 end
