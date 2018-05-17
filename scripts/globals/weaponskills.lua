7d0149d4f997969fc6c098a510e4df6ca2256456
Matthew James Davis <davismj@foursails.co>
2018-05-17 15:01:55 +0900

    fix(weaponskills): Check for nil offhand in fencer computation
    If you had no item in the sub slot at all, the fencer check would break on a nil. This change properly checks for nils in the sub slot.

--- scripts/globals/weaponskills.lua
+++ scripts/globals/weaponskills.lua
@@ -99,16 +99,16 @@ function doPhysicalWeaponskill(attacker, target, wsID, tp, primary, action, taCh
         end
 
         nativecrit = nativecrit + (attacker:getMod(dsp.mod.CRITHITRATE)/100) + attacker:getMerit(dsp.merit.CRIT_HIT_RATE)/100 - target:getMerit(dsp.merit.ENEMY_CRIT_RATE)/100;
-        
+
         -- Handle Fencer
         local mainEquip = attacker:getStorageItem(0, 0, dsp.slot.MAIN);
         local subEquip = attacker:getStorageItem(0, 0, dsp.slot.SUB);
         if (mainEquip:isTwoHanded() == false and mainEquip:isHandToHand() == false) then
-            if (subEquip:getSkillType() == dsp.skill.NONE or subEquip:isShield()) then
+            if (subEquip == nil or subEquip:getSkillType() == dsp.skill.NONE or subEquip:isShield()) then
                 nativecrit = nativecrit + attacker:getMod(dsp.mod.FENCER_CRITHITRATE) / 100;
             end
         end
-        
+
         if (attacker:hasStatusEffect(dsp.effect.INNIN) and attacker:isBehind(target, 23)) then -- Innin acc boost attacker is behind target
             nativecrit = nativecrit + attacker:getStatusEffect(dsp.effect.INNIN):getPower();
         end
@@ -769,16 +769,16 @@ end;
         end
 
         nativecrit = nativecrit + (attacker:getMod(dsp.mod.CRITHITRATE)/100) + attacker:getMerit(dsp.merit.CRIT_HIT_RATE)/100 - target:getMerit(dsp.merit.ENEMY_CRIT_RATE)/100;
-        
+
         -- Handle Fencer
         local mainEquip = attacker:getStorageItem(0, 0, dsp.slot.MAIN);
         local subEquip = attacker:getStorageItem(0, 0, dsp.slot.SUB);
         if (mainEquip:isTwoHanded() == false and mainEquip:isHandToHand() == false) then
-            if (subEquip:getSkillType() == dsp.skill.NONE or subEquip:isShield()) then
+            if (subEquip == nil or subEquip:getSkillType() == dsp.skill.NONE or subEquip:isShield()) then
                 nativecrit = nativecrit + attacker:getMod(dsp.mod.FENCER_CRITHITRATE) / 100;
             end
         end
-        
+
         if (attacker:hasStatusEffect(dsp.effect.INNIN) and attacker:isBehind(target, 23)) then -- Innin crit boost if attacker is behind target
             nativecrit = nativecrit + attacker:getStatusEffect(dsp.effect.INNIN):getPower();
         end
