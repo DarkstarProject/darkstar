9f85395bcff488511b99ab08e307075cdb4cb85f
Matthew James Davis <davismj@foursails.co>
2018-05-18 22:22:34 +0900

    quest(the weight of your limits): Script Steel Cyclone quest

---
+++ scripts/zones/The_Sanctuary_of_ZiTah/mobs/Greenman.lua
@@ -0,0 +1,17 @@
+----------------------------------
+-- Area: The Sanctuary of Zi'Tah (121)
+--  MOB: Greenman
+--   ID: 17273295
+-- Note: Popped by qm1
+-- Involved in Quest: The Weight Of Your Limits
+-- !pos -325.406 -0.198 471.459 121
+-----------------------------------
+require("scripts/globals/wsquest")
+
+function onMobInitialize(mob)
+    mob:setMobMod(dsp.mobMod.EXP_BONUS,-100)
+end
+
+function onMobDeath(mob,player,isKiller)
+    dsp.wsquest.handleWsnmDeath(dsp.wsquest.steel_cyclone,player)
+end
