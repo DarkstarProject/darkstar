5e3de36f8ab9193b36d73dd4c8c544925e1a5319
TeoTwawki <arawn.annwn@yandex.com>
2018-05-19 17:48:14 -0400

    commands to modify skill level and craft ranks.

--- scripts/globals/status.lua
+++ scripts/globals/status.lua
@@ -2233,6 +2233,31 @@ dsp.skill =
 };
 
 ------------------------------------
+-- Craft Skill Ranks
+------------------------------------
+
+dsp.craftRank =
+{
+    AMATEUR     = 0,
+    RECRUIT     = 1,
+    INITIATE    = 2,
+    NOVICE      = 3,
+    APPRENTICE  = 4,
+    JOURNEYMAN  = 5,
+    CRAFTSMAN   = 6,
+    ARTISAN     = 7,
+    ADEPT       = 8,
+    VETERAN     = 9,
+    EXPERT      = 10,
+    AUTHORITY   = 11,
+    LUMINARY    = 12,
+    MASTER      = 13,
+    GRANDMASTER = 14,
+    LEGEND      = 15
+    -- 16+ invalid
+}
+
+------------------------------------
 -- Recast IDs
 ------------------------------------
 
