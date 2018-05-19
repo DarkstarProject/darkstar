e6de7706ad5bcd9b886d0b4baed5151e13741203
TeoTwawki <arawn.annwn@yandex.com>
2018-05-19 15:51:57 -0400

    remove semicolon..

--- scripts/zones/The_Garden_of_RuHmet/mobs/Jailer_of_Faith.lua
+++ scripts/zones/The_Garden_of_RuHmet/mobs/Jailer_of_Faith.lua
@@ -2,7 +2,7 @@
 -- Area: The Garden of Ru'Hmet
 --   NM: Jailer of Faith
 -----------------------------------
-mixins = {require("scripts/mixins/job_special")};
+mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 
 function onMobSpawn(mob)
