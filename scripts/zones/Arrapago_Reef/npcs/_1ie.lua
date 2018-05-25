a569abf4ca95f2f07a786870457f2a3a52f21d4b
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-05-26 07:06:52 +0900

    Updates to Iron Gate (Lamian Key)
    Using npc_utils for a more 'lenient' trade as retail does it.
    each THF key still retains its own "if" for individual percentage chances based on what is used for when we get that figured out.
    one day when i'm bored with nothing to do, maybe I can pick locks for a few days.

--- scripts/zones/Arrapago_Reef/npcs/_1ie.lua
+++ scripts/zones/Arrapago_Reef/npcs/_1ie.lua
@@ -6,61 +6,59 @@
 package.loaded["scripts/zones/Arrapago_Reef/TextIDs"] = nil
 -----------------------------------
 require("scripts/zones/Arrapago_Reef/TextIDs")
+require("scripts/globals/npc_util")
 require("scripts/globals/status")
+-----------------------------------
 
 function onTrade(player,npc,trade)
-    if npc:getAnimation() == ANIMATION_CLOSE_DOOR then
-        if trade:hasItemQty(2219,1) and trade:getItemCount() == 1 then
-            npc:openDoor(ANIMATION_OPEN_DOOR)
+    if npc:getAnimation() == dsp.anim.CLOSE_DOOR then
+        if npcUtil.tradeHas(trade, 2219) then
+            npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             player:showText(npc,KEY_BREAKS,2219)
-            player:tradeComplete()
-        elseif trade:hasItemQty(1022,1) and trade:getItemCount() == 1 and player:getMainJob() == dsp.job.THF then -- thief's tools
+            player:confirmTrade()
+        elseif npcUtil.tradeHas(trade, 1022) and player:getMainJob() == dsp.job.THF then -- thief's tools
             if math.random(1,2) == 1 then -- TODO: figure out actual percentage chance to pick locks; 50% for now
                 player:showText(npc,LOCK_SUCCESS,1022)
-                npc:openDoor(ANIMATION_OPEN_DOOR)
+                npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             else
                 player:showText(npc,LOCK_FAIL,1022)
             end
-            player:tradeComplete()
-        elseif trade:hasItemQty(1023,1) and trade:getItemCount() == 1 and player:getMainJob() == dsp.job.THF then -- living key
+            player:confirmTrade()
+        elseif npcUtil.tradeHas(trade, 1023) and player:getMainJob() == dsp.job.THF then -- living key
             if math.random(1,2) == 1 then -- TODO: figure out actual percentage chance to pick locks; 50% for now
                 player:showText(npc,LOCK_SUCCESS,1023)
-                npc:openDoor(ANIMATION_OPEN_DOOR)
+                npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             else
                 player:showText(npc,LOCK_FAIL,1023)
             end
-            player:tradeComplete()
-        elseif trade:hasItemQty(1115,1) and trade:getItemCount() == 1 and player:getMainJob() == dsp.job.THF then -- skeleton key
+            player:confirmTrade()
+        elseif npcUtil.tradeHas(trade, 1115) and player:getMainJob() == dsp.job.THF then -- skeleton key
             if math.random(1,2) == 1 then -- TODO: figure out actual percentage chance to pick locks; 50% for now
                 player:showText(npc,LOCK_SUCCESS,1115)
-                npc:openDoor(ANIMATION_OPEN_DOOR)
+                npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             else
                 player:showText(npc,LOCK_FAIL,1115)
             end
-            player:tradeComplete()
+            player:confirmTrade()
         end
     end
 end
 
 function onTrigger(player,npc)
-    if player:getZPos() < 120 and npc:getAnimation() == ANIMATION_CLOSE_DOOR then
+    if player:getZPos() < 120 and npc:getAnimation() == dsp.anim.CLOSE_DOOR then
         if player:getMainJob() == dsp.job.THF then
             player:showText(npc,DOOR_IS_LOCKED2, 2219, 1022) -- message only THF's get
         else
             player:showText(npc,DOOR_IS_LOCKED, 2219)
         end
-    elseif player:getZPos() >= 120 and npc:getAnimation() == ANIMATION_CLOSE_DOOR then
+    elseif player:getZPos() >= 120 and npc:getAnimation() == dsp.anim.CLOSE_DOOR then
         player:messageSpecial(YOU_UNLOCK_DOOR) -- message from "inside" of door
-        npc:openDoor(ANIMATION_OPEN_DOOR)
+        npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
     end
 end
 
 function onEventUpdate(player,csid,option,target)
-    -- printf("CSID: %u",csid)
-    -- printf("RESULT: %u",option)
 end
 
 function onEventFinish(player,csid,option,target)
-    -- printf("CSID: %u",csid)
-    -- printf("RESULT: %u",option)
 end
