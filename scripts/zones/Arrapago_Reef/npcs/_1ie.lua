2ceaadca627d30f3cc40f76431ae20ac8c50adbf
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-05-25 18:03:46 -0500

    change all to messageSpecial reflecting ID change

--- scripts/zones/Arrapago_Reef/npcs/_1ie.lua
+++ scripts/zones/Arrapago_Reef/npcs/_1ie.lua
@@ -14,30 +14,30 @@ function onTrade(player,npc,trade)
     if npc:getAnimation() == dsp.anim.CLOSE_DOOR then
         if npcUtil.tradeHas(trade, 2219) then
             npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
-            player:showText(npc,KEY_BREAKS,2219)
+            player:messageSpecial(KEY_BREAKS,2219)
             player:confirmTrade()
         elseif npcUtil.tradeHas(trade, 1022) and player:getMainJob() == dsp.job.THF then -- thief's tools
             if math.random(1,2) == 1 then -- TODO: figure out actual percentage chance to pick locks; 50% for now
-                player:showText(npc,LOCK_SUCCESS,1022)
+                player:messageSpecial(LOCK_SUCCESS,1022)
                 npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             else
-                player:showText(npc,LOCK_FAIL,1022)
+                player:messageSpecial(LOCK_FAIL,1022)
             end
             player:confirmTrade()
         elseif npcUtil.tradeHas(trade, 1023) and player:getMainJob() == dsp.job.THF then -- living key
             if math.random(1,2) == 1 then -- TODO: figure out actual percentage chance to pick locks; 50% for now
-                player:showText(npc,LOCK_SUCCESS,1023)
+                player:messageSpecial(LOCK_SUCCESS,1023)
                 npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             else
-                player:showText(npc,LOCK_FAIL,1023)
+                player:messageSpecial(LOCK_FAIL,1023)
             end
             player:confirmTrade()
         elseif npcUtil.tradeHas(trade, 1115) and player:getMainJob() == dsp.job.THF then -- skeleton key
             if math.random(1,2) == 1 then -- TODO: figure out actual percentage chance to pick locks; 50% for now
-                player:showText(npc,LOCK_SUCCESS,1115)
+                player:messageSpecial(LOCK_SUCCESS,1115)
                 npc:openDoor(dsp.anim.ANIMATION_OPEN_DOOR)
             else
-                player:showText(npc,LOCK_FAIL,1115)
+                player:messageSpecial(LOCK_FAIL,1115)
             end
             player:confirmTrade()
         end
@@ -47,9 +47,9 @@ end
 function onTrigger(player,npc)
     if player:getZPos() < 120 and npc:getAnimation() == dsp.anim.CLOSE_DOOR then
         if player:getMainJob() == dsp.job.THF then
-            player:showText(npc,DOOR_IS_LOCKED2, 2219, 1022) -- message only THF's get
+            player:messageSpecial(DOOR_IS_LOCKED2, 2219, 1022) -- message only THF's get
         else
-            player:showText(npc,DOOR_IS_LOCKED, 2219)
+            player:messageSpecial(DOOR_IS_LOCKED, 2219)
         end
     elseif player:getZPos() >= 120 and npc:getAnimation() == dsp.anim.CLOSE_DOOR then
         player:messageSpecial(YOU_UNLOCK_DOOR) -- message from "inside" of door
