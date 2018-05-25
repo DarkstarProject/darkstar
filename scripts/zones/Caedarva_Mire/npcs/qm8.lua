4c6c865b91f378772761991f6e6134de26dbda45
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-05-26 07:13:23 +0900

    Minor updates for qm8 (Lamian Key)
    npcutils works here but still does not give retail message. 
    npcutils ALSO complets the if statement even though the condition is not met (key not given) and causes the timer to reset so that you cannot get another key until the next day. This is NOT retail behavior.
    the way it is scripted now is retail accurate with messages as well as timer resets.
    i don't see any reason to script it two different ways, one for dsp and one for personal use to maintain retail accuracy just to say we are using the shiny new tools.

--- scripts/zones/Caedarva_Mire/npcs/qm8.lua
+++ scripts/zones/Caedarva_Mire/npcs/qm8.lua
@@ -6,16 +6,19 @@
 package.loaded["scripts/zones/Caedarva_Mire/TextIDs"] = nil
 -----------------------------------
 require("scripts/zones/Caedarva_Mire/TextIDs")
+-----------------------------------
 
 function onTrade(player,npc,trade)
 end
 
 function onTrigger(player,npc)
-    if player:getVar("[TIMER]Lamian_Fang_Key") ~= VanadielDayOfTheYear() then
+    local fangKeyTimer = player:getVar("[TIMER]Lamian Fang Key")
+
+    if fangKeyTimer < os.time() then
         if player:getFreeSlotsCount() > 0 and player:hasItem(2219) == false then
             player:addItem(2219,1)
             player:messageSpecial(ITEM_OBTAINED,2219)
-            player:setVar("[TIMER]Lamian_Fang_Key", VanadielDayOfTheYear()) -- Can obtain key once per vanadiel day
+            player:setVar("[TIMER]Lamian Fang Key",getConquestTally())
         else
             player:messageSpecial(ITEM_CANNOT_BE_OBTAINED,2219)
         end
@@ -25,11 +28,7 @@ function onTrigger(player,npc)
 end
 
 function onEventUpdate(player,csid,option)
-    -- printf("CSID: %u",csid)
-    -- printf("RESULT: %u",option)
 end
 
 function onEventFinish(player,csid,option)
-    -- printf("CSID: %u",csid)
-    -- printf("RESULT: %u",option)
 end
