c74f1c8130efaa9cb4d9d93e3564857969ac52fe
Kain <26943220+kaincenteno@users.noreply.github.com>
2018-05-26 12:14:17 -0700

    added desynth recipe for Cotton Brais

--- sql/synth_recipes.sql
+++ sql/synth_recipes.sql
@@ -4647,6 +4647,7 @@ INSERT INTO `synth_recipes` VALUES (4532,0,0,0,0,0,0,0,33,0,0,4100,4242,13571,0,
 INSERT INTO `synth_recipes` VALUES (4533,0,0,0,0,46,0,0,0,0,0,4100,4242,12723,0,0,0,0,0,0,0,834,820,820,820,2,5,6,7); -- Wool Bracers (desynth)
 INSERT INTO `synth_recipes` VALUES (4534,0,0,0,0,0,0,31,0,0,0,4100,4242,18868,0,0,0,0,0,0,0,650,0,0,0,1,0,0,0); -- Lady Bell (desynth)
 INSERT INTO `synth_recipes` VALUES (4535,0,0,0,0,0,0,85,0,0,0,4100,4242,13356,0,0,0,0,0,0,0,746,812,0,0,1,1,0,0); -- Death Earring (desynth)
+INSERT INTO `synth_recipes` VALUES (4536,0,0,0,0,19,0,0,0,0,0,4100,4242,12849,0,0,0,0,0,0,0,850,818,818,818,1,5,6,7); -- Cotton Brais (desynth)
 
 -- INSERT INTO `synth_recipes` VALUES (ID,Type,KeyItem,AL,BO,CL,CK,GO,LE,SM,WD,Crystal,HQCrystal,I1,I2,I3,I4,I5,I6,I7,I8,R1,R2,R3,R4,Q1,Q2,Q3,Q4); -- template
 -- crystals = fire(4096,4238) ice(4097,4239) wind(4098,4240) earth(4099,4241) lightning(4100,4242) water(4101,4243) light(4102,4244) dark(4103,4245)
