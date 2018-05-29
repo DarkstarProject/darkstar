8484ad13855740b196fd8075f2700b8dd4daed5f
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-05-29 16:11:19 -0500

    handle the varied speed bonus based on time

--- scripts/globals/effects/costume.lua
+++ scripts/globals/effects/costume.lua
@@ -7,7 +7,13 @@
 function onEffectGain(target,effect)
     target:costume(effect:getPower())
     if effect:getPower() == 1999 then
-        target:speed(target:speed()+10) 
+        if effect:getTimeRemaining() / 1000 >= 7170 then
+            target:speed(target:speed()+5)
+            effect:setPower(19991)
+        else
+            target:speed(target:speed()+10)
+            effect:setPower(19992)
+        end
     end
 end
 
@@ -16,7 +22,9 @@ end
 
 function onEffectLose(target,effect)
     target:costume(0)
-    if effect:getPower() == 1999 then
+    if effect:getPower() == 19991 then
+        target:speed(target:speed()-5)
+    elseif effect:getPower() == 19992 then
         target:speed(target:speed()-10)
     end
 end
