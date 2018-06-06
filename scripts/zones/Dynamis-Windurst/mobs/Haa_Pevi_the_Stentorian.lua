e0d34805e2da273f3b0b41db0ff60c0502b5eae6
David Norris <KnowOne134@gmail.com>
2018-06-05 19:54:02 -0500

    Dynamis Windy job spec

--- scripts/zones/Dynamis-Windurst/mobs/Haa_Pevi_the_Stentorian.lua
+++ scripts/zones/Dynamis-Windurst/mobs/Haa_Pevi_the_Stentorian.lua
@@ -5,6 +5,14 @@
 -----------------------------------
 
 function onMobSpawn(mob)
+    mob:setLocalVar("HpTrigger", math.random(40,65));
+end;
+
+function onMobFight(mob,target)
+    if (mob:getHPP() < mob:getLocalVar("HpTrigger") and mob:getLocalVar("astral_flow") == 0) then
+        mob:useMobAbility(1023);
+        mob:setLocalVar("astral_flow", 1);
+    end
 end;
 
 function onMobDeath(mob, player, isKiller)
--- scripts/zones/Dynamis-Windurst/mobs/Vanguard_Skirmisher.lua
+++ scripts/zones/Dynamis-Windurst/mobs/Vanguard_Skirmisher.lua
@@ -1,9 +1,7 @@
 -----------------------------------
 -- Area: Dynamis Windurst
---  NPC: Loo Hepe the Eyepiercer
--- Boss Trigger for MEGA BOSS
+--  NPC: Vanguard Skirmisher
 -----------------------------------
-
 mixins = {require("scripts/mixins/job_special")};
 require("scripts/globals/status");
 
@@ -14,11 +12,4 @@ function onMobDeath(mob, player, isKiller)
 end;
 
 function onMobDespawn(mob)
-
-    -- If last boss trigger is killed -> pop the boss
-    if (GetServerVariable("[DynaWindurst]Boss_Trigger") == 8) then
-        SpawnMob(17543169);
-        SetServerVariable("[DynaWindurst]Boss_Trigger",16);
-    end
-
 end;
