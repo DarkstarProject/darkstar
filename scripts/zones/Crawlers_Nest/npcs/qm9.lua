ce4194a1c6f3e31e0b25701596d39bc71e220485
wrenffxi <oliviapekoe@gmail.com>
2018-06-13 19:24:51 -0400

    CNest and Zvahl Baileys IDs

--- scripts/zones/Crawlers_Nest/npcs/qm9.lua
+++ scripts/zones/Crawlers_Nest/npcs/qm9.lua
@@ -4,40 +4,33 @@
 -- Used In Quest: A Boy's Dream
 -- !pos -18 -8 124 197
 -----------------------------------
-package.loaded["scripts/zones/Crawlers_Nest/TextIDs"] = nil;
+package.loaded["scripts/zones/Crawlers_Nest/TextIDs"] = nil
 -----------------------------------
-require("scripts/globals/settings");
-require("scripts/zones/Crawlers_Nest/TextIDs");
+require("scripts/zones/Crawlers_Nest/TextIDs")
+require("scripts/zones/Crawlers_Nest/MobIDs")
+require("scripts/globals/npc_util")
+require("scripts/globals/quests")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
-    local DreadbugTimer = player:getVar("DreadbugNM_Timer");
-    local DreadbugDay = player:getVar("DreadbugNM_Day");
-    local MyDay = VanadielDayOfTheYear();
-    local aBoysDream = player:getQuestStatus(SANDORIA, A_BOY_S_DREAM);
-
-    if (MyDay ~= DreadbugDay and aBoysDream == QUEST_ACCEPTED) then
-        local canSpawn = (os.time() - DreadbugTimer) > 30;
-
-        if (canSpawn) then
-            SpawnMob(17584425):updateClaim(player); -- Despawn after 3 minutes (-12 seconds for despawn delay).
-            player:setVar("DreadbugNM_Timer",os.time()+180);
-            player:setVar("DreadbugNM_Day",VanadielDayOfTheYear());
-            player:messageSpecial(SENSE_OF_FOREBODING);
+    if player:getQuestStatus(SANDORIA, A_BOY_S_DREAM) == QUEST_ACCEPTED and VanadielDayOfTheYear() ~= player:getVar("DreadbugNM_Day") then
+        if os.time() > player:getVar("DreadbugNM_Timer") + 30 and npcUtil.popFromQM(player, npc, DREADBUG, true, false) then
+            player:messageSpecial(SENSE_OF_FOREBODING)
+            player:setVar("DreadbugNM_Timer", os.time() + 180)
+            player:setVar("DreadbugNM_Day", VanadielDayOfTheYear())
         else
-            player:messageSpecial(NOTHING_SEEMS_TO_HAPPEN);
+            player:messageSpecial(NOTHING_SEEMS_TO_HAPPEN)
         end
     else
-        player:messageSpecial(NOTHING_WILL_HAPPEN_YET);
+        player:messageSpecial(NOTHING_WILL_HAPPEN_YET)
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-end;
-
+end
