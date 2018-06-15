4032e41558d19f4635e6718b0f356cbab1c34c09
Zynjec <zynjec@gmail.com>
2018-06-15 00:49:35 -0400

    Cleaned up NPC Shops (B)

--- scripts/zones/Bastok_Mines/npcs/Galdeo.lua
+++ scripts/zones/Bastok_Mines/npcs/Galdeo.lua
@@ -1,37 +1,36 @@
 -----------------------------------
 -- Area: Bastok Mines
 --  NPC: Galdeo
--- Only sells when Bastok controlls Li'Telor Region
+--  Li'Telor Regional Merchant
 -----------------------------------
-package.loaded["scripts/zones/Bastok_Mines/TextIDs"] = nil;
+package.loaded["scripts/zones/Bastok_Mines/TextIDs"] = nil
 -----------------------------------
-require("scripts/zones/Bastok_Mines/TextIDs");
-require("scripts/globals/conquest");
-require("scripts/globals/shop");
------------------------------------
+require("scripts/zones/Bastok_Mines/TextIDs")
+require("scripts/globals/conquest")
+require("scripts/globals/shop")
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
     local RegionOwner = GetRegionOwner(dsp.region.LITELOR);
-    if (RegionOwner ~= dsp.nation.BASTOK) then
-        player:showText(npc,GALDEO_CLOSED_DIALOG);
+
+    if RegionOwner ~= dsp.nation.BASTOK then
+        player:showText(npc, GALDEO_CLOSED_DIALOG)
     else
-        player:showText(npc,GALDEO_OPEN_DIALOG);
         local stock =
         {
-            623,    119, -- Bay Leaves
-            4154,  6440  -- Holy Water
+            623,   119,    -- Bay Leaves
+            4154, 6440     -- Holy Water
         }
-        dsp.shop.general(player, stock, BASTOK);
 
+        player:showText(npc, GALDEO_OPEN_DIALOG)
+        dsp.shop.general(player, stock, BASTOK)
     end
-
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-end;
+end
