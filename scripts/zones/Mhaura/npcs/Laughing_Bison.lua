6018ff89e8d666653df5f830f409d0c60baa581e
Caladius <Caladius@users.noreply.github.com>
2018-06-18 15:56:56 -0400

    Removed dumb tabs and localized Vars

--- scripts/zones/Mhaura/npcs/Laughing_Bison.lua
+++ scripts/zones/Mhaura/npcs/Laughing_Bison.lua
@@ -12,12 +12,12 @@ function onTrade(player,npc,trade)
 end;
 
 function onTrigger(player,npc)
-	-- Each boat comes every 1152 seconds/8 game hours, 4 hour offset between Selbina and Aht Urghan
+    -- Each boat comes every 1152 seconds/8 game hours, 4 hour offset between Selbina and Aht Urghan
     -- Original timer: local timer = 1152 - ((os.time() - 1009810584)%1152);
-   timer = 1152 - ((os.time() - 1009810802)%1152);
-   destination = 0; -- Selbina, set to 1 for Al Zhabi
-   direction = 0; -- Arrive, 1 for depart
-   waiting = 216; -- Offset for Selbina
+    local timer = 1152 - ((os.time() - 1009810802)%1152);
+    local destination = 0; -- Selbina, set to 1 for Al Zhabi
+    local direction = 0; -- Arrive, 1 for depart
+    local waiting = 216; -- Offset for Selbina
 
    -- Next ferry is Al Zhabi for higher values.
     if (timer >= 576) then
