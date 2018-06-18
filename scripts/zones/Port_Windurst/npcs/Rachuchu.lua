43d25b3d87dbd0cbeed4cc8b7cb315d9691d50b0
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-06-18 15:11:37 -0500

    Add magic animations to taru students in Orastery

--- scripts/zones/Port_Windurst/npcs/Rachuchu.lua
+++ scripts/zones/Port_Windurst/npcs/Rachuchu.lua
@@ -3,18 +3,24 @@
 --  NPC: Rachuchu
 -- Working 100%
 -----------------------------------
-require("scripts/globals/settings");
+require("scripts/globals/npc_util")
+require("scripts/globals/settings")
+require("scripts/globals/magic")
 -----------------------------------
 
+function onSpawn(npc)
+    npcUtil.castingAnimation(npc, dsp.magic.spellGroup.WHITE, 10.5)
+end
+
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
-    player:startEvent(234);
-end;
+    player:startEvent(234)
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-end;
+end
