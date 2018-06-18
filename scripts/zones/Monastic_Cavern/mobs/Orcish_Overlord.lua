ea68cd340e0ffb16d3d2ddb521ca8598bfee0282
wrenffxi <oliviapekoe@gmail.com>
2018-06-18 19:00:21 -0400

    spawn mechanics, 2hrs, and additional effects for beastman kings

--- scripts/zones/Monastic_Cavern/mobs/Orcish_Overlord.lua
+++ scripts/zones/Monastic_Cavern/mobs/Orcish_Overlord.lua
@@ -1,31 +1,31 @@
 -----------------------------------
--- Area: Qulun_Dome
---   NM: Diamond_Quadav
--- Note: PH for Za Dha Adamantking PH
+-- Area: Monastic Cavern
+--   NM: Orcish Overlord
+-- Note: PH for Overlord Bakgodek
 -- TODO: messages should be zone-wide
 -----------------------------------
-package.loaded["scripts/zones/Qulun_Dome/TextIDs"] = nil
+package.loaded["scripts/zones/Monastic_Cavern/TextIDs"] = nil
 -----------------------------------
 mixins = {require("scripts/mixins/job_special")}
-require("scripts/zones/Qulun_Dome/TextIDs")
-require("scripts/zones/Qulun_Dome/MobIDs")
+require("scripts/zones/Monastic_Cavern/TextIDs")
+require("scripts/zones/Monastic_Cavern/MobIDs")
 require("scripts/globals/status")
 -----------------------------------
 
 function onMobInitialize(mob)
     -- the quest version of this NM doesn't drop gil
-    if mob:getID() >= AFFABLE_ADAMANTKING_OFFSET then
+    if (mob:getID() >= UNDERSTANDING_OVERLORD_OFFSET) then
         mob:setMobMod(dsp.mobMod.GIL_MAX, -1)
     end
 end
 
 function onMobEngaged(mob,target)
-    mob:showText(mob, DIAMOND_QUADAV_ENGAGE)
+    mob:showText(mob, ORCISH_OVERLORD_ENGAGE)
 end
 
 function onMobDeath(mob, player, isKiller)
     if isKiller then
-        mob:showText(mob, DIAMOND_QUADAV_DEATH)
+        mob:showText(mob, ORCISH_OVERLORD_DEATH)
     end
 end
 
@@ -33,21 +33,21 @@ function onMobDespawn(mob)
     local nqId = mob:getID()
 
     -- the quest version of this NM doesn't respawn or count toward hq nm
-    if nqId == DIAMOND_QUADAV then
+    if nqId == ORCISH_OVERLORD then
         local hqId = mob:getID() + 1
-        local ToD = GetServerVariable("[POP]Za_Dha_Adamantking")
-        local kills = GetServerVariable("[PH]Za_Dha_Adamantking")
+        local ToD = GetServerVariable("[POP]Overlord_Bakgodek")
+        local kills = GetServerVariable("[PH]Overlord_Bakgodek")
         local popNow = (math.random(1,5) == 3 or kills > 6)
 
         if os.time() > ToD and popNow then
             DisallowRespawn(nqId, true)
             DisallowRespawn(hqId, false)
             UpdateNMSpawnPoint(hqId)
-            GetMobByID(hqId):setRespawnTime(math.random(75600,86400))
+            GetMobByID(hqId):setRespawnTime(math.random(75600, 86400))
         else
             UpdateNMSpawnPoint(nqId)
-            mob:setRespawnTime(math.random(75600,86400))
-            SetServerVariable("[PH]Za_Dha_Adamantking", kills + 1)
+            mob:setRespawnTime(math.random(75600, 86400))
+            SetServerVariable("[PH]Overlord_Bakgodek", kills + 1)
         end
     end
 end
--- scripts/zones/Qulun_Dome/MobIDs.lua
+++ scripts/zones/Qulun_Dome/MobIDs.lua
@@ -1,2 +1,6 @@
-DIAMOND_QUADAV              = 17383442;
-AFFABLE_ADAMANTKING_OFFSET  = 17383444;
+-----------------------------------
+-- Area: Qulun Dome (148)
+-----------------------------------
+
+DIAMOND_QUADAV              = 17383442
+AFFABLE_ADAMANTKING_OFFSET  = 17383444
