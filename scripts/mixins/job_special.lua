e7416de63a648e0e76b02808052a5862ccd10837
wrenffxi <oliviapekoe@gmail.com>
2018-06-21 09:31:23 -0400

    style guide the job_special mixin

--- scripts/mixins/job_special.lua
+++ scripts/mixins/job_special.lua
@@ -10,18 +10,18 @@ mainQuote           0           special message displayed when mob uses main spe
 subQuote            0           special message displayed when mob uses sub special.
 --]]
 
-require("scripts/globals/mixins");
-require("scripts/globals/status");
+require("scripts/globals/mixins")
+require("scripts/globals/status")
 
-g_mixins = g_mixins or {};
+g_mixins = g_mixins or {}
 
 g_mixins.job_special = function(mob)
 
     mob:addListener("SPAWN", "JOB_SPECIAL_SPAWN", function(mob)
-        mob:setLocalVar("useSpecAtHpMin", 40);
-        mob:setLocalVar("useSpecAtHpMax", 60);
-        mob:setLocalVar("useMainSpecAtHPP", math.random(40,60));
-    end);
+        mob:setLocalVar("useSpecAtHpMin", 40)
+        mob:setLocalVar("useSpecAtHpMax", 60)
+        mob:setLocalVar("useMainSpecAtHPP", math.random(40,60))
+    end)
     
     mob:addListener("COMBAT_TICK", "JOB_SPECIAL_CTICK", function(mob)
         local defaultAbility = {
@@ -39,53 +39,52 @@ g_mixins.job_special = function(mob)
             [dsp.job.NIN] = dsp.jsa.MIJIN_GAKURE,
             [dsp.job.DRG] = dsp.jsa.CALL_WYVERN,
             [dsp.job.SMN] = dsp.jsa.ASTRAL_FLOW,
-        };
+        }
 
-        if (mob:getLocalVar("usedMainSpec") == 0 and mob:getHPP() < mob:getLocalVar("useMainSpecAtHPP") and mob:getBattleTime() > mob:getLocalVar("waitJobSpec")) then
-            local default = defaultAbility[mob:getMainJob()];
-            local spec = mob:getLocalVar("mainSpec");
-            local quote = mob:getLocalVar("mainQuote");
-            if (quote > 0) then
+        if mob:getLocalVar("usedMainSpec") == 0 and mob:getHPP() < mob:getLocalVar("useMainSpecAtHPP") and mob:getBattleTime() >= mob:getLocalVar("waitJobSpec") then
+            local default = defaultAbility[mob:getMainJob()]
+            local spec = mob:getLocalVar("mainSpec")
+            local quote = mob:getLocalVar("mainQuote")
+            if quote > 0 then
                 mob:messageText(mob, quote)
             end
-            if (spec > 0) then
-                mob:useMobAbility(spec);
-            elseif (default ~= nil) then
-                mob:useMobAbility(default);
+            if spec > 0 then
+                mob:useMobAbility(spec)
+            elseif default ~= nil then
+                mob:useMobAbility(default)
             end
-            mob:setLocalVar("usedMainSpec", 1);
+            mob:setLocalVar("usedMainSpec", 1)
         end
 
-        if (mob:getLocalVar("usedSubSpec") == 0 and mob:getHPP() < mob:getLocalVar("useSubSpecAtHPP") and mob:getBattleTime() > mob:getLocalVar("waitJobSpec")) then
-            local default = defaultAbility[mob:getSubJob()];
-            local spec = mob:getLocalVar("subSpec");
-            local quote = mob:getLocalVar("subQuote");
-            if (quote > 0) then
+        if mob:getLocalVar("usedSubSpec") == 0 and mob:getHPP() < mob:getLocalVar("useSubSpecAtHPP") and mob:getBattleTime() >= mob:getLocalVar("waitJobSpec") then
+            local default = defaultAbility[mob:getSubJob()]
+            local spec = mob:getLocalVar("subSpec")
+            local quote = mob:getLocalVar("subQuote")
+            if quote > 0 then
                 mob:messageText(mob, quote)
             end
-            if (spec > 0) then
-                mob:useMobAbility(spec);
-            elseif (default ~= nil) then
-                mob:useMobAbility(default);
+            if spec > 0 then
+                mob:useMobAbility(spec)
+            elseif default ~= nil then
+                mob:useMobAbility(default)
             end
-            mob:setLocalVar("usedSubSpec", 1);
+            mob:setLocalVar("usedSubSpec", 1)
         end
-
-    end);
+    end)
 
     mob:addListener("DISENGAGE", "JOB_SPECIAL_DISENGAGE", function(mob, target)
-        mob:setLocalVar("usedMainSpec", 0);
-        mob:setLocalVar("usedSubSpec", 0);
+        mob:setLocalVar("usedMainSpec", 0)
+        mob:setLocalVar("usedSubSpec", 0)
 
-        local hpMin =  mob:getLocalVar("useSpecAtHpMin");
-        local hpMax =  mob:getLocalVar("useSpecAtHpMax");
+        local hpMin =  mob:getLocalVar("useSpecAtHpMin")
+        local hpMax =  mob:getLocalVar("useSpecAtHpMax")
   
-        mob:setLocalVar("useMainSpecAtHPP", math.random(hpMin,hpMax));
-        if (mob:getLocalVar("useSubSpecAtHPP") > 0) then
-            mob:setLocalVar("useSubSpecAtHPP", math.random(hpMin,hpMax));
+        mob:setLocalVar("useMainSpecAtHPP", math.random(hpMin,hpMax))
+        if mob:getLocalVar("useSubSpecAtHPP") > 0 then
+            mob:setLocalVar("useSubSpecAtHPP", math.random(hpMin,hpMax))
         end
-    end);
+    end)
     
 end
 
-return g_mixins.job_special;
+return g_mixins.job_special
