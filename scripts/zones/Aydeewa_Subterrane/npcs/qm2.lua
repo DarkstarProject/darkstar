99c91102b811712509aa900766318d04400fe229
wrenffxi <oliviapekoe@gmail.com>
2018-06-23 12:35:41 -0400

    Aydeewa Subterrane

--- scripts/zones/Aydeewa_Subterrane/npcs/qm2.lua
+++ scripts/zones/Aydeewa_Subterrane/npcs/qm2.lua
@@ -3,18 +3,20 @@
 --  NPC: ??? (Spawn Pandemonium Warden)
 -- !pos 200 33 -140 68
 -----------------------------------
-package.loaded["scripts/zones/Aydeewa_Subterrane/TextIDs"] = nil;
+package.loaded["scripts/zones/Aydeewa_Subterrane/TextIDs"] = nil
 -----------------------------------
-require("scripts/zones/Aydeewa_Subterrane/TextIDs");
-require("scripts/zones/Aydeewa_Subterrane/MobIDs");
+require("scripts/zones/Aydeewa_Subterrane/TextIDs")
+require("scripts/zones/Aydeewa_Subterrane/MobIDs")
+require("scripts/globals/npc_util")
+-----------------------------------
 
 function onTrade(player,npc,trade)
-    if (not GetMobByID(PANDEMONIUM_WARDEN):isSpawned() and trade:hasItemQty(2572,1) and trade:getItemCount() == 1) then -- Pandemonium key
-        SpawnMob(PANDEMONIUM_WARDEN):updateClaim(player); -- Pandemonium Warden
-        player:tradeComplete();
+    if npcUtil.tradeHas(trade, 2572) and not GetMobByID(PANDEMONIUM_WARDEN):isSpawned() then -- Pandemonium Key
+        player:confirmTrade()
+        SpawnMob(PANDEMONIUM_WARDEN):updateClaim(player)
     end
-end;
+end
 
 function onTrigger(player,npc)
-    player:messageSpecial(NOTHING_HAPPENS);
-end;
+    player:messageSpecial(NOTHING_HAPPENS)
+end
