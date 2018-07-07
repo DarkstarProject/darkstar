3fb39f55ea965b2897146b2f82552183b2a4a74e
Zynjec <zynjec@gmail.com>
2018-07-07 14:22:08 -0400

    Fixed error in Luison's roam script.

--- scripts/zones/Abyssea-La_Theine/mobs/Luison.lua
+++ scripts/zones/Abyssea-La_Theine/mobs/Luison.lua
@@ -8,7 +8,7 @@ function onMobSpawn(mob)
 end;
 
 function onMobRoam(mob)
-    local spawnTime = mob:getLocalVar("transformTime");
+    local transformTime = mob:getLocalVar("transformTime");
     local roamChance = math.random(1,100);
     local roamMoonPhase = VanadielMoonPhase();
 
