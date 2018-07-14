db787749b90cbec3131baeaccfc281603c259805
wrenffxi <oliviapekoe@gmail.com>
2018-07-13 21:32:16 -0400

    Added Murrain Chigoe script

---
+++ scripts/zones/Abyssea-Attohwa/mobs/Murrain_Chigoe.lua
@@ -0,0 +1,9 @@
+-----------------------------------
+-- Area: Abyssea-Attohwa
+--  MOB: Murrain Chigoe
+-- Note: This mob does not need chigoe mixin
+-- TODO: En-Disease
+-----------------------------------
+
+function onMobDeath(mob, player, isKiller)
+end
