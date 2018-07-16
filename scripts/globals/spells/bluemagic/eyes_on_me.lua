b346e7589bc74c32006bdbac833ede5a2ba57b8e
Dynas13 <36946058+Dynas13@users.noreply.github.com>
2018-07-16 09:42:23 -0500

    Updated CHR modifiers
    Spell was pulling outdated information from ffxiclopedia. Changed the source info and CHR mod to .4 to reflect the 40% CHR modifier.

--- scripts/globals/spells/bluemagic/eyes_on_me.lua
+++ scripts/globals/spells/bluemagic/eyes_on_me.lua
@@ -32,7 +32,7 @@ end;
 function onSpellCast(caster,target,spell)
 
     local params = {};
-    -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
+    -- This data should match information on https://www.bg-wiki.com/bg/Calculating_Blue_Magic_Damage
     local multi = 2.625;
     if (caster:hasStatusEffect(dsp.effect.AZURE_LORE)) then
         multi = multi + 2.0;
@@ -46,7 +46,7 @@ function onSpellCast(caster,target,spell)
         params.agi_wsc = 0.0;
         params.int_wsc = 0.0;
         params.mnd_wsc = 0.0;
-        params.chr_wsc = 0.2;
+        params.chr_wsc = 0.4;
     damage = BlueMagicalSpell(caster, target, spell, params, CHR_BASED);
     damage = BlueFinalAdjustments(caster, target, spell, damage, params);
     
