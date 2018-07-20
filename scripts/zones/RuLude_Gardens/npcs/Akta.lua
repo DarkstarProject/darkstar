a9bba0769f54214987cebe96e1d015cb98b90a5d
Wiggo <30469395+Wiggo32@users.noreply.github.com>
2018-07-19 21:56:38 -0500

    Add Unlisted Qualities Quest

--- scripts/zones/RuLude_Gardens/npcs/Akta.lua
+++ scripts/zones/RuLude_Gardens/npcs/Akta.lua
@@ -1,20 +1,38 @@
 -----------------------------------
--- Area: Ru'Lud Gardens
---  NPC: Akta
--- Standard Info NPC
+-- Area: Ru'Lude Gardens
+-- !pos: 7 0 -67 243
 -----------------------------------
-require("scripts/globals/settings");
+package.loaded["scripts/zones/RuLude_Gardens/TextIDs"] = nil
+-----------------------------------
+require("scripts/zones/RuLude_Gardens/TextIDs")
+require("scripts/globals/quests")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
-    player:startEvent(116);
-end;
+    local FellowQuest = player:getVar("[Quest]Unlisted_Qualities")
+    local FellowName = player:getFellowNameId()
+    if player:getQuestStatus(JEUNO,UNLISTED_QUALITIES) == QUEST_ACCEPTED and player:getMaskBit(FellowQuest,0) == false then
+        player:startEvent(10103,0,0,0,0,0,0,0,FellowName)
+    else
+        player:startEvent(116)
+    end
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-end;
+    if csid == 10103 then
+        player:setMaskBit(player:getVar("[Quest]Unlisted_Qualities"),"[Quest]Unlisted_Qualities",0,true)
+        player:setFellowSize(option)
+    end
+--[[
+Adventuring Fellow Size Options:
+    3   Pretty tall
+    2   Around average
+    1   On the small side
+--]]
+end
