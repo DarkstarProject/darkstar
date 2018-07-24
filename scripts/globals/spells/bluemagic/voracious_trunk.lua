71c8c262337b4bd60476e778377503a98917973c
TeoTwawki <TeoTwawki@users.noreply.github.com>
2018-07-24 09:46:55 -0400

    avoid nil return.
    technically, zero is effect ko. but in this use that will never pass into an actual message.
    
    we really shouldn't have made `stealStatusEffect()` return 0 by default, but I don't want to mass replace all those `~= 0` checks and then handle the resulting nils in hundreds of scripts today..

--- scripts/globals/spells/bluemagic/voracious_trunk.lua
+++ scripts/globals/spells/bluemagic/voracious_trunk.lua
@@ -16,17 +16,18 @@ require("scripts/globals/status")
 require("scripts/globals/msg")
 -----------------------------------------
 
-function onMagicCastingCheck(caster,target,spell)
+function onMagicCastingCheck(caster, target, spell)
     return 0
 end
 
-function onSpellCast(caster,target,spell)
+function onSpellCast(caster, target, spell)
     local resist = applyResistanceAbility(caster, target, dsp.magic.ele.WIND, 0, 0)
     local StealChance = math.random(1,100)
-    
-    if (resist > 0.0625 and StealChance < 90) then
+    local stolen = 0
+
+    if resist > 0.0625 and StealChance < 90 then
         stolen = caster:stealStatusEffect(target)
-        if stolen ~=0 then
+        if stolen ~= 0 then
             spell:setMsg(dsp.msg.basic.MAGIC_STEAL);
         else
             spell:setMsg(dsp.msg.basic.MAGIC_NO_EFFECT);
