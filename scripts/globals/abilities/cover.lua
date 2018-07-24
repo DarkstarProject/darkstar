3451925b718ba6cab16efdd91dddf7644042c742
Dynas13 <36946058+Dynas13@users.noreply.github.com>
2018-07-24 13:31:55 -0500

    Update cover.lua

--- scripts/globals/abilities/cover.lua
+++ scripts/globals/abilities/cover.lua
@@ -7,14 +7,12 @@ function onAbilityCheck(player,target,ability)
 end;
 
 function onUseAbility(player,target,ability)
---formula from https://www.bg-wiki.com/bg/Cover
-local baseDuration = 15;
-local bonusTime = utils.clamp(math.floor((player:getMod(dsp.mod.VIT) + player:getMod(dsp.mod.MND) - target:getMod(dsp.mod.VIT) * 2) / 4), 0, 15)
-local duration = baseDuration + bonusTime;
-
-player:addStatusEffect(player,dsp.effect.COVER,0,0,duration)
-  if player:physicalDmgTaken() >= 0 then
+    --formula from https://www.bg-wiki.com/bg/Cover
+    local baseDuration = 15;
+    local bonusTime = utils.clamp(math.floor((player:getMod(dsp.mod.VIT) + player:getMod(dsp.mod.MND) - target:getMod(dsp.mod.VIT) * 2) / 4), 0, 15)
+    local duration = baseDuration + bonusTime;
+    
+    player:addStatusEffect(player,dsp.effect.COVER,0,0,duration)
     player.addMod(dsp.mod.Enmity, +200)
     target:lowerEnmity(target, 10)
-  end
-end
+end;
