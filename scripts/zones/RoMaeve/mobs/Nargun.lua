4abfa0faf2b8a007d81a9ff308249f7ba627dbc4
TeoTwawki <arawn.annwn@yandex.com>
2018-07-28 20:14:55 -0400

    Table mobmod..

--- scripts/zones/RoMaeve/mobs/Nargun.lua
+++ scripts/zones/RoMaeve/mobs/Nargun.lua
@@ -7,7 +7,7 @@ require("scripts/globals/magic")
 -----------------------------------
 
 function onMobInitialize(mob)
-    mob:setMobMod(MOBMOD_ADD_EFFECT, 1)
+    mob:setMobMod(dsp.mobMod.ADD_EFFECT, 1)
 end
 
 function onAdditionalEffect(mob,target,damage)
