e2ef6cbe760bdcb6d151a505d18772aec71cba23
WiggoCaptures <39559608+WiggoCaptures@users.noreply.github.com>
2018-07-30 16:15:27 -0500

    clean up hieroglyphics.lua ...see notes...
    -added event cs for DynaValkurm win
    -removed unused instance stuff
    -add support for 3 different cs's depending on dyna status by modifying checkFirstDyna which also condensed the elseif tree allowing use of only one cs event based on what the 2nd param returns.
    -cleaned up event params and defined what they are
    -added support for the KI that removed the "once a day" restriction from dynamis [RHAPSODY_IN_AZURE]

--- scripts/zones/Valkurm_Dunes/npcs/Hieroglyphics.lua
+++ scripts/zones/Valkurm_Dunes/npcs/Hieroglyphics.lua
@@ -4,68 +4,67 @@
 -- Dynamis Valkurm_Dunes Enter
 -- !pos 117 -10 133 103
 -----------------------------------
-package.loaded["scripts/zones/Valkurm_Dunes/TextIDs"] = nil;
+package.loaded["scripts/zones/Valkurm_Dunes/TextIDs"] = nil
 -----------------------------------
-require("scripts/globals/settings");
-require("scripts/globals/keyitems");
-require("scripts/globals/dynamis");
-require("scripts/globals/missions");
-require("scripts/zones/Valkurm_Dunes/TextIDs");
+require("scripts/zones/Valkurm_Dunes/TextIDs")
+require("scripts/globals/keyitems")
+require("scripts/globals/missions")
+require("scripts/globals/settings")
+require("scripts/globals/dynamis")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
-    if ((player:hasCompletedMission(COP,DARKNESS_NAMED) or FREE_COP_DYNAMIS == 1) and player:hasKeyItem(dsp.ki.VIAL_OF_SHROUDED_SAND)) then
-        local realDay = os.time();
-        local dynaWaitxDay = player:getVar("dynaWaitxDay");
-        local dynaUniqueID = GetServerVariable("[DynaValkurm]UniqueID");
+    if player:getVar("DynaValkurm_Win") == 1 then
+        player:startEvent(39)
+    elseif (player:hasCompletedMission(COP,DARKNESS_NAMED) or FREE_COP_DYNAMIS == 0) and player:hasKeyItem(dsp.ki.VIAL_OF_SHROUDED_SAND) then
+        local realDay = os.time()
+        local dynaWaitxDay = player:getVar("dynaWaitxDay")
 
-        if (checkFirstDyna(player,7)) then
-            player:startEvent(58,7,0x010002,0x060E,1,0x195,dsp.ki.VIAL_OF_SHROUDED_SAND,4236,4237);
-        elseif (player:getMainLvl() < DYNA_LEVEL_MIN) then
-            player:messageSpecial(PLAYERS_HAVE_NOT_REACHED_LEVEL,DYNA_LEVEL_MIN);
-        elseif ((dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 60 * 60)) < realDay or (player:getVar("DynamisID") == dynaUniqueID and dynaUniqueID > 0)) then
-            player:startEvent(58,7,0x010003,0x060E,1,0xFFFF5DC2,dsp.ki.VIAL_OF_SHROUDED_SAND,4236,4237);
+        if player:getMainLvl() < DYNA_LEVEL_MIN then
+            player:messageSpecial(PLAYERS_HAVE_NOT_REACHED_LEVEL,DYNA_LEVEL_MIN)
+        elseif (dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 60 * 60)) < realDay then
+--          Params: DynamisCity, CS option, KI, Support Job Option, junk, KI, Timeless Hrgls, Perpetual Hrgls
+            player:startEvent(58,7,checkFirstDyna(player,7),dsp.ki.PRISMATIC_HOURGLASS,1,0,dsp.ki.VIAL_OF_SHROUDED_SAND,4236,4237)
         else
-            dayRemaining = math.floor(((dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 60 * 60)) - realDay)/3456);
-            player:messageSpecial(YOU_CANNOT_ENTER_DYNAMIS,dayRemaining,7);
+            dayRemaining = math.floor(((dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 60 * 60)) - realDay)/3456)
+            player:messageSpecial(YOU_CANNOT_ENTER_DYNAMIS,dayRemaining,7)
         end
     else
-        player:messageSpecial(MYSTERIOUS_VOICE);
+        player:messageSpecial(MYSTERIOUS_VOICE)
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
-    -- printf("updateRESULT: %u",option);
-end;
+    -- printf("updateRESULT: %u",option)
+end
 
 function onEventFinish(player,csid,option)
-    -- printf("finishRESULT: %u",option);
+    -- printf("finishRESULT: %u",option)
      print(option)
 
-    if csid == 58 then
-        if (checkFirstDyna(player,7)) then
-            player:setVar("Dynamis_Status",bit.bor(player:getVar("Dynamis_Status"),128));
+    if csid == 39 then
+        player:setVar("DynaValkurm_Win", 0)
+    elseif csid == 58 then
+        if checkFirstDyna(player,7) then
+            player:setVar("Dynamis_Status",bit.bor(player:getVar("Dynamis_Status"),128))
         end
         if option == 0 or option == 1 then
-            if option == 1 then
-                player:setVar("Dynamis_subjob", 1)
-            else
-                player:setVar("Dynamis_subjob", 0)
-            end
+            player:setVar("Dynamis_subjob", option)
             player:setVar("Dynamis_Entry", 1)
-            local realDay = os.time();
+            local realDay = os.time()
             if (DYNA_MIDNIGHT_RESET == true) then
-                realDay = getMidnight() - 86400;
+                realDay = getMidnight() - 86400
             end
-            local dynaWaitxDay = player:getVar("dynaWaitxDay");
+            local dynaWaitxDay = player:getVar("dynaWaitxDay")
 
-            if ((dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 60 * 60)) < realDay) then
-                player:setVar("dynaWaitxDay",realDay);
+            if (dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 60 * 60)) < realDay
+                and not player:hasKeyItem(dsp.ki.RHAPSODY_IN_AZURE) then
+                    player:setVar("dynaWaitxDay",realDay)
             end
-            player:setPos(100,-8,131,47,0x27);
+            player:setPos(100,-8,131,47,0x27)
         end
     end
-end;
+end
