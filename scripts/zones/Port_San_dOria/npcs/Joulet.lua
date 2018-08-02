3a440096c8d7a01026c5a3436eadbc107d41a25d
Zynjec <zynjec@gmail.com>
2018-08-01 20:01:22 -0400

    Fixed NPC Text for Gallijaux and Joulet

--- scripts/zones/Port_San_dOria/npcs/Joulet.lua
+++ scripts/zones/Port_San_dOria/npcs/Joulet.lua
@@ -52,9 +52,9 @@ function onTrigger(player,npc)
     if (player:getQuestStatus(SANDORIA,THE_COMPETITION) == QUEST_AVAILABLE and player:getQuestStatus(SANDORIA,THE_RIVALRY) == QUEST_AVAILABLE) then -- If you haven't started either quest yet
         player:startEvent(304, 4401, 4289); -- Moat Carp = 4401, 4289 = Forest Carp
     elseif (player:getQuestStatus(SANDORIA,THE_RIVALRY) == QUEST_ACCEPTED) then
-        player:messageSpecial(JOULET_HELP_OTHER_BROTHER, 0, 0, 0, 0, true);
+        player:showText(npc, JOULET_HELP_OTHER_BROTHER);
     elseif ((player:getQuestStatus(SANDORIA,THE_COMPETITION)) == QUEST_ACCEPTED) then
-        player:messageSpecial(JOULET_CARP_STATUS, 0, player:getVar("theCompetitionFishCountVar"), 0, 0, true);
+        player:showText(npc, JOULET_CARP_STATUS, 0, player:getVar("theCompetitionFishCountVar"));
     end
 end;
 
