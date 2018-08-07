739489c7a3ef089dc0d9b5d27ba8cf906178ec89
EpicTaru <epicone@gmail.com>
2018-08-07 14:33:33 -0500

    -- Added Chocobo Jig II to abilities.sql -- Added script for the ability

---
+++ scripts/globals/abilities/chocobo_jig_ii.lua
@@ -0,0 +1,31 @@
+-----------------------------------
+-- Ability: Chocobo jig ii
+-- Increases Movement Speed to party members within the area of effect.
+-- Obtained: Dancer Level 70
+-- TP Required: 0
+-- Recast Time: 1:00
+-- Duration: 2:00
+-----------------------------------
+ 
+require("scripts/globals/settings");
+require("scripts/globals/status");
+
+-----------------------------------
+-- onAbilityCheck
+-----------------------------------
+
+function onAbilityCheck(player,target,ability)
+    return 0,0;
+end;
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onUseAbility(player,target,ability)
+    local baseDuration = 120;
+    local durationMultiplier = 1.0 + utils.clamp(player:getMod(dsp.mod.JIG_DURATION), 0, 50) / 100;
+    local finalDuration = math.floor(baseDuration * durationMultiplier);
+    
+    target:addStatusEffect(dsp.effect.QUICKENING,20,0,finalDuration);
+end;
