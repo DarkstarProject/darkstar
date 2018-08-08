657b6d051cc887fe1b74d7fc96637f239d2be47f
TeoTwawki <arawn.annwn@yandex.com>
2018-08-07 23:07:33 -0400

    Fix diaga.lua and impact.lua which were broken prior to my changes because somebody messed up params badly and the semicolon was all that stopped it from being all one "word" all on one line...
    https://github.com/DarkstarProject/darkstar/blob/9f44b184a2b25741da1e4024572a19610bbb535a/scripts/globals/spells/diaga.lua#L31
    
    impact actually did this ***three times***..........

--- scripts/globals/spells/diaga.lua
+++ scripts/globals/spells/diaga.lua
@@ -13,30 +13,33 @@ function onMagicCastingCheck(caster,target,spell)
 end
 
 function onSpellCast(caster,target,spell)
-
     -- calculate raw damage
     local basedmg = caster:getSkillLevel(dsp.skill.ENFEEBLING_MAGIC) / 4
+
     local params = {}
     params.dmg = basedmg
     params.multiplier = 1
     params.skillType = dsp.skill.ENFEEBLING_MAGIC
     params.attribute = dsp.mod.INT
     params.hasMultipleTargetReduction = false
+    params.diff = caster:getStat(dsp.mod.INT)-target:getStat(dsp.mod.INT)
+    params.attribute = dsp.mod.INT
+    params.skillType = dsp.skill.ENFEEBLING_MAGIC
+    params.bonus = 1.0
 
     local dmg = calculateMagicDamage(caster, target, spell, params)
-
     dmg = utils.clamp(dmg, 1, 12)
 
-    -- get resist multiplier (1x if no resist)
-    local params = {}params.diff = caster:getStat(dsp.mod.INT)-target:getStat(dsp.mod.INT)params.attribute = dsp.mod.INTparams.skillType = dsp.skill.ENFEEBLING_MAGICparams.bonus = 1.0resist = applyResistance(caster, target, spell, params)
-    -- get the resisted damage
+    -- Get resist multiplier (1x if no resist)
+    local resist = applyResistance(caster, target, spell, params)
+    -- Get the resisted damage
     dmg = dmg*resist
-    -- add on bonuses (staff/day/weather/jas/mab/etc all go in this function)
+    -- Add on bonuses (staff/day/weather/jas/mab/etc all go in this function)
     dmg = addBonuses(caster,spell,target,dmg)
-    -- add in target adjustment
+    -- Add in target adjustment
     dmg = adjustForTarget(target,dmg,spell:getElement())
 
-    -- add in final adjustments including the actual damage dealt
+    -- Add in final adjustments including the actual damage dealt
     local final = finalMagicAdjustments(caster,target,spell,dmg)
 
     -- Calculate duration.
@@ -66,5 +69,4 @@ function onSpellCast(caster,target,spell)
     end
 
     return final
-
 end
--- scripts/globals/spells/impact.lua
+++ scripts/globals/spells/impact.lua
@@ -12,14 +12,22 @@ function onMagicCastingCheck(caster,target,spell)
 end
 
 function onSpellCast(caster,target,spell)
-    local dINT = caster:getStat(dsp.mod.INT) - target:getStat(dsp.mod.INT)
     local params = {}
-    params.diff = nil
     params.attribute = dsp.mod.INT
-    params.skillType = 37
-    params.bonus = 0
+    params.bonus = 1.0
+    params.diff = caster:getStat(dsp.mod.INT)-target:getStat(dsp.mod.INT)
+    params.dmg = 939
     params.effect = nil
+    params.hasMultipleTargetReduction = false
+    params.multiplier = 2.335
+    params.resistBonus = 1.0
+    params.skillType = 37
+    params.skillType = dsp.skill.ELEMENTAL_MAGIC
+
     local resist = applyResistance(caster, target, spell, params)
+    local duration = 180 * resist -- BG wiki suggests only duration gets effected by resist, not stat amount.
+
+    -- Todo: loop to avoid repeatedly doing same thing for each stat
     local STR_Loss = ((target:getStat(dsp.mod.STR) / 100) * 20) -- Should be 20%
     local DEX_Loss = ((target:getStat(dsp.mod.DEX) / 100) * 20)
     local VIT_Loss = ((target:getStat(dsp.mod.VIT) / 100) * 20)
@@ -27,63 +35,40 @@ function onSpellCast(caster,target,spell)
     local INT_Loss = ((target:getStat(dsp.mod.INT) / 100) * 20)
     local MND_Loss = ((target:getStat(dsp.mod.MND) / 100) * 20)
     local CHR_Loss = ((target:getStat(dsp.mod.CHR) / 100) * 20)
-    local duration = 180 * resist -- BG wiki suggests only duration gets effected by resist.
-
     if (target:hasStatusEffect(dsp.effect.STR_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "STR: '%s' ", STR_Loss ) )
         target:addStatusEffect(dsp.effect.STR_DOWN,STR_Loss,0,duration)
     end
     if (target:hasStatusEffect(dsp.effect.DEX_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "DEX: '%s' ", DEX_Loss ) )
         target:addStatusEffect(dsp.effect.DEX_DOWN,DEX_Loss,0,duration)
     end
     if (target:hasStatusEffect(dsp.effect.VIT_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "VIT: '%s' ", VIT_Loss ) )
         target:addStatusEffect(dsp.effect.VIT_DOWN,VIT_Loss,0,duration)
     end
     if (target:hasStatusEffect(dsp.effect.AGI_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "AGI: '%s' ", AGI_Loss ) )
         target:addStatusEffect(dsp.effect.AGI_DOWN,AGI_Loss,0,duration)
     end
     if (target:hasStatusEffect(dsp.effect.INT_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "INT: '%s' ", INT_Loss ) )
         target:addStatusEffect(dsp.effect.INT_DOWN,INT_Loss,0,duration)
     end
     if (target:hasStatusEffect(dsp.effect.MND_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "MND: '%s' ", MND_Loss ) )
         target:addStatusEffect(dsp.effect.MND_DOWN,MND_Loss,0,duration)
     end
     if (target:hasStatusEffect(dsp.effect.CHR_DOWN) == false) then
-        -- caster:PrintToPlayer( string.format( "CHR: '%s' ", CHR_Loss ) )
         target:addStatusEffect(dsp.effect.CHR_DOWN,CHR_Loss,0,duration)
     end
 
-    --diverting use of doElementalNuke till spellParams is implemented for this spell
+    -- Diverting use of doElementalNuke till spellParams is implemented for this spell
+    -- local dmg = doElementalNuke(caster, target, spell, params)
 
-local params = {}params.dmg = 939params.multiplier = 2.335params.hasMultipleTargetReduction = falseparams.resistBonus = 1.0   --local dmg = doElementalNuke(caster, target, spell, params)
-    --calculate raw damage
-    local params = {}
-    params.dmg = 939
-    params.multiplier = 2.335
-    params.skillType = dsp.skill.ELEMENTAL_MAGIC
-    params.attribute = dsp.mod.INT
-    params.hasMultipleTargetReduction = false
-
+    -- Calculate raw damage
     local dmg = calculateMagicDamage(caster, target, spell, params)
-    --get resist multiplier (1x if no resist)
-    local params = {}
-    params.diff = caster:getStat(dsp.mod.INT)-target:getStat(dsp.mod.INT)
-    params.attribute = dsp.mod.INT
-    params.skillType = dsp.skill.ELEMENTAL_MAGIC
-    params.bonus = 1.0
-    local resist = applyResistance(caster, target, spell, params)
-    --get the resisted damage
+    -- Get the resisted damage
     dmg = dmg*resist
-    --add on bonuses (staff/day/weather/jas/mab/etc all go in this function)
+    -- Add on bonuses (staff/day/weather/jas/mab/etc all go in this function)
     dmg = addBonuses(caster,spell,target,dmg)
-    --add in target adjustment
+    -- Add in target adjustment
     dmg = adjustForTarget(target,dmg,spell:getElement())
-    --add in final adjustments
+    -- Add in final adjustments
     dmg = finalMagicAdjustments(caster,target,spell,dmg)
 
     return dmg
