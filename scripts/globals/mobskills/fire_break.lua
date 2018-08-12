6de7599ec8e69e8f86f8e6b567185019d0460066
TeoTwawki <TeoTwawki@users.noreply.github.com>
2018-08-12 17:56:28 -0400

    .

--- scripts/globals/mobskills/fire_break.lua
+++ scripts/globals/mobskills/fire_break.lua
@@ -3,10 +3,8 @@
 --  Description: Deals fire damage to enemies within a fan-shaped area originating from the caster.
 --  Type: Magical (Fire)
 ---------------------------------------------
-
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/status")
-
 ---------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
