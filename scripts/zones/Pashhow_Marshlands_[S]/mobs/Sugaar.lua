75a1abc4f4205b8f7e6652152a4422be1e07e779
n0pax <42299623+n0pax@users.noreply.github.com>
2018-08-11 22:15:17 -0400

    Sugaar NM Code

---
+++ scripts/zones/Pashhow_Marshlands_[S]/mobs/Sugaar.lua
@@ -0,0 +1,36 @@
+-----------------------------------
+-- Area: Pashhow Marshlands [S] (90)
+--  NM:  Sugaar
+-- !pos -416.496,24.11,-441.589,132
+-----------------------------------
+require("scripts/globals/status");
+require("scripts/globals/magic");
+require("scripts/globals/msg");
+
+function onMobSpawn(mob)
+    mob:setMod(dsp.mod.STORETP, 20);
+end;
+
+function onAdditionalEffect(mob, player)
+    local resist = applyResistanceAddEffect(mob,player,dsp.magic.ele.WIND,dsp.effect.SILENCE);
+    if (resist <= 0.5) then
+        return 0,0,0;
+    else
+        local duration = 30;
+        if (mob:getMainLvl() > player:getMainLvl()) then
+            duration = duration + (mob:getMainLvl() - player:getMainLvl())
+        end
+        duration = utils.clamp(duration,1,45);
+        duration = duration * resist;
+        if (not player:hasStatusEffect(dsp.effect.SILENCE)) then
+            player:addStatusEffect(dsp.effect.SILENCE, 1, 0, duration);
+        end
+        return dsp.subEffect.SILENCE, dsp.msg.basic.ADD_EFFECT_STATUS, dsp.effect.SILENCE;
+    end
+end;
+
+function onMobDeath(mob, player, isKiller)
+end;
+
+function onMobDespawn(mob)
+end;
