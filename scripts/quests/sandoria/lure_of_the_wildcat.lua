93849c9d82980f1c834d7948e8f52301fe3ee3a8
Tahir Akhlaq <xdemolish@hotmail.com>
2018-08-14 22:25:52 +0100

    cleaned up some typos, moved starter cities to quest table

--- scripts/quests/sandoria/lure_of_the_wildcat.lua
+++ scripts/quests/sandoria/lure_of_the_wildcat.lua
@@ -1,44 +1,18 @@
 require("scripts/globals/quests")
+require("scripts/globals/zone")
 
 local this_quest = {}
 local name = "LURE_OF_THE_WILDCAT"
-local logid = dsp.quests.enums.log_ids.sandoria
-local id = 113
+local logid = dsp.quests.enums.log_ids.SANDORIA
+local id = dsp.quests.quest_ids.sandoria.LURE_OF_THE_WILDCAT
 
 this_quest.id = id
 this_quest.name = name
 
--- npcs[zone] = { ["name"] = { onTrade, onTrigger, ..} }
-this_quest.npcs = {}
-
--- south sandy
-this_quest.npcs[230] = {}
-this_quest.npcs[230]["Amutiyaal"] =
-{
-    onTrade = function(player, npc, trade)
-    end,
-    onTrigger = function(player, npc)
-    end,
-    onEventUpdate = function(player, csid, option)
-    end,
-    onEventFinish = function(player, csid, option)
-    end
-}
-
--- mobs[zone] = { ["name"] = { onMobDeath, onMobDespawn, ..} }
-this_quest.mobs = {}
-this_quest.mobs[230] = {}
-this_quest.mobs[230]["Shitstain"] =
-{
-    onMobDeath = function(mob, target, isKiller)
-    end,
-}
-
-
 this_quest.repeatable = false,
 this_quest.vars =
 {
-    main = "[Q]".."["logid"]".."["..id.."]",
+    main = "[Q]".."["..logid.."]".."["..id.."]",
     preserve_main_on_complete = false, -- do we keep main var on quest completion
     additional =
     {
@@ -56,6 +30,26 @@ this_quest.rewards =
             title = NEW_ADVENTURER, keyitems = { DARK_KEY },
             items = { itemid = 17440, qty = 1 }
         }
+    }
+}
+
+
+-- npcs[zone] = { ["name"] = { onTrade, onTrigger, ..} }
+this_quest.npcs = {}
+
+-- south sandy
+this_quest.npcs[dsp.zone.SOUTHERN_SAN_DORIA] =
+{
+    ["Amutiyaal"] =
+    {
+        onTrade = function(player, npc, trade)
+        end,
+        onTrigger = function(player, npc)
+        end,
+        onEventUpdate = function(player, csid, option)
+        end,
+        onEventFinish = function(player, csid, option)
+        end
     }
 }
 dsp.quests.quests[logid][name] = this_quest
