2effc1f62dabf9a2d5d0ce6d54ea594e7990ad79
TeoTwawki <arawn.annwn@yandex.com>
2018-08-22 12:56:57 -0400

    Erase DIMENSIONAL_PORTAL_UNLOCK setting from existence - just comment out the mission check if you want this feature.
    Present me questions how past me got things merged sometimes.

--- scripts/zones/Konschtat_Highlands/npcs/Dimensional_Portal.lua
+++ scripts/zones/Konschtat_Highlands/npcs/Dimensional_Portal.lua
@@ -3,31 +3,28 @@
 --  NPC: Dimensional_Portal
 -- !pos 220.000 19.104 140.000 108
 -----------------------------------
-package.loaded["scripts/zones/Konschtat_Highlands/TextIDs"] = nil;
+package.loaded["scripts/zones/Konschtat_Highlands/TextIDs"] = nil
 -----------------------------------
-require("scripts/globals/settings");
-require("scripts/globals/keyitems");
-require("scripts/zones/Konschtat_Highlands/TextIDs");
+require("scripts/zones/Konschtat_Highlands/TextIDs")
+require("scripts/globals/keyitems")
 -----------------------------------
 
-function onTrade(player,npc,trade)
-end;
+function onTrade(player, npc, trade)
+end
 
-function onTrigger(player,npc)
-
-    if (player:getCurrentMission(COP) > THE_WARRIOR_S_PATH) or (DIMENSIONAL_PORTAL_UNLOCK == true) then
-        player:startEvent(915);
+function onTrigger(player, npc)
+    if player:getCurrentMission(COP) > THE_WARRIOR_S_PATH then
+        player:startEvent(915)
     else
-        player:messageSpecial(ALREADY_OBTAINED_TELE+1); -- Telepoint Disappeared
+        player:messageSpecial(ALREADY_OBTAINED_TELE +1) -- Telepoint Disappeared
     end
+end
 
-end;
+function onEventUpdate(player, csid, option)
+end
 
-function onEventUpdate(player,csid,option)
-end;
-
-function onEventFinish(player,csid,option)
-    if (csid == 915 and option == 1) then
-        player:setPos(-635.599,-2.799,163.8,193,33); -- To AlTaieu {R}
+function onEventFinish(player, csid, option)
+    if csid == 915 and option == 1 then
+        player:setPos(-635.599,-2.799,163.8,193,33) -- To AlTaieu {R}
     end
-end;
+end
