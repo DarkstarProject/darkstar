e96fbe1d1c6931127750d5f9c02d3d42d8ba4016
wrenffxi <oliviapekoe@gmail.com>
2018-09-12 11:07:06 -0400

    fixed typos in last rebase messageBase in conquest.lua is now properly set from zones table

--- scripts/globals/conquest.lua
+++ scripts/globals/conquest.lua
@@ -1245,7 +1245,7 @@ dsp.conquest.onConquestUpdate = function(zone, updatetype)
     local region = zone:getRegionID()
     local owner = GetRegionOwner(region)
     local players = zone:getPlayers()
-    local messageBase = CONQUEST_BASE
+    local messageBase = zones[zone].text.CONQUEST_BASE
     local ranking = getConquestBalance()
     
     for _, player in pairs(players) do
--- scripts/zones/Dangruf_Wadi/npcs/qm5.lua
+++ scripts/zones/Dangruf_Wadi/npcs/qm5.lua
@@ -1,7 +1,7 @@
 -----------------------------------
 --  NPC: ??? (QM5)
 -- Type: Eggblix (dice roll game part 3)
--- !zone 191
+-- !pos -462.436 2.456 -141.171 191
 -- Involved in quest "As Thick As Thieves"
 -----------------------------------
 local ID = require("scripts/zones/Dangruf_Wadi/IDs")
@@ -20,23 +20,23 @@ function onTrade(player,npc,trade)
             local rand2 = math.random(1,999)
 
             if (rand1 > rand2) then
-                player:messageSpecial(id.text.YOU_PLACE_ITEM,0,4362)
+                player:messageSpecial(ID.text.YOU_PLACE_ITEM,0,4362)
                 player:startEvent(138,1092,0,rand1,rand2) -- complete 3/3 gamble mini quest
             else
-                player:messageSpecial(id.text.YOU_PLACE_ITEM,0,4362)
+                player:messageSpecial(ID.text.YOU_PLACE_ITEM,0,4362)
                 player:startEvent(141,0,0,rand1,rand2) -- player looses
             end
         elseif thickAsThievesGamblingCS < 4 then -- trading out of order
-            player:messageSpecial(id.text.JUST_WONT_DO)
+            player:messageSpecial(ID.text.JUST_WONT_DO)
         elseif thickAsThievesGamblingCS > 4 then
-            player:messageSpecial(id.text.BEAT_EGGBLIX)
+            player:messageSpecial(ID.text.BEAT_EGGBLIX)
         end
     end
 
 end
 
 function onTrigger(player,npc)
-    player:messageSpecial(id.text.BROKEN_EGGS)
+    player:messageSpecial(ID.text.BROKEN_EGGS)
 end
 
 function onEventUpdate(player,csid,option)
@@ -46,16 +46,16 @@ function onEventFinish(player,csid,option)
 
     if (csid == 138 or csid == 141) and option == 2 then -- player gives up
         player:confirmTrade()
-        player:messageSpecial(id.text.YOU_GAVE_UP)
+        player:messageSpecial(ID.text.YOU_GAVE_UP)
     elseif csid == 141 and option == 1 then -- player looses dice game
         player:confirmTrade()
-        player:messageSpecial(id.text.GOBLIN_BEAT_YOU)
+        player:messageSpecial(ID.text.GOBLIN_BEAT_YOU)
         if OLD_SCHOOL_ENABLED then
             player:setVar("thickAsThievesGamblingCS",-1) -- start over
         end
     elseif csid == 138 and option == 0 then -- player wins dice game
         player:confirmTrade()
-        player:messageSpecial(id.text.YOU_BEAT_GOBLIN)
+        player:messageSpecial(ID.text.YOU_BEAT_GOBLIN)
         player:setVar("thickAsThievesGamblingCS",5)
     end
 
