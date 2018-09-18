ed7d74cb0b99fe286920e1dc38d16a8449f995ce
David Norris <Knowone134@gmail.com>
2018-09-17 19:12:18 -0500

    add zone id check back

--- scripts/zones/Mount_Zhayolm/npcs/_1p3.lua
+++ scripts/zones/Mount_Zhayolm/npcs/_1p3.lua
@@ -82,7 +82,7 @@ function onInstanceCreated(player,target,instance)
         local party = player:getParty();
         if (party ~= nil) then
             for i,v in ipairs(party) do
-                if v:getID() ~= player:getID() then
+                if v:getID() ~= player:getID() and v:getZoneID() == player:getZoneID() then
                     v:setInstance(instance);
                     v:startEvent(208, 2);
                     v:delKeyItem(dsp.ki.LEBROS_ASSAULT_ORDERS);
