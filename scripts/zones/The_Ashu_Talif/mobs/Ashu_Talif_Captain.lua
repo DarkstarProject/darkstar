48c533edbf2076572cf809c9905f2984b1cca4ee
David Norris <Knowone134@gmail.com>
2018-09-22 21:33:38 -0500

    despawn mobs on instance complete and have them stop attacking you

--- scripts/zones/The_Ashu_Talif/mobs/Ashu_Talif_Captain.lua
+++ scripts/zones/The_Ashu_Talif/mobs/Ashu_Talif_Captain.lua
@@ -7,15 +7,18 @@ local ID = require("scripts/zones/The_Ashu_Talif/IDs")
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:setUnkillable(true);
-    mob:setMobMod(dsp.mobMod.SCRIPTED_2HOUR, 1);
-end;
+    mob:setUnkillable(true)
+    mob:setMobMod(dsp.mobMod.SCRIPTED_2HOUR, 1)
+end
 
 function onMobFight(mob, target)
     -- The captain gives up at <= 20% HP. Everyone disengages
-    local instance = mob:getInstance();
+    local instance = mob:getInstance()
     if (mob:getHPP() <= 20 and instance:completed() == false) then
         instance:complete();
+        for i,v in pairs(TheAshuTalif.mobs[2]) do
+            DespawnMob(v, instance)
+        end
     end
 
     mob:addListener("WEAPONSKILL_STATE_ENTER", "WS_START_MSG", function(mob, skillID)
@@ -29,45 +32,45 @@ function onMobFight(mob, target)
         elseif(skillId == 938) then
             mob:showText(mob,ID.text.FOR_THE_BLACK_COFFIN);
         end
-    end);
-end;
+    end)
+end
 
 function onMobRoam(mob)
-    local jumped = mob:getLocalVar("jump");
-    local ready = mob:getLocalVar("ready");
+    local jumped = mob:getLocalVar("jump")
+    local ready = mob:getLocalVar("ready")
 
     -- Becomes ready when the Crew is engaged. Jump down!
     if (ready == 1 and jumped == 0) then
         mob:showText(mob,ID.text.OVERPOWERED_CREW);
 
-        mob:hideName(true);
-        mob:entityAnimationPacket("jmp0");
+        mob:hideName(true)
+        mob:entityAnimationPacket("jmp0")
 
         mob:timer(2000, function(mob)
-            mob:setPos(0,-22,13,192);
-            mob:entityAnimationPacket("jmp1");
+            mob:setPos(0,-22,13,192)
+            mob:entityAnimationPacket("jmp1")
 
             mob:showText(mob,ID.text.TEST_YOUR_BLADES);
 
             mob:timer(2000, function(mob)
-                mob:hideName(false);
-                mob:untargetable(false);
+                mob:hideName(false)
+                mob:untargetable(false)
             end)
         end)
 
-        mob:setLocalVar("jump", 1);
+        mob:setLocalVar("jump", 1)
     end
 
-end;
+end
 
 function onMobEngaged(mob,target)
-end;
+end
 
 function onMobDisengage(mob,target)
-end;
+end
 
 function onMobDeath(mob, player, isKiller)
-end;
+end
 
 function onMobDespawn(mob)
-end;
+end
