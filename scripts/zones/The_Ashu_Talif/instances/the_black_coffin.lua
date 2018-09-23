9b499b06e2c263f487c3db0826ebe3e49a454267
David Norris <Knowone134@gmail.com>
2018-09-22 21:32:27 -0500

    remove players from instance on complete that have previously beat mission

--- scripts/zones/The_Ashu_Talif/instances/the_black_coffin.lua
+++ scripts/zones/The_Ashu_Talif/instances/the_black_coffin.lua
@@ -18,59 +18,61 @@ end;
 function onInstanceCreated(instance)
     SpawnMob(ID.mob.GESSHO, instance);
     for i,v in pairs(ID.mob[1]) do
-        SpawnMob(v, instance);
+        SpawnMob(v, instance)
     end
-end;
+end
 
 function onInstanceTimeUpdate(instance, elapsed)
     updateInstanceTime(instance, elapsed, ID.text)
-end;
+end
 
 function onInstanceFailure(instance)
 
-    local chars = instance:getChars();
+    local chars = instance:getChars()
 
     for i,v in pairs(chars) do
         v:messageSpecial(ID.text.MISSION_FAILED,10,10);
         v:startEvent(102);
     end
-end;
+end
 
 function onInstanceProgressUpdate(instance, progress)
 
     if (progress == 5) then
         for i,v in pairs(ID.mob[2]) do
-            SpawnMob(v, instance);
+            SpawnMob(v, instance)
         end
     elseif (progress >= 10 and instance:completed() == false) then
         local v = GetMobByID(ID.mob.GESSHO, instance);
 
         if(v:isAlive()) then
-            v:setLocalVar("ready",2);
+            v:setLocalVar("ready",2)
         end
 
-        local chars = instance:getChars();
+        local chars = instance:getChars()
 
         for i,v in pairs(chars) do
-            v:startEvent(102);
+            v:startEvent(102)
         end
 
-        instance:complete();
+        instance:complete()
     end
 
-end;
+end
 
 function onInstanceComplete(instance)
 
-    local chars = instance:getChars();
+    local chars = instance:getChars()
 
     for i,v in pairs(chars) do
         if (v:getCurrentMission(TOAU) == THE_BLACK_COFFIN and v:getVar("AhtUrganStatus") == 1) then
-            v:setVar("AhtUrganStatus", 2);
-            v:startEvent(101);
+            v:setVar("AhtUrganStatus", 2)
+            v:startEvent(101)
+        else
+            v:startEvent(102)
         end
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
 end
