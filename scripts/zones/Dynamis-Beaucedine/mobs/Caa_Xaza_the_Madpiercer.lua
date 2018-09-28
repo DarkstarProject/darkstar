353603ccc1ba01e330b2fce067c11c6fb69cdd76
wrenffxi <oliviapekoe@gmail.com>
2018-09-28 14:33:32 -0400

    added beastman mixin and coins to Dynamis Beaucedine beastmen

--- scripts/zones/Dynamis-Beaucedine/mobs/Caa_Xaza_the_Madpiercer.lua
+++ scripts/zones/Dynamis-Beaucedine/mobs/Caa_Xaza_the_Madpiercer.lua
@@ -2,8 +2,16 @@
 -- Area: Dynamis Beaucedine
 --  MOB: Caa_Xaza_the_Madpiercer
 -----------------------------------
-mixins = {require("scripts/mixins/job_special")}
+mixins =
+{
+    require("scripts/mixins/dynamis_beastmen"),
+    require("scripts/mixins/job_special")
+}
 -----------------------------------
+
+function onMobSpawn(mob)
+    mob:setLocalVar("dynamis_currency", 1449)
+end
 
 function onMobDeath(mob, player, isKiller)
 end
--- scripts/zones/Dynamis-Beaucedine/mobs/Kuu_Xuka_the_Nimble.lua
+++ scripts/zones/Dynamis-Beaucedine/mobs/Kuu_Xuka_the_Nimble.lua
@@ -2,8 +2,16 @@
 -- Area: Dynamis Beaucedine
 --  MOB: Kuu_Xuka_the_Nimble
 -----------------------------------
-mixins = {require("scripts/mixins/job_special")}
+mixins =
+{
+    require("scripts/mixins/dynamis_beastmen"),
+    require("scripts/mixins/job_special")
+}
 -----------------------------------
+
+function onMobSpawn(mob)
+    mob:setLocalVar("dynamis_currency", 1449)
+end
 
 function onMobDeath(mob, player, isKiller)
 end
