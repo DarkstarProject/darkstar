d020a51ea3ab2a8672b4b99613804ce744d13e42
David Norris <Knowone134@gmail.com>
2018-09-30 20:12:52 -0500

    rebase changes

--- scripts/zones/Lebros_Cavern/instances/troll_fugitives.lua
+++ scripts/zones/Lebros_Cavern/instances/troll_fugitives.lua
@@ -9,18 +9,18 @@ require("scripts/zones/Lebros_Cavern/IDs");
 
 function afterInstanceRegister(player)
     local instance = player:getInstance();
-    player:messageSpecial(Lebros.text.ASSAULT_23_START, 23);
-    player:messageSpecial(Lebros.text.TIME_TO_COMPLETE, instance:getTimeLimit());
+    player:messageSpecial(ID.text.ASSAULT_23_START, 23);
+    player:messageSpecial(ID.text.TIME_TO_COMPLETE, instance:getTimeLimit());
 end;
 
 function onInstanceCreated(instance)
 
-    for i,v in pairs(Lebros.mobs[23]) do
+    for i,v in pairs(ID.mob[23]) do
         SpawnMob(v, instance);
     end
 
-    local rune = instance:getEntity(bit.band(Lebros.npcs.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC);
-    local box = instance:getEntity(bit.band(Lebros.npcs.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC);
+    local rune = instance:getEntity(bit.band(ID.npc.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC);
+    local box = instance:getEntity(bit.band(ID.npc.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC);
     rune:setPos(-376.272,-9.893,89.189,0);
     box:setPos(-384.097,-10,84.954,49);
 
@@ -35,7 +35,7 @@ function onInstanceFailure(instance)
     local chars = instance:getChars();
 
     for i,v in pairs(chars) do
-        v:messageSpecial(Lebros.text.MISSION_FAILED,10,10);
+        v:messageSpecial(ID.text.MISSION_FAILED,10,10);
         v:startEvent(102);
     end
 end;
@@ -53,11 +53,11 @@ function onInstanceComplete(instance)
     local chars = instance:getChars();
 
     for i,v in pairs(chars) do
-        v:messageSpecial(Lebros.text.RUNE_UNLOCKED, 8, 8);
+        v:messageSpecial(ID.text.RUNE_UNLOCKED, 8, 8);
     end
 
-    local rune = instance:getEntity(bit.band(Lebros.npcs.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC);
-    local box = instance:getEntity(bit.band(Lebros.npcs.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC);
+    local rune = instance:getEntity(bit.band(ID.npc.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC);
+    local box = instance:getEntity(bit.band(ID.npc.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC);
     rune:setStatus(dsp.status.NORMAL);
     box:setStatus(dsp.status.NORMAL);
 
