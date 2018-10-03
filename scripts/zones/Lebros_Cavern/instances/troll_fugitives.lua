cc5ba48099c4df65cdfc025bc19082cec809eeff
David Norris <Knowone134@gmail.com>
2018-10-03 12:54:41 -0500

    local ID change

--- scripts/zones/Lebros_Cavern/instances/troll_fugitives.lua
+++ scripts/zones/Lebros_Cavern/instances/troll_fugitives.lua
@@ -3,65 +3,65 @@
 -- Assault: Troll Fugitives
 --
 -----------------------------------
+local ID = require("scripts/zones/Lebros_Cavern/IDs")
 require("scripts/globals/instance")
-require("scripts/zones/Lebros_Cavern/IDs");
 -----------------------------------
 
 function afterInstanceRegister(player)
-    local instance = player:getInstance();
-    player:messageSpecial(ID.text.ASSAULT_23_START, 23);
-    player:messageSpecial(ID.text.TIME_TO_COMPLETE, instance:getTimeLimit());
-end;
+    local instance = player:getInstance()
+    player:messageSpecial(ID.text.ASSAULT_23_START, 23)
+    player:messageSpecial(ID.text.TIME_TO_COMPLETE, instance:getTimeLimit())
+end
 
 function onInstanceCreated(instance)
 
     for i,v in pairs(ID.mob[23]) do
-        SpawnMob(v, instance);
+        SpawnMob(v, instance)
     end
 
-    local rune = instance:getEntity(bit.band(ID.npc.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC);
-    local box = instance:getEntity(bit.band(ID.npc.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC);
-    rune:setPos(-376.272,-9.893,89.189,0);
-    box:setPos(-384.097,-10,84.954,49);
+    local rune = instance:getEntity(bit.band(ID.npc.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC)
+    local box = instance:getEntity(bit.band(ID.npc.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC)
+    rune:setPos(-376.272,-9.893,89.189,0)
+    box:setPos(-384.097,-10,84.954,49)
 
-end;
+end
 
 function onInstanceTimeUpdate(instance, elapsed)
     updateInstanceTime(instance, elapsed, Lebros.text)
-end;
+end
 
 function onInstanceFailure(instance)
 
-    local chars = instance:getChars();
+    local chars = instance:getChars()
 
     for i,v in pairs(chars) do
-        v:messageSpecial(ID.text.MISSION_FAILED,10,10);
-        v:startEvent(102);
+        v:messageSpecial(ID.text.MISSION_FAILED,10,10)
+        v:startEvent(102)
     end
-end;
+end
 
 function onInstanceProgressUpdate(instance, progress)
 
     if (progress >= 15) then
-        instance:complete();
+        instance:complete()
     end
 
-end;
+end
 
 function onInstanceComplete(instance)
 
-    local chars = instance:getChars();
+    local chars = instance:getChars()
 
     for i,v in pairs(chars) do
-        v:messageSpecial(ID.text.RUNE_UNLOCKED, 8, 8);
+        v:messageSpecial(ID.text.RUNE_UNLOCKED, 8, 8)
     end
 
-    local rune = instance:getEntity(bit.band(ID.npc.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC);
-    local box = instance:getEntity(bit.band(ID.npc.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC);
-    rune:setStatus(dsp.status.NORMAL);
-    box:setStatus(dsp.status.NORMAL);
+    local rune = instance:getEntity(bit.band(ID.npc.RUNE_OF_RELEASE, 0xFFF), dsp.objType.NPC)
+    local box = instance:getEntity(bit.band(ID.npc.ANCIENT_LOCKBOX, 0xFFF), dsp.objType.NPC)
+    rune:setStatus(dsp.status.NORMAL)
+    box:setStatus(dsp.status.NORMAL)
 
-end;
+end
 
 function onEventUpdate(player,csid,option)
 end
