b27712cfdafb2a805d8f6caee6ed5a0d5be1a1af
wrenffxi <oliviapekoe@gmail.com>
2018-10-10 16:03:53 -0400

    Fix haste samba applying haste mod twice, which was causing player to attack very fast, then overflow attack very slow.

--- scripts/globals/effects/haste_samba_haste_effect.lua
+++ scripts/globals/effects/haste_samba_haste_effect.lua
@@ -3,9 +3,10 @@
 -- JA Haste 5-10%
 -----------------------------------
 require("scripts/globals/status")
+-----------------------------------
+
 function onEffectGain(target, effect)
     target:addMod(dsp.mod.HASTE_ABILITY, effect:getPower())
-    target:addMod(dsp.mod.HASTE_ABILITY,effect:getPower())
 end
 
 function onEffectTick(target, effect)
