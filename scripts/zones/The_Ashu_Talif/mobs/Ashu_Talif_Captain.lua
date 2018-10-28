4b46af7cb587a447f63385aadbd9920f6798a759
KnowOne134 <35616771+KnowOne134@users.noreply.github.com>
2018-10-28 09:45:19 -0500

    Update Ashu_Talif_Captain.lua
    global ID change and add gesho to despawn so he can stop attacking the captin on instince complete

--- scripts/zones/The_Ashu_Talif/mobs/Ashu_Talif_Captain.lua
+++ scripts/zones/The_Ashu_Talif/mobs/Ashu_Talif_Captain.lua
@@ -2,7 +2,7 @@
 -- Area: The Ashu Talif (The Black Coffin)
 --  MOB: Ashu Talif Crew
 -----------------------------------
-require("scripts/globals/instance");
+require("scripts/globals/instance")
 local ID = require("scripts/zones/The_Ashu_Talif/IDs")
 -----------------------------------
 
@@ -16,7 +16,8 @@ function onMobFight(mob, target)
     local instance = mob:getInstance()
     if (mob:getHPP() <= 20 and instance:completed() == false) then
         instance:complete();
-        for i,v in pairs(TheAshuTalif.mobs[2]) do
+        DespawnMob(ID.mob.GESSHO, instance)
+        for i,v in pairs(ID.mob.[2]) do
             DespawnMob(v, instance)
         end
     end
@@ -24,13 +25,13 @@ function onMobFight(mob, target)
     mob:addListener("WEAPONSKILL_STATE_ENTER", "WS_START_MSG", function(mob, skillID)
         -- Vulcan Shot
         if(skillId == 254) then
-            mob:showText(mob,ID.text.FOR_EPHRAMAD);
+            mob:showText(mob,ID.text.FOR_EPHRAMAD)
             mob:timer(3000, function(mob)
-                mob:showText(mob,ID.text.TROUBLESOME_SQUABS);
+                mob:showText(mob,ID.text.TROUBLESOME_SQUABS)
             end)
         -- Circle Blade
         elseif(skillId == 938) then
-            mob:showText(mob,ID.text.FOR_THE_BLACK_COFFIN);
+            mob:showText(mob,ID.text.FOR_THE_BLACK_COFFIN)
         end
     end)
 end
@@ -41,7 +42,7 @@ function onMobRoam(mob)
 
     -- Becomes ready when the Crew is engaged. Jump down!
     if (ready == 1 and jumped == 0) then
-        mob:showText(mob,ID.text.OVERPOWERED_CREW);
+        mob:showText(mob,ID.text.OVERPOWERED_CREW)
 
         mob:hideName(true)
         mob:entityAnimationPacket("jmp0")
@@ -50,7 +51,7 @@ function onMobRoam(mob)
             mob:setPos(0,-22,13,192)
             mob:entityAnimationPacket("jmp1")
 
-            mob:showText(mob,ID.text.TEST_YOUR_BLADES);
+            mob:showText(mob,ID.text.TEST_YOUR_BLADES)
 
             mob:timer(2000, function(mob)
                 mob:hideName(false)
