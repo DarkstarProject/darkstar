d2bc2a0d735c3da8a1cda25486eccd5df3f8a580
David Norris <Knowone134@gmail.com>
2018-11-14 09:43:12 -0600

    local item IDs, add better comments

--- scripts/zones/Arrapago_Remnants/npcs/Socket.lua
+++ scripts/zones/Arrapago_Remnants/npcs/Socket.lua
@@ -1,7 +1,8 @@
 -----------------------------------
 -- Area: Arrapago Remnants
 -- NPC: Socket
--- Trade Cells to pop NM
+-- Trade Slavage Cells to pop Wahzil
+-- Wahzil drops 2x the Cells traded
 -----------------------------------
 local ID = require("scripts/zones/Arrapago_Remnants/IDs")
 -----------------------------------
@@ -10,8 +11,10 @@ function onTrade(player,npc,trade)
     local instance = npc:getInstance()
     local mob = instance:getEntity(bit.band(ID.mob[2][3].wahzil, 0xFFF), dsp.objType.MOB)
     local COUNT = trade:getItemCount()
+    local INCUS_CELL = 5365
+    local SPISSATUS_CELL = 5384
         
-    for i = 5365, 5384 do
+    for i = INCUS_CELL, SPISSATUS_CELL do
         if COUNT <= 5 and trade:hasItemQty(i,COUNT) then
             SpawnMob(ID.mob[2][3].wahzil, instance):updateClaim(player)
             player:tradeComplete()
