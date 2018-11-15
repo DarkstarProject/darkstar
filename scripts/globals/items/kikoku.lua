8d45116aec21544c33a584035641b91a8014709c
nfaletra <autkast33@gmail.com>
2018-11-15 00:11:53 -0700

    Fix Kikoku

--- scripts/globals/items/kikoku.lua
+++ scripts/globals/items/kikoku.lua
@@ -11,7 +11,7 @@ require("scripts/globals/status")
 function onAdditionalEffect(player, target, damage)
     local chance = 10
 
-    if math.random(100) <= chance and applyResistanceAddEffect(player, target, dsp.magic.ele.ICE, 0) > 0.5) then
+    if math.random(100) <= chance and applyResistanceAddEffect(player, target, dsp.magic.ele.ICE, 0) > 0.5 then
         target:addStatusEffect(dsp.effect.PARALYSIS, 17, 0, 30) -- Power needs verification/adjustment.
         return dsp.subEffect.PARALYSIS, dsp.msg.basic.ADD_EFFECT_STATUS, dsp.effect.PARALYSIS
     end
