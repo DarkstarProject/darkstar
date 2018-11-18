a9eb63e58d00885084590f25e6d2559b0161b900
wrenffxi <oliviapekoe@gmail.com>
2018-11-18 17:09:55 -0500

    rage mode mixin

--- scripts/zones/Valley_of_Sorrows/mobs/Aspidochelone.lua
+++ scripts/zones/Valley_of_Sorrows/mobs/Aspidochelone.lua
@@ -3,18 +3,18 @@
 --  HNM: Aspidochelone
 -----------------------------------
 local ID = require("scripts/zones/Valley_of_Sorrows/IDs")
+mixins = {require("scripts/mixins/rage")}
 require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/titles")
 -----------------------------------
 
-function onMobInitialize(mob)
-end
-
 function onMobSpawn(mob)
     if LandKingSystem_NQ > 0 or LandKingSystem_HQ > 0 then
         GetNPCByID(ID.npc.ADAMANTOISE_QM):setStatus(dsp.status.DISAPPEAR)
     end
+
+    mob:setLocalVar("[rage]timer", 3600) -- 60 minutes
 end
 
 function onMobDeath(mob, player, isKiller)
