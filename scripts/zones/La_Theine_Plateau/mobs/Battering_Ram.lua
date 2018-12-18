b016a0e42a0f64482588517410c4e399c6fdea4e
nfaletra <autkast33@gmail.com>
2018-12-18 15:29:25 -0700

    Remove testing spawn chance XD

--- scripts/zones/La_Theine_Plateau/mobs/Battering_Ram.lua
+++ scripts/zones/La_Theine_Plateau/mobs/Battering_Ram.lua
@@ -12,6 +12,6 @@ end
 
 function onMobDespawn(mob)
     if not phOnDespawn(mob, ID.mob.BLOODTEAR_PH, 10, math.random(75600, 86400)) then -- 21-24 hours
-        phOnDespawn(mob, ID.mob.LUMBERING_LAMBERT_PH, 100, 1200) -- 20 min
+        phOnDespawn(mob, ID.mob.LUMBERING_LAMBERT_PH, 10, 1200) -- 20 min
     end
 end
