9746053842e58905d0de7de7896d0f901d0cca59
TeoTwawki <arawn.annwn@yandex.com>
2018-12-24 13:12:52 -0500

    Add Fortune Fruit and Black Chocobo Cap to usable items.

---
+++ scripts/globals/items/bunch_of_fortune_fruits.lua
@@ -0,0 +1,30 @@
+-----------------------------------------
+-- ID: 6498
+-- Item: Bunch of Fortune Fruits
+-- Food Effect: 30Min, All Races
+-----------------------------------------
+-- Charisma +7
+-- may have unknown hidden effects
+-----------------------------------------
+require("scripts/globals/status")
+-----------------------------------------
+
+function onItemCheck(target)
+    local result = 0
+    if target:hasStatusEffect(dsp.effect.FOOD) or target:hasStatusEffect(dsp.effect.FIELD_SUPPORT_FOOD) then
+        result = 246
+    end
+    return result
+end
+
+function onItemUse(target)
+    target:addStatusEffect(dsp.effect.FOOD,0,0,1800,6498)
+end
+
+function onEffectGain(target, effect)
+    target:addMod(dsp.mod.CHR, 7)
+end
+
+function onEffectLose(target, effect)
+    target:delMod(dsp.mod.CHR, 7)
+end
