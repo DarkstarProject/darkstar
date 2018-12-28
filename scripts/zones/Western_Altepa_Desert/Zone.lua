69eb25deb33c7c8624c1706ebb79c950308b9790
wrenffxi <oliviapekoe@gmail.com>
2018-12-28 05:01:19 -0500

    style guide: western altepa desert added altepa gate to ID.npc and adjusted column luas accordingly

--- scripts/zones/Western_Altepa_Desert/Zone.lua
+++ scripts/zones/Western_Altepa_Desert/Zone.lua
@@ -3,55 +3,54 @@
 -- Zone: Western_Altepa_Desert (125)
 --
 -----------------------------------
-local ID = require("scripts/zones/Western_Altepa_Desert/IDs");
-require("scripts/globals/icanheararainbow");
-require("scripts/globals/chocobo_digging");
-require("scripts/globals/conquest");
-require("scripts/globals/weather");
-require("scripts/globals/zone");
+local ID = require("scripts/zones/Western_Altepa_Desert/IDs")
+require("scripts/globals/icanheararainbow")
+require("scripts/globals/chocobo_digging")
+require("scripts/globals/conquest")
+require("scripts/globals/weather")
 -----------------------------------
 
 function onChocoboDig(player, precheck)
     return dsp.chocoboDig.start(player, precheck)
-end;
+end
 
 function onInitialize(zone)
-    UpdateNMSpawnPoint(ID.mob.KING_VINEGARROON);
-    GetMobByID(ID.mob.KING_VINEGARROON):setRespawnTime(math.random(900, 10800));
-end;
+    UpdateNMSpawnPoint(ID.mob.KING_VINEGARROON)
+    GetMobByID(ID.mob.KING_VINEGARROON):setRespawnTime(math.random(900, 10800))
+end
 
-function onZoneIn( player, prevZone)
-    local cs = -1;
+function onZoneIn(player, prevZone)
+    local cs = -1
 
-    if ( player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
-        player:setPos( -19.901, 13.607, 440.058, 78);
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
+        player:setPos(-19.901, 13.607, 440.058, 78)
     end
 
-    if ( triggerLightCutscene( player)) then -- Quest: I Can Hear A Rainbow
-        cs = 2;
+    if triggerLightCutscene(player) then -- Quest: I Can Hear A Rainbow
+        cs = 2
     end
 
-    return cs;
-end;
+    return cs
+end
 
 function onConquestUpdate(zone, updatetype)
     dsp.conq.onConquestUpdate(zone, updatetype)
-end;
+end
 
-function onRegionEnter( player, region)
-end;
+function onRegionEnter(player, region)
+end
 
-function onEventUpdate( player, csid, option)
-    if ( csid == 2) then
-        lightCutsceneUpdate(player); -- Quest: I Can Hear A Rainbow
+function onEventUpdate(player, csid, option)
+    if csid == 2 then
+        lightCutsceneUpdate(player) -- Quest: I Can Hear A Rainbow
     end
-end;
+end
 
-function onEventFinish( player, csid, option)
-    if (csid == 2) then
-        lightCutsceneFinish(player); -- Quest: I Can Hear A Rainbow
+function onEventFinish(player, csid, option)
+    if csid == 2 then
+        lightCutsceneFinish(player) -- Quest: I Can Hear A Rainbow
     end
-end;
+end
 
 function onZoneWeatherChange(weather)
     local KV = GetMobByID(ID.mob.KING_VINEGARROON)
@@ -61,4 +60,4 @@ function onZoneWeatherChange(weather)
     elseif KV:getCurrentAction() == dsp.act.ROAMING and weather ~= dsp.weather.DUST_STORM and weather ~= dsp.weather.SAND_STORM then
         DespawnMob(ID.mob.KING_VINEGARROON)
     end
-end;
+end
