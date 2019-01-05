e6da1e632d85c37d73552c6d3143d6fb2c3b2c2f
wrenffxi <oliviapekoe@gmail.com>
2019-01-05 11:20:08 -0500

    The Sanctuary of Zi'Tah (style)

--- scripts/zones/The_Sanctuary_of_ZiTah/mobs/Master_Coeurl.lua
+++ scripts/zones/The_Sanctuary_of_ZiTah/mobs/Master_Coeurl.lua
@@ -8,4 +8,4 @@ require("scripts/globals/regimes")
 function onMobDeath(mob, player, isKiller)
     dsp.regime.checkRegime(player, mob, 117, 1, dsp.regime.type.FIELDS)
     dsp.regime.checkRegime(player, mob, 118, 2, dsp.regime.type.FIELDS)
-end;
+end
