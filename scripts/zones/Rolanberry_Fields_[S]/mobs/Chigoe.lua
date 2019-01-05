563e20a97b754cd3a89ea67067a3b569adeb3dea
wrenffxi <oliviapekoe@gmail.com>
2019-01-05 14:27:43 -0500

    Fill in missing Area: in mob headers

--- scripts/zones/Rolanberry_Fields_[S]/mobs/Chigoe.lua
+++ scripts/zones/Rolanberry_Fields_[S]/mobs/Chigoe.lua
@@ -1,11 +1,9 @@
 -----------------------------------
--- Area:
+-- Area: Rolanberry Fields [S]
 --  MOB: Chigoe
 -----------------------------------
-
---require("scripts/globals/titles");
-mixins = { require("scripts/mixins/families/chigoe") }
+mixins = {require("scripts/mixins/families/chigoe")}
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
-end;
+end
