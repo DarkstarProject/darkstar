3854fee97ee69d1d331a4985cc368af47976f4c9
David Norris <Knowone134@gmail.com>
2019-01-07 18:51:59 -0600

    ZNM Claret

--- scripts/zones/Mount_Zhayolm/mobs/Claret.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Claret.lua
@@ -1,32 +1,33 @@
 -----------------------------------
 -- Area: Mount Zhayolm
---  MOB: Claret
+--  ZNM: Claret
 -- !pos 501 -9 53
 -- Spawned with Pectin: @additem 2591
 -- Wiki: http://ffxiclopedia.wikia.com/wiki/Claret
 -----------------------------------
-require("scripts/globals/magic");
-require("scripts/globals/status");
+require("scripts/globals/magic")
+require("scripts/globals/status")
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:addMod(dsp.mod.REGEN, math.floor(mob:getMaxHP()*.004));
-    mob:addMod(dsp.mod.BINDRES, 40);
-    mob:SetAutoAttackEnabled(false);
-end;
+    mob:addMod(dsp.mod.REGEN, math.floor(mob:getMaxHP()*.004))
+    mob:addMod(dsp.mod.BINDRES, 40)
+    mob:addMod(dsp.mod.MOVE, 15)
+    mob:SetAutoAttackEnabled(false)
+end
 
 function onMobFight(mob, target)
-    if (mob:checkDistance(target) < 3) then
-        if (target:hasStatusEffect(dsp.effect.POISON) == false) then
-            target:addStatusEffect(dsp.effect.POISON, 100, 3, math.random(3,6) * 3); -- Poison for 3-6 ticks.
+    if mob:checkDistance(target) < 3 then
+        if target:hasStatusEffect(dsp.effect.POISON) == false then
+            target:addStatusEffect(dsp.effect.POISON, 100, 3, math.random(3,6) * 3) -- Poison for 3-6 ticks.
         else
-            if (target:getStatusEffect(dsp.effect.POISON):getPower() < 100) then
-                target:delStatusEffect(dsp.effect.POISON);
-                target:addStatusEffect(dsp.effect.POISON, 100, 3, math.random(3,6) * 3); -- Poison for 3-6 ticks.
+            if target:getStatusEffect(dsp.effect.POISON):getPower() < 100 then
+                target:delStatusEffect(dsp.effect.POISON)
+                target:addStatusEffect(dsp.effect.POISON, 100, 3, math.random(3,6) * 3) -- Poison for 3-6 ticks.
             end
         end
     end
-end;
+end
 
 function onMobDeath(mob, player, isKiller)
-end;
+end
