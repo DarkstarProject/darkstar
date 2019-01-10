d54f0d55550de0202a844193f518ff90f66b0d8e
TeoTwawki <arawn.annwn@yandex.com>
2019-01-09 23:01:04 -0500

    Correct spawn behavior of Lumber Jack - he does not spawn with enmity
    Todos:
    1) Willow should repop in 30 min if Jack despawns without being killed.
    2) does Jack agro at any level?

--- scripts/zones/Batallia_Downs/mobs/Weeping_Willow.lua
+++ scripts/zones/Batallia_Downs/mobs/Weeping_Willow.lua
@@ -5,17 +5,20 @@
 
 function onMobFight(mob,target)
     if (mob:getHPP() <= 50 and mob:getLocalVar("Saplings") < 1) then
-        SpawnMob(mob:getID()+1):updateEnmity(target);
-        SpawnMob(mob:getID()+2):updateEnmity(target);
-        SpawnMob(mob:getID()+3):updateEnmity(target);
-        SpawnMob(mob:getID()+4):updateEnmity(target);
-        SpawnMob(mob:getID()+5):updateEnmity(target);
-        mob:setLocalVar("Saplings", 1);
+        SpawnMob(mob:getID()+1):updateEnmity(target)
+        SpawnMob(mob:getID()+2):updateEnmity(target)
+        SpawnMob(mob:getID()+3):updateEnmity(target)
+        SpawnMob(mob:getID()+4):updateEnmity(target)
+        SpawnMob(mob:getID()+5):updateEnmity(target)
+        mob:setLocalVar("Saplings", 1)
     end
-end;
+end
 
 function onMobDeath(mob, player, isKiller)
-    local JACK = mob:getID()+6;
-    SpawnMob(JACK):updateEnmity(player);
+end
+
+function onMobDespawn(mob)
+    local JACK = mob:getID()+6
+    SpawnMob(JACK)
     GetMobByID(JACK):setPos( mob:getXPos(), mob:getYPos(), mob:getZPos(), 0);
-end;
+end
