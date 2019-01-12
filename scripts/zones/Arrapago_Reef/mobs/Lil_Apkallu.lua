6648176039ee931c3520f774f9afd1192955969c
KnowOne134 <35616771+KnowOne134@users.noreply.github.com>
2019-01-12 13:40:13 -0600

    Onmobspawn

--- scripts/zones/Arrapago_Reef/mobs/Lil_Apkallu.lua
+++ scripts/zones/Arrapago_Reef/mobs/Lil_Apkallu.lua
@@ -9,6 +9,9 @@ require("scripts/globals/status")
 
 function onMobInitialize(mob)
     mob:setMobMod(dsp.mobMod.IDLE_DESPAWN, 300)
+end
+
+function onMobSpawn(mob)
     mob:setLocalVar("[rage]timer", 3600) -- 60 minutes
 end
 
