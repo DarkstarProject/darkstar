fd66b67d18071a2c36d45e0aa62e752a576154e3
David Norris <Knowone134@gmail.com>
2019-01-13 21:16:45 -0600

    amber scutum stacks

--- scripts/globals/mobskills/amber_scutum.lua
+++ scripts/globals/mobskills/amber_scutum.lua
@@ -5,7 +5,6 @@
 -- Type: Enhancing
 -- Utsusemi/Blink absorb: N/A
 -- Range: Self
--- TODO: be able to stack the buff over itself for increased DEF
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/status")
@@ -16,9 +15,14 @@ function onMobSkillCheck(target,mob,skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = dsp.effect.DEFENSE_BOOST
-    
-    skill:setMsg(MobBuffMove(mob, typeEffect, 50, 0, 60))
+    local status = mob:getStatusEffect(dsp.effect.DEFENSE_BOOST)
+    local power = 100
+    if status ~= nil then
+        -- This is as accurate as we get until effects applied by mob moves can use subpower..
+        power = status:getPower() * 2
+    end
 
-    return typeEffect
+    skill:setMsg(MobBuffMove(mob, dsp.effect.DEFENSE_BOOST, power, 0, 60))
+
+    return dsp.effect.DEFENSE_BOOST
 end
--- scripts/zones/Mount_Zhayolm/mobs/Brass_Borer.lua
+++ scripts/zones/Mount_Zhayolm/mobs/Brass_Borer.lua
@@ -5,7 +5,7 @@
 mixins = {require("scripts/mixins/rage")}
 require("scripts/globals/status")
 ----------------------------------
--- TODO: Halting movement during stance change. Stack Amber Scutum Def Boost effects for super cannonball
+-- TODO: Halting movement during stance change.
 function onMobInitialize(mob)
     mob:setMobMod(dsp.mobMod.IDLE_DESPAWN, 300)
 end
@@ -14,6 +14,7 @@ function onMobSpawn(mob)
     mob:setLocalVar("[rage]timer", 3600) -- 60 minutes
     mob:setLocalVar("formTime", os.time() + math.random(43,47))
     mob:setLocalVar("defUp", math.random(25,50))
+    mob:setLocalVar("DEF",math.random(3,5))
     mob:setMobMod(dsp.mobMod.ADD_EFFECT, 1)
 end
 
@@ -41,7 +42,7 @@ function onMobFight(mob,target)
         mob:delMod(dsp.mod.MDEF,10)
         mob:setLocalVar("formTime", os.time() + math.random(43,47))
     end
-    if mob:getHPP() < mob:getLocalVar("defUp") and mob:getLocalVar("usedMainSpec") <= 5 then
+    if mob:getHPP() < mob:getLocalVar("defUp") and mob:getLocalVar("usedMainSpec") <= mob:getLocalVar("DEF") then
         mob:useMobAbility(1815)
         mob:setLocalVar("usedMainSpec", mob:getLocalVar("usedMainSpec") +1)
     elseif mob:getLocalVar("usedMainSpec") >= 5 and mob:getLocalVar("Cannonball") == 0 then
