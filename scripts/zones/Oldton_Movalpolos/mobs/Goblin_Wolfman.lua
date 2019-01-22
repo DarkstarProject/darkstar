f3f1433ba8a14b026b7b33647dea1173c07fe5cd
Justin Foehner <jmf9808@rit.edu>
2019-01-21 20:35:10 -0500

    Added suggested changes

--- scripts/zones/Oldton_Movalpolos/mobs/Goblin_Wolfman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Goblin_Wolfman.lua
@@ -2,7 +2,6 @@
 -- Area: Oldton Movalpolos
 --  MOB: Goblin Wolfman
 -----------------------------------
-local ID = require("scripts/zones/Oldton_Movalpolos/IDs");
 require("scripts/globals/settings");
 require("scripts/globals/status");
 -----------------------------------
@@ -12,15 +11,4 @@ function onMobInitialize(mob)
 end;
 
 function onMobDeath(mob, player, isKiller)
-end;
-
-function onMobDespawn(mob)
-    local npc = GetNPCByID(ID.npc.SCRAWLED_WRITING);
-        switch (math.random(1,3)): caseof
-        {
-            [1] = function (x) npc:setPos(-16.806, 7.718, 14.155); end,
-            [2] = function (x) npc:setPos(-18.0, 12.0, -115.0); end, --approximate retail position 
-            [3] = function (x) npc:setPos(-150.0, 8.0, -252.0); end, --approximate retial position
-        }
-    npc:updateNPCHideTime(900); --15 minute until unhide
 end;
