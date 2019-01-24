5a5eabd3262704470b56abcef4940d8e0cfa5d97
Brian Kesecker <45717129+bkesecker@users.noreply.github.com>
2019-01-24 09:27:32 -0500

    Refactor and clean-up
    Still need to test and verify

--- scripts/zones/Uleguerand_Range/npcs/qm3.lua
+++ scripts/zones/Uleguerand_Range/npcs/qm3.lua
@@ -15,8 +15,8 @@ function onTrade(player,npc,trade)
     local louverancesPath = player:getVar("COP_Louverance_s_Path")
 
     -- Taking a logical guess what criteria displays what message.
-    if (overTheHillsAndFarAway == QUEST_ACCEPTED and npcUtil.tradeHas(trade, 1729)) then -- 1729 = Moblin Hotrok
-        if (louverancesPath >= 10) then
+    if overTheHillsAndFarAway == QUEST_ACCEPTED and npcUtil.tradeHas(trade, 1729) then -- 1729 = Moblin Hotrok
+        if louverancesPath >= 10 then
             if (player:hasTitle(dsp.title.COMPANION_OF_LOUVERANCE) or player:hasTitle(dsp.title.TRUE_COMPANION_OF_LOUVERANCE)) then
                 player:startEvent(10, 0, 1729, dsp.ki.MAP_OF_THE_ULEGUERAND_RANGE, 0, 0, 1, 0)
                 --                                                                       ^ 1 = Remembers you and girl from Tavnazia and asks to TRADE dagger for map
@@ -25,7 +25,7 @@ function onTrade(player,npc,trade)
                 --                                                                 ^ 1   ^ 1 = Remembers you and girl from Tavnazia and asks for the dagger   
             end
         else
-            if (player:hasTitle(dsp.title.COMPANION_OF_LOUVERANCE) or player:hasTitle(dsp.title.TRUE_COMPANION_OF_LOUVERANCE)) then
+            if player:hasTitle(dsp.title.COMPANION_OF_LOUVERANCE) or player:hasTitle(dsp.title.TRUE_COMPANION_OF_LOUVERANCE) then
                 player:startEvent(10, 0, 1729, dsp.ki.MAP_OF_THE_ULEGUERAND_RANGE, 0, 1, 0, 0)
                 --                                                                    ^ 1 = Doesn't recognize you and asks to TRADE dagger for map
                 -- or
@@ -43,9 +43,9 @@ function onTrigger(player,npc)
 
     local overTheHillsAndFarAway = player:getQuestStatus(SANDORIA, OVER_THE_HILLS_AND_FAR_AWAY)
 
-    if (overTheHillsAndFarAway == QUEST_COMPLETED) then
+    if overTheHillsAndFarAway == QUEST_COMPLETED then
         player:messageSpecial(ID.text.NOTHING_OUT_OF_ORDINARY)
-    elseif (overTheHillsAndFarAway == QUEST_ACCEPTED) then
+    elseif overTheHillsAndFarAway == QUEST_ACCEPTED then
         player:messageSpecial(ID.text.SOMETHING_GLITTERING)
         player:messageSpecial(ID.text.WHAT_LIES_BENEATH, 0, 1729)     
     else
@@ -59,15 +59,8 @@ end
 
 function onEventFinish(player,csid,option)
 
-    if (csid == 10) then
+    if csid == 10 and npcUtil.completeQuest(player, SANDORIA, OVER_THE_HILLS_AND_FAR_AWAY, {gil = 2000, xp = 2000, ki = dsp.ki.MAP_OF_THE_ULEGUERAND_RANGE}) then
         player:confirmTrade()
-        player:addGil(GIL_RATE * 2000)
-        player:messageSpecial(ID.text.GIL_OBTAINED, GIL_RATE * 2000)
-        player:addExp(EXP_RATE * 2000)
-        player:addKeyItem(dsp.ki.MAP_OF_THE_ULEGUERAND_RANGE)
-        player:messageSpecial(ID.text.KEYITEM_OBTAINED, dsp.ki.MAP_OF_THE_ULEGUERAND_RANGE)
-        player:addFame(SANDORIA, 30)
-        player:completeQuest(SANDORIA, OVER_THE_HILLS_AND_FAR_AWAY)
     end
 
 end
