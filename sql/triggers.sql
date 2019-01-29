1436deed345a4d9413c8d2fc38206f0cd00f2c82
David Norris <Knowone134@gmail.com>
2019-01-28 18:28:59 -0600

    changed to update

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -45,28 +45,28 @@ CREATE TRIGGER char_delete
     BEFORE DELETE ON chars
     FOR EACH ROW
 BEGIN
-    DELETE FROM `account_ip_record` WHERE `charid` = OLD.charid;
-    DELETE FROM `char_blacklist`    WHERE `charid_owner` = OLD.charid;
-    DELETE FROM `char_effects`      WHERE `charid` = OLD.charid;
-    DELETE FROM `char_equip`        WHERE `charid` = OLD.charid;
-    DELETE FROM `char_exp`          WHERE `charid` = OLD.charid;
-    DELETE FROM `char_inventory`    WHERE `charid` = OLD.charid;
-    DELETE FROM `char_jobs`         WHERE `charid` = OLD.charid;
-    DELETE FROM `char_look`         WHERE `charid` = OLD.charid;
-    DELETE FROM `char_merit`        WHERE `charid` = OLD.charid;
-    DELETE FROM `char_pet`          WHERE `charid` = OLD.charid;
-    DELETE FROM `char_points`       WHERE `charid` = OLD.charid;
-    DELETE FROM `char_unlocks`      WHERE `charid` = OLD.charid;
-    DELETE FROM `char_profile`      WHERE `charid` = OLD.charid;
-    DELETE FROM `char_recast`       WHERE `charid` = OLD.charid;
-    DELETE FROM `char_skills`       WHERE `charid` = OLD.charid;
-    DELETE FROM `char_spells`       WHERE `charid` = OLD.charid;
-    DELETE FROM `char_stats`        WHERE `charid` = OLD.charid;
-    DELETE FROM `char_storage`      WHERE `charid` = OLD.charid;
-    DELETE FROM `char_style`        WHERE `charid` = OLD.charid;
-    DELETE FROM `char_vars`         WHERE `charid` = OLD.charid;
-    DELETE FROM `auction_house`     WHERE `seller` = OLD.charid;
-    DELETE FROM `delivery_box`      WHERE `charid` = OLD.charid;
+    DELETE FROM `char_blacklist` WHERE `charid_owner` = OLD.charid;
+    DELETE FROM `char_effects`   WHERE `charid` = OLD.charid;
+    DELETE FROM `char_equip`     WHERE `charid` = OLD.charid;
+    DELETE FROM `char_exp`       WHERE `charid` = OLD.charid;
+    DELETE FROM `char_inventory` WHERE `charid` = OLD.charid;
+    DELETE FROM `char_jobs`      WHERE `charid` = OLD.charid;
+    DELETE FROM `char_look`      WHERE `charid` = OLD.charid;
+    DELETE FROM `char_merit`     WHERE `charid` = OLD.charid;
+    DELETE FROM `char_pet`       WHERE `charid` = OLD.charid;
+    DELETE FROM `char_points`    WHERE `charid` = OLD.charid;
+    DELETE FROM `char_unlocks`   WHERE `charid` = OLD.charid;
+    DELETE FROM `char_profile`   WHERE `charid` = OLD.charid;
+    DELETE FROM `char_recast`    WHERE `charid` = OLD.charid;
+    DELETE FROM `char_skills`    WHERE `charid` = OLD.charid;
+    DELETE FROM `char_spells`    WHERE `charid` = OLD.charid;
+    DELETE FROM `char_stats`     WHERE `charid` = OLD.charid;
+    DELETE FROM `char_storage`   WHERE `charid` = OLD.charid;
+    DELETE FROM `char_style`     WHERE `charid` = OLD.charid;
+    DELETE FROM `char_vars`      WHERE `charid` = OLD.charid;
+    DELETE FROM `auction_house`  WHERE `seller` = OLD.charid;
+    DELETE FROM `delivery_box`   WHERE `charid` = OLD.charid;
+    UPDATE `accounts_ip_record` SET `charid`  = 0 where `charid` = OLD.charid;
     UPDATE `delivery_box` SET sent = 0 WHERE box = 2 AND received = 0 AND sent = 1 AND senderid = OLD.charid;
 END $$
 
