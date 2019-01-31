f2e1296bd936611b35dc5bd7201418eb48ff481c
Matthew James Davis <davismj@foursails.co>
2019-01-31 22:33:59 +0900

    ws: Make the attack multiplier vary by TP
    Iron Tempest and Sturmwind both receive a varying attack modifier based on TP. This change renames params.atkmulti to params.atk100, params.atk200, params.atk300, consistent with other TP-varying parameters.
    
    Iron Tempest and Sturmwind are given a 2.0 and 3.5 multipler at 2 and 3 tp respectively according to jp wiki figures.
    
    Sturmwind: http://wiki.ffo.jp/html/860.html
    Iron Tempest: http://wiki.ffo.jp/html/859.html

--- scripts/globals/weaponskills/catastrophe.lua
+++ scripts/globals/weaponskills/catastrophe.lua
@@ -26,7 +26,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
-    params.atkmulti = 1
+    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1;
 
     if USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.str_wsc = 0.4 params.agi_wsc = 0.0 params.int_wsc = 0.4
@@ -38,7 +38,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
         local drain = math.floor(damage * 0.4)
         player:addHP(drain)
     end
-    
+
     -- Apply aftermath
     if damage > 0 then
         dsp.aftermath.addStatusEffect(player, tp, dsp.slot.MAIN, dsp.aftermath.type.RELIC)
--- scripts/globals/weaponskills/exenterator.lua
+++ scripts/globals/weaponskills/exenterator.lua
@@ -27,7 +27,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
-    params.atkmulti = 1.0
+    params.atk100 = 1.0; params.atk200 = 1.0; params.atk300 = 1.0;
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.agi_wsc = 0.7 + (player:getMerit(dsp.merit.EXENTERATOR) / 100)
