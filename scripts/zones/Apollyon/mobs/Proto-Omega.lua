a46e83ce06f36d13f92d117e519e699df6d19098
wrenffxi <oliviapekoe@gmail.com>
2019-02-07 21:55:17 -0500

    proto-thing immunities (from wiki and watching vids) proto-omega counter, regen, and movement speed

--- scripts/zones/Apollyon/mobs/Proto-Omega.lua
+++ scripts/zones/Apollyon/mobs/Proto-Omega.lua
@@ -9,6 +9,8 @@ require("scripts/globals/mobs")
 
 function onMobInitialize(mob)
     mob:setMobMod(dsp.mobMod.ADD_EFFECT, 1);
+    mob:setMod(dsp.mod.COUNTER, 10) -- "Possesses a Counter trait"
+    mob:setMod(dsp.mod.REGEN, 25) -- "Posseses an Auto-Regen (low to moderate)"
 end;
 
 function onMobSpawn(mob)
@@ -16,6 +18,7 @@ function onMobSpawn(mob)
     mob:setMod(dsp.mod.UDMGPHYS, -75);
     mob:setMod(dsp.mod.UDMGRANGE, -75);
     mob:setMod(dsp.mod.UDMGMAGIC, 0);
+    mob:setMod(dsp.mod.MOVE, 100) -- "Moves at Flee Speed in Quadrupedal stance and in the Final Form"
 end;
 
 function onMobFight(mob,target)
@@ -32,6 +35,7 @@ function onMobFight(mob,target)
         mob:setMod(dsp.mod.UDMGPHYS, 0);
         mob:setMod(dsp.mod.UDMGRANGE, 0);
         mob:setMod(dsp.mod.UDMGMAGIC, -75);
+        mob:setMod(dsp.mod.MOVE, 0)
     end
 
     if (currentForm == 1) then
@@ -49,6 +53,7 @@ function onMobFight(mob,target)
             mob:setMod(dsp.mod.UDMGPHYS, -50);
             mob:setMod(dsp.mod.UDMGRANGE, -50);
             mob:setMod(dsp.mod.UDMGMAGIC, -50);
+            mob:setMod(dsp.mod.MOVE, 100)
             mob:addStatusEffect(dsp.effect.REGAIN,7,3,0); -- The final form has Regain,
             mob:getStatusEffect(dsp.effect.REGAIN):setFlag(dsp.effectFlag.DEATH);
             currentForm = 2;
