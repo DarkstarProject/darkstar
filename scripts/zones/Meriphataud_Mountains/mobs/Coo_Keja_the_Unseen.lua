eecd68f2464923281175dd33aa6ffbaf92473ebb
wrenffxi <oliviapekoe@gmail.com>
2019-02-15 09:34:33 -0500

    mobs

--- scripts/zones/Meriphataud_Mountains/mobs/Coo_Keja_the_Unseen.lua
+++ scripts/zones/Meriphataud_Mountains/mobs/Coo_Keja_the_Unseen.lua
@@ -2,6 +2,17 @@
 -- Area: Meriphataud Mountains (119)
 --   NM: Coo Keja the Unseen
 -----------------------------------
+mixins = {require("scripts/mixins/job_special")}
+-----------------------------------
+
+function onMobSpawn(mob)
+    dsp.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            {id = dsp.jsa.MIJIN_GAKURE, hpp = math.random(10, 15)},
+        },
+    })
+end
 
 function onMobDeath(mob, player, isKiller)
 end
