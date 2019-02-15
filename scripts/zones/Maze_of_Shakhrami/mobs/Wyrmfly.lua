76a1712d134a6fed6e806ad8572809ee48de2ab1
wrenffxi <oliviapekoe@gmail.com>
2019-02-15 12:51:31 -0500

    mobs

--- scripts/zones/Maze_of_Shakhrami/mobs/Wyrmfly.lua
+++ scripts/zones/Maze_of_Shakhrami/mobs/Wyrmfly.lua
@@ -6,7 +6,16 @@
 local ID = require("scripts/zones/Maze_of_Shakhrami/IDs")
 require("scripts/globals/quests")
 require("scripts/globals/status")
+require("scripts/globals/mobs")
 -----------------------------------
+
+function onMobInitialize(mob)
+    mob:setMobMod(dsp.mobMod.ADD_EFFECT, 1)
+end
+
+function onAdditionalEffect(mob, target, damage)
+    return dsp.mob.onAddEffect(mob, target, damage, dsp.mob.ae.POISON)
+end
 
 function onMobDeath(mob, player, isKiller)
     if player:getQuestStatus(WINDURST, ECO_WARRIOR_WIN) ~= QUEST_AVAILABLE and player:getVar("ECO_WARRIOR_ACTIVE") == 238 and player:hasStatusEffect(dsp.effect.LEVEL_RESTRICTION) then
