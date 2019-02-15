c3c0e0b2cd5b7d8fbd680274efa94e3e60883407
wrenffxi <oliviapekoe@gmail.com>
2019-02-15 12:45:38 -0500

    style guide

--- scripts/zones/Maze_of_Shakhrami/mobs/Lost_Soul.lua
+++ scripts/zones/Maze_of_Shakhrami/mobs/Lost_Soul.lua
@@ -1,10 +1,10 @@
 -----------------------------------
 -- Area: Maze of Shakhrami
---  MOB: Lost_Soul
+--   NM: Lost_Soul
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
-    if (player:getVar("EquippedforAllOccasions") == 1) then
-        player:setVar("EquippedforAllOccasions",2);
+    if player:getVar("EquippedforAllOccasions") == 1 then
+        player:setVar("EquippedforAllOccasions", 2)
     end
-end;
+end
--- scripts/zones/Maze_of_Shakhrami/npcs/qm2.lua
+++ scripts/zones/Maze_of_Shakhrami/npcs/qm2.lua
@@ -4,38 +4,39 @@
 -- Type: Quest NPC
 -- !pos 143 9 -219 198
 -----------------------------------
-local ID = require("scripts/zones/Maze_of_Shakhrami/IDs");
-require("scripts/globals/keyitems");
-require("scripts/globals/npc_util");
-require("scripts/globals/quests");
-require("scripts/globals/status");
+local ID = require("scripts/zones/Maze_of_Shakhrami/IDs")
+require("scripts/globals/keyitems")
+require("scripts/globals/npc_util")
+require("scripts/globals/quests")
+require("scripts/globals/status")
 -----------------------------------
 
-function onTrade(player,npc,trade)
-end;
+function onTrade(player, npc, trade)
+end
 
-function onTrigger(player,npc)
-    if (player:getQuestStatus(WINDURST,ECO_WARRIOR_WIN) ~= QUEST_AVAILABLE and
+function onTrigger(player, npc)
+    if
+        player:getQuestStatus(WINDURST, ECO_WARRIOR_WIN) ~= QUEST_AVAILABLE and
         player:getVar("ECO_WARRIOR_ACTIVE") == 238 and
         player:hasStatusEffect(dsp.effect.LEVEL_RESTRICTION) and
         not player:hasKeyItem(dsp.ki.INDIGESTED_MEAT)
-    ) then
-        if (player:getVar("ECOR_WAR_WIN-NMs_killed") == 1) then
-            npcUtil.giveKeyItem(player, dsp.ki.INDIGESTED_MEAT);
-        elseif (
+    then
+        if player:getVar("ECOR_WAR_WIN-NMs_killed") == 1 then
+            npcUtil.giveKeyItem(player, dsp.ki.INDIGESTED_MEAT)
+        elseif
             not GetMobByID(ID.mob.WYRMFLY_OFFSET+0):isSpawned() and
             not GetMobByID(ID.mob.WYRMFLY_OFFSET+1):isSpawned() and
             not GetMobByID(ID.mob.WYRMFLY_OFFSET+2):isSpawned()
-        ) then
-            SpawnMob(ID.mob.WYRMFLY_OFFSET+0):updateClaim(player);
-            SpawnMob(ID.mob.WYRMFLY_OFFSET+1):updateClaim(player);
-            SpawnMob(ID.mob.WYRMFLY_OFFSET+2):updateClaim(player);
+        then
+            SpawnMob(ID.mob.WYRMFLY_OFFSET+0):updateClaim(player)
+            SpawnMob(ID.mob.WYRMFLY_OFFSET+1):updateClaim(player)
+            SpawnMob(ID.mob.WYRMFLY_OFFSET+2):updateClaim(player)
         end
     end
-end;
+end
 
-function onEventUpdate(player,csid,option)
-end;
+function onEventUpdate(player, csid, option)
+end
 
-function onEventFinish(player,csid,option)
-end;
+function onEventFinish(player, csid, option)
+end
