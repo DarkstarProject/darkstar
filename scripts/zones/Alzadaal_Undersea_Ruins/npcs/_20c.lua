c0b8cb01ef2b2e7a855e8555978731e73dad53f6
David Norris <Knowone134@gmail.com>
2019-02-20 18:50:44 -0600

    style

--- scripts/zones/Alzadaal_Undersea_Ruins/npcs/_20c.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/npcs/_20c.lua
@@ -8,15 +8,15 @@ require("scripts/globals/besieged")
 local ID = require("scripts/zones/Alzadaal_Undersea_Ruins/IDs")
 -----------------------------------
 
-function onTrigger(player,npc)
+function onTrigger(player, npc)
     if player:hasKeyItem(dsp.keyItem.NYZUL_ISLE_ASSAULT_ORDERS) then
         player:messageSpecial(ID.text.CANNOT_LEAVE, dsp.keyItem.NYZUL_ISLE_ASSAULT_ORDERS)
     elseif player:getZPos() >= 77 and player:getZPos() <= 79 then
-        player:messageSpecial(ID.text.STAGING_POINT)
+        player:messageSpecial(ID.text.STAGING_POINT_NYZULE)
         player:messageSpecial(ID.text.IMPERIAL_CONTROL)
         player:startEvent(107)
     elseif player:getZPos() >= 80 and player:getZPos() <= 82 then
-        player:messageSpecial(ID.text.STAGING_POINT)
+        player:messageSpecial(ID.text.STAGING_POINT_NYZULE)
         player:messageSpecial(ID.text.IMPERIAL_CONTROL)
         player:startEvent(106)
     else
@@ -24,10 +24,10 @@ function onTrigger(player,npc)
     end
 end
 
-function onEventUpdate(player,csid,option,target)
+function onEventUpdate(player, csid, option)
 end
 
-function onEventFinish(player,csid,option,target)
+function onEventFinish(player, csid, option)
 --[[    if csid == 106 and option == 0 then
         Todo add function that when entering staging point that a player looses all agro on mobs
     end]]
--- scripts/zones/Alzadaal_Undersea_Ruins/npcs/_20d.lua
+++ scripts/zones/Alzadaal_Undersea_Ruins/npcs/_20d.lua
@@ -7,15 +7,15 @@ require("scripts/globals/keyitems")
 local ID = require("scripts/zones/Alzadaal_Undersea_Ruins/IDs")
 -----------------------------------
 
-function onTrigger(player,npc)
+function onTrigger(player, npc)
     if player:hasKeyItem(dsp.keyItem.NYZUL_ISLE_ASSAULT_ORDERS) then
         player:messageSpecial(ID.text.CANNOT_LEAVE, dsp.keyItem.NYZUL_ISLE_ASSAULT_ORDERS)
     elseif player:getZPos() >= -39.1 and player:getZPos() <= -37 then
-        player:messageSpecial(ID.text.STAGING_POINT)
+        player:messageSpecial(ID.text.STAGING_POINT_NYZUL)
         player:messageSpecial(ID.text.IMPERIAL_CONTROL)
         player:startEvent(115)
     elseif player:getZPos() >= -42 and player:getZPos() <= -40 then
-        player:messageSpecial(ID.text.STAGING_POINT)
+        player:messageSpecial(ID.text.STAGING_POINT_NYZUL)
         player:messageSpecial(ID.text.IMPERIAL_CONTROL)
         player:startEvent(114)
     else
@@ -23,10 +23,10 @@ function onTrigger(player,npc)
     end
 end
 
-function onEventUpdate(player,csid,option,target)
+function onEventUpdate(player, csid, option)
 end
 
-function onEventFinish(player,csid,option,target)
+function onEventFinish(player, csid, option)
 --[[    if csid == 114 and option == 0 then
         Todo add function that when entering staging point that a player looses all agro on mobs
     end]]
