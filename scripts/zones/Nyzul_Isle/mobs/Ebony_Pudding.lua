55248f047c6f4d18f38e9614304d9616273be64e
David Norris <Knowone134@gmail.com>
2019-03-26 22:23:01 -0500

    2 mobs share all mobs and specified group

--- scripts/zones/Nyzul_Isle/mobs/Ebony_Pudding.lua
+++ scripts/zones/Nyzul_Isle/mobs/Ebony_Pudding.lua
@@ -6,7 +6,7 @@
 
 function onMobDeath(mob, player, isKiller)
     local instance = mob:getInstance()
-    if isKiller then
+    if (instance:getStage() == nyzul.objective.ELIMINATE_SPECIFIED_ENEMIES or instance:getStage() == nyzul.objective.ELIMINATE_ALL_ENEMIES) and isKiller then
         instance:setProgress(instance:getProgress() +1)
     end
 end
