6c5a6fd854f89f3eee525227d3d25b048858342f
isotor <heavyarms@protonmail.com>
2019-04-19 17:02:59 +0300

    forgot thr 3 <_<

--- scripts/globals/mobskills/drain_whip.lua
+++ scripts/globals/mobskills/drain_whip.lua
@@ -22,7 +22,7 @@ function onMobWeaponSkill(target, mob, skill)
     local info = MobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, dsp.magic.ele.DARK, dmgmod, TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, dsp.attackType.MAGICAL, dsp.damageType.DARK, MOBPARAM_IGNORE_SHADOWS)
 
-    local rnd = math.random()
+    local rnd = math.random(3)
     if rnd == 1 then
         drainEffect = MOBDRAIN_TP
     elseif rnd == 2 then
