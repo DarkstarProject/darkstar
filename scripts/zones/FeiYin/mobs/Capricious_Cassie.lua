3fd4083ae31a3f0cd653fe4629643742f6471a6d
isotor <heavyarms@protonmail.com>
2019-04-19 11:49:08 +0300

    add alliance draw-in mod to Cassie

--- scripts/zones/FeiYin/mobs/Capricious_Cassie.lua
+++ scripts/zones/FeiYin/mobs/Capricious_Cassie.lua
@@ -6,14 +6,15 @@ mixins = {require("scripts/mixins/rage")}
 require("scripts/globals/titles")
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(dsp.mobMod.DRAW_IN, 2)
+end
+
 function onMobDeath(mob, player, isKiller)
-    player:addTitle(dsp.title.CASSIENOVA);
-end;
+    player:addTitle(dsp.title.CASSIENOVA)
+end
 
 function onMobDespawn(mob)
-
-    -- Set Capricious_Cassie's spawnpoint and respawn time (21-24 hours)
-    UpdateNMSpawnPoint(mob:getID());
-    mob:setRespawnTime(math.random(75600,86400));
-
-end;
+    UpdateNMSpawnPoint(mob:getID())
+    mob:setRespawnTime(math.random(75600, 86400)) -- 21-24 hours
+end
