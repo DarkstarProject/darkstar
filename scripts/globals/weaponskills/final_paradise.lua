e7706b4d6b435d58d7d2d9e2236698f4435f7fc4
TeoTwawki <TeoTwawki@users.noreply.github.com>
2019-05-03 07:07:52 -0400

    OCD attack

--- scripts/globals/weaponskills/final_paradise.lua
+++ scripts/globals/weaponskills/final_paradise.lua
@@ -9,13 +9,12 @@
 -- 100%TP    200%TP    300%TP
 -- 1.00      1.50      2.00
 -----------------------------------
-require("scripts/globals/status")
-require("scripts/globals/settings")
 require("scripts/globals/weaponskills")
+require("scripts/globals/settings")
+require("scripts/globals/status")
 -----------------------------------
 
 function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
-
     local params = {}
     params.numHits = 2
     params.ftp100 = 1 params.ftp200 = 1.5 params.ftp300 = 2
@@ -23,7 +22,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
-    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1;
+    params.atk100 = 1 params.atk200 = 1 params.atk300 = 1
 
     if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
         params.vit_wsc = 0.8
@@ -31,5 +30,4 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, tp, primary, action, taChar, params)
     return tpHits, extraHits, criticalHit, damage
-
 end
