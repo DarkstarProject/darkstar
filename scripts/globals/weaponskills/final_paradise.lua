4baf1d84d729d40b0225da60aafcc5f8e6a048da
TeoTwawki <TeoTwawki@users.noreply.github.com>
2019-05-03 13:45:14 -0400

    more ocd

--- scripts/globals/weaponskills/final_paradise.lua
+++ scripts/globals/weaponskills/final_paradise.lua
@@ -24,7 +24,7 @@ function onUseWeaponSkill(player, target, wsID, tp, primary, action, taChar)
     params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
     params.atk100 = 1 params.atk200 = 1 params.atk300 = 1
 
-    if (USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
+    if USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.vit_wsc = 0.8
     end
 
