a2b4a3e7a5f2af1f556c5e0820ba22b8acfaddb7
isotor <heavyarms@protonmail.com>
2019-05-06 08:05:10 +0300

    Fix so obtaining synth support removes any prior support + formatting

--- scripts/zones/Bastok_Markets/npcs/Ulrike.lua
+++ scripts/zones/Bastok_Markets/npcs/Ulrike.lua
@@ -4,36 +4,37 @@
 -- Type: Goldsmithing Synthesis Image Support
 -- !pos -218.399 -7.824 -56.203 235
 -----------------------------------
-require("scripts/globals/status");
-require("scripts/globals/crafting");
-local ID = require("scripts/zones/Bastok_Markets/IDs");
+local ID = require("scripts/zones/Bastok_Markets/IDs")
+require("scripts/globals/status")
+require("scripts/globals/crafting")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
-    local guildMember = isGuildMember(player,6);
-    local SkillCap = getCraftSkillCap(player, dsp.skill.GOLDSMITHING);
-    local SkillLevel = player:getSkillLevel(dsp.skill.GOLDSMITHING);
+    local guildMember = isGuildMember(player, 6)
+    local SkillCap = getCraftSkillCap(player, dsp.skill.GOLDSMITHING)
+    local SkillLevel = player:getSkillLevel(dsp.skill.GOLDSMITHING)
 
-    if (guildMember == 1) then
-        if (player:hasStatusEffect(dsp.effect.GOLDSMITHING_IMAGERY) == false) then
-            player:startEvent(304,SkillCap,SkillLevel,2,201,player:getGil(),0,9,0);
+    if guildMember == 1 then
+        if player:hasStatusEffect(dsp.effect.GOLDSMITHING_IMAGERY) == false then
+            player:startEvent(304, SkillCap, SkillLevel, 2, 201, player:getGil(), 0, 9, 0)
         else
-            player:startEvent(304,SkillCap,SkillLevel,2,201,player:getGil(),6975,9,0);
+            player:startEvent(304, SkillCap, SkillLevel, 2, 201, player:getGil(), 6975, 9, 0)
         end
     else
-        player:startEvent(304);
+        player:startEvent(304)
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-    if (csid == 304 and option == 1) then
-        player:messageSpecial(ID.text.GOLDSMITHING_SUPPORT,0,3,2);
-        player:addStatusEffect(dsp.effect.GOLDSMITHING_IMAGERY,1,0,120);
+    if csid == 304 and option == 1 then
+        player:delStatusEffectsByFlag(dsp.effectFlag.SYNTH_SUPPORT, true)
+        player:addStatusEffect(dsp.effect.GOLDSMITHING_IMAGERY, 1, 0, 120)
+        player:messageSpecial(ID.text.GOLDSMITHING_SUPPORT, 0, 3, 2)
     end
-end;
+end
--- scripts/zones/Metalworks/npcs/Romero.lua
+++ scripts/zones/Metalworks/npcs/Romero.lua
@@ -4,36 +4,37 @@
 -- Type: Smithing Synthesis Image Support
 -- !pos -106.336 2.000 26.117 237
 -----------------------------------
-require("scripts/globals/status");
-require("scripts/globals/crafting");
-local ID = require("scripts/zones/Metalworks/IDs");
+local ID = require("scripts/zones/Metalworks/IDs")
+require("scripts/globals/status")
+require("scripts/globals/crafting")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
-    local guildMember = isGuildMember(player,8);
-    local SkillCap = getCraftSkillCap(player,dsp.skill.SMITHING);
-    local SkillLevel = player:getSkillLevel(dsp.skill.SMITHING);
+    local guildMember = isGuildMember(player, 8)
+    local SkillCap = getCraftSkillCap(player, dsp.skill.SMITHING)
+    local SkillLevel = player:getSkillLevel(dsp.skill.SMITHING)
 
-    if (guildMember == 1) then
-        if (player:hasStatusEffect(dsp.effect.SMITHING_IMAGERY) == false) then
-            player:startEvent(105,SkillCap,SkillLevel,2,207,player:getGil(),0,0,0);
+    if guildMember == 1 then
+        if player:hasStatusEffect(dsp.effect.SMITHING_IMAGERY) == false then
+            player:startEvent(105, SkillCap, SkillLevel, 2, 207, player:getGil(), 0, 0, 0)
         else
-            player:startEvent(105,SkillCap,SkillLevel,2,207,player:getGil(),7127,0,0);
+            player:startEvent(105, SkillCap, SkillLevel, 2, 207, player:getGil(), 7127, 0, 0)
         end
     else
-        player:startEvent(105); -- Standard Dialogue
+        player:startEvent(105)
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-    if (csid == 105 and option == 1) then
-        player:messageSpecial(ID.text.SMITHING_SUPPORT,0,2,2);
-        player:addStatusEffect(dsp.effect.SMITHING_IMAGERY,1,0,120);
+    if csid == 105 and option == 1 then
+        player:delStatusEffectsByFlag(dsp.effectFlag.SYNTH_SUPPORT, true)
+        player:addStatusEffect(dsp.effect.SMITHING_IMAGERY, 1, 0, 120)
+        player:messageSpecial(ID.text.SMITHING_SUPPORT, 0, 2, 2)
     end
-end;
+end
