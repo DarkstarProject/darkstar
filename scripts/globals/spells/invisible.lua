ab8fcf156ca095d6865fc8cd74fb15214c9566b4
isotor <heavyarms@protonmail.com>
2019-05-07 18:41:13 +0300

    Create sneak and invis duration mods

--- scripts/globals/spells/invisible.lua
+++ scripts/globals/spells/invisible.lua
@@ -1,12 +1,13 @@
 -----------------------------------------
 -- Spell: Invisible
 -- Lessens chance of being detected by sight.
--- Duration is random number between 30 seconds and 5 minutes
+-- Duration is random number between 30 seconds and 5 minutes.
 -----------------------------------------
 require("scripts/globals/magic")
 require("scripts/globals/msg")
 require("scripts/globals/settings")
 require("scripts/globals/status")
+-----------------------------------------
 
 function onMagicCastingCheck(caster, target, spell)
     return 0
@@ -15,19 +16,14 @@ end
 function onSpellCast(caster, target, spell)
     if not target:hasStatusEffect(dsp.effect.INVISIBLE) then
 
-        -- last 7-9 minutes
-        local duration = calculateDuration(math.random(420, 540), spell:getSkillType(), spell:getSpellGroup(), caster, target)
-        duration = calculateDurationForLvl(duration, 25, target:getMainLvl())
+        local duration = calculateDuration(math.random(42, 54) * 10, spell:getSkillType(), spell:getSpellGroup(), caster, target)
 
-        -- FIXME: Create mod and use that instead of an itemID check
-        if (target:getEquipID(dsp.slot.BACK) == 13692) then -- skulker's cape
-            duration = duration + 30
-        end
+        duration = calculateDurationForLvl(duration, 25, target:getMainLvl()) + player:getMod(dsp.mod.INVISIBLE_DURATION)
 
         spell:setMsg(dsp.msg.basic.MAGIC_GAIN_EFFECT)
         target:addStatusEffect(dsp.effect.INVISIBLE, 0, 10, math.floor(duration * SNEAK_INVIS_DURATION_MULTIPLIER))
     else
-        spell:setMsg(dsp.msg.basic.MAGIC_NO_EFFECT) -- no dsp.effect.
+        spell:setMsg(dsp.msg.basic.MAGIC_NO_EFFECT)
     end
 
     return dsp.effect.INVISIBLE
--- scripts/globals/spells/sneak.lua
+++ scripts/globals/spells/sneak.lua
@@ -1,33 +1,29 @@
 -----------------------------------------
 -- Spell: Sneak
--- Lessens chance of being detected by sound
--- Duration is random number between 30 seconds and 5 minutes
+-- Lessens chance of being detected by sound.
+-- Duration is random number between 30 seconds and 5 minutes.
 -----------------------------------------
 require("scripts/globals/magic")
 require("scripts/globals/msg")
 require("scripts/globals/settings")
 require("scripts/globals/status")
+-----------------------------------------
 
-function onMagicCastingCheck(caster,target,spell)
+function onMagicCastingCheck(caster, target, spell)
     return 0
 end
 
-function onSpellCast(caster,target,spell)
-    if (not target:hasStatusEffect(dsp.effect.SNEAK)) then
+function onSpellCast(caster, target, spell)
+    if not target:hasStatusEffect(dsp.effect.SNEAK) then
 
-        -- last 7-9 minutes
-        local duration = calculateDuration(math.random(420, 540), spell:getSkillType(), spell:getSpellGroup(), caster, target)
-        duration = calculateDurationForLvl(duration, 20, target:getMainLvl())
+        local duration = calculateDuration(math.random(42, 54) * 10, spell:getSkillType(), spell:getSpellGroup(), caster, target)
 
-        -- FIXME: Create mod and use that instead of an itemID check
-        if (target:getEquipID(dsp.slot.BACK) == 13692) then -- skulker's cape
-            duration = duration + 30
-        end
+        duration = calculateDurationForLvl(duration, 20, target:getMainLvl()) + player:getMod(dsp.mod.SNEAK_DURATION)
         
         spell:setMsg(dsp.msg.basic.MAGIC_GAIN_EFFECT)
         target:addStatusEffect(dsp.effect.SNEAK, 0, 10, math.floor(duration * SNEAK_INVIS_DURATION_MULTIPLIER))
     else
-        spell:setMsg(dsp.msg.basic.MAGIC_NO_EFFECT) -- no dsp.effect.
+        spell:setMsg(dsp.msg.basic.MAGIC_NO_EFFECT)
     end
 
     return dsp.effect.SNEAK
