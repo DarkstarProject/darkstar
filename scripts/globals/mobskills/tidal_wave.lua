a43eb7b791d7ad1b5d7f3e58f2e7c8019a8a45e6
isotor <heavyarms@protonmail.com>
2019-05-07 09:42:29 +0300

    Fix Astral Flow BP params / consitency

--- scripts/globals/mobskills/tidal_wave.lua
+++ scripts/globals/mobskills/tidal_wave.lua
@@ -17,7 +17,7 @@ function onMobWeaponSkill(target, mob, skill)
 
     dmgmod = 3
     info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg() * 9,dsp.magic.ele.WATER,dmgmod,TP_NO_EFFECT,1)
-    dmg = MobFinalAdjustments(info.dmg,mob,skill,target,dsp.attackType.MAGICAL,dsp.damageType.WATER,MOBPARAM_IGNORE_SHADOWS)
+    dmg = MobFinalAdjustments(info.dmg,mob,skill,target,dsp.attackType.MAGICAL,dsp.damageType.WATER,MOBPARAM_WIPE_SHADOWS)
     target:takeDamage(dmg, mob, dsp.attackType.MAGICAL, dsp.damageType.WATER)
     return dmg
 
