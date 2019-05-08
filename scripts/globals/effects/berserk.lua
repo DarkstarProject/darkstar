9944e392419b2113362395021ddbfb3d72935bab
NeuromancerXI <3996176+neuromancerxi@users.noreply.github.com>
2019-05-07 20:51:51 -0700

    The great migration and reduction of 25.

--- scripts/globals/effects/berserk.lua
+++ scripts/globals/effects/berserk.lua
@@ -7,16 +7,16 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target,effect)
-    target:addMod(dsp.mod.ATTP, 25 + effect:getPower())
-    target:addMod(dsp.mod.RATTP, 25 + effect:getPower())
-    target:addMod(dsp.mod.DEFP, -25 - effect:getPower())
+    target:addMod(dsp.mod.ATTP, effect:getPower())
+    target:addMod(dsp.mod.RATTP, effect:getPower())
+    target:addMod(dsp.mod.DEFP, - effect:getPower())
 end
 
 function onEffectTick(target,effect)
 end
 
 function onEffectLose(target,effect)
-    target:delMod(dsp.mod.ATTP, 25 + effect:getPower())
-    target:delMod(dsp.mod.RATTP, 25 + effect:getPower())
-    target:delMod(dsp.mod.DEFP, -25 - effect:getPower())
+    target:delMod(dsp.mod.ATTP, effect:getPower())
+    target:delMod(dsp.mod.RATTP, effect:getPower())
+    target:delMod(dsp.mod.DEFP, - effect:getPower())
 end
