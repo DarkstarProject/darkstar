6b21d276d68f7f14902740db1d8b0d5cb8cd5b7f
isotor <heavyarms@protonmail.com>
2019-05-12 20:13:12 +0300

    Set spawn chance and respawn Ram NMs

--- scripts/zones/Konschtat_Highlands/mobs/Tremor_Ram.lua
+++ scripts/zones/Konschtat_Highlands/mobs/Tremor_Ram.lua
@@ -1,6 +1,6 @@
 -----------------------------------
 -- Area: Konschtat Highlands
---  Mob: Tremor Ram
+--   NM: Tremor Ram
 -----------------------------------
 local ID = require("scripts/zones/Konschtat_Highlands/IDs")
 -----------------------------------
@@ -11,8 +11,7 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    -- If Steelflect doesn't pop next, fallback onto Rampaging Ram
-    if not dsp.mob.phOnDespawn(mob, ID.mob.STEELFLEECE_PH, 10, math.random(75600, 86400)) then -- 21-24 hours
-        dsp.mob.phOnDespawn(mob, ID.mob.RAMPAGING_RAM_PH, 10, 1200) -- 20 min
+    if not dsp.mob.phOnDespawn(mob, ID.mob.STEELFLEECE_PH, 5, math.random(75600, 86400)) then -- 21-24 hours
+        dsp.mob.phOnDespawn(mob, ID.mob.RAMPAGING_RAM_PH, 5, 3600) -- 1 hour
     end
 end
--- scripts/zones/La_Theine_Plateau/mobs/Battering_Ram.lua
+++ scripts/zones/La_Theine_Plateau/mobs/Battering_Ram.lua
@@ -1,8 +1,8 @@
 -----------------------------------
 -- Area: La Theine Plateau
---  MOB: Battering Ram
+--   NM: Battering Ram
 -----------------------------------
-local ID = require("scripts/zones/La_Theine_Plateau/IDs");
+local ID = require("scripts/zones/La_Theine_Plateau/IDs")
 -----------------------------------
 require("scripts/globals/mobs")
 -----------------------------------
@@ -11,7 +11,7 @@ function onMobDeath(mob, player, isKiller)
 end
 
 function onMobDespawn(mob)
-    if not dsp.mob.phOnDespawn(mob, ID.mob.BLOODTEAR_PH, 10, math.random(75600, 86400)) then -- 21-24 hours
-        dsp.mob.phOnDespawn(mob, ID.mob.LUMBERING_LAMBERT_PH, 10, 1200) -- 20 min
+    if not dsp.mob.phOnDespawn(mob, ID.mob.BLOODTEAR_PH, 5, math.random(75600, 86400)) then -- 21-24 hours
+        dsp.mob.phOnDespawn(mob, ID.mob.LUMBERING_LAMBERT_PH, 5, 3600) -- 1 hour
     end
 end
