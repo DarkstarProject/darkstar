1fba6f9b86cf56b7c80eed16098e8c16deddcd46
kaincenteno <kain.centeno@catsangel.com>
2019-05-25 14:49:06 -0700

    Added events for "Say It With a Handbag" quest line

---
+++ scripts/zones/Windurst_Waters_[S]/npcs/Door_Acolyte_hostel_up.lua
@@ -0,0 +1,34 @@
+-----------------------------------
+-- Area: Windurst Waters (S)
+--  NPC: Door Acolyte hostel
+-- !pos 125 -2 216 94
+-----------------------------------
+local ID = require("scripts/zones/Windurst_Waters_[S]/IDs")
+require("scripts/globals/keyitems")
+require("scripts/globals/missions")
+require("scripts/globals/settings")
+require("scripts/globals/quests")
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end
+
+function onTrigger(player,npc)
+    local sayItWithAHandbag = player:getQuestStatus(CRYSTAL_WAR, dsp.quest.id.crystalWar.SAY_IT_WITH_A_HANDBAG)
+    local sayItWithAHandbagCS = player:getVar("sayItWithAHandbagCS")
+    if (sayItWithAHandbag == QUEST_ACCEPTED and sayItWithAHandbagCS == 0) then
+        player:startEvent(171)
+    end
+end
+
+function onEventUpdate(player,csid,option)
+end
+
+function onEventFinish(player,csid,option)
+    if (csid == 171 and option == 1) then
+        player:setVar("sayItWithAHandbagCS", 1)
+    end
+    if (csid == 171 and option == 0) then
+        -- bug: quest music continues (doesn't affect quest line)
+    end
+end
