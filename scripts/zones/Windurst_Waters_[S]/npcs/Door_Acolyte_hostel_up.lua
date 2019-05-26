25bf44368c857ae5438417c5e36f5d4cfbaf81f5
kaincenteno <kain.centeno@catsangel.com>
2019-05-26 09:56:34 -0700

    added comment of npc location and type

--- scripts/zones/Windurst_Waters_[S]/npcs/Door_Acolyte_hostel_up.lua
+++ scripts/zones/Windurst_Waters_[S]/npcs/Door_Acolyte_hostel_up.lua
@@ -1,6 +1,8 @@
 -----------------------------------
 -- Area: Windurst Waters (S)
 --  NPC: Door Acolyte hostel
+-- Type: Quest NPC
+-- !pos  146.619,-8.525,242.874 94
 -----------------------------------
 local ID = require("scripts/zones/Windurst_Waters_[S]/IDs")
 require("scripts/globals/keyitems")
@@ -15,7 +17,7 @@ end
 function onTrigger(player,npc)
     local sayItWithAHandbag = player:getQuestStatus(CRYSTAL_WAR, dsp.quest.id.crystalWar.SAY_IT_WITH_A_HANDBAG)
     local sayItWithAHandbagCS = player:getVar("sayItWithAHandbagCS")
-    if (sayItWithAHandbag == QUEST_ACCEPTED and sayItWithAHandbagCS == 0) then
+    if sayItWithAHandbag == QUEST_ACCEPTED and sayItWithAHandbagCS == 0 then
         player:startEvent(171)
     end
 end
@@ -24,10 +26,10 @@ function onEventUpdate(player,csid,option)
 end
 
 function onEventFinish(player,csid,option)
-    if (csid == 171 and option == 1) then
+    if csid == 171 and option == 1 then
         player:setVar("sayItWithAHandbagCS", 1)
     end
-    if (csid == 171 and option == 0) then
+    if csid == 171 and option == 0 then
         -- bug: quest music continues (doesn't affect quest line)
     end
 end
