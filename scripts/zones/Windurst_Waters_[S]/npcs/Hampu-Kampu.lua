cbd9233d529c987a3ee5f68f23db12c2727cd8bf
kaincenteno <kain.centeno@catsangel.com>
2019-05-26 09:51:16 -0700

    added comment of npc location added require npc_util fixed style removed debug print statements removed CS setVar setting to 0 replaced code to use npcUtil for completeQuest and giveKeyItem

--- scripts/zones/Windurst_Waters_[S]/npcs/Hampu-Kampu.lua
+++ scripts/zones/Windurst_Waters_[S]/npcs/Hampu-Kampu.lua
@@ -2,11 +2,13 @@
 -- Area: Windurst Waters (S)
 --  NPC: Hampu-Kampu
 -- Type: Quest NPC
+-- !pos  -115.597,-1.000,-158.703 94
 -----------------------------------
 local ID = require("scripts/zones/Windurst_Waters_[S]/IDs")
-require("scripts/globals/quests");
-require("scripts/globals/keyitems");
-
+require("scripts/globals/quests")
+require("scripts/globals/keyitems")
+require("scripts/globals/npc_util")
+-----------------------------------
 
 function onTrade(player,npc,trade)
 end
@@ -15,21 +17,18 @@ function onTrigger(player,npc)
     local sayItWithAHandbag = player:getQuestStatus(CRYSTAL_WAR, dsp.quest.id.crystalWar.SAY_IT_WITH_A_HANDBAG)
     local sayItWithAHandbagCS = player:getVar("sayItWithAHandbagCS")
 
-    if (sayItWithAHandbag == QUEST_COMPLETED) then
+    if sayItWithAHandbag == QUEST_COMPLETED then
         player:startEvent(175)
-    elseif (player:hasKeyItem(dsp.ki.REPAIRED_HANDBAG) and sayItWithAHandbagCS == 4) then
+    elseif player:hasKeyItem(dsp.ki.REPAIRED_HANDBAG) and sayItWithAHandbagCS == 4 then
         player:startEvent(174)
-        print("repaired bag and 4")
-    elseif (player:hasKeyItem(dsp.ki.TORN_PATCHES_OF_LEATHER) or sayItWithAHandbagCS == 3) then
+    elseif player:hasKeyItem(dsp.ki.TORN_PATCHES_OF_LEATHER) or sayItWithAHandbagCS == 3 then
         player:startEvent(173)
-        print(player:hasKeyItem(dsp.ki.REPAIRED_HANDBAG))
-    elseif (sayItWithAHandbag == QUEST_ACCEPTED and sayItWithAHandbagCS == 1) then
+    elseif sayItWithAHandbag == QUEST_ACCEPTED and sayItWithAHandbagCS == 1 then
         player:startEvent(172)
-    elseif (sayItWithAHandbag == QUEST_ACCEPTED) then
+    elseif sayItWithAHandbag == QUEST_ACCEPTED then
         player:startEvent(170)
-    elseif (sayItWithAHandbag == QUEST_AVAILABLE) then
+    elseif sayItWithAHandbag == QUEST_AVAILABLE then
         player:startEvent(169)
-        player:setVar("sayItWithAHandbagCS",0)
     end
 end
 
@@ -37,18 +36,15 @@ function onEventUpdate(player,csid,option)
 end
 
 function onEventFinish(player,csid,option)
-    if (csid == 174) then -- Option doesn't matter as NPC will take key item if yes or no
-        if (player:getFreeSlotsCount() < 1) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED,19110);
-        else
+    if csid == 174 then -- Option doesn't matter as NPC will take key item if yes or no
+        if npcUtil.completeQuest(player, CRYSTAL_WAR, dsp.quest.id.crystalWar.SAY_IT_WITH_A_HANDBAG, {
+            item = 19110 -- Trainee's Needle
+        }) then
             player:delKeyItem(dsp.ki.REPAIRED_HANDBAG)
-            player:addItem(19110)
-            player:completeQuest(CRYSTAL_WAR, dsp.quest.id.crystalWar.SAY_IT_WITH_A_HANDBAG)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 19110)
         end
-    elseif (csid == 172) then
-            player:giveKeyItem(dsp.ki.TORN_PATCHES_OF_LEATHER)
-    elseif (csid == 169) then
+    elseif csid == 172 then
+            npcUtil.giveKeyItem(player, dsp.ki.TORN_PATCHES_OF_LEATHER)
+    elseif csid == 169 then
         player:addQuest(CRYSTAL_WAR, dsp.quest.id.crystalWar.SAY_IT_WITH_A_HANDBAG)
     end
 end 
