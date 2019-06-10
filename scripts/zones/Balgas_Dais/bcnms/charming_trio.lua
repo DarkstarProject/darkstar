e2337457328dbfc69ed2111951dc0dd2e87181b9
wrenffxi <oliviapekoe@gmail.com>
2019-06-10 02:02:33 -0400

    clean up remaining balga's dais bcnm luas

--- scripts/zones/Balgas_Dais/bcnms/charming_trio.lua
+++ scripts/zones/Balgas_Dais/bcnms/charming_trio.lua
@@ -1,28 +1,12 @@
 -----------------------------------
--- Area: Balgas_Dais
--- Name: Charming Trio
+-- Charming Trio
+-- Balga's Dais BCNM20, Cloudy Orb
+-- !additem 1551
 -----------------------------------
-
-require("scripts/globals/titles");
 require("scripts/globals/battlefield")
-require("scripts/globals/quests");
-
 -----------------------------------
--- EXAMPLE SCRIPT
---
--- What should go here:
--- giving key items, playing ENDING cutscenes
---
--- What should NOT go here:
--- Handling of "battlefield" status, spawning of monsters,
--- putting loot into treasure pool,
--- enforcing ANY rules (SJ/number of people/etc), moving
--- chars around, playing entrance CSes (entrance CSes go in bcnm.lua)
 
 function onBattlefieldInitialise(battlefield)
-    -- i have loot, spawn it after i win
-    -- loot is found in the npcs/Armoury_Crate.lua,
-    -- using the battlefield's id to grab the loot table
     battlefield:setLocalVar("loot", 1)
 end
 
@@ -30,38 +14,23 @@ function onBattlefieldTick(battlefield, tick)
     dsp.battlefield.onBattlefieldTick(battlefield, tick)
 end
 
--- After registering the BCNM via bcnmRegister(bcnmid)
-function onBattlefieldRegister(player,battlefield)
-end;
+function onBattlefieldRegister(player, battlefield)
+end
 
--- Physically entering the BCNM via bcnmEnter(bcnmid)
-function onBattlefieldEnter(player,battlefield)
-end;
+function onBattlefieldEnter(player, battlefield)
+end
 
--- Leaving the BCNM by every mean possible, given by the LeaveCode
--- 1=Select Exit on circle
--- 2=Winning the BC
--- 3=Disconnected or warped out
--- 4=Losing the BC
--- via bcnmLeave(1) or bcnmLeave(2). LeaveCodes 3 and 4 are called
--- from the core when a player disconnects or the time limit is up, etc
-
-function onBattlefieldLeave(player,battlefield,leavecode)
--- print("leave code "..leavecode);
-
-    if leavecode == dsp.battlefield.leaveCode.WON then -- play end CS. Need time and battle id for record keeping + storage
+function onBattlefieldLeave(player, battlefield, leavecode)
+    if leavecode == dsp.battlefield.leaveCode.WON then
         local name, clearTime, partySize = battlefield:getRecord()
         player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), 0)
     elseif leavecode == dsp.battlefield.leaveCode.LOST then
-        player:startEvent(32002);
+        player:startEvent(32002)
     end
+end
 
-end;
+function onEventUpdate(player, csid, option)
+end
 
-function onEventUpdate(player,csid,option)
--- print("bc update csid "..csid.." and option "..option);
-end;
-
-function onEventFinish(player,csid,option)
--- print("bc finish csid "..csid.." and option "..option);
-end;
+function onEventFinish(player, csid, option)
+end
