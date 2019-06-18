fd9d4ef575e6d99d34be71deb318671b02f5bb30
Zach Toogood <zrtoogood@gmail.com>
2019-06-19 00:11:52 +0300

    Add effects to Nanaa

--- scripts/zones/Chamber_of_Oracles/mobs/Nanaa_Mihgo.lua
+++ scripts/zones/Chamber_of_Oracles/mobs/Nanaa_Mihgo.lua
@@ -3,11 +3,14 @@
 -- MOB: Nanaa Mihgo
 -- AMK 10 BCNM
 -----------------------------------
-mixins = {require("scripts/mixins/job_special")};
+mixins = {require("scripts/mixins/job_special")}
+require("scripts/globals/mobs")
 -----------------------------------
 
 function onMobSpawn(mob)
+    mob:setMobMod(dsp.mobMod.ADD_EFFECT, 1)
     mob:addMod(dsp.mod.TRIPLE_ATTACK, 30)
+    mob:addMod(dsp.mod.REGEN, 100)
     dsp.mix.jobSpecial.config(mob, {
     specials =
         {
@@ -16,6 +19,12 @@ function onMobSpawn(mob)
             },
         },
     })
+end
+
+function onAdditionalEffect(mob, target, damage)
+    local effects = { dsp.mob.ae.PARALYZE, dsp.mob.ae.SILENCE, dsp.mob.ae.STUN, dsp.mob.ae.POISON }
+    local randomEffect = effects[math.random(#effects)]
+    return dsp.mob.onAddEffect(mob, target, damage, randomEffect)
 end
 
 function onMobDeath(mob, player, isKiller)
