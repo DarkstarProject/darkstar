1d1d0421e174a2969ac6b128f29da96b0f2b2531
wrenffxi <oliviapekoe@gmail.com>
2019-06-27 23:27:50 -0400

    BCNM content: Waughroon Shrine

--- scripts/zones/Waughroon_Shrine/mobs/Flesh_Eater.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Flesh_Eater.lua
@@ -1,14 +1,17 @@
 -----------------------------------
 -- Area: Waughroon Shrine
---  Mob: Flayer Franz
+--  Mob: Flesh Eater
 -- BCNM: The Worm's Turn
 -----------------------------------
 require("scripts/globals/status")
+require("scripts/globals/mobs")
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:setMobMod(dsp.mobMod.SUPERLINK, mob:getShortID())
-    mob:setMobMod(dsp.mobMod.DRAW_IN, 1)
+    local mobId = mob:getID()
+    local offset = (mobId - ID.mob.FLAYER_FRANZ_OFFSET) % 17
+    mob:setMobMod(dsp.mobMod.SUPERLINK, GetMobByID(mobId - offset):getShortID())
+    mob:setMobMod(dsp.mobMod.CHARMABLE, 1)
 end
 
 function onMobDeath(mob, player, isKiller)
