4dc199c74ceeeaa607188e6d81c0f8dcea586222
Zach Toogood <zrtoogood@gmail.com>
2019-06-30 01:14:53 +0300

    Fix mistakes in comments - geomantic -> geomagnetic - <Geomagnetron> -> <keyitem>

--- scripts/globals/geomagnetic_fount.lua
+++ scripts/globals/geomagnetic_fount.lua
@@ -13,9 +13,9 @@ dsp.geomagneticFount.checkFount = function(player, npc)
     local charged = player:getVar("SOA") == 1
 
     if theGeomagnetron and charged then
-        player:messageSpecial(zones[player:getZoneID()].text.GEOMAGNETRON_ATTUNED + 2) -- Your device has already been attuned to a geomantic fount in the corresponding locale.
+        player:messageSpecial(zones[player:getZoneID()].text.GEOMAGNETRON_ATTUNED + 2) -- Your device has already been attuned to a geomagnetic fount in the corresponding locale.
     elseif theGeomagnetron then
-        player:messageSpecial(zones[player:getZoneID()].text.GEOMAGNETRON_ATTUNED, 0, dsp.ki.GEOMAGNETRON) -- Your <Geomagnetron> has been attuned to a geomantic fount in the corresponding locale.
+        player:messageSpecial(zones[player:getZoneID()].text.GEOMAGNETRON_ATTUNED, 0, dsp.ki.GEOMAGNETRON) -- Your <keyitem> has been attuned to a geomagnetic fount in the corresponding locale.
         player:setVar("SOA", 1)
     else
         player:messageSpecial(zones[player:getZoneID()].text.GEOMAGNETRON_ATTUNED + 1) -- A faint energy wafts up from the ground.
