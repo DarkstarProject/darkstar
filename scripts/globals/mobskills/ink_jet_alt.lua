5a14f6042df7246e460badaeac2c357c4c47bb44
TeoTwawki <TeoTwawki@users.noreply.github.com>
2019-07-01 19:55:26 -0400

    remove unneeded variable

--- scripts/globals/mobskills/ink_jet_alt.lua
+++ scripts/globals/mobskills/ink_jet_alt.lua
@@ -16,13 +16,11 @@ function onMobSkillCheck(target,mob,skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    local typeEffect = dsp.effect.BLINDNESS
-
-    MobStatusEffectMove(mob, target, typeEffect, 20, 0, 180)
-
     local dmgmod = 1
     local info = MobMagicalMove(mob,target,skill,mob:getWeaponDmg()*2.5,dsp.magic.ele.DARK,dmgmod,TP_NO_EFFECT)
     local dmg = MobFinalAdjustments(info.dmg,mob,skill,target,dsp.attackType.MAGICAL,dsp.damageType.DARK,MOBPARAM_IGNORE_SHADOWS)
+
+    MobStatusEffectMove(mob, target, dsp.effect.BLINDNESS, 20, 0, 180)
 
     target:takeDamage(dmg, mob, dsp.attackType.MAGICAL, dsp.damageType.DARK)
     return dmg
