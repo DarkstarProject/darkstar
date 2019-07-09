031115f1749f751e6935f3c76c4f0bed0f94833d
David Norris <Knowone134@gmail.com>
2019-07-08 21:03:03 -0500

    JOJ adds

--- scripts/zones/AlTaieu/mobs/Jailer_of_Justice.lua
+++ scripts/zones/AlTaieu/mobs/Jailer_of_Justice.lua
@@ -2,29 +2,38 @@
 -- Area: Al'Taieu
 --  NM:  Jailer of Justice
 -----------------------------------
-require("scripts/globals/status");
-require("scripts/globals/magic");
+require("scripts/globals/status")
+require("scripts/globals/magic")
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(dsp.mobMod.IDLE_DESPAWN, 300)
+end
+
 function onMobFight(mob, target)
-    local popTime = mob:getLocalVar("lastPetPop");
+    local popTime = mob:getLocalVar("lastPetPop")
     -- ffxiclopedia says 30 sec, bgwiki says 1-2 min..
     -- Going with 60 seconds until I see proof of retails timing.
     if (os.time() - popTime > 60) then
-        local alreadyPopped = false;
+        local alreadyPopped = false
         for Xzomit = mob:getID()+1, mob:getID()+6 do
             if (alreadyPopped == true) then
-                break;
+                break
             else
                 if (not GetMobByID(Xzomit):isSpawned()) then
-                    SpawnMob(Xzomit, 300):updateEnmity(target);
-                    mob:setLocalVar("lastPetPop", os.time());
-                    alreadyPopped = true;
+                    SpawnMob(Xzomit, 300):updateEnmity(target)
+                    mob:setLocalVar("lastPetPop", os.time())
+                    alreadyPopped = true
                 end
             end
         end
     end
-end;
+end
+
+function onMobDespawn(mob)
+    for i = 1,6 do DespawnMob(mob:getID() + i) end
+end
 
 function onMobDeath(mob, player, isKiller)
-end;
+    for i = 1,6 do DespawnMob(mob:getID() + i) end
+end
--- scripts/zones/AlTaieu/mobs/Qnxzomit.lua
+++ scripts/zones/AlTaieu/mobs/Qnxzomit.lua
@@ -3,17 +3,24 @@
 --  MOB: Qn'xzomit
 -- Note: Pet for JOL and JOJ
 -----------------------------------
-mixins = {require("scripts/mixins/job_special")};
-local ID = require("scripts/zones/AlTaieu/IDs");
+mixins = {require("scripts/mixins/job_special")}
+local ID = require("scripts/zones/AlTaieu/IDs")
 -----------------------------------
 
+function onMobFight(mob, target)
+    if mob:getID() < ID.mob.JAILER_OF_LOVE and mob:getLocalVar("Mijin_Gakure") == 0 and mob:getBattleTime() > 10 then
+	    mob:setLocalVar("Mijin_Gakure",1)
+		mob:useMobAbility(731) -- Mijin_Gakure 
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
-end;
+end
 
 function onMobDespawn(mob)
-    if (mob:getID() > ID.mob.JAILER_OF_LOVE) then
-        local JoL = GetMobByID(ID.mob.JAILER_OF_LOVE);
-        local xzomitsKilled = JoL:getLocalVar("JoL_Qn_xzomit_Killed");
-        JoL:setLocalVar("JoL_Qn_xzomit_Killed", xzomitsKilled + 1);
+    if mob:getID() > ID.mob.JAILER_OF_LOVE then
+        local JoL = GetMobByID(ID.mob.JAILER_OF_LOVE)
+        local xzomitsKilled = JoL:getLocalVar("JoL_Qn_xzomit_Killed")
+        JoL:setLocalVar("JoL_Qn_xzomit_Killed", xzomitsKilled + 1)
     end
-end;
+end
