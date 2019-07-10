c8b96b186f61323f8b397b153ac108298fb4a596
David Norris <Knowone134@gmail.com>
2019-07-10 18:54:03 -0500

    FUC**** tabs lol

--- scripts/zones/Promyvion-Mea/npcs/_0k0.lua
+++ scripts/zones/Promyvion-Mea/npcs/_0k0.lua
@@ -8,6 +8,6 @@ require("scripts/globals/npc_util")
 
 function onTrigger(player,npc)
     if player:getQuestStatus(JEUNO,dsp.quest.id.jeuno.SHADOWS_OF_THE_DEPARTED) == QUEST_ACCEPTED and not player:hasKeyItem(dsp.ki.PROMYVION_MEA_SLIVER) then 
-		npcUtil.giveKeyItem(player, dsp.ki.PROMYVION_MEA_SLIVER)
+        npcUtil.giveKeyItem(player, dsp.ki.PROMYVION_MEA_SLIVER)
     end
 end
