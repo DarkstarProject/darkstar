2d92c220bb5384763ca4cd388890b4257746e29b
wrenffxi <oliviapekoe@gmail.com>
2019-07-15 13:35:14 -0400

    BCNM content for Sacrificial Chamber

--- scripts/zones/Sacrificial_Chamber/mobs/Rauu_the_Whaleswooner.lua
+++ scripts/zones/Sacrificial_Chamber/mobs/Rauu_the_Whaleswooner.lua
@@ -1,18 +1,14 @@
 -----------------------------------
 -- Area: Sacrificial Chamber
---  MOB: Cyaneous-toed_Yallberry
--- BCNM: Jungle Boogymen
+--  MOB: Rauu the Whaleswooner
+-- BCNM: Amphibian Assault
 -----------------------------------
-mixins =
-{
-    require("scripts/mixins/families/tonberry"),
-    require("scripts/mixins/job_special")
-}
+mixins = {require("scripts/mixins/job_special")}
 require("scripts/globals/status")
 -----------------------------------
 
 function onMobSpawn(mob)
-    mob:setMobMod(dsp.mobMod.SUPERLINK, GetMobByID(mob:getID() - 2):getShortID())
+    mob:setMobMod(dsp.mobMod.SUPERLINK, GetMobByID(mob:getID() - 1):getShortID())
 end
 
 function onMobDeath(mob, player, isKiller)
