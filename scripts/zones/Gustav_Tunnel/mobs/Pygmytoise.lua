45bece03cda465f6abfa5e52420dab92ff34a6bc
TeoTwawki <arawn.annwn@yandex.com>
2019-07-19 22:06:45 -0400

    Adjust Adamantoise family elemental resistances to hopefully be more retail accurate. Earth and lighting severely resist. These are resistant to some degree to all elements, with ice resistance being least.

--- scripts/zones/Gustav_Tunnel/mobs/Pygmytoise.lua
+++ scripts/zones/Gustav_Tunnel/mobs/Pygmytoise.lua
@@ -6,10 +6,6 @@ require("scripts/globals/regimes")
 require("scripts/globals/status")
 -----------------------------------
 
-function onMobInitialize(mob)
-    mob:setMod(dsp.mod.DMGMAGIC, -50) -- "Highly resistant to magic damage"
-end
-
 function onMobDeath(mob, player, isKiller)
     dsp.regime.checkRegime(player, mob, 770, 2, dsp.regime.type.GROUNDS)
 end;
