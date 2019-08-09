4cf5cbb100ac74cd2d240b907358c27bb30644e5
Andrew6ta <46385877+Andrew6ta@users.noreply.github.com>
2019-08-09 15:49:25 -0500

    remove tabs

--- src/map/entities/automatonentity.cpp
+++ src/map/entities/automatonentity.cpp
@@ -153,22 +153,22 @@ uint8 CAutomatonEntity::addBurden(uint8 element, int8 burden)
 
 uint8 CAutomatonEntity::getDeployDuration()
 {
-	return static_cast<uint8>(m_deployDuration);
+    return static_cast<uint8>(m_deployDuration);
 }
 
 void CAutomatonEntity::updateDeployDuration(int dSec)
 {
-	m_deployDuration += dSec;
-	//cap maneuver duration after 4 minutes
-	if(m_deployDuration > 240)
-	{
-		m_deployDuration = 240;
-	}
+    m_deployDuration += dSec;
+    //cap maneuver duration after 4 minutes
+    if(m_deployDuration > 240)
+    {
+        m_deployDuration = 240;
+    }
 }
 
 void CAutomatonEntity::resetDeployDuration()
 {
-	m_deployDuration = 0;
+    m_deployDuration = 0;
 }
 
 void CAutomatonEntity::PostTick()
