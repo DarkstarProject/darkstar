4438e8ac338bbd8129c9ddc1a2cfefa47f50d770
wrenffxi <oliviapekoe@gmail.com>
2019-08-10 18:07:11 -0400

    remove obsolete Avatar_Icon lua from Valkurm remove empty onMobSpawn from all Dyna mobs add dynamis_beastmen mixin to Hydras/Kindreds in Tav/Xarc

--- scripts/zones/Dynamis-Tavnazia/mobs/Kindred_Ranger.lua
+++ scripts/zones/Dynamis-Tavnazia/mobs/Kindred_Ranger.lua
@@ -2,7 +2,11 @@
 -- Area: Dynamis - Tavnazia
 --  Mob: Kindred Ranger
 -----------------------------------
-mixins = {require("scripts/mixins/job_special")}
+mixins =
+{
+    require("scripts/mixins/dynamis_beastmen"),
+    require("scripts/mixins/job_special")
+}
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
