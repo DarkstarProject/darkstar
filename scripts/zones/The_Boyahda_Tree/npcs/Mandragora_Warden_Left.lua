ce047f3172101c2ddf05a7acbf063749c2484987
kaincenteno <kain.centeno@catsangel.com>
2019-08-10 22:08:42 -0700

    npc_list.sql: Renamed 2 Mandragora_Warden to Mandragora_Warden_Left and Mandragora_Warden_Right as they offer different CS. The third one was left as is.
    Mandragora_Warden.lua:
    Added CS if player is holding item.
    Added standard CS (as currently npc only spoke if on mission)
    Removed other texts as they were not found anywhere (maybe invalid)
    
    Mandragora_Warden_Left.lua and Mandragora_Warden_Right.lua:
    Files not existing, created new ones.
    Added standard CS (they do not have other ones)
    
    Yoran-Oran.lua:
    Removed ending semicolons.
    No Logic was changed.
    Added Parameter for CS 439 as they were missing
    Fixed Parameter for CS 470 as he was showing blank text for it.
    
    IDs.lua
    Added 2 texts being user by Mandragora_Warden.lua

---
+++ scripts/zones/The_Boyahda_Tree/npcs/Mandragora_Warden_Left.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+-- Area: The Boyahda Tree
+--  NPC: Mandragora Warden
+-- Type: NPC
+-- !pos 99.068 8.548 122.514 153
+-----------------------------------
+
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end
+
+function onTrigger(player,npc)
+    player:startEvent(11)
+end
+
+function onEventUpdate(player,csid,option)
+end
+
+function onEventFinish(player,csid,option)
+end
---
+++ scripts/zones/The_Boyahda_Tree/npcs/Mandragora_Warden_Right.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+-- Area: The Boyahda Tree
+--  NPC: Mandragora Warden
+-- Type: NPC
+-- !pos 101.978 8.369 121.969 153
+-----------------------------------
+
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end
+
+function onTrigger(player,npc)
+    player:startEvent(12)
+end
+
+function onEventUpdate(player,csid,option)
+end
+
+function onEventFinish(player,csid,option)
+end
