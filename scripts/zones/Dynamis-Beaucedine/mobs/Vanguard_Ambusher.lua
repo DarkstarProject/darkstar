a8bedcbc1f2d87aac747aca5201af69fb9eec298
wrenffxi <oliviapekoe@gmail.com>
2019-08-14 12:38:46 -0400

    fix improperly escapes parens in requires

--- scripts/zones/Dynamis-Beaucedine/mobs/Vanguard_Ambusher.lua
+++ scripts/zones/Dynamis-Beaucedine/mobs/Vanguard_Ambusher.lua
@@ -7,8 +7,8 @@ mixins =
     require("scripts/mixins/dynamis_beastmen"),
     require("scripts/mixins/job_special")
 }
-local ID = require"scripts/zones/Dynamis-Beaucedine/IDs"
-require"scripts/globals/mobs"
+local ID = require("scripts/zones/Dynamis-Beaucedine/IDs")
+require("scripts/globals/mobs")
 -----------------------------------
 
 function onMobDeath(mob, player, isKiller)
