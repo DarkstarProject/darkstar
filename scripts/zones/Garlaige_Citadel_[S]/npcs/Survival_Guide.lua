c2ccd9c27d5849840dd92b877172fc664433b464
Brian Wuest <brian.wuest@outlook.com>
2019-10-12 11:53:54 -0500

    Committing Survival Guide Pull

---
+++ scripts/zones/Garlaige_Citadel_[S]/npcs/Survival_Guide.lua
@@ -0,0 +1,12 @@
+require("scripts/globals/survival_guide")
+
+local eventId = 8500
+local guideIndex = 94
+
+function onTrigger(player, targetNpc)
+    dsp.survival_guide.onTrigger(player, eventId, guideIndex)
+end
+
+function onEventFinish(player, csid, option, targetNpc)
+    dsp.survival_guide.onEventFinish(player, csid, eventId, option)
+end
