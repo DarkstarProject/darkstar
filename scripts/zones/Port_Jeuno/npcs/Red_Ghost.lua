4260fba69e7644e794c951442f9bcf051f3167b1
kaincenteno <kain.centeno@catsangel.com>
2019-10-21 08:16:35 -0700

    Fixing patrol for Red Ghost in Lower Jeuno
    Only lines 10 to 28 have been changed, all other line that shows as
    changed are only removal of end-line semicolons to align with style
    guide.
    
    This fix was grabbed in part from https://github.com/DarkstarProject/darkstar/blob/c78bd68becd20a7250d78af03fcc7b2cf9972a78/scripts/zones/Port_Jeuno/npcs/Red_Ghost.lua
    from https://github.com/Wiggo32 's PR
    
    Tested and fixed it. This fix only resolves the NPC stopped patroling
    after reaching the last point in path, it does not solve the NPC from
    jumping all over the place to catch up to the current location the
    server thinks where it should be at (if it wasn't stopped)
    
    If this gets approved I'll go ahead and do the same for NPCs that
    currently do not have patrol set up, like several in Windurst
    which should make the areas more lively (I know its something cosmetic
    but it's a good to have, specially for new people in server)

--- scripts/zones/Port_Jeuno/npcs/Red_Ghost.lua
+++ scripts/zones/Port_Jeuno/npcs/Red_Ghost.lua
@@ -3,59 +3,51 @@
 --  NPC: Red Ghost
 -- Standard Info NPC
 -----------------------------------
-require("scripts/globals/quests");
-require("scripts/globals/pathfind");
+require("scripts/globals/quests")
+require("scripts/globals/pathfind")
 -----------------------------------
 
 local path =
 {
--96.823616, 0.001000, -3.722488,
--96.761887, 0.001000, -2.632236,
--96.698341, 0.001000, -1.490001,
--96.636963, 0.001000, -0.363672,
--96.508736, 0.001000, 2.080966,
--96.290009, 0.001000, 6.895948,
--96.262505, 0.001000, 7.935584,
--96.282127, 0.001000, 6.815756,
--96.569176, 0.001000, -7.781419,
--96.256729, 0.001000, 8.059505,
--96.568405, 0.001000, -7.745419,
--96.254066, 0.001000, 8.195477,
--96.567200, 0.001000, -7.685426
-};
+    -97, 0, -4,
+    -97, 0,  8,
+    -97, 0, -8,
+}
+
 function onSpawn(npc)
-    npc:initNpcAi();
-    npc:setPos(dsp.path.first(path));
-    onPath(npc);
-end;
+    npc:initNpcAi()
+    npc:setPos(dsp.path.first(path))
+    onPath(npc)
+end
 
 function onPath(npc)
-    dsp.path.patrol(npc, path);
-end;
+    dsp.path.patrol(npc, path)
+    if not npc:isFollowingPath() then
+        npc:pathThrough(dsp.path.first(path), dsp.path.flag.NONE)
+    end
+end
 
 function onTrade(player,npc,trade)
-end;
+end
 
 function onTrigger(player,npc)
+    player:setLocalVar("npcID",npc:getID())
+    npc:speed(0)
+    npc:timer(300000, function(npc) npc:speed(12) end) -- Creates timer to prevent PC DCing which makes NPC stop patrol
     local WildcatJeuno = player:getCharVar("WildcatJeuno");
     if (player:getQuestStatus(JEUNO,dsp.quest.id.jeuno.LURE_OF_THE_WILDCAT) == QUEST_ACCEPTED and player:getMaskBit(WildcatJeuno,15) == false) then
-        player:startEvent(314);
+        player:startEvent(314)
     else
-        player:startEvent(34);
+        player:startEvent(34)
     end
-
-    -- wait until event is over
-    npc:wait();
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option,npc)
     if (csid == 314) then
-        player:setMaskBit(player:getCharVar("WildcatJeuno"),"WildcatJeuno",15,true);
+        player:setMaskBit(player:getCharVar("WildcatJeuno"),"WildcatJeuno",15,true)
     end
-
-    npc:wait(0);
-
-end;
+    GetNPCByID(player:getLocalVar("npcID")):speed(12)
+end
