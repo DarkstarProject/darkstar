601a200db24e4d21d24d0da49cde59cbfcc12a3d
wrenffxi <oliviapekoe@gmail.com>
2019-11-07 02:42:09 -0500

    * Fix Trial By Lightning entry crashing game server. * Fix Trial By Lightning skip cutscene check.

--- scripts/zones/Cloister_of_Storms/bcnms/trial_by_lightning.lua
+++ scripts/zones/Cloister_of_Storms/bcnms/trial_by_lightning.lua
@@ -22,7 +22,7 @@ end
 function onBattlefieldLeave(player, battlefield, leavecode)
     if leavecode == dsp.battlefield.leaveCode.WON then
         local name, clearTime, partySize = battlefield:getRecord()
-        local arg8 = (player:hasCompletedQuest(OTHER_AREAS_LOG, dsp.quest.id.outlands.TRIAL_BY_LIGHTNING)) and 1 or 0
+        local arg8 = (player:hasCompletedQuest(OTHER_AREAS_LOG, dsp.quest.id.otherAreas.TRIAL_BY_LIGHTNING)) and 1 or 0
         player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), arg8)
     elseif leavecode == dsp.battlefield.leaveCode.LOST then
         player:startEvent(32002)
