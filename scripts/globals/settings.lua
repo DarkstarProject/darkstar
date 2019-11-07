260912bb348e9d40ed35dbbf7ba1de66ef09fa6b
Omnione <10185476+Omnione@users.noreply.github.com>
2019-11-07 20:04:22 +0000

    Added everything needed for testing caskets, only west and east ronfare for now.

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -33,8 +33,11 @@ ENABLE_GROUNDS_TOMES  = 1; -- Enables Grounds of Valor
 REGIME_WAIT = 1; -- Make people wait till 00:00 game time as in retail. If it's 0, there is no wait time.
 
 -- TREASURE CASKETS
-ENABLE_CASKETS = 0; -- Set to 1 to enabled caskets to spawn on mob death in applicable zones, 0 to disable.
-CASKET_DROP_RATE = 1.000; -- Multiplies the base droprate for caskets by this amount (base droprate = 0.1 max is clamped to 1.0) so 10.000 is max CASKET_DROP_RATE.
+-- Multiplies the base droprate for caskets by this amount.
+-- max is clamped to 1.0 (100%)
+-- Retail droprate = 0.1 (10%) with no other effects active
+-- Set to 0 to disable caskets.
+CASKET_DROP_RATE = 0.000;
 
 -- Setting to lock content more accurately to the content you have defined above
 -- This generally results in a more accurate presentation of your selected expansions
---
+++ scripts/mixins/spawn_casket.lua
@@ -0,0 +1,28 @@
+-- Casket Mixins
+require("scripts/globals/settings")
+require("scripts/globals/caskets")
+require("scripts/globals/status")
+require("scripts/globals/mixins")
+
+
+g_mixins = g_mixins or {}
+
+---------------------------------------------------------
+-- Casket zone check
+---------------------------------------------------------
+g_mixins.spawn_casket = function(mob)
+    mob:addListener("DEATH", "DEATH_SPAWN_CASKET", function(mob, player, isKiller)
+        local mobPos = mob:getPos()
+
+        if mob:getMaster() ~= nil then
+            local master = mob:getMaster()
+            if master:isMob() then -- sanity check, ensuring the mob killed is not a player's pet.
+                dsp.caskets.SpawnCasket(player, mob, mobPos.x, mobPos.y, mobPos.z, mobPos.rot)
+            end
+        else
+            dsp.caskets.SpawnCasket(player, mob, mobPos.x, mobPos.y, mobPos.z, mobPos.rot)
+        end
+    end);
+end
+
+return g_mixins.spawn_casket;
