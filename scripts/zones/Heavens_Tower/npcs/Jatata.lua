297b2b7df63b968a98b19ab9312ae1eeca8b6d2c
wrenffxi <oliviapekoe@gmail.com>
2019-11-29 14:09:33 -0500

    heaven's tower style guide

--- scripts/zones/Heavens_Tower/npcs/Jatata.lua
+++ scripts/zones/Heavens_Tower/npcs/Jatata.lua
@@ -4,15 +4,15 @@
 -- Standard Info NPC
 -----------------------------------
 
-function onTrade(player,npc,trade)
-end;
+function onTrade(player, npc, trade)
+end
 
-function onTrigger(player,npc)
-    player:startEvent(77);
-end;
+function onTrigger(player, npc)
+    player:startEvent(77)
+end
 
-function onEventUpdate(player,csid,option)
-end;
+function onEventUpdate(player, csid, option)
+end
 
-function onEventFinish(player,csid,option)
-end;
+function onEventFinish(player, csid, option)
+end
