292fe6fe14b32ea78de26b7acbfa4e9b5a7d28f4
TeoTwawki <arawn.annwn@yandex.com>
2019-12-07 18:49:18 -0500

    test

--- scripts/zones/Port_Jeuno/npcs/Red_Ghost.lua
+++ scripts/zones/Port_Jeuno/npcs/Red_Ghost.lua
@@ -9,9 +9,22 @@ require("scripts/globals/pathfind")
 
 local path =
 {
-    -97, 0, -4,
-    -97, 0,  8,
-    -97, 0, -8,
+    -96.823616, 0.001, -3.722488,
+    -96.761887, 0.001, -2.632236,
+    -96.698341, 0.001, -1.490001,
+    -96.636963, 0.001, -0.363672,
+    -96.508736, 0.001,  2.080966,
+    -96.290009, 0.001,  6.895948,
+    -96.262505, 0.001,  7.935584,
+    -96.282127, 0.001,  6.815756,
+    -96.569176, 0.001, -7.781419,
+    -96.256729, 0.001,  8.059505,
+    -96.568405, 0.001, -7.745419,
+    -96.254066, 0.001,  8.195477,
+    -96.567200, 0.001, -7.685426
+--    -97, 0, -4,
+--    -97, 0,  8,
+--    -97, 0, -8,
 }
 
 function onSpawn(npc)
@@ -31,11 +44,13 @@ function onTrade(player,npc,trade)
 end
 
 function onTrigger(player,npc)
-    player:setLocalVar("npcID",npc:getID())
-    npc:speed(0)
-    npc:timer(300000, function(npc) npc:speed(12) end) -- Creates timer to prevent PC DCing which makes NPC stop patrol
-    local WildcatJeuno = player:getCharVar("WildcatJeuno");
-    if (player:getQuestStatus(JEUNO,dsp.quest.id.jeuno.LURE_OF_THE_WILDCAT) == QUEST_ACCEPTED and player:getMaskBit(WildcatJeuno,15) == false) then
+    local WildcatJeuno = player:getCharVar("WildcatJeuno")
+    npc:clearPath() -- Clear path and stop moving!
+
+    if
+        player:getQuestStatus(JEUNO,dsp.quest.id.jeuno.LURE_OF_THE_WILDCAT) == QUEST_ACCEPTED
+        and not player:getMaskBit(WildcatJeuno, 15)
+    then
         player:startEvent(314)
     else
         player:startEvent(34)
@@ -46,8 +61,10 @@ function onEventUpdate(player,csid,option)
 end
 
 function onEventFinish(player,csid,option,npc)
-    if (csid == 314) then
-        player:setMaskBit(player:getCharVar("WildcatJeuno"),"WildcatJeuno",15,true)
+    if csid == 314 then
+        player:setMaskBit(player:getCharVar("WildcatJeuno"), "WildcatJeuno", 15, true)
     end
-    GetNPCByID(player:getLocalVar("npcID")):speed(12)
+
+    -- Resume pathing
+    onPath(npc)
 end
