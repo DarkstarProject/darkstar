9aa2a36e9278a2703dcc0fc896dfb28327d8b343
Daniel R. Koris <korisd@gmail.com>
2020-01-09 15:09:14 -0700

    PR: Lua: Adjusted ShellraV and ProtectraV
    Their merits no longer exist, so set their base
    power to bg-wiki values

--- scripts/globals/spells/protectra_v.lua
+++ scripts/globals/spells/protectra_v.lua
@@ -13,13 +13,7 @@ end
 function onSpellCast(caster, target, spell)
     local duration = calculateDuration(1800, spell:getSkillType(), spell:getSpellGroup(), caster, target, false)
     duration = calculateDurationForLvl(duration, 75, target:getMainLvl())
-
-    local meritBonus = caster:getMerit(dsp.merit.PROTECTRA_V)
-    local power = 175 + meritBonus
-    if (meritBonus > 0) then -- certain mobs can cast this spell, so don't apply the -5 for having 0 merits.
-        power = power + meritBonus - 5
-    end
-    --printf("Protectra V Power: %d", power)    
+    local power = 220 -- bg-wiki
 
     local typeEffect = dsp.effect.PROTECT
     if target:addStatusEffect(typeEffect, power, 0, duration) then
