ccdb26aac1b00dcee40ff266c340dc9645600652
Corey Sotiropoulos <cocosolos@pm.me>
2020-01-13 22:43:13 -0500

    Fix an issue with null player being passed to death listener

--- src/map/entities/battleentity.cpp
+++ src/map/entities/battleentity.cpp
@@ -1179,8 +1179,10 @@ void CBattleEntity::Spawn()
 
 void CBattleEntity::Die()
 {
-    auto PKiller {GetEntity(m_OwnerID.targid)};
-    PAI->EventHandler.triggerListener("DEATH", this, PKiller);
+    if (CBaseEntity* PKiller = GetEntity(m_OwnerID.targid))
+        PAI->EventHandler.triggerListener("DEATH", this, PKiller);
+    else
+        PAI->EventHandler.triggerListener("DEATH", this);
     SetBattleTargetID(0);
 }
 
