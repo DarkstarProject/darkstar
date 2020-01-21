ee6297e4d6f579a00b32875b99bbdf30cea504ae
kaincenteno <kain.centeno@catsangel.com>
2020-01-20 22:05:57 -0800

    Docking Message when traveling to Nashmau Added ID for message (as it's not part of CS) When loading into the boat, it will store the Vanadiel hour and minute. It will check the time it should be arriving depending on the boat schedule. It will subtract the time the boat will dock and the time the char zoned in then use that as a timer so the text shows to the player on the correct time. Also removed semicolons

--- scripts/zones/Silver_Sea_route_to_Nashmau/Zone.lua
+++ scripts/zones/Silver_Sea_route_to_Nashmau/Zone.lua
@@ -7,26 +7,41 @@ local ID = require("scripts/zones/Silver_Sea_route_to_Nashmau/IDs")
 -----------------------------------
 
 function onInitialize(zone)
-end;
+end
 
 function onZoneIn(player,prevZone)
-    local cs = -1;
-
-    return cs;
-end;
+    local cs = -1
+    -- Gets current vanadiel time and compares it with arrival time to start a timer to show a message before it docks.
+    local vHour = VanadielHour()
+    local vMin  = VanadielMinute()
+    local dockHour
+    local dockMin = 40
+    if vHour < 5 then
+        dockHour = 4
+    elseif vHour < 13 then
+        dockHour = 12
+    elseif vHour < 21 then
+        dockHour = 20
+    end
+    local earthSecsAtZoneIn = math.floor(2.4 * ((vHour * 60) + vMin))
+    local earthSecsAtDocking = math.floor(2.4 * ((dockHour * 60) + dockMin))
+    local earthSecsUntilDock = math.floor(earthSecsAtDocking - earthSecsAtZoneIn)
+    player:timer(math.floor(1000 * earthSecsUntilDock), function(player) player:messageSpecial(ID.DOCKING_IN_NASHMAU) end)
+    return cs
+end
 
 function onTransportEvent(player,transport)
-    player:startEvent(1025);
-end;
+    player:startEvent(1025)
+end
 
 function onRegionEnter(player,region)
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
     if (csid == 1025) then
-        player:setPos(0,0,0,0,53);
+        player:setPos(0,0,0,0,53)
     end
-end;
+end
