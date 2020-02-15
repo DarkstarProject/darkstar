9dee7891bd1e591896e38a6610bc16ea4f29aa92
zircon-tpl <60901633+zircon-tpl@users.noreply.github.com>
2020-02-14 23:21:53 -0800

    Commits from sidestream, 2020/02/01 to 2020/02/14

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -3555,6 +3555,7 @@ inline int32 CLuaBaseEntity::addItem(lua_State *L)
             else
             {
                 ShowWarning(CL_YELLOW"charplugin::AddItem: Item <%i> is not found in a database\n" CL_RESET, itemID);
+                break;
             }
         }
     }
