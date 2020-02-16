4c6543e7f862666a3c91f32a8298a5276ae1c137
Blargedy Mcgee <59blargedy@gmail.com>
2020-02-15 22:27:56 -0500

    updated tails of woe bcnm

--- scripts/zones/Horlais_Peak/mobs/Cottontail.lua
+++ scripts/zones/Horlais_Peak/mobs/Cottontail.lua
@@ -4,5 +4,17 @@
 -- BCNM: Tails of Woe
 -----------------------------------
 
+require("scripts/globals/status")
+
+function onMobInitialize(mob)
+   mob:setMobMod(tpz.mobMod.NO_DROPS, 1) -- works
+   
+end;
+
+function onMobSpawn(mob) 
+ 	mob:setMobMod(tpz.mobMod.CHARMABLE, 1)
+ 	--mob:addMod(tpz.effect.ACCURACY_BOOST,1)--bunnies should be fairly accurate on the nm, but are not.
+end;
+
 function onMobDeath(mob, player, isKiller)
-end
+end;
--- scripts/zones/Horlais_Peak/mobs/Helltail_Harry.lua
+++ scripts/zones/Horlais_Peak/mobs/Helltail_Harry.lua
@@ -4,5 +4,21 @@
 -- BCNM: Tails of Woe
 -----------------------------------
 
+require("scripts/globals/status")
+
+
+function onMobInitialize(mob)
+      mob:setMobMod(tpz.mobMod.NO_DROPS, 1)
+  
+end;
+
+function onMobSpawn(mob)
+    mob:setMod(tpz.mod.SLEEPRES,1000)--needs to be set as full immunity but worried that setting it will knock out chance of lullaby 
+    mob:setMod(tpz.mod.SILENCERES,900)
+    mob:setMod(tpz.mod.LULLABYRES, 700);--this is for lullaby since he can be slept with it, just not easily
+    mob:setMobMod(tpz.mobMod.CHARMABLE, 0)
+end;
+
 function onMobDeath(mob, player, isKiller)
-end
+
+end;
