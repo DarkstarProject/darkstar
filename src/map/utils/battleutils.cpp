b55281ef25b8a8821beb273de570afd66a16fb57
Autkast <autkast33@gmail.com>
2020-02-18 07:09:05 -0700

    Fix IncrediblyEasyPrey typo

--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -3867,7 +3867,7 @@ namespace battleutils
                 CharmTime = 1800000;
                 break;
 
-            case EMobDifficulty::IncredibyEasyPrey:
+            case EMobDifficulty::IncrediblyEasyPrey:
             case EMobDifficulty::EasyPrey:
                 CharmTime = 1200000;
                 break;
@@ -4025,7 +4025,7 @@ namespace battleutils
         case EMobDifficulty::TooWeak:
             charmChance = 90.f;
             break;
-        case EMobDifficulty::IncredibyEasyPrey:
+        case EMobDifficulty::IncrediblyEasyPrey:
         case EMobDifficulty::EasyPrey:
             charmChance = 75.f;
             break;
@@ -4378,7 +4378,7 @@ namespace battleutils
             switch (mobCheck)
             {
             case EMobDifficulty::TooWeak:
-            case EMobDifficulty::IncredibyEasyPrey:
+            case EMobDifficulty::IncrediblyEasyPrey:
                 BindBreakChance = 10;
                 break;
 
