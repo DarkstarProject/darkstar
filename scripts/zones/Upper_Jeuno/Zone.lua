7bb2c402b73024786365f8700447bea8d425e202
TeoTwawki <arawn.annwn@yandex.com>
2020-02-20 19:06:08 -0500

    Kill semicolon

--- scripts/zones/Upper_Jeuno/Zone.lua
+++ scripts/zones/Upper_Jeuno/Zone.lua
@@ -11,49 +11,49 @@ require("scripts/globals/chocobo")
 
 function onInitialize(zone)
     tpz.chocobo.initZone(zone)
-end;
+end
 
 function onZoneIn(player,prevZone)
-    local cs = -1;
-    local month = tonumber(os.date("%m"));
-    local day = tonumber(os.date("%d"));
+    local cs = -1
+    local month = tonumber(os.date("%m"))
+    local day = tonumber(os.date("%d"))
     -- Retail start/end dates vary, I am going with Dec 5th through Jan 5th.
     if ((month == 12 and day >= 5) or (month == 1 and day <= 5)) then
-        player:ChangeMusic(0,239);
-        player:ChangeMusic(1,239);
+        player:ChangeMusic(0,239)
+        player:ChangeMusic(1,239)
         -- No need for an 'else' to change it back outside these dates as a re-zone will handle that.
     end
 
     -- COP mission 1-1
     if (player:getCurrentMission(COP) == tpz.mission.id.cop.THE_RITES_OF_LIFE and player:getCharVar("PromathiaStatus") == 0) then
-        cs = 2;
+        cs = 2
     -- MOG HOUSE EXIT
     elseif player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
-        player:setPos(46.2,-5,-78,172);
+        player:setPos(46.2,-5,-78,172)
         if player:getMainJob() ~= player:getCharVar("PlayerMainJob") and player:getGMLevel() == 0 then
-            cs = 30004;
+            cs = 30004
         end
-        player:setCharVar("PlayerMainJob",0);
+        player:setCharVar("PlayerMainJob",0)
     end
 
-    return cs;
-end;
+    return cs
+end
 
 function onConquestUpdate(zone, updatetype)
     tpz.conq.onConquestUpdate(zone, updatetype)
-end;
+end
 
 function onRegionEnter(player,region)
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
     if (csid == 30004 and option == 0) then
-        player:setHomePoint();
-        player:messageSpecial(ID.text.HOMEPOINT_SET);
+        player:setHomePoint()
+        player:messageSpecial(ID.text.HOMEPOINT_SET)
     elseif (csid == 2) then
-        player:setCharVar("PromathiaStatus",1);
+        player:setCharVar("PromathiaStatus",1)
     end
-end;
+end
--- scripts/zones/Windurst_Walls/Zone.lua
+++ scripts/zones/Windurst_Walls/Zone.lua
@@ -13,68 +13,68 @@ require("scripts/globals/zone")
 -----------------------------------
 
 function onInitialize(zone)
-    zone:registerRegion(1, -2,-17,140, 2,-16,142);
-end;
+    zone:registerRegion(1, -2,-17,140, 2,-16,142)
+end
 
 function onZoneIn(player,prevZone)
-    local cs = -1;
+    local cs = -1
     -- MOG HOUSE EXIT
     if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
-        position = math.random(1,5) - 123;
-        player:setPos(-257.5,-5.05,position,0);
+        position = math.random(1,5) - 123
+        player:setPos(-257.5,-5.05,position,0)
         if player:getMainJob() ~= player:getCharVar("PlayerMainJob") and player:getGMLevel() == 0 then
-            cs = 30004;
+            cs = 30004
         end
-        player:setCharVar("PlayerMainJob",0);
+        player:setCharVar("PlayerMainJob",0)
     elseif (ENABLE_ASA == 1 and player:getCurrentMission(ASA) == tpz.mission.id.asa.A_SHANTOTTO_ASCENSION
         and (prevZone == tpz.zone.WINDURST_WATERS or prevZone == tpz.zone.WINDURST_WOODS) and player:getMainLvl()>=10) then
-        cs = 510;
+        cs = 510
     elseif (player:getCurrentMission(WINDURST) == tpz.mission.id.windurst.MOON_READING and player:getCharVar("MissionStatus") == 4) then
-        cs = 443;
+        cs = 443
     end
 
-    return cs;
-end;
+    return cs
+end
 
 function onConquestUpdate(zone, updatetype)
     tpz.conq.onConquestUpdate(zone, updatetype)
-end;
+end
 
 function onRegionEnter(player,region)
     switch (region:GetRegionID()): caseof
     {
         [1] = function (x)  -- Heaven's Tower enter portal
-            player:startEvent(86);
+            player:startEvent(86)
         end,
     }
-end;
+end
 
 function onRegionLeave(player,region)
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
     if (csid == 86) then
-        player:setPos(0,0,-22.40,192,242);
+        player:setPos(0,0,-22.40,192,242)
     elseif (csid == 30004 and option == 0) then
-        player:setHomePoint();
-        player:messageSpecial(ID.text.HOMEPOINT_SET);
+        player:setHomePoint()
+        player:messageSpecial(ID.text.HOMEPOINT_SET)
     elseif (csid == 510) then
-        player:startEvent(514);
+        player:startEvent(514)
     elseif (csid == 514) then
-        player:completeMission(ASA,tpz.mission.id.asa.A_SHANTOTTO_ASCENSION);
-        player:addMission(ASA,tpz.mission.id.asa.BURGEONING_DREAD);
-        player:setCharVar("ASA_Status",0);
+        player:completeMission(ASA,tpz.mission.id.asa.A_SHANTOTTO_ASCENSION)
+        player:addMission(ASA,tpz.mission.id.asa.BURGEONING_DREAD)
+        player:setCharVar("ASA_Status",0)
     elseif (csid == 443) then
-        player:completeMission(WINDURST,tpz.mission.id.windurst.MOON_READING);
-        player:setCharVar("MissionStatus",0);
-        player:setRank(10);
-        player:addGil(GIL_RATE*100000);
-        player:messageSpecial(ID.text.GIL_OBTAINED,GIL_RATE*100000);
-        player:addItem(183);
-        player:messageSpecial(ID.text.ITEM_OBTAINED,183);
-        player:addTitle(tpz.title.VESTAL_CHAMBERLAIN);
+        player:completeMission(WINDURST,tpz.mission.id.windurst.MOON_READING)
+        player:setCharVar("MissionStatus",0)
+        player:setRank(10)
+        player:addGil(GIL_RATE*100000)
+        player:messageSpecial(ID.text.GIL_OBTAINED,GIL_RATE*100000)
+        player:addItem(183)
+        player:messageSpecial(ID.text.ITEM_OBTAINED,183)
+        player:addTitle(tpz.title.VESTAL_CHAMBERLAIN)
     end
-end;
+end
