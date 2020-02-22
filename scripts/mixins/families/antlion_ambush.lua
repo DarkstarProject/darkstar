9ad4a60a021dd541ffed430aebc7d5b614eda7c4
MrHappyAsthma <mrhappyasthma@gmail.com>
2020-02-21 19:20:41 -0800

    Bulk remove semicolons from Lua scripts/mixins.
    Command applied:
    find . -type f -name "*.lua" | xargs -d '\n' sed -i 's/;//g'
    
    Semicolons are not appropriate in Lua per the style guidelines:
    https://github.com/project-topaz/topaz/blob/master/CONTRIBUTING.md

--- scripts/mixins/families/antlion_ambush.lua
+++ scripts/mixins/families/antlion_ambush.lua
@@ -7,19 +7,19 @@ g_mixins.families = g_mixins.families or {}
 
 g_mixins.families.antlion_ambush = function(mob)
     mob:addListener("SPAWN", "ANTLION_AMBUSH_SPAWN", function(mob)
-        mob:hideName(true);
-        mob:untargetable(true);
-        mob:AnimationSub(0);
-        mob:wait(2000);
+        mob:hideName(true)
+        mob:untargetable(true)
+        mob:AnimationSub(0)
+        mob:wait(2000)
     end)
     mob:addListener("ENGAGE", "ANTLION_AMBUSH_ENGAGE", function(mob, target)
-        mob:useMobAbility(278); -- Pit Ambush
+        mob:useMobAbility(278) -- Pit Ambush
     end)
     mob:addListener("DISENGAGE", "ANTLION_AMBUSH_DISENGAGE", function(mob)
-        mob:hideName(true);
-        mob:untargetable(true);
-        mob:AnimationSub(0);
-        mob:wait(2000);
+        mob:hideName(true)
+        mob:untargetable(true)
+        mob:AnimationSub(0)
+        mob:wait(2000)
     end)
 end
 
--- scripts/mixins/families/gears.lua
+++ scripts/mixins/families/gears.lua
@@ -7,30 +7,30 @@ g_mixins = g_mixins or {}
 g_mixins.gears = function(mob)
 
     mob:addListener("COMBAT_TICK", "GEARS_CTICK", function(mob)
-        local mobHPP = mob:getHPP();
+        local mobHPP = mob:getHPP()
         if (mobHPP >= 26 and mobHPP <= 49) then
             if mob:AnimationSub() ~= 1 then
-                mob:AnimationSub(1); -- double gear
+                mob:AnimationSub(1) -- double gear
                 mob:setMobMod(tpz.mobMod.SKILL_LIST, 151)
             end
         if mob:getLocalVar("Def1") == 0 then
-            mob:delMod(tpz.mod.MDEF, 10);
-            mob:delMod(tpz.mod.DEF, 20);
-            mob:setLocalVar("Def1",1);
+            mob:delMod(tpz.mod.MDEF, 10)
+            mob:delMod(tpz.mod.DEF, 20)
+            mob:setLocalVar("Def1",1)
         end
         elseif mobHPP <= 25 then
             if mob:AnimationSub() ~= 2 then
-                mob:AnimationSub(2); -- single gear
+                mob:AnimationSub(2) -- single gear
                 mob:setMobMod(tpz.mobMod.SKILL_LIST, 152)
             end
             if mob:getLocalVar("Def2") == 0 then
-                mob:delMod(tpz.mod.MDEF, 10);
-                mob:delMod(tpz.mod.DEF, 20);
-                mob:setLocalVar("Def2",1);
+                mob:delMod(tpz.mod.MDEF, 10)
+                mob:delMod(tpz.mod.DEF, 20)
+                mob:setLocalVar("Def2",1)
             end
         elseif mobHPP > 50 then
             if mob:AnimationSub() ~= 0 then
-                mob:AnimationSub(0); -- tripple gear
+                mob:AnimationSub(0) -- tripple gear
                 mob:setMobMod(tpz.mobMod.SKILL_LIST, 150)
             end
         end
